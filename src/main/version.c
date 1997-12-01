53f4e7a3a0ac1aa7962250e2844a997288dc09f6
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
1997-12-01 22:12:22 +0000

    PrintGreeting: help()
    git-svn-id: https://svn.r-project.org/R/trunk@330 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/version.c
+++ src/main/version.c
@@ -25,8 +25,10 @@ void PrintGreeting(void)
 	Rprintf("Version %s.%s %s (%s %s, %s)\n\n", R_MAJOR, R_MINOR, R_STATUS, R_MONTH, R_DAY, R_YEAR);
 	Rprintf("R is free software and comes with ABSOLUTELY NO WARRANTY.\n");
 	Rprintf("You are welcome to redistribute it under certain conditions.\n");
-	Rprintf("Type\t\"license()\" for details."
-		"\n\nType\t\"demo()\" for some demos, or\n\t\"help.start()\" for a HTML browser with on-line help.\n\n");
+	Rprintf("Type\t\"license()\" for details.\n\n"
+		"Type\t\"demo()\" for some demos,"
+		" \"help()\" for on-line help, or\n\t\"help.start()\""
+		" for a HTML browser interface to help.\n\n");
 }
 
 SEXP do_version(SEXP call, SEXP op, SEXP args, SEXP env)
