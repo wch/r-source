70ab119767dd85c8fb73e2bb488c822a38e1eb2f
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
1998-01-08 18:33:42 +0000

    new "include.lowest=.." argument to hist(.), cut(.) and underlying C code
    git-svn-id: https://svn.r-project.org/R/trunk@524 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/appl/binning.c
+++ src/appl/binning.c
@@ -1,5 +1,5 @@
 /*
- *  R : A Computer Langage for Statistical Data Analysis
+ *  R : A Computer Language for Statistical Data Analysis
  *  Copyright (C) 1995, 1996  Robert Gentleman and Ross Ihaka
  *
  *  This program is free software; you can redistribute it and/or modify
@@ -18,98 +18,117 @@
  */
 
 #include "Arith.h"
-
-extern int R_NaInteger;
-extern double R_NaReal;
-
+/* instead of #include "Defn.h" : */
 #define NA_INTEGER	R_NaInt
-#define NA_REAL		R_NaReal
 
-/* bincode cuts up the data using half open intervals defined as [a,b)
+
+
+/* bincode  cuts up the data using half open intervals defined as [a,b)
    bincode2 cuts up the data using half open intervals defined as (a,b]
+
+ MM: Shouldn't we afford to collapse these two into one,
+     There would have to be 'n' extra "if" evaluations only ... ?
+
 */
-int bincode(double *x, int *pn, double *breaks, int *pnb, int *code, int *naok)
+int
+bincode(double *x, int *pn, double *breaks, int *pnb, int *code,
+	int *include_border, int *naok)
 {
 	int i, lo, hi;
-	int n, nb, new;
+	int n, nb1, new;
 
 	n = *pn;
-	nb = *pnb;
+	nb1 = *pnb - 1;
 
-	for( i=0 ; i<n ; i++)
-		if(FINITE(x[i])) {
-			lo = 0;
-			hi = nb-1;
-			if(x[i] < breaks[lo] || breaks[hi] <= x[i])
-				code[i] = NA_INTEGER;
-			else {
-				while(hi-lo >= 2) {
-					new = (hi+lo)/2;
-					if(x[i] >= breaks[new])
-						lo = new;
-					else
-						hi = new;
-				}
-				code[i] = lo+1;
+	for(i=0 ; i<n ; i++)
+	 if(FINITE(x[i])) {
+		lo = 0;
+		hi = nb1;
+		if(x[i] <  breaks[lo] || breaks[hi] < x[i] ||
+		   (x[i] == breaks[hi] && ! *include_border))
+			code[i] = NA_INTEGER;
+		else {
+			while(hi-lo >= 2) {
+				new = (hi+lo)/2;
+				if(x[i] >= breaks[new])
+					lo = new;
+				else
+					hi = new;
 			}
+			code[i] = lo+1;
 		}
+	 } else if (! *naok)
+		error("NA's in .C(\"bincode\",... NAOK=FALSE)");
 	return 0;
 }
 
-int bincode2(double *x, int *pn, double *breaks, int *pnb, int *code, int *naok)
+int
+bincode2(double *x, int *pn, double *breaks, int *pnb, int *code,
+	 int *include_border, int *naok)
 {
-        int i, lo, hi;
-        int n, nb, new;
+	int i, lo, hi;
+	int n, nb1, new;
 
-        n = *pn;
-        nb = *pnb;
+	n = *pn;
+	nb1 = *pnb - 1;
 
-        for( i=0 ; i<n ; i++)
-                if(FINITE(x[i])) {
-                        lo = 0;
-                        hi = nb-1;
-                        if(x[i] <= breaks[lo] || breaks[hi] < x[i])
-                                code[i] = NA_INTEGER;
-                        else {
-                                while(hi-lo >= 2) {
-                                        new = (hi+lo)/2;
-                                        if(x[i] > breaks[new])
-                                                lo = new;
-                                        else
-                                                hi = new;
-                                }
-                                code[i] = lo+1;
-                        }
-                }
-        return 0;
+	for(i=0 ; i<n ; i++)
+	 if(FINITE(x[i])) {
+		lo = 0;
+		hi = nb1;
+		if(x[i] <  breaks[lo] || breaks[hi] < x[i] ||
+		   (x[i] == breaks[lo] && ! *include_border))
+			code[i] = NA_INTEGER;
+		else {
+			while(hi-lo >= 2) {
+				new = (hi+lo)/2;
+				if(x[i] > breaks[new])
+					lo = new;
+				else
+					hi = new;
+			}
+			code[i] = lo+1;
+		}
+	 } else if (! *naok)
+		error("NA's in .C(\"bincode\",... NAOK=FALSE)");
+	return 0;
 }
 
 
-int bincount(double *x, int *pn, double *breaks, int *pnb, int *count, int *naok)
+/* bincount is called by  hist(.)  [only]
+ *
+ * bincount *counts* like bincode2, i.e. half open intervals defined as (a,b]
+ */
+int
+bincount(double *x, int *pn, double *breaks, int *pnb, int *count,
+	 int *include_border, int *naok)
 {
 	int i, lo, hi;
-	int n, nb, new;
+	int n, nb1, new;
 
 	n = *pn;
-	nb = *pnb;
+	nb1 = *pnb - 1;
 
-	for( i=0 ; i<nb-1 ; i++)
+	for(i=0; i<nb1; i++)
 		count[i] = 0;
 
-	for( i=0 ; i<n ; i++)
-		if(FINITE(x[i])) {
-			lo = 0;
-			hi = nb-1;
-			if(breaks[lo] <= x[i] && x[i] < breaks[hi]) {
-				while(hi-lo >= 2) {
-					new = (hi+lo)/2;
-					if(x[i] >= breaks[new])
-						lo = new;
-					else
-						hi = new;
-				}
-				count[lo] += 1;
+	for(i=0 ; i<n ; i++)
+	 if(FINITE(x[i])) {
+		lo = 0;
+		hi = nb1;
+		if(breaks[lo] <= x[i] &&
+		   (x[i] < breaks[hi] ||
+		    (x[i]==breaks[hi] && *include_border))) {
+			while(hi-lo >= 2) {
+				new = (hi+lo)/2;
+				if(x[i] > breaks[new])
+					lo = new;
+				else
+					hi = new;
 			}
+			count[lo] += 1;
 		}
+	 } else if (! *naok)
+		error("NA's in .C(\"bincode\",... NAOK=FALSE)");
 	return 0;
 }
