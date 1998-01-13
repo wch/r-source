7450705607fb3874fca472870d66d09151962c64
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
1998-01-13 15:00:08 +0000

    comments only
    git-svn-id: https://svn.r-project.org/R/trunk@564 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/plot.c
+++ src/main/plot.c
@@ -1,5 +1,5 @@
 /*
- *  R : A Computer Langage for Statistical Data Analysis
+ *  R : A Computer Language for Statistical Data Analysis
  *  Copyright (C) 1995, 1996  Robert Gentleman and Ross Ihaka
  *
  *  This program is free software; you can redistribute it and/or modify
@@ -104,7 +104,7 @@ SEXP do_devoff(SEXP call, SEXP op, SEXP args, SEXP env)
 	return R_NilValue;
 }
 
-void Specify2(char*, SEXP);
+void Specify2(char*, SEXP);/* from ./par.c */
 
 void ProcessInlinePars(SEXP s)
 {
@@ -274,8 +274,7 @@ SEXP FixupCex(SEXP cex)
 	return ans;
 }
 
-	/*  plot.new(ask)  */
-	/*  create a new plot  */
+	/*  plot.new(ask)  --  create a new plot  */
 
 SEXP do_plot_new(SEXP call, SEXP op, SEXP args, SEXP env)
 {
@@ -299,8 +298,7 @@ SEXP do_plot_new(SEXP call, SEXP op, SEXP args, SEXP env)
 }
 
 
-	/*  plot.window(xlim, ylim, log) */
-	/*  define world coordinates  */
+	/*  plot.window(xlim, ylim, log, ...) --  define world coordinates  */
 
 SEXP do_plot_window(SEXP call, SEXP op, SEXP args, SEXP env)
 {
