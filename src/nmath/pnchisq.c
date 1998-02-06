4c5d6eb07ef7bed22ce86c5035f86296210a1e09
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
1998-02-06 17:52:54 +0000

    "fix" for overflow; (still "bad")
    git-svn-id: https://svn.r-project.org/R/trunk@688 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/nmath/pnchisq.c
+++ src/nmath/pnchisq.c
@@ -1,5 +1,6 @@
 /*
- *  algorithm as 275 appl.statist. (1992), vol.41, no.2
+ *  Algorithm AS 275 appl.statist. (1992), vol.41, no.2
+ *
  *  computes the noncentral chi-square distribution function with
  *  positive real degrees of freedom f and nonnegative noncentrality
  *  parameter theta
@@ -10,15 +11,27 @@
 #define TRUE	1
 #define FALSE	0
 
+/*----------- DEBUGGING -------------*/
+/* #define DEBUG
+/* rather use  make CFLAGS='-DDEBUG -g -I/usr/local/include -I../include'
+
+ * -- Feb.1, 1998 (R 0.62 alpha); M.Maechler:  still have
+ - INFINITE loop \
+ - bad precision / in some cases
+
+ */
+#ifdef DEBUG
+# include <stdio.h>
+#endif
+
 double pnchisq(double x, double f, double theta)
 {
-    double ans, lam, n, u, v, x2, f2, t, term, bound;
-    int flag;
+    double ans, lam, u, v, x2, f2, t, term, bound, twon;
+    int n, flag;
 
     static double errmax = 1e-12;
     static double zero = 0;
-    static double one = 1;
-    static double two = 2;
+    static double half = 0.5;
     static int itrmax = 100;
 
 #ifdef IEEE_754
@@ -41,56 +54,73 @@ double pnchisq(double x, double f, double theta)
 	return 1;
 #endif
 
-    lam = theta / two;
+
+    lam = theta * half;
+#ifdef DEBUG
+	  printf("pnchisq(x=%12g, f=%12g, theta=%12g):\n",x,f,theta);
+#endif
 
     /* evaluate the first term */
 
-    n = one;
-    u = exp(-lam);
-    v = u;
-    x2 = x / two;
-    f2 = f / two;
-    t = pow(x2, f2) * exp(-x2) / exp(lgamma((f2 + one)));
+    v = u = exp(-lam);
+    x2 = x * half;
+    f2 = f * half;
+    /* The following overflows very soon, eg. x=f=150
+       t = pow(x2, f2) * exp(-x2) / exp(lgamma((f2 + 1))); */
+    t = exp(f2*log(x2) -x2 - lgamma(f2 + 1));
 
     /* there is no need to test ifault si */
-    /* already been checked */
+    /* already been checked */ /*^^^^^^^^ ?????? */
 
     term = v * t;
     ans = term;
+#ifdef DEBUG
+    printf("\t v=exp(-th/2)=%12g, x/2=%12g, f/2=%12g ==> t=%12g\n",v,x2,f2,t);
+#endif
 
     /* check if (f+2n) is greater than x */
 
     flag = FALSE;
+    n = 1; twon = n*2;
     for(;;) {
-	if (f + two * n - x > zero) {
+#ifdef DEBUG
+    printf(" _OL_: n=%d",n);
+#endif
+	if (f + twon - x > zero) {
 
-	    /* find the error bound and */
-	    /* check for convergence */
-
+	    /* find the error bound and check for convergence */
 	    flag = TRUE;
 	    goto L10;
 	}
 	for(;;) {
-
+#ifdef DEBUG
+    printf(" il: n=%d",n);
+#endif
 	    /* evaluate the next term of the */
 	    /* expansion and then the partial sum */
 
-	    u = u * lam / n;
-	    v = v + u;
-	    t = t * x / (f + two * n);
+	    u *= lam / n;
+	    v += u;
+	    t *= x / (f + twon);
 	    term = v * t;
-	    ans = ans + term;
-	    n = n + one;
+	    ans += term;
+	    n++; twon = n*2;
 	    if (!flag)
 		break;
 	  L10:
-	    bound = t * x / (f + two * n - x);
-	    if (bound <= errmax || (int)n > itrmax)
-		goto L20;
+	    bound = t * x / (f + twon - x);
+#ifdef DEBUG
+    printf("\tL10: n=%d; term=%12g; bound=%12g\n",n,term,bound);
+#endif
+	    if (bound <= errmax || n > itrmax)
+		goto L_End;
 	}
     }
-  L20:
+  L_End:
     if (bound > errmax)
 	ML_ERROR(ME_PRECISION);
+#ifdef DEBUG
+    printf("\tL_End: n=%d; term=%12g; bound=%12g\n",n,term,bound);
+#endif
     return ans;
 }
