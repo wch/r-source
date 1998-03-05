5db9e32f5e436914f135becd5b0cc7ca2e1cae73
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
1998-03-05 11:08:27 +0000

    added sign.c
    git-svn-id: https://svn.r-project.org/R/trunk@826 00db46b3-68df-0310-9c12-caf00c1e9a41

---
+++ src/nmath/sign.c
@@ -0,0 +1,42 @@
+/*
+ *  Mathlib : A C Library of Special Functions
+ *  Copyright (C) 1998 Ross Ihaka
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ *  SYNOPSIS
+ *
+ *    #include "Mathlib.h"
+ *    double sign(double x);
+ *
+ *  DESCRIPTION
+ *
+ *    This function computes the  'signum(.)' function:
+ *
+ *    	sign(x) =  1  if x > 0
+ *    	sign(x) =  0  if x == 0
+ *    	sign(x) = -1  if x < 0
+ */
+
+#include "Mathlib.h"
+
+double sign(double x)
+{
+#ifdef IEEE_754
+    if (ISNAN(x))
+	return x;
+#endif
+    return ((x > 0) ? 1 : ((x == 0)? 0 : -1));
+}
