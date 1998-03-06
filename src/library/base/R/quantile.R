82c418ba5bab0aa173e2887eafd4da22a64cd840
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
1998-03-06 13:27:43 +0000

    quantile.default() should now work with Inf's as well.
    git-svn-id: https://svn.r-project.org/R/trunk@861 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/quantile.R
+++ src/library/base/R/quantile.R
@@ -1,25 +1,27 @@
 quantile <- function(x, ...) UseMethod("quantile")
 
-quantile.default <- function (x, probs = seq(0, 1, 0.25), na.rm = FALSE)
-{
-	if (na.rm)
-		x <- x[!is.na(x)]
-	else if (any(!is.finite(x)))
-	 stop("Missing values, NaN\'s and Inf\'s not allowed if na.rm=FALSE")
-	n <- length(x)
-	if (any(probs < 0 | probs > 1))
-		stop("probs outside [0,1]")
-	qs <- if(n > 0) {
-		index <- 1 + (n - 1) * probs
-		lo <- floor(index)
-		hi <- ceiling(index)
-		x <- sort(x, partial = unique(c(lo, hi)))
-		x[lo] + (x[hi] - x[lo]) * (index - lo)
-	} else {#-- n = 0
-		rep(as.numeric(NA), length(probs))
-	}
-	names(qs) <- paste(formatC(100 * probs, format= "fg", wid=1,
-				   dig= max(2,.Options$digits)),
-			   "%", sep = "")
-	qs
+quantile.default <-
+function (x, probs = seq(0, 1, 0.25), na.rm = FALSE) {
+  if (na.rm)
+    x <- x[!is.na(x)]
+  else if (any(is.na(x)))
+    stop("Missing values and NaN's not allowed if `na.rm' is FALSE")
+  n <- length(x)
+  if (any(probs < 0 | probs > 1))
+    stop("probs outside [0,1]")
+  if (n > 0) {
+    index <- 1 + (n - 1) * probs
+    lo <- floor(index)
+    hi <- ceiling(index)
+    x <- sort(x, partial = unique(c(lo, hi)))
+    i <- (index > lo)
+    qs <- x[lo]
+    qs[i] <- qs[i] + (x[hi[i]] - x[lo[i]]) * (index[i] - lo[i])
+  } else {
+    qs <- rep(as.numeric(NA), length(probs))
+  }
+  names(qs) <- paste(formatC(100 * probs, format = "fg", wid = 1,
+                             dig = max(2,.Options$digits)),
+                     "%", sep = "")
+  qs
 }
