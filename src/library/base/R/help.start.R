747a2c70e30fefa246777f7887b74ce7e0ffa119
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
1998-03-19 17:13:04 +0000

    new directory structure
    git-svn-id: https://svn.r-project.org/R/trunk@937 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/help.start.R
+++ src/library/base/R/help.start.R
@@ -1,8 +1,9 @@
-help.start <- function(gui = "irrelevant", browser="netscape")
+help.start <- function (gui = "irrelevant", browser = "netscape", remote = NULL)
 {
-  file <- "$RHOME/doc/html/index.html"
-  cat("If",browser," is already running,	it is *not* restarted,\n",
-      "and you must switch to its window.\nOtherwise, be patient..\n")
-  system(paste(browser, " -remote \"openURL(", file,
-               ")\" 2>/dev/null || ", browser, " ", file, " &", sep = ""))
+ url <- paste(if (is.null(remote)) "$RHOME" else remote,
+              "/doc/html/index.html", sep = "")
+ cat("If", browser, " is already running,\tit is *not* restarted,\n",
+     "and you must switch to its window.\nOtherwise, be patient..\n")
+ system(paste(browser, " -remote \"openURL(", url, ")\" 2>/dev/null || ",
+              browser, " ", url, " &", sep = ""))
 }
