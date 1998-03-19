8dc5112d2b90d35b757182c010726f8318febe10
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
1998-03-19 17:09:51 +0000

    new   finite=FALSE  argument
    git-svn-id: https://svn.r-project.org/R/trunk@931 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/range.R
+++ src/library/base/R/range.R
@@ -1,2 +1,3 @@
-range <- function(..., na.rm=FALSE)
-c(min(..., na.rm=na.rm),max(..., na.rm=na.rm))
+range <- function(..., na.rm=FALSE, finite=FALSE)
+	if(finite) { x <- c(...); x <- x[is.finite(x)]; c(min(x), max(x))
+	} else c(min(..., na.rm=na.rm),max(..., na.rm=na.rm))
