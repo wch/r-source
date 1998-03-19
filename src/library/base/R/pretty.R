c01613b1dbaa6f30374c9c96993be3e3c1936f80
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
1998-03-19 17:11:13 +0000

    upgrade (as in 0.61.2)
    git-svn-id: https://svn.r-project.org/R/trunk@934 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/pretty.R
+++ src/library/base/R/pretty.R
@@ -5,6 +5,8 @@ pretty <- function(x, n=5, shrink.sml = 2^-3) {
           	return(x)
 	if(is.na(n <- n[1]) || n < 1)
 		stop("invalid n value")
+	if(!is.numeric(shrink.sml) || shrink.sml <= 1e-8)
+		stop("argument `shrink.sml' must be numeric > 1e-8")
 	z <- .C("pretty",l=min(x),u=max(x),n=as.integer(n),
                 shrink = as.real(shrink.sml))
 	seq(z$l,z$u,length=z$n+1)
