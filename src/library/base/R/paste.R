2e1108615332ddcafdecf2ff835310b86ea14c4f
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
1998-03-24 17:40:29 +0000

    comment has proposal
    git-svn-id: https://svn.r-project.org/R/trunk@973 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/paste.R
+++ src/library/base/R/paste.R
@@ -1,4 +1,4 @@
-paste <- function (..., sep = " ", collapse=NULL) 
+paste <- function (..., sep = " ", collapse=NULL)
 {
         args <- list(...)
 	if(is.null(args)) ""
@@ -7,3 +7,15 @@ paste <- function (..., sep = " ", collapse=NULL)
 		.Internal(paste(args, sep, collapse))
 	}
 }
+##=== Could we extend  paste(.) to (optionally) accept a
+##    2-vector for collapse ?    With the following functionality
+
+##- paste.extra <- function(r, collapse=c(", "," and ")) {
+##-         n <- length(r)
+##-         if(n <= 1) paste(r)
+##-         else
+##-           paste(paste(r[-n],collapse=collapse[1]),
+##-                 r[n], sep=collapse[min(2,length(collapse))])
+##- }
+
+
