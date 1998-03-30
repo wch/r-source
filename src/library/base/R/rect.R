6afd22f4e15cfb7da3cd5b914ba59bc137bedd4a
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
1998-03-30 14:11:56 +0000

    rect(.) indicate proper "par" args
    git-svn-id: https://svn.r-project.org/R/trunk@994 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/rect.R
+++ src/library/base/R/rect.R
@@ -1,11 +1,13 @@
-rect <- function(xleft, ybottom, xright, ytop, col=NULL, border=par("fg"), lty=NULL, xpd=FALSE) {
-	.Internal(rect(
-		as.double(xleft),
-		as.double(ybottom),
-		as.double(xright),
-		as.double(ytop),
-		col=col,
-		border=border,
-		lty=lty,
-		xpd=xpd))
+rect <-
+function(xleft, ybottom, xright, ytop,
+         col=NULL, border=par("fg"), lty=NULL, xpd=FALSE) {
+ .Internal(rect(
+	as.double(xleft),
+	as.double(ybottom),
+	as.double(xright),
+	as.double(ytop),
+	col=col,
+	border=border,
+	lty=lty,
+	xpd=xpd))
 }
