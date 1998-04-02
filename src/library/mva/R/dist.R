1999ce83f55365a30e1cce5d49635c8d239b3cbe
leisch <leisch@00db46b3-68df-0310-9c12-caf00c1e9a41>
1998-04-02 09:15:56 +0000

    added argument diag and upper
    git-svn-id: https://svn.r-project.org/R/trunk@1012 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/mva/R/dist.R
+++ src/library/mva/R/dist.R
@@ -1,5 +1,5 @@
 dist <-
-function(x, method="euclidian")
+function(x, method="euclidian", diag=FALSE, upper=FALSE)
 {
 	method <-  pmatch(method, c("euclidian", "maximum",
 		"manhattan", "canberra", "binary"))
@@ -19,22 +19,10 @@ function(x, method="euclidian")
 		as.integer(method))[[4]]
 	attr(d, "Size") <- nrow(x)
 	attr(d, "Labels") <- dimnames(x)[[1]]
+        attr(d, "Diag") <- diag
+        attr(d, "Upper") <- upper
 	class(d) <- "dist"
 	return(d)
-}
-
-print.dist <-
-function(d)
-{
-	size <- attr(d, "Size")
-	df <- matrix(NA, size, size)
-	df[row(df) > col(df)] <- d
-	labels <- attr(d, "Labels")
-	if(is.null(labels))
-		dimnames(df) <- list(1:size,1:size)
-	else
-		dimnames(df) <- list(labels,labels)
-	print(df[-1,-size], na="")
 }
 
 names.dist <-
@@ -48,3 +36,56 @@ attr(d, "Labels")
 	attr(d, "Labels") <- n
 	d
 }
+
+as.matrix.dist <- function(obj)
+{
+  size <- attr(obj, "Size")
+  df <- matrix(0, size, size)
+  df[row(df) > col(df)] <- obj
+  df <- df + t(df)
+  labels <- attr(obj, "Labels")
+  if(is.null(labels))
+    dimnames(df) <- list(1:size,1:size)
+  else
+    dimnames(df) <- list(labels,labels)
+  
+  df
+}
+
+print.dist <-
+function(obj, diag=NULL, upper=NULL)
+{
+  if(missing(diag)){
+    if(is.null(attr(obj, "Diag")))
+      diag <- FALSE
+    else
+     diag <-  attr(obj, "Diag")
+  }
+  
+  if(missing(upper)){
+    if(is.null(attr(obj, "Upper")))
+      upper <- FALSE
+    else
+      upper <-  attr(obj, "Upper")
+  }
+  
+  size <- attr(obj, "Size")
+  df <- as.matrix(obj)
+  
+  if(!upper) {
+    df[row(df) < col(df)] <- NA
+  }
+
+  if(!diag) {
+    df[row(df) == col(df)] <- NA
+  }
+
+  if(diag || upper) {
+    print(df, na="")
+  }
+  else {
+    print(df[-1,-size], na="")
+  }
+}
+
+
