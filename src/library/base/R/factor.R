17ccea7f884c43bd0ff46aa04ec6dd6cf64eafdf
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
1998-04-09 06:57:53 +0000

    factor( .. ordered=F)
    git-svn-id: https://svn.r-project.org/R/trunk@1056 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/factor.R
+++ src/library/base/R/factor.R
@@ -1,8 +1,7 @@
-"factor" <-
-  function (x, levels = sort(unique(x), na.last = TRUE),
+"factor" <- function (x, levels = sort(unique(x), na.last = TRUE),
 	labels=as.character(levels), exclude = NA, ordered = FALSE)
 {
-  if (length(x) == 0) 
+  if (length(x) == 0)
     return(character(0))
   exclude <- as.vector(exclude, typeof(x))
   levels <- levels[is.na(match(levels, exclude))]
@@ -11,22 +10,14 @@
   attr(f, "levels") <- if (length(labels) == length(levels)) labels
     else if(length(labels) == 1) paste(labels, seq(along = levels), sep = "")
   else stop("invalid labels argument in \"factor\"")
-  if (ordered) 
-    attr(f, "class") <- c("ordered", "factor")
-  else
-    attr(f, "class") <- "factor"
+  attr(f, "class") <- c(if(ordered)"ordered", "factor")
   f
 }
 
 
-"is.factor" <-
-  function(x)
-  inherits(x, "factor")
+"is.factor" <- function(x) inherits(x, "factor")
 
-
-"levels" <-
-  function(x)
-  attr(x, "levels")
+"levels" <- function(x) attr(x, "levels")
 
 "levels<-" <-
   function(x, value) {
@@ -35,30 +26,23 @@
       x
     }
 
-codes <-
-  function(x, ...)
-  UseMethod("codes")
+codes <- function(x, ...) UseMethod("codes")
 
-codes.factor <-
-  function(x)
-  {
-    attributes(x) <- NULL
-    x
-  }
+codes.factor <- function(x)
+{
+  attributes(x) <- NULL
+  x
+}
 
-"as.factor" <-
-  function (x)
-  if (is.factor(x)) x else factor(x)
+"as.factor" <- function (x) if (is.factor(x)) x else factor(x)
 
-
-"as.vector.factor" <-
-  function(x, type="any")
-  {
-    if (type == "any" || type == "character" || type == "logical" || type == "list")
-      as.vector(levels(x)[x], type)
-    else
-      as.vector(unclass(x), type)
-  }
+"as.vector.factor" <- function(x, type="any")
+{
+  if (type== "any" || type== "character" || type == "logical" || type == "list")
+    as.vector(levels(x)[x], type)
+  else
+    as.vector(unclass(x), type)
+}
 
 
 "print.factor" <-
@@ -71,72 +55,68 @@ codes.factor <-
   }
 
 
-"Math.factor" <-
-  function(e1,e2)
-  stop(paste('"',.Generic,'"', " not meaningful for factors", sep=""))
+"Math.factor" <- function(e1,e2)
+	stop(paste('"',.Generic,'"', " not meaningful for factors", sep=""))
 
-"Ops.factor" <-
-  function(e1, e2)
-  {
-    ok <- switch(.Generic, "=="=, "!="=TRUE, FALSE)
-    if (!ok) stop(paste('"',.Generic,'"', " not meaningful for factors", sep=""))
-    nas <- is.na(e1) | is.na(e2)
-    if (nchar(.Method[1])) {
-      l1 <- levels(e1)
-      e1 <- l1[e1]
-    }
-    if (nchar(.Method[2])) {
-      l2 <- levels(e2)
-      e2 <- l2[e2]
-    }
-    if (all(nchar(.Method)) && (length(l1) != length(l2) ||
-                                !all(sort(l2) == sort(l1))))
-      stop("Level sets of factors are different")
-    value <- NextMethod(.Generic)
-    value[nas] <- NA
-    value
+"Ops.factor" <- function(e1, e2)
+{
+  ok <- switch(.Generic, "=="=, "!="=TRUE, FALSE)
+  if (!ok) stop(paste('"',.Generic,'"', " not meaningful for factors", sep=""))
+  nas <- is.na(e1) | is.na(e2)
+  if (nchar(.Method[1])) {
+    l1 <- levels(e1)
+    e1 <- l1[e1]
   }
-
-"[.factor" <-
-  function(x, i)
-  {
-    y <- NextMethod("[")
-    levels(y) <- levels(x)
-    class(y) <- class(x)
-    y
+  if (nchar(.Method[2])) {
+    l2 <- levels(e2)
+    e2 <- l2[e2]
   }
+  if (all(nchar(.Method)) && (length(l1) != length(l2) ||
+                              !all(sort(l2) == sort(l1))))
+    stop("Level sets of factors are different")
+  value <- NextMethod(.Generic)
+  value[nas] <- NA
+  value
+}
 
-"[<-.factor" <-
-  function(x, i, value)
-  {
-    lx <- levels(x)
-    cx <- class(x)
-    nas <- is.na(x)
-    if (is.factor(value))
-      value <- levels(value)[value]
-    m <- match(value, lx)
-    if (any(is.na(m) && !is.na(value)))
-      warning("invalid factor level, NAs generated")
-    class(x) <- NULL
-    x[i] <- m
-    levels(x) <- lx
-    class(x) <- cx
-    x
-  }
+"[.factor" <- function(x, i)
+{
+  y <- NextMethod("[")
+  levels(y) <- levels(x)
+  class(y) <- class(x)
+  y
+}
+
+"[<-.factor" <- function(x, i, value)
+{
+  lx <- levels(x)
+  cx <- class(x)
+  nas <- is.na(x)
+  if (is.factor(value))
+    value <- levels(value)[value]
+  m <- match(value, lx)
+  if (any(is.na(m) && !is.na(value)))
+    warning("invalid factor level, NAs generated")
+  class(x) <- NULL
+  x[i] <- m
+  levels(x) <- lx
+  class(x) <- cx
+  x
+}
 
 
-# ordered factors ...
+## ordered factors ...
 
 "ordered" <-
   function (x, levels = sort(unique(x), na.last = TRUE),
-	    labels=as.character(levels), exclude = NA, ordered = TRUE) 
+	    labels=as.character(levels), exclude = NA)
 {
   if (is.ordered(x)) return(x)
   if (is.factor(x)) {
     class(x) <- c("ordered", class(x))
     return(x)
   }
-  if (length(x) == 0) 
+  if (length(x) == 0)
     return(character(0))
   exclude <- as.vector(exclude, typeof(x))
   levels <- levels[is.na(match(levels, exclude))]
@@ -146,20 +126,13 @@ codes.factor <-
   attr(f, "levels") <- if (length(labels) == length(levels)) labels
     else if(length(labels) == 1) paste(labels, seq(along = levels), sep = "")
   else stop("invalid labels argument in \"ordered\"")
-  if (ordered) 
-    attr(f, "class") <- c("ordered", "factor")
-  else
-    attr(f, "class") <- "factor"
+  attr(f, "class") <- c("ordered", "factor")
   f
 }
 
-"is.ordered" <-
-  function(x)
-  inherits(x, "ordered")
+"is.ordered" <- function(x) inherits(x, "ordered")
 
-"as.ordered" <-
-  function(x)
-  if (is.ordered(x)) x else ordered(x)
+"as.ordered" <- function(x) if (is.ordered(x)) x else ordered(x)
 
 "print.ordered" <-
   function (x, quote=FALSE) {
@@ -170,22 +143,21 @@ codes.factor <-
   cat("Levels: ",paste(levels(x), collapse=" < "), "\n")
 }
 
-"Ops.ordered" <-
-  function(e1, e2)
-  {
-    nas <- is.na(e1) | is.na(e2)
-    if (nchar(.Method[1])) {
-      l1 <- levels(e1)
-      e1 <- l1[e1]
-    }
-    if (nchar(.Method[2])) {
-      l2 <- levels(e2)
-      e2 <- l2[e2]
-    }
-    if (all(nchar(.Method)) && (length(l1) != length(l2) ||
-                                !all(sort(l2) == sort(l1))))
-      stop("Level sets of factors are different")
-    value <- get(.Generic, mode="function")(e1,e2)
-    value[nas] <- NA
-    value
+"Ops.ordered" <- function(e1, e2)
+{
+  nas <- is.na(e1) | is.na(e2)
+  if (nchar(.Method[1])) {
+    l1 <- levels(e1)
+    e1 <- l1[e1]
   }
+  if (nchar(.Method[2])) {
+    l2 <- levels(e2)
+    e2 <- l2[e2]
+  }
+  if (all(nchar(.Method)) && (length(l1) != length(l2) ||
+                              !all(sort(l2) == sort(l1))))
+    stop("Level sets of factors are different")
+  value <- get(.Generic, mode="function")(e1,e2)
+  value[nas] <- NA
+  value
+}
