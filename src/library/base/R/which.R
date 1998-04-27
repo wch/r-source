497696624e189b3b81d3cacfda42f58b61bcda77
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
1998-04-27 16:37:45 +0000

    which(.) now returns integer
    git-svn-id: https://svn.r-project.org/R/trunk@1136 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/which.R
+++ src/library/base/R/which.R
@@ -1,4 +1,6 @@
+## should return  integer :
 which <- function(x) {
-	if(is.logical(x)) seq(x)[x]
+	if(is.logical(x))
+          if((n <- length(x))) (1:n)[x] else integer(0)
 	else stop("argument to \"which\" is not logical")
 }
