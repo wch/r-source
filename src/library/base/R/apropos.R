2d94ed3e4487376921d37f9746f4ee9ce49a9aae
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
1998-04-27 16:37:10 +0000

    new find(.) function; apropos has  mode="any"
    git-svn-id: https://svn.r-project.org/R/trunk@1134 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/apropos.R
+++ src/library/base/R/apropos.R
@@ -1,11 +1,47 @@
-apropos <- function (pattern, where = FALSE)
+apropos <- function (what, where = FALSE, mode = "any")
 {
-  if (!is.character(pattern))
-    stop("`pattern' must be a character string")
+  if(!is.character(what))
+    what <- as.character(substitute(what))
   x <- character(0)
+  check.mode <- mode != "any"
   for (i in seq(search())) {
-    ll <- length(li <- ls(pos = i, pattern = pattern, all.names = TRUE))
-    if (ll) x <- c(x, if (where) structure(li, names = rep(i, ll)) else li)
+    ll <- length(li <- ls(pos = i, pattern = what, all.names = TRUE))
+    if (ll) {
+        if(check.mode)
+          ll <- length(li <- li[sapply(li, function(x)
+                                       exists(x, where = i,
+                                              mode = mode, inherits=FALSE))])
+	x <- c(x, if (where) structure(li, names = rep(i, ll)) else li)
+    }
   }
   x
 }
+
+find <- function(what, mode = "any", numeric. = FALSE, simple.words=TRUE) {
+ if(!is.character(what))
+	what <- as.character(substitute(what))
+ if(simple.words)
+        what <- gsub("\\.","\\\\.", paste("^",what,"$", sep=""))
+ len.s <- length(sp <- search())
+ ind <- logical(len.s)
+ if((check.mode <- mode != "any"))
+	nam <- character(len.s)
+ for (i in 1:len.s) {
+	ll <- length(li <- ls(pos = i, pattern = what, all.names = TRUE))
+        ind[i] <- ll > 0
+        if(ll >= 2) warning(paste(ll, "occurrences in", sp[i]))
+        if(check.mode && ind[i]) nam[i] <- li[1]
+ }
+ ## found name in  search()[ ind ]
+
+ if(check.mode && any(ind)) {
+          ii <- which(ind)
+          mode.ok <- sapply(ii, function(i) exists(nam[i], where = i,
+						mode = mode, inherits=FALSE))
+          ii <- ii[mode.ok]
+          if(numeric.) structure(ii, names=sp[ii]) else sp[ii]
+ } else {
+          if(numeric.) structure(which(ind), names=sp[ind]) else sp[ind]
+ }
+}
+
