312c335801b0507f3ffc7fb4f1f265d350d48bde
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
1998-04-27 16:36:21 +0000

    objects(<number>) works
    git-svn-id: https://svn.r-project.org/R/trunk@1133 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/attach.R
+++ src/library/base/R/attach.R
@@ -1,4 +1,4 @@
-attach <- function(what, pos=2, name=deparse(substitute(what))) 
+attach <- function(what, pos=2, name=deparse(substitute(what)))
 .Internal(attach(what, pos, name))
 
 detach <- function(name, pos=2)
@@ -15,19 +15,21 @@ detach <- function(name, pos=2)
 }
 
 objects <-
-function (name, pos = -1, envir=pos.to.env(pos), all.names = FALSE, pattern) 
+function (name, pos = -1, envir=pos.to.env(pos), all.names = FALSE, pattern)
 {
-	if (!missing(name)) {
-		name <- substitute(name)
-		if (!is.character(name)) 
+ if (!missing(name)) {
+	 if(!is.numeric(name) || name != (pos <- as.integer(name))) {
+		 name <- substitute(name)
+		 if (!is.character(name))
 			name <- deparse(name)
-		pos <- match(name, search())
-		envir <- pos.to.env(pos)
-	}
-	all.names <- .Internal(ls(envir, all.names))
-	if(!missing(pattern))
-		grep(pattern, all.names, value = TRUE)
-	else all.names
+		 pos <- match(name, search())
+	 }
+	 envir <- pos.to.env(pos)
+ }
+ all.names <- .Internal(ls(envir, all.names))
+ if(!missing(pattern))
+	grep(pattern, all.names, value = TRUE)
+ else all.names
 }
 
-ls <- objects
+ls <- .Alias(objects)
