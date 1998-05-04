b51693e4e64ff8f7896c53a99e4f15259b1660c8
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
1998-05-04 07:38:59 +0000

    added doc; improved error handling, now return num.figures
    git-svn-id: https://svn.r-project.org/R/trunk@1150 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/layout.R
+++ src/library/base/R/layout.R
@@ -1,109 +1,86 @@
+##--- NOTE:
+##    when no device is open, layout() should open the default device,
+## as  par(.) does
+##
+## !!!!
 
-lcm <- function(x) { paste(x, "cm") }
+lcm <- function(x) paste(x, "cm")#-> 3 characters (used in layout!)
 
-layout <- function(mat, 
-		   widths=rep(1, dim(mat)[2]),
-		   heights=rep(1, dim(mat)[1]),
-		   respect=F)
+layout <-
+function(mat, widths=rep(1, dim(mat)[2]),
+	 heights=rep(1, dim(mat)[1]), respect=FALSE)
 {
-	if (is.matrix(mat) &&
-	    (is.logical(respect) || (is.matrix(respect) &&
-				     dim(respect)[1] == dim(mat)[1] &&
-				     dim(respect)[2] == dim(mat)[2])))
-	{
-		num.rows <- dim(mat)[1]
-		num.cols <- dim(mat)[2]
-		num.figures <- max(mat)
-			# check that each value in 1..n is mentioned
-		for (i in 1:num.figures)
-			if (match(i, mat, nomatch=0) == 0)
-				num.figures <- 0
-		if (num.figures > 0)
-		{
-			if (mode(widths) == "character")
-				cm.widths <- grep("cm", widths)
-			else
-				cm.widths <- NULL
-			if (mode(heights) == "character")
-				cm.heights <- grep("cm", heights)
-			else
-				cm.heights <- NULL
-			# pad widths/heights with 1's
-			# and remove "cm" tags
-			if (length(widths) < num.cols)
-				widths <- c(widths, 
-					    rep(1, num.cols-length(widths)))
-			if (mode(widths) == "character")
-			{
-				widths[cm.widths] <- 
-					substring(widths[cm.widths], 1, 
-					          nchar(widths[cm.widths])-3)
-				widths <- as.numeric(widths)
-			}
-			col.widths <- widths
-			if (length(heights) < num.rows)
-				heights <- c(heights, 
-					     rep(1, num.rows-length(heights)))
-			if (mode(heights) == "character")
-			{
-				heights[cm.heights] <- 
-					substring(heights[cm.heights], 1, 
-					          nchar(heights[cm.heights])-3)
-				heights <- as.numeric(heights)
-			}
-			row.heights <- heights
-			if (is.matrix(respect))
-			{
-				respect.mat <- respect
-				respect <- as.integer(2)
-			}
-			else
-			{
-				respect.mat <- matrix(0, num.rows, num.cols)
-				respect <- as.integer(respect)
-			}
-			.Internal(layout(num.rows, num.cols, mat,
-					 as.integer(num.figures), 
-					 col.widths, row.heights,
-					 cm.widths, cm.heights,
-					 respect, respect.mat))
-		}
-		else
-		{
-			cat("Layout matrix must contain at least one reference\n")
-			cat("to each of the values 1..n (n > 0)\n")
-		}
-			
+	storage.mode(mat) <- "integer"
+	mat <- as.matrix(mat) # or barf
+	if(!is.logical(respect)) {
+		respect <- as.matrix(respect)#or barf
+		if(!is.matrix(respect) || any(dim(respect) != dim(mat)))
+		  stop("'respect' must be logical or matrix with same dimension as 'mat'")
 	}
-	else
-	{
-		cat("The first parameter to 'layout' must be a matrix\n")
-		cat("and the 'respect' parameter must be a logical or\n")
-		cat("a matrix\n")
+	num.figures <- as.integer(max(mat))
+	## check that each value in 1..n is mentioned
+	for (i in 1:num.figures)
+	  if (match(i, mat, nomatch=0) == 0)
+	    stop(paste("Layout matrix must contain at least one reference\n",
+		       "  to each of the values {1..n}; here  n = ",
+		       num.figures,"\n", sep=""))
+
+	dm <- dim(mat)
+	num.rows <- dm[1]
+	num.cols <- dm[2]
+
+	cm.widths  <- if (is.character(widths)) grep("cm", widths)
+	cm.heights <- if (is.character(heights))grep("cm", heights)
+
+	## pad widths/heights with 1's	and remove "cm" tags
+	pad1.rm.cm <- function(v, cm.v, len) {
+		if ((ll <- length(v)) < len)
+		  v <- c(v, rep(1, len-ll))
+		if (is.character(v)) {
+			wcm <- v[cm.v]
+			v[cm.v] <- substring(wcm, 1, nchar(wcm)-3)
+		}
+		as.numeric(v)
 	}
+	widths	<- pad1.rm.cm(widths, cm.widths,  len = num.cols)
+	heights <- pad1.rm.cm(heights,cm.heights, len = num.rows)
+
+	if (is.matrix(respect)) {
+		respect.mat <- as.integer(respect)
+		respect <- 2
+	} else {# respect: logical  |--> 0 or 1
+		respect.mat <- matrix(as.integer(0), num.rows, num.cols)
+	}
+	.Internal(layout(num.rows, num.cols,
+			 mat,# integer
+			 as.integer(num.figures),
+			 col.widths = widths,
+			 row.heights = heights,
+			 cm.widths,
+			 cm.heights,
+			 respect = as.integer(respect),
+			 respect.mat))
+	invisible(num.figures)
 }
 
 layout.show <- function(n=1)
 {
-	# show the regions that will be allocated to the next
-	# n figures
+	## show the regions that will be allocated to the next
+	## n figures
 
-	# cheat to make sure that current plot is figure 1
+	## cheat to make sure that current plot is figure 1
 	oma.saved <- par("oma")
 	par(oma=rep(0,4))
 	par(oma=oma.saved)
 
-	mar.saved <- par("mar")
-	par(mar=rep(0,4))
-
-	for (i in 1:n)
-	{
+	o.par <- par(mar=rep(0,4))
+	on.exit(par(o.par))
+	for (i in 1:n) {
 		plot.new()
 		box()
 		text(0.5, 0.5, i)
 	}
 
-	par(mar=mar.saved)
 }
 
 
