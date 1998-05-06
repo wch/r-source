7a41da321752cb9ae4d443d8b657ba0626b72243
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
1998-05-06 16:54:02 +0000

    added "modulus","argument" to complex
    git-svn-id: https://svn.r-project.org/R/trunk@1173 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/vector.R
+++ src/library/base/R/vector.R
@@ -5,6 +5,16 @@ integer <- function(length = 0) vector("integer", length)
 double <- function(length = 0) vector("double", length)
 real <- .Alias(double)
 numeric <- .Alias(double)
-complex <- function(length = 0, real = numeric(), imaginary = numeric())
-.Internal(complex(length, real, imaginary))
+complex <- function(length.out = 0,
+                    real = numeric(), imaginary = numeric(),
+                    modulus = 1, argument = 0) {
+        if(missing(modulus) && missing(argument)) {
+                ## assume 'real' and 'imaginary'
+                .Internal(complex(length.out, real, imaginary))
+        } else {
+                n <- max(length.out, length(argument), length(modulus))
+                rep(modulus,length.out=n) *
+                  exp(1i * rep(argument, length.out=n))
+        }
+}
 
