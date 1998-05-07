da796ac20b2170bb6ff0969e437c4e6e83f4f37f
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
1998-05-07 12:11:56 +0000

    Make solve() generic.
    git-svn-id: https://svn.r-project.org/R/trunk@1178 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/solve.R
+++ src/library/base/R/solve.R
@@ -1,10 +1,10 @@
-qr.solve <- function(a,b, tol = 1e-7)
+qr.solve <- function(a, b, tol = 1e-7)
 {
 	if( !is.qr(a) )
 		a <- qr(a, tol = tol)
 	nc <- ncol(a$qr)
 	if( a$rank != nc )
-		stop("singular matrix 'a' in solve")
+		stop("singular matrix `a' in solve")
 	if( missing(b) ) {
 		if( nc != nrow(a$qr) )
 			stop("only square matrices can be inverted")
@@ -13,5 +13,7 @@ qr.solve <- function(a,b, tol = 1e-7)
 	b<-as.matrix(b)
 	return(qr.coef(a,b))
 }
+
+solve <- function(a, b, ...) UseMethod("solve")
+solve.default <- qr.solve
 solve.qr <- qr.solve
-solve <- qr.solve
