4d8f5510feaebbf8b9bddef627fd16d764cda49c
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
1998-05-22 18:01:05 +0000

    new "n=-1" argument; help file updated
    git-svn-id: https://svn.r-project.org/R/trunk@1270 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/scan.R
+++ src/library/base/R/scan.R
@@ -1,6 +1,14 @@
-scan <- function(file="", what=0, nmax=-1, sep="", skip=0, nlines=0,
-	na.strings="NA", flush=FALSE, strip.white=FALSE, quiet=FALSE) {
-	if( !missing(sep) )
-		na.strings<-c(na.strings,"")
-	.Internal(scan(file, what, nmax, sep, skip, nlines, na.strings,flush,strip.white, quiet))
+scan <-
+function(file="", what= double(0), nmax=-1, n=-1, sep="", skip=0, nlines=0,
+         na.strings="NA", flush=FALSE, strip.white=FALSE, quiet=FALSE) {
+	if(!missing(sep))
+        	na.strings<-c(na.strings,"")
+        if(!missing(n)) {
+          	if(missing(nmax))
+                  	nmax <- n/length(what)
+                else
+                	stop("Either specify 'nmax' or 'n', but not both.")
+        }
+	.Internal(scan(file, what, nmax, sep, skip, nlines,
+                       na.strings,flush,strip.white, quiet))
 }
