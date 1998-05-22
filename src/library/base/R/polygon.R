5a03e02a98a40d10ac7aa9eb7df86f8661e8ceeb
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
1998-05-22 11:53:46 +0000

    New arguments `density' and `angle' (not used yet).
    git-svn-id: https://svn.r-project.org/R/trunk@1264 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/polygon.R
+++ src/library/base/R/polygon.R
@@ -1,6 +1,10 @@
 polygon <-
-function(x, y=NULL, border=par("fg"), ...)
+function(x, y=NULL, density = -1, angle = 45, border=par("fg"), ...)
 {
-	xy <- xy.coords(x, y)
-	.Internal(polygon(xy$x, xy$y, border=border, ...))
+  if (!missing(density))
+    .NotYetUsed("density")
+  if (!missing(angle))
+    .NotYetUsed("angle")
+  xy <- xy.coords(x, y)
+  .Internal(polygon(xy$x, xy$y, border=border, ...))
 }
