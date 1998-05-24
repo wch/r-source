5540ab513de26c9c37665ed48a72293e86286af0
ihaka <ihaka@00db46b3-68df-0310-9c12-caf00c1e9a41>
1998-05-24 22:44:48 +0000

    New (very experimental) persp function.
    git-svn-id: https://svn.r-project.org/R/trunk@1283 00db46b3-68df-0310-9c12-caf00c1e9a41

---
+++ src/library/base/R/persp.R
@@ -0,0 +1,11 @@
+persp <-
+function(z, x, y, theta=0, phi=15, r=sqrt(3), d=1, ...)
+{
+	s <- max(diff(range(z)), diff(range(x)), diff(range(y)))/2
+	h <- d / (d + r - 1)
+	.Internal(persp(
+		(z - mean(range(z))) / s,
+		(x - mean(range(x))) / s,
+		(y - mean(range(y))) / s,
+		c(theta, phi, r, d), ...))
+}
