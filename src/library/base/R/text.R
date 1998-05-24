b994c9b1d2ca440a4c816e225c7add97b3e0ed07
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
1998-05-24 18:39:11 +0000

    Try to make text() take the second pos arg as labels if the first one already gives the y coordinates (BBC).
    git-svn-id: https://svn.r-project.org/R/trunk@1280 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/text.R
+++ src/library/base/R/text.R
@@ -1,6 +1,9 @@
-as.char.or.expr <-
-function(x) if(is.expression(x)) x else unlist(strsplit(as.character(x),"\n"))
+as.char.or.expr <- function(x) {
+  if (is.expression(x)) x else unlist(strsplit(as.character(x), "\n"))
+}
 
 text <-
-function(x, y=NULL, labels = seq(along=x), ...)
-	.Internal(text(xy.coords(x,y), as.char.or.expr(labels), ...))
+function(x, y=NULL, labels = seq(along=x), ...) {
+  if (!missing(y) && !is.numeric(y)) { labels <- y; y <- NULL }
+  .Internal(text(xy.coords(x,y), as.char.or.expr(labels), ...))
+}
