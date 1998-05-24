41c33074640b7bf13d7c4c53c9ac82205acbd7c3
ihaka <ihaka@00db46b3-68df-0310-9c12-caf00c1e9a41>
1998-05-24 23:59:12 +0000

    z,x,y, -> x,y,z
    git-svn-id: https://svn.r-project.org/R/trunk@1285 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/persp.R
+++ src/library/base/R/persp.R
@@ -1,5 +1,5 @@
 persp <-
-function(z, x, y, theta=0, phi=15, r=sqrt(3), d=1, ...)
+function(x, y, z, theta=0, phi=15, r=sqrt(3), d=1, ...)
 {
 	s <- max(diff(range(z)), diff(range(x)), diff(range(y)))/2
 	h <- d / (d + r - 1)
