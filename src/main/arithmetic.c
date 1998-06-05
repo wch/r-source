cf84f815d7bb919eddb9235a99c27aad60baa8cc
ihaka <ihaka@00db46b3-68df-0310-9c12-caf00c1e9a41>
1998-06-05 09:14:58 +0000

    Un-ifdef-ed the ptukey and qtukey functions.
    git-svn-id: https://svn.r-project.org/R/trunk@1350 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/arithmetic.c
+++ src/main/arithmetic.c
@@ -1311,9 +1311,9 @@ SEXP do_math4(SEXP call, SEXP op, SEXP args, SEXP env)
 #endif
 #ifdef UNIMP
     case 10: return math4(op, CAR(args), CADR(args), CADDR(args), CADDDR(args), dtukey);
+#endif
     case 11: return math4(op, CAR(args), CADR(args), CADDR(args), CADDDR(args), ptukey);
     case 12: return math4(op, CAR(args), CADR(args), CADDR(args), CADDDR(args), qtukey);
-#endif
     default:
 	errorcall(lcall, "unimplemented real function\n");
     }
