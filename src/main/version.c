690ce48134453152345cebab553d886396ab1dd9
ihaka <ihaka@00db46b3-68df-0310-9c12-caf00c1e9a41>
1998-06-06 20:51:52 +0000

    Updates to the credits.
    git-svn-id: https://svn.r-project.org/R/trunk@1372 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/version.c
+++ src/main/version.c
@@ -21,12 +21,17 @@
 
 void PrintGreeting(void)
 {
-	Rprintf("\nR : Copyright %s, Robert Gentleman and Ross Ihaka\n", R_YEAR);
-	Rprintf("Version %s.%s %s (%s %s, %s)\n\n", R_MAJOR, R_MINOR, R_STATUS, R_MONTH, R_DAY, R_YEAR);
+	Rprintf("\nR : Copyright %s, The R Development Core Team\n", R_YEAR);
+	Rprintf("Version %s.%s %s (%s %s, %s)\n\n",
+		R_MAJOR, R_MINOR, R_STATUS, R_MONTH, R_DAY, R_YEAR);
+
 	Rprintf("R is free software and comes with ABSOLUTELY NO WARRANTY.\n");
 	Rprintf("You are welcome to redistribute it under certain conditions.\n");
-	Rprintf("Type\t\"license()\" for details.\n\n"
-		"Type\t\"demo()\" for some demos,"
+	Rprintf("Type\t\"?license\" or \"?licence\" for distribution details.\n\n");
+	Rprintf("R is a collaborative project with many contributors.\n"
+		"Type\t?contributors\" for a list.\n\n");
+
+	Rprintf("Type\t\"demo()\" for some demos,"
 		" \"help()\" for on-line help, or\n\t\"help.start()\""
 		" for a HTML browser interface to help.\n\n");
 }
