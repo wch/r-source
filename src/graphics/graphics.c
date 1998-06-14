8d2e6b4beea46e8925bf20ee4aafa5fb6e5b70a2
pd <pd@00db46b3-68df-0310-9c12-caf00c1e9a41>
1998-06-14 12:56:20 +0000

    Last minute release changes: 	- mention R CMD in CHANGES 	- add text on new Makefile structure in INSTALL 	- RedHat systems in PLATFORMS 	- Notes on obsolescence in PROJECTS, TASKS 	- Release date & status 	- Move fort77 down in search order (is f2c driver on RedHat...) 	- One-letter fix affecting coplot in graphics.c
    git-svn-id: https://svn.r-project.org/R/trunk@1446 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/graphics/graphics.c
+++ src/graphics/graphics.c
@@ -2893,7 +2893,7 @@ void GText(double x, double y, int coords, char *str,
       if(!dd->dp.xpd) {
 	double xtest = x;
 	double ytest = y;
-	GConvert(&xtest, &ytest, coords, NFC, dd);
+	GConvert(&xtest, &ytest, coords, NDC, dd);
 	if(xtest < 0 || ytest < 0 ||
 	   xtest > 1 || ytest > 1)
 	  return;
