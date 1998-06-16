dc6a3df6f7517b8e881a88add00c64e2bb508416
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
1998-06-16 12:35:18 +0000

    Made text() generic.
    git-svn-id: https://svn.r-project.org/R/branches/R-0-62-patches@1460 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/text.R
+++ src/library/base/R/text.R
@@ -2,7 +2,9 @@ as.char.or.expr <- function(x) {
   if (is.expression(x)) x else unlist(strsplit(as.character(x), "\n"))
 }
 
-text <- function(x, y = NULL, labels = seq(along = x), adj = NULL, ...) {
+text <- function(x, ...) UseMethod("text")
+text.default <- function(x, y = NULL, labels = seq(along = x), adj =
+                         NULL, ...) { 
   if (!missing(y) && (is.character(y) || is.expression(y))) {
     labels <- y; y <- NULL
   }
