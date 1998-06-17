b1d2b4317f53e2b5d9de1ed05e9adeddabc42168
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
1998-06-17 15:39:39 +0000

    xyinch()
    git-svn-id: https://svn.r-project.org/R/trunk@1473 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/units.R
+++ src/library/base/R/units.R
@@ -1,6 +1,6 @@
 cm <- function(x) 2.54*x
 
-xinch <- function(x=1)
-	x * diff(par("usr")[1:2])/par("pin")[1]
-yinch <- function(x=1)
-	x * diff(par("usr")[3:4])/par("pin")[2]
+xinch <- function(x=1) x * diff(par("usr")[1:2])/par("pin")[1]
+yinch <- function(y=1) y * diff(par("usr")[3:4])/par("pin")[2]
+xyinch <- function(xy=1)
+ { u <- par("usr"); xy * c(u[2]-u[1], u[4]-u[3]) / par("pin") }
