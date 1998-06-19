e3b9f448ddf5ba13bbfeb151db8b6ee10da35fe2
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
1998-06-19 18:07:27 +0000

    ESS "##"
    git-svn-id: https://svn.r-project.org/R/trunk@1510 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/predict.glm.R
+++ src/library/base/R/predict.glm.R
@@ -1,12 +1,11 @@
-# file predict.glm.R
-# copyright (C) 1998 W. N. Venables and B. D. Ripley
-#
+#### copyright (C) 1998 W. N. Venables and B. D. Ripley
+
 predict.glm <-
-function(object, newdata=NULL, type = c("link", "response"), se.fit = F,
+function(object, newdata=NULL, type = c("link", "response"), se.fit = FALSE,
          dispersion = NULL, ...)
 {
-  predict.lm <- 
-  function (object, newdata = model.frame(object), se=F) 
+  predict.lm <-
+  function (object, newdata = model.frame(object), se=FALSE)
   {
     X <- model.matrix(delete.response(terms(object)), newdata)
     n <- NROW(object$qr$qr)
@@ -14,26 +13,25 @@ function(object, newdata=NULL, type = c("link", "response"), se.fit = F,
     p1 <- 1:p
     piv <- object$qr$pivot[p1]
     r <- resid(object)
-    f <- fitted(object)
+    ## f <- fitted(object)
     w <- weights(object)
-    if (is.null(w)) rss <- sum(r^2)
-    else rss <- sum(r^2 * w)
+    rss <- if (is.null(w)) sum(r^2) else sum(r^2 * w)
     R <- chol2inv(object$qr$qr[p1, p1, drop = FALSE])
     est <- object$coefficients[piv]
-    predictor <- c(X[, piv, drop = F] %*% est)
+    predictor <- c(X[, piv, drop = FALSE] %*% est)
     if(se) {
       ip <- real(NROW(X))
-      for (i in (1:NROW(X))) {
+      for (i in 1:NROW(X)) {
         xi <- X[i, piv]
         ip[i] <- xi %*% R %*% xi
       }
       list(fit = predictor, se = sqrt(ip))
     } else predictor
   }
-  
+
   type <- match.arg(type)
   if(!se.fit) {
-    #No standard errors
+    ## No standard errors
     if(missing(newdata))
       pred <- switch(type,
                      link = object$linear.predictors,
@@ -46,7 +44,7 @@ function(object, newdata=NULL, type = c("link", "response"), se.fit = F,
     }
   } else {
     pred <- predict.lm(object, newdata, se.fit)
-    # summary.survreg has no ... argument.
+    ## summary.survreg has no ... argument.
     if(inherits(object, "survreg")) dispersion <- 1.
     if(is.null(dispersion) || dispersion == 0)
       dispersion <- summary(object, dispersion=dispersion)$dispersion
