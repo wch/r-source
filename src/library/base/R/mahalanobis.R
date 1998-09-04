ba04d48a50db0c8c891daf166ef56b9b02a6bc3f
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
1998-09-04 16:21:28 +0000

    ..
    git-svn-id: https://svn.r-project.org/R/trunk@2077 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/mahalanobis.R
+++ src/library/base/R/mahalanobis.R
@@ -1,19 +1,9 @@
 mahalanobis <- function(x, center, cov, inverted=FALSE)
-{  
-
-  if(is.vector(x)){
-    x <- matrix(x, ncol=length(x))
-  }
-  else {
-    x <- as.matrix(x)
-  }
-  
-  x <- sweep(x, 2, center)
-
-  if(!inverted){
+{
+  x <- if(is.vector(x)) matrix(x, ncol=length(x)) else as.matrix(x)
+  x <- sweep(x, 2, center)# = (x - center)
+  if(!inverted)
     cov <- solve(cov)
-  }
-  
   retval <- apply((x%*%cov) * x, 1, sum)
   names(retval) <- rownames(x)
   retval
