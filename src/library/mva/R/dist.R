a338e441eed6e8b7727657f60363a065f89b1da6
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
1998-09-10 17:09:00 +0000

    RE-INDENTING: ALL in  mva
    git-svn-id: https://svn.r-project.org/R/trunk@2111 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/mva/R/dist.R
+++ src/library/mva/R/dist.R
@@ -1,91 +1,91 @@
 dist <-
-function(x, method="euclidian", diag=FALSE, upper=FALSE)
+    function(x, method="euclidian", diag=FALSE, upper=FALSE)
 {
-	method <-  pmatch(method, c("euclidian", "maximum",
-		"manhattan", "canberra", "binary"))
-	if(is.na(method))
-		stop("invalid distance method")
-	if(method == -1)
-		stop("ambiguous distance method")
+    method <-  pmatch(method, c("euclidian", "maximum",
+				"manhattan", "canberra", "binary"))
+    if(is.na(method))
+	stop("invalid distance method")
+    if(method == -1)
+	stop("ambiguous distance method")
 
-	x <- as.matrix(x)
-	len <- nrow(x)*(nrow(x) - 1)/2
+    x <- as.matrix(x)
+    len <- nrow(x)*(nrow(x) - 1)/2
 
-	d <- .C("dist",
-		as.double(x),
-		nrow(x),
-		ncol(x),
-		double(len),
-		as.integer(method))[[4]]
-	attr(d, "Size") <- nrow(x)
-	attr(d, "Labels") <- dimnames(x)[[1]]
-        attr(d, "Diag") <- diag
-        attr(d, "Upper") <- upper
-	class(d) <- "dist"
-	return(d)
+    d <- .C("dist",
+	    as.double(x),
+	    nrow(x),
+	    ncol(x),
+	    double(len),
+	    as.integer(method))[[4]]
+    attr(d, "Size") <- nrow(x)
+    attr(d, "Labels") <- dimnames(x)[[1]]
+    attr(d, "Diag") <- diag
+    attr(d, "Upper") <- upper
+    class(d) <- "dist"
+    return(d)
 }
 
 names.dist <-
-function(d)
-attr(d, "Labels")
+    function(d)
+    attr(d, "Labels")
 
 "names<-.dist" <- function(d, n)
 {
-	if(length(n) != attr(d, "Size"))
-		stop("invalid names for dist object")
-	attr(d, "Labels") <- n
-	d
+    if(length(n) != attr(d, "Size"))
+	stop("invalid names for dist object")
+    attr(d, "Labels") <- n
+    d
 }
 
 as.matrix.dist <- function(obj)
 {
-  size <- attr(obj, "Size")
-  df <- matrix(0, size, size)
-  df[row(df) > col(df)] <- obj
-  df <- df + t(df)
-  labels <- attr(obj, "Labels")
-  if(is.null(labels))
-    dimnames(df) <- list(1:size,1:size)
-  else
-    dimnames(df) <- list(labels,labels)
-  
-  df
+    size <- attr(obj, "Size")
+    df <- matrix(0, size, size)
+    df[row(df) > col(df)] <- obj
+    df <- df + t(df)
+    labels <- attr(obj, "Labels")
+    if(is.null(labels))
+	dimnames(df) <- list(1:size,1:size)
+    else
+	dimnames(df) <- list(labels,labels)
+
+    df
 }
 
 print.dist <-
-function(obj, diag=NULL, upper=NULL)
+    function(obj, diag=NULL, upper=NULL)
 {
-  if(missing(diag)){
-    if(is.null(attr(obj, "Diag")))
-      diag <- FALSE
-    else
-     diag <-  attr(obj, "Diag")
-  }
-  
-  if(missing(upper)){
-    if(is.null(attr(obj, "Upper")))
-      upper <- FALSE
-    else
-      upper <-  attr(obj, "Upper")
-  }
-  
-  size <- attr(obj, "Size")
-  df <- as.matrix(obj)
-  
-  if(!upper) {
-    df[row(df) < col(df)] <- NA
-  }
+    if(missing(diag)){
+	if(is.null(attr(obj, "Diag")))
+	    diag <- FALSE
+	else
+	    diag <-  attr(obj, "Diag")
+    }
 
-  if(!diag) {
-    df[row(df) == col(df)] <- NA
-  }
+    if(missing(upper)){
+	if(is.null(attr(obj, "Upper")))
+	    upper <- FALSE
+	else
+	    upper <-  attr(obj, "Upper")
+    }
 
-  if(diag || upper) {
-    print(df, na="")
-  }
-  else {
-    print(df[-1,-size], na="")
-  }
+    size <- attr(obj, "Size")
+    df <- as.matrix(obj)
+
+    if(!upper) {
+	df[row(df) < col(df)] <- NA
+    }
+
+    if(!diag) {
+	df[row(df) == col(df)] <- NA
+    }
+
+    if(diag || upper) {
+	print(df, na="")
+    }
+    else {
+	print(df[-1,-size], na="")
+    }
 }
 
 
