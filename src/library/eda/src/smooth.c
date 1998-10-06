dce62859c62819d0c91be14f3474af9bfb38ab12
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
1998-10-06 12:56:08 +0000

    -Wall..
    git-svn-id: https://svn.r-project.org/R/trunk@2405 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/eda/src/smooth.c
+++ src/library/eda/src/smooth.c
@@ -79,13 +79,6 @@ static void split(double *x, double *y, int n, double *d)
 	*d = absdiff(x, y, n);
 }
 
-static void a3rs(double *x, double *y, double *z, int n, double *d)
-{
-	a3r(x, z, y, n, d);
-	split(z, y, n, d);
-}
-
-
 static void a3rsr(double *x, double *y, double *z, double *w, int n, double *d)
 {
 	int i;
@@ -100,7 +93,8 @@ static void a3rsr(double *x, double *y, double *z, double *w, int n, double *d)
 	while (*d > 0.0);
 }
 
-tukeysmooth(double *x, double *y, double *z, double *w, int *n, double *d)
+int tukeysmooth(double *x, double *y, double *z, double *w, int *n, double *d)
 {
-	a3rsr(x, y, z, w, *n, d);
+    a3rsr(x, y, z, w, *n, d);
+    return 0;
 }
