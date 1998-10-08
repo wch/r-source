8b1c6f9f52b886aad84cff9fe7b664dde905f36b
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
1998-10-08 13:36:20 +0000

    PrtUtil
    git-svn-id: https://svn.r-project.org/R/trunk@2484 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/nmath/rhyper.c
+++ src/nmath/rhyper.c
@@ -35,7 +35,7 @@
  *    Journal of Statistical Computation and Simulation 22, 127-145.
  */
 
-#include "Defn.h"/* only for  REprintf(.) below  [-Wall] */
+#include "PrtUtil.h"
 #include "Mathlib.h"
 
 /* TRUE and FALSE conflict with the MAC */
