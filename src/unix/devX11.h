1f7e9a6c47b1d8b5f7caf8470a75eec7947ae7bb
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
1998-10-15 19:00:54 +0000

    added suggestion from old devices.c
    git-svn-id: https://svn.r-project.org/R/trunk@2564 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/unix/devX11.h
+++ src/unix/devX11.h
@@ -4,3 +4,6 @@
 int X11ConnectionNumber();
 void ProcessEvents(void);
 int X11DeviceDriver(DevDesc*, char*, double, double, double);
+#ifdef SOON
+int XFigDeviceDriver(char**, int, double*, int);
+#endif
