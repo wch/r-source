ab5ee1f9c171f3e5c088711b0c2cfddcc4a61278
ihaka <ihaka@00db46b3-68df-0310-9c12-caf00c1e9a41>
1998-10-20 02:04:02 +0000

    Another BDR fix for deparsing PP_BINARY2.
    git-svn-id: https://svn.r-project.org/R/trunk@2592 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/deparse.c
+++ src/main/deparse.c
@@ -436,6 +436,10 @@ static void deparse2buff(SEXP s)
 			break;
 		    }
 		}
+		else if (fop == PP_BINARY2) {
+		    if (length(s) != 2)
+			fop = PP_FUNCALL;
+		}
 		switch (fop) {
 		case PP_IF:
 		    print2buff("if (");
