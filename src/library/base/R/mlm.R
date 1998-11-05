a8067eb2e5c6cb9643626ef0f3c24b35b1a71ca1
pd <pd@00db46b3-68df-0310-9c12-caf00c1e9a41>
1998-11-05 20:29:27 +0000

    aov() files from Brian Ripley. Hopefully all of them this time...
    git-svn-id: https://svn.r-project.org/R/trunk@2744 00db46b3-68df-0310-9c12-caf00c1e9a41

---
+++ src/library/base/R/mlm.R
@@ -0,0 +1,42 @@
+#### copyright (C) 1998 B. D. Ripley
+summary.mlm <- function(object, ...)
+{
+  coef <- coef(object)
+  ny <- ncol(coef)
+  if(is.null(ny)) return(NextMethod("summary"))
+  effects <- object$effects
+  resid <- residuals(object)
+  fitted <- fitted(object)
+  ynames <- colnames(coef)
+  if(is.null(ynames)) {
+    lhs <- object$terms[[2]]
+    if(mode(lhs) == "call" && lhs[[1]] == "cbind")
+      ynames <- as.character(lhs)[-1]
+    else ynames <- paste("Y", seq(ny), sep = "")
+  }
+  value <- vector("list", ny)
+  names(value) <- paste("Response", ynames)
+  cl <- class(object)
+  class(object) <- cl[match("mlm", cl):length(cl)][-1]
+  for(i in seq(ny)) {
+    object$coefficients <- coef[, i]
+    object$residuals <- resid[, i]
+    object$fitted.values <- fitted[, i]
+    object$effects <- effects[, i]
+    object$call$formula[[2]] <- object$terms[[2]] <- as.name(ynames[i])
+    value[[i]] <- summary(object, ...)
+  }
+  class(value) <- "listof"
+  value
+}
+
+anova.mlm <- function(...) stop("no anova method implemented for mlm models")
+
+deviance.mlm <- function(object, ...)
+{
+  if(is.null(w <- object$weights)) res <- object$residuals^2
+  else res <- w * object$residuals^2
+  drop(rep(1, nrow(res)) %*% res)
+}
+
+plot.mlm <- function (...) .NotYetImplemented() 
