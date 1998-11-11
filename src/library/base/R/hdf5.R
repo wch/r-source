1f2ba14ffbf01771abec5c94f4e4ecd20f108697
ihaka <ihaka@00db46b3-68df-0310-9c12-caf00c1e9a41>
1998-11-11 19:24:04 +0000

    Updates for HDF
    git-svn-id: https://svn.r-project.org/R/trunk@2806 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/hdf5.R
+++ src/library/base/R/hdf5.R
@@ -1,3 +1,4 @@
-hdf5save <- function(file, ...) .Internal(hdf5save(file, ...))
+# Don't wrap hdf5save yet.  It doesn't work ...
+# hdf5save <- function(file, ...) .Internal(hdf5save(file, ...))
 
 hdf5load <- function(file, load=TRUE) .Internal(hdf5load(file, load=TRUE))
