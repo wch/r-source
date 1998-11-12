66e2e50fdce314cc387043426a8e2576535d58e6
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
1998-11-12 15:39:48 +0000

    col/row names arg: "do.NULL"
    git-svn-id: https://svn.r-project.org/R/trunk@2817 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/matrix.R
+++ src/library/base/R/matrix.R
@@ -2,7 +2,7 @@ matrix <- function(data=NA, nrow=1, ncol=1, byrow=FALSE, dimnames=NULL) {
     if(missing(nrow))	nrow <- ceiling(length(data)/ncol)
     else if(missing(ncol))	ncol <- ceiling(length(data)/nrow)
     x <- .Internal(matrix(data, nrow, ncol, byrow))
-    dimnames(x)<-dimnames
+    dimnames(x) <- dimnames
     x
 }
 nrow <- function(x) dim(x)[1]
@@ -11,31 +11,34 @@ ncol <- function(x) dim(x)[2]
 NROW <- function(x) if(is.array(x)||is.data.frame(x)) nrow(x) else length(x)
 NCOL <- function(x) if(is.array(x)||is.data.frame(x)) ncol(x) else as.integer(1)
 
-rownames <- function(x) {
+rownames <- function(x, do.NULL = TRUE, prefix = "row")
+{
     dn <- dimnames(x)
-    if(is.null(dn)) dn else dn[[1]]
+    if(!is.null(dn[[1]]))
+	dn[[1]]
+    else {
+	if(do.NULL) NULL else paste(prefix, seq(length=NROW(x)), sep="")
+    }
 }
 "rownames<-" <- function(x, value) {
     dn <- dimnames(x)
     dimnames(x) <- list(value, if(!is.null(dn)) dn[[2]])
     x
 }
-colnames <- function(x) {
+colnames <- function(x, do.NULL = TRUE, prefix = "col")
+{
     dn <- dimnames(x)
-    if(is.null(dn)) dn else dn[[2]]
+    if(!is.null(dn[[2]]))
+	dn[[2]]
+    else {
+	if(do.NULL) NULL else paste(prefix, seq(length=NCOL(x)), sep="")
+    }
 }
 "colnames<-" <- function(x, value) {
     dn <- dimnames(x)
     dimnames(x) <- list(if(!is.null(dn)) dn[[1]], value)
     x
 }
-
-COLNAMES <- function(x, prefix = "col")
-    if(is.null(n <- colnames(x)))
-	paste(prefix, seq(length=NCOL(x)), sep="") else n
-ROWNAMES <- function(x, prefix = "row")
-    if(is.null(n <- rownames(x)))
-	paste(prefix, seq(length=NROW(x)), sep="") else n
 
 row <- function(x, as.factor=FALSE) {
     if(as.factor) factor(.Internal(row(x)), labels=rownames(x))
