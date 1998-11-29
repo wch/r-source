7e58bd80ff05ef2efe4d730827f43d07dce512c2
bates <bates@00db46b3-68df-0310-9c12-caf00c1e9a41>
1998-11-29 22:38:02 +0000

    Fixed syntax error in seventh line of open.account function.
    git-svn-id: https://svn.r-project.org/R/trunk@3004 00db46b3-68df-0310-9c12-caf00c1e9a41

--- demos/language/scoping.R
+++ demos/language/scoping.R
@@ -21,7 +21,7 @@ open.account <- function(total) {
 	 deposit = function(amount) {
 	     if(amount <= 0)
 		 stop("Deposits must be positive!\n")
-	     total < <- total + amount
+	     total <- total + amount
 	     cat(amount,"deposited. Your balance is", total, "\n\n")
 	 },
 	 withdraw = function(amount) {
