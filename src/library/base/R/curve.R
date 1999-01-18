f079d158219157649b03c7dfa9e3c095cc895fd2
bates <bates@00db46b3-68df-0310-9c12-caf00c1e9a41>
1999-01-18 14:49:56 +0000

    Revised group function to avoid grep patterns that caused problems in Solaris 2.6
    git-svn-id: https://svn.r-project.org/R/branches/R-0-63-patches@3345 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/curve.R
+++ src/library/base/R/curve.R
@@ -2,29 +2,21 @@ curve <- function(expr, from, to, n=101, add=FALSE, type="l",
                   ylab = NULL, ...)
 {
     sexpr <- substitute(expr)
-    if(is.call(sexpr)) expr <- sexpr
-    expr.t <- paste(deparse(expr), collapse=" ")
-
-    if(!(lx <- length(grep("\\<x\\>", expr.t)))
-       || length(grep(pat <- "^function *\\(x\\) *", expr.t))) {
-        is.f <- is.function(expr)
-        fun <- if(is.f) expr else eval(parse(text=expr.t))
-        if(!(is.f ||is.function(fun)))
-            stop("'expr' must be a function or contain 'x'")
-        if(is.null(ylab))
-            ylab <-
-                if(is.f) paste(deparse(sexpr), "(x)")
-                else if(lx) sub(pat, "", expr.t)# drop "function(x)"
-                else paste(expr.t,"(x)")
+    if(is.name(sexpr)) {
+        fcall <- paste(sexpr, "(x)")
+        expr <- parse(text=fcall)
+	if(is.null(ylab)) ylab <- fcall
     } else {
-        fun <- NULL
-        if(is.null(ylab)) ylab <- expr.t
+        if(!(is.call(sexpr) && match("x", all.vars(sexpr), nomatch=0)))
+            stop("'expr' must be a function or an expression containing 'x'")
+        expr <- sexpr
+        if(is.null(ylab)) ylab <- deparse(sexpr)
     }
     lims <- delay(par("usr"))
     if(missing(from)) from <- lims[1]
     if(missing(to)) to <- lims[2]
     x <- seq(from,to,length=n)
-    y <- if(is.null(fun)) eval(expr) else fun(x)
+    y <- eval(expr, envir=list(x = x), enclos=sys.frame(sys.parent(1)))
     if(add)
 	lines(x, y, ...)
     else
