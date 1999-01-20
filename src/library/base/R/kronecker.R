3955998967edbe514eebd6d60e69818b992da807
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
1999-01-20 07:43:18 +0000

    cosmetic
    git-svn-id: https://svn.r-project.org/R/branches/R-0-63-patches@3369 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/kronecker.R
+++ src/library/base/R/kronecker.R
@@ -1,9 +1,13 @@
+##---  the argument `make.dimnames'  is  not yet documented
+##--- and it doesn't work when TRUE mostly..
+##--- (very unclear debugging however ...)  --- Martin Maechler
+
 kronecker <- function (X, Y, FUN = "*", make.dimnames = FALSE, ...)
 {
     dX <- dim(X <- as.array(X))
     dY <- dim(Y <- as.array(Y))
     ld <- length(dX) - length(dY)
-    ## pad with unit dims if required
+    ## pad with unit dims if required:
     if (ld<0)
         dX <- dim(X) <- c(dX, rep(1, -ld))
     else if(ld > 0)
@@ -18,10 +22,10 @@ kronecker <- function (X, Y, FUN = "*", make.dimnames = FALSE, ...)
     dim(opobj) <- dX * dY
     if(make.dimnames) {
         outerPaste <- function(x,y) {
-            if((nx <- is.null(x)) && (ny <- is.null(y))) 
+            if((iNx <- is.null(x)) && (iNy <- is.null(y))) 
                 NULL
-            else outer(if(nx) "" else x,
-                       if(ny) "" else y, FUN="paste", sep=":")
+            else outer(if(iNx) "" else x,
+                       if(iNy) "" else y, FUN="paste", sep=":")
         }
         dimnames(opobj) <-
             lapply(0:1, function(i)do.call("outerPaste", dn[i*ld + (1:ld)]))
