98e6bfa53098e9ae05dbc4d49940f30f4b8d6619
ihaka <ihaka@00db46b3-68df-0310-9c12-caf00c1e9a41>
1999-01-26 03:41:13 +0000

    Fixes for octal specification of line textures.
    git-svn-id: https://svn.r-project.org/R/trunk@3431 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/graphics.c
+++ src/main/graphics.c
@@ -32,6 +32,8 @@
 #include <string.h>
 #include <stdlib.h>
 
+static char HexDigits[] = "0123456789ABCDEF";
+
 
 /*--->> Documentation now in  ../include/Graphics.h  "API" ----- */
 
@@ -4093,8 +4095,6 @@ static unsigned int hexdigit(int digit)
     return digit - '0';	/* never occurs but avoid compiler warnings */
 }
 
-static char HexDigits[] = "0123456789ABCDEF";
-
 #ifdef UNUSED
 /* Integer to Hex Digit */
 
@@ -4328,7 +4328,7 @@ SEXP LTYget(unsigned int lty)
 {
     SEXP ans;
     int i, ndash;
-    char dash[8];
+    unsigned char dash[8];
     unsigned int l;
 
     for(i = 0; linetype[i].name; i++) {
@@ -4344,7 +4344,7 @@ SEXP LTYget(unsigned int lty)
     PROTECT(ans = allocVector(STRSXP, 1));
     STRING(ans)[0] = allocString(ndash);
     for(i=0 ; i<ndash ; i++) {
-	CHAR(STRING(ans)[0])[i] = dash[i] + '0';
+	CHAR(STRING(ans)[0])[i] = HexDigits[dash[i]];
     }
     UNPROTECT(1);
     return ans;
