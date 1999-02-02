d74890ff74cf9782be1a24bcca758274b96edc3c
r <r@00db46b3-68df-0310-9c12-caf00c1e9a41>
1999-02-02 23:40:52 +0000

    Added Gnome() command
    git-svn-id: https://svn.r-project.org/R/trunk@3516 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/x11.R
+++ src/library/base/R/x11.R
@@ -1,4 +1,10 @@
 X11 <- function(display="", width=7, height=7, pointsize=12)
     .Internal(X11(display, width, height, pointsize))
 
-x11 <- X11
+x11 <- .Alias(X11)
+
+Gnome <- function(display="", width=7, height=7, pointsize=12)
+    .Internal(Gnome(display, width, height, pointsize))
+
+gnome <- .Alias(Gnome)
+GNOME <- .Alias(Gnome)
