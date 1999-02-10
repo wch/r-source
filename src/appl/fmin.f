0f6dea463536a6b0ad6014ba5915a58b99e9db67
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
1999-02-10 10:12:22 +0000

    much less goto
    git-svn-id: https://svn.r-project.org/R/trunk@3599 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/appl/fmin.f
+++ src/appl/fmin.f
@@ -1,3 +1,5 @@
+C R's  optimize() :   function	fmin(ax,bx,f,tol)
+C =    ==========		~~~~~~~~~~~~~~~~~
 c
 c      an approximation  x  to the point where  f  attains a minimum  on
 c  the interval  (ax,bx)  is determined.
@@ -37,10 +39,16 @@ c  minimization without derivatives, prentice-hall, inc. (1973).
 c
 c
       double precision function fmin(ax,bx,f,tol)
+
+      implicit none
       double precision ax,bx,f,tol
+c
+      EXTERNAL  d1mach
+      double precision  dabs,dsqrt,d1mach
+
       double precision  a,b,c,d,e,eps,xm,p,q,r,tol1,t2,u,v,w,fu,fv,fw,
      2    fx,x,tol3
-      double precision  dabs,dsqrt,d1mach
+
 c
 c  c is the squared inverse of the golden ratio
       c=0.5d0*(3.0d0-dsqrt(5.0d0))
@@ -48,7 +56,7 @@ c
 c  eps is approximately the square root of the relative machine
 c  precision.
 c
-   10 eps=d1mach(4)
+      eps=d1mach(4)
       tol1=eps+1.0d0
       eps=dsqrt(eps)
 c
@@ -67,7 +75,7 @@ c
       fw=fx
       tol3=tol/3.0d0
 c
-c  main loop starts here
+c  main loop starts here -----------------------------------
 c
    20 xm=0.5d0*(a+b)
       tol1=eps*dabs(x)+tol3
@@ -75,90 +83,112 @@ c
 c
 c  check stopping criterion
 c
-      if (dabs(x-xm).le.(t2-0.5d0*(b-a))) go to 190
+      if (dabs(x-xm).le.(t2-0.5d0*(b-a))) go to 900
       p=0.0d0
       q=0.0d0
       r=0.0d0
-      if (dabs(e).le.tol1) go to 50
+      if (dabs(e).gt.tol1) then
+c     
+c     fit parabola
+c     
+         r=(x-w)*(fx-fv)
+         q=(x-v)*(fx-fw)
+         p=(x-v)*q-(x-w)*r
+         q=2.0d0*(q-r)
+         if (q.gt.0.0d0) then
+            p=-p
+         else
+            q=-q
+         endif
+         r=e
+         e=d
+      endif
+
+      if ((dabs(p).ge.dabs(0.5d0*q*r)).or.(p.le.q*(a-x))
+     2                                .or.(p.ge.q*(b-x))) then
+
 c
-c  fit parabola
+c     a golden-section step
+c     
+         if (x.lt.xm) then
+            e=b-x
+         else
+            e=a-x
+         endif
+         d=c*e
+         
+      else
 c
-      r=(x-w)*(fx-fv)
-      q=(x-v)*(fx-fw)
-      p=(x-v)*q-(x-w)*r
-      q=2.0d0*(q-r)
-      if (q.le.0.0d0) go to 30
-      p=-p
-      go to 40
-   30 q=-q
-   40 r=e
-      e=d
-   50 if ((dabs(p).ge.dabs(0.5d0*q*r)).or.(p.le.q*(a-x))
-     2          .or.(p.ge.q*(b-x))) go to 60
-c
-c  a parabolic-interpolation step
-c
-      d=p/q
-      u=x+d
-c
-c  f must not be evaluated too close to ax or bx
-c
-      if (((u-a).ge.t2).and.((b-u).ge.t2)) go to 90
-      d=tol1
-      if (x.ge.xm) d=-d
-      go to 90
-c
-c  a golden-section step
-c
-   60 if (x.ge.xm) go to 70
-      e=b-x
-      go to 80
-   70 e=a-x
-   80 d=c*e
-c
-c  f must not be evaluated too close to x
-c
-   90 if (dabs(d).lt.tol1) go to 100
-      u=x+d
-      go to 120
-  100 if (d.le.0.0d0) go to 110
-      u=x+tol1
-      go to 120
-  110 u=x-tol1
-  120 fu=f(u)
+c     a parabolic-interpolation step
+c     
+         d=p/q
+         u=x+d
+c     
+c     f must not be evaluated too close to ax or bx
+c     
+         if (((u-a).ge.t2).and.((b-u).ge.t2)) go to 90
+         d=tol1
+         if (x.ge.xm) d=-d
+      endif
+         
+c     
+c f must not be evaluated too close to x
+c     
+ 90   if (dabs(d).ge.tol1) then
+         u=x+d
+      else
+         if (d.gt.0.0d0) then
+            u=x+tol1
+         else
+            u=x-tol1
+         endif
+      endif
+      fu=f(u)
+
 c
 c  update  a, b, v, w, and x
 c
-      if (fx.gt.fu) go to 140
-      if (u.ge.x) go to 130
-      a=u
-      go to 140
-  130 b=u
-  140 if (fu.gt.fx) go to 170
-      if (u.ge.x) go to 150
-      b=x
-      go to 160
-  150 a=x
-  160 v=w
-      fv=fw
-      w=x
-      fw=fx
-      x=u
-      fx=fu
-      go to 20
-  170 if ((fu.gt.fw).and.(w.ne.x)) go to 180
-      v=w
-      fv=fw
-      w=u
-      fw=fu
-      go to 20
-  180 if ((fu.gt.fv).and.(v.ne.x).and.(v.ne.w)) go to 20
-      v=u
-      fv=fu
+      if (fx.le.fu) then
+         if (u.lt.x) then
+            a=u
+         else
+            b=u
+         endif
+      endif
+
+      if (fu.le.fx) then
+         
+         if (u.lt.x) then
+            b=x
+         else
+            a=x
+         endif
+         v=w
+         fv=fw
+         w=x
+         fw=fx
+         x=u
+         fx=fu
+         
+      else
+
+ 170     if ((fu.le.fw).or.(w.eq.x)) then
+            v=w
+            fv=fw
+            w=u
+            fw=fu
+         else
+            if ((fu.le.fv).or.(v.eq.x).or.(v.eq.w)) then
+               v=u
+               fv=fu
+            endif
+         endif
+      endif
+      
       go to 20
 c
 c  end of main loop
 c
-  190 fmin=x
+  900 fmin=x
       return
       end
