72bebe37876e12dc05fd96dc7cde31a239d0412d
pd <pd@00db46b3-68df-0310-9c12-caf00c1e9a41>
1999-02-25 22:39:33 +0000

    get/assign take character pos=
    git-svn-id: https://svn.r-project.org/R/branches/R-0-63-patches@3736 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/assign.R
+++ src/library/base/R/assign.R
@@ -1,4 +1,8 @@
 assign <-
     function(x, value, pos=-1, envir=pos.to.env(pos), inherits=FALSE,
 	     immediate=TRUE)
-    .Internal(assign(x, value, envir, inherits))
+    {
+	if ( is.character(pos) )
+	    pos <- match(pos,search())
+    	.Internal(assign(x, value, envir, inherits))
+    }
--- src/library/base/R/get.R
+++ src/library/base/R/get.R
@@ -1,3 +1,7 @@
 get <-
     function(x, pos=-1, envir=pos.to.env(pos), mode="any", inherits=TRUE)
-    .Internal(get(x, envir, mode, inherits))
+    {
+	if (is.character(pos)) 
+	    pos<-match(pos,search()) 
+	.Internal(get(x, envir, mode, inherits))
+    }
