f3033647bf067f2c08a205cc622f62891d6c47c5
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
1999-03-04 17:40:04 +0000

    locator(n=512)
    git-svn-id: https://svn.r-project.org/R/branches/R-0-63-patches@3767 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/locator.R
+++ src/library/base/R/locator.R
@@ -1,7 +1,18 @@
-locator <- function(n=1) {
-    z <- .Internal(locator(n))
+###-- 'msg' interface should be device dependent... -> "next" version
+locator <- function(n=512, msg = "Click left to locate points;  right to end") 
+{
+    do.msg <- is.character(msg) && nchar(msg) > 0
+    if(do.msg) cat(msg,": ",sep="")
+    z <- .Internal(locator(n))# n <= 0 gives error
+    if(do.msg) cat("\n")
     x <- z[[1]]
     y <- z[[2]]
-    n <- z[[3]]
-    if(n==0) NULL else list(x=x[1:n],y=y[1:n])
+    if((n <- z[[3]]) > 0) list(x=x[1:n], y=y[1:n]) 
+}
+locator <- function(n=512)
+{
+    z <- .Internal(locator(n))# n <= 0 gives error
+    x <- z[[1]]
+    y <- z[[2]]
+    if((n <- z[[3]]) > 0) list(x=x[1:n], y=y[1:n]) 
 }
