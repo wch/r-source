db479596aaadef173d55935e5d1dbd745b7c3356
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
1999-03-04 14:07:41 +0000

    ..
    git-svn-id: https://svn.r-project.org/R/branches/R-0-63-patches@3762 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/qqnorm.R
+++ src/library/base/R/qqnorm.R
@@ -1,13 +1,12 @@
-### returns value, extra protection.  BDR 29/5/98.
 qqnorm <-
     function(y, ylim, main="Normal Q-Q Plot",
 	     xlab="Theoretical Quantiles", ylab="Sample Quantiles",
 	     plot.it=TRUE, ...)
 {
     y <- y[!is.na(y)]
-    if(!length(y)) stop("y is empty")
+    if(0 == (n <- length(y))) stop("y is empty")
     if (missing(ylim)) ylim <- range(y)
-    x <- qnorm(ppoints(length(y)))[order(order(y))]
+    x <- qnorm(ppoints(n))[order(order(y))]
     if(plot.it)
 	plot(x, y, main= main, xlab= xlab, ylab= ylab, ylim= ylim, ...)
     invisible(list(x = x, y = y))
