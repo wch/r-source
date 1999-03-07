94c454daf3c39b5fefe9128cff8696a61ae5627e
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
1999-03-07 21:23:50 +0000

    Add function page()
    git-svn-id: https://svn.r-project.org/R/trunk@3794 00db46b3-68df-0310-9c12-caf00c1e9a41

---
+++ src/library/base/R/page.R
@@ -0,0 +1,14 @@
+page <- function(x) {
+    subx <- substitute(x)
+    if( is.name(subx) )
+	subx<-deparse(subx)
+    if (!is.character(subx) || length(subx) != 1)
+	stop("page requires a name")
+    if(exists(subx, inherits=TRUE)) {
+        file <- tempfile("Rpage.")
+        dput(get(subx, inherits=TRUE), file)
+        on.exit(unlink(file))
+	file.show(file, title=subx)
+    } else
+	stop(paste("no object named \"", subx, "\" to edit",sep=""))
+}
