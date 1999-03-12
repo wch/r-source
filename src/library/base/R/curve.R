e966ae99f1071b1a53391a0567004fe9ada3d641
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
1999-03-12 10:45:31 +0000

    curve() improved for log="x"
    git-svn-id: https://svn.r-project.org/R/branches/R-0-63-patches@3859 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/curve.R
+++ src/library/base/R/curve.R
@@ -1,5 +1,5 @@
 curve <- function(expr, from, to, n=101, add=FALSE, type="l",
-                  ylab = NULL, ...)
+                  ylab = NULL, log=NULL, ...)
 {
     sexpr <- substitute(expr)
     if(is.name(sexpr)) {
@@ -15,10 +15,17 @@ curve <- function(expr, from, to, n=101, add=FALSE, type="l",
     lims <- delay(par("usr"))
     if(missing(from)) from <- lims[1]
     if(missing(to)) to <- lims[2]
-    x <- seq(from,to,length=n)
+    lg <- if(length(log)) log else ""
+    x <-
+        if(lg != "" && "x" %in% strsplit(lg, NULL)[[1]]) {
+            rm(list="log",envir=sys.frame(1))# else: warning
+            if(any(c(from,to)<=0))
+                stop("`from' & `to' must be > 0  with  log=\"x\"")
+            exp(seq(log(from), log(to), length=n))
+        } else seq(from,to,length=n)
     y <- eval(expr, envir=list(x = x), enclos=sys.frame(sys.parent(1)))
     if(add)
 	lines(x, y, ...)
     else
-	plot(x, y, type="l", ylab = ylab, ...)
+	plot(x, y, type="l", ylab = ylab, log=lg, ...)
 }
