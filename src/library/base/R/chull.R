654235e23708e89f181bca1d9251de4cfb16b45f
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
1999-03-30 05:49:08 +0000

    `R-core indent style'
    git-svn-id: https://svn.r-project.org/R/trunk@3953 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/chull.R
+++ src/library/base/R/chull.R
@@ -1,17 +1,17 @@
 chull <- function(x, y=NULL)
 {
-  X <- xy.coords(x, y, recycle = TRUE)
-  x <- cbind(X$x, X$y)
-  n <- nrow(x)
-  z <- .C("chull",
-                n=as.integer(n),
-                as.double(x),
-                as.integer(n),
-                as.integer(1:n),
-                integer(n),
-                integer(n),
-                ih=integer(n),
-                nh=integer(1),
-                il=integer(n))
-  rev(z$ih[1:z$nh])
+    X <- xy.coords(x, y, recycle = TRUE)
+    x <- cbind(X$x, X$y)
+    n <- nrow(x)
+    z <- .C("chull",
+            n=as.integer(n),
+            as.double(x),
+            as.integer(n),
+            as.integer(1:n),
+            integer(n),
+            integer(n),
+            ih=integer(n),
+            nh=integer(1),
+            il=integer(n))
+    rev(z$ih[1:z$nh])
 }
