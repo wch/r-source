e43fecad89d78ca24a1b9af19883f594b710bc84
rgentlem <rgentlem@00db46b3-68df-0310-9c12-caf00c1e9a41>
1999-04-20 17:02:30 +0000

    fixed bug, R_Warnings wasn't being protected.
    git-svn-id: https://svn.r-project.org/R/trunk@4215 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/memory.c
+++ src/main/memory.c
@@ -516,6 +516,7 @@ void markPhase(void)
     markSExp(R_CommentSxp);
 
     markSExp(R_GlobalEnv);	           /* Global environent */
+    markSExp(R_Warnings);	           /* Warnings, if any */
 
     for (i = 0; i < HSIZE; i++)	           /* Symbol table */
 	markSExp(R_SymbolTable[i]);
