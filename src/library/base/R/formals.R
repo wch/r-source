9818e7cf08e75bd421c5748a62be4c316e9eb88d
rgentlem <rgentlem@00db46b3-68df-0310-9c12-caf00c1e9a41>
1999-04-26 19:50:01 +0000

    added Peter's modification from bug 151
    git-svn-id: https://svn.r-project.org/R/trunk@4283 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/formals.R
+++ src/library/base/R/formals.R
@@ -1,6 +1,6 @@
 formals <- function(fun=sys.function(sys.parent())) {
     if(is.character(fun))
-	fun <- get(fun, mode = "function")
+	fun <- get(fun, mode = "function", envir = sys.frame(sys.parent()))
     .Internal(formals(fun))
 }
 body <- function(fun=sys.function(sys.parent())) {
