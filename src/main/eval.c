d7223afd64e4b352f27f702cf6d690f43b0169df
pd <pd@00db46b3-68df-0310-9c12-caf00c1e9a41>
1999-04-28 10:07:28 +0000

    PROTECT error (found in devel branch)
    git-svn-id: https://svn.r-project.org/R/branches/R-0-64-patches@4416 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/eval.c
+++ src/main/eval.c
@@ -1018,6 +1018,7 @@ SEXP do_eval(SEXP call, SEXP op, SEXP args, SEXP rho)
 	UNPROTECT(1);
     }
     if (PRIMVAL(op)) {
+	PROTECT(expr);
         PROTECT(env = allocVector(VECSXP, 2));
         PROTECT(encl = allocVector(STRSXP, 2));
 	STRING(encl)[0] = mkChar("value");
@@ -1026,7 +1027,7 @@ SEXP do_eval(SEXP call, SEXP op, SEXP args, SEXP rho)
         VECTOR(env)[1] = ScalarLogical(R_Visible);
         setAttrib(env, R_NamesSymbol, encl);
         expr = env;
-        UNPROTECT(2);
+        UNPROTECT(3);
     }
     UNPROTECT(1);
     return expr;
