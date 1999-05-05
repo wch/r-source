bb23ab00bc760287018ee417f36baeb943de3467
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
1999-05-05 06:37:09 +0000

    add relevel() function
    git-svn-id: https://svn.r-project.org/R/branches/R-0-64-patches@4506 00db46b3-68df-0310-9c12-caf00c1e9a41

---
+++ src/library/base/R/relevel.R
@@ -0,0 +1,21 @@
+#### copyright (C) 1998 B. D. Ripley
+relevel <- function(x, ref, ...) UseMethod("relevel")
+
+relevel.default <- function(x, ref, ...)
+    stop("relevel only for factors")
+
+relevel.ordered <- function(x, ref, ...)
+    stop("relevel only for factors")
+
+relevel.factor <- function(x, ref, ...)
+{
+    lev <- levels(x)
+    if(is.character(ref))
+        ref <- match(ref, lev)
+    if(is.na(ref))
+        stop("ref must be an existing level")
+    nlev <- length(x)
+    if(ref < 1 || ref > nlev)
+        stop(paste("ref =", ref, "must be in 1 :", nlev))
+    factor(x, levels = lev[c(ref, seq(along=lev)[-ref])])
+}
