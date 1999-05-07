de37ffec4aa9398dc9d379826d4903e81f736738
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
1999-05-07 16:49:34 +0000

    dump fixed at C level
    git-svn-id: https://svn.r-project.org/R/branches/R-0-64-patches@4555 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/dump.R
+++ src/library/base/R/dump.R
@@ -1,8 +1,1 @@
-dump <-
-function (list, fileout = "dumpdata")
-{
-    digits <- options("digits")
-    on.exit(options(digits = digits))
-    options(digits = 12)
-    .Internal(dump(list, fileout))
-}
+dump <- function (list, fileout = "dumpdata") { .Internal(dump(list, fileout)) }
