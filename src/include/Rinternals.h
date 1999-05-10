621f09a2e8e6b6348faf70c1b6ee817c33610710
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
1999-05-10 06:30:20 +0000

    Add copyrights, General Cons Cell Attributes macros
    git-svn-id: https://svn.r-project.org/R/branches/R-0-64-patches@4565 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/include/Rinternals.h
+++ src/include/Rinternals.h
@@ -1,3 +1,22 @@
+/*
+ *  R : A Computer Language for Statistical Data Analysis
+ *  Copyright (C) 1995, 1996  Robert Gentleman and Ross Ihaka
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
 #ifndef _R_INTERNALS_H_
 #define _R_INTERNALS_H_
 
@@ -114,6 +133,13 @@ typedef struct SEXPREC {
 	} promsxp;
     } u;
 } SEXPREC, *SEXP;
+
+/* General Cons Cell Attributes */
+#define ATTRIB(x)	((x)->attrib)
+#define OBJECT(x)	((x)->sxpinfo.obj)
+#define MARK(x)		((x)->sxpinfo.mark)
+#define TYPEOF(x)	((x)->sxpinfo.type)
+#define NAMED(x)	((x)->sxpinfo.named)
 
 
 /* Vector Access Macros */
