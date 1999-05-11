553e47e28ad4d310b880dd1da163c480ba1274c3
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
1999-05-11 06:32:24 +0000

    New function boxplot.formula().
    git-svn-id: https://svn.r-project.org/R/trunk@4578 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/boxplot.R
+++ src/library/base/R/boxplot.R
@@ -1,19 +1,20 @@
 boxplot <- function(x, ...) UseMethod("boxplot")
 
-boxplot.default <- function(x, ..., range=1.5, width=NULL, varwidth=FALSE,
-		    notch=FALSE, names.x, data=sys.frame(sys.parent()),
-		    plot=TRUE, border=par("fg"), col=NULL, log="", pars=NULL)
+boxplot.default <-
+function(x, ..., range = 1.5, width = NULL, varwidth = FALSE, notch =
+         FALSE, names.x, data = sys.frame(sys.parent()), plot = TRUE,
+         border = par("fg"), col = NULL, log = "", pars = NULL)
 {
-    args <- list(x,...)
+    args <- list(x, ...)
     namedargs <-
 	if(!is.null(attributes(args)$names))
 	    attributes(args)$names != ""
 	else
-	    rep(FALSE, length=length(args))
+	    rep(FALSE, length = length(args))
     pars <- c(args[namedargs], pars)
     groups <-
 	if(is.language(x)) {
-	    if (inherits(x, "formula") && length(x) == 3) {
+	    if(inherits(x, "formula") && length(x) == 3) {
 		groups <- eval(x[[3]], data, sys.frame(sys.parent()))
 		x <- eval(x[[2]], data, sys.frame(sys.parent()))
 		split(x, groups)
@@ -21,7 +22,7 @@ boxplot.default <- function(x, ..., range=1.5, width=NULL, varwidth=FALSE,
 	}
 	else {
 	    groups <- args[!namedargs]
-	    if (length(groups) == 1 && is.list(x)) x else groups
+	    if(length(groups) == 1 && is.list(x)) x else groups
 	}
     if(0 == (n <- length(groups)))
 	stop("invalid first argument")
@@ -34,23 +35,41 @@ boxplot.default <- function(x, ..., range=1.5, width=NULL, varwidth=FALSE,
     for(i in 1:n)
 	groups[i] <- list(boxplot.stats(groups[[i]], range)) # do.conf=notch)
     if(plot) {
-	bxp(groups, width, varwidth=varwidth, notch=notch,
-	    border=border, col=col, log=log, pars=pars)
+	bxp(groups, width, varwidth = varwidth, notch = notch, border =
+            border, col = col, log = log, pars = pars) 
 	invisible(groups)
     }
     else groups
 }
 
+boxplot.formula <- function(formula, data = NULL, subset, na.action, ...)
+{
+    if(missing(formula) || (length(formula) != 3))
+        stop("formula missing or incorrect")
+    if(missing(na.action))
+        na.action <- options()$na.action
+    m <- match.call(expand.dots = FALSE)
+    if(is.matrix(eval(m$data, sys.frame(sys.parent()))))
+        m$data <- as.data.frame(data)
+    m$... <- NULL
+    m[[1]] <- as.name("model.frame")
+    mf <- eval(m, sys.frame(sys.parent()))
+    response <- attr(attr(mf, "terms"), "response")
+    boxplot(split(mf[[response]], mf[[-response]]), ...)
+}
+
 boxplot.stats <- function(x, coef = 1.5, do.conf=TRUE, do.out=TRUE)
 {
     nna <- !is.na(x)
-    n <- length(nna)# including +/- Inf
-    stats <- fivenum(x, na.rm=TRUE)
+    n <- length(nna)                    # including +/- Inf
+    stats <- fivenum(x, na.rm = TRUE)
     iqr <- diff(stats[c(2, 4)])
-    out <- x < (stats[2]-coef*iqr) | x > (stats[4]+coef*iqr)
-    if(coef > 0) stats[c(1, 5)] <- range(x[!out], na.rm=TRUE)
-    conf <- if(do.conf)stats[3]+c(-1.58, 1.58)*diff(stats[c(2, 4)])/sqrt(n)
-    list(stats=stats, n=n, conf=conf, out=if(do.out)x[out&nna]else numeric(0))
+    out <- x < (stats[2] - coef * iqr) | x > (stats[4] + coef * iqr)
+    if(coef > 0) stats[c(1, 5)] <- range(x[!out], na.rm = TRUE)
+    conf <- if(do.conf)
+        stats[3] + c(-1.58, 1.58) * diff(stats[c(2, 4)]) / sqrt(n)
+    list(stats = stats, n = n, conf = conf,
+         out = if(do.out) x[out & nna] else numeric(0))
 }
 
 bxp <- function(z, notch=FALSE, width=NULL, varwidth=FALSE,
@@ -105,13 +124,13 @@ bxp <- function(z, notch=FALSE, width=NULL, varwidth=FALSE,
                         z[[i]]$stats[is.finite(z[[i]]$stats)],
                         z[[i]]$out[is.finite(z[[i]]$out)])
     }
-    width <- if (!is.null(width)) {
-	if (length(width) != n | any(is.na(width)) | any(width <= 0))
+    width <- if(!is.null(width)) {
+	if(length(width) != n | any(is.na(width)) | any(width <= 0))
 	    stop("invalid boxplot widths")
 	0.8 * width/max(width)
     }
-    else if (varwidth) 0.8 * sqrt(unlist(lapply(z, "[[", "n"))/nmax)
-    else if (n == 1) 0.4
+    else if(varwidth) 0.8 * sqrt(unlist(lapply(z, "[[", "n"))/nmax)
+    else if(n == 1) 0.4
     else rep(0.8, n)
 
     if(is.null(pars$ylim)) ylim <- limits
