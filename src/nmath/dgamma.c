1fb01158ca223f2a35ffe3510d27da34acb0941e
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
1999-05-21 15:47:00 +0000

    ..
    git-svn-id: https://svn.r-project.org/R/trunk@4667 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/nmath/dgamma.c
+++ src/nmath/dgamma.c
@@ -18,12 +18,12 @@
  *
  *  SYNOPSIS
  *
- *    #include "Mathlib.h"
- *    double dgamma(double x, double shape, double scale);
+ *	#include "Mathlib.h"
+ *	double dgamma(double x, double shape, double scale);
  *
  *  DESCRIPTION
  *
- *    Computes the density of the gamma distribution.
+ *     Computes the density of the gamma distribution.
  */
 
 #include "Mathlib.h"
@@ -50,6 +50,7 @@ double dgamma(double x, double shape, double scale)
 	}
 	return 1 / scale;
     }
-    x = x / scale;
+    x /= scale;
     return exp((shape - 1) * log(x) - lgammafn(shape) - x) / scale;
 }
+
