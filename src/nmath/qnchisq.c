7198da890786474abc031e6883437be9b23f2fd4
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
1999-05-21 15:38:59 +0000

    s/Langage/Language/
    git-svn-id: https://svn.r-project.org/R/trunk@4666 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/nmath/qnchisq.c
+++ src/nmath/qnchisq.c
@@ -1,5 +1,5 @@
 /*
- *  R : A Computer Langage for Statistical Data Analysis
+ *  R : A Computer Language for Statistical Data Analysis
  *  Copyright (C) 1995, 1996  Robert Gentleman and Ross Ihaka
  *
  *  This program is free software; you can redistribute it and/or modify
@@ -21,33 +21,37 @@
 
 double qnchisq(double p, double n, double lambda)
 {
-	double ux, lx, nx;
-	double acu = 1.0e-12;
+    double ux, lx, nx;
+    double acu = 1.0e-12;
 
 #ifdef IEEE_754
-	if (ISNAN(p) || ISNAN(n) || ISNAN(lambda))
-		return p + n + lambda;
-	if (!FINITE(n)) {
-		ML_ERROR(ME_DOMAIN);
-		return ML_NAN;
-	}
+    if (ISNAN(p) || ISNAN(n) || ISNAN(lambda))
+	return p + n + lambda;
+    if (!FINITE(n)) {
+	ML_ERROR(ME_DOMAIN);
+	return ML_NAN;
+    }
 #endif
-	n = floor(n + 0.5);
-	if (p < 0 || p >= 1 || n < 1 || lambda < 0) {
-		ML_ERROR(ME_DOMAIN);
-		return ML_NAN;
-	}
-	if (p == 0)
-		return 0;
-	for (ux = 1.0; pnchisq(ux, n, lambda) < p; ux *= 2);
-	for (lx = ux; pnchisq(lx, n, lambda) > p; lx *= 0.5);
-	do {
-		nx = 0.5 * (lx + ux);
-		if (pnchisq(nx, n, lambda) > p)
-			ux = nx;
-		else
-			lx = nx;
-	}
-	while ((ux - lx) / nx > acu);
-	return 0.5 * (ux + lx);
+    n = floor(n + 0.5);
+    if (p < 0 || p >= 1 || n < 1 || lambda < 0) {
+	ML_ERROR(ME_DOMAIN);
+	return ML_NAN;
+    }
+    if (p == 0)
+	return 0;
+    /* Invert pnchisq(.) finding an upper and lower bound;
+       then interval halfing : */
+
+    /* FIXME: Use less precision here (see pnchisq() !) */
+    for (ux = 1.0; pnchisq(ux, n, lambda) < p; ux *= 2);
+    for (lx = ux; pnchisq(lx, n, lambda) > p; lx *= 0.5);
+    do {
+	nx = 0.5 * (lx + ux);
+	if (pnchisq(nx, n, lambda) > p)
+	    ux = nx;
+	else
+	    lx = nx;
+    }
+    while ((ux - lx) / nx > acu);
+    return 0.5 * (ux + lx);
 }
