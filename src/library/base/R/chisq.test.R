436fa650b79e983bf37025c9c268e4c5ef641900
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
1999-06-02 19:26:44 +0000

    Add code for MC simulation of p-values.
    git-svn-id: https://svn.r-project.org/R/trunk@4780 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/chisq.test.R
+++ src/library/base/R/chisq.test.R
@@ -1,5 +1,6 @@
-chisq.test <- function(x, y = NULL, correct = TRUE,
-		       p = rep(1 / length(x), length(x)))
+chisq.test <- 
+function(x, y = NULL, correct = TRUE, p = rep(1 / length(x), length(x)),
+         simulate.p.value = FALSE, B = 2000)
 {
     DNAME <- deparse(substitute(x))
     if (is.data.frame(x))
@@ -22,19 +23,51 @@ chisq.test <- function(x, y = NULL, correct = TRUE,
 
     if (any(x < 0) || any(is.na(x)))
 	stop("all entries of x must be nonnegative and finite")
+    if ((n <- sum(x)) == 0)
+        stop("at least one entry of x must be positive")
 
     if (is.matrix(x)) {
 	METHOD <- "Pearson's Chi-square test"
-	E <- outer(apply(x, 1, sum), apply(x, 2, sum), "*") / sum(x)
-	if (correct && nrow(x) == 2 && ncol(x) == 2) {
-	    YATES <- .5
-	    METHOD <- paste(METHOD, "with Yates' continuity correction")
-	}
-	else
-	    YATES <- 0
+        nr <- nrow(x)
+        nc <- ncol(x)
+        sr <- apply(x, 1, sum)
+        sc <- apply(x, 2, sum)
+	E <- outer(sr, sc, "*") / n
 	dimnames(E) <- dimnames(x)
-	STATISTIC <- sum((abs(x - E) - YATES)^2 / E)
-	PARAMETER <- (nrow(x) - 1) * (ncol(x) - 1)
+        if (simulate.p.value && all(sr > 0) && all(sc > 0)) {
+            METHOD <- paste(METHOD,
+                            "with simulated p-value\n\t (based on", B,
+                            "replicates)")
+            tmp <- .C("chisqsim",
+                      as.integer(nr),
+                      as.integer(nc),
+                      as.integer(sr),
+                      as.integer(sc),
+                      as.integer(n),
+                      as.integer(B),
+                      as.double(E),
+                      integer(nr * nc),
+                      double(n + 1),
+                      integer(nc),
+                      results = double(B))
+            STATISTIC <- sum((x - E) ^ 2 / E)
+            PARAMETER <- NA
+            PVAL <- sum(tmp$results >= STATISTIC) / B
+        }
+        else {
+            if (simulate.p.value)
+                warning(paste("Cannot compute simulated p-value",
+                              "with zero marginals"))
+            if (correct && nrow(x) == 2 && ncol(x) == 2) {
+                YATES <- .5
+                METHOD <- paste(METHOD, "with Yates' continuity correction")
+            }
+            else
+                YATES <- 0
+            STATISTIC <- sum((abs(x - E) - YATES)^2 / E)
+            PARAMETER <- (nr - 1) * (nc - 1)
+            PVAL <- 1 - pchisq(STATISTIC, PARAMETER)
+        }
     }
     else {
 	if (length(x) == 1)
@@ -42,17 +75,17 @@ chisq.test <- function(x, y = NULL, correct = TRUE,
 	if (length(x) != length(p))
 	    stop("x and p must have the same number of elements")
 	METHOD <- "Chi-square test for given probabilities"
-	E <- sum(x) * p
+	E <- n * p
 	names(E) <- names(x)
 	STATISTIC <- sum((x - E) ^ 2 / E)
 	PARAMETER <- length(x) - 1
+        PVAL <- 1 - pchisq(STATISTIC, PARAMETER)
     }
 
     names(STATISTIC) <- "X-squared"
     names(PARAMETER) <- "df"
-    if (any(E < 5))
+    if (any(E < 5) && is.finite(PARAMETER))
 	warning("Chi-square approximation may be incorrect")
-    PVAL <- 1 - pchisq(STATISTIC, PARAMETER)
 
     structure(list(statistic = STATISTIC,
 		   parameter = PARAMETER,
