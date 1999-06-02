33c5e2f48a23308ad13c33622b82c05400569c73
pd <pd@00db46b3-68df-0310-9c12-caf00c1e9a41>
1999-06-02 11:47:01 +0000

    group generic problem fix-fix
    git-svn-id: https://svn.r-project.org/R/trunk@4775 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/eval.c
+++ src/main/eval.c
@@ -1164,12 +1164,14 @@ int DispatchGroup(char* group, SEXP call, SEXP op, SEXP args, SEXP rho,
     SEXP class, s, t, m, meth, sxp, gr, newrho;
     char buf[512], generic[128], *pt;
     /* check whether we are processing the default method */
-    sprintf(buf, "%s", PRIMNAME(op) );
-    pt = strtok(buf, ".");
-    pt = strtok(NULL, ".");
+    if ( isSymbol(CAR(call)) ) {
+	sprintf(buf, "%s", CHAR(PRINTNAME(CAR(call))) );
+	pt = strtok(buf, ".");
+	pt = strtok(NULL, ".");
 
-    if( pt != NULL && !strcmp(pt, "default") )
-	return 0;
+	if( pt != NULL && !strcmp(pt, "default") )
+	    return 0;
+    }
 
     if( !strcmp(group, "Ops") )
 	nargs = length(args);
