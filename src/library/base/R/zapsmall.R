da87037e9aa8c572e154fe548208bd55b61087b0
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
1999-06-14 13:10:55 +0000

    zapsmall(<cplx>)
    git-svn-id: https://svn.r-project.org/R/branches/R-0-64-patches@4829 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/zapsmall.R
+++ src/library/base/R/zapsmall.R
@@ -1,6 +1,6 @@
 zapsmall <- function(x, digits = .Options$digits)
 {
     if(all(ina <- is.na(x))) return(x)
-    mx <- max(abs(x[!ina]))
+    mx <- max((if(!is.complex(x)) abs else Mod)(x[!ina]))
     round(x, digits = if(mx > 0) max(0, digits - log10(mx)) else digits)
 }
