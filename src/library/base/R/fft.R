2d186c7bec7c39c33751b2143c11ba5d71d0a216
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
1999-06-22 16:54:34 +0000

    convolve() improvements
    git-svn-id: https://svn.r-project.org/R/branches/R-0-64-patches@4910 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/fft.R
+++ src/library/base/R/fft.R
@@ -7,9 +7,19 @@ mvfft <- function(z, inverse=FALSE)
 nextn <- function(n, factors=c(2,3,5))
     .Internal(nextn(n, factors))
 
-convolve <- function(x, y, conj=TRUE) {
+convolve <- function(x, y, conj=TRUE, circular=TRUE) {
     n <- length(x)
-    if(length(y) != n)
-	stop("length mismatch in convolution")
-    Re(fft(fft(x)* (if(conj)Conj(fft(y)) else fft(y)), inv=TRUE))/n
+    ny <- length(y)
+    Real <- is.numeric(x) && is.real(y)
+    if(circular) {
+        if(ny != n)
+            stop("length mismatch in convolution")
+    }
+    else { ## "open": Pad with zeros
+        x <- c(rep(0, ny - 1), x)
+        n <- length(y <- c(y, rep(0, n - 1)))# n = nx+ny-1
+    }
+    x <- fft(fft(x)* (if(conj)Conj(fft(y)) else fft(y)), inv=TRUE)
+    if(Real) Re(x)/n else x/n
 }
+
