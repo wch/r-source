62b874a9c4a89cba9ea3799248cde6da6587083e
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
1999-06-26 16:13:31 +0000

    change new convolve() argument [proposed by Martyn Plummer]
    git-svn-id: https://svn.r-project.org/R/branches/R-0-64-patches@4953 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/fft.R
+++ src/library/base/R/fft.R
@@ -7,19 +7,25 @@ mvfft <- function(z, inverse=FALSE)
 nextn <- function(n, factors=c(2,3,5))
     .Internal(nextn(n, factors))
 
-convolve <- function(x, y, conj=TRUE, circular=TRUE) {
+convolve <- function(x, y, conj=TRUE, type=c("circular","open","filter")) {
+    type <- match.arg(type)
     n <- length(x)
     ny <- length(y)
-    Real <- is.numeric(x) && is.real(y)
-    if(circular) {
+    Real <- is.numeric(x) && is.numeric(y)
+    ## switch(type, circular = ..., )
+    if(type == "circular") {
         if(ny != n)
             stop("length mismatch in convolution")
     }
-    else { ## "open": Pad with zeros
-        x <- c(rep(0, ny - 1), x)
+    else { ## "open" or "filter": Pad with zeros
+        n1 <- ny - 1
+        x <- c(rep(0, n1), x)
         n <- length(y <- c(y, rep(0, n - 1)))# n = nx+ny-1
     }
     x <- fft(fft(x)* (if(conj)Conj(fft(y)) else fft(y)), inv=TRUE)
-    if(Real) Re(x)/n else x/n
+    if(type == "filter")
+        (if(Real) Re(x) else x)[-c(1:n1, (n-n1+1):n)]/n
+    else
+        (if(Real) Re(x) else x)/n
 }
 
