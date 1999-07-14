550a46eb6f85ad162dd4d9be0d82eb82b277a68e
(no author) <(no author)@00db46b3-68df-0310-9c12-caf00c1e9a41>
1999-07-14 09:28:30 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.39.2'.
    git-svn-id: https://svn.r-project.org/R/branches/unlabeled-1.39.2@5170 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/zapsmall.R
+++
@@ -1,6 +0,0 @@
-zapsmall <- function(x, digits = .Options$digits)
-{
-    if(all(ina <- is.na(x))) return(x)
-    mx <- max((if(!is.complex(x)) abs else Mod)(x[!ina]))
-    round(x, digits = if(mx > 0) max(0, digits - log10(mx)) else digits)
-}
