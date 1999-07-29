7f933614cfef338e5c0a370a4d0af348dc1ade24
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
1999-07-29 12:53:08 +0000

    AIX can't calloc 0 items
    git-svn-id: https://svn.r-project.org/R/trunk@5376 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/ts/src/pacf.c
+++ src/library/ts/src/pacf.c
@@ -103,7 +103,7 @@ void setup_starma(int *na, double *x, int *pn, double *xreg, int *pm,
     resid = Calloc(n, double);
     phi = Calloc(ir, double);
     theta = Calloc(ir, double);
-    reg = Calloc(n*m, double);
+    reg = Calloc(1+n*m, double); /* AIX can't calloc 0 items */
     delta = *dt;
     for(i = 0; i < n; i++) w[i] = wkeep[i] = x[i];
     for(i = 0; i < n*m; i++) reg[i] = xreg[i];
