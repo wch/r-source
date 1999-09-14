d5e22023207fd0c6aa47732420046685074e9190
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
1999-09-14 08:39:10 +0000

    Adjust for new behavior of recycling and abs().
    git-svn-id: https://svn.r-project.org/R/branches/R-0-65-patches@5783 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/zapsmall.R
+++ src/library/base/R/zapsmall.R
@@ -1,6 +1,9 @@
 zapsmall <- function(x, digits = .Options$digits)
 {
-    if(all(ina <- is.na(x))) return(x)
-    mx <- max((if(!is.complex(x)) abs else Mod)(x[!ina]))
+    if (length(digits) == 0)
+        stop("invalid digits")
+    if (all(ina <- is.na(x)))
+        return(x)
+    mx <- max(abs(x[!ina]))
     round(x, digits = if(mx > 0) max(0, digits - log10(mx)) else digits)
 }
