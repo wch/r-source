e1508e642da02f32670b17c90a09615a0c1ee348
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
1999-09-14 15:38:10 +0000

    add missing F77_* functions
    git-svn-id: https://svn.r-project.org/R/branches/R-0-65-patches@5796 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/include/Rdefines.h
+++ src/include/Rdefines.h
@@ -107,8 +107,13 @@
 #define Memcpy(p,q,n)  memcpy( p, q, (size_t)( (n) * sizeof(*p) ) )
 
 /* S Like Fortran Interface */
+/* These may not be adequate everywhere. Convex had _ prepending common
+   blocks, and some compilers may need to specify Fortran linkage */
 
 #define F77_CALL(x)    F77_SYMBOL(x)
 #define F77_NAME(x)    F77_SYMBOL(x)
+#define F77_SUB(x)     F77_SYMBOL(x)
+#define F77_COM(x)     F77_SYMBOL(x)
+#define F77_COMDECL(x) F77_SYMBOL(x)
 
 #endif
