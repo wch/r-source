fcbfe938d8744c4a263b858ec2fb988389675b46
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
1999-09-16 07:08:31 +0000

    approx did not handle NAs properly
    git-svn-id: https://svn.r-project.org/R/branches/R-0-65-patches@5819 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/approx.R
+++ src/library/base/R/approx.R
@@ -17,6 +17,9 @@ approx <- function (x, y=NULL, xout, method = "linear", n = 50,
     ok <- !(is.na(x) | is.na(y))
     x <- x[ok]
     y <- y[ok]
+    nx <-length(x)
+    if (nx < 2)
+	stop("approx requires at least two non-missing values to interpolate")
     o <- order(x)
     x <- x[o]
     y <- y[o]
