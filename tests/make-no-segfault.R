4e947194c414df282aea60980942eb8536a98913
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
1999-09-24 08:38:01 +0000

    stop.list for internet,interactive,socket
    git-svn-id: https://svn.r-project.org/R/branches/R-0-65-patches@5926 00db46b3-68df-0310-9c12-caf00c1e9a41

--- tests/make-no-segfault.R
+++ tests/make-no-segfault.R
@@ -6,26 +6,40 @@ cat("options(error.halt = FALSE)",
     "# don't stop on error in batch\n##~~~~~~~~~~~~~~\n")
 
 core.pkgs <- .packages(all=TRUE,lib.loc = .lib.loc[length(.lib.loc)])
+#
+stop.list <- vector("list", length(core.pkgs))
+names(stop.list) <- core.pkgs
+{ ## -- Stop List for "base" :
+    inet.list <- c(apropos("download\."),"CRAN.packages", "update.packages")
+    socket.fun <- apropos("socket")# those *do* segfault...
+    ## "Interactive" ones:
+    dev.int <- c("X11", "windows", "macintosh")
+    misc.int <- c("bug.report", "browser")
+    stop.list[["base"]] <- c(inet.list, socket.fun, dev.int, misc.int)
+}
+
 for (pkg in core.pkgs) {
   cat("### Package ", pkg, "\n",
       "###         ", rep("~",nchar(pkg)), "\n", collapse="", sep="")
-  if(pkg == "base")
+  if(pkg == "base") {
       this.pos <- length(search())
+  }
   else {
       library(pkg, character = TRUE)
       cat("library(",pkg,", character = TRUE)\n")
       this.pos <- 2
   }
 
-  for(nm in ls(pos = this.pos)){
-    if(is.function(f <- get(nm,pos = this.pos))) {
-        cat("\n## ", nm, " :\n")
-        cat("f <- get(\"",nm,"\", pos = ", this.pos, ")\n", sep="")
-        cat("f()\nf(NULL)\nf(NULL,NULL)\n",
-            "f(list())\nf(logical(0))\nf(character(0))\n",
-            "f(FALSE)\n",
-            sep="")
-    }
+  for(nm in ls(pos = this.pos)) {
+      if(!(nm %in% stop.list[[pkg]]) &&
+         is.function(f <- get(nm,pos = this.pos))) {
+          cat("\n## ", nm, " :\n")
+          cat("f <- get(\"",nm,"\", pos = ", this.pos, ")\n", sep="")
+          cat("f()\nf(NULL)\nf(NULL,NULL)\n",
+              "f(list())\nf(logical(0))\nf(character(0))\n",
+              "f(FALSE)\n",
+              sep="")
+      }
   }
   if(pkg != "base") {
       detach(pos=this.pos)
