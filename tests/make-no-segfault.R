5765a2a3844fbf65d3f1c87ec39f30af2da853e7
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
1999-09-29 22:12:33 +0000

    .
    git-svn-id: https://svn.r-project.org/R/branches/R-0-65-patches@5984 00db46b3-68df-0310-9c12-caf00c1e9a41

--- tests/make-no-segfault.R
+++ tests/make-no-segfault.R
@@ -9,18 +9,27 @@ core.pkgs <- .packages(all=TRUE,lib.loc = .lib.loc[length(.lib.loc)])
 #
 stop.list <- vector("list", length(core.pkgs))
 names(stop.list) <- core.pkgs
-{ ## -- Stop List for "base" :
-    inet.list <- c(apropos("download\."),"CRAN.packages", "update.packages")
-    socket.fun <- apropos("socket")# those *do* segfault...
-    ## "Interactive" ones:
-    dev.int <- c("X11", "windows", "macintosh")
-    misc.int <- c("bug.report", "browser")
-    stop.list[["base"]] <- c(inet.list, socket.fun, dev.int, misc.int)
-}
+
+## -- Stop List for "base" :
+stop.list[["base"]] <-
+    if(nchar(getenv("R_TESTLOTS"))) {## SEVERE TESTING, try almost ALL
+	"bug.report"
+    } else {
+	inet.list <- c(apropos("download\."),
+                       apropos("^url\."), apropos("\.url"),
+                       "CRAN.packages", "update.packages")
+	socket.fun <- apropos("socket")# those *do* segfault...
+	## "Interactive" ones:
+	dev.int <- c("X11", "windows", "macintosh")
+        edit.int <- c("fix", "edit", "vi", "emacs", "pico", "xemacs", "xedit")
+	misc.int <- c("bug.report", "browser",
+		      "data.entry", "dataentry", "de", apropos("^de\."))
+	c(inet.list, socket.fun, dev.int, edit.int, misc.int)
+    }
 
 for (pkg in core.pkgs) {
   cat("### Package ", pkg, "\n",
-      "###         ", rep("~",nchar(pkg)), "\n", collapse="", sep="")
+      "###	   ", rep("~",nchar(pkg)), "\n", collapse="", sep="")
   if(pkg == "base") {
       this.pos <- length(search())
   }
@@ -32,13 +41,13 @@ for (pkg in core.pkgs) {
 
   for(nm in ls(pos = this.pos)) {
       if(!(nm %in% stop.list[[pkg]]) &&
-         is.function(f <- get(nm,pos = this.pos))) {
-          cat("\n## ", nm, " :\n")
-          cat("f <- get(\"",nm,"\", pos = ", this.pos, ")\n", sep="")
-          cat("f()\nf(NULL)\nf(NULL,NULL)\n",
-              "f(list())\nf(logical(0))\nf(character(0))\n",
-              "f(FALSE)\n",
-              sep="")
+	 is.function(f <- get(nm,pos = this.pos))) {
+	  cat("\n## ", nm, " :\n")
+	  cat("f <- get(\"",nm,"\", pos = ", this.pos, ")\n", sep="")
+	  cat("f()\nf(NULL)\nf(NULL,NULL)\n",
+	      "f(list())\nf(logical(0))\nf(character(0))\n",
+	      "f(FALSE)\n",
+	      sep="")
       }
   }
   if(pkg != "base") {
