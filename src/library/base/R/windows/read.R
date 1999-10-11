2fdcba4c89da693dfc9d54da32d16895a89c1a39
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
1999-10-11 09:11:30 +0000

    move windows R files to base/R/windows
    git-svn-id: https://svn.r-project.org/R/trunk@6135 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/windows/read.R
+++ src/library/base/R/windows/read.R
@@ -4,8 +4,9 @@ read.fwf <- function(file, widths, sep = "", as.is = FALSE,
     FILE <- tempfile("R.")
     on.exit(unlink(FILE))
     args <- paste("-f", deparse(paste("A", widths, sep = "", collapse = " ")),
-                  "-s", deparse(sep), "-o", FILE, file)
-    cmd <- paste(file.path(R.home(), "bin", "fwf2table"))
+                  "-s", deparse(sep), "-o",
+                  paste('"', FILE, '"', sep=''), paste('"', file, '"', sep=''))
+    cmd <- paste('"',file.path(R.home(), "bin", "fwf2table"), '"', sep='')
     system(paste("perl", cmd, args))
     read.table(file = FILE, header = FALSE, sep = sep, as.is = as.is,
 	       skip = skip, row.names = row.names, col.names = col.names)
