b97b0acaf71cffb6b037c9c78be01c555d78c95c
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
1999-10-11 10:47:28 +0000

    some #ifdefs in help files make system in read.fwf invisible on Windows
    git-svn-id: https://svn.r-project.org/R/trunk@6137 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/windows/read.R
+++ src/library/base/R/windows/read.R
@@ -7,7 +7,7 @@ read.fwf <- function(file, widths, sep = "", as.is = FALSE,
                   "-s", deparse(sep), "-o",
                   paste('"', FILE, '"', sep=''), paste('"', file, '"', sep=''))
     cmd <- paste('"',file.path(R.home(), "bin", "fwf2table"), '"', sep='')
-    system(paste("perl", cmd, args))
+    system(paste("perl", cmd, args), invisible=TRUE)
     read.table(file = FILE, header = FALSE, sep = sep, as.is = as.is,
 	       skip = skip, row.names = row.names, col.names = col.names)
 }
