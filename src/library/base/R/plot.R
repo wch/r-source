c7a580f261c396ccea54ba1b5043b4101544e8aa
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
1999-10-20 20:52:27 +0000

    plot.factor(.)
    git-svn-id: https://svn.r-project.org/R/trunk@6233 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/plot.R
+++ src/library/base/R/plot.R
@@ -133,11 +133,17 @@ plot.default <- function(x, y=NULL, type="p", xlim=NULL, ylim=NULL,
 
 plot.factor <- function(x, y, legend.text=levels(y), ...)
 {
-    if (missing(y))
-	barplot(table(x), ...)
-    else if (is.factor(y)) 
-        barplot(table(y, x), legend.text=legend.text, ...)
-    else if (is.numeric(y))
+    if(missing(y) || is.factor(y)) ## <==> will do barplot(.)
+        axisnames <- if(length(dargs <- list(...)) > 0) {
+            nam <- names(dargs)
+            ((any("axes" == nam) && dargs$axes) ||
+             (any("xaxt" == nam) && dargs$xaxt != "n"))
+        } else TRUE
+    if (missing(y)) {
+	barplot(table(x), axisnames=axisnames, ...)
+    } else if (is.factor(y)) {
+        barplot(table(y, x), legend.text=legend.text, axisnames=axisnames, ...)
+    } else if (is.numeric(y))
 	boxplot(y ~ x, ...)
     else NextMethod("plot")
 }
@@ -160,12 +166,12 @@ plot.formula <- function(formula, data = NULL, subset, na.action,
 	    opar <- par(ask = ask)
 	    on.exit(par(opar))
 	}
-        if (is.null(list(...)[["xlab"]])) {
-            for (i in varnames[-response]) plot(mf[[i]], y, xlab = i, 
-                                                ylab = ylab, ...)
-        } else {
-            for (i in varnames[-response]) plot(mf[[i]], y, ylab = ylab, ...)
-        }
+	xn <- .Alias(varnames[-response])
+	if (is.null(list(...)[["xlab"]])) {
+	    for (i in xn) plot(mf[[i]], y, ylab = ylab, xlab = i, ...)
+	} else {
+	    for (i in xn) plot(mf[[i]], y, ylab = ylab, ...)
+	}
     }
     else plot.data.frame(mf)
 }
