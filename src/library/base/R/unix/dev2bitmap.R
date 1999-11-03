942d2636aa5650df2c76ab875e182e2221ee70f7
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
1999-11-03 18:11:07 +0000

    split up system.*.R files
    git-svn-id: https://svn.r-project.org/R/trunk@6334 00db46b3-68df-0310-9c12-caf00c1e9a41

---
+++ src/library/base/R/unix/dev2bitmap.R
@@ -0,0 +1,30 @@
+dev2bitmap <- function(file, type="png256", height=6, width=6, res=72,
+                   pointsize, ...)
+{
+    gsexe <- getenv("R_GSCMD")
+    if(is.null(gsexe) || nchar(gsexe) == 0) {
+        gsexe <- "gs"
+        rc <- system(paste(gsexe, "-help > /dev/null"))
+        if(rc != 0) stop("Sorry, gs cannot be found")
+    }
+    gshelp <- system(paste(gsexe, "-help"), intern=TRUE)
+    st <- grep("^Available", gshelp)
+    en <- grep("^Search", gshelp)
+    gsdevs <- gshelp[(st+1):(en-1)]
+    devs <- c(strsplit(gsdevs, " "), recursive=TRUE)
+    if(match(type, devs, 0) == 0)
+        stop(paste(paste("Device ", type, "is not available"),
+                   "Available devices are",
+                   paste(gsdevs, collapse="\n"), sep="\n"))
+    if(missing(pointsize)) pointsize <- 1.5*min(width, height)
+    tmp <- tempfile("Rbit")
+    on.exit(unlink(tmp))
+    dev.print(device=postscript, file=tmp, width=width, height=height,
+              pointsize=pointsize, paper="special", horizontal=FALSE, ...)
+    cmd <- paste(gsexe, " -dNOPAUSE -dBATCH -q -sDEVICE=", type,
+                 " -r", res,
+                 " -g", ceiling(res*width), "x", ceiling(res*height),
+                 " -sOutputFile=", file, " ", tmp, sep="")
+    system(cmd)
+    invisible()
+}
---
+++ src/library/base/R/unix/zip.R
@@ -0,0 +1,16 @@
+zip.file.extract <- function(file, zipname="R.zip")
+{
+    ## somewhat system-specific.
+    unzip <- options()$unzip
+    if(!length(unzip)) return(file)
+    path <- sub("[^/]*$","", file)
+    topic <- substr(file, nchar(path)+1, 1000)
+    if(file.exists(file.path(path, zipname))) {
+        tempdir <- sub("[^/]*$", "", tempfile())
+        if(!system(paste(unzip, "-o",
+                         file.path(path, zipname), topic, "-d", tempdir,
+                         " > /dev/null")))
+            file <- paste(tempdir,  topic, sep="")
+    }
+    file
+}
