15e876a6b6154cca274eb33dfe70da660fad22d4
(no author) <(no author)@00db46b3-68df-0310-9c12-caf00c1e9a41>
1999-11-07 10:05:04 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.6.4'.
    git-svn-id: https://svn.r-project.org/R/branches/unlabeled-1.6.4@6370 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/notyet.R
+++
@@ -1,8 +0,0 @@
-.NotYetImplemented <- function() {
-    stop(paste("`", as.character(sys.call(sys.parent())[[1]]), "' ",
-	       "is not implemented yet", sep = ""))
-}
-
-.NotYetUsed <- function(x) {
-    warning(paste("argument `", x, "' is not used (yet)", sep = ""))
-}
