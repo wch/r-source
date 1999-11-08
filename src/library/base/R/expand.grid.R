7aed4ed9e3f38cfcae659e9b744e2bc76e1e6423
pd <pd@00db46b3-68df-0310-9c12-caf00c1e9a41>
1999-11-08 18:59:39 +0000

    merged from main
    git-svn-id: https://svn.r-project.org/R/branches/unlabeled-1.6.4@6398 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/expand.grid.R
+++ src/library/base/R/expand.grid.R
@@ -3,21 +3,17 @@
 ##
 expand.grid <- function(...) {
     ## x should either be a list or a set of vectors or factors
-    args <- list(...)
-    if(!length(args)) return(NULL)
-    a1 <- args[[1]]
-    if(length(args) == 1 && is.list(a1)) args <- a1
-    nargs <- length(args)
-    if (nargs == 1)
-        return(
-               if (is.null(args[[1]])) as.data.frame(list())
-               else data.frame(args)
-               )
+    nargs <- length(args <- list(...))
+    if(! nargs) return(as.data.frame(list()))
+    if(nargs == 1 && is.list(a1 <- args[[1]]))
+        nargs <- length(args <- a1)
+    if(nargs <= 1)
+        return(as.data.frame(if(nargs==0||is.null(args[[1]])) list() else args))
     cargs <- args
     nmc <- paste("Var", 1:nargs, sep="")
     nm <- names(args)
     if(is.null(nm)) nm <- nmc
-    nmc[nchar(nm)>0] <- nm[nchar(nm)>0]
+    if(any(ng0 <- nchar(nm) > 0)) nmc[ng0] <- nm[ng0]
     names(cargs) <- nmc
     rep.fac <- 1
     orep <- final.len <- prod(sapply(args, length))
