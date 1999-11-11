4510f2a4fcc979b0e11863fc5237c930f1c74a14
leisch <leisch@00db46b3-68df-0310-9c12-caf00c1e9a41>
1999-11-11 11:40:23 +0000

    new files
    git-svn-id: https://svn.r-project.org/R/trunk@6443 00db46b3-68df-0310-9c12-caf00c1e9a41

---
+++ src/library/base/R/parse.dcf.R
@@ -0,0 +1,89 @@
+parse.dcf <- function(text=NULL, file="", fields=NULL, versionfix=FALSE)
+{
+
+    parse.dcf.entry <- function(text, fields=NULL, versionfix=FALSE)
+    {
+        contlines <- grep("^[ \t]+", text)
+        
+        if(is.null(fields)){
+            if(length(contlines))
+                fields <- sub("^([^:]*):.*$", "\\1", text[-contlines])
+            else
+                fields <- sub("^([^:]*):.*$", "\\1", text)
+        }
+        
+        retval <- as.list(rep(NA, length(fields)))
+        names(retval) <- fields
+        
+        for(d in 1:length(text)){
+            if(any(contlines == d))
+                y <- sub("^[ \t]+(.*)$", "\\1", text[d])
+            else{
+                x <- sub("^([^:]*):.*$", "\\1", text[d])
+                y <- sub("^[^:]*:[ \t]*(.*)$", "\\1", text[d])
+            }
+            
+            if(versionfix & x=="Version")
+                y <- unlist(strsplit(y, " "))[1]
+            
+            if(any(fields==x))
+                if(is.na(retval[[x]]))
+                    retval[[x]] <- y
+                else
+                    retval[[x]] <- paste(retval[[x]], y, sep="\n")
+        }
+        retval
+    }
+
+    if(missing(text))
+        text <- scan(file=file, what="",  sep="\n", quiet=TRUE)
+    
+    ## remove empty lines
+    ok <- grep("^[ \t]+$", text)
+    if(length(ok)>0){
+        text <- text[!ok]
+    }
+
+    ## use the field name of the first line as record separator
+    recsep <- sub("^([^:]*):.*$", "\\1", text[1])
+
+    start <- grep(paste("^", recsep, ":", sep=""), text)
+    start <- c(start, length(text)+1)
+    retval <- list()
+    for(k in 1:(length(start)-1)){
+        retval[[k]] <- parse.dcf.entry(text[start[k]:(start[k+1]-1)],
+                                               fields=fields,
+                                               versionfix=versionfix)
+    }
+
+    if(!is.null(fields))
+        retval <- t(sapply(retval, unlist))
+    else if(length(retval)==1)
+        retval <- unlist(retval, recursive=FALSE)
+
+    retval
+}
+
+
+package.contents <- function(pkg, lib=.lib.loc){
+    
+    contentsfile <- system.file("CONTENTS", pkg=pkg, lib=lib)
+    if(contentsfile == "")
+        stop("Cannot find CONTENTS file")
+    
+    contents <- scan("", file=contentsfile, sep="\n", quiet=TRUE)
+    parse.dcf(contents, fields=c("Entry", "Keywords", "Description"))
+}
+
+package.description <- function(pkg, lib=.lib.loc){
+    
+    contentsfile <- system.file("DESCRIPTION", pkg=pkg, lib=lib)
+    if(contentsfile == "")
+        stop("Cannot find DESCRIPTION file")
+
+    contents <- scan("", file=contentsfile, sep="\n", quiet=TRUE)
+    parse.dcf(contents, versionfix=TRUE)
+}
+
+
+
