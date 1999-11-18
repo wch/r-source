140b315b8c2339082fb4658d493e9384fb0735c1
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
1999-11-18 11:06:25 +0000

    don`t bail out on empty CONTENTS; +verbose
    git-svn-id: https://svn.r-project.org/R/trunk@6607 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/help.search.R
+++ src/library/base/R/help.search.R
@@ -1,6 +1,7 @@
 help.search <- function(what, fields = c("name", "title"),
                         apropos, keyword, whatis, ignore.case = TRUE,
-                        package = NULL, lib.loc = .lib.loc) {
+                        packages = NULL, lib.loc = .lib.loc,
+                        verbose = .Options$verbose) {
     TABLE <- c("name", "alias", "title", "keyword")
     if (!missing(what)) {
         if (!is.character(what) || (length(what) > 1))
@@ -34,35 +35,46 @@ help.search <- function(what, fields = c("name", "title"),
     } else {
         stop("don't know what to search")
     }
-    
+
+    if(verbose) RepC <- function(n, ch = " ") paste(rep(ch, n), collapse="")
     db <- NULL
     for (lib in lib.loc) {
-        if (!is.null(package))
-            pkgs <- package
-        else
-            pkgs <- .packages(all.available = TRUE, lib.loc = lib)
+        if(verbose) {
+            cat("\nLIBRARY ",lib,"\n",
+                RepC(8), RepC(ch="=", nchar(lib)),"\n",sep="")
+            np <- 0
+        }
+        pkgs <- {
+            if(is.null(packages)) .packages(all.available = TRUE, lib.loc = lib)
+            else packages }
         for (p in pkgs) {
+            if(verbose) cat("", p, if((np <- np + 1)%% 5 == 0) "\n")
             cfile <- system.file("CONTENTS", pkg = p, lib = lib)
             if (cfile != "") {
-                ctext <- scan("", file = cfile, sep = "\n", quiet =
-                              TRUE)
-                ctext <- parse.dcf(ctext,
-                                   fields = c("Entry", "Aliases",
-                                   "Description", "Keywords"))
-                nr <- NROW(ctext)
-                db <- rbind(db, cbind(rep(p, nr),
-                                      rep(lib, nr),
-                                      ctext))
+                ctext <- scan("", file = cfile, sep = "\n", quiet = TRUE)
+                if(length(ctext)) {
+                    ctext <- parse.dcf(ctext,
+                                       fields = c("Entry", "Aliases",
+                                       "Description", "Keywords"))
+                    nr <- NROW(ctext)
+                    db <- rbind(db,
+                                cbind(rep(p, nr), rep(lib, nr), ctext))
+                } else
+                warning(paste("Empty \"CONTENTS\" file of pkg", p, "in", lib))
             }
         }
+        if(verbose && np %% 5) cat("\n")
     }
     colnames(db) <- c("pkg", "lib", TABLE)
 
+    if(verbose) cat("\nDatabase of dimension", dim(db))
     i <- NULL
-    for (f in fields) {
+    for (f in fields)
         i <- c(i, grep(what, db[, f], ignore.case = ignore.case))
-    }
+
     db <- db[sort(unique(i)), , drop = FALSE]
+    if(verbose) cat(", matched", NROW(db),"entries.\n")
+
     fields <- paste(fields, collapse = " or ")
     if (NROW(db) > 0) {
         FILE <- tempfile()
@@ -73,7 +85,7 @@ help.search <- function(what, fields = c("name", "title"),
         dbtit <- paste(db[ , "title"], sep = "")
         cat(paste(format(dbnam), dbtit, sep = "   "),
             sep = "\n", file = FILE, append = TRUE)
-        file.show(FILE, delete.file = TRUE)        
+        file.show(FILE, delete.file = TRUE)
     } else {
         cat(paste("No objects found with ", fields, " matching `", what,
                   "'\n", sep = ""))
