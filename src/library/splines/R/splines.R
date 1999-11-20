f3f4d5f493276845295a8b0d34ca4562974efee0
bates <bates@00db46b3-68df-0310-9c12-caf00c1e9a41>
1999-11-20 03:35:40 +0000

    Commented out interp.spline, periodic.spline, spline.value, and linear.interp as I don't think they are needed any more.  We'll see if anyone shrieks.
    git-svn-id: https://svn.r-project.org/R/trunk@6637 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/splines/R/splines.R
+++ src/library/splines/R/splines.R
@@ -1,4 +1,4 @@
-### $Id: splines.R,v 1.1 1999/11/10 21:14:35 bates Exp $
+### $Id: splines.R,v 1.2 1999/11/20 03:35:40 bates Exp $
 
 bs <-
   function(x, df = NULL, knots = NULL, degree = 3, intercept = FALSE,
@@ -152,92 +152,92 @@ predict.ns <-
   do.call("ns", a)
 }
 
-interp.spline <-
-  function(x, y, ord = 4)
-{
-  x <- as.numeric(x)
-  lenx <- length(x)
-  y <- as.numeric(y)
-  leny <- length(y)
-  if(leny!=lenx)
-    stop("Lengths of x and y must be equal")
-  ind <- order(x)
-  x <- x[ind]
-  y <- y[ind]
-  ordm1 <- ord - 1
-  knots <- c(x[1:ordm1] + x[1] - x[ord], x,
-             x[lenx + (1:ordm1) - ordm1] + x[lenx] - x[lenx - ordm1])
-  derivs <- c(2, integer(lenx), 2)
-  x <- c(x[1], x, x[lenx])
-  sys.mat <- spline.des(knots, x, ord = ord, derivs = derivs)$design
-  coeff <- solve(sys.mat, c(0, y, 0))
-  list(knots = knots, coeff = coeff, order = ord)
-}
+# interp.spline <-
+#   function(x, y, ord = 4)
+# {
+#   x <- as.numeric(x)
+#   lenx <- length(x)
+#   y <- as.numeric(y)
+#   leny <- length(y)
+#   if(leny!=lenx)
+#     stop("Lengths of x and y must be equal")
+#   ind <- order(x)
+#   x <- x[ind]
+#   y <- y[ind]
+#   ordm1 <- ord - 1
+#   knots <- c(x[1:ordm1] + x[1] - x[ord], x,
+#              x[lenx + (1:ordm1) - ordm1] + x[lenx] - x[lenx - ordm1])
+#   derivs <- c(2, integer(lenx), 2)
+#   x <- c(x[1], x, x[lenx])
+#   sys.mat <- spline.des(knots, x, ord = ord, derivs = derivs)$design
+#   coeff <- solve(sys.mat, c(0, y, 0))
+#   list(knots = knots, coeff = coeff, order = ord)
+# }
 
-periodic.spline <-
-  function(x, y, knots, period = 2 * pi, ord = 4)
-{
-  x <- as.vector(x, "double")
-  y <- as.vector(y, "double")
-  lenx <- length(x)
-  if(lenx!=length(y))
-    stop("Lengths of x and y must match")
-  ind <- order(x)
-  x <- x[ind]
-  y <- y[ind]
-  if(any((x[-1] - x[ - lenx]) <= 0))
-    stop("Values of x must be strictly increasing")
-  if(!missing(knots))
-    period <- knots[length(knots) + 1 - ord] - knots[1]
-  else knots <- c(x[(lenx - (ord - 2)):lenx] - period, x, x[1:ord] + 
-                  period)
-  if((x[lenx] - x[1]) >= period)
-    stop("The range of x values exceeds one period")
-  coeff.mat <- spline.des(knots, x, ord)$design
-  sys.mat <- coeff.mat[, (1:lenx)]
-  sys.mat[, 1:(ord - 1)] <-
-    sys.mat[, 1:(ord - 1)] + coeff.mat[, lenx + (1:(ord - 1))]
-  qrstr <- qr(sys.mat)
-  coeff <- qr.coef(qrstr, y)
-  coeff <- c(coeff, coeff[1:(ord - 1)])
-  list(knots = knots, coeff = coeff, order = ord,
-       period = period, m = coeff.mat)
-}
+# periodic.spline <-
+#   function(x, y, knots, period = 2 * pi, ord = 4)
+# {
+#   x <- as.vector(x, "double")
+#   y <- as.vector(y, "double")
+#   lenx <- length(x)
+#   if(lenx!=length(y))
+#     stop("Lengths of x and y must match")
+#   ind <- order(x)
+#   x <- x[ind]
+#   y <- y[ind]
+#   if(any((x[-1] - x[ - lenx]) <= 0))
+#     stop("Values of x must be strictly increasing")
+#   if(!missing(knots))
+#     period <- knots[length(knots) + 1 - ord] - knots[1]
+#   else knots <- c(x[(lenx - (ord - 2)):lenx] - period, x, x[1:ord] + 
+#                   period)
+#   if((x[lenx] - x[1]) >= period)
+#     stop("The range of x values exceeds one period")
+#   coeff.mat <- spline.des(knots, x, ord)$design
+#   sys.mat <- coeff.mat[, (1:lenx)]
+#   sys.mat[, 1:(ord - 1)] <-
+#     sys.mat[, 1:(ord - 1)] + coeff.mat[, lenx + (1:(ord - 1))]
+#   qrstr <- qr(sys.mat)
+#   coeff <- qr.coef(qrstr, y)
+#   coeff <- c(coeff, coeff[1:(ord - 1)])
+#   list(knots = knots, coeff = coeff, order = ord,
+#        period = period, m = coeff.mat)
+# }
 
-spline.value <-
-  function(bspstr, x = seq(knots[ord], knots[ncoeff + 1], len = lenx),
-           deriv = 0, lenx = length(x))
-{
-  if(is.null(bspstr$knots))
-    stop("bspstr must be a B-spline structure")
-  if(missing(x) && missing(lenx))
-    stop("either x or lenx must be given to spline.value")
-  knots <- bspstr$knots
-  ord <- bspstr$order
-  ncoeff <- length(bspstr$coeff)
-  x.orig <- x
-  if(!is.null(period <- bspstr$period)) {
-    ind <- x < knots[ord]
-    if(any(ind))
-      x[ind] <- x[ind] + period * (1 + (knots[ord] - x[ind]) %/% period)
-    ind <- x > knots[ncoeff + 1]
-    if(any(ind))
-      x[ind] <- x[ind] - period * (1 + (x[ind] - knots[ncoeff + 1]) %/% period)
-  }
-  xind <- order(x)
-  x <- x[xind]
-  rind <- order(xind)
-  z <- .C("spline_value",
-          as.double(bspstr$knots),
-          as.double(bspstr$coeff),
-          as.integer(length(bspstr$coeff)),
-          as.integer(bspstr$order),
-          as.double(x),
-          as.integer(lenx),
-          as.integer(deriv),
-          y = double(lenx))
-  list(x = x.orig, y = z$y[rind])
-}
+# spline.value <-
+#   function(bspstr, x = seq(knots[ord], knots[ncoeff + 1], len = lenx),
+#            deriv = 0, lenx = length(x))
+# {
+#   if(is.null(bspstr$knots))
+#     stop("bspstr must be a B-spline structure")
+#   if(missing(x) && missing(lenx))
+#     stop("either x or lenx must be given to spline.value")
+#   knots <- bspstr$knots
+#   ord <- bspstr$order
+#   ncoeff <- length(bspstr$coeff)
+#   x.orig <- x
+#   if(!is.null(period <- bspstr$period)) {
+#     ind <- x < knots[ord]
+#     if(any(ind))
+#       x[ind] <- x[ind] + period * (1 + (knots[ord] - x[ind]) %/% period)
+#     ind <- x > knots[ncoeff + 1]
+#     if(any(ind))
+#       x[ind] <- x[ind] - period * (1 + (x[ind] - knots[ncoeff + 1]) %/% period)
+#   }
+#   xind <- order(x)
+#   x <- x[xind]
+#   rind <- order(xind)
+#   z <- .C("spline_value",
+#           as.double(bspstr$knots),
+#           as.double(bspstr$coeff),
+#           as.integer(length(bspstr$coeff)),
+#           as.integer(bspstr$order),
+#           as.double(x),
+#           as.integer(lenx),
+#           as.integer(deriv),
+#           y = double(lenx))
+#   list(x = x.orig, y = z$y[rind])
+# }
 
 spline.des <- function(knots, x, ord = 4, derivs = integer(nx))
 {
@@ -271,30 +271,30 @@ spline.des <- function(knots, x, ord = 4, derivs = integer(nx))
   list(knots = knots, order = ord, derivs = derivs, design = design[ind,  ])
 }
 
-linear.interp <-
-  function(x, y, x0 = seq(x[1], x[lenx], len = lenx0), lenx0 = length(x0))
-{
-  x <- as.numeric(x)
-  y <- as.numeric(y)
-  lenx <- length(x)
-  if(length(y)!=lenx)
-    stop("lengths of x and y must be the same")
-  xind <- order(x)
-  x <- x[xind]
-  y <- y[xind]
-  if(missing(x0) && missing(lenx0))
-    stop("either x0 or lenx0 must be given")
-  x0 <- as.numeric(x0)
-  x0ind <- order(x0)
-  x0ord <- x0[x0ind]
-  nvals <- length(x0)
-  if(x0ord[1] < x[1] || x0ord[nvals] > x[lenx])
-    stop(paste("x0 values must be in the range", x[1], "to", x[lenx]))
-  y <- .C("lin_interp",
-          x,
-          y,
-          x0,
-          y = double(nvals),
-          as.integer(nvals))$y
-  list(x = x0, y = y[order(x0ind)])
-}
+# linear.interp <-
+#   function(x, y, x0 = seq(x[1], x[lenx], len = lenx0), lenx0 = length(x0))
+# {
+#   x <- as.numeric(x)
+#   y <- as.numeric(y)
+#   lenx <- length(x)
+#   if(length(y)!=lenx)
+#     stop("lengths of x and y must be the same")
+#   xind <- order(x)
+#   x <- x[xind]
+#   y <- y[xind]
+#   if(missing(x0) && missing(lenx0))
+#     stop("either x0 or lenx0 must be given")
+#   x0 <- as.numeric(x0)
+#   x0ind <- order(x0)
+#   x0ord <- x0[x0ind]
+#   nvals <- length(x0)
+#   if(x0ord[1] < x[1] || x0ord[nvals] > x[lenx])
+#     stop(paste("x0 values must be in the range", x[1], "to", x[lenx]))
+#   y <- .C("lin_interp",
+#           x,
+#           y,
+#           x0,
+#           y = double(nvals),
+#           as.integer(nvals))$y
+#   list(x = x0, y = y[order(x0ind)])
+# }
