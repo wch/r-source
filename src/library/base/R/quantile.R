d5d06eb31779b2dde28edf82678860d7f03978ac
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
1999-12-03 19:26:02 +0000

    IQR: names=F
    git-svn-id: https://svn.r-project.org/R/branches/R-0-90-patches@6846 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/quantile.R
+++ src/library/base/R/quantile.R
@@ -35,5 +35,4 @@ quantile.default <-
 }
 
 IQR <- function (x, na.rm = FALSE)
-    as.vector(diff(quantile(as.numeric(x), c(0.25, 0.75), na.rm=na.rm)))
-
+    diff(quantile(as.numeric(x), c(0.25, 0.75), na.rm = na.rm, names = FALSE))
