2deab3a5dd4b5aa6a5f01fcafffc8e7b91927496
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
1999-12-10 16:22:11 +0000

    CheckFormals(); etc: fixed PR#361 (RG+MM)
    git-svn-id: https://svn.r-project.org/R/branches/R-0-90-patches@6969 00db46b3-68df-0310-9c12-caf00c1e9a41

--- tests/make-no-segfault.R
+++ tests/make-no-segfault.R
@@ -25,7 +25,7 @@ stop.list[["base"]] <-
 	## deprecated anyway --pd
 	misc.2 <- c("help.start", "print.plot",
 		    "gctorture", "q", "quit",
-                    "function",##<<<< SHOWS INFINITE LOOPS + BUS error
+                    ## now `works': "function",
 		    "data.entry", "dataentry", "de", apropos("^de\."))
 	c(inet.list, socket.fun, dev.int, edit.int, misc.int, misc.2)
     }
