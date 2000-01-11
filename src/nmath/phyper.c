788c3059b8632a9147b10d3d36c8de70b774c3d8
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-01-11 23:08:22 +0000

    "R Development Core"..
    git-svn-id: https://svn.r-project.org/R/trunk@7526 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/nmath/phyper.c
+++ src/nmath/phyper.c
@@ -1,7 +1,7 @@
 /*
  *  Mathlib : A C Library of Special Functions
  *  Copyright (C) 1998 Ross Ihaka
- *  Copyright (C) 1999 R Core Team
+ *  Copyright (C) 1999 The R Development Core Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
