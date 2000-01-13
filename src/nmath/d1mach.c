adc0124df1d1759398da9a11a9c62257b5b3cf79
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-01-13 07:51:53 +0000

    comments only added
    git-svn-id: https://svn.r-project.org/R/trunk@7546 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/nmath/d1mach.c
+++ src/nmath/d1mach.c
@@ -1,6 +1,7 @@
 /*
  *  Mathlib - A Mathematical Function Library
  *  Copyright (C) 1998  Ross Ihaka
+ *  Copyright (C) 2000 The R Development Core Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -21,16 +22,28 @@
 
 #include "Mathlib.h"
 
+/*-- FIXME:  Eliminate calls to these
+ *   =====   o   from C code when
+ *	     o   it is only used to initialize "static" variables (threading)
+ *  and use the DBL_... constants instead
+ */
+
 double d1mach(int i)
 {
     switch(i) {
     case 1: return DBL_MIN;
     case 2: return DBL_MAX;
 
-    case 3: return pow((double)i1mach(10), -(double)i1mach(14));
-    case 4: return pow((double)i1mach(10), 1-(double)i1mach(14));
+    case 3: /* = FLT_RADIX  ^ - DBL_MANT_DIG
+	      for IEEE:  = 2^-53 = 1.110223e-16 = .5*DBL_EPSILON */
+	return pow((double)i1mach(10), -(double)i1mach(14));
 
-    case 5: return log10(2.0);
+    case 4: /* = FLT_RADIX  ^ (1- DBL_MANT_DIG) =
+	      for IEEE:  = 2^52 = 4503599627370496 = 1/DBL_EPSILON */
+	return pow((double)i1mach(10), 1-(double)i1mach(14));
+
+    case 5: return log10(2.0);/* = M_LOG10_2 in ../include/Mathlib.h */
+
 
     default: return 0.0;
     }
