3fbc86ce5786e900aac9fd5addf5f3522ec4462a
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-01-17 15:34:38 +0000

    more
    git-svn-id: https://svn.r-project.org/R/trunk@7624 00db46b3-68df-0310-9c12-caf00c1e9a41

--- tests/arith.R
+++ tests/arith.R
@@ -1,3 +1,10 @@
-## fft():
 options(digits=7)
+
+## powers, including complex ones
+outer(a <- -4:12,     -2:7, "^")
+m <- outer(-4:12 +0i, b <- seq(-.5,2, by=.5), "^")
+dimnames(m) <- list(paste(a), "^" = sapply(b,format))
+round(m,3)
+
+## fft():
 for(n in 1:30) cat("\nn=",n,":", round(fft(1:n), 8),"\n")
