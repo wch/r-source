730af18469dc02a485755a6827b19cf42c44a35e
pd <pd@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-01-20 23:30:16 +0000

    Data frame editing
    git-svn-id: https://svn.r-project.org/R/trunk@7681 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/edit.R
+++ src/library/base/R/edit.R
@@ -1,13 +1,76 @@
-edit <- function(name=NULL, file="", editor=options()$editor)
-    .Internal(edit(name,file, editor))
+edit <-
+    function(name,...)UseMethod("edit")
 
-vi <- function(name=NULL, file="") edit(name, file, editor="vi")
+edit.default<-
+    function (name = NULL, file = "", editor = options()$editor)
+    .Internal(edit(name, file, editor))
 
-emacs <- function(name=NULL, file="") edit(name, file, editor="emacs")
+edit.data.frame<-
+    function(name,factor.mode=c("numeric","character"),...)
+{
+    if (getenv("DISPLAY")=="")
+        return (edit.default(name,...))
 
-xemacs <- function(name=NULL, file="") edit(name, file, editor="xemacs")
+    if (!all(sapply(name,is.vector)|sapply(name,is.factor)))
+        stop("Can only handle vector and factor elements")
 
-xedit <- function(name=NULL, file="") edit(name, file, editor="xedit")
+    factor.mode <- match.arg(factor.mode)
 
-pico <- function(name=NULL, file="") edit(name, file, editor="pico")
+    as.num.or.char<-function(x)
+    {
+        if (is.character(x))
+            x
+        else if (is.factor(x) && factor.mode == "character")
+            as.character(x)
+        else
+            as.numeric(x)
+    }
+
+    attrlist <- lapply(name, attributes)
+    datalist <- lapply(name, as.num.or.char)
+    factors <- which(sapply(name,is.factor))
+    modes <- lapply(datalist, mode)
+    out <- .Internal(dataentry(datalist,modes))
+    lengths <- sapply(out, length)
+    maxlength <- max(lengths)
+    for (i in which(lengths != maxlength))
+         out[[i]] <- c(out[[i]],rep(NA,maxlength-lengths[i]))
+    for (i in factors) {
+        a <- attrlist[[i]]
+        if (factor.mode == "numeric") {
+            o <- as.integer(out[[i]])
+            ok <- is.na(o) | o > 0 & o <= length(a$levels)
+            if (any(!ok)) {
+                warning(paste("invalid factor levels in",
+                               names(out)[i]))
+                o[o <= 0] <- NA
+                o[o > length(a$levels)] <- NA
+            }
+        } else {
+            o <- out[[i]]
+            if (any(is.na(match(o,c(a$levels,NA)))))
+                warning(paste("invalid factor levels in",
+                               names(out)[i]))
+            o <- match(o, a$levels)
+        }
+        attributes(o) <- a
+        out[[i]] <- o
+    }
+    as.data.frame(out)
+}
+
+vi <- function(name=NULL, file="")
+    edit.default(name, file, editor="vi")
+
+emacs <- function(name=NULL, file="")
+    edit.default(name, file, editor="emacs")
+
+xemacs <- function(name=NULL, file="")
+    edit.default(name, file, editor="xemacs")
+
+xedit <- function(name=NULL, file="")
+    edit.default(name, file, editor="xedit")
+
+pico <- function(name=NULL, file="")
+    edit.default(name, file, editor="pico")
 
--- src/library/base/R/fix.R
+++ src/library/base/R/fix.R
@@ -1,12 +1,15 @@
-fix <- function(x) {
+"fix" <-
+    function (x, ...)
+{
     subx <- substitute(x)
-    if( is.name(subx) )
-	subx<-deparse(subx)
+    if (is.name(subx))
+        subx <- deparse(subx)
     if (!is.character(subx) || length(subx) != 1)
-	stop("fix requires a name")
-    if(exists(subx, inherits=TRUE))
-	x <- edit(get(subx))
+        stop("fix requires a name")
+    if (exists(subx, inherits = TRUE))
+        x <- edit(get(subx), ...)
     else
-	stop(paste("no object named \"", subx, "\" to edit",sep=""))
-    assign(subx, x, env=.GlobalEnv)
+        x <- edit(function(){},...)
+    assign(subx, x, env = .GlobalEnv)
 }
+
