7e706c07ceb0ccb5db00a1e710273390bf8e5ffb
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-02-01 19:57:03 +0000

    yet another R_D fix; sorry
    git-svn-id: https://svn.r-project.org/R/trunk@7922 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/include/R_ext/Mathlib.h
+++ src/include/R_ext/Mathlib.h
@@ -381,18 +381,19 @@ void ml_error(int n);
 #define R_DT_0	(lower_tail ? R_D__0 : R_D__1)		/* 0 */
 #define R_DT_1	(lower_tail ? R_D__1 : R_D__0)		/* 1 */
 
-#define R_D_Lval(p)	(lower_tail ? p	 : 1 - (p))	/*  p  */
-#define R_D_Cval(p)	(lower_tail ? 1 - (p) : p )	/*  1 - p */
+#define R_D_Lval(p)	(lower_tail ? (p) : (1 - (p)))	/*  p  */
+#define R_D_Cval(p)	(lower_tail ? (1 - (p)) : (p))	/*  1 - p */
 
-#define R_D_val(x)	(log_p	? log(x) : x)		/*  x  in pF(x,..) */
-#define R_D_qIv(p)	(log_p	? exp(p) : p)		/*  p  in qF(p,..) */
-#define R_D_exp(x)	(log_p	?  x	 : exp(x))	/* exp(x) */
-#define R_D_log(p)	(log_p	?  p	 : log(p))	/* log(p) */
+#define R_D_val(x)	(log_p	? log(x) : (x))		/*  x  in pF(x,..) */
+#define R_D_qIv(p)	(log_p	? exp(p) : (p))		/*  p  in qF(p,..) */
+#define R_D_exp(x)	(log_p	?  (x)	 : exp(x))	/* exp(x) */
+#define R_D_log(p)	(log_p	?  (p)	 : log(p))	/* log(p) */
 
 #define R_DT_val(x)	R_D_val(R_D_Lval(x))		/*  x  in pF */
-#define R_DT_qIv(p)	R_D_qIv(R_D_Lval(p))		/*  p  in qF */
 #define R_DT_Cval(x)	R_D_val(R_D_Cval(x))		/*  1 - x */
-#define R_DT_CIv(x)	R_D_qIv(R_D_Cval(x))		/*  1 - x */
+#define R_DT_qIv(p)	R_D_Lval(R_D_qIv(p))		/*  p  in qF ! */
+#define R_DT_CIv(p)	R_D_Cval(R_D_qIv(p))		/*  1 - p in qF */
+
 #define R_DT_exp(x)	R_D_exp(R_D_Lval(x))		/* exp(x) */
 #define R_DT_Cexp(x)	R_D_exp(R_D_Cval(x))		/* exp(1 - x) */
 #define R_DT_log(p)	R_D_log(R_D_Lval(p))		/* log(p) */
@@ -480,7 +481,7 @@ double	lbeta(double, double);
 
 	/* Normal Distribution */
 
-/* These 3 will be re-defined in  ./S_compat.h  (when that is used) : */
+/* These 3 will be re-defined in  ./S_compat.h	(when that is used) : */
 #define pnorm pnorm5
 #define qnorm qnorm5
 #define dnorm dnorm4
