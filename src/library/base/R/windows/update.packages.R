f1e5b140d927795ad7b5364f1a27242d40951b0e
leisch <leisch@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-02-02 10:14:07 +0000

    download.file in seperate file
    git-svn-id: https://svn.r-project.org/R/trunk@7936 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/windows/update.packages.R
+++ src/library/base/R/windows/update.packages.R
@@ -54,39 +54,6 @@ install.packages <- function(pkgs, lib, CRAN=getOption("CRAN"),
 }
 
 
-download.file <- function(url, destfile, method="auto")
-{
-    method <- match.arg(method,
-                        c("auto", "wget", "lynx", "cp","socket"))
-
-    if(method == "auto") {
-        if(length(grep("^file:", url)))
-            method <- "cp"
-        else if(system("wget --help", invisible=TRUE)==0)
-            method <- "wget"
-        else if(shell("lynx -help", invisible=TRUE)==0)
-            method <- "lynx"
-        else if (length(grep("^http:",url))==0)
-            method <- "socket"
-        else
-            stop("No download method found")
-    }
-
-    if(method=="wget")
-        status <- system(paste("wget", url, "-O", destfile))
-    else if(method=="lynx")
-        status <- shell(paste("lynx -dump", url, ">", destfile))
-    else if(method=="cp") {
-        url <- sub("^file:", "", url)
-        status <- system(paste("cp", url, destfile))
-    }
-    else if (method=="socket"){
-        status<-0
-        httpclient(url,check.MIME.type=TRUE,file=destfile)
-    }
-    invisible(status)
-}
-
 download.packages <- function(pkgs, destdir, available=NULL,
                               CRAN=getOption("CRAN"),
                               contriburl=contrib.url(CRAN),
