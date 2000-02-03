67fd81348dc51616cd5257c91d6bc241b8a3c5d9
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-02-03 09:42:06 +0000

    enhanced princomp
    git-svn-id: https://svn.r-project.org/R/trunk@7948 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/mva/R/princomp.R
+++ src/library/mva/R/princomp.R
@@ -1,35 +1,43 @@
-princomp <- function(x, cor = FALSE, scores = TRUE,
+princomp <- function(x, cor = FALSE, scores = TRUE, covmat = NULL,
                      subset = rep(TRUE, nrow(as.matrix(x)))) {
-    ## it is tempting to add use="all.obs" which could be passed to cov
-    ## or cor but then the calculation of N is complicated.
-    z<- as.matrix(x)[subset, , drop = FALSE]
-    N <- nrow(z)
-    if (cor)
-        cv <- get("cor", envir = .GlobalEnv)(z)
-    else
-        cv <- cov(z)
-    ## (svd can be used but gives different signs for some vectors)
+    if (!missing(x)) z <- as.matrix(x)[subset, , drop = FALSE]
+    else z <- NULL
+    if (is.list(covmat)) {
+        if(any(is.na(match(c("cov", "n.obs"), names(covmat)))))
+            stop("covmat is not a valid covariance list")
+        cv <- covmat$cov
+        n.obs <- covmat$n.obs
+        cen <- covmat$center
+    } else if(is.matrix(covmat)) {
+        cv <- covmat
+        n.obs <- NA
+        cen <- NULL
+    } else if(is.null(covmat)){
+        covmat <- cov.wt(z)
+        n.obs <- covmat$n.obs
+        cv <- covmat$cov * (1 - 1/n.obs) # for S-PLUS compatibility
+        cen <- covmat$center
+    } else stop("covmat is of unknown type")
+    if (cor) {
+        sds <- sqrt(diag(cv))
+        cv <- cv/(sds %o% sds)
+    }
     edc <- eigen(cv)
+    if (any(edc$values < 0))
+        stop("covariance matrix is not non-negative definite")
     cn <- paste("Comp.", 1:ncol(cv), sep = "")
     names(edc$values) <- cn
     dimnames(edc$vectors) <- list(dimnames(x)[[2]], cn)
+    sdev <- sqrt(edc$values)
+    if (cor) sc <- sds
+    else sc <- rep(1, ncol(z))
+    names(sc) <- colnames(cv)
     scr <- NULL
-    if (cor) {
-        sdev <- sqrt(edc$values)
-        sc <- (apply(z, 2, var)*(N-1)/N)^0.5
-        if (scores)
-            scr<- (scale(z, center = TRUE, scale = TRUE)
-                   %*% edc$vectors)*sqrt(N/(N-1))
-    } else {
-        sdev <- sqrt(edc$values*(N-1)/N)
-        sc <- rep(1, ncol(z))
-        if (scores)
-            scr<- (scale(z, center = TRUE, scale = FALSE)
-                   %*% edc$vectors)
-   }
-    names(sc) <- dimnames(x)[[2]]
+    if (scores && !missing(x))
+        scr <- scale(z, center = TRUE, scale = FALSE) %*% edc$vectors
+    if (is.null(cen)) cen <- rep(NA, nrow(cv))
     edc <-list(sdev = sdev, loadings = edc$vectors,
-               center = apply(z, 2, mean), scale = sc, n.obs = N,
+               center = cen, scale = sc, n.obs = n.obs,
                scores = scr, call = match.call())
     ## The Splus function also return list elements factor.sdev,
     ## correlations and coef, but these are not documented in the help.
@@ -39,7 +47,8 @@ princomp <- function(x, cor = FALSE, scores = TRUE,
     edc
 }
 
-print.princomp <- function(x) {
+print.princomp <- function(x)
+{
     cat("Call:\n"); dput(x$call)
     cat("\nStandard deviations:\n")
     print(x$sdev)
