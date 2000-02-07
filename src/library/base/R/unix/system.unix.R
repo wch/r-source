babdae2efaa265a85e82e9415c4a2cc370b1ae47
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-02-07 06:32:09 +0000

    file arg check in unlink()
    git-svn-id: https://svn.r-project.org/R/trunk@8007 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/unix/system.unix.R
+++ src/library/base/R/unix/system.unix.R
@@ -1,6 +1,6 @@
 system <- function(command, intern = FALSE, ignore.stderr = FALSE)
     .Internal(system(if(ignore.stderr) paste(command, "2>/dev/null") else
-                     command, intern))
+		     command, intern))
 
 unix <- function(call, intern = FALSE) {
     .Deprecated("system")
@@ -9,16 +9,20 @@ unix <- function(call, intern = FALSE) {
 
 ##--- The following 2  should/could really be done in C [platform !] :
 tempfile <- function(pattern = "file") {
+    if(!is.character(pattern)) stop("argument must be character")
     system(paste("for p in", paste(pattern, collapse = " "), ";",
 		 "do echo ${TMPDIR:-/tmp}/$p$$; done"),
 	   intern = TRUE)
 }
 
-unlink <- function(x) { system(paste("rm -rf ", paste(x, collapse = " "))) }
+unlink <- function(x) {
+    if(!is.character(x)) stop("argument must be character")
+    system(paste("rm -rf ", paste(x, collapse = " ")))
+}
 
 dir.create <- function(path)
 {
     if(!is.character(path) || (length(path) > 1) || !nchar(path))
-        stop("invalid `path' argument")
+	stop("invalid `path' argument")
     invisible(system(paste("mkdir", path)) == 0)
 }
