03b71e6d0c3b66c7a387456aa6abc3c71b759eac
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-02-07 06:38:05 +0000

    fix inf.loop + some improvement
    git-svn-id: https://svn.r-project.org/R/trunk@8013 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/nmath/pnchisq.c
+++ src/nmath/pnchisq.c
@@ -1,9 +1,9 @@
 /*
- *  Algorithm AS 275 appl.statist. (1992), vol.41, no.2
+ *  Algorithm AS 275 Appl.Statist. (1992), vol.41, no.2
  *  original  (C) 1992 Royal Statistical Society
  *  Copyright (C) 2000 The R Development Core Team
  *
- *  computes the noncentral chi-square distribution function with
+ *  Computes the noncentral chi-square distribution function with
  *  positive real degrees of freedom f and nonnegative noncentrality
  *  parameter theta
  */
@@ -12,11 +12,10 @@
 
 /*----------- DEBUGGING -------------
  *
- *	make CFLAGS='-DDEBUG_pnch -g -I/usr/local/include -I../include'
+ *	make CFLAGS='-DDEBUG_pnch ....'
 
- * -- Feb.1, 1998 (R 0.62 alpha); M.Maechler:  still have
-	- INFINITE loop \
-	- bad precision / in some cases
+ * -- Feb.6, 2000 (R pre0.99); M.Maechler:  still have
+ * bad precision & non-convergence in some cases (x ~= f, both LARGE)
  */
 #ifdef DEBUG_pnch
 # include "PrtUtil.h"
@@ -24,12 +23,11 @@
 
 double pnchisq(double x, double f, double theta, int lower_tail, int log_p)
 {
-    double ans, lam, u, v, x2, f2, t, term, bound, twon;
+    double ans, lam, u, v, x2, f2, t, term, bound, f_x_2n, f_2n;
     int n, flag;
 
     const static double errmax = 1e-12;
-    const static double half = 0.5;
-    const static int itrmax = 100;
+    const static int itrmax = 10000;/*FIXME: should depend on errmax and theta*/
 
 #ifdef IEEE_754
     if (ISNAN(x) || ISNAN(f) || ISNAN(theta))
@@ -45,7 +43,7 @@ double pnchisq(double x, double f, double theta, int lower_tail, int log_p)
     if(!R_FINITE(x))
 	return R_DT_1;
 
-    lam = theta * half;
+    lam = .5 * theta;
 #ifdef DEBUG_pnch
     REprintf("pnchisq(x=%12g, f=%12g, theta=%12g):\n",x,f,theta);
 #endif
@@ -53,13 +51,18 @@ double pnchisq(double x, double f, double theta, int lower_tail, int log_p)
     /* evaluate the first term */
 
     v = u = exp(-lam);
-    x2 = x * half;
-    f2 = f * half;
-    /* careful not to overflow .. : */
-    t = exp(f2*log(x2) -x2 - lgammafn(f2 + 1));
-
-    /* there is no need to test ifault si */
-    /* already been checked */ /*^^^^^^^^ ?????? */
+    x2 = .5 * x;
+    f2 = .5 * f;
+    f_x_2n = f - x;
+    if(f2 * DBL_EPSILON > 0.125 &&
+       fabs(t = x2 - f2) < sqrt(DBL_EPSILON) * f2) {
+	/* evade cancellation error */
+	t = exp((1 - t)*(2 - t/(f2 + 1))) / sqrt(2*M_PI*(f2 + 1));
+    }
+    else {
+	/* careful not to overflow .. : */
+	t = exp(f2*log(x2) -x2 - lgammafn(f2 + 1));
+    }
 
     term = v * t;
     ans = term;
@@ -70,12 +73,14 @@ double pnchisq(double x, double f, double theta, int lower_tail, int log_p)
     /* check if (f+2n) is greater than x */
 
     flag = LFALSE;
-    n = 1; twon = n*2;
+    n = 1;
+    f_2n = f + 2.;/* = f + 2*n */
+    f_x_2n += 2.;/* = f - x + 2*n */
     for(;;) {
 #ifdef DEBUG_pnch
 	REprintf(" _OL_: n=%d",n);
 #endif
-	if (f + twon - x > 0.) {
+	if (f_x_2n > 0) {
 
 	    /* find the error bound and check for convergence */
 	    flag = LTRUE;
@@ -90,14 +95,14 @@ double pnchisq(double x, double f, double theta, int lower_tail, int log_p)
 
 	    u *= lam / n;
 	    v += u;
-	    t *= x / (f + twon);
+	    t *= x / f_2n;
 	    term = v * t;
 	    ans += term;
-	    n++; twon = n*2;
-	    if (!flag)
+	    n++; f_2n += 2; f_x_2n += 2;
+	    if (!flag && n <= itrmax)
 		break;
 	L10:
-	    bound = t * x / (f + twon - x);
+	    bound = t * x / f_x_2n;
 #ifdef DEBUG_pnch
 	    REprintf("\tL10: n=%d; term=%12g; bound=%12g\n",n,term,bound);
 #endif
@@ -112,5 +117,5 @@ L_End:
 #ifdef DEBUG_pnch
     REprintf("\tL_End: n=%d; term=%12g; bound=%12g\n",n,term,bound);
 #endif
-    return ans;
+    return R_DT_val(ans);
 }
