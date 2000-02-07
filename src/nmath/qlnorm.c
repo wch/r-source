eee806d9fcd182e124e650646c7ea3633e148653
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-02-07 06:36:34 +0000

    cleaner check
    git-svn-id: https://svn.r-project.org/R/trunk@8011 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/nmath/qlnorm.c
+++ src/nmath/qlnorm.c
@@ -33,7 +33,6 @@ double qlnorm(double p, double logmean, double logsd, int lower_tail, int log_p)
     R_Q_P01_check(p);
 
     if (p == R_DT_1)	return ML_POSINF;
-    if (R_DT_qIv(p) > 0)
-	return exp(qnorm(p, logmean, logsd, lower_tail, log_p));
-    return 0;
+    if (p == R_DT_0)	return 0;
+    return exp(qnorm(p, logmean, logsd, lower_tail, log_p));
 }
