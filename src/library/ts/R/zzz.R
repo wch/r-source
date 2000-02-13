ebb035a039a7dcaae89bb090af0e91aac6e5c497
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-02-13 08:45:06 +0000

    remove `preliminary' banner
    git-svn-id: https://svn.r-project.org/R/trunk@8136 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/ts/R/zzz.R
+++ src/library/ts/R/zzz.R
@@ -1,8 +1,8 @@
 .First.lib <- function(lib, pkg)
 {
     library.dynam("ts", pkg, lib)
-    if(interactive() || getOption("verbose"))
-	cat("\n	   This is a preliminary time series package for R\n",
-	    "	   See `library(help=ts)' for details.\n\n")
+#    if(interactive() || getOption("verbose"))
+#	cat("\n	   This is a preliminary time series package for R\n",
+#	    "	   See `library(help=ts)' for details.\n\n")
 }
 options(ts.S.compat = FALSE)
