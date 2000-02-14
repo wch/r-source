5cab57f4d25ca5a6c76ed3dcf987433e597199f0
tlumley <tlumley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-02-14 21:31:20 +0000

    qnchisq #ifdefed back in do_math3
    git-svn-id: https://svn.r-project.org/R/trunk@8170 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/arithmetic.c
+++ src/main/arithmetic.c
@@ -1452,9 +1452,10 @@ SEXP do_math3(SEXP call, SEXP op, SEXP args, SEXP env)
 
     case 34:  return Math3_1(args, dnchisq);
     case 35:  return Math3_2(args, pnchisq);
-#ifdef UNIMP
+/* #ifdef UNIMP */
+/* This appears to be IMP already */
     case 36:  return Math3_2(args, qnchisq);
-#endif
+/* #endif */
 
 #ifdef UNIMP
     case 37:  return Math3_1(args, dnt);
