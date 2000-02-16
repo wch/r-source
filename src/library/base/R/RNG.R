0d4545d3ddd026013413a8b94fd82a80609e3a86
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-02-16 15:29:13 +0000

    make kind character string in set.seed
    git-svn-id: https://svn.r-project.org/R/trunk@8200 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/RNG.R
+++ src/library/base/R/RNG.R
@@ -28,5 +28,16 @@ RNGkind <- function(kind = NULL, normal.kind = NULL)
     if(do.set || !is.null(normal.kind)) invisible(r) else r
 }
 
-set.seed <- function(seed, kind = NULL)
-    invisible(.Internal(set.seed(seed, kind)))
+set.seed <- function(seed, kind = NULL) {
+    kinds <- c("Wichmann-Hill", "Marsaglia-Multicarry", "Super-Duper",
+               "Mersenne-Twister", "Knuth-TAOCP", "user-supplied")
+    if(length(kind) > 0) {
+	if(!is.character(kind) || length(kind) > 1)
+	    stop("'kind' must be a character string of length 1 (RNG to be used).")
+	if(is.na(i.knd <- pmatch(kind, kinds) - 1))
+	    stop(paste("'",kind,"' is not a valid abbrevation of an RNG",
+		       sep=""))
+    } else i.knd <- NULL
+
+    invisible(.Internal(set.seed(seed, i.knd)))
+}
