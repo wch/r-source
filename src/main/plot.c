1a6fb1114311dad85452b318587fe1e94109c729
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-02-19 19:36:21 +0000

    move test for axis type up in do_axis since 1.105 was causing stack imbalance
    git-svn-id: https://svn.r-project.org/R/trunk@8270 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/plot.c
+++ src/main/plot.c
@@ -766,6 +766,20 @@ SEXP do_axis(SEXP call, SEXP op, SEXP args, SEXP env)
     side = asInteger(CAR(args));
     if (side < 1 || side > 4)
 	errorcall(call, "invalid axis number");
+
+    /* Check the axis type parameter, if it is 'n', */
+    /* there is nothing to do */
+
+    if (side == 1 || side == 3) {
+	if (dd->gp.xaxt == 'n') {
+	    GRestorePars(dd);	    return R_NilValue;
+	}
+    } else if (side == 2 || side == 4) {
+	if (dd->gp.yaxt == 'n') {
+	    GRestorePars(dd);	    return R_NilValue;
+	}
+    }
+
     args = CDR(args);
 
     /* tick-label locations;  these are coerced lower down */
@@ -863,19 +877,6 @@ SEXP do_axis(SEXP call, SEXP op, SEXP args, SEXP env)
     col = dd->gp.col;
     fg = dd->gp.fg;
 
-    /* Check the axis type parameter, if it is 'n', */
-    /* there is nothing to do */
-
-    if (side == 1 || side == 3) {
-	if (dd->gp.xaxt == 'n') {
-	    GRestorePars(dd);	    return R_NilValue;
-	}
-    } else if (side == 2 || side == 4) {
-	if (dd->gp.yaxt == 'n') {
-	    GRestorePars(dd);	    return R_NilValue;
-	}
-    }
-    else errorcall(call, "invalid \"side\" value");
 
     x = dd->gp.usr[0];
     y = dd->gp.usr[2];
