fc3550a6326e7c2dca82a2886e7944003aafb402
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-04-03 10:07:46 +0000

    typo in error message. Use PATH_MAX for the file name length.
    git-svn-id: https://svn.r-project.org/R/trunk@8663 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/devPS.c
+++ src/main/devPS.c
@@ -621,7 +621,7 @@ void PostScriptText(FILE *fp, double x, double y,
 /* Part 3.  Device Driver State. */
 
 typedef struct {
-    char filename[128];
+    char filename[PATH_MAX];
 
     char papername[64];	 /* paper name */
     int paperwidth;	 /* paper width in inches */
@@ -716,9 +716,9 @@ int PSDeviceDriver(DevDesc *dd, char *file, char *paper, char *family,
 
     /* Check and extract the device parameters */
 
-    if(strlen(file) > 127) {
+    if(strlen(file) > PATH_MAX - 1) {
 	free(dd);
-	error("filename to long in postscript");
+	error("filename too long in postscript");
     }
 
     /* allocate new postscript device description */
