c7c71ae6bebbe63b1182c614be7229a3cf966867
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-04-10 06:42:19 +0000

    phase 1 of moving X11 support to a shared library
    git-svn-id: https://svn.r-project.org/R/trunk@8776 00db46b3-68df-0310-9c12-caf00c1e9a41

---
+++ src/unix/X11/rotated.h
@@ -0,0 +1,59 @@
+/* ************************************************************************ */
+
+
+/* Header file for the `xvertext 5.0' routines.
+
+   Copyright (c) 1993 Alan Richardson (mppa3@uk.ac.sussex.syma) */
+
+
+/* ************************************************************************ */
+
+#ifndef _XVERTEXT_INCLUDED_
+#define _XVERTEXT_INCLUDED_
+
+
+#define XV_VERSION	5.0
+#define XV_COPYRIGHT \
+      "xvertext routines Copyright (c) 1993 Alan Richardson"
+
+
+/* ---------------------------------------------------------------------- */
+
+
+/* text alignment */
+
+#define NONE		 0
+#define TLEFT		 1
+#define TCENTRE		 2
+#define TRIGHT		 3
+#define MLEFT		 4
+#define MCENTRE		 5
+#define MRIGHT		 6
+#define BLEFT		 7
+#define BCENTRE		 8
+#define BRIGHT		 9
+
+
+/* ---------------------------------------------------------------------- */
+
+/* Protoized : C++ or ANSI C */
+double	XRotVersion(char*, int);
+void	XRotSetMagnification(double);
+void	XRotSetBoundingBoxPad(int);
+int	XRotDrawString(Display*, XFontStruct*, double,
+		       Drawable, GC, int, int, char*);
+int	XRotDrawImageString(Display*, XFontStruct*, double,
+			    Drawable, GC, int, int, char*);
+int	XRotDrawAlignedString(Display*, XFontStruct*, double,
+			      Drawable, GC, int, int, char*, int);
+int	XRotDrawAlignedImageString(Display*, XFontStruct*, double,
+				   Drawable, GC, int, int, char*, int);
+XPoint *XRotTextExtents(Display*, XFontStruct*, double,
+			int, int, char*, int);
+
+/* ---------------------------------------------------------------------- */
+
+#endif /* _XVERTEXT_INCLUDED_ */
+
+
+
