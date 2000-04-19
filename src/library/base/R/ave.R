50f582461b4008f09fc9f7649109441323046184
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-04-19 07:41:20 +0000

    ave(.)
    git-svn-id: https://svn.r-project.org/R/branches/R-1-0-patches@8885 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/ave.R
+++ src/library/base/R/ave.R
@@ -1,18 +1,15 @@
 ave <- function (x, ..., FUN = mean)
 {
-    l <- list(...)
-    if (is.null(l)) {
-	x[] <- FUN(x)
-    }
-    else {
-	g <- 1
-	nlv <- 1
-	for (i in 1:length(l)) {
-	    l[[i]] <- li <- as.factor(l[[i]])
-	    g <- g + nlv * (as.numeric(li) - 1)
-	    nlv <- nlv * length(levels(li))
-	}
-	x[] <- unlist(lapply(split(x, g), FUN))[g]
-    }
+    n <- length(l <- list(...))
+    x[] <- if (n) {
+        g <- 1
+        nlv <- 1
+        for (i in 1:n) {
+            l[[i]] <- li <- as.factor(l[[i]])
+            g <- g + nlv * (as.numeric(li) - 1)
+            nlv <- nlv * length(levels(li))
+        }
+        unlist(lapply(split(x, g), FUN))[g]
+    } else FUN(x)
     x
 }
