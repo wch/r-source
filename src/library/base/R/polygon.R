c8875d54e73a867860d30ef3cedff144478de283
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-04-28 08:37:32 +0000

    linebreaks
    git-svn-id: https://svn.r-project.org/R/trunk@8986 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/polygon.R
+++ src/library/base/R/polygon.R
@@ -1,9 +1,8 @@
-polygon <- function(x, y=NULL, col=NA, border=NULL, lty=NULL, xpd=NULL, density = -1, angle = 45, ...)
+polygon <- function(x, y=NULL, col=NA, border=NULL, lty=NULL, xpd=NULL,
+		    density = -1, angle = 45, ...)
 {
-    if (!missing(density))
-	.NotYetUsed("density")
-    if (!missing(angle))
-	.NotYetUsed("angle")
+    if (!missing(density))	.NotYetUsed("density")
+    if (!missing(angle))	.NotYetUsed("angle")
     xy <- xy.coords(x, y)
     ##-- FIXME: what if 'log' is active, for x or y?
     .Internal(polygon(xy$x, xy$y, col, border, lty, xpd, ...))
