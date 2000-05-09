bd637029e1b6a12e0c2fca3b5a6067241b174c9d
pd <pd@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-05-09 17:50:24 +0000

    Initial sketch
    git-svn-id: https://svn.r-project.org/R/branches/R-Tk@9129 00db46b3-68df-0310-9c12-caf00c1e9a41

---
+++ src/library/base/R/Tk.R
@@ -0,0 +1,82 @@
+# ------ Basics ------
+
+TkOpt <- function() {
+    name2opt <- function(x) if ( x != "") paste("-",x,sep="") else ""
+    val2string <- function(x) {
+        if (is.function(x)) return(.Tkcallback(x))
+        if (is.environment(x)) return (TkID(x))
+        if (x != "") paste("{",x,"}",sep="")
+    }
+    m <- evalq(match.call(expand.dots=F),parent.frame())$...
+    nm <- names(m)
+    val <- evalq(list(...), parent.frame())
+    if (length(val)==0) return("")
+    nm <- if(is.null(nm)) rep("",length(val)) else sapply(nm, name2opt)
+    val <- sapply(val, val2string)
+    paste(as.vector(t(cbind(nm,val))), collapse=" ")
+}
+
+TkID <- function(win) get("ID", envir = win)
+
+TkNew.win <- function(ID){
+    win <- evalq(new.env(), TkWin)
+    assign("ID", ID, envir=win)
+    win
+}
+
+TkNew.subwin <- function(parent) {
+    ID <- evalq({num.subwin<-num.subwin+1;
+           paste(ID,num.subwin,sep=".")}, parent)
+    New.win(ID)
+}
+
+TkWin <- local({num.subwin<-0 ; environment()})
+
+.TkRoot <- TkNew.win("")
+
+TkVar <- structure(NULL,class="tkvar")
+
+"$.tkvar" <- function(x, name) .Tk(paste("set", name))
+"$<-.tkvar" <- function(x, name, value) { .Tk(paste("set", name, value)) ; x }
+
+
+# ------ Widgets ------
+
+TkToplevel <- function(parent = .TkRoot, ...)
+{
+    win <- TkNew.subwin(parent)
+    .Tk(paste("toplevel", TkID(win), TkOpt()))
+    win
+}
+
+TkLabel <- function(parent, ...) {
+    win <- TkNew.subwin(parent)
+    .Tk(paste("label", TkID(win), TkOpt()))
+    win
+}
+
+TkButton <- function(parent, ...)
+{
+    win <- TkNew.subwin(parent)
+    .Tk(paste("button", TkID(win), TkOpt()))
+    win
+}
+
+TkRadioButton <- function(parent, ...)
+{
+    win <- TkNew.subwin(parent)
+    .Tk(paste("radiobutton", TkID(win), TkOpt()))
+    win
+}
+
+# ------ Window & Geometry managers ------
+
+TkPack <- function(...) {
+    .Tk(paste("pack",TkOpt()))
+}
+
+"TkTitle<-" <- function(x, value) {
+    .Tk(paste("wm title",TkID(x),"{",value,"}"))
+    x
+}
+
