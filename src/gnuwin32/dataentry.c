91556e976a56e4679ca6a14182413db68af0a8ca
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-05-12 06:20:54 +0000

    row->col typo
    git-svn-id: https://svn.r-project.org/R/trunk@9181 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/dataentry.c
+++ src/gnuwin32/dataentry.c
@@ -1152,7 +1152,7 @@ void de_mousedown(control c, int buttons, point xy)
 	    crow = wrow;
 	    highlightrect();
 	    find_coords(crow, ccol, &x, &y);
-	    bw = BOXW(crow+colmin-1);
+	    bw = BOXW(ccol + colmin - 1);
 	    rr = rect(x + text_xoffset, y - text_yoffset - 1,
 		      bw - text_xoffset - 2,
 		      box_h - text_yoffset - 2);
