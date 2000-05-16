488a34b3cc8c2a6c2b2f80efaa51a469bc714c6f
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-05-16 09:10:28 +0000

    greppable
    git-svn-id: https://svn.r-project.org/R/trunk@9233 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/names.R
+++ src/library/base/R/names.R
@@ -1,15 +1,6 @@
-names <-
-    function(x, ...)
-    UseMethod("names")
+names <- function(x, ...) UseMethod("names")
+"names<-" <- function(x, ...) UseMethod("names<-")
 
-names.default <-
-    function(x)
-    .Internal(names(x))
+names.default <- function(x) .Internal(names(x))
 
-"names<-" <-
-    function(x, ...)
-    UseMethod("names<-")
-
-"names<-.default" <-
-    function(x, value)
-    .Internal("names<-"(x, value))
+"names<-.default" <- function(x, value) .Internal("names<-"(x, value))
