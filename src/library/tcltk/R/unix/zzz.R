4b4c93f05970c1d1deab6ecef8ed28aba4fec349
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-05-30 14:45:24 +0000

    remove provide(tcltk) add .Last.lib function
    git-svn-id: https://svn.r-project.org/R/trunk@9380 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tcltk/R/unix/zzz.R
+++ src/library/tcltk/R/unix/zzz.R
@@ -1,5 +1,10 @@
 .First.lib <- function(lib, pkg) {
     library.dynam("tcltk", pkg, lib)
-    .C("tcltk_init")
-    provide(tcltk)
+    .C("tcltk_init", PACKAGE="tcltk")
+}
+
+.Last.lib <- function(libpath) {
+    .C("delTcl", PACKAGE="tcltk")
+    dyn.unload(file.path(libpath, "libs",
+                         paste("tcltk", .Platform$"dynlib.ext", sep="")))
 }
