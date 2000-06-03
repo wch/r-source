1384c483ec42e728a4e991ee24681266daa0d34f
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-06-03 15:36:44 +0000

    lty in lines() defaults to par("lty") now
    git-svn-id: https://svn.r-project.org/R/trunk@9429 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/lines.R
+++ src/library/base/R/lines.R
@@ -1,5 +1,7 @@
 lines <- function(x, ...) UseMethod("lines")
 
-lines.default <- function(x, y=NULL, type="l", col=par("col"), ...) {
-    plot.xy(xy.coords(x, y), type=type, col=col, ...)
+lines.default <- function(x, y=NULL, type="l", col=par("col"),
+                          lty=par("lty"), ...)
+{
+    plot.xy(xy.coords(x, y), type=type, col=col, lty=lty, ...)
 }
