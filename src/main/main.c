77ff15cca7a05c0d4a3a36d611a27d4a30d52657
rgentlem <rgentlem@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-06-20 23:11:35 +0000

    changes to debug: put in where option
    git-svn-id: https://svn.r-project.org/R/trunk@9631 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/main.c
+++ src/main/main.c
@@ -113,7 +113,7 @@ char *R_PromptString(int browselevel, int type)
 
 static void R_ReplConsole(SEXP rho, int savestack, int browselevel)
 {
-    int c, status;
+    int c, status, browsevalue;
     char *bufp, buf[1024];
 
     R_IoBufferWriteReset(&R_ConsoleIob);
@@ -164,8 +164,15 @@ static void R_ReplConsole(SEXP rho, int savestack, int browselevel)
 
 	    R_IoBufferReadReset(&R_ConsoleIob);
 	    R_CurrentExpr = R_Parse1Buffer(&R_ConsoleIob, 1, &status);
-	    if (browselevel && ParseBrowser(R_CurrentExpr, rho))
-		return;
+	    if (browselevel) {
+		browsevalue = ParseBrowser(R_CurrentExpr, rho);
+		if(browsevalue == 1 )
+		    return;
+		if(browsevalue == 2 ) {
+		    R_IoBufferWriteReset(&R_ConsoleIob);
+		    break;
+		}
+	    }		    
 	    R_Visible = 0;
 	    R_EvalDepth = 0;
 	    PROTECT(R_CurrentExpr);
@@ -475,7 +482,7 @@ void mainloop(void)
 static void printwhere(void)
 {
   RCNTXT *cptr;
-  int lct = 0;
+  int lct = 1;
 
   for (cptr = R_GlobalContext; cptr; cptr = cptr->nextcontext) {
     if ((cptr->callflag & CTXT_FUNCTION) && 
@@ -510,7 +517,7 @@ static int ParseBrowser(SEXP CExpr, SEXP rho)
 	if (!strcmp(CHAR(PRINTNAME(CExpr)),"where")) {
 	    printwhere();
 	    DEBUG(rho)=1;
-	    rval=1;
+	    rval=2;
 	}
     }
     return rval;
