2e3b30e5bfb70410b9d583c80b475938303a0757
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-07-21 14:55:25 +0000

    use HTML link in ~/.R for help(, htmlhelp=T) after help.start()
    git-svn-id: https://svn.r-project.org/R/branches/R-1-1-patches@9938 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/unix/help.start.R
+++ src/library/base/R/unix/help.start.R
@@ -2,14 +2,17 @@ help.start <- function (gui = "irrelevant", browser = getOption("browser"),
 			remote = NULL) {
     if(is.null(browser))
 	stop("Invalid browser name, check options(\"browser\").")
+    cat("Making links in ~/.R ...\n")
+    system(paste("${R_HOME}/bin/help.links",
+		 paste(unique(.lib.loc), sep=" ", collapse=" "),
+		 sep =" "))
     url <- paste(if (is.null(remote)) "$HOME/.R" else remote,
 		 "/doc/html/index.html", sep = "")
     cat("If", browser, " is already running,\tit is *not* restarted,\n",
 	"and you must switch to its window.\nOtherwise, be patient..\n")
-    system(paste("${R_HOME}/bin/help.links",
-		 paste(unique(.lib.loc), sep=" ", collapse=" "),
-		 sep =" "))
     system(paste(browser, " -remote \"openURL(", url, ")\" 2>/dev/null || ",
 		 browser, " ", url, " &", sep = ""))
+    assign("help.start.has.been.run", TRUE,
+           pos=match("package:base", search()))
     options(htmlhelp=TRUE)
 }
