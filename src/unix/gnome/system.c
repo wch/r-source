472c085dfbf32d93c91f611f4e0678f5b652a17b
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-07-21 06:35:12 +0000

    more on Sys.sleep
    git-svn-id: https://svn.r-project.org/R/trunk@9935 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/unix/gnome/system.c
+++ src/unix/gnome/system.c
@@ -333,5 +333,5 @@ void gnome_start(int ac, char **av, Rstart Rp)
 
 SEXP do_syssleep(SEXP call, SEXP op, SEXP args, SEXP rho)
 {
-    error("Sys.sleep is not implemented on this system")
+    error("Sys.sleep is not implemented on this system");
 }
