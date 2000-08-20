bc78dc3d57d16d71067887e28262c2b6ee1cdae0
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-08-20 17:27:33 +0000

    tweak unlink
    git-svn-id: https://svn.r-project.org/R/trunk@10344 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/extra.c
+++ src/gnuwin32/extra.c
@@ -113,7 +113,6 @@ SEXP do_dircreate(SEXP call, SEXP op, SEXP args, SEXP env)
 }
 
 
-
 #include <sys/types.h>
 #include <sys/stat.h>
 
@@ -125,8 +124,11 @@ static int R_unlink_one(char *dir, char *name, int recursive)
 
     if(strcmp(name, ".") == 0) return 0;
     if(strcmp(name, "..") == 0) return 0;
-    strcpy(tmp, dir); strcat(tmp, "\\");
-    strcat(tmp, name);
+    if(strlen(dir)) {
+	strcpy(tmp, dir); 
+	if(*(dir + strlen(dir) - 1) != '\\') strcat(tmp, "\\");
+	strcat(tmp, name);
+    } else strcpy(tmp, name);
     return (recursive ? R_unlink(tmp, 1): unlink(tmp)) !=0;
 }
 
