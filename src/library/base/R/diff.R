35324e60b65fa7f49bb9ea898824b2ec080f257d
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-09-06 08:01:44 +0000

    fix from R-release
    git-svn-id: https://svn.r-project.org/R/trunk@10552 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/diff.R
+++ src/library/base/R/diff.R
@@ -1,27 +1,31 @@
 diff <- function(x, ...) UseMethod("diff")
 
+if(FALSE) ## when method dispatching will work for autoloaded objects
+    autoload("diff.ts", file = "ts")
+
 diff.default <- function(x, lag = 1, differences = 1, ...)
 {
-    ismat <- is.matrix(x)
-    if (ismat)
-	xlen <- dim(x)[1]
-    else xlen <- length(x)
-    if (lag < 1 | differences < 1)
-	stop("Bad value for lag or differences")
-    if (lag * differences >= xlen)
-	return(x[0])
-    r <- x
-    class(r) <- NULL # don't want class-specific subset methods
-    s <- 1:lag
-    if (is.matrix(r)) {
-	for (i in 1:differences)
-	    r <- r[-s, , drop = FALSE] - r[-(nrow(r) + 1 - s), , drop = FALSE]
+    ## the following clause will be replaced by autoload("diff.ts",...)
+    if(is.ts(x)) {
+        require(ts)
+        UseMethod("diff")
     }
-    else for (i in 1:differences)
-	r <- r[-s] - r[-(length(r) + 1 - s)]
-    xtsp <- attr(x, "tsp")
-    if (!is.null(xtsp))
-        tsp(r) <- c(xtsp[1] + lag*differences*xtsp[3], xtsp[2], xtsp[3])
+    ismat <- is.matrix(x)
+    xlen <- if(ismat) dim(x)[1] else length(x)
+    if (length(lag) > 1 || length(differences) > 1 ||
+        lag < 1 || differences < 1)
+	stop("`lag' and `differences' must be integers >= 1")
+    if (lag * differences >= xlen)
+	return(x[0]) # empty of proper mode
+    r <- unclass(x)  # don't want class-specific subset methods
+    i1 <- -1:-lag
+    if (ismat)
+	for (i in 1:differences)
+	    r <- r[i1, , drop = FALSE] -
+                r[-nrow(r):-(nrow(r)-lag+1), , drop = FALSE]
+    else
+        for (i in 1:differences)
+            r <- r[i1] - r[-length(r):-(length(r)-lag+1)]
     class(r) <- class(x)
     r
 }
