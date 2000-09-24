7b2e1045c762aac38443338f24af95bf81698840
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-09-24 22:29:21 +0000

    Add exact conditional tests and confidence intervals for the 2 x 2 x k case.
    git-svn-id: https://svn.r-project.org/R/trunk@10704 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/ctest/R/mantelhaen.test.R
+++ src/library/ctest/R/mantelhaen.test.R
@@ -1,4 +1,7 @@
-mantelhaen.test <- function(x, y = NULL, z = NULL, correct = TRUE)
+mantelhaen.test <-
+function(x, y = NULL, z = NULL,
+         alternative = c("two.sided", "less", "greater"),
+         correct = TRUE, exact = FALSE, conf.level = 0.95)
 {
     DNAME <- deparse(substitute(x))
     if(is.array(x)) {
@@ -37,20 +40,206 @@ mantelhaen.test <- function(x, y = NULL, z = NULL, correct = TRUE)
     K <- dim(x)[3]
 
     if((I == 2) && (J == 2)) {
-        ## Classical Mantel-Haenszel 2 x 2 x K test
-        s.x <- apply(x, c(1, 3), sum)
-        s.y <- apply(x, c(2, 3), sum)
-        n <- apply(x, 3, sum)
-        DELTA <- abs(sum(x[1, 1, ] - s.x[1, ] * s.y[1, ] / n))
-        YATES <- ifelse(correct && (DELTA >= .5), .5, 0)
-        STATISTIC <- ((DELTA - YATES)^2 /
-                      sum(apply(rbind(s.x, s.y), 2, prod)
-                          / (n^2 * (n - 1))))
-        PARAMETER <- 1
-        names(STATISTIC) <- "Mantel-Haenszel X-square"
-        METHOD <- paste("Mantel-Haenszel chi-square test",
-                        ifelse(YATES, "with", "without"),
-                        "continuity correction")
+        ## 2 x 2 x K case
+        alternative <- match.arg(alternative)
+        if(!missing(conf.level) &&
+           (length(conf.level) != 1 || !is.finite(conf.level) ||
+            conf.level < 0 || conf.level > 1))
+            stop("conf.level must be a single number between 0 and 1")
+
+        NVAL <- 1
+        names(NVAL) <- "common odds ratio"
+        
+        if(!exact) {
+            ## Classical Mantel-Haenszel 2 x 2 x K test
+            s.x <- apply(x, c(1, 3), sum)
+            s.y <- apply(x, c(2, 3), sum)
+            n <- apply(x, 3, sum)
+            DELTA <- abs(sum(x[1, 1, ] - s.x[1, ] * s.y[1, ] / n))
+            YATES <- ifelse(correct && (DELTA >= .5), .5, 0)
+            STATISTIC <- ((DELTA - YATES)^2 /
+                          sum(apply(rbind(s.x, s.y), 2, prod)
+                              / (n^2 * (n - 1))))
+            PARAMETER <- 1
+            names(STATISTIC) <- "Mantel-Haenszel X-square"
+            names(PARAMETER) <- "df"
+            METHOD <- paste("Mantel-Haenszel chi-square test",
+                            ifelse(YATES, "with", "without"),
+                            "continuity correction")
+            s.diag <- sum(x[1, 1, ] * x[2, 2, ] / n)
+            s.offd <- sum(x[1, 2, ] * x[2, 1, ] / n)
+            ## Mantel-Haenszel (1959) estimate of the common odds ratio
+            ESTIMATE <- s.diag / s.offd
+            ## Robins et al. (1986) estimate of the standard deviation
+            ## of the log of the Mantel-Haenszel estimator
+            sd <-
+                sqrt(  sum((x[1,1,] + x[2,2,]) * x[1,1,] * x[2,2,]
+                           / n^2)
+                     / (2 * s.diag^2)
+                     + sum((  (x[1,1,] + x[2,2,]) * x[1,2,] * x[2,1,]
+                            + (x[1,2,] + x[2,1,]) * x[1,1,] * x[2,2,])
+                           / n^2)
+                     / (2 * s.diag * s.offd)
+                     + sum((x[1,2,] + x[2,1,]) * x[1,2,] * x[2,1,]
+                           / n^2)
+                     / (2 * s.offd^2))
+            CINT <-
+                switch(alternative,
+                       less =
+                       c(0, ESTIMATE * exp(qnorm(conf.level) * sd)),
+                       greater =
+                       c(ESTIMATE * exp(qnorm(1 - conf.level) * sd), Inf),
+                       two.sided = {
+                           ESTIMATE * exp(c(1, -1) * 
+                                          qnorm((1 - conf.level) / 2) *
+                                          sd)
+                       })
+            RVAL <- list(statistic = STATISTIC,
+                         parameter = PARAMETER,
+                         p.value = 1 - pchisq(STATISTIC, PARAMETER))
+        }
+        else {
+            ## Exact inference for the 2 x 2 x k case can be carried out
+            ## conditional on the strata margins, similar to the case
+            ## for Fisher's exact test (k = 1).  Again, the distribution
+            ## of S (in our case, sum(x[2, 1, ]) to be consistent with
+            ## the notation in Mehta et al. (1985), is of the form
+            ##    P(S = s) \propto d(s) * or^s,   lo <= s <= hi
+            ## where or is the common odds ratio in the k tables (and
+            ## d(.) is a product hypergeometric distribution).
+
+            METHOD <- paste("Exact conditional test of independence",
+                            "in 2 x 2 x k tables")
+            m <- apply(x, c(2, 3), sum)[1, ]
+            n <- apply(x, c(2, 3), sum)[2, ]
+            t <- apply(x, c(1, 3), sum)[1, ]
+            s <- sum(x[1, 1, ])
+            lo <- sum(pmax(0, t - n))
+            hi <- sum(pmin(m, t))
+            d.save <- .C("d2x2xk",
+                         as.integer(K),
+                         as.double(m),
+                         as.double(n),
+                         as.double(t),
+                         d = double(hi - lo + 1),
+                         PACKAGE = "ctest")$d
+
+            dn2x2xk <- function(ncp = 1) {
+                ## Note that this returns the whole vector of length
+                ## hi - lo + 1.
+                if(ncp == 1)
+                    return(d.save)
+                else {
+                    d <- d.save * ncp ^ (0 : (hi - lo))
+                    d / sum(d)
+                }
+            }
+            pn2x2xk <- function(q, ncp = 1, upper.tail = FALSE) {
+                if(ncp == 0) {
+                    if(upper.tail)
+                        return(as.numeric(q <= lo))
+                    else
+                        return(as.numeric(q >= lo))
+                }
+                if(ncp^(hi - lo) == Inf) {
+                    if(upper.tail)
+                        return(as.numeric(q <= hi))
+                    else
+                        return(as.numeric(q >= hi))
+                }
+                u <- lo : hi
+                d <- dn2x2xk(ncp)
+                if(upper.tail)
+                    sum(d[u >= q])
+                else
+                    sum(d[u <= q])
+            }
+            PVAL <-
+                switch(alternative,
+                       less = pn2x2xk(s, 1),
+                       greater = pn2x2xk(s, 1, upper = TRUE),
+                       two.sided = {
+                           ## Note that we need a little fuzz.
+                           relErr <- 1 + 10 ^ (-7)
+                           d <- d.save  # same as dn2x2xk(1)
+                           sum(d[d <= d[s - lo + 1] * relErr])
+                       })
+            ## Determine the MLE for ncp by solving E(S) = s, where the
+            ## expectation is with respect to the above distribution.
+            mle <- function(x) {
+                if(x == lo)
+                    return(0)
+                if(x == hi)
+                    return(Inf)
+                mn2x2xk <- function(ncp) {
+                    if(ncp == 0)
+                        return(lo)
+                    if(ncp^(hi - lo) == Inf)
+                        return(hi)
+                    q <- lo : hi
+                    d <- dn2x2xk(ncp)
+                    sum(q * d)
+                }
+                mu <- mn2x2xk(1)
+                if(mu > x)
+                    uniroot(function(t)
+                            mn2x2xk(t) - x,
+                            c(0, 1))$root
+                else if(mu < x)
+                    1 / uniroot(function(t)
+                                mn2x2xk(1/t) - x,
+                                c(.Machine$double.eps, 1))$root
+                else
+                    1
+            }
+            ESTIMATE <- mle(s)
+            ## Determine confidence intervals for the odds ratio.
+            ncp.U <- function(x, alpha) {
+                if(x == hi)
+                    return(Inf)
+                p <- pn2x2xk(x, 1)
+                if(p < alpha)
+                    uniroot(function(t) pn2x2xk(x, t) - alpha,
+                            c(0, 1))$root
+                else if(p > alpha)
+                    1 / uniroot(function(t) pn2x2xk(x, 1/t) - alpha,
+                                c(.Machine$double.eps, 1))$root
+                else
+                    1
+            }
+            ncp.L <- function(x, alpha) {
+                if(x == lo)
+                    return(0)
+                p <- pn2x2xk(x, 1, upper = TRUE)
+                if(p > alpha)
+                    uniroot(function(t)
+                            pn2x2xk(x, t, upper = TRUE) - alpha,
+                            c(0, 1))$root
+            else if (p < alpha)
+                1 / uniroot(function(t)
+                            pn2x2xk(x, 1/t, upper = TRUE) - alpha,
+                            c(.Machine$double.eps, 1))$root
+            else
+                1
+            }
+            CINT <- switch(alternative,
+                           less = c(0, ncp.U(s, 1 - conf.level)),
+                           greater = c(ncp.L(s, 1 - conf.level), Inf),
+                           two.sided <- {
+                               alpha <- (1 - conf.level) / 2
+                               c(ncp.L(s, alpha), ncp.U(s, alpha))
+                           })
+            RVAL <- list(p.value = PVAL)
+        }
+        
+        names(ESTIMATE) <- names(NVAL)
+        attr(CINT, "conf.level") <- conf.level
+        RVAL <- c(RVAL,
+                  list(conf.int = CINT,
+                       estimate = ESTIMATE,
+                       null.value = NVAL,
+                       alternative = alternative))
+
     }
     else {
         ## Generalized Cochran-Mantel-Haenszel I x J x K test
@@ -77,15 +266,16 @@ mantelhaen.test <- function(x, y = NULL, z = NULL, correct = TRUE)
         STATISTIC <- crossprod(n, qr.solve(V, n))
         PARAMETER <- df
         names(STATISTIC) <- "Cochran-Mantel-Haenszel M^2"
+        names(PARAMETER) <- "df"
         METHOD <- "Cochran-Mantel-Haenszel test"
+        RVAL <- list(statistic = STATISTIC,
+                     parameter = PARAMETER,
+                     p.value = 1 - pchisq(STATISTIC, PARAMETER))
     }
 
-    names(PARAMETER) <- "df"
-    RVAL <- list(statistic = STATISTIC,
-                 parameter = PARAMETER,
-                 p.value = 1 - pchisq(STATISTIC, PARAMETER),
-                 method = METHOD,
-                 data.name = DNAME)
+    RVAL <- c(RVAL,
+              list(method = METHOD,
+                   data.name = DNAME))
     class(RVAL) <- "htest"
     return(RVAL)
 }
