d68a96f52eb0e7f06751ce7aa7bd39fd0c44e110
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-09-29 16:28:07 +0000

    R.version.string: use "-"
    git-svn-id: https://svn.r-project.org/R/trunk@10741 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/profile/Common.R
+++ src/library/profile/Common.R
@@ -14,11 +14,11 @@ T <- TRUE
 F <- FALSE
 R.version <- structure(R.Version(), class = "simple.list")
 version <- .Alias(R.version)# for S-compatibility
-R.version.string <- local({
-    cc <- function(...) paste(..., collapse=" ")
-    paste(cc("R version", paste(version[c("major","minor")],collapse=".")),
-          cc(version[c("year", "month","day")]), sep=", ")
-})
+R.version.string <-
+    paste(paste("R version", paste(version[c("major","minor")], collapse=".")),
+          paste(version[c("year", "month","day")], collapse="-"),
+          sep=", ", collapse=" ")
+
 .Machine <- Machine()
 .Platform <- Platform()
 
