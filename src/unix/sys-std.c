0056cf98fd9b5e0a21a0eb3ef24143fff43794a0
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-10-09 21:47:30 +0000

    typo in last change
    git-svn-id: https://svn.r-project.org/R/trunk@10871 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/unix/sys-std.c
+++ src/unix/sys-std.c
@@ -652,7 +652,7 @@ static void (* OldHandler)(void);
 
 
 #ifdef HAVE_TIMES
-#include <times.h>
+#include <time.h>
 #include <sys/times.h>
 #ifndef CLK_TCK
 /* this is in ticks/second, generally 60 on BSD style Unix, 100? on SysV */
