c862ce01c29fcc2efee0695a10b0dbaf59373b86
tlumley <tlumley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-10-13 16:52:46 +0000

    use .Primitive("c") in rm() to prevent overwriting
    git-svn-id: https://svn.r-project.org/R/trunk@10943 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/rm.R
+++ src/library/base/R/rm.R
@@ -2,7 +2,7 @@ rm <-
     function(..., list=character(0), pos=-1, envir=pos.to.env(pos), inherits=FALSE)
 {
     names<- as.character(substitute(list(...)))[-1]
-    list<-c(list, names)
+    list<-.Primitive("c")(list, names)
     .Internal(remove(list, envir, inherits))
 }
 
