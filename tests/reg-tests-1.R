9c437bfd312b91ae0d1b2fc7b146e88894bb650f
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-10-13 20:32:56 +0000

    Add code for PR 644.
    git-svn-id: https://svn.r-project.org/R/trunk@10944 00db46b3-68df-0310-9c12-caf00c1e9a41

--- tests/reg-tests-1.R
+++ tests/reg-tests-1.R
@@ -5,3 +5,12 @@ y <- ts(rnorm(24),freq=12)
 x <- ts(rnorm(24),freq=12)
 arima0(y,xreg=x,seasonal=list(order=c(0,1,0)))
 ## Comments:
+
+## PR 644 (crash using fisher.test on Windows)
+## By: Uwe Ligges <ligges@statistik.uni-dortmund.de>
+library(ctest)
+x <- matrix(c(2, 2, 4, 8, 6, 0, 1, 1, 7, 8, 1, 3, 1, 3, 7, 4, 2, 2, 2,
+              1, 1, 0, 0, 0, 0, 0, 1, 1, 2, 0, 1, 1, 0, 2, 1, 0, 0, 0),
+            nc = 2)
+fisher.test(x)
+## Comments:
