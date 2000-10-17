5b4e4047a470aadd6d49d3cfbe315180506d62ea
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-10-17 13:06:50 +0000

    F77_SYMBOL -> F77_CALL
    git-svn-id: https://svn.r-project.org/R/trunk@10974 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/appl/bakslv.c
+++ src/appl/bakslv.c
@@ -96,9 +96,9 @@ void bakslv(double *t, int *ldt, int *n,
     nn = *ldb;
 
     for (j = 0; j < p; ++j) {/* for each right-hand side */
-       F77_SYMBOL(dcopy)(n, &b[j * nn], &c__1,
+       F77_CALL(dcopy)(n, &b[j * nn], &c__1,
 			    &x[j * nn], &c__1);
-       F77_SYMBOL(dtrsl)(t, ldt, n, &x[j * nn], job, info);
+       F77_CALL(dtrsl)(t, ldt, n, &x[j * nn], job, info);
        if (*info != 0) {
 	 return;
        }
