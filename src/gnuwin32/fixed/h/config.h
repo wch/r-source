85b7d4c53dc2fa3407c7551ebeb07e239a28b1d3
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-10-17 21:21:41 +0000

    Unix catch-up
    git-svn-id: https://svn.r-project.org/R/trunk@10975 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/fixed/h/config.h
+++ src/gnuwin32/fixed/h/config.h
@@ -107,6 +107,9 @@
    GUI to be built. */
 #undef HAVE_X11
 
+/* Define if you have IEEE 754 floating point arithmetic. */
+#define IEEE_754 1
+
 /* Define if log() is wrong for 0/-1. */
 #undef LOG_BROKEN
 
