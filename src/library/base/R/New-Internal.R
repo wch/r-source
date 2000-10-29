633c4ebc4bd7bbf158dee52957fa34d577e7bd19
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-10-29 09:12:42 +0000

    change gc column labels
    git-svn-id: https://svn.r-project.org/R/trunk@11094 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/New-Internal.R
+++ src/library/base/R/New-Internal.R
@@ -88,7 +88,7 @@ format.info <- function(x).Internal(format.info(x))
 gc <- function(verbose = getOption("verbose"))
     matrix(.Internal(gc(verbose))/c(1,1,10,10,1,1,10,10),2,4,
            dimnames = list(c("Ncells","Vcells"),
-           c("used", "(Mb)", "total", "(Mb)")))
+           c("used", "(Mb)", "gc trigger", "(Mb)")))
 gcinfo <- function(verbose).Internal(gcinfo(verbose))
 gctorture <- function(on=TRUE)invisible(.Internal(gctorture(on)))
 gray <- function(level).Internal(gray(level))
