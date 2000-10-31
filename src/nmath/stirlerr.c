c00d3d179bd744de62e68ca20646a763bd5cad6f
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-10-31 08:08:19 +0000

    adapted from C.Loader
    git-svn-id: https://svn.r-project.org/R/trunk@11115 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/nmath/stirlerr.c
+++ src/nmath/stirlerr.c
@@ -1,36 +1,25 @@
 /*
- * The author of this software is Clive Loader, clive@bell-labs.com.
- * Copyright (c) 1999-2000 Lucent Technologies, Bell Laboratories.
- * Permission to use, copy, modify, and distribute this software for any
- * purpose without fee is hereby granted, with the exceptions noted below,
- * and provided that this entire notice is included in all copies of any
- * software which is or includes a copy or modification of this software
- * and in all copies of the supporting documentation for such software.
- * THIS SOFTWARE IS BEING PROVIDED "AS IS", WITHOUT ANY EXPRESS OR IMPLIED
- * WARRANTY.  IN PARTICULAR, NEITHER THE AUTHOR NOR LUCENT TECHNOLOGIES
- * MAKE ANY REPRESENTATION OR WARRANTY OF ANY KIND CONCERNING THE
- * MERCHANTABILITY OF THIS SOFTWARE OR ITS FITNESS FOR ANY PARTICULAR PURPOSE.
+ *  AUTHOR
+ *    Catherine Loader, catherine@research.bell-labs.com.
+ *    October 23, 2000.
  *
- * This code provides functions dbinom(x,n,p) and dpois(x,lb) for computing
- * binomial and Poisson probabilities, that attempt to be accurate for
- * a full range of parameter values (standard algorithms are often
- * inaccurate with large parameters).
+ *  DESCRIPTION
  *
- * NTYPE, defined below, is the type used for the n and x arguments.
- * For 32-bit integers, the maximum n is 2^31-1=2147483647.
- * If larger n is required, NTYPE must be double.
+ *    Computes the log of the error term in Stirling's formula.
+ *      For n > 15, uses the series 1/12n - 1/360n^3 + ...
+ *      For n <=15, integers or half-integers, uses stored values.
+ *      For other n < 15, uses lgamma directly (don't use this to
+ *        write lgamma!)
  *
  * Merge in to R:
  * Copyright (C) 2000, The R Core Development Team
  */
+
 #include "nmath.h"
 
-typedef int NTYPE;/* int for R */
+/* stirlerr(n) = log(n!) - log( sqrt(2*pi*n)*(n/e)^n ) */
 
-/* stirlerr : "Error" of Stirling Expansion ( n! ) = delta(n) [C.Loader(1999)]
-   stirlerr(n) = log[ n! / sqrt(2*pi*n)*(n/e)^n ]
-*/
-double stirlerr(NTYPE n)
+double stirlerr(double n)
 {
 
 #define S0 0.083333333333333333333       /* 1/12 */
@@ -39,25 +28,54 @@ double stirlerr(NTYPE n)
 #define S3 0.000595238095238095238095238 /* 1/1680 */
 #define S4 0.0008417508417508417508417508/* 1/1188 */
 
-    static const double sfe[16] = {
-	0, 0.081061466795327258219670264,
-	0.041340695955409294093822081, 0.0276779256849983391487892927,
-	0.020790672103765093111522771, 0.0166446911898211921631948653,
-	0.013876128823070747998745727, 0.0118967099458917700950557241,
-	0.010411265261972096497478567, 0.0092554621827127329177286366,
-	0.008330563433362871256469318, 0.0075736754879518407949720242,
-	0.006942840107209529865664152, 0.0064089941880042070684396310,
-	0.005951370112758847735624416, 0.0055547335519628013710386899
+/*
+  error for 0, 0.5, 1.0, 1.5, ..., 14.5, 15.0.
+*/
+    static double sferr_halves[31] = {
+	0.0, /* n=0 - wrong, place holder only */
+	0.1534264097200273452913848,  /* 0.5 */
+	0.0810614667953272582196702,  /* 1.0 */
+	0.0548141210519176538961390,  /* 1.5 */
+	0.0413406959554092940938221,  /* 2.0 */
+	0.03316287351993628748511048, /* 2.5 */
+	0.02767792568499833914878929, /* 3.0 */
+	0.02374616365629749597132920, /* 3.5 */
+	0.02079067210376509311152277, /* 4.0 */
+	0.01848845053267318523077934, /* 4.5 */
+	0.01664469118982119216319487, /* 5.0 */
+	0.01513497322191737887351255, /* 5.5 */
+	0.01387612882307074799874573, /* 6.0 */
+	0.01281046524292022692424986, /* 6.5 */
+	0.01189670994589177009505572, /* 7.0 */
+	0.01110455975820691732662991, /* 7.5 */
+	0.010411265261972096497478567, /* 8.0 */
+	0.009799416126158803298389475, /* 8.5 */
+	0.009255462182712732917728637, /* 9.0 */
+	0.008768700134139385462952823, /* 9.5 */
+	0.008330563433362871256469318, /* 10.0 */
+	0.007934114564314020547248100, /* 10.5 */
+	0.007573675487951840794972024, /* 11.0 */
+	0.007244554301320383179543912, /* 11.5 */
+	0.006942840107209529865664152, /* 12.0 */
+	0.006665247032707682442354394, /* 12.5 */
+	0.006408994188004207068439631, /* 13.0 */
+	0.006171712263039457647532867, /* 13.5 */
+	0.005951370112758847735624416, /* 14.0 */
+	0.005746216513010115682023589, /* 14.5 */
+	0.005554733551962801371038690  /* 15.0 */
     };
     double nn;
 
-    if (n<16)
-	return(sfe[(int)n]);
-    nn = (double)n;
-    nn = nn*nn;
+    if (n <= 15.0) { 
+	nn = n + n;
+	if (nn == (int)nn) return(sferr_halves[(int)nn]);
+	return(lgamma(n + 1.) - (n + 0.5)*log(n) + n - M_LN_SQRT_2PI);
+    }
+
+    nn = n*n;
     if (n>500) return((S0-S1/nn)/n);
     if (n> 80) return((S0-(S1-S2/nn)/nn)/n);
     if (n> 35) return((S0-(S1-(S2-S3/nn)/nn)/nn)/n);
-    /* 16 <= n <= 35 : */
+    /* 15 < n <= 35 : */
     return((S0-(S1-(S2-(S3-S4/nn)/nn)/nn)/nn)/n);
 }
