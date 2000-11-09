6d4cdc6a276dcb7b0b2ffb6176208acbad54ef12
pd <pd@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-11-09 22:23:02 +0000

    cleanups and cosmetics
    git-svn-id: https://svn.r-project.org/R/trunk@11299 00db46b3-68df-0310-9c12-caf00c1e9a41

--- demos/tcltk/tkfilefind.R
+++ demos/tcltk/tkfilefind.R
@@ -1,37 +1,45 @@
 require(tcltk) || stop("tcltk support is absent")
 
-tkfilefind<-function(path=getwd(),all.names=FALSE, multiple=FALSE){
+tkfilefind<-function(path=getwd(), all.names=FALSE, multiple=FALSE){
     tclRequire("::Utility")
     tclRequire("Hierarchy")
     .Tcl("namespace import -force ::Utility::*")
-    done<-FALSE
-    base<-tktoplevel()
+    done <- FALSE
+    base <- tktoplevel()
     on.exit({if(!done) tkdestroy(base)})
-    tkwm.title(base,"Directory Tree")
-    dirtree<-tkwidget(base,"hierarchy_dir",root=path,showparent="Parent",showfiles=1,showall=as.integer(all.names),selectmode=if(multiple) "multiple" else "browse")
-    tkpack(dirtree,fill="both",expand=1)
+    tkwm.title(base, "Directory Tree")
+    dirtree<-tkwidget(base, "hierarchy_dir",
+                      root=path,
+                      showparent="Parent",
+                      showfiles=1,
+                      showall=all.names,
+                      selectmode=if(multiple) "multiple" else "browse")
+    tkpack(dirtree, fill="both", expand=1)
 
-    .tclfilename<-NULL
-    selected<-function(){
-        index<-.Tcl(paste(.Tk.ID(dirtree),"curselection"))
-        fname<-.Tcl(paste(.Tk.ID(dirtree)," get ", index))
+    .tclfilename <- NULL
+    selected <- function(){
+        index <- tkcurselection(dirtree)
+        index <- strsplit(index," ")[[1]] # multiple selections
+        fname <- tkget(dirtree, index)
         tkdestroy(base)
         if (fname==""){
-            .tclfilename<<-NULL
+            .tclfilename <<- NULL
             return()
         }
         fnamelist<-strsplit(fname,"}")[[1]]
         for (i in seq(along=fnamelist)){
-            fnamelist[i]<-gsub("[ ]*{","",fnamelist[i])
-            fnamelist[i]<-paste(strsplit(fnamelist[i]," ")[[1]],collapse=Platform()$file.sep)
+            fnamelist[i] <- gsub("[ ]*{","",fnamelist[i])
+            fnamelist[i] <- paste(strsplit(fnamelist[i]," ")[[1]],
+                                  collapse=Platform()$file.sep)
         }
-        .tclfilename<<-fnamelist
+        .tclfilename <<- fnamelist
     }
 
     buttonframe<-tkframe(base)
-    okbut<-tkbutton(buttonframe,text="Select",command=function() selected())
-    qbut<-tkbutton(buttonframe,text="Quit",command=function() tkdestroy(base))
-    tkpack(okbut,qbut,side="left")
+    okbut<-tkbutton(buttonframe, text="Select", command=selected)
+    qbut<-tkbutton(buttonframe, text="Quit",
+                   command=function() tkdestroy(base))
+    tkpack(okbut, qbut, side="left")
     tkpack(buttonframe)
 
     tkwait.window(base)
