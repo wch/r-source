4f26c591b4ed3602ed4b433fa628e8b668b32a40
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-11-10 20:29:00 +0000

    plot.ts(x,y) configurable
    git-svn-id: https://svn.r-project.org/R/trunk@11310 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/ts.R
+++ src/library/base/R/ts.R
@@ -218,44 +218,61 @@ print.ts <- function(x, calendar, ...)
 }
 
 plot.ts <-
-function (x, y = NULL, type = "l", xlim = NULL, ylim = NULL, xlab =
-          "Time", ylab, log = "", col = par("col"), bg = NA, pch =
-          par("pch"), cex = par("cex"), lty = par("lty"), lwd =
-          par("lwd"), axes = TRUE, frame.plot = axes, ann = par("ann"),
-          main = NULL, plot.type = c("multiple", "single"), ...)
+function (x, y = NULL, type = "l", xlim = NULL, ylim = NULL,
+	  xlab = "Time", ylab, log = "",
+	  col = par("col"), bg = NA,
+	  pch = par("pch"), cex = par("cex"),
+	  lty = par("lty"), lwd = par("lwd"),
+	  axes = TRUE, frame.plot = axes, ann = par("ann"),
+	  main = NULL, plot.type = c("multiple", "single"),
+	  xy.labels = n <= 150, xy.lines = do.lab, ...)
 {
     xlabel <- if (!missing(x)) deparse(substitute(x)) else NULL
     ylabel <- if (!missing(y)) deparse(substitute(y)) else NULL
     plot.type <- match.arg(plot.type)
     if(plot.type == "multiple" && NCOL(x) > 1) {
-        m <- match.call()
-        m[[1]] <- as.name("plot.mts")
-        return(eval(m, parent.frame()))
+	m <- match.call()
+	m[[1]] <- as.name("plot.mts")
+	return(eval(m, parent.frame()))
     }
     x <- as.ts(x)
     if(!is.null(y)) {
 	## want ("scatter") plot of y ~ x
 	y <- hasTsp(y)
-        if(NCOL(x) > 1 || NCOL(y) > 1)
-            stop("scatter plots only for univariate time series")
-        if(is.ts(x) && is.ts(y)){
-            xy <- ts.intersect(x, y)
-            xy <- xy.coords(xy[,1], xy[,2], xlabel, ylabel, log)
-        } else
-            xy <- xy.coords(x, y, xlabel, ylabel, log)
-	xlab <- xy$xlab
+	if(NCOL(x) > 1 || NCOL(y) > 1)
+	    stop("scatter plots only for univariate time series")
+	if(is.ts(x) && is.ts(y)){
+	    xy <- ts.intersect(x, y)
+	    xy <- xy.coords(xy[,1], xy[,2], xlabel, ylabel, log)
+	} else
+	    xy <- xy.coords(x, y, xlabel, ylabel, log)
+	xlab <- if (missing(xlab)) xy$xlab else xlab
 	ylab <- if (missing(ylab)) xy$ylab else ylab
 	xlim <- if (is.null(xlim)) range(xy$x[is.finite(xy$x)]) else xlim
 	ylim <- if (is.null(ylim)) range(xy$y[is.finite(xy$y)]) else ylim
-	plot.default(xy, type = "n", xlab = xlab, ylab = ylab, xlim =
-                     xlim, ylim = ylim, log = log, col = col, bg = bg,
-                     pch = pch, axes = axes, frame.plot = frame.plot,
-                     ann = ann, main = main, ...)
-	text(xy, labels =
-             if(all(tsp(x)==tsp(y))) formatC(time(x), wid = 1)
-             else seq(along = x),
-	     col = col, cex = cex)
-	lines(xy, col = col, lty = lty, lwd = lwd)
+	n <- length(xy $ x) #-> default for xy.l(ines|abels)
+	if(!is.logical(xy.labels)) {
+	    if(!is.character(xy.labels))
+		stop("`xy.labels' must be logical or character")
+	    do.lab <- TRUE
+	} else do.lab <- xy.labels
+	    
+        ptype <-
+            if(do.lab) "n" else if(missing(type)) "p" else type
+	plot.default(xy, type = ptype, 
+		     xlab = xlab, ylab = ylab,
+		     xlim = xlim, ylim = ylim, log = log, col = col, bg = bg,
+		     pch = pch, axes = axes, frame.plot = frame.plot,
+		     ann = ann, main = main, ...)
+	if(do.lab)
+	    text(xy, labels =
+		 if(is.character(xy.labels)) xy.labels
+		 else if(all(tsp(x) == tsp(y))) formatC(time(x), wid = 1)
+		 else seq(along = x),
+		 col = col, cex = cex)
+	if(xy.lines)
+	    lines(xy, col = col, lty = lty, lwd = lwd,
+                  type = if(do.lab) "c" else "l")
 	return(invisible())
     }
     if(missing(ylab)) ylab <- xlabel
@@ -270,13 +287,13 @@ function (x, y = NULL, type = "l", xlim = NULL, ylim = NULL, xlab =
 			  col = col[(i-1) %% length(col) + 1],
 			  lty = lty[(i-1) %% length(lty) + 1],
 			  lwd = lwd[(i-1) %% length(lwd) + 1],
-			  bg  =  bg[(i-1) %% length(bg)  + 1],
+			  bg  =	 bg[(i-1) %% length(bg)	 + 1],
 			  pch = pch[(i-1) %% length(pch) + 1],
 			  type = type)
     }
     else {
 	lines.default(time.x, x, col = col[1], bg = bg, lty = lty[1],
-                      lwd = lwd[1], pch = pch[1], type = type)
+		      lwd = lwd[1], pch = pch[1], type = type)
     }
     if (ann)
 	title(main = main, xlab = xlab, ylab = ylab, ...)
