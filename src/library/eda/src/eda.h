3942816f6ddedd6724486a89e70e628ad5d85044
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-11-30 07:48:46 +0000

    smooth(*, kind = ++)
    git-svn-id: https://svn.r-project.org/R/trunk@11602 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/eda/src/eda.h
+++ src/library/eda/src/eda.h
@@ -12,19 +12,21 @@ typedef enum {
 double med3(double u, double v, double w);
 int   imed3(double u, double v, double w);
 
-int sm_3 (double *x, double *y, int n, int end_rule);
-int sm_3R(double *x, double *y, double *z, int n, int end_rule);
-int sm_3RSR(double *x, double *y, double *z, double *w, int n,
-	    int end_rule, int split_ends);
-int sm_split3(double *x, double *y, int n, int do_ends);
+Rboolean sm_3(double *x, double *y,           int n, int end_rule);
+int sm_3R   (double *x, double *y, double *z, int n, int end_rule);
+int sm_3RS3R(double *x, double *y, double *z, int n,
+	     int end_rule, Rboolean split_ends);
+int sm_3RSS (double *x, double *y, double *z, int n,
+	     int end_rule, Rboolean split_ends);
+int sm_3RSR (double *x, double *y, double *z, double *w, int n,
+	     int end_rule, Rboolean split_ends);
+Rboolean sm_split3(double *x, double *y, int n, Rboolean do_ends);
 
 /* Callable from R : */
-void tukeysmooth(double *x, double *y, double *z, double *w,
-		 int *n, int *end_rule, int *iter);
+void Rsm_3RSR (double *x, double *y, int *n, int *end_rule, int *iter);
+void Rsm_3RSS (double *x, double *y, int *n, int *end_rule, int *iter);
+void Rsm_3RS3R(double *x, double *y, int *n, int *end_rule, int *iter);
+void Rsm_3R   (double *x, double *y, int *n, int *end_rule, int *iter);
+void Rsm_3    (double *x, double *y, int *n, int *end_rule, int *changed);
 
-void Rsm_3(double *x, double *y, int *n, int *end_rule, int *changed);
-
-void Rsm_3R(double *x, double *y, double *z,
-	    int *n, int *end_rule, int *iter);
-
-void Rsm_split3(double *x, double *y, int *n, int *do_ends, int *changed);
+void Rsm_S    (double *x, double *y, int *n, int *do_ends, int *changed);
