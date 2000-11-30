afe88780994d56ca5ab281927a424b1bfa95a62c
tlumley <tlumley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-11-30 20:42:30 +0000

    model.matrix.lm uses environment(formula) if not NULL (PR#753)
    git-svn-id: https://svn.r-project.org/R/trunk@11610 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/update.R
+++ src/library/base/R/update.R
@@ -24,7 +24,7 @@ update.default <-
 }
 
 update.formula <- function (old, new) {
-    env <- environment(old)
+    env <- environment(as.formula(old))
     tmp <- .Internal(update.formula(as.formula(old), as.formula(new)))
     out <- formula(terms.formula(tmp))
     environment(out) <- env
