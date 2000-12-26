4d4a3aea9dbf6b354e6a84a9ac978a5cdf2238e1
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-12-26 18:14:54 +0000

    Ensure that .NotYetUsed() only gives a warning.
    git-svn-id: https://svn.r-project.org/R/branches/R-1-2-patches@11958 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/polygon.R
+++ src/library/base/R/polygon.R
@@ -1,9 +1,9 @@
 polygon <- function(x, y=NULL, col=NA, border=NULL, lty=NULL, xpd=NULL,
 		    density = -1, angle = 45, ...)
 {
-    if (!missing(density))	.NotYetUsed("density")
-    if (!missing(angle))	.NotYetUsed("angle")
+    if (!missing(density)) .NotYetUsed("density", error = FALSE)
+    if (!missing(angle))   .NotYetUsed("angle",   error = FALSE)
     xy <- xy.coords(x, y)
-    ##-- FIXME: what if 'log' is active, for x or y?
+    ##-- FIXME: what if `log' is active, for x or y?
     .Internal(polygon(xy$x, xy$y, col, border, lty, xpd, ...))
 }
