8fb60dbbc2b45077559583fa38f68235fb3cb306
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-01-09 17:06:24 +0000

    mean(numeric(0),trim=.1)
    git-svn-id: https://svn.r-project.org/R/branches/R-1-2-patches@12169 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/mean.R
+++ src/library/base/R/mean.R
@@ -5,14 +5,14 @@ mean.default <- function(x, trim = 0, na.rm = FALSE) {
 	x <- x[!is.na(x)]
     trim <- trim[1]
     n <- length(c(x, recursive=TRUE)) # for data.frame
-    if(trim > 0) {
-        if(mode(x) == "complex")
-            stop("trimmed means are not defined for complex data")
+    if(trim > 0 && n > 0) {
+	if(mode(x) == "complex")
+	    stop("trimmed means are not defined for complex data")
 	if(trim >= 0.5) return(median(x, na.rm=FALSE))
 	lo <- floor(n*trim)+1
 	hi <- n+1-lo
 	x <- sort(x, partial=unique(c(lo, hi)))[lo:hi]
-        n <- hi-lo+1
+	n <- hi-lo+1
     }
     sum(x)/n
 }
