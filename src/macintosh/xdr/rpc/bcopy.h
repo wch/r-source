2651006e600bfc4b5411f7cd2d6e372e4641ff0e
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-01-25 09:41:14 +0000

    new src/macintosh/*/* dirs
    git-svn-id: https://svn.r-project.org/R/branches/R-1-2-patches@12595 00db46b3-68df-0310-9c12-caf00c1e9a41

---
+++ src/macintosh/xdr/rpc/bcopy.h
@@ -0,0 +1,15 @@
+/*********************************************************************
+ * RPC for the Windows NT Operating System
+ * 1993 by Martin F. Gergeleit
+ *
+ * RPC for the Windows NT Operating System COMES WITH ABSOLUTELY NO 
+ * WARRANTY, NOR WILL I BE LIABLE FOR ANY DAMAGES INCURRED FROM THE 
+ * USE OF. USE ENTIRELY AT YOUR OWN RISK!!!
+ *********************************************************************/
+
+void bcopy(char *,char*,int);
+void bcopy_nf(char *,char *,int);
+void bcopy_fn(char *,char *,int);
+void bcopy_ff(char *,char *,int);
+void bzero(char*,int);
+int  bcmp(char *,char *,int);
