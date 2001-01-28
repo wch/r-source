86c6c68cc2c5a51c8e73ec82bd26fd7814a8615e
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-01-28 15:40:17 +0000

    Defn.h defines required math/fp headers
    git-svn-id: https://svn.r-project.org/R/branches/R-1-2-patches@12661 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/optim.c
+++ src/main/optim.c
@@ -17,17 +17,11 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#ifndef macintosh
-#include <math.h>
-#else
-#include <fp.h>
-#endif /* mac */
-
-#include "Defn.h"
+#include <Defn.h>
 #include <Rdefines.h>		/* for CREATE_STRING_VECTOR */
-#include "R_ext/Random.h"	/* for the random number generation in
+#include <R_ext/Random.h>	/* for the random number generation in
 				   samin() */
-#include "R_ext/Applic.h"	/* setulb() */
+#include <R_ext/Applic.h>	/* setulb() */
 
 static SEXP getListElement(SEXP list, char *str)
 {
