eb33b6acb9693765d24e94e3d92b3c93428aca14
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-01-28 17:12:19 +0000

    missing ; in #ifdef-ed section
    git-svn-id: https://svn.r-project.org/R/branches/R-1-2-patches@12662 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/unix/sys-std.c
+++ src/unix/sys-std.c
@@ -722,6 +722,6 @@ SEXP do_syssleep(SEXP call, SEXP op, SEXP args, SEXP rho)
 #else
 SEXP do_syssleep(SEXP call, SEXP op, SEXP args, SEXP rho)
 {
-    error("Sys.sleep is not implemented on this system")
+    error("Sys.sleep is not implemented on this system");
 }
 #endif
