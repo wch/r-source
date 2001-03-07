fcc5c3014174e62e9cd449519280633f876a391e
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-03-07 17:01:38 +0000

    s/static/const/
    git-svn-id: https://svn.r-project.org/R/trunk@13065 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/nmath/log1p.c
+++ src/nmath/log1p.c
@@ -47,7 +47,7 @@ double log1p(double x)
      *			    significant figures required  30.93
      *				 decimal places required  32.01
      */
-     static /* const */ double alnrcs[43] = {
+    const double alnrcs[43] = {
 	+.10378693562743769800686267719098e+1,
 	-.13364301504908918098766041553133e+0,
 	+.19408249135520563357926199374750e-1,
