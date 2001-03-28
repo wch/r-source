191516d189965796a398b3126c25b783fa647035
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-03-28 19:14:59 +0000

    branch update
    git-svn-id: https://svn.r-project.org/R/trunk@13274 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/extra.c
+++ src/gnuwin32/extra.c
@@ -302,6 +302,25 @@ SEXP do_flushconsole(SEXP call, SEXP op, SEXP args, SEXP env)
     TCHAR szCSDVersion[ 128 ];
     } OSVERSIONINFO; */
 
+
+#define VER_NT_WORKSTATION              0x0000001
+#define VER_NT_DOMAIN_CONTROLLER        0x0000002
+#define VER_NT_SERVER                   0x0000003
+
+#define VER_SERVER_NT                       0x80000000
+#define VER_WORKSTATION_NT                  0x40000000
+#define VER_SUITE_SMALLBUSINESS             0x00000001
+#define VER_SUITE_ENTERPRISE                0x00000002
+#define VER_SUITE_BACKOFFICE                0x00000004
+#define VER_SUITE_COMMUNICATIONS            0x00000008
+#define VER_SUITE_TERMINAL                  0x00000010
+#define VER_SUITE_SMALLBUSINESS_RESTRICTED  0x00000020
+#define VER_SUITE_EMBEDDEDNT                0x00000040
+#define VER_SUITE_DATACENTER                0x00000080
+#define VER_SUITE_SINGLEUSERTS              0x00000100
+/* next one is a guess */
+#define VER_SUITE_PERSONAL                  0x00000200
+
 typedef struct _OSVERSIONINFOEX {
   DWORD dwOSVersionInfoSize;
   DWORD dwMajorVersion;
@@ -321,7 +340,6 @@ SEXP do_winver(SEXP call, SEXP op, SEXP args, SEXP env)
     char isNT[8]="??", ver[256];
     SEXP ans;
     OSVERSIONINFO verinfo;
-    OSVERSIONINFOEX verinfoex;
 
     checkArity(op, args);
     verinfo.dwOSVersionInfoSize = sizeof(OSVERSIONINFO);
@@ -356,17 +374,36 @@ SEXP do_winver(SEXP call, SEXP op, SEXP args, SEXP env)
     }
 
     if((int)verinfo.dwMajorVersion >= 5) {
-	
-	verinfo.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEX);
-	if(GetVersionEx(&verinfoex))
-	    sprintf(ver, "Windows %d.%d (build %d) Service Pack %d.%d (%s)",
-		    (int)verinfoex.dwMajorVersion, 
-		    (int)verinfoex.dwMinorVersion,
-		    LOWORD(verinfoex.dwBuildNumber),
-		    (int)verinfoex.wServicePackMajor,
-		    (int)verinfoex.wServicePackMinor,
-		    verinfoex.szCSDVersion);
-	else {
+	OSVERSIONINFOEX osvi;
+	osvi.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEX);
+	if(GetVersionEx(&osvi)) {
+	    char tmp[]="", *desc= tmp, *type = tmp;
+	    if(osvi.dwMajorVersion == 5 && osvi.dwMinorVersion == 0)
+		desc = "2000";
+	    if(osvi.dwMajorVersion == 5 && osvi.dwMinorVersion == 1)
+		desc = "XP";
+            if ( osvi.wProductType == VER_NT_WORKSTATION ) {
+               if( osvi.wSuiteMask & VER_SUITE_PERSONAL )
+                  type = "Personal";
+               else
+                  type = "Professional";
+            } else if ( osvi.wProductType == VER_NT_SERVER )
+            {
+               if( osvi.wSuiteMask & VER_SUITE_DATACENTER )
+                  type = "DataCenter Server";
+               else if( osvi.wSuiteMask & VER_SUITE_ENTERPRISE )
+                  type = "Advanced Server";
+               else
+		   type = "Server";
+            }
+ 	    
+	    sprintf(ver, 
+		    "Windows %s %s (build %d) Service Pack %d.%d",
+		    desc, type,
+		    LOWORD(osvi.dwBuildNumber),
+		    (int)osvi.wServicePackMajor,
+		    (int)osvi.wServicePackMinor);
+	} else {
 	    sprintf(ver, "Windows 2000 %d.%d (build %d) %s",
 		    (int)verinfo.dwMajorVersion, (int)verinfo.dwMinorVersion,
 		    LOWORD(verinfo.dwBuildNumber), verinfo.szCSDVersion);
