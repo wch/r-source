c50857a6dc716705395d1b06f5e300cec489f62a
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-04-02 21:36:58 +0000

    update from former tests/is-things.R
    git-svn-id: https://svn.r-project.org/R/trunk@13324 00db46b3-68df-0310-9c12-caf00c1e9a41

--- demos/language/is-things.R
+++ demos/language/is-things.R
@@ -10,6 +10,7 @@ cat("\nNumber of base objects:\t\t", length(ls.base),
 ## 0.50 : 33
 ## 0.60 : 34
 ## 0.62 : 35
+## 0.63 : 37
 
 ## This can be useful:	Which of the  builtin functions are "primitive" ?
 is.primitive <- function(obj)  is.function(obj) && is.null(args(obj))
@@ -25,7 +26,7 @@ is.method <- function(fname) {
 
 is.ALL <- function(obj, func.names = ls(pos=length(search())),
 		   not.using = c("is.single", "is.loaded",
-                     "is.empty.model", "is.element"),
+                     "is.empty.model", "is.R", "is.element"),
 		   true.only = FALSE, debug = FALSE)
 {
     ## Purpose: show many 'attributes' of  R object __obj__
@@ -52,7 +53,7 @@ is.ALL <- function(obj, func.names = ls(pos=length(search())),
 	rr <- if(is.primitive(fn) || length(formals(fn))>0)  fn(obj) else fn()
 	if(!is.logical(rr)) cat("f=",f," --- rr	 is NOT logical	 = ",rr,"\n")
 	##if(1!=length(rr))   cat("f=",f," --- rr NOT of length 1; = ",rr,"\n")
-	if(true.only && length(rr)==1 && rr) r <- c(r, f)
+	if(true.only && length(rr)==1 && !is.na(rr) && rr) r <- c(r, f)
 	else if(!true.only) r[[f]] <- rr
     }
     if(debug)cat("\n")
@@ -61,70 +62,37 @@ is.ALL <- function(obj, func.names = ls(pos=length(search())),
 
 print.isList <- function(r, ...)
 {
-    ## Purpose:	 print METHOD  for  'isList' objects
-    ## -------------------------------------------------------------------------
-    ## Arguments:
-    ## -------------------------------------------------------------------------
-    ## Author: Martin Maechler, Date: 12 Mar 97, 15:07
+    ## Purpose:	 print METHOD  for `isList' objects
+    ## ------------------------------------------------
+    ## Author: Martin Maechler, Date: 12 Mar 1997
     ## >>>>> needs  cmp.logical <<
     if(is.list(r)) {
 	nm <- format(names(r))
-	rr <- lapply(r,cmp.logical)
+	rr <- lapply(r, symnum, na = "NA")
 	for(i in seq(along=r)) cat(nm[i],":",rr[[i]],"\n", ...)
     } else NextMethod("print", ...)
-}
-
-cmp.logical <- function(log.v, empty.dimnames= 2)
-{
-    ## Purpose: compact printing of logicals
-    ## -------------------------------------------------------------------------
-    ## Arguments: log.v : logical vector
-    ## -------------------------------------------------------------------------
-    ## Author: Martin Maechler, Date: 13 Dec 1996, Sept 98.
-    if(!is.logical(log.v)) {
-	warning("coercing argument to logical")
-	mode(log.v) <- "logical"
-    }
-    r <- if((lv <- length(log.v))) c(".","|")[ 1+ log.v] else  "()"
-    if(is.array(log.v)) {
-        dim(r) <- d <- dim(log.v)
-        rk <- length(d)
-        do.empty <- is.logical(empty.dimnames)
-        if(do.empty) {
-            do.empty <- empty.dimnames
-            empty.dimnames <- if(do.empty) 1:rk else integer(0)
-        }
-        dimnames(r) <-
-            if(!do.empty && !is.null(n <- dimnames(log.v)))
-                lapply(n, abbreviate, minlength=1)
-            else if(length(empty.dimnames)) {
-                n <- vector(mode="list", rk)
-                for(i in empty.dimnames)
-                    n[[i]] <- rep("", d[i])
-                n
-            }
-    } else if(lv && !is.null(n <- names(log.v)))
-        names(r) <- abbreviate(n,minlength=1)
-
-    class(r) <- "noquote"
-    r
 }
 
 
 is.ALL(NULL)
 ##fails: is.ALL(NULL, not.using = c("is.single", "is.loaded"))
 is.ALL(NULL,   true.only = TRUE)
+all.equal(NULL, pairlist())
+## list() != NULL == pairlist() :
 is.ALL(list(), true.only = TRUE)
+
+(pl <- is.ALL(pairlist(1,    list(3,"A")), true.only = TRUE))
+(ll <- is.ALL(    list(1,pairlist(3,"A")), true.only = TRUE))
+all.equal(pl[pl != "is.pairlist"],
+          ll[ll != "is.vector"])## TRUE
 
 is.ALL(1:5)
 is.ALL(array(1:24, 2:4))
 is.ALL(1 + 3)
 e13 <- expression(1 + 3)
 is.ALL(e13)
-## fails (0.50-a) [is.loaded]
-##   is.ALL(e13, not.using=c("is.single", "is.finite", "is.na"))
-is.ALL(y ~ x) #--> (0.49):  NA	for 'is.na' (& is.finite)
-
+is.ALL(substitute(expression(a + 3), list(a=1)), true.only = TRUE)
+is.ALL(y ~ x) #--> NA	 for is.na & is.finite
 
 is0 <- is.ALL(numeric(0))
 is0.ok <- 1 == (lis0 <- sapply(is0, length))
@@ -141,7 +109,8 @@ is.ALL(cbind(1:3), true=TRUE)
 is.ALL(structure(1:7, names = paste("a",1:7,sep="")))
 is.ALL(structure(1:7, names = paste("a",1:7,sep="")), true.only = TRUE)
 
-x <- 1:20 ; y <- 5 + 6*x + rnorm(20) ; lm.xy <- lm(y ~ x)
-					#now (0.62) fails: is.ALL(lm.xy)
+x <- 1:20 ; y <- 5 + 6*x + rnorm(20)
+lm.xy <- lm(y ~ x)
+is.ALL(lm.xy)
 is.ALL(structure(1:7, names = paste("a",1:7,sep="")))
 is.ALL(structure(1:7, names = paste("a",1:7,sep="")), true.only = TRUE)
