e9e59679ab48562563ce51e8c0a88bf160a5dae1
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-04-06 06:41:55 +0000

    match arg names to documentation
    git-svn-id: https://svn.r-project.org/R/trunk@13378 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/connections.R
+++ src/library/base/R/connections.R
@@ -62,8 +62,8 @@ url <- function(description, open = "", encoding = getOption("encoding"))
     .Internal(url(description, open, encoding))
 
 gzfile <- function(description, open = "",
-                   encoding = getOption("encoding"), compress = 6)
-    .Internal(gzfile(description, open, encoding, compress))
+                   encoding = getOption("encoding"), compression = 6)
+    .Internal(gzfile(description, open, encoding, compression))
 
 textConnection <- function(object, open = "r")
     .Internal(textConnection(deparse(substitute(object)), object, open))
