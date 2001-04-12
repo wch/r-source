b944fec61109fec466713175f215b6ae2f086f59
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-04-12 10:40:40 +0000

    demo tests: non-base packages can be added more easily; now again do not lm.glm and nlm; become strict
    git-svn-id: https://svn.r-project.org/R/trunk@13468 00db46b3-68df-0310-9c12-caf00c1e9a41

--- tests/demos.R
+++ tests/demos.R
@@ -2,11 +2,20 @@
 .ptime <- proc.time()
 .Random.seed <- c(0,rep(7654, 3))
 
-demos <- list.files(file.path(system.file(), "demo"), pattern = "\\.R$")
-## Drop these for strict testing.
-demos <- demos[is.na(match(demos, c("nlm", "lm.glm")))]
+## Currently only do `base', but others make sense, not tcltk though :
 
-for(nam in sub("\\.R$", "", demos))
-    demo(nam, character.only = TRUE)
+## Drop these for strict testing {and add them to \testonly{.} examples
+## in ../src/library/base/man/demo.Rd }:
+dont <- list(base = c("nlm", "lm.glm")
+             )
+for(pkg in c("base")) { ## maybe other packages; 
+
+    demos <- list.files(file.path(system.file(pkg = pkg), "demo"),
+                        pattern = "\\.R$")
+    demos <- demos[is.na(match(demos, paste(dont[[pkg]], "R",sep=".")))]
+
+    for(nam in sub("\\.R$", "", demos))
+        demo(nam, character.only = TRUE)
+}
 
 cat("Time elapsed: ", proc.time() - .ptime, "\n")
