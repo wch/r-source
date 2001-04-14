8f8e7049235bb9a73fcdb8fef539493e46ee5122
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-04-14 23:18:27 +0000

    Output can now be to a connection as well.
    git-svn-id: https://svn.r-project.org/R/trunk@13512 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/write.table.R
+++ src/library/base/R/write.table.R
@@ -4,10 +4,12 @@ function (x, file = "", append = FALSE, quote = TRUE, sep = " ",
     col.names = TRUE, qmethod = c("escape", "double"))
 {
     qmethod <- match.arg(qmethod)
-    if (!is.data.frame(x))
+
+    if(!is.data.frame(x))
 	x <- data.frame(x)
-    else if (is.logical(quote) && quote)
-	quote <- which(unlist(lapply(x, function(x) is.character(x) || is.factor(x))))
+    else if(is.logical(quote) && quote)
+	quote <- which(unlist(lapply(x, function(x)
+                                     is.character(x) || is.factor(x))))
     if(dec != ".") {
     	num <- which(unlist(lapply(x, is.numeric)))
 	x[num] <- lapply(x[num],
@@ -19,58 +21,67 @@ function (x, file = "", append = FALSE, quote = TRUE, sep = " ",
         x[i] <- na
     p <- ncol(x)
     d <- dimnames(x)
-    if (is.logical(quote))
-	quote <- if (quote) 1 : p else NULL
-    else if (is.numeric(quote)) {
-	if (any(quote < 1 | quote > p))
-	    stop("invalid numbers in quote")
+    
+    if(is.logical(quote))
+	quote <- if(quote) 1 : p else NULL
+    else if(is.numeric(quote)) {
+	if(any(quote < 1 | quote > p))
+	    stop("invalid numbers in `quote'")
     }
     else
-	stop("invalid quote specification")
+	stop("invalid `quote' specification")
 
     rn <- FALSE
-    if (is.logical(row.names)) {
-	if (row.names) {
+    if(is.logical(row.names)) {
+	if(row.names) {
 	    x <- cbind(d[[1]], x)
             rn <- TRUE
         }
     }
     else {
 	row.names <- as.character(row.names)
-	if (length(row.names) == nrow(x))
+	if(length(row.names) == nrow(x))
 	    x <- cbind(row.names, x)
 	else
-	    stop("invalid row.names specification")
+	    stop("invalid `row.names' specification")
     }
-    if (!is.null(quote) && (p < ncol(x)))
+    if(!is.null(quote) && (p < ncol(x)))
 	quote <- c(0, quote) + 1
 
-    if (is.logical(col.names))
+    if(is.logical(col.names))
         col.names <- if(is.na(col.names) && rn) c("", d[[2]])
-        else if (col.names) d[[2]] else NULL
+        else if(col.names) d[[2]] else NULL
     else {
 	col.names <- as.character(col.names)
-	if (length(col.names) != p)
-	    stop("invalid col.names specification")
+	if(length(col.names) != p)
+	    stop("invalid `col.names' specification")
     }
-    if (!is.null(col.names)) {
-	if (append)
+
+    if(file == "")
+        file <- stdout()
+    else if(is.character(file)) {
+        file <- file(file, ifelse(append, "a", "w"))
+        on.exit(close(file))
+    }
+    if(!inherits(file, "connection"))
+        stop("argument `file' must be a character string or connection")
+    
+    if(!is.null(col.names)) {
+	if(append)
 	    warning("appending column names to file")
-	if (!is.null(quote))
+	if(!is.null(quote))
 	    col.names <- paste("\"", col.names, "\"", sep = "")
-	cat(col.names, file = file, sep = rep(sep, p - 1), append = append)
-	cat(eol, file = file, append = TRUE)
-	append <- TRUE
+        writeLines(paste(col.names, collapse = sep), file, sep = eol)
     }
 
     qstring <-                          # quoted embedded quote string
         switch(qmethod,
                "escape" = '\\\\"',
                "double" = '""')
-    for (i in quote)
+    for(i in quote)
 	x[, i] <- paste('"', gsub('"', qstring, x[, i]), '"', sep = "")
 
-
-    cat(t(x), file = file, sep = c(rep(sep, ncol(x) - 1), eol),
-	append = append)
+    writeLines(paste(c(t(x)), c(rep(sep, ncol(x) - 1), eol),
+                     sep = "", collapse = ""),
+               file, sep = "")
 }
