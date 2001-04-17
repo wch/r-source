ca5649c542f61f9d3cc0f1b0b4f2917819a427b6
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-04-17 13:26:20 +0000

    unix catch-up
    git-svn-id: https://svn.r-project.org/R/trunk@13576 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/fixed/bin/Rd2dvi.sh
+++ src/gnuwin32/fixed/bin/Rd2dvi.sh
@@ -8,11 +8,11 @@
 
 R_PAPERSIZE=${R_PAPERSIZE-a4}
 
-revision='$Revision: 1.10 $'
+revision='$Revision: 1.11 $'
 version=`set - ${revision}; echo ${2}`
 version="Rd2dvi.sh ${version}
 
-Copyright (C) 2000 The R Core Development Team.
+Copyright (C) 2000-2001 The R Core Development Team.
 This is free software; see the GNU General Public Licence version 2
 or later for copying conditions.  There is NO warranty." 
 
@@ -28,12 +28,16 @@ Options:
       --debug		turn on shell debugging (set -x)
       --no-clean	do not remove created temporary files
       --no-preview	do not preview generated output file
+      --os=NAME		use OS subdir \`NAME\' (unix, mac or windows)
+      --OS=NAME		the same as \`--os\'
   -o, --output=FILE	write output to FILE
       --pdf		generate PDF output
       --title=NAME	use NAME as the title of the document
   -V, --verbose		report on what is done
 
 Report bugs to <r-bugs@r-project.org>."
+
+TEXINPUTS=.:${R_HOME}/doc/manual:${TEXINPUTS}
 
 start_dir=`pwd`
 
@@ -43,11 +47,9 @@ out_ext="dvi"
 output=""
 preview=${xdvi-xdvi.bat}
 verbose=false
+OSdir=windows
 
-TEXINPUTS=.:${R_HOME}/doc/manual:${TEXINPUTS}
-
-file_sed='s/[_$]/\\&/g'
-
+${verbose} "Parsing arguments ..."
 while test -n "${1}"; do
   case ${1} in
     -h|--help)
@@ -77,6 +79,8 @@ while test -n "${1}"; do
       ;;
     --output=*)
       output=`echo "${1}" | sed -e 's/[^=]*=//'` ;;
+    --OS=*|--os=*)
+      OSdir=`echo "${1}" | sed -e 's/[^=]*=//'` ;;
     -V|--verbose)
       verbose=echo ;;
     --|*)
@@ -91,21 +95,72 @@ fi
 
 if ${debug}; then set -x; fi
 
-toc="\\Rdcontents{\\R{} topics documented:}"
-if [ ${#} -eq 1 -a -d "${1}" ]; then
-  if test -d ${1}/man; then
-    dir=${1}/man
+get_dcf_field () {
+  ## Get one field including all continuation lines from a DCF file.
+  ws="[         ]"              # space and tab
+  sed -n "/^${1}:/,/^[^ ]/{p;}" ${2} | \
+    sed -n "/^${1}:/{s/^${1}:${ws}*//;p;}
+            /^${ws}/{s/^${ws}*//;p;}"
+}
+
+Rdconv_dir_or_files_to_LaTeX () {
+  ## Convert Rd files in a dir or a list of Rd files to LaTeX, appending
+  ## the result to OUTFILE.
+  ## Usage:
+  ##   Rdconv_dir_or_files_to_LaTeX OUTFILE DIR
+  ##   Rdconv_dir_or_files_to_LaTeX OUTFILE FILES
+
+  ${verbose} $@
+
+  out="${1}"
+  shift
+
+  if test -d ${1}; then
+    files=`ls ${1}/*.[Rr]d`
+    if test -d ${1}/${OSdir}; then
+      files="${files} `ls ${1}/${OSdir}/*.[Rr]d`"
+    fi
+    files=`LC_ALL=C echo ${files} | sort`
   else
-    dir=${1}
+    files="${@}"
   fi
-  ${verbose} "Rd2dvi: \`Rdconv -t latex' ${dir}/ Rd files ..."
-  files=`LC_ALL=C find ${dir} -name "*.[Rr]d" -print | \
-    sed -e '/unix\//d' | sort`
-  subj="all in \\file{`echo ${dir} | sed ${file_sed}`}"
-  ${verbose} "files = ${files}"
+
+  echo "Converting Rd files to LaTeX ..."
+
+  for f in ${files}; do
+    echo ${f}
+    ${R_CMD} Rdconv -t latex ${f} >> ${out}
+  done
+
+}
+
+is_bundle=no
+file_sed='s/[_$]/\\&/g'
+
+toc="\\Rdcontents{\\R{} topics documented:}"
+if test -d "${1}"; then
+  if test -f ${1}/DESCRIPTION; then
+    if test -n "`grep '^Bundle:' ${1}/DESCRIPTION`"; then
+      echo "Hmm ... looks like a package bundle"
+      is_bundle=yes
+      bundle_name=`get_dcf_field Bundle "${1}/DESCRIPTION"`
+      bundle_pkgs=`get_dcf_field Contains "${1}/DESCRIPTION"`
+      title=${title-"Bundle \`${bundle_name}'"}
+    else
+      echo "Hmm ... looks like a package"
+      title=${title-"Package \`${1}'"}
+      dir=${1}/man
+    fi
+  else
+    if test -d ${1}/man; then
+      dir=${1}/man
+    else
+      dir=${1}
+    fi
+    subj="all in \\file{`echo ${dir} | sed ${file_sed}`}"
+  fi
 else
-  files="${@}"
-  if [ ${#} -gt 1 ]; then
+  if test ${#} -gt 1 ; then
     subj=" etc.";
   else
     subj=
@@ -113,11 +168,9 @@ else
   fi
   subj="\\file{`echo ${1} | sed ${file_sed}`}${subj}"
 fi
+title=${title-"\\R{} documentation}} \\par\\bigskip{{\\Large of ${subj}"}
 
-${verbose} ""
-${verbose} "subj: ${subj}"
-${verbose} ""
-
+## Prepare for building the documentation.
 if test -f ${output}; then
   echo "file \`${output}' exists; please remove first"
   exit 1
@@ -129,12 +182,10 @@ if test -d ${build_dir}; then
   rm -rf ${build_dir} || echo "cannot write to build dir" && exit 2
 fi
 mkdir ${build_dir}
-
 sed 's/markright{#1}/markboth{#1}{#1}/' \
   ${R_HOME}/doc/manual/Rd.sty > ${build_dir}/Rd.sty
 
-title=${title-"\\R{} documentation}} \\par\\bigskip{{\\Large of ${subj}"}
-
+## Rd2.tex part 1: header
 cat > ${build_dir}/Rd2.tex <<EOF
 \\documentclass[${R_PAPERSIZE}paper]{book}
 \\usepackage[${R_RD4DVI-ae}]{Rd}
@@ -144,17 +195,31 @@ cat > ${build_dir}/Rd2.tex <<EOF
 \\chapter*{}
 \\begin{center}
 {\\textbf{\\huge ${title}}}
-\\par\\medskip{\\large \\today}
+\\par\\bigskip{\\large \\today}
 \\end{center}
-${toc}
 EOF
 
-echo "Converting Rd files to LaTeX ..."
-for f in ${files}; do
-  echo ${f}
-  Rcmd Rdconv -t latex ${f} >> ${build_dir}/Rd2.tex
-done
+## Rd2.tex part 2: body
+if test ${is_bundle} = no; then
+  echo ${toc} >> ${build_dir}/Rd2.tex
+  Rdconv_dir_or_files_to_LaTeX ${build_dir}/Rd2.tex ${dir-${@}}
+else
+  ## <FIXME>
+  ## echo "\\tableofcontents{}" >> ${build_dir}/Rd2.tex
+  echo ${toc} >> ${build_dir}/Rd2.tex
+  ## </FIXME>
+  for p in ${bundle_pkgs}; do
+    echo "Bundle package: \`${p}'"
+    (echo "\\chapter*{Package \`${p}'}"
+      ## <FIXME>
+      ## echo ${toc}
+      ## </FIXME>
+      ) >> ${build_dir}/Rd2.tex
+    Rdconv_dir_or_files_to_LaTeX ${build_dir}/Rd2.tex ${1}/${p}/man
+  done
+fi
 
+## Rd2.tex part 3: footer
 cat >> ${build_dir}/Rd2.tex <<EOF
 \\printindex
 \\end{document}
@@ -167,6 +232,8 @@ ${R_MAKEINDEXCMD-makeindex} Rd2
 ${R_LATEXCMD-latex} Rd2
 cd ${start_dir}
 cp ${build_dir}/Rd2.${out_ext} ${output}
+echo "Done"
+
 if ${clean}; then
   rm -rf ${build_dir}
 else
