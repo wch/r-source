cd310e505e52cd85215cb42d06e3a91874cdf098
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-04-18 10:31:09 +0000

    add missing declarations (-u)
    git-svn-id: https://svn.r-project.org/R/trunk@13608 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/xxxpr.f
+++ src/main/xxxpr.f
@@ -2,24 +2,30 @@ C Mostly for S compatibility.
 C These now all call C functions via F77_NAME(.) in ./print.c :
 
       subroutine intpr(label, nchar, data, ndata)
+      integer nchar, ndata
       character*(*) label
       integer data(ndata)
+      integer nc
       nc = nchar
       if(nc .lt. 0) nc = len(label)
       call intpr0(label, nc, data, ndata)
       end
 
       subroutine realpr(label, nchar, data, ndata)
+      integer nchar, ndata
       character*(*) label
       real data(ndata)
+      integer nc
       nc = nchar
       if(nc .lt. 0) nc = len(label)
       call realp0(label, nc, data, ndata)
       end
 
       subroutine dblepr(label, nchar, data, ndata)
+      integer nchar, ndata
       character*(*) label
       double precision data(ndata)
+      integer nc
       nc = nchar
       if(nc .lt. 0) nc = len(label)
       call dblep0(label, nc, data, ndata)
