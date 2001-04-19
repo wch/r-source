c130cc9d7bb4b8f9946493e68b2e9bb6e82ebb24
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-04-19 17:56:45 +0000

    reformat, tidy up conditions
    git-svn-id: https://svn.r-project.org/R/trunk@13654 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/appl/sock.c
+++ src/appl/sock.c
@@ -43,7 +43,10 @@ if defined(__MWERKS__) && defined(Macintosh)
 #include <types.h>
 #endif
 #include <signal.h>
+#ifdef HAVE_ERRNO_H
 #include <errno.h>
+#endif
+
 #if defined(Win32)
 #  include <winsock.h>
 #  include <io.h>
@@ -60,7 +63,8 @@ if defined(__MWERKS__) && defined(Macintosh)
 #    include <netinet/tcp.h>
 #  endif
 #endif
-#include "R_ext/Error.h"
+
+#include <R_ext/Error.h>
 #include "sock.h"
 
 #if defined(__hpux) || defined(MACINTOSH)
@@ -75,193 +79,179 @@ static char socket_msg[] = "sockets are not available on this system\n";
 
 static int Sock_error(Sock_error_t perr, int e, int he)
 {
-  if (perr != NULL) {
-    perr->error = e;
-    perr->h_error = he;
-  }
-  return -1;
+    if (perr != NULL) {
+	perr->error = e;
+	perr->h_error = he;
+    }
+    return -1;
 }
 
 #ifdef MACINTOSH
 extern void __sinit(void);
 extern int __initialize (void *ignoredParameter);
 int __initialize(void *ignoredParameter) {
-  __sinit();
-  return(0);
+    __sinit();
+    return(0);
 }
 #endif
 
 int Sock_init()
 {
 #if defined(Win32)
-  WSADATA wsaData;
-  WORD wVers = MAKEWORD(1, 1);
-  if (WSAStartup(wVers, &wsaData) != 0)
-    return 1;
+    WSADATA wsaData;
+    WORD wVers = MAKEWORD(1, 1);
+    if (WSAStartup(wVers, &wsaData) != 0)
+	return 1;
 #elif defined(MACINTOSH)
-  GUSISetup(GUSIwithInternetSockets);
+    GUSISetup(GUSIwithInternetSockets);
 #elif defined(SIGPIPE)
-  struct sigaction act;
-  if (sigaction(SIGPIPE, (struct sigaction *)NULL, &act) < 0)
-    return 1;
-  if (act.sa_handler == SIG_DFL) {
-    act.sa_handler = SIG_IGN;
-    if (sigaction(SIGPIPE, &act, (struct sigaction *)NULL) < 0)
-      return 1;
-  }
+    struct sigaction act;
+    if (sigaction(SIGPIPE, (struct sigaction *)NULL, &act) < 0)
+	return 1;
+    if (act.sa_handler == SIG_DFL) {
+	act.sa_handler = SIG_IGN;
+	if (sigaction(SIGPIPE, &act, (struct sigaction *)NULL) < 0)
+	    return 1;
+    }
 #endif
-  return 0;
+    return 0;
 }
 
 int Sock_open(Sock_port_t port, Sock_error_t perr)
 {
-#if !(defined(Unix) || defined(Macintosh)) || defined(HAVE_BSD_NETWORKING) 
-/* Jago :was
-              #if !defined(Unix) || defined(HAVE_BSD_NETWORKING) 
-*/
-  int sock;
-  struct sockaddr_in server;
+#if defined(Win32) || defined(HAVE_BSD_NETWORKING) 
+    int sock;
+    struct sockaddr_in server;
 
-  if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    return Sock_error(perr, errno, 0);
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	return Sock_error(perr, errno, 0);
 
-  server.sin_family = AF_INET;
-  server.sin_addr.s_addr = INADDR_ANY;
-  server.sin_port = htons((short)port);
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons((short)port);
 
-  if ((bind(sock, (struct sockaddr *)&server, sizeof(server)) < 0) ||
-      (listen(sock, MAXBACKLOG) < 0))
-    return Sock_error(perr, errno, 0);
-  return sock;
+    if ((bind(sock, (struct sockaddr *)&server, sizeof(server)) < 0) ||
+	(listen(sock, MAXBACKLOG) < 0))
+	return Sock_error(perr, errno, 0);
+    return sock;
 #else
-  error(socket_msg);
-  return(-1);
+    error(socket_msg);
+    return(-1);
 #endif
 }
 
 int Sock_listen(int fd, char *cname, int buflen, Sock_error_t perr)
 {
-#if !(defined(Unix) || defined(Macintosh)) || defined(HAVE_BSD_NETWORKING) 
-/* Jago: was
-              #if !defined(Unix) || defined(HAVE_BSD_NETWORKING)
-*/
-  struct sockaddr_in net_client;
-  SOCKLEN_T len = sizeof(struct sockaddr);
-  int retval;
-  struct hostent *hostptr;
+#if defined(Win32) || defined(HAVE_BSD_NETWORKING) 
+    struct sockaddr_in net_client;
+    SOCKLEN_T len = sizeof(struct sockaddr);
+    int retval;
+    struct hostent *hostptr;
 
-  do
-    retval = accept(fd, (struct sockaddr *)(&net_client), &len);
-  while (retval == -1 && errno == EINTR);
-  if (retval == -1)
-    return Sock_error(perr, errno, 0);
+    do
+	retval = accept(fd, (struct sockaddr *)(&net_client), &len);
+    while (retval == -1 && errno == EINTR);
+    if (retval == -1)
+	return Sock_error(perr, errno, 0);
 
-  if (cname != NULL && buflen > 0) {
-    size_t nlen;
-    char *name;
-    struct in_addr *iaddr = &(net_client.sin_addr);
-    hostptr = gethostbyaddr((char *)iaddr, sizeof(struct in_addr), AF_INET);
-    name = (hostptr == NULL) ? "unknown" :  hostptr->h_name;
-    nlen = strlen(name);
-    if (buflen < nlen + 1)
-      nlen = buflen - 1;
-    strncpy(cname, name, nlen);
-    cname[nlen] = 0;
-  }
-  return retval;
+    if (cname != NULL && buflen > 0) {
+	size_t nlen;
+	char *name;
+	struct in_addr *iaddr = &(net_client.sin_addr);
+	hostptr = gethostbyaddr((char *)iaddr, sizeof(struct in_addr), 
+				AF_INET);
+	name = (hostptr == NULL) ? "unknown" :  hostptr->h_name;
+	nlen = strlen(name);
+	if (buflen < nlen + 1)
+	    nlen = buflen - 1;
+	strncpy(cname, name, nlen);
+	cname[nlen] = 0;
+    }
+    return retval;
 #else
-  error(socket_msg);
-  return(-1);
+    error(socket_msg);
+    return(-1);
 #endif
 }
 
 int Sock_connect(Sock_port_t port, char *sname, Sock_error_t perr)
 {
-#if !(defined(Unix) || defined(Macintosh)) || defined(HAVE_BSD_NETWORKING) 
-/* Jago: was
-     #if !defined(Unix) || defined(HAVE_BSD_NETWORKING)
-*/
-  struct sockaddr_in server;
-  struct hostent *hp;
-  int sock;
-  int retval;
+#if defined(Win32) || defined(HAVE_BSD_NETWORKING) 
+    struct sockaddr_in server;
+    struct hostent *hp;
+    int sock;
+    int retval;
 
-  if (! (hp = gethostbyname(sname))
-      || (sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    return Sock_error(perr, errno, h_errno);
+    if (! (hp = gethostbyname(sname))
+	|| (sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	return Sock_error(perr, errno, h_errno);
 
-  memcpy((char *)&server.sin_addr, hp->h_addr_list[0], hp->h_length);
-  server.sin_port = htons((short)port);
-  server.sin_family = AF_INET;
+    memcpy((char *)&server.sin_addr, hp->h_addr_list[0], hp->h_length);
+    server.sin_port = htons((short)port);
+    server.sin_family = AF_INET;
 
-  do
-    retval = connect(sock, (struct sockaddr *) &server, sizeof(server));
-  while (retval == -1 && errno == EINTR);
-  if (retval == -1) {
-    Sock_error(perr, errno, 0);
+    do
+	retval = connect(sock, (struct sockaddr *) &server, sizeof(server));
+    while (retval == -1 && errno == EINTR);
+    if (retval == -1) {
+	Sock_error(perr, errno, 0);
 #ifdef Win32
-    closesocket(sock);
+	closesocket(sock);
 #else
-    close(sock);
+	close(sock);
 #endif
-    return -1;
-  }
-  return sock;
+	return -1;
+    }
+    return sock;
 #else
-  error(socket_msg);
-  return(-1);
+    error(socket_msg);
+    return(-1);
 #endif
 }
 
 int Sock_close(int fd, Sock_error_t perr)
 {
 #ifdef Win32
-  if (closesocket(fd) != 0)
-    return Sock_error(perr, WSAENOTSOCK, 0);
+    if (closesocket(fd) != 0)
+	return Sock_error(perr, WSAENOTSOCK, 0);
 #else
-  if (close(fd) < 0)
-    return Sock_error(perr, errno, 0);
+    if (close(fd) < 0)
+	return Sock_error(perr, errno, 0);
 #endif
-  else
-    return 0;
+    else
+	return 0;
 }
 
 ssize_t Sock_read(int fd, void *buf, size_t size, Sock_error_t perr)
 {
-#if !(defined(Unix) || defined(Macintosh))|| defined(HAVE_BSD_NETWORKING) 
-/* Jago: was
- #if !defined(Unix) || defined(HAVE_BSD_NETWORKING)
-*/
-  ssize_t retval;
-  do
-    retval = recv(fd, buf, size, 0);
-  while (retval == -1 && errno == EINTR);
-  if (retval == -1)
-    return Sock_error(perr, errno, 0);
-  else
-    return retval;
+#if defined(Win32) || defined(HAVE_BSD_NETWORKING) 
+    ssize_t retval;
+    do
+	retval = recv(fd, buf, size, 0);
+    while (retval == -1 && errno == EINTR);
+    if (retval == -1)
+	return Sock_error(perr, errno, 0);
+    else
+	return retval;
 #else
-  error(socket_msg);
-  return(-1);
+    error(socket_msg);
+    return(-1);
 #endif
 }
 
 ssize_t Sock_write(int fd, void *buf, size_t size, Sock_error_t perr)
 {
-#if !(defined(Unix) || defined(Macintosh)) || defined(HAVE_BSD_NETWORKING) 
-/* Jago: was
- #if !defined(Unix) || defined(HAVE_BSD_NETWORKING)
-*/
-  ssize_t retval;
-  do
-    retval = send(fd, buf, size, 0);
-  while (retval == -1 && errno == EINTR);
-  if (retval == -1)
-    return Sock_error(perr, errno, 0);
-  else
-    return retval;
+#if defined(Win32) || defined(HAVE_BSD_NETWORKING) 
+    ssize_t retval;
+    do
+	retval = send(fd, buf, size, 0);
+    while (retval == -1 && errno == EINTR);
+    if (retval == -1)
+	return Sock_error(perr, errno, 0);
+    else
+	return retval;
 #else
-  error(socket_msg);
-  return(-1);
+    error(socket_msg);
+    return(-1);
 #endif
 }
