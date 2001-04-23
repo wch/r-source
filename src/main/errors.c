00f5d3eaf76ad470ca5c1334b9ccb05b8988e2ad
pd <pd@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-04-23 10:23:53 +0000

    Don't vmaxset(NULL) until sure that we're not restarting (PR#877). Couple of other changes suggested by Robert should prevent similar problems with PP stack and eval depth.
    git-svn-id: https://svn.r-project.org/R/branches/R-1-2-patches@13728 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/errors.c
+++ src/main/errors.c
@@ -395,7 +395,6 @@ void jump_to_toplevel()
     R_FlushConsole();
     R_ClearerrConsole();
     R_ParseError = 0;
-    vmaxset(NULL);
     if (R_GlobalContext->cend != NULL)
 	(R_GlobalContext->cend) ();
     for (c = R_GlobalContext; c; c = c->nextcontext) {
@@ -410,6 +409,7 @@ void jump_to_toplevel()
 	if (c->callflag == CTXT_TOPLEVEL)
 	    break;
     }
+    vmaxset(NULL);
     if ( !R_Interactive && !haveHandler && inError ) {
 	REprintf("Execution halted\n");
 	R_CleanUp(SA_NOSAVE, 1, 0); /* quit, no save, no .Last, status=1 */
@@ -458,7 +458,8 @@ void Rf_resetStack(int topLevelReset) {
 	REprintf("Lost warning messages\n");
     inError=0;
     inWarning=0;
-    R_PPStackTop = 0;
+    R_PPStackTop = R_ToplevelContext->cstacktop;
+    R_EvalDepth = R_ToplevelContext->evaldepth;
     R_Warnings = R_NilValue;
     R_CollectWarnings = 0;
     if(topLevelReset) {
