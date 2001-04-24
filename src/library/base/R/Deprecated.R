bd1f628d20064f30d9705be2abf3c7954abe4f7b
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-04-24 07:14:21 +0000

    deprecate {read.table,scan,source}.url, httpclient,   "socket" method of download.file
    git-svn-id: https://svn.r-project.org/R/trunk@13752 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/Deprecated.R
+++ src/library/base/R/Deprecated.R
@@ -15,3 +15,100 @@ getenv <- function(x)
     Sys.getenv(x)
 }
 
+"httpclient" <-
+    function (url, port = 80, error.is.fatal = TRUE, check.MIME.type = TRUE,
+              file = tempfile(), drop.ctrl.z = TRUE)
+{
+    .Deprecated()
+    allowed.MIME.types <- c("text/", "application/postscript",
+                            "application/x-latex")
+    urlel <- strsplit(url, "/")[[1]]
+    if (urlel[1] != "http:")
+        stop("Not an http:// URL")
+    host <- urlel[3]
+    rurl <- paste(c("", urlel[-(1:3)]), collapse = "/")
+    a <- make.socket(host, port = port)
+    on.exit(close.socket(a))
+    headreq <- paste("HEAD", rurl, "HTTP/1.0\r\nConnection: Keep-Alive\r\nAccept: text/plain\r\n\r\n")
+    write.socket(a, headreq)
+    head <- read.socket(a, maxlen = 8000)
+    b <- strsplit(head, "\n")[[1]]
+    if (length(grep("200 OK", b[1])) == 0) {
+        if (error.is.fatal)
+            stop(b[1])
+        else warning(b[1])
+        return(file)
+    }
+    if (check.MIME.type && length(unlist(lapply(allowed.MIME.types,
+                                                function(x) grep(x, strsplit(grep("Content-Type:", b,
+                                                                                  value = TRUE), ":")[[1]][2])))) == 0) {
+        if (error.is.fatal)
+            stop(grep("Content-Type:", b, value = TRUE))
+        else warning(grep("Content-Type:", b, value = TRUE))
+        return(file)
+    }
+    len <- as.numeric(strsplit(grep("Content-Length", b, value = TRUE),
+                               ":")[[1]][2])
+    getreq <- paste("GET", rurl, "HTTP/1.0\r\nConnection: Keep-Alive\r\nAccept: text/plain\r\n\r\n")
+    write.socket(a, getreq)
+    junk <- read.socket(a, maxlen = nchar(head))
+    data <- ""
+    b <- strsplit(c(head, junk), "\n")
+    nn <- length(b[[1]])
+    if (length(b[[2]]) > nn)
+        data <- paste(b[[2]][-(1:nn)], collapse = "\n")
+    while (nchar(data) < len) {
+        data <- paste(data, read.socket(a, maxlen = len - nchar(data)),
+                      sep = "")
+    }
+    if (drop.ctrl.z)
+        data <- gsub("\032", "", data, extended = FALSE)
+    cat(data, file = file)
+    return(file)
+}
+
+"read.table.url" <-
+    function (url, method, ...)
+{
+    .Deprecated("read.table(url())")
+    f <- tempfile()
+    if (download.file(url, destfile=f, method=method) == 0) {
+        data <- read.table(f, ...)
+        unlink(f)
+    } else {
+        unlink(f)
+        stop("transfer failure")
+    }
+    return(data)
+}
+
+"scan.url" <-
+    function (url, file=tempfile(), method, ...)
+{
+    .Deprecated("scan(url())")
+    if (download.file(url, dest=file, method=method) != 0){
+        unlink(file)
+        stop("transfer failed")
+    }
+    data <- scan(file, ...)
+    unlink(file)
+    return(data)
+}
+
+"source.url" <-
+    function (url, file=tempfile(), method, ...)
+{
+    .Deprecated("source(url())")
+    if (download.file(url, dest=file, method=method) != 0) {
+        unlink(file)
+        stop("transfer failure")
+    }
+    m <- match.call()
+    m[[1]] <- as.name("source")
+    m$url <- NULL
+    m$port <- NULL
+    m$file <- file
+    eval(m, parent.frame())
+    unlink(file)
+}
+
