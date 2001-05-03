eb31e2c5261b3a2a6758afcdc557947e03a67d4c
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-05-03 13:35:58 +0000

    New function Rdpp(), a simple Rd preprocessor.
    git-svn-id: https://svn.r-project.org/R/trunk@13915 00db46b3-68df-0310-9c12-caf00c1e9a41

--- share/perl/R/Rd.pm
+++ share/perl/R/Rd.pm
@@ -27,28 +27,51 @@ sub info {
     my ($class, $file) = @_;
     $class = ref($class) || $class;
 
-    my $fh = new FileHandle "< $file" or croak "open($file): $!\n";
     my $self = {};
-    while(<$fh>){
-        if(/\\name\{([^\}]*)\}/){
-	    $self->{"name"} = $1;
-	}
-        if(/\\title\{\s*([^\}]*)\s*\}/s){
-	    $self->{"title"} = $1;
-	    $self->{"title"} =~ s/\s+/ /sg;
-	}
-        if(/\\alias\{([^\}]*)\}/){
-	    push @{$self->{"aliases"}}, $1;
-	}
-        if(/\\keyword\{([^\}]*)\}/){
-	    push @{$self->{"keywords"}}, $1;
-	}
-	    
+    $_ = Rdpp($file);
+    if(/\\name\{([^\}]*)\}/){
+	$self->{"name"} = $1;
     }
-    $fh->close;
+    if(/\\title\{\s*([^\}]*)\s*\}/s){
+	$self->{"title"} = $1;
+	$self->{"title"} =~ s/\s+/ /sg;
+    }
+    if(/\\alias\{([^\}]*)\}/){
+	push @{$self->{"aliases"}}, $1;
+    }
+    if(/\\keyword\{([^\}]*)\}/){
+	push @{$self->{"keywords"}}, $1;
+    }
     bless $self, $class;
 }
 
+sub Rdpp {
+
+    my $file = $_[0];
+    my $OS = $_[1];
+    my $fh = new FileHandle "< $file" or croak "open($file): $!\n";
+    my $skipping;
+    my $text;
+    $OS = "unix" unless $OS;    
+    while(<$fh>) {
+        if (/^#ifdef\s+([A-Za-z0-9]+)/o) {
+            if ($1 ne $OS) { $skipping = 1; }
+            next;
+        }
+        if (/^#ifndef\s+([A-Za-z0-9]+)/o) {
+            if ($1 eq $OS) { $skipping = 1; }
+            next;
+        }
+        if (/^#endif/o) {
+            $skipping = 0;
+            next;
+        }
+        next if $skipping > 0;
+	next if /^\s*%/o;
+        $text .= $_;
+    }
+    close($fh);
+    $text;
+}
+
 1;
-
-
