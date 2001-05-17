56bf3f73d3689c33f9e8c86c17a2925b79245f3a
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-05-17 11:10:53 +0000

    add row labels to the result
    git-svn-id: https://svn.r-project.org/R/trunk@14198 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/mva/R/cancor.R
+++ src/library/mva/R/cancor.R
@@ -37,9 +37,10 @@ cancor <- function(x, y, xcenter=TRUE, ycenter=TRUE)
     ## compute svd(Qx'Qy)
     z <- La.svd(qr.qty(qx, qr.qy(qy, diag(1, nr, dy)))[1:dx,, drop = FALSE],
                 dx, dy)
-    list(cor = z$d,
-	 xcoef = backsolve((qx$qr)[1:dx, 1:dx, drop = FALSE], z$u),
-	 ycoef = backsolve((qy$qr)[1:dy, 1:dy, drop = FALSE], t(z$vt)),
-	 xcenter = xcenter,
+    xcoef <- backsolve((qx$qr)[1:dx, 1:dx, drop = FALSE], z$u)
+    rownames(xcoef) <- colnames(x)
+    ycoef <-  backsolve((qy$qr)[1:dy, 1:dy, drop = FALSE], t(z$vt))
+    rownames(ycoef) <- colnames(y)
+    list(cor = z$d, xcoef = xcoef, ycoef = ycoef, xcenter = xcenter,
 	 ycenter = ycenter)
 }
