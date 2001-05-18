f6b93479a508534efd7ae39cf2ebde542e5f63a8
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-05-18 10:49:01 +0000

    case of package name now matters
    git-svn-id: https://svn.r-project.org/R/trunk@14216 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tcltk/R/windows/zzz.R
+++ src/library/tcltk/R/windows/zzz.R
@@ -1,8 +1,8 @@
 .First.lib <- function(lib, pkg)
 {
     if(!nchar(Sys.getenv("TCL_LIBRARY"))) stop("TCL_LIBRARY is not set")
-    library.dynam("TclTk", pkg, lib)
-    .C("tcltk_start", PACKAGE="TclTk")
+    library.dynam("tcltk", pkg, lib)
+    .C("tcltk_start", PACKAGE="tcltk")
     extra <- system.file("exec", package = "tcltk")
     extra <- gsub("\\\\", "/", extra)
     invisible(addTclPath(extra))
@@ -10,9 +10,9 @@
 
 .Last.lib <- function(libpath) {
     if(is.loaded(symbol.C("tcltk_end"))) {
-        .C("tcltk_end", PACKAGE="TclTk")
-        dyn.unload(file.path(libpath, "libs", "TclTk.dll"))
-        num <- match("TclTk", .Dyn.libs)
+        .C("tcltk_end", PACKAGE="tcltk")
+        dyn.unload(file.path(libpath, "libs", "tcltk.dll"))
+        num <- match("tcltk", .Dyn.libs)
         assign(".Dyn.libs", .Dyn.libs[-num], envir=.AutoloadEnv)
     }
 }
