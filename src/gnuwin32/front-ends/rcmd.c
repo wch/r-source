28748fc9641309ff1ca3ca7134d91fa68475e407
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-05-23 09:55:16 +0000

    Add R_OSTYPE
    git-svn-id: https://svn.r-project.org/R/trunk@14329 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/front-ends/rcmd.c
+++ src/gnuwin32/front-ends/rcmd.c
@@ -48,6 +48,7 @@ int main (int argc, char **argv)
    
     putenv("R_CMD=Rcmd");
     putenv("R_UNDER_WINDOWS=1");
+    putenv("R_OSTYPE=windows");
 
     strcpy(PATH, "PATH=");
     strcat(PATH, RHome); strcat(PATH, "\\bin;");
