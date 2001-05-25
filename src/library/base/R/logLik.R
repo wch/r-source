fc8d22760b54da4974f39ee9dbee02bbbc267129
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-05-25 12:24:47 +0000

    logLik[.lm]() in base
    git-svn-id: https://svn.r-project.org/R/trunk@14355 00db46b3-68df-0310-9c12-caf00c1e9a41

---
+++ src/library/base/R/logLik.R
@@ -0,0 +1,33 @@
+## from package:nls
+logLik <- function(object, ...) UseMethod("logLik")
+
+## from package:nlme 
+logLik.lm <- 
+  ## log-likelihood for lm objects
+  function(object, REML = FALSE)
+{
+    res <- resid(object)
+    p <- object$rank
+    N <- length(res)
+    if(is.null(w <- object$weights)) {
+        w <- rep(1, N)
+    } else {
+        excl <- w == 0			# eliminating zero weights
+        if (any(excl)) {
+            res <- res[!excl]
+            N <- length(res)
+            w <- w[!excl]
+        }
+    }
+    N0 <- N
+    if(REML) N <- N - p
+    val <- .5* (sum(log(w)) - N * (log(2 * pi) + 1 - log(N) +
+                                   log(sum(w*res^2))))
+    if(REML) val <- val - sum(log(abs(diag(object$qr$qr)[1:p])))
+    attr(val, "nall") <- N0
+    attr(val, "nobs") <- N
+    attr(val, "df") <- p + 1
+    class(val) <- "logLik"
+    val
+}
+
