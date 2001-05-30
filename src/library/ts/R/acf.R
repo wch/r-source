00b416704330dda0c3dc1b26826a05488b7ecb57
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-05-30 12:21:53 +0000

    improved plot.acf()
    git-svn-id: https://svn.r-project.org/R/trunk@14410 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/ts/R/acf.R
+++ src/library/ts/R/acf.R
@@ -110,49 +110,55 @@ pacf.mts <- function(x, lag.max = NULL, plot = TRUE, na.action = na.fail, ...)
 plot.acf <-
     function (x, ci = 0.95, type = "h", xlab = "Lag", ylab = NULL,
               ylim = NULL, main = NULL, ci.col="blue",
-              ci.type=c("white", "ma"), ...)
+              ci.type = c("white", "ma"), max.mfrow = 5,
+              mar = if(nser > 2) c(3,3,2,1.2) else par("mar"),
+              mgp = if(nser > 2) c(1.5,0.6,0) else par("mgp"),
+              cex.main = if(nser > 2) 1 else par("cex.main"),
+              ...)
 {
     ci.type <- match.arg(ci.type)
-    nser <- ncol(x$lag)
-    if(nser > 1) {
-        opar <- par(mfrow = rep(min(nser, 5), 2))
-        on.exit(par(opar))
-    }
+    if((nser <- ncol(x$lag)) < 1) stop("x$lag must have at least 1 column")
     if (is.null(ylab))
-        ylab <- switch(x$type, correlation = "ACF", covariance = "ACF",
-            partial = "Partial ACF")
-    if (is.null(snames <- x$snames)) {
-        snames <- if (nser == 1)
-            paste("Series ", x$series)
-        else paste("Series ", 1:nser)
-    }
-    with.ci <- (ci > 0) && (x$type != "covariance")
+        ylab <- switch(x$type,
+                       correlation = "ACF",
+                       covariance = "ACF (cov)",
+                       partial = "Partial ACF")
+    if (is.null(snames <- x$snames))
+        snames <- paste("Series ", if (nser == 1) x$series else 1:nser)
+
+    with.ci <- ci > 0 && x$type != "covariance"
     with.ci.ma <- with.ci && ci.type == "ma" && x$type == "correlation"
+    clim0 <- if (with.ci) qnorm((1 + ci)/2)/sqrt(x$n.used) else c(0, 0)
+
+    if(nser > 1) { # at most m x m (m := max.mfrow  (= 5 by default): 
+        sn.abbr <- if(nser > 2) abbreviate(snames) else .Alias(snames)
+        nr <- min(nser, max.mfrow)
+        opar <- par(mfrow = rep(nr, 2), mar = mar, mgp = mgp,
+                    cex.main = cex.main)
+        on.exit(par(opar))
+        cat("par(*) : ");str(par("mfrow","cex",
+                                 "cex.main","cex.axis","cex.lab","cex.sub"))
+    }
     for (i in 1:nser) for (j in 1:nser) {
-        clim <- c(0, 0)
-        if (with.ci)
-            clim <- qnorm((1 + ci)/2)/sqrt(x$n.used)
-        if (with.ci.ma && i == j)
-            clim <- clim * sqrt(cumsum(c(1, 2*x$acf[-1, i, j]^2)))
+        clim <- if (with.ci.ma && i == j)
+            clim0 * sqrt(cumsum(c(1, 2*x$acf[-1, i, j]^2))) else clim0
         if (is.null(ylim)) {
             ymin <- min(c(x$acf[, i, j], -clim))
             ymax <- max(c(x$acf[, i, j], clim))
             ylim <- c(ymin, ymax)
         }
         plot(x$lag[, i, j], x$acf[, i, j], type = type, xlab = xlab,
-            ylab = if(j==1) ylab else "", ylim = ylim, ...)
+             ylab = if(j==1) ylab else "", ylim = ylim, ...)
         abline(h = 0)
         if (with.ci && ci.type == "white")
             abline(h = c(clim, -clim), col = ci.col, lty = 2)
-        if (with.ci.ma && i == j) {
+        else if (with.ci.ma && i == j) {
             lines(x$lag[, i, j], clim, col = ci.col, lty = 2)
             lines(x$lag[, i, j], -clim, col = ci.col, lty = 2)
         }
-        if (!is.null(main))
-            title(main)
-        else if (i == j)
-            title(snames[i])
-        else title(paste(snames[i], "&", snames[j]))
+        title(if (!is.null(main)) main else
+              if (i == j) snames[i] else paste(sn.abbr[i], "&", sn.abbr[j]),
+              line = if(nser > 2) 1 else 2)
     }
 }
 
@@ -167,7 +173,7 @@ ccf <- function(x, y, lag.max = NULL,
     colnames(X) <- c(deparse(substitute(x)), deparse(substitute(y)))
     acf.out <- acf(X, lag.max = lag.max, plot = FALSE, type = type)
     lag <- c(rev(acf.out$lag[-1,2,1]), 0, acf.out$lag[,1,2])
-    y <- c(rev(acf.out$acf[-1,2,1]), 0, acf.out$acf[,1,2])
+    y   <- c(rev(acf.out$acf[-1,2,1]), 0, acf.out$acf[,1,2])
     acf.out$acf <- array(y, dim=c(length(y),1,1))
     acf.out$lag <- array(lag, dim=c(length(y),1,1))
     acf.out$snames <- paste(acf.out$snames, collapse = " & ")
