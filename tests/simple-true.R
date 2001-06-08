6377b8f6b9a2b72501562450148f582f8272b3e3
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-06-08 18:11:34 +0000

    fixed tests for 0-data.frames bug(fix)
    git-svn-id: https://svn.r-project.org/R/trunk@14569 00db46b3-68df-0310-9c12-caf00c1e9a41

--- tests/simple-true.R
+++ tests/simple-true.R
@@ -66,6 +66,24 @@ l1 <- c(TRUE,FALSE,TRUE)
 (! as.logical(as.data.frame(FALSE)[,1]))
 all(l1 == as.logical(as.data.frame(l1)[,1]))
 
+## empty data.frames :
+x <- data.frame(a=1:3)
+x30 <- x[, -1]; all(dim(x30) == c(3,0))
+x01 <- x[-(1:3), , drop = FALSE]
+all(dim(x01) == 0:1)
+all(dim(x) == dim(rbind(x, x01)))
+## bugs up to 1.2.3 :
+all(dim(x30) == dim(m30 <- as.matrix(x30)))
+all(dim(x01) == dim(m01 <- as.matrix(x01)))
+all(dim(x30) == dim(as.data.frame(m30)))
+all(dim(x01) == dim(as.data.frame(m01)))
+all(dim(x01) == dim(   data.frame(m01)))
+## *still* bug in data.frame() -- need to fix unlist(.) ??!
+if(FALSE){
+all(dim(x30) == dim(   data.frame(m30)))
+all(dim(x) == dim(cbind(x, x30)))
+}
+
 ### Subsetting
 
 ## bug PR#425
