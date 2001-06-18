2e5c8e5877cbdcf2ea0defe06125259882e10a09
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-06-18 07:43:51 +0000

    get consistent list of screen devices
    git-svn-id: https://svn.r-project.org/R/trunk@14713 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/device.R
+++ src/library/base/R/device.R
@@ -1,5 +1,5 @@
 dev.interactive <- function()
-    interactive() && .Device %in% c("X11", "GTK", "windows", "Macintosh")
+    interactive() && .Device %in% c("X11", "GTK", "gnome", "windows", "Macintosh")
 
 dev.list <- function()
 {
@@ -102,7 +102,7 @@ dev.print <- function(device = postscript, ...)
     current.device <- dev.cur()
     nm <- names(current.device)[1]
     if(nm == "null device") stop("no device to print from")
-    if(nm != "X11" && nm != "windows" && nm != "gtk"  && nm != "gnome")
+    if(!(nm %in% c("X11", "GTK", "gnome", "windows", "Macintosh")))
         stop("can only print from screen device")
     oc <- match.call()
     oc[[1]] <- as.name("dev.copy")
@@ -157,7 +157,7 @@ dev.copy2eps <- function(...)
     current.device <- dev.cur()
     nm <- names(current.device)[1]
     if(nm == "null device") stop("no device to print from")
-    if(nm != "X11" && nm != "windows" && nm != "gtk"  && nm != "gnome")
+    if(!(nm %in% c("X11", "GTK", "gnome", "windows", "Macintosh")))
         stop("can only print from screen device")
     oc <- match.call()
     oc[[1]] <- as.name("dev.copy")
