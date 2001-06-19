69026cc4cebab0acbeddf3d227294ede60adc473
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-06-19 19:59:21 +0000

    approx() and approxfun() "synced"
    git-svn-id: https://svn.r-project.org/R/trunk@14736 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/approx.R
+++ src/library/base/R/approx.R
@@ -1,54 +1,53 @@
-approx<-function (x, y = NULL, xout, method = "linear", n = 50, yleft,
-    yright, rule = 1, f = 0, ties=mean)
+### Note if you change: ./approxfun.R  is very close to this
+approx <-
+    function (x, y = NULL, xout, method = "linear", n = 50,
+	      yleft, yright, rule = 1, f = 0, ties = mean)
 {
     x <- xy.coords(x, y)
     y <- x$y
     x <- x$x
     if (!is.numeric(x) || !is.numeric(y))
-        stop("approx: x and y must be numeric")
+	stop("x and y must be numeric")
     nx <- length(x)
     if (nx != length(y))
-        stop("x and y must have equal lengths")
+	stop("x and y must have equal lengths")
     if (nx < 2)
-        stop("approx requires at least two values to interpolate")
+	stop("approx requires at least two values to interpolate")
     method <- pmatch(method, c("linear", "constant"))
     if (is.na(method))
-        stop("approx: invalid interpolation method")
-    ok <- !(is.na(x) | is.na(y))
-    x <- x[ok]
-    y <- y[ok]
-    ## unique xs
-    nx <- length(x)
-    if (!is.character(ties) || ties!="ordered"){
-        if (length(ux<-unique(x))<nx){
-            if (missing(ties))
-                warning("Collapsing to unique x values")
-            y<-tapply(y,x,ties)
-            x<-sort(ux)
-            nx<-length(x)
-        } else {
-            o <- order(x)
-            x <- x[o]
-            y <- y[o]
-        }
+	stop("invalid interpolation method")
+    if(any(na <- is.na(x) | is.na(y))) {
+	ok <- !na
+	x <- x[ok]
+	y <- y[ok]
+	nx <- length(x)
+    }
+    if (!is.character(ties) || ties != "ordered") {
+	if (length(ux <- unique(x)) < nx) {
+	    if (missing(ties))
+		warning("Collapsing to unique x values")
+	    y <- as.vector(tapply(y,x,ties))# as.v: drop dim & dimn.
+	    x <- sort(ux)
+	    nx <- length(x)
+	} else {
+	    o <- order(x)
+	    x <- x[o]
+	    y <- y[o]
+	}
     }
     if (nx < 2)
-        stop("approx requires at least two non-missing values to interpolate")
+	stop("need at least two unique non-missing values to interpolate")
     if (missing(yleft))
-        yleft <- if (rule == 1)
-            NA
-        else y[1]
+	yleft <- if (rule == 1) NA else y[1]
     if (missing(yright))
-        yright <- if (rule == 1)
-            NA
-        else y[length(y)]
+	yright <- if (rule == 1) NA else y[length(y)]
     if (missing(xout)) {
-        if (n <= 0)
-            stop("approx requires n >= 1")
-        xout <- seq(x[1], x[nx], length = n)
+	if (n <= 0)
+	    stop("approx requires n >= 1")
+	xout <- seq(x[1], x[nx], length = n)
     }
     y <- .C("R_approx", as.double(x), as.double(y), nx, xout = as.double(xout),
-        length(xout), as.integer(method), as.double(yleft), as.double(yright),
-        as.double(f), NAOK = TRUE, PACKAGE = "base")$xout
+	length(xout), as.integer(method), as.double(yleft), as.double(yright),
+	as.double(f), NAOK = TRUE, PACKAGE = "base")$xout
     list(x = xout, y = y)
 }
--- src/library/base/R/approxfun.R
+++ src/library/base/R/approxfun.R
@@ -1,12 +1,13 @@
+### Note if you change: ./approx.R  is very close to this
 approxfun <-
-    function(x, y = NULL, method = "linear", yleft, yright, rule = 1,
-	     f = 0, ties = mean)
+    function(x, y = NULL, method = "linear",
+	     yleft, yright, rule = 1, f = 0, ties = mean)
 {
     x <- xy.coords(x, y)
     y <- x$y
     x <- x$x
     if (!is.numeric(x) || !is.numeric(y))
-	stop("approx: x and y must be numeric")
+	stop("x and y must be numeric")
     n <- length(x)
     if (n != length(y))
 	stop("x and y must have equal lengths")
@@ -14,31 +15,34 @@ approxfun <-
 	stop("approx requires at least two values to interpolate")
     method <- pmatch(method, c("linear", "constant"))
     if (is.na(method))
-	stop("Invalid interpolation method")
-    ok <- !(is.na(x) | is.na(y))
-    x <- x[ok]
-    y <- y[ok]
-    if (!is.character(ties) || (ties != "ordered")) {
-	if (length(ux <- unique(x)) < length(x)) {
+	stop("invalid interpolation method")
+    if(any(o <- is.na(x) | is.na(y))) {
+	o <- !o
+	x <- x[o]
+	y <- y[o]
+	n <- length(x)
+    }
+    if (!is.character(ties) || ties != "ordered") {
+	if (length(ux <- unique(x)) < n) {
 	    if (missing(ties))
 		warning("Collapsing to unique x values")
-            y <- tapply(y,x,ties)
-	    ##BETTER y <- as.vector(tapply(y,x,ties))
+	    y <- as.vector(tapply(y,x,ties))# as.v: drop dim & dimn.
 	    x <- sort(ux)
-            rm(ux)
+	    n <- length(x)
+	    rm(ux)
 	} else {
 	    o <- order(x)
 	    x <- x[o]
 	    y <- y[o]
-	    rm(o)
 	}
     }
+    if (n < 2)
+	stop("need at least two unique non-missing values to interpolate")
     if (missing(yleft))
 	yleft <- if(rule == 1) NA else y[1]
     if (missing(yright))
 	yright <- if(rule == 1) NA else y[length(y)]
-    rm(ok, rule)
-    n<-as.integer(length(x)) ## may have been changed by tie removal
+    rm(o, rule)
     function(v) .C("R_approx", as.double(x), as.double(y),
 		   n, xout = as.double(v), length(v), as.integer(method),
 		   as.double(yleft), as.double(yright),
