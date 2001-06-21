337ac6f5d2f1cd25064b2186af6fe0c9ecedf7b9
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-06-21 17:39:58 +0000

    Remove cor.test() formula interface.
    git-svn-id: https://svn.r-project.org/R/trunk@14758 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/ctest/R/cor.test.R
+++ src/library/ctest/R/cor.test.R
@@ -128,32 +128,3 @@ function(x, y, alternative = c("two.sided", "less", "greater"),
 		   data.name = DNAME),
 	      class = "htest")
 }
-
-cor.test.formula <-
-function(formula, data, subset, na.action, ...)
-{
-    if(missing(formula)
-       || (length(formula) != 3)
-       || (length(attr(terms(formula[-2]), "term.labels")) != 1)
-       || (length(attr(terms(formula[-3]), "term.labels")) != 1))
-        stop("formula missing or incorrect")
-    if(missing(na.action))
-        na.action <- getOption("na.action")
-    m <- match.call(expand.dots = FALSE)
-    if(is.matrix(eval(m$data, parent.frame())))
-        m$data <- as.data.frame(data)
-    m[[1]] <- as.name("model.frame")
-    m$... <- NULL
-    mf <- eval(m, parent.frame())
-    DNAME <- paste(names(mf), collapse = " by ")
-    names(mf) <- NULL
-    response <- attr(attr(mf, "terms"), "response")
-    g <- as.factor(mf[[-response]])
-    if(nlevels(g) != 2)
-        stop("grouping factor must have exactly 2 levels")
-    DATA <- split(mf[[response]], g)
-    names(DATA) <- c("x", "y")
-    y <- do.call("cor.test", c(DATA, list(...)))
-    y$data.name <- DNAME
-    y
-}
