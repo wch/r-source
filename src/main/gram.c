9829ddea189345db052143b3b2747bcb5d454c5f
(no author) <(no author)@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-06-22 11:07:34 +0000

    This commit was manufactured by cvs2svn to create branch 'R-1-3-patches'.
    git-svn-id: https://svn.r-project.org/R/branches/R-1-3-patches@14771 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/gram.c
+++ src/main/gram.c
@@ -491,7 +491,7 @@ static const short yycheck[] = {     0,
     -1,    -1,    -1,    -1,    -1,    -1,    53
 };
 /* -*-C-*-  Note some compilers choke on comments on `#line' lines.  */
-#line 3 "/usr/lib/bison.simple"
+#line 3 "/usr/share/bison/bison.simple"
 /* This file comes from bison-1.28.  */
 
 /* Skeleton output parser for bison,
@@ -705,7 +705,7 @@ __yy_memcpy (char *to, char *from, unsigned int count)
 #endif
 #endif
 
-#line 217 "/usr/lib/bison.simple"
+#line 217 "/usr/share/bison/bison.simple"
 
 /* The user can define YYPARSE_PARAM as the name of an argument to be passed
    into yyparse.  The argument should have type void *.
@@ -1327,7 +1327,7 @@ case 73:
     break;}
 }
    /* the action file gets copied in in place of this dollarsign */
-#line 543 "/usr/lib/bison.simple"
+#line 543 "/usr/share/bison/bison.simple"
 
   yyvsp -= yylen;
   yyssp -= yylen;
