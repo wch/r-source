456af93211df3986bb0f90de51009008dc8270cc
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-06-25 14:38:52 +0000

    update search indices too from help.start
    git-svn-id: https://svn.r-project.org/R/branches/R-1-3-patches@14792 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/windows/help.start.R
+++ src/library/base/R/windows/help.start.R
@@ -9,6 +9,7 @@ help.start <- function(gui = "irrelevant", browser = "irrelevant")
         cat("updating HTML package listing\n")
         flush.console()
         make.packages.html(.lib.loc)
+        make.search.html(.Library)
         a <- gsub("/", "\\\\", a)
         cat("If nothing happens, you have to open `", a, "' yourself\n")
         .Internal(help.start());
