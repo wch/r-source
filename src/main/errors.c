4e733a8fd792cf3ca92860837a92d783a9723122
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-07-12 10:06:11 +0000

    Dropped checking of R_BrowseLevel for mask in R_ReturnOrRestart.
    git-svn-id: https://svn.r-project.org/R/branches/R-1-releases@14975 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/errors.c
+++ src/main/errors.c
@@ -705,10 +705,7 @@ void R_ReturnOrRestart(SEXP val, SEXP env, Rboolean restart)
     int mask;
     RCNTXT *c;
 
-    if (R_BrowseLevel > 0)
-	mask = CTXT_BROWSER | CTXT_FUNCTION;
-    else
-	mask = CTXT_FUNCTION;
+    mask = CTXT_BROWSER | CTXT_FUNCTION;
 
     for (c = R_GlobalContext; c; c = c->nextcontext) {
 	if (c->callflag & mask && c->cloenv == env)
