e81038bd2fdff768c1533765ee6d48aee59da068
murrell <murrell@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-07-17 01:27:08 +0000

    Fix for abline() to listen to par(xpd) settings, including via "..."
    git-svn-id: https://svn.r-project.org/R/branches/R-1-3-patches@15021 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/plot.c
+++ src/main/plot.c
@@ -2528,6 +2528,45 @@ SEXP do_title(SEXP call, SEXP op, SEXP args, SEXP env)
 /*  abline(a, b, h, v, col, lty, lwd, ...)
     draw lines in intercept/slope form.  */
 
+static void getxlimits(double *x, DevDesc *dd) {
+    /*
+     * xpd = 0 means clip to current plot region
+     * xpd = 1 means clip to current figure region
+     * xpd = 2 means clip to device region
+     */
+    switch (dd->gp.xpd) {
+    case 0:
+	x[0] = dd->gp.usr[0];
+	x[1] = dd->gp.usr[1];
+	break;
+    case 1:
+	x[0] = GConvertX(0, NFC, USER, dd);
+	x[1] = GConvertX(1, NFC, USER, dd);
+	break;
+    case 2:
+	x[0] = GConvertX(0, NDC, USER, dd);
+	x[1] = GConvertX(1, NDC, USER, dd);
+	break;
+    }
+}
+
+static void getylimits(double *y, DevDesc *dd) {
+    switch (dd->gp.xpd) {
+    case 0:
+	y[0] = dd->gp.usr[2];
+	y[1] = dd->gp.usr[3];
+	break;
+    case 1:
+	y[0] = GConvertY(0, NFC, USER, dd);
+	y[1] = GConvertY(1, NFC, USER, dd);
+	break;
+    case 2:
+	y[0] = GConvertY(0, NDC, USER, dd);
+	y[1] = GConvertY(1, NDC, USER, dd);
+	break;
+    }
+}
+
 SEXP do_abline(SEXP call, SEXP op, SEXP args, SEXP env)
 {
     SEXP a, b, h, v, untf, col, lty, lwd;
@@ -2572,6 +2611,8 @@ SEXP do_abline(SEXP call, SEXP op, SEXP args, SEXP env)
 
     GSavePars(dd);
 
+    ProcessInlinePars(args, dd); 
+
     nlines = 0;
 
     if (a != R_NilValue) {
@@ -2594,8 +2635,19 @@ SEXP do_abline(SEXP call, SEXP op, SEXP args, SEXP env)
 	else
 	    dd->gp.lty = dd->dp.lty;
 	GMode(1, dd);
-	x[0] = dd->gp.usr[0];
-	x[1] = dd->gp.usr[1];
+	/* FIXME?
+	 * Seems like the logic here is just draw from xmin to xmax
+	 * and you're guaranteed to draw at least from ymin to ymax
+	 * This MAY cause a problem at some stage when the line being
+	 * drawn is VERY steep -- and the problem is worse now that
+	 * abline will potentially draw to the extents of the device
+	 * (when xpd=NA).  NOTE that R's internal clipping protects the
+	 * device drivers from stupidly large numbers, BUT there is 
+	 * still a risk that we could produce a number which is too
+	 * big for the computer's brain.
+	 * Paul.
+	 */
+	getxlimits(x, dd);
 	if (R_FINITE(dd->gp.lwd)) {
 	    if (LOGICAL(untf)[0] == 1 && (dd->gp.xlog || dd->gp.ylog)) {
 		double xx[101], yy[101];
@@ -2639,8 +2691,7 @@ SEXP do_abline(SEXP call, SEXP op, SEXP args, SEXP env)
 	    dd->gp.lwd = REAL(lwd)[nlines % nlwd];
 	    aa = REAL(h)[i];
 	    if (R_FINITE(aa) && R_FINITE(dd->gp.lwd)) {
-		x[0] = dd->gp.usr[0];
-		x[1] = dd->gp.usr[1];
+		getxlimits(x, dd);
 		y[0] = aa;
 		y[1] = aa;
 		GLine(x[0], y[0], x[1], y[1], USER, dd);
@@ -2660,8 +2711,7 @@ SEXP do_abline(SEXP call, SEXP op, SEXP args, SEXP env)
 	    dd->gp.lwd = REAL(lwd)[nlines % nlwd];
 	    aa = REAL(v)[i];
 	    if (R_FINITE(aa) && R_FINITE(dd->gp.lwd)) {
-		y[0] = dd->gp.usr[2];
-		y[1] = dd->gp.usr[3];
+		getylimits(y, dd);
 		x[0] = aa;
 		x[1] = aa;
 		GLine(x[0], y[0], x[1], y[1], USER, dd);
