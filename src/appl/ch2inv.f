222da4d22226a08ffef9d058714f9d8a690b39c0
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-07-31 15:00:53 +0000

    fix comments (no "k"
    git-svn-id: https://svn.r-project.org/R/branches/R-1-3-patches@15162 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/appl/ch2inv.f
+++ src/appl/ch2inv.f
@@ -20,34 +20,31 @@ c  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 c
 c-----------------------------------------------------------------------
 c
-c	ch2inv() computes the inverse of a positive-definite symmetric
-c	matrix from its choleski factorization.	 This can be used (for
-c	example) to compute the dispersion matrix for the estimated
-c	parameters in a regression analysis.
+c ch2inv(x, ldx, n, v, info) computes the inverse of a positive-definite
+c		symmetric matrix from its choleski factorization.
+c This can be used, e.g., to compute the dispersion matrix for the estimated
+c parameters in a regression analysis.
 c
-c	On Entry
+c On Entry
 c
-c	  x	    double precision(ldx,k)
-c		    the choleski decomposition or the
-c		    qr decomposition as computed by dqrdc
-c		    or dqrdc2
+c	x	double precision(ldx,n)	 {ldx >= n}
+c		the choleski decomposition or the qr decomposition
+c		as computed by dqrdc or dqrdc2.	 Only the
+c		UPPER TRIANGULAR part,	x(i,j), 1 <= i <= j <= n, is accessed.
 c
-c	  ldx	    integer
-c		    the leading dimension of the array x
+c	ldx	integer, ldx >= n
+c		the leading dimension of the array x
 c
-c	  n	    integer
-c		    the number of rows of the matrix x
+c	n	integer
+c		the number of rows and columns of the matrix x
 c
-c	  k	    integer
-c		    the number of columns in the matrix k
+c On Return
 c
-c	On Return
+c	v	double precision(n,n)
+c		the value of inverse(x'x)
 c
-c	  v	    double precision(k,k)
-c		    the value of inverse(x'x)
-c
-c	This version dated Aug 24, 1996.
-c	Ross Ihaka, University of Auckland.
+c This version dated Aug 24, 1996.
+c Ross Ihaka, University of Auckland.
 c
       subroutine ch2inv(x, ldx, n, v, info)
 c     implicit none
