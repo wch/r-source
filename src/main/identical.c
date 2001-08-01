fc4eb69376dd2c5dd6bd109f7fa38ae3d1fe9f85
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-08-01 07:45:28 +0000

    Document identical in NEWS remove C++ comments
    git-svn-id: https://svn.r-project.org/R/trunk@15187 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/identical.c
+++ src/main/identical.c
@@ -1,3 +1,21 @@
+/*
+ *  R : A Computer Language for Statistical Data Analysis
+ *  Copyright (C) 2001  R Development Core Team
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 
 #include <R.h>
 #include <Rdefines.h>
@@ -157,13 +175,13 @@ static Rboolean compute_identical(SEXP x, SEXP y)
     return(PRIMOFFSET(x) == PRIMOFFSET(y) ? TRUE : FALSE);
   case ENVSXP:
     return(x == y ? TRUE : FALSE);
-    //  case PROMSXP: 
+    /*  case PROMSXP: */
     /* test for equality of the substituted expression -- or should
        we require both expression and environment to be identical? */
-    //#define PREXPR(x)	((x)->u.promsxp.expr)
-    //#define PRENV(x)	((x)->u.promsxp.env)
-    //    return(compute_identical(subsititute(PREXPR(x), PRENV(x)),
-    //			     subsititute(PREXPR(y), PRENV(y))));
+    /*#define PREXPR(x)	((x)->u.promsxp.expr)
+    #define PRENV(x)	((x)->u.promsxp.env)
+        return(compute_identical(subsititute(PREXPR(x), PRENV(x)),
+    			     subsititute(PREXPR(y), PRENV(y))));*/
   default:
     /* these are all supposed to be types that represent constant
        entities, so no further testing required ?? */
