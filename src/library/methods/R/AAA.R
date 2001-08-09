4b15bf224c04b8e2e61d852d8dfc440d0f95e0cb
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-08-09 11:12:51 +0000

    It's method{.Platform$file.sep} not method.so package method needs DLL built before loading the R code
    git-svn-id: https://svn.r-project.org/R/trunk@15372 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/methods/R/AAA.R
+++ src/library/methods/R/AAA.R
@@ -1,8 +1,9 @@
 pkg <- Sys.getenv("R_PACKAGE_DIR")
 if(nchar(pkg) > 0) {
-  cat("Looks like we're doing INSTALL:  will dynload the package .so file\n")
-  dyn.load(file.path(pkg, "libs", "methods.so"))
+  cat("Looks like we're doing INSTALL:  will dynload the package DLL\n")
+  dyn.load(file.path(pkg, "libs",
+                     paste("methods", .Platform$file.sep, sep="")))
   cat("dynamic load complete\n")
-} 
+}
 rm(pkg)
 
