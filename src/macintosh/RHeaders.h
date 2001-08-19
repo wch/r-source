efd8de30a4323bfb1b9cc79808eb88ff67984d48
iacus <iacus@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-08-19 19:10:47 +0000

    new prefix file
    git-svn-id: https://svn.r-project.org/R/trunk@15601 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/macintosh/RHeaders.h
+++ src/macintosh/RHeaders.h
@@ -1,8 +1,11 @@
-#ifdef __MRC__  /* Apple' C/C++ Compiler */
- #include <Carbon.h>
-#else /* CodeWarrior */
-#include <MacHeadersCarbon.h>
-#endif
+//#ifdef __MRC__  /* Apple' C/C++ Compiler */
+//  #include <Carbon.h>
+//#else /* CodeWarrior */
+//  #include <MacHeadersCarbon.h>
+//#endif /* __MRC__ */
+
+#define TARGET_API_MAC_CARBON 1
+
 
 #ifndef macintosh
 #define macintosh 1
@@ -12,8 +15,5 @@
 
 #ifndef __MRC__
 #define CW_F2C_MAC 1
-#endif
-
 #include "config.h"
-
 #endif /* __MRC__ */
