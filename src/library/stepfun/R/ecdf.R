8f72c804d68af9dcb1aa48d962a2885b744bbe1c
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-08-24 17:24:09 +0000

    doc/usage fixing for QA
    git-svn-id: https://svn.r-project.org/R/trunk@15692 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stepfun/R/ecdf.R
+++ src/library/stepfun/R/ecdf.R
@@ -14,23 +14,24 @@ ecdf <- function (x)
     rval
 }
 
-print.ecdf <- function (Fn, digits=options("digits")[[1]]-2, ...)
+print.ecdf <- function (x, digits= getOption("digits") - 2, ...)
 {
-    numform <- function(x)	paste(formatC(x, dig=digits), collapse=", ")
+    numform <- function(x) paste(formatC(x, dig=digits), collapse=", ")
     cat("Empirical CDF \nCall: ")
-    print(attr(Fn, "call"), ...)
-    n <- length(x <- eval(expression(x),env = environment(Fn)))
+    print(attr(x, "call"), ...)
+    n <- length(xx <- eval(expression(x),env = environment(x)))
     i1 <- 1:min(3,n)
     i2 <- if(n>=4) max(4,n-1):n else integer(0)
-    cat(" x[1:",n,"] = ", numform(x[i1]),
-	if(n>3) ", ", if(n>5) " ..., ", numform(x[i2]), "\n", sep = "")
-    invisible(Fn)
+    cat(" x[1:",n,"] = ", numform(xx[i1]),
+	if(n>3) ", ", if(n>5) " ..., ", numform(xx[i2]), "\n", sep = "")
+    invisible(x)
 }
 
-summary.ecdf <- function(Fn, ...){
+summary.ecdf <- function(object, ...)
+{
     cat("Empirical CDF:	 ",
-	eval(expression(n),env = environment(Fn)),"obs.\n")
-    summary(knots(Fn), ...)
+	eval(expression(n),env = environment(object)),"obs.\n")
+    summary(knots(object), ...)
 }
 
 
--- src/library/stepfun/R/stepfun.R
+++ src/library/stepfun/R/stepfun.R
@@ -22,71 +22,72 @@ stepfun <- function (x, y, f = 0, ties = "ordered")
 is.stepfun <- function(x) is.function(x) && inherits(x, "stepfun")
 
 ## Quite obvious  that I will want to have  knots.spline(..)  etc......
-knots <- function(Fn, ...) UseMethod("knots")
+knots         <- function(Fn, ...) UseMethod("knots")
 knots.stepfun <- function(Fn, ...) eval(expression(x), env=environment(Fn))
 
 
-print.stepfun <- function (Fn, digits = getOption("digits") - 2, ...)
+print.stepfun <- function (x, digits = getOption("digits") - 2, ...)
 {
     numform <- function(x) paste(formatC(x, dig = digits), collapse=", ")
     i1 <- function(n) 1:min(3, n)
     i2 <- function(n) if(n >= 4) max(4, n-1):n else integer(0)
     cat("Step function\nCall: ")
-    print(attr(Fn, "call"), ...)
-    env <- environment(Fn)
-    n <- length(x <- eval(expression(x), env = env))
-    cat(" x[1:", n, "] = ", numform(x[i1(n)]),
-	if(n > 3) ", ", if(n > 5) " ..., ", numform(x[i2(n)]), "\n", sep = "")
+    print(attr(x, "call"), ...)
+    env <- environment(x)
+    n <- length(xx <- eval(expression(x), env = env))
+    cat(" x[1:", n, "] = ", numform(xx[i1(n)]),
+	if(n > 3) ", ", if(n > 5) " ..., ", numform(xx[i2(n)]), "\n", sep = "")
     y <- eval(expression(c(yleft, y)), env = env)
     cat(n+1, " step heights = ", numform(y[i1(n+1)]),
 	if(n+1 > 3) ", ", if(n+1 > 5) " ..., ", numform(y[i2(n+1)]), "\n",
 	sep = "")
-    invisible(Fn)
+    invisible(x)
 }
 
-summary.stepfun <- function(Fn){
-    n <- eval(expression(n),env = environment(Fn))
+summary.stepfun <- function(object, ...)
+{
+    n <- eval(expression(n),env = environment(object))
     if(!is.integer(n) || n < 1) stop("not a valid step function")
     ## n <- n-1
     cat("Step function with continuity 'f'=",
-	format(eval(expression(f),env = environment(Fn))),
+	format(eval(expression(f),env = environment(object))),
 	", ", n, "knots at\n")
     summ <- if(n>6) summary else function(x) x
-    print(summ(knots(Fn)))
+    print(summ(knots(object)))
     cat(if(n>6)"\n" else"  ", "and	", n+1," step heights (y) at\n", sep="")
-    print(summ(eval(expression(c(yleft,y)),env = environment(Fn))))
+    print(summ(eval(expression(c(yleft,y)),env = environment(object))))
     invisible()
 }
 
 ## Purpose: plot method for  stepfun (step function) objects
 ## --------------------------------------------------------------------
-## Arguments: for numeric Fn, do empirical CDF;	  ==> `` ?plot.step ''
+## Arguments: for numeric `x', do empirical CDF;	  ==> `` ?plot.step ''
 ## --------------------------------------------------------------------
 ## Author: Martin Maechler <maechler@stat.math.ethz.ch>
 ##	      1990, U.Washington, Seattle; improved, Dec.1993
 ##	      Ported to R :  Sept.1997.
 plot.stepfun <-
-    function(Fn, xval, xlim, xlab = "x", ylab = "f(x)", main = NULL,
+    function(x, xval, xlim, xlab = "x", ylab = "f(x)", main = NULL,
              add = FALSE, verticals = TRUE, do.points = TRUE,
 	     pch = par("pch"), col.points=par("col"), cex.points=par("cex"),
 	     col.hor = par("col"), col.vert= par("col"),
              lty = par("lty"), lwd = par("lwd"),
              ...)
 {
-    if(!is.stepfun(Fn)) { #- make it work when called explicitly with data
-	if(is.numeric(Fn)) {
-	    sarg <- substitute(Fn)
-	    Fn <- ecdf(Fn)
-	    attr(Fn,"call") <- call("ecdf", sarg)
-	} else stop("plot.stepfun called with wrong argument `Fn'")
+    if(!is.stepfun(x)) { #- make it work when called explicitly with data
+	if(is.numeric(x)) {
+	    sarg <- substitute(x)
+	    x <- ecdf(x)
+	    attr(x,"call") <- call("ecdf", sarg)
+	} else stop("plot.stepfun called with wrong argument `x'")
     }
     if(missing(main))
 	main <- {
-	    cl <- attr(Fn,"call")
+	    cl <- attr(x,"call")
 	    deparse(if(!is.null(cl))cl else sys.call())
 	}
 
-    knF <- knots(Fn)
+    knF <- knots(x)
     xval <- if (missing(xval)) knF else sort(xval)
     if (missing(xlim)) {
 	dr <- diff(rx <- range(xval))
@@ -98,11 +99,11 @@ plot.stepfun <-
 
     ## Careful for heights of horizontals -- these depend on f
     ti <- c(xlim[1]-dr, knF, xlim[2]+dr)
-    ti.l <-	 ti[-length(ti)]
-    ti.r <-	 ti[-1]
-    y <- Fn(.5*(ti.l+ti.r))
+    ti.l <- ti[-length(ti)]
+    ti.r <- ti[-1]
+    y <- x(0.5*(ti.l + ti.r))
     n <- length(y)
-    Fn.kn <- Fn(knF)
+    Fn.kn <- x(knF)
 
     ##------------------------ Plotting ----------------------------
 
