02c8d2668d8850ead096237660c8667a92cc8107
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-08-25 16:39:57 +0000

    use Windows background colour for select.list and GUI preferences editor
    git-svn-id: https://svn.r-project.org/R/branches/R-1-3-patches@15713 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/extra.c
+++ src/gnuwin32/extra.c
@@ -845,7 +845,7 @@ SEXP do_selectlist(SEXP call, SEXP op, SEXP args, SEXP rho)
     wselect = newwindow(multiple ? "Select" : "Select one", 
 			rect(0, 0, xmax, ymax),
 			Titlebar | Centered | Modal);
-    setbackground(wselect, LightGrey);
+    setbackground(wselect, myGetSysColor(COLOR_MENU));
     if(multiple)
 	f_list = newmultilist(clist, rect(10, 10, 35+8*mw, ylist), NULL);
     else
