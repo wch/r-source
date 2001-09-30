8f3a963e4a8a032e9b3738927166132af08be97e
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-09-30 10:13:36 +0000

    New function read.00Index().
    git-svn-id: https://svn.r-project.org/R/trunk@16162 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/indexes.R
+++ src/library/base/R/indexes.R
@@ -1,2 +1,26 @@
-index.search <- function(topic, path, file="AnIndex", type="help")
+index.search <- function(topic, path, file = "AnIndex", type = "help")
     .Internal(index.search(topic, path, file, .Platform$file.sep, type))
+
+read.00Index <-
+function(file)
+{
+    if(is.character(file)) {
+        if(file == "") file <- stdin()
+        else {
+            file <- file(file, "r")
+            on.exit(close(file))
+        }
+    }
+    if(!inherits(file, "connection"))
+        stop("argument `file' must be a character string or connection")
+    
+    x <- paste(readLines(file), collapse = "\n")
+    x <- strsplit(gsub("\n[ \t]+", " ", x), "\n")
+    x <- strsplit(unlist(x), "[ \t]")
+    y <- cbind(unlist(lapply(x, "[[", 1)),
+               unlist(lapply(x, function(t)
+                             paste(t[-c(1, which(nchar(t) == 0))],
+                                   collapse = " "))))
+    colnames(y) <- c("Item", "Description")
+    y
+}
