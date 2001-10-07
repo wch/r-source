19a939d3283baeea6a08cf3462cfe57c0ff13ef7
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-10-07 20:49:27 +0000

    merge versions of Rd2contents*
    git-svn-id: https://svn.r-project.org/R/trunk@16260 00db46b3-68df-0310-9c12-caf00c1e9a41

--- share/perl/Rd2contents.pl
+++ share/perl/Rd2contents.pl
@@ -1,11 +1,64 @@
-use File::Basename;
+# Copyright (C) 1997-2001 R Development Core Team
+#
+# This document is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# A copy of the GNU General Public License is available via WWW at
+# http://www.gnu.org/copyleft/gpl.html.  You can also obtain it by
+# writing to the Free Software Foundation, Inc., 59 Temple Place,
+# Suite 330, Boston, MA  02111-1307  USA.
 
-$ARGV[0] =~ /([^\/]*)$/;
+use File::Basename;
+use Getopt::Long;
+use R::Utils;
+
+@knownoptions = ("o|output:s", "os|OS:s");
+
+GetOptions (@knownoptions) || &usage();
+
+sub usage {
+  print STDERR <<END;
+Usage: perl Rd2contents.pl [options] FILE
+
+Prepare the CONTENTS file for a directory
+
+Options:
+  -o, --output=OUT	use \`OUT\' as the output file
+      --os=NAME		use OS subdir \`NAME\' (unix, mac or windows)
+      --OS=NAME		the same as \`--os\'.
+END
+  exit 0;
+}
+
+my $OSdir = "unix";
+$OSdir = $opt_os if $opt_os;
+
+my $out = 0;
+if(defined $opt_o && length($opt_o)) { $out = $opt_o; }
+
+if($OSdir eq "mac") {$ARGV[0] =~ /([^\:]*)$/;} else {$ARGV[0] =~ /([^\/]*)$/;}
+
 my $pkg = $1;
 
-while(<$ARGV[0]/man/*.Rd>){ &do_one; }
-if(-d  "$ARGV[0]/man/unix") {
-    while(<$ARGV[0]/man/unix/*.Rd>){ &do_one; }
+my $outfile;
+if($out) {
+    open(OUTFILE, "> $out") or die "Couldn't open CONTENTS file";
+    $outfile = OUTFILE;
+} else {
+    $outfile = STDOUT;
+}
+
+while(glob file_path($ARGV[0], "man", "*.Rd")){ &do_one; }
+
+if(-d  &file_path($ARGV[0], "man", $OSdir)) {
+    while(glob file_path($ARGV[0], "man",  $OSdir, "*.Rd")){ &do_one; }
 }
 
 sub do_one {
@@ -42,10 +95,10 @@ sub do_one {
     }
 
     $, = " ";
-    print "Entry: $rdname\n";
-    print "Aliases: @aliases\n";
-    print "Keywords: @keywords\n";
-    print "Description: $rdtitle\n";
-    print "URL: ../../../library/$pkg/html/$file.html\n\n";
+    print $outfile "Entry: $rdname\n";
+    print $outfile "Aliases: @aliases\n";
+    print $outfile "Keywords: @keywords\n";
+    print $outfile "Description: $rdtitle\n";
+    print $outfile "URL: ../../../library/$pkg/html/$file.html\n\n";
 }
 
