a818281fdc9b07731f52408314cf669abc5900ce
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-10-08 07:00:21 +0000

    Cosmetics, mostly.
    git-svn-id: https://svn.r-project.org/R/trunk@16269 00db46b3-68df-0310-9c12-caf00c1e9a41

--- share/perl/Rd2contents.pl
+++ share/perl/Rd2contents.pl
@@ -19,15 +19,15 @@ use File::Basename;
 use Getopt::Long;
 use R::Utils;
 
-@knownoptions = ("o|output:s", "os|OS:s");
+my @knownoptions = ("o|output:s", "os|OS:s");
 
 GetOptions (@knownoptions) || &usage();
 
 sub usage {
   print STDERR <<END;
-Usage: perl Rd2contents.pl [options] FILE
+Usage: [R CMD] perl Rd2contents.pl [options] FILE
 
-Prepare the CONTENTS file for a directory
+Prepare the CONTENTS file for a directory.
 
 Options:
   -o, --output=OUT	use \`OUT\' as the output file
@@ -37,70 +37,69 @@ END
   exit 0;
 }
 
-my $OSdir = "unix";
+## <FIXME>
+## Currently, R_OSTYPE is always set on Unix/Windows.
+my $OSdir = R_getenv("R_OSTYPE", "mac");
+## </FIXME>
 $OSdir = $opt_os if $opt_os;
 
 my $out = 0;
-if(defined $opt_o && length($opt_o)) { $out = $opt_o; }
+$out = $opt_o if(defined $opt_o && length($opt_o));
 
-if($OSdir eq "mac") {$ARGV[0] =~ /([^\:]*)$/;} else {$ARGV[0] =~ /([^\/]*)$/;}
+if($OSdir eq "mac") {
+    $ARGV[0] =~ /([^\:]*)$/;
+} else {
+    $ARGV[0] =~ /([^\/]*)$/;
+}
 
 my $pkg = $1;
 
 my $outfile;
 if($out) {
-    open(OUTFILE, "> $out") or die "Couldn't open CONTENTS file";
+    open(OUTFILE, "> $out") or die "Cannot write to \`$out'";
     $outfile = OUTFILE;
 } else {
     $outfile = STDOUT;
 }
 
-while(glob file_path($ARGV[0], "man", "*.Rd")){ &do_one; }
-while(glob file_path($ARGV[0], "man", "*.rd")){ &do_one; }
-
+while(glob file_path($ARGV[0], "man", "*.[Rr]d")) { &do_one; }
 if(-d  &file_path($ARGV[0], "man", $OSdir)) {
-    while(glob file_path($ARGV[0], "man",  $OSdir, "*.Rd")){ &do_one; }
-    while(glob file_path($ARGV[0], "man",  $OSdir, "*.rd")){ &do_one; }
+    while(glob file_path($ARGV[0], "man",  $OSdir, "*.[Rr]d")){ &do_one; }
 }
 
 sub do_one {
     my $file = basename($_, (".Rd", ".rd"));
+    my ($text, $rdname, $rdtitle, @aliases, @keywords);
 
-    open(rdfile, "<$_");
-    undef $text;
-
-    while(<rdfile>){
+    open(RDFILE, "< $_");
+    while(<RDFILE>){
 	$text .= $_;
     }
-    close rdfile;
+    close RDFILE;
 
     $text =~ /\\name\{\s*([^\}]+)\s*\}/s;
-    my $rdname = $1;
+    $rdname = $1;
     $rdname =~ s/\n/ /sg;
 
     $text =~ /\\title\{\s*([^\}]+)\s*\}/s;
-    my $rdtitle = $1;
+    $rdtitle = $1;
     $rdtitle =~ s/\n/ /sg;
 
-    undef @aliases;
-    while($text =~ s/\\alias\{\s*(.*)\s*\}//){
-	$alias = $1;
+    while($text =~ s/\\alias\{\s*(.*)\s*\}//) {
+	my $alias = $1;
 	$alias =~ s/\\%/%/g;
 	push @aliases, $alias;
     }
 
-    undef @keywords;
-    while($text =~ s/\\keyword\{\s*(.*)\s*\}//){
-	$keyword = $1;
+    while($text =~ s/\\keyword\{\s*(.*)\s*\}//) {
+	my $keyword = $1;
 	$keyword =~ s/\\%/%/g;
 	push @keywords, $keyword;
     }
 
-    $, = " ";
     print $outfile "Entry: $rdname\n";
-    print $outfile "Aliases: @aliases\n";
-    print $outfile "Keywords: @keywords\n";
+    print $outfile "Aliases: " . join(" ", @aliases) . "\n";
+    print $outfile "Keywords: " . join(" ", @keywords) . "\n";
     print $outfile "Description: $rdtitle\n";
     print $outfile "URL: ../../../library/$pkg/html/$file.html\n\n";
 }
-
