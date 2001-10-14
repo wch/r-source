eac1daacba213221847261cd337859d0b1ce1e94
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-10-14 11:21:37 +0000

    Merge code for extracting name, title, aliases and keywords from Rd files.
    git-svn-id: https://svn.r-project.org/R/trunk@16357 00db46b3-68df-0310-9c12-caf00c1e9a41

--- share/perl/R/Rd.pm
+++ share/perl/R/Rd.pm
@@ -25,24 +25,35 @@ use R::Rdtools;
 
 sub info {
 
-    my ($class, $file) = @_;
+    my ($class, $file, $OS) = @_;
     $class = ref($class) || $class;
 
     my $self = {};
-    $_ = Rdpp($file);
-    if(/\\name\{([^\}]*)\}/){
-	$self->{"name"} = $1;
+
+    $OS = "unix" unless $OS;
+    my $text = &Rdpp($file, $OS);
+
+    $text =~ /\\name\{\s*([^\}]+)\s*\}/s;
+    $self->{"name"} = $1;
+    $self->{"name"} =~ s/\n/ /sg;
+
+    $text =~ /\\title\{\s*([^\}]+)\s*\}/s;
+    $self->{"title"} = $1;
+    $self->{"title"} =~ s/\n/ /sg;
+    $self->{"title"} =~ s/\s+/ /sg;
+
+    while($text =~ s/\\alias\{\s*(.*)\s*\}//) {
+        my $alias = $1;
+        $alias =~ s/\\%/%/g;
+        push @{$self->{"aliases"}}, $alias;
     }
-    if(/\\title\{\s*([^\}]*)\s*\}/s){
-	$self->{"title"} = $1;
-	$self->{"title"} =~ s/\s+/ /sg;
+
+    while($text =~ s/\\keyword\{\s*(.*)\s*\}//) {
+        my $keyword = $1;
+        $keyword =~ s/\\%/%/g;
+        push @{$self->{"keywords"}}, $keyword;
     }
-    if(/\\alias\{([^\}]*)\}/){
-	push @{$self->{"aliases"}}, $1;
-    }
-    if(/\\keyword\{([^\}]*)\}/){
-	push @{$self->{"keywords"}}, $1;
-    }
+
     bless $self, $class;
 }
 
