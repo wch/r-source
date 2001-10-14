70cc9f3592b7e4e150714ab44ae2cb8ee4ea4cc7
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-10-14 11:21:48 +0000

    New function list_files_with_exts.
    git-svn-id: https://svn.r-project.org/R/trunk@16358 00db46b3-68df-0310-9c12-caf00c1e9a41

--- share/perl/R/Utils.pm
+++ share/perl/R/Utils.pm
@@ -6,7 +6,7 @@ use FileHandle;
 use Exporter;
 
 @ISA = qw(Exporter);
-@EXPORT = qw(R_getenv R_version file_path);
+@EXPORT = qw(R_getenv R_version file_path list_files_with_exts);
 
 
 #**********************************************************
@@ -62,6 +62,21 @@ sub file_path {
     my $filesep = "/";
     $filesep = ":" if($main::OSdir eq "mac");
     join($filesep, @args);
+}
+
+sub list_files_with_exts {
+    my ($dir, $exts) = @_;
+    $exts = ".*" unless $exts;
+    opendir(DIR, $dir) or die "cannot opendir $dir: $!";
+    my @files = grep { /$exts$/ && -f "$dir/$_" } readdir(DIR);
+    closedir(DIR);
+    ## We typically want the paths to the files, see also the R variant
+    ## listFilesWithExts() used in some of the QA tools.
+    my @paths;
+    foreach my $file (@files) {
+	push @paths, &file_path($dir, $file);
+    }
+    @paths;
 }
 
 
