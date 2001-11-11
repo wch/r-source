c7e7f6a7fefbdc9ddd95a199cf97bf10fe04a353
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-11-11 20:09:46 +0000

    Set browser option to the one used by help.start().
    git-svn-id: https://svn.r-project.org/R/trunk@16685 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/unix/help.start.R
+++ src/library/base/R/unix/help.start.R
@@ -2,15 +2,26 @@ help.start <- function (gui = "irrelevant", browser = getOption("browser"),
 			remote = NULL) {
     if(is.null(browser))
 	stop("Invalid browser name, check options(\"browser\").")
+    if(browser != getOption("browser")) {
+        msg <- paste("Changing the default browser",
+                     "(as specified by the `browser' option)",
+                     "to the given browser so that it gets used",
+                     "for all future help requests.")
+        writeLines(strwrap(msg, exdent = 4))
+        options(browser = browser)
+    }
     cat("Making links in ~/.R ...\n")
     .Script("sh", "help-links.sh", paste(unique(.lib.loc), collapse = " "))
     url <- paste(if (is.null(remote)) "$HOME/.R" else remote,
 		 "/doc/html/index.html", sep = "")
-    cat("If", browser, " is already running,\tit is *not* restarted,\n",
-	"and you must switch to its window.\nOtherwise, be patient..\n")
+    writeLines(strwrap(paste("If", browser, "is already running,",
+                             "it is *not* restarted, and you must",
+                             "switch to its window."),
+                       exdent = 4))
+    writeLines("Otherwise, be patient ...")
     system(paste(browser, " -remote \"openURL(", url, ")\" 2>/dev/null || ",
 		 browser, " ", url, " &", sep = ""))
     assign("help.start.has.been.run", TRUE,
-           pos=match("package:base", search()))
-    options(htmlhelp=TRUE)
+           pos = match("package:base", search()))
+    options(htmlhelp = TRUE)
 }
