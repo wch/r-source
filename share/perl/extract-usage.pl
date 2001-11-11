51b1b0ccc34d85cacae0cd2e667ad2c5dc4bdb5a
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-11-11 01:12:01 +0000

    Allow dealing with multiple Rd objects in one file.
    git-svn-id: https://svn.r-project.org/R/trunk@16665 00db46b3-68df-0310-9c12-caf00c1e9a41

--- share/perl/extract-usage.pl
+++ share/perl/extract-usage.pl
@@ -23,7 +23,7 @@ use Getopt::Long;
 use R::Rdtools;
 use R::Utils;
 
-my $revision = ' $Revision: 1.7 $ ';
+my $revision = ' $Revision: 1.8 $ ';
 my $version;
 my $name;
 
@@ -33,15 +33,15 @@ $version = $1;
 
 sub usage {
     print STDERR <<END;
-Usage: [R CMD] perl /path/to/extract-usage.pl [options] FILE
+Usage: [R CMD] perl /path/to/extract-usage.pl [options] FILE OUT
 
 Extract usage information for use by codoc from the Rd source files
-listed in FILE.
+listed in FILE, writing results to OUT.
 
 Options:
   -h, --help            print short help message and exit
   -v, --version         print version info and exit
-      --mode=MODE       use operation mode MODE (codoc or args)
+      --mode=MODE       use operation mode MODE (codoc, args or style)
       --os=NAME         use OS subdir \`NAME\' (unix, mac or windows)
       --OS=NAME         the same as \`--os\'.
 
@@ -71,24 +71,32 @@ open(OUTFILE, "> $ARGV[1]") or die "Can't open output file";
 while (<INFILE>) {
     chomp;
     open RDFILE, "< $_";
-    print OUTFILE "# usages in file $_\n";
+    ## <NOTE>
+    ## This is really dangerous ...
+    my @chunks = split(/\\name/, &Rdpp($_, $OSdir));
+    ## </NOTE>
+    foreach my $text (@chunks) {
+	next if($text !~ /^\s*{\s*([^}]*[^}\s])\s*}.*/);
+	print OUTFILE "# usages in documentation object $1\n";
+	print OUTFILE "# arglist: ", join(" ", get_arglist($text)), "\n"
+	  unless ($opt_mode eq "codoc");
 
-    my $text = &Rdpp($_, $OSdir);
+	## Put \name back in front as get_usages() needs it.  Could also
+	## pass on the name we just determined ...
+	$text = "\\name" . $text;
 
-    print OUTFILE "# arglist: ", join(" ", get_arglist($text)), "\n"
-      unless ($opt_mode eq "codoc");
-
-    {
-	local $/;		# unset for get_usages()
-	%usages = get_usages($text, $opt_mode);
-    }
-    
-    foreach my $key (keys(%usages)){
-	$usages{$key} =~ s/ *\\.?dots/ .../g;
-	if ($key !~ /^</) {
-	    print OUTFILE "$key <- function$usages{$key} NULL\n";
+	{
+	    local $/;		# unset for get_usages()
+	    %usages = get_usages($text, $opt_mode);
 	}
+
+	foreach my $key (keys(%usages)){
+	    $usages{$key} =~ s/ *\\.?dots/ .../g;
+	    if ($key !~ /^</) {
+		print OUTFILE "$key <- function$usages{$key} NULL\n";
+	    }
+	}
+
+	print OUTFILE "\n";
     }
-    
-    print OUTFILE "\n";
 }
