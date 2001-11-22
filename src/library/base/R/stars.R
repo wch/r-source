e67828295fd72b672cdd59a6d36a3550b079ac57
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-11-22 14:31:23 +0000

    stars()
    git-svn-id: https://svn.r-project.org/R/trunk@16833 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/stars.R
+++ src/library/base/R/stars.R
@@ -1,15 +1,21 @@
-### T. Dye <tdye@lava.net>, July 1999
 ### This code started life as spatial star plots by David A. Andrews.
 ### See http://www.udallas.edu:8080/~andrews/software/software.html
+### T. Dye <tdye@lava.net>, July 1999;  many improvements by MM
 
 stars <-
 function(x, full = TRUE, scale = TRUE, radius = TRUE,
-	 labels = dimnames(x)[[1]],
-         locations = NULL, xlim = NULL, ylim = NULL, len = 1,
-         colors = NULL,
+	 labels = dimnames(x)[[1]], locations = NULL,
+         nrow = NULL, ncol = NULL, len = 1,
          key.loc = NULL, key.labels = NULL, key.xpd = TRUE,
-         draw.segments = FALSE, axes = FALSE,
-         cex = 0.8, lwd = 0.25, ...)
+         xlim = NULL, ylim = NULL, flip.labels = NULL,
+         draw.segments = FALSE, col.segments = 1:n.seg,
+         col.stars = NA,
+         axes = FALSE, frame.plot = axes,
+         main = NULL, sub = NULL, xlab = "", ylab = "",
+         cex = 0.8, lwd = 0.25, lty = par("lty"), xpd = FALSE,
+         mar = pmin(par("mar"),
+                    1.1+ c(2*axes+ (xlab != ""), 2*axes+ (ylab != ""), 1,0)),
+         ...)
 {
     if (is.data.frame(x))
 	x <- data.matrix(x)
@@ -20,20 +26,25 @@ function(x, full = TRUE, scale = TRUE, radius = TRUE,
 
     n.loc <- nrow(x)
     n.seg <- ncol(x)
-    deg <- pi / 180
 
-    seg.colors <- if(!is.null(colors)) colors else 1:n.seg
-
-    if (is.null(locations)) {		# make loc matrix
-	md <- ceiling(sqrt(n.loc)) # =>  md^2 >= n.loc
-        loc0 <- 2.1* 1:md
-        locations <- expand.grid(loc0, rev(loc0))[1:n.loc, ]
+    if (is.null(locations)) { ## Default (x,y) locations matrix
+	if(is.null(nrow))
+            nrow <- ceiling(if(!is.numeric(ncol)) sqrt(n.loc) else n.loc/ncol)
+	if(is.null(ncol))
+            ncol <- ceiling(n.loc/nrow)
+        if(nrow * ncol < n.loc)
+            stop("nrow * ncol <  number of observations")
+        ff <- if(!is.null(labels)) 2.3 else 2.1
+        locations <- expand.grid(ff * 1:ncol, ff * nrow:1)[1:n.loc, ]
+        if(!is.null(labels) && (missing(flip.labels) ||
+                                !is.logical(flip.labels)))
+            flip.labels <- ncol * mean(nchar(labels)) > 30
     }
     else {
         if (is.numeric(locations) && length(locations) == 2) {
             ## all stars around the same origin
             locations <- cbind(rep(locations[1],n.loc),
-                         rep(locations[2],n.loc))
+                               rep(locations[2],n.loc))
             if(!missing(labels) && n.loc > 1)
                 warning("labels don't make sense for a single location")
             else labels <- NULL
@@ -46,8 +57,11 @@ function(x, full = TRUE, scale = TRUE, radius = TRUE,
             if (n.loc != nrow(locations))
                 stop("number of rows of locations and x must be equal.")
         }
+        if(missing(flip.labels) || !is.logical(flip.labels))
+            flip.labels <- FALSE # have no grid
     }
-
+    xloc <- locations[,1]
+    yloc <- locations[,2]
     ## Angles start at zero and pace around the circle counter
     ## clock-wise in equal increments.
     angles <-
@@ -63,112 +77,101 @@ function(x, full = TRUE, scale = TRUE, radius = TRUE,
 
     ## Missing values are treated as 0
     x[is.na(x)] <- 0
-
     if (scale) {
-	x <- sweep(x,2,apply(x,2,max),FUN="/")
+	x <- sweep(x,2,apply(x,2,max), FUN="/")
 	## Columns of 0s will put NAs in x, next line gets rid of them
 	x[is.na(x)] <- 0
     }
+    mx <- max(x <- x * len)
 
-    x <- x * len
+    if(is.null(xlim)) xlim <- range(xloc) + c(-mx, mx)
+    if(is.null(ylim)) ylim <- range(yloc) + c(-mx, mx)
 
-    if(is.null(xlim)) xlim <- range(locations[,1] + max(x), locations[,1] - max(x))
-    if(is.null(ylim)) ylim <- range(locations[,2] + max(x), locations[,2] - max(x))
+    deg <- pi / 180
 
-    ## The asp argument keeps everything square
+    ## The asp argument keeps everything (the symbols!) square
+    op <- par(mar = mar, xpd = xpd) ; on.exit(par(op))
     plot(0, type="n", ..., xlim=xlim, ylim=ylim,
-	 xlab="", ylab="", asp = 1, axes = axes)
+	 main = main, sub = sub, xlab = xlab, ylab=ylab, asp = 1, axes = axes)
+
+    if(!is.null(labels)) {
+        ## vertical text offset from center
+        y.off <- (mx * (if(full) 1 else 0.1)) +
+            (if(flip.labels) cex*par("cxy")[2] *
+             ((1:n.loc)%%2 - if(full) .4 else 0) else 0)
+        ##DBG cat("mx=",format(mx),"y.off:"); str(y.off)
+    }
+
+    s.x <- xloc + x * rep(cos(angles), rep(n.loc,n.seg))
+    s.y <- yloc + x * rep(sin(angles), rep(n.loc,n.seg))
 
     if ( draw.segments ) {
+        aangl <- c(angles, if(full)2*pi else pi)
 	## for each location, draw a segment diagram
-	for ( i in 1:n.loc ) {
-	    poly.x <- NA
-	    poly.y <- NA
-	    start.x <- x[i,] * cos( angles ) + locations[i,1]
-	    start.y <- x[i,] * sin( angles ) + locations[i,2]
+	for (i in 1:n.loc) {
+	    px <- py <- numeric()
 
-### FIXME : we can do without the following inner loop !
-
+### FIXME : do without the following inner loop! (same FIXME for "key")
 	    for (j in 1:n.seg) {
-		poly.x <- c(poly.x,locations[i,1],start.x[j])
-		poly.y <- c(poly.y,locations[i,2],start.y[j])
-
-		next.angle <-
-		    if(j < n.seg)
-			angles[j+1]
-		    else (if(full) 360 else 180) * deg
-
-		k <- seq(from = angles[j], to = next.angle, by = deg)
-		poly.x <- c(poly.x, x[i,j] * cos(k) + locations[i,1], NA)
-		poly.y <- c(poly.y, x[i,j] * sin(k) + locations[i,2], NA)
+		k <- seq(from = aangl[j], to = aangl[j+1], by = 1*deg)
+		px <- c(px, xloc[i], s.x[i,j], x[i,j]*cos(k) + xloc[i], NA)
+		py <- c(py, yloc[i], s.y[i,j], x[i,j]*sin(k) + yloc[i], NA)
 	    }
-	    polygon(poly.x, poly.y, col = seg.colors, lwd=lwd)
-	    if (!is.null(labels))
-		text(locations[i,1], locations[i,2] - if(full)max(x) else 0.1 * max(x),
-		     labels[i], cex=cex, adj=c(0.5,1))
+	    polygon(px, py, col = col.segments, lwd=lwd, lty=lty)
 	}
     } # Segment diagrams
-
     else { # Draw stars instead
-	for ( i in 1:n.loc ) {
-	    temp.x <- x[i,] * cos( angles ) + locations[i,1]
-	    temp.y <- x[i,] * sin( angles ) + locations[i,2]
+
+	for (i in 1:n.loc) {
+	    polygon(s.x[i,], s.y[i,], lwd=lwd, lty=lty, col = col.stars[i])
 	    if (radius)
-		segments(rep(locations[i,1],n.seg),
-			 rep(locations[i,2],n.seg),
-			 temp.x, temp.y, lwd=lwd)
-	    lines(c(temp.x, temp.x[1]),
-		  c(temp.y, temp.y[1]), lwd=lwd)
-	    if (!is.null(labels))
-		text(locations[i,1], locations[i,2] - if(full)max(x) else 0.1 * max(x),
-		     labels[i], cex=cex, adj=c(0.5,1))
+		segments(rep(xloc[i],n.seg),
+			 rep(yloc[i],n.seg),
+			 s.x[i,], s.y[i,], lwd=lwd, lty=lty)
 	}
     }
 
-    if ( !is.null(key.loc) ) { ## Draw unit key
+    if (!is.null(labels))
+        text(xloc, yloc - y.off, labels, cex=cex, adj=c(0.5, 1))
 
+    if ( !is.null(key.loc) ) { ## Draw unit key
         ## allow drawing outside plot region (inside figure region):
-        op <- par(xpd = key.xpd); on.exit(par(op))
+        par(xpd = key.xpd) # had `xpd' already above
         key.x.coord <- cos(angles) * len + key.loc[1]
         key.y.coord <- sin(angles) * len + key.loc[2]
 	if ( draw.segments ) {
-	    key.x <- NA
-	    key.y <- NA
-	    for (j in 1:n.seg){
-		key.x <- c(key.x,key.loc[1],key.x.coord[j])
-		key.y <- c(key.y,key.loc[2],key.y.coord[j])
+	    px <- py <- numeric()
+	    for (j in 1:n.seg) {
+		px <- c(px, key.loc[1],key.x.coord[j])
+		py <- c(py, key.loc[2],key.y.coord[j])
 		k <- angles[j] + deg
-		next.angle <-
-		    if (j < n.seg) angles[j+1]
-		    else (if(full) 360 else 180) * deg
-
+		next.angle <- aangl[j+1]
 		while (k < next.angle) {
-		    key.x <- c(key.x, len * cos(k) + key.loc[1])
-		    key.y <- c(key.y, len * sin(k) + key.loc[2])
+		    px <- c(px, len * cos(k) + key.loc[1])
+		    py <- c(py, len * sin(k) + key.loc[2])
 		    k <- k + deg
 		}
-		key.x <- c(key.x, len * cos(next.angle) + key.loc[1], NA)
-		key.y <- c(key.y, len * sin(next.angle) + key.loc[2], NA)
+		px <- c(px, len * cos(next.angle) + key.loc[1], NA)
+		py <- c(py, len * sin(next.angle) + key.loc[2], NA)
 	    }
-	    polygon(key.x, key.y, col = seg.colors, lwd=lwd)
+	    polygon(px, py, col = col.segments, lwd=lwd, lty=lty)
 	}
 	else { # draw unit star
+	    polygon(key.x.coord, key.y.coord, lwd=lwd, lty=lty)
 	    if (radius)
 		segments(rep(key.loc[1],n.seg), rep(key.loc[2],n.seg),
-			 key.x.coord, key.y.coord, lwd=lwd)
-	    lines(c(key.x.coord, key.x.coord[1]),
-		  c(key.y.coord, key.y.coord[1]), lwd=lwd)
+			 key.x.coord, key.y.coord, lwd=lwd, lty=lty)
 	}
 	if (is.null(key.labels))
 	    key.labels <- dimnames(x)[[2]]
 
 	lab.angl <- angles +
-	    if(draw.segments) (angles[2] - angles[1]) / 2 else 0
+            if(draw.segments) (angles[2] - angles[1]) / 2 else 0
 
 	label.x <- cos(lab.angl) * len * 1.1 + key.loc[1]
 	label.y <- sin(lab.angl) * len * 1.1 + key.loc[2]
 
-        ##-- FIXME : Do the following witout loop !
+        ##-- FIXME : Do the following witout loop {need not use adj but ..)!
 	for (k in 1:n.seg) {
 	    text.adj <-
                 c(## horizontal
@@ -187,5 +190,8 @@ function(x, full = TRUE, scale = TRUE, radius = TRUE,
                  labels= key.labels[k], cex = cex, adj = text.adj)
 	}
     } # Unit key is drawn and labelled
-    invisible()
+
+    if (frame.plot) box(...)
+
+    invisible(locations)
 }
