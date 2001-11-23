070951bae2b837001aca1662452551ec3df8ac7a
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-11-23 14:07:30 +0000

    rle() returns class and has inverse
    git-svn-id: https://svn.r-project.org/R/trunk@16849 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/rle.R
+++ src/library/base/R/rle.R
@@ -3,8 +3,26 @@ rle <- function(x) {
         stop("x must be a vector")
     n <- length(x)
     if (n == 0)
-        return(list(lengths = numeric(0), values = x))
+        return(list(lengths = integer(0), values = x))
     y <- x[-1] != x[-n]
     i <- c(which(y | is.na(y)), n)
-    list(lengths = diff(c(0, i)), values = x[i])
+    structure(list(lengths = diff(c(0:0, i)), values = x[i]),
+              class = "rle")
 }
+
+print.rle <- function(x, digits = getOption("digits"), ...) {
+    cat("Run Length Encoding\n  lengths:")
+    str(x$lengths)
+    cat("  values :")
+    str(x$values, digits = digits)
+    invisible(x)
+}
+
+inverse.rle <- function(x, ...)
+{
+    if(is.null(le <- x$lengths) ||
+       is.null(v  <- x$values) || length(le) != length(v))
+        stop("invalid `rle' structure")
+    rep(v, le)
+}
+
