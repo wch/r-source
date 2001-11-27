1029715b916bd51d5e4c8a97f48218415f134d42
iacus <iacus@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-11-27 23:55:38 +0000

    definitions of new.file and file.edit functions
    git-svn-id: https://svn.r-project.org/R/trunk@16927 00db46b3-68df-0310-9c12-caf00c1e9a41

---
+++ src/library/base/R/mac/fileedit.R
@@ -0,0 +1,30 @@
+file.edit <-
+    function(file){
+         
+    if( missing(file) ){
+         new.file()
+         return(invisible())
+     }
+         
+     if(file.exists(file)) {
+       .Internal(file.edit(file))
+       return(invisible())
+     } else {
+         new.file(file)
+         return(invisible())
+     }
+
+}
+
+
+new.file <-
+    function(file){
+    
+    if( missing(file) )
+     file <- "NewFile"
+   .Internal(new.file(file))
+       
+     return(invisible())
+    
+    
+}
