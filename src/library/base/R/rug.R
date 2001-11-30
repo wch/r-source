ebb5eb4b5ef4791a6ce9a4128c49ad22122f375d
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-11-30 08:57:05 +0000

    rug() with log : PR#1188
    git-svn-id: https://svn.r-project.org/R/trunk@16997 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/rug.R
+++ src/library/base/R/rug.R
@@ -1,16 +1,23 @@
-rug<- function(x, ticksize = 0.03, side = 1, lwd = 0.5, col) {
+rug <- function(x, ticksize = 0.03, side = 1, lwd = 0.5, col,
+		quiet = getOption("warn") < 0) {
     x <- as.vector(x)
     ok <- is.finite(x)
     x <- x[ok]
     oldtick <- par(tck = ticksize)
     on.exit(par(oldtick))
     if( !missing(col) ) {
-        oldcol<-par(fg = col)
-        on.exit(par(oldcol), add=TRUE)
+	oldcol <- par(fg = col)
+	on.exit(par(oldcol), add=TRUE)
     }
-    usr <- par("usr")
-    usr <- if (side %% 2 == 1)  usr[1:2] else usr[3:4]
-    if(any(x < usr[1] | x > usr[2]))
-        warning("some values will be clipped")
+    if(!quiet) {
+	u <- par("usr")
+	u <- if (side %% 2 == 1) {
+	    if(par("xlog")) 10^u[1:2] else u[1:2]
+	} else {
+	    if(par("ylog")) 10^u[3:4] else u[3:4]
+	}
+	if(any(x < u[1] | x > u[2]))
+	    warning("some values will be clipped")
+    }
     axis(side, at = x, lab = FALSE, lwd = lwd)
 }
