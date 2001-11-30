65a432e982ae5177f19c12e6c2664074c43c1ff5
murrell <murrell@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-11-30 23:04:59 +0000

    Fix to the way we wait for Expose event on creation of X11 device
    git-svn-id: https://svn.r-project.org/R/trunk@17010 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/modules/X11/devX11.c
+++ src/modules/X11/devX11.c
@@ -1185,11 +1185,15 @@ newX11_Open(NewDevDesc *dd, newX11Desc *xd, char *dsp, double w, double h,
 	    
 	    /* Gobble expose events */
 	    
-	    XNextEvent(display, &event);
-	    if (event.xany.type == Expose) {
-		while (event.xexpose.count)
-		    XNextEvent(display, &event);
-	    }
+	    while ( XPeekEvent(display, &event), 
+		    !XCheckTypedEvent(display, Expose, &event))
+                ;
+	    /* XNextEvent(display, &event);
+	       if (event.xany.type == Expose) {
+	       while (event.xexpose.count)
+	       XNextEvent(display, &event);
+	       }
+	    */
 	}
     } else { /* PIXMAP */
 	xd->windowWidth = iw = w;
