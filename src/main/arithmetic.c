b89eaf1430b50831b322a539da14d9c1ccd5af1b
tlumley <tlumley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-12-04 21:03:50 +0000

    work-around AIX nm bug
    git-svn-id: https://svn.r-project.org/R/trunk@17162 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/arithmetic.c
+++ src/main/arithmetic.c
@@ -74,8 +74,11 @@ int matherr(struct exception *exc)
 #endif
 
 #ifdef IEEE_754
+#ifndef _AIX
 const double R_Zero_Hack = 0.0;	/* Silence the Sun compiler */
-
+#else
+double R_Zero_Hack = 0.0;
+#endif
 typedef union
 {
     double value;
