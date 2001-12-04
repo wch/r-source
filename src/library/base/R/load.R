4bf3c08d706dd9882081f67eaf7562e8711c6909
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-12-04 17:16:07 +0000

    Changed save, load, load.image to allow compression, use of connections.
    git-svn-id: https://svn.r-project.org/R/trunk@17138 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/load.R
+++ src/library/base/R/load.R
@@ -1,14 +1,57 @@
-load <- function(file,envir = parent.frame())
-    .Internal(load(file,envir))
+load <- function (file, envir = parent.frame()) {
+    if (is.character(file)) {
+        if (capabilities("libz"))
+            # if zlib is available just open with gzfile, whether
+            # file is compressed or not; zlib should work either way.
+            con <- gzfile(file, "rb")
+        else
+            con <- file(file, "rb")
+        on.exit(close(con))
+    }
+    else if (inherits(file, "connection"))
+        con <- file
+    else stop("bad file argument")
+
+    magic <- readChar(con, 5)
+
+    if (regexpr("RD[AX]2\n", magic) == -1) {
+        # Not a verion 2 magic number, so try the old way.
+        if (is.character(file)) {
+            close(con)
+            on.exit()
+        }
+        else stop("loading from connections not compatible with magic number")
+        .Internal(load(file, envir))
+    }
+    else .Internal(loadFromConn(con, envir))
+}
 
 save <- function(..., list = character(0), file = "", ascii = FALSE,
-	         version = NULL, envir = parent.frame())
+	         version = NULL, envir = parent.frame(), compress = FALSE)
 {
     names <- as.character( substitute( list(...)))[-1]
     list<- c(list, names)
-    invisible(.Internal(save(list, file, ascii, version, envir)))
+    if (! is.null(version) && version == 1)
+        invisible(.Internal(save(list, file, ascii, version, envir)))
+    else {
+        if (is.character(file)) {
+            if (file == "") stop("`file' must be non-empty string")
+            if (compress) con <- gzfile(file, "wb")
+            else con <- file(file, "wb")
+            on.exit(close(con))
+        }
+        else if (inherits(file, "connection"))
+            con <- file
+        else stop("bad file argument")
+        invisible(.Internal(saveToConn(list, con, ascii, version, envir)))
+    }
 }
 
-save.image <- function (file = ".RData", version = NULL)
+save.image <- function (file = ".RData", version = NULL, ascii = FALSE,
+                        compress = FALSE) {
+    if (! is.character(file) || file == "")
+        stop("`file' must be non-empty string")
     save(list = ls(envir = .GlobalEnv, all.names = TRUE), file = file,
-         version = version, envir = .GlobalEnv)
+         version = version, ascii = ascii, compress = compress,
+         envir = .GlobalEnv)
+}
