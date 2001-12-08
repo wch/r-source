dc2f0166d07bfe21226cd8dcc8bdf43090444dc6
pd <pd@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-12-08 00:03:59 +0000

    changes from maintainer-build run
    git-svn-id: https://svn.r-project.org/R/trunk@17276 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/gram.c
+++ src/main/gram.c
@@ -503,7 +503,7 @@ static const short yycheck[] = {    14,
     55
 };
 /* -*-C-*-  Note some compilers choke on comments on `#line' lines.  */
-#line 3 "/usr/lib/bison.simple"
+#line 3 "/usr/share/bison/bison.simple"
 /* This file comes from bison-1.28.  */
 
 /* Skeleton output parser for bison,
@@ -717,7 +717,7 @@ __yy_memcpy (char *to, char *from, unsigned int count)
 #endif
 #endif
 
-#line 217 "/usr/lib/bison.simple"
+#line 217 "/usr/share/bison/bison.simple"
 
 /* The user can define YYPARSE_PARAM as the name of an argument to be passed
    into yyparse.  The argument should have type void *.
@@ -1363,7 +1363,7 @@ case 79:
     break;}
 }
    /* the action file gets copied in in place of this dollarsign */
-#line 543 "/usr/lib/bison.simple"
+#line 543 "/usr/share/bison/bison.simple"
 
   yyvsp -= yylen;
   yyssp -= yylen;
@@ -3281,6 +3281,7 @@ int yylex(void)
     case '@':
     case LEFT_ASSIGN:
     case RIGHT_ASSIGN:
+    case EQ_ASSIGN:
 	EatLines = 1;
 	break;
 
