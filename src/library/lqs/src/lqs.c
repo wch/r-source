1be112229ee9f2ef5b1241a85c0d2d61b788efe5
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2001-12-17 20:03:53 +0000

    fix warnings in PR#1184
    git-svn-id: https://svn.r-project.org/R/trunk@17455 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/lqs/src/lqs.c
+++ src/library/lqs/src/lqs.c
@@ -259,7 +259,7 @@ lqs_fitlots(double *x, double *y, int *n, int *p, int *qn,
 	}
     }/* for(trial in 0:ntrials) */
 
-    *crit = best;
+    *crit = (best < 0.0) ? 0.0 : best;
     if(*sample) PutRNGstate();
     /* lqs_free(); */
 }
