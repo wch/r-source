ce7eddb7def6d6ff8f7b938f91513331b3b2959a
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2002-01-02 11:53:57 +0000

    add arima.sim allow common graphical parameters in ... in ts.plot
    git-svn-id: https://svn.r-project.org/R/trunk@17618 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/ts/R/ts.R
+++ src/library/ts/R/ts.R
@@ -1,7 +1,24 @@
-ts.plot <- function(..., gpars=list())
+ts.plot <- function(..., gpars = list())
 {
-    sers <- ts.union(...)
+    dots <- list(...)
+    pars <- c("xlab", "ylab", "xlim", "ylim", "col", "lty", "lwd",
+              "type", "main", "sub", "log")
+    m <- names(dots) %in% pars
+    if(length(m)) {
+        gpars <- c(gpars, dots[m])
+        dots <- dots[!m]
+    }
+    sers <- do.call("ts.union", dots)
     if(is.null(gpars$ylab))
         gpars$ylab <- if(NCOL(sers) > 1) "" else deparse(substitute(...))
-    do.call("plot.ts", c(list(sers, plot.type="single"), gpars))
+    do.call("plot.ts", c(list(sers, plot.type = "single"), gpars))
+}
+
+arima.sim <- function(n, model, rand.gen = rnorm,
+                      innov = rand.gen(n, ...), n.start = 100, ...)
+{
+    x <- ts(c(rnorm(n.start), innov[1:n]), start = 1 - n.start)
+    if(length(model$ma)) x <- filter(x, c(1, model$ma), sides = 1)
+    if(length(model$ar)) x <- filter(x, model$ar, method = "recursive")
+    as.ts(x[-(1:n.start)])
 }
