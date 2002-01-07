9195d66a33bc3ece5340af7534d807f90d9af2b7
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2002-01-07 10:19:54 +0000

    fix PR#1238, identify
    git-svn-id: https://svn.r-project.org/R/branches/R-1-4-patches@17661 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/identify.R
+++ src/library/base/R/identify.R
@@ -8,8 +8,9 @@ identify.default <- function(x, y=NULL, labels=seq(along=x), pos=FALSE,
         on.exit(par(opar))
     }
     xy <- xy.coords(x, y)
-    z <- .Internal(identify(xy$x, xy$y, as.character(labels),
-			    n, plot, offset))
+    x <- xy$x
+    y <- xy$y
+    z <- .Internal(identify(x, y, as.character(labels), n, plot, offset))
     i <- seq(z[[1]])[z[[1]]]
-    if(pos) list(ind= i, pos= z[[2]][z[[1]]]) else i
+    if(pos) list(ind = i, pos = z[[2]][z[[1]]]) else i
 }
