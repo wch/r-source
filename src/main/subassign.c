ad7c2c08bf3d8df11a5fcbc1ad1cb1616559136e
jmc <jmc@00db46b3-68df-0310-9c12-caf00c1e9a41>
2002-01-09 15:50:42 +0000

    allow empty arg list to "[<-", as well as the list of length 1 contaning R_MissingArg, as the parser generates (PR#1247)
    git-svn-id: https://svn.r-project.org/R/trunk@17685 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/subassign.c
+++ src/main/subassign.c
@@ -1125,14 +1125,20 @@ static SEXP listAssign1(SEXP call, SEXP x, SEXP subs, SEXP y)
 static void SubAssignArgs(SEXP args, SEXP *x, SEXP *s, SEXP *y)
 {
     SEXP p;
-    if (length(args) < 3)
+    if (length(args) < 2)
 	error("SubAssignArgs: invalid number of arguments");
     *x = CAR(args);
-    *s = p = CDR(args);
-    while (CDDR(p) != R_NilValue)
-	p = CDR(p);
-    *y = CADR(p);
-    SETCDR(p, R_NilValue);
+    if(length(args) == 2) {
+	*s = R_NilValue;
+	*y = CADR(args);
+    }
+    else {
+	*s = p = CDR(args);
+	while (CDDR(p) != R_NilValue)
+	    p = CDR(p);
+	*y = CADR(p);
+	SETCDR(p, R_NilValue);
+    }
 }
 
 
@@ -1204,6 +1210,7 @@ SEXP do_subassign_dflt(SEXP call, SEXP op, SEXP args, SEXP rho)
     case VECSXP:
 	switch (nsubs) {
 	case 0:
+	    x = VectorAssign(call, x, R_MissingArg, y);
 	    break;
 	case 1:
 	    x = VectorAssign(call, x, CAR(subs), y);
