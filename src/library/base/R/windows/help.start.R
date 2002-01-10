885f2e9d6555d12ab7dd14f3ec8eac0af9c4b28d
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2002-01-10 15:18:50 +0000

    check access rights for updating indices help.start(F) to suppress these
    git-svn-id: https://svn.r-project.org/R/branches/R-1-4-patches@17699 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/windows/help.start.R
+++ src/library/base/R/windows/help.start.R
@@ -1,18 +1,19 @@
-help.start <- function(gui = "irrelevant", browser = "irrelevant")
+help.start <- function(update = TRUE, gui = "irrelevant",
+                       browser = "irrelevant")
 {
     a <- file.path(R.home(), "doc/html/rwin.html")
     if(!file.exists(a))
         a <- file.path(R.home(), "doc/html/rwin.htm")
     if(!file.exists(a))
         stop("I can't find the html help")
-    else {
+    if(update) {
         cat("updating HTML package listing\n")
         flush.console()
-        make.packages.html(.libPaths())
-        make.search.html(.Library)
-        a <- gsub("/", "\\\\", a)
-        cat("If nothing happens, you have to open `", a, "' yourself\n")
-        .Internal(help.start());
+        try(make.packages.html(.libPaths()))
+        try(make.search.html(.Library))
     }
+    a <- gsub("/", "\\\\", a)
+    cat("If nothing happens, you have to open `", a, "' yourself\n")
+    .Internal(help.start());
     invisible("")
 }
