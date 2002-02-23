c5658e2b3d71d2cc153651a30aa760e6f615bc70
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2002-02-23 18:26:01 +0000

    download.info -> internet.into set show.error.messages to save confusing Ole Christensen
    git-svn-id: https://svn.r-project.org/R/trunk@18407 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/profile/Common.R
+++ src/library/profile/Common.R
@@ -31,8 +31,9 @@ options(warn = 0)
 options(help.try.all.packages = FALSE)
 options(CRAN = "http://cran.r-project.org")
 options(timeout = 60)
-options(download.info = 2)
+options(internet.info = 2)
 options(encoding = native.enc)
+options(show.error.messages = FALSE)
 
 .First <- function() {
     require("ctest", quietly = TRUE)
