fc1db1e54b16f2104fc637b8e7e321c4d240d190
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2002-03-09 10:39:08 +0000

    move dcabs1 to cmmplbas.f
    git-svn-id: https://svn.r-project.org/R/trunk@18608 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/appl/blas.f
+++ src/appl/blas.f
@@ -89,14 +89,6 @@ c
    50 continue
       return
       end
-      double precision function dcabs1(z)
-      double complex z,zz
-      double precision t(2)
-      equivalence (zz,t(1))
-      zz = z
-      dcabs1 = dabs(t(1)) + dabs(t(2))
-      return
-      end
       subroutine  dcopy(n,dx,incx,dy,incy)
 c
 c     copies a vector, x, to a vector, y.
--- src/modules/lapack/cmplxblas.f
+++ src/modules/lapack/cmplxblas.f
@@ -1,3 +1,11 @@
+      double precision function dcabs1(z)
+      double complex z,zz
+      double precision t(2)
+      equivalence (zz,t(1))
+      zz = z
+      dcabs1 = dabs(t(1)) + dabs(t(2))
+      return
+      end
       double precision function dzasum(n,zx,incx)
 c
 c     takes the sum of the absolute values.
