7edc6b0ab17042a8259d3a64d34b4d895c8c494f
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2002-03-15 11:08:00 +0000

    move AirPassenger examples (slow) to test-Ts
    git-svn-id: https://svn.r-project.org/R/trunk@18718 00db46b3-68df-0310-9c12-caf00c1e9a41

--- tests/ts-tests.R
+++ tests/ts-tests.R
@@ -93,3 +93,29 @@ data(UKgas)
 (fit <- StructTS(log10(UKgas), type = "BSM", fixed=c(0, NA, NA, NA)))
 (fit <- StructTS(log10(UKgas), type = "BSM", fixed=c(NA, 0, NA, NA)))
 (fit <- StructTS(log10(UKgas), type = "BSM", fixed=c(NA, NA, NA, 0)))
+
+### from AirPassengers
+data(AirPassengers)
+## The classic `airline model', by full ML
+(fit <- arima(log10(AirPassengers), c(0, 1, 1),
+              seasonal = list(order=c(0, 1 ,1), period=12)))
+update(fit, method = "CSS")
+update(fit, x=window(log10(AirPassengers), start = 1954))
+pred <- predict(fit, n.ahead = 24)
+tl <- pred$pred - 1.96 * pred$se
+tu <- pred$pred + 1.96 * pred$se
+ts.plot(AirPassengers, 10^tl, 10^tu, log = "y", lty = c(1,2,2))
+
+## full ML fit is the same if the series is reversed, CSS fit is not
+ap0 <- rev(log10(AirPassengers))
+attributes(ap0) <- attributes(AirPassengers)
+arima(ap0, c(0, 1, 1), seasonal = list(order=c(0, 1 ,1), period=12))
+arima(ap0, c(0, 1, 1), seasonal = list(order=c(0, 1 ,1), period=12),
+      method = "CSS")
+
+## Structural Time Series
+ap <- log10(AirPassengers) - 2
+(fit <- StructTS(ap, type= "BSM"))
+par(mfrow=c(1,2))
+plot(cbind(ap, fitted(fit)), plot.type = "single")
+plot(cbind(ap, tsSmooth(fit)), plot.type = "single")
