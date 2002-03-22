a3028ba3a16f12986c85335a234360c8528505eb
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2002-03-22 18:09:35 +0000

    drop proper names attrib.
    git-svn-id: https://svn.r-project.org/R/trunk@18846 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/qsort.c
+++ src/main/qsort.c
@@ -34,8 +34,8 @@ SEXP do_qsort(SEXP call, SEXP op, SEXP args, SEXP rho)
     x_int = !x_real && (TYPEOF(x) == INTSXP || TYPEOF(x) == LGLSXP);
     PROTECT(sx = (x_real || x_int) ? duplicate(x) : coerceVector(x, REALSXP));
     /* if x has names, drop them, since they won't be ordered : */
-    if (!isNull(getAttrib(x, R_NamesSymbol)))
-	setAttrib(x, R_NamesSymbol, R_NilValue);
+    if (!isNull(getAttrib(sx, R_NamesSymbol)))
+	setAttrib(sx, R_NamesSymbol, R_NilValue);
     indx_ret = asLogical(CADR(args));
     n = LENGTH(x);
     if(x_int) ivx = INTEGER(sx); else vx = REAL(sx);
