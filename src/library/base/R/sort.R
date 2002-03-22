8b7820aa1961bdfd39a10b04fcbf7bebe135bbfe
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2002-03-22 18:33:08 +0000

    new sort(*, method=) for experimentation -- momentarily
    git-svn-id: https://svn.r-project.org/R/trunk@18850 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/sort.R
+++ src/library/base/R/sort.R
@@ -1,30 +1,52 @@
-sort <- function(x, partial=NULL, na.last=NA)
+sort <- function(x, partial=NULL, na.last=NA,
+                 method = c("quick","shell"), index.return = FALSE)
 {
-    isfact <- is.factor(x)
-    if(isfact){
+    if(isfact <- is.factor(x)) {
 	lev <- levels(x)
 	nlev <- nlevels(x)
+        x <- c(x)
+    } else if(!is.atomic(x))
+        stop("`x' must be atomic")
+    if(has.na <- any(ina <- is.na(x))) {
+        x <- x[!ina]
+        nas <- x[ina]
     }
-    nas <- x[is.na(x)]
-    x <- c(x[!is.na(x)])
     if(!is.null(partial)) {
         if(!all(is.finite(partial))) stop("non-finite `partial'")
 	y <- .Internal(psort(x, partial))
-    } else {
-	nms <- names(x)
-	if(!is.null(nms)) {
-	    o <- order(x)
-	    y <- x[o]
-	    names(y) <- nms[o]
-	}
-	else
-	    y <- .Internal(sort(x))
     }
-    if(!is.na(na.last)) {
-	if(!na.last) y <- c(nas, y)
-	else if (na.last) y <- c(y, nas)
+    else {
+        nms <- names(x)
+        meth <- if(is.null(nms) && is.numeric(x))
+            match.arg(method) else "shell"
+        ## work around sys.function() {called from match.arg(.)} bug :
+        if(is.character(meth) && length(meth)== 1)
+            method <- meth
+        else { ## Bug happened! -- e.g. example(mosaicplot)
+            if(missing(method) || substr(method,1,1)=="q")
+                method <- "quick"
+            else method <- "shell"
+            warning("`meth' is wrong -- bug from match.arg():",
+                    meth,"\n setting method to", method)
+        }
+        switch(method,
+               "quick" = {
+                   y <- .Internal(qsort(x, index.return))
+               },
+               "shell" = {
+                   if(!is.null(nms)) {
+                       o <- order(x)
+                       y <- x[o]
+                       names(y) <- nms[o]
+                   }
+                   else
+                       y <- .Internal(sort(x))
+               })
     }
-    if(isfact) y <- factor(y, levels=1:nlev, labels=lev)
+    if(!is.na(na.last) && has.na)
+	y <- if(!na.last) c(nas, y) else c(y, nas)
+    if(isfact)
+        y <- factor(y, levels=1:nlev, labels=lev)
     y
 }
 
@@ -47,6 +69,8 @@ order <- function(..., na.last = TRUE)
 
 sort.list <- function(x, partial = NULL, na.last = TRUE)
 {
+    if(!is.atomic(x))
+        stop("`x' must be atomic")
     if(!is.null(partial))
         .NotYetUsed("partial != NULL")
     .Internal(order(na.last, x))
