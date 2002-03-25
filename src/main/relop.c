5fd0a1d2f864d98d6cf72d0d9512be7dfb54d5db
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2002-03-25 22:29:44 +0000

    typo -- revert for now.
    git-svn-id: https://svn.r-project.org/R/trunk@18911 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/relop.c
+++ src/main/relop.c
@@ -420,11 +420,8 @@ static SEXP string_relop(RELOP_TYPE code, SEXP s1, SEXP s2)
     switch (code) {
     case EQOP:
 	for (i = 0; i < n; i++) {
-	    if ((STRING_ELT(s1, i % n1) == NA_STRING) ||
-		(STRING_ELT(s2, i % n2) == NA_STRING))
-		LOGICAL(ans)[i] = NA_LOGICAL;
- 	    else if (strcmp(CHAR(STRING_ELT(s1, i % n1)),
- 			    CHAR(STRING_ELT(s2, i % n2))) == 0)
+	    if (strcmp(CHAR(STRING_ELT(s1, i % n1)),
+		       CHAR(STRING_ELT(s2, i % n2))) == 0)
 		LOGICAL(ans)[i] = 1;
 	    else
 		LOGICAL(ans)[i] = 0;
@@ -432,11 +429,8 @@ static SEXP string_relop(RELOP_TYPE code, SEXP s1, SEXP s2)
 	break;
     case NEOP:
 	for (i = 0; i < n; i++) {
- 	    if ((STRING_ELT(s1, i % n1) == NA_STRING) ||
- 		(STRING_ELT(s2, i % n2) == NA_STRING))
-		LOGICAL(ans)[i] = NA_LOGICAL;
- 	    else if (strcmp(CHAR(STRING_ELT(s1, i % n1)),
-			    CHAR(STRING_ELT(s2, i % n2))) != 0)
+	    if (streql(CHAR(STRING_ELT(s1, i % n1)),
+		       CHAR(STRING_ELT(s2, i % n2))) != 0)
 		LOGICAL(ans)[i] = 0;
 	    else
 		LOGICAL(ans)[i] = 1;
@@ -444,11 +438,8 @@ static SEXP string_relop(RELOP_TYPE code, SEXP s1, SEXP s2)
 	break;
     case LTOP:
 	for (i = 0; i < n; i++) {
- 	    if ((STRING_ELT(s1, i % n1) == NA_STRING) ||
- 		(STRING_ELT(s2, i % n2) == NA_STRING))
-		LOGICAL(ans)[i] = NA_LOGICAL;
- 	    else if (STRCMP(CHAR(STRING_ELT(s1, i % n1)),
-			    CHAR(STRING_ELT(s2, i % n2))) < 0)
+	    if (STRCMP(CHAR(STRING_ELT(s1, i % n1)),
+		       CHAR(STRING_ELT(s2, i % n2))) < 0)
 		LOGICAL(ans)[i] = 1;
 	    else
 		LOGICAL(ans)[i] = 0;
@@ -456,10 +447,7 @@ static SEXP string_relop(RELOP_TYPE code, SEXP s1, SEXP s2)
 	break;
     case GTOP:
 	for (i = 0; i < n; i++) {
- 	    if ((STRING_ELT(s1, i % n1) == NA_STRING) ||
- 		(STRING_ELT(s2, i % n2) == NA_STRING))
- 		    LOGICAL(ans)[i] = NA_LOGICAL;
-	    else if (STRCMP(CHAR(STRING_ELT(s1, i % n1)),
+	    if (STRCMP(CHAR(STRING_ELT(s1, i % n1)),
 		       CHAR(STRING_ELT(s2, i % n2))) > 0)
 		LOGICAL(ans)[i] = 1;
 	    else
@@ -468,11 +456,8 @@ static SEXP string_relop(RELOP_TYPE code, SEXP s1, SEXP s2)
 	break;
     case LEOP:
 	for (i = 0; i < n; i++) {
- 	    if ((STRING_ELT(s1, i % n1) == NA_STRING) ||
- 		(STRING_ELT(s2, i % n2) == NA_STRING))
- 		    LOGICAL(ans)[i] = NA_LOGICAL;
- 	    else if (STRCMP(CHAR(STRING_ELT(s1, i % n1)),
-			    CHAR(STRING_ELT(s2, i % n2))) <= 0)
+	    if (STRCMP(CHAR(STRING_ELT(s1, i % n1)),
+		       CHAR(STRING_ELT(s2, i % n2))) <= 0)
 		LOGICAL(ans)[i] = 1;
 	    else
 		LOGICAL(ans)[i] = 0;
@@ -480,11 +465,8 @@ static SEXP string_relop(RELOP_TYPE code, SEXP s1, SEXP s2)
 	break;
     case GEOP:
 	for (i = 0; i < n; i++) {
- 	    if ((STRING_ELT(s1, i % n1) == NA_STRING) ||
- 		(STRING_ELT(s2, i % n2) == NA_STRING))
- 		    LOGICAL(ans)[i] = NA_LOGICAL;
- 	    else if (STRCMP(CHAR(STRING_ELT(s1, i % n1)),
-			    CHAR(STRING_ELT(s2, i % n2))) >= 0)
+	    if (STRCMP(CHAR(STRING_ELT(s1, i % n1)),
+		       CHAR(STRING_ELT(s2, i % n2))) >= 0)
 		LOGICAL(ans)[i] = 1;
 	    else
 		LOGICAL(ans)[i] = 0;
@@ -494,3 +476,4 @@ static SEXP string_relop(RELOP_TYPE code, SEXP s1, SEXP s2)
     UNPROTECT(2);
     return ans;
 }
+
