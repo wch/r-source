b801676da664f164e875ac3b731aced6dd234bae
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2002-03-26 09:04:33 +0000

    revert for now: giving spurious warnings
    git-svn-id: https://svn.r-project.org/R/trunk@18923 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/library.R
+++ src/library/base/R/library.R
@@ -18,7 +18,7 @@ function(package, help, lib.loc = NULL, character.only = FALSE,
                  call. = FALSE)
         }
         ## which version was this package built under?
-        if((built <- !is.na(fields[1, "Built"]))) {
+        if((built <- fields[1, "Built"]) != "NA") {
             builtunder <- substring(strsplit(built, ";")[[1]][1], 3)
             if(nchar(builtunder) &&
                compareVersion(current, builtunder) < 0) {
