e9dcde194d04a4cd8af61ff28f1062e1ee56c5c9
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2002-03-29 18:52:18 +0000

    Added check to getConnection that argument is not too large.
    git-svn-id: https://svn.r-project.org/R/trunk@19000 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/connections.c
+++ src/main/connections.c
@@ -78,7 +78,8 @@ Rconnection getConnection(int n)
 {
     Rconnection con = NULL;
 
-    if(n < 0 || n == NA_INTEGER || !(con = Connections[n]))
+    if(n < 0 || n >= NCONNECTIONS || n == NA_INTEGER ||
+       !(con = Connections[n]))
 	error("invalid connection");
     return con;
 
@@ -89,7 +90,8 @@ Rconnection getConnection_no_err(int n)
 {
     Rconnection con = NULL;
 
-    if(n < 0 || n == NA_INTEGER || !(con = Connections[n]))
+    if(n < 0 || n >= NCONNECTIONS || n == NA_INTEGER ||
+       !(con = Connections[n]))
 	return NULL;
     return con;
 
