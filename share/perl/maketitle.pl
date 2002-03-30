f7ee9fee8071454b1a567b75d5f2785510014ef9
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2002-03-30 23:08:16 +0000

    Rewrite from scratch using R::Dcf and new R::Utils::formatDL().
    git-svn-id: https://svn.r-project.org/R/trunk@19026 00db46b3-68df-0310-9c12-caf00c1e9a41

--- share/perl/maketitle.pl
+++ share/perl/maketitle.pl
@@ -1,83 +1,16 @@
-# Perl script to create a TITLE file from the package DESCRIPTION.
-#
-# <FIXME>
-#  This script was obtained by running a2p on an Awk script.
-#  It could be considerably improved, using R::Dcf etc.
-# </FIXME>
+## Perl script to create a TITLE file from the package DESCRIPTION.
+##   Usage: [R CMD] perl /path/to/maketitle.pl FILE
 
-$[ = 1;			# set array base to 1
-$, = ' ';		# set output field separator
-$\ = "\n";		# set output record separator
+use R::Dcf;
+use R::Utils;
 
-$active = 0;
-$n = 0;
+my $description = new R::Dcf($ARGV[0]);
 
-while (<>) {
-    chomp;	# strip record separator
-    @Fld = split(' ', $_, 9999);
+print formatDL($description->{"Package"},
+	       $description->{"Title"},
+	       "table", 72, 16), "\n";
 
-    if ($_ =~ /^Package:/) {
-	$package = $Fld[2];
-    }
-    elsif ($_ =~ /^Title:/) {
-	$active = 1;
-	$n = $#Fld - 1;
-	for ($i = 2; $i <= $#Fld; $i++) {
-	    $a{$i - 1} = $Fld[$i];
-	}
-    }
-    elsif ($_ =~ /^[ \t]/) {
-	if ($active == 1) {
-	    for ($i = 1; $i <= $#Fld; $i++) {
-		$a{$n + $i} = $Fld[$i];
-	    }
-	    $n = $n + $#Fld;
-	}
-    }
-    else {
-	$active = 0;
-    }
-}
-
-$S = '';
-$lc = 16;
-$rc = 72;
-$old = $package;
-$len = length($old);
-if ($len > $lc - 3) {
-    $S = $old;
-    $old = "";
-    for ($j = 1; $j < $lc; $j++) {
-	$old = $old . ' ';
-    }
-}
-else {
-    for ($j = 1; $j < $lc - $len; $j++) {
-	$old = $old . ' ';
-    }
-}
-for ($i = 1; $i <= $n; $i++) {
-    $new = $old . ' ' . $a{$i};
-    if (length($new) > $rc) {
-	if (length($S) > 0) {
-	    $S = $S . "\n" . $old;
-	}
-	else {
-	    $S = $old;
-	}
-	$old = '';
-	for ($j = 1; $j < $lc; $j++) {
-	    $old = $old . ' ';
-	}
-	$new = $old . ' ' . $a{$i};
-    }
-    $old = $new;
-}
-if (length($S) > 0) {
-    $S = $S . "\n" . $new;
-}
-else {
-    $S = $old;
-}
-print $S;
-
+### Local Variables: ***
+### mode: perl ***
+### perl-indent-level: 4 ***
+### End: ***
