8253f2516ffc46cfd53221e939b05f524bce4a4b
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2002-04-04 10:19:38 +0000

    + GPL header
    git-svn-id: https://svn.r-project.org/R/trunk@19139 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/sprintf.c
+++ src/main/sprintf.c
@@ -1,3 +1,23 @@
+/*
+ *  R : A Computer Language for Statistical Data Analysis
+ *  Copyright (C) 2002     the R Development Core Team
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Written by Jonathan Rougier, email J.C.Rougier@durham.ac.uk
+*/
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif
@@ -9,11 +29,7 @@
 /* Simple wrapper for C sprintf function: does very little checking to
    see that the format conversion character and the argument are
    compatible.  This is the user's responsibility!
-
-   Written by Jonathan Rougier, email J.C.Rougier@durham.ac.uk
-
 */
-
 SEXP do_sprintf(SEXP call, SEXP op, SEXP args, SEXP env)
 {
     int nargs;
