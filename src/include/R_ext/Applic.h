f0cc5b59d3c404bfbfa328d143d7d5e858d53f5a
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2002-04-05 08:55:24 +0000

    translate Fortrsn subroutines to void not int, pace f2c
    git-svn-id: https://svn.r-project.org/R/trunk@19176 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/include/R_ext/Applic.h
+++ src/include/R_ext/Applic.h
@@ -180,144 +180,144 @@ typedef /* Subroutine */ int (*S_fp)();
 /* ../appl/blas.f ---> see also ./Linpack.h */
 extern double
 F77_NAME(dasum)(int *, double *, int *);
-extern int
+extern void
 F77_NAME(daxpy)(int *, double *, double *, int *, double *, int *);
-extern int
+extern void
 F77_NAME(dcopy)(int *, double *, int *, double *, int *);
 extern double
 F77_NAME(ddot)(int *, double *, int *, double *, int *);
-extern int
+extern void
 F77_NAME(dgbmv)(char *, int *, int *, int *, int *, double *, double *,
 		int *, double *, int *, double *, double *, int *);
-extern int
+extern void
 F77_NAME(dgemm)(char *, char *, int *, int *, int *, double *, double *, int *,
 	double *, int *, double *, double *, int *);
-extern int
+extern void
 F77_NAME(dgemv)(char *, int *, int *, double *, double *, int *, double *,
 		int *, double *, double *, int *);
-extern int
+extern void
 F77_NAME(dger)(int *, int *, double *, double *, int *, double *, int *,
 	       double *, int *);
 extern double
 F77_NAME(dnrm2)(int *, double *, int *incx);
-extern int
+extern void
 F77_NAME(drot)(int *, double *, int *, double *, int *, double *, double *);
-extern int
+extern void
 F77_NAME(drotg)(double *, double *, double *, double *);
-extern int
+extern void
 F77_NAME(drotm)(int *, double *, int *, double *, int *, double *);
-extern int
+extern void
 F77_NAME(drotmg)(double *dd1, double *dd2, double * dx1, double *, double *);
-extern int
+extern void
 F77_NAME(dsbmv)(char *, int *, int *, double *, double *, int *, double *,
 		int *, double *, double *, int *);
-extern int
+extern void
 F77_NAME(dscal)(int *, double *, double *, int *);
-extern int
+extern void
 F77_NAME(dspmv)(char *, int *, double *, double *, double *, int *, double *,
 		double *, int *);
-extern int
+extern void
 F77_NAME(dspr)(char *, int *, double *, double *, int *, double *);
-extern int
+extern void
 F77_NAME(dspr2)(char *, int *, double *, double *, int *, double *, int *,
 		double *);
-extern int
+extern void
 F77_NAME(dswap)(int *, double *, int *, double *, int *);
-extern int
+extern void
 F77_NAME(dsymm)(char *, char *, int *, int *, double *, double *, int *,
 		double *, int *, double *, double *, int *);
-extern int
+extern void
 F77_NAME(dsymv)(char *, int *, double *, double *, int *, double *, int *,
 		double *, double *, int *);
-extern int
+extern void
 F77_NAME(dsyr)(char *, int *, double *, double *, int *, double *, int *);
-extern int
+extern void
 F77_NAME(dsyr2)(char *, int *, double *, double *, int *, double *,
 		int *, double *, int *);
-extern int
+extern void
 F77_NAME(dsyr2k)(char *, char *, int *, int *, double *, double *, int *,
 		 double *, int *, double *, double *, int *);
-extern int
+extern void
 F77_NAME(dsyrk)(char *, char *, int *, int *, double *, double *, int *,
 		double *, double *, int *);
-extern int
+extern void
 F77_NAME(dtbmv)(char *, char *, char *, int *, int *, double *, int *,
 		double *, int *);
-extern int
+extern void
 F77_NAME(dtbsv)(char *, char *, char *, int *, int *, double *, int *,
 		double *, int *);
-extern int
+extern void
 F77_NAME(dtpmv)(char *, char *, char *, int *, double *, double *, int *);
-extern int
+extern void
 F77_NAME(dtpsv)(char *, char *, char *, int *, double *, double *, int *);
-extern int
+extern void
 F77_NAME(dtrmm)(char *, char *, char *, char *, int *, int *, double *,
 		double *, int *, double *, int *);
-extern int
+extern void
 F77_NAME(dtrmv)(char *, char *, char *, int *, double *, int *, double *,
 		int *);
-extern int
+extern void
 F77_NAME(dtrsm)(char *, char *, char *, char *, int *, int *, double *,
 		double *, int *, double *, int *);
-extern int
+extern void
 F77_NAME(dtrsv)(char *, char *, char *, int *, double *, int *, double *,
 		int *);
-extern int
+extern void
 F77_NAME(idamax)(int *, double *, int *);
-extern int
+extern void
 F77_NAME(lsame)(char *, char *);
 
 /*----*/
-int F77_NAME(ch2inv)(double *x, int *ldx, int *n, double *v, int *info);
-int F77_NAME(chol)(double *a, int *lda, int *n, double *v, int *info);
+void F77_NAME(ch2inv)(double *x, int *ldx, int *n, double *v, int *info);
+void F77_NAME(chol)(double *a, int *lda, int *n, double *v, int *info);
 
-int F77_NAME(dpoco)(double *a, int *lda, int *n, double *rcond,
-		    double *z__, int *info);
-int F77_NAME(dpodi)(double *a, int *lda, int *n, double *det, int *job);
-int F77_NAME(dpofa)(double *a, int *lda, int *n, int *info);
-int F77_NAME(dposl)(double *a, int *lda, int *n, double *b);
+void F77_NAME(dpoco)(double *a, int *lda, int *n, double *rcond,
+		     double *z__, int *info);
+void F77_NAME(dpodi)(double *a, int *lda, int *n, double *det, int *job);
+void F77_NAME(dpofa)(double *a, int *lda, int *n, int *info);
+void F77_NAME(dposl)(double *a, int *lda, int *n, double *b);
 /* find qr decomposition, dqrdc2() is basis of R's qr() */
-int F77_NAME(dqrdc)(double *x, int *ldx, int *n, int *p,
-		    double *qraux, int *jpvt, double *work, int *job);
-int F77_NAME(dqrdc2)(double *x, int *ldx, int *n, int *p,
-		     double *tol, int *rank,
-		     double *qraux, int *pivot, double *work);
-int F77_NAME(dqrls)(double *x, int *n, int *p, double *y, int *ny,
-		    double *tol, double *b, double *rsd,
-		    double *qty, int *k,
-		    int *jpvt, double *qraux, double *work);
+void F77_NAME(dqrdc)(double *x, int *ldx, int *n, int *p,
+		     double *qraux, int *jpvt, double *work, int *job);
+void F77_NAME(dqrdc2)(double *x, int *ldx, int *n, int *p,
+		      double *tol, int *rank,
+		      double *qraux, int *pivot, double *work);
+void F77_NAME(dqrls)(double *x, int *n, int *p, double *y, int *ny,
+		     double *tol, double *b, double *rsd,
+		     double *qty, int *k,
+		     int *jpvt, double *qraux, double *work);
 /* solve for QR coefficients */
-int F77_NAME(dqrsl)(double *x, int *ldx, int *n, int *k,
-		    double *qraux, double *y,
-		    double *qy, double *qty, double *b,
-		    double *rsd, double *xb, int *job, int *info);
+void F77_NAME(dqrsl)(double *x, int *ldx, int *n, int *k,
+		     double *qraux, double *y,
+		     double *qy, double *qty, double *b,
+		     double *rsd, double *xb, int *job, int *info);
 
 /* ../appl/dqrutl.f */
-int F77_NAME(dqrqty)(double *x, int *n, int *k, double *qraux,
-		     double *y, int *ny, double *qty);
-int F77_NAME(dqrqy)(double *x, int *n, int *k, double *qraux,
-		    double *y, int *ny, double *qy);
-int F77_NAME(dqrcf)(double *x, int *n, int *k, double *qraux,
-		    double *y, int *ny, double *b, int *info);
-int F77_NAME(dqrrsd)(double *x, int *n, int *k, double *qraux,
+void F77_NAME(dqrqty)(double *x, int *n, int *k, double *qraux,
+		      double *y, int *ny, double *qty);
+void F77_NAME(dqrqy)(double *x, int *n, int *k, double *qraux,
+		     double *y, int *ny, double *qy);
+void F77_NAME(dqrcf)(double *x, int *n, int *k, double *qraux,
+		     double *y, int *ny, double *b, int *info);
+void F77_NAME(dqrrsd)(double *x, int *n, int *k, double *qraux,
 		     double *y, int *ny, double *rsd);
-int F77_NAME(dqrxb)(double *x, int *n, int *k, double *qraux,
-		    double *y, int *ny, double *xb);
+void F77_NAME(dqrxb)(double *x, int *n, int *k, double *qraux,
+		     double *y, int *ny, double *xb);
 /*---*/
 
-int F77_NAME(dsvdc)(double *x, int *ldx, int *n, int *p,
-		    double *s, double *e,
-		    double *u, int *ldu, double *v, int *ldv,
-		    double *work, int *job, int *info);
-int F77_NAME(dtrco)(double *t, int *ldt, int *n, double *rcond,
-		    double *z__, int *job);
-int F77_NAME(dtrsl)(double *t, int *ldt, int *n, double *b, int *job,
-		    int *info);
+void F77_NAME(dsvdc)(double *x, int *ldx, int *n, int *p,
+		     double *s, double *e,
+		     double *u, int *ldu, double *v, int *ldv,
+		     double *work, int *job, int *info);
+void F77_NAME(dtrco)(double *t, int *ldt, int *n, double *rcond,
+		     double *z__, int *job);
+void F77_NAME(dtrsl)(double *t, int *ldt, int *n, double *b, int *job,
+		     int *info);
 
 double Brent_fmin(double ax, double bx, double (*f)(double, void *),
 		  void *info, double tol);
-int F77_NAME(lminfl)(double *x, int *ldx, int *n, int *k, double *qraux,
-		     double *resid, double *hat, double *coef, double *sigma);
+void F77_NAME(lminfl)(double *x, int *ldx, int *n, int *k, double *qraux,
+		      double *resid, double *hat, double *coef, double *sigma);
 
 /* ../appl/interv.c */
 int findInterval(double *xt, int n, double x,
@@ -369,8 +369,8 @@ void samin(int n, double *pb, double *yb, optimfn fn, int maxit,
 	   int tmax, double ti, int trace, void *ex);
 
 /* ../main/qsort.c -- only F77 ones: */
-int F77_NAME(qsort4)(double *v, int *indx, int *ii, int *jj);
-int F77_NAME(qsort3)(double *v,            int *ii, int *jj);
+void F77_NAME(qsort4)(double *v, int *indx, int *ii, int *jj);
+void F77_NAME(qsort3)(double *v,            int *ii, int *jj);
 
 
 #ifdef  __cplusplus
