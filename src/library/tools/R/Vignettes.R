7c3032b71c0943f6f9cf711a1bc3aa499f1efca2
leisch <leisch@00db46b3-68df-0310-9c12-caf00c1e9a41>
2002-04-05 11:36:26 +0000

    added vignette building to R CMD build
    git-svn-id: https://svn.r-project.org/R/trunk@19182 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/Vignettes.R
+++ src/library/tools/R/Vignettes.R
@@ -1,31 +1,12 @@
+## run a tangle+source and a weave on all vignettes of a package
+
 checkVignettes <- function(package, dir, lib.loc = NULL,
                            tangle=TRUE, weave=TRUE,
                            workdir=c("tmp", "src", "cur"),
                            keepfiles = FALSE)
-{    
-
-    ## Argument handling.
-    if(!missing(package)) {
-        if(length(package) != 1)
-            stop("argument 'package' must be of length 1")
-        docdir <- file.path(.find.package(package, lib.loc), "doc")
-        ## Using package installed in @code{dir} ...
-    }
-    else {
-        if(missing(dir))
-            stop("you must specify 'package' or 'dir'")
-        ## Using sources from directory @code{dir} ...
-        if(!file.exists(dir))
-            stop(paste("directory", sQuote(dir), "does not exist"))
-        else
-            ## maybe perform tilde expansion on @code{dir}
-            docdir <- file.path(dirname(dir), basename(dir), "inst", "doc")
-    }
-    
-    if(!file.exists(docdir)) return()
-
-    exts <- outer(c("r", "s", "R", "S"), c("nw","tex"), paste, sep="")
-    files <- .listFilesWithExts(docdir, exts)
+{
+    vigns <- pkgVignettes(package=package, dir=dir, lib.loc=lib.loc)
+    if(is.null(vigns)) return(NULL)
     
     workdir <- match.arg(workdir)
     wd <- getwd()
@@ -36,7 +17,7 @@ checkVignettes <- function(package, dir, lib.loc = NULL,
     }
     else{
         keepfiles <- TRUE
-        if(workdir=="src") setwd(docdir)
+        if(workdir=="src") setwd(vigns$dir)
     }
     
     outConn <- textConnection("out", "w")
@@ -53,7 +34,7 @@ checkVignettes <- function(package, dir, lib.loc = NULL,
                    source=list())
 
     
-    for(f in files){
+    for(f in vigns$docs){
         if(tangle){
             yy <- try(Stangle(f, quiet=TRUE))
             if(inherits(yy, "try-error"))
@@ -98,3 +79,95 @@ print.checkVignettes <- function(x, ...)
     mycat(x$tangle, "*** Tangle Errors ***")
     mycat(x$source, "*** Source Errors ***")    
 }    
+
+
+
+## get an object of class pkgVignettes which contains a list of Sweave
+## files and the name of the directory which contains them
+
+pkgVignettes <- function(package, dir, lib.loc = NULL)
+{
+    ## Argument handling.
+    if(!missing(package)) {
+        if(length(package) != 1)
+            stop("argument 'package' must be of length 1")
+        docdir <- file.path(.find.package(package, lib.loc), "doc")
+        ## Using package installed in @code{dir} ...
+    }
+    else {
+        if(missing(dir))
+            stop("you must specify 'package' or 'dir'")
+        ## Using sources from directory @code{dir} ...
+        if(!file.exists(dir))
+            stop(paste("directory", sQuote(dir), "does not exist"))
+        else
+            ## maybe perform tilde expansion on @code{dir}
+            docdir <- file.path(dirname(dir), basename(dir), "inst", "doc")
+    }
+    
+    if(!file.exists(docdir)) return(NULL)
+
+    exts <- outer(c("r", "s", "R", "S"), c("nw","tex"), paste, sep="")
+    docs <- .listFilesWithExts(docdir, exts)
+    
+    z <- list(docs=docs, dir=docdir)
+    class(z) <- "pkgVignettes"
+    z
+}
+
+
+## run a weave and pdflatex on all vignettes of a package and try to
+## remove all temporary files that were created
+
+buildVignettes <-function(package, dir, lib.loc = NULL)
+{
+    vigns <- pkgVignettes(package=package, dir=dir, lib.loc=lib.loc)
+    if(is.null(vigns)) return(NULL)
+    
+    wd <- getwd()
+    setwd(vigns$dir)
+
+    on.exit(setwd(wd))
+
+    origfiles <- list.files()
+
+
+    if("makefile" %in% tolower(origfiles))
+    {
+        yy <- system(paste(Sys.getenv("MAKE"), "pdf"))
+        if(yy>0) return("Error: running 'make pdf' failed")
+        yy <- system(paste(Sys.getenv("MAKE"), "clean"))
+        if(yy>0) return("Error: running 'make clean' failed")
+
+    }
+    else{
+        pdfs <- character(0)
+        for(f in vigns$docs){
+            
+            f <- basename(f)
+            bf <- sub("\\..[^\\.]*$", "", f)
+            bft <- paste(bf, ".tex", sep="")
+            pdfs <- c(pdfs, paste(bf, ".pdf", sep=""))
+            
+            yy <- try(Sweave(f, quiet=TRUE))
+            if(inherits(yy, "try-error")) return(yy)
+            yy <- system(paste(file.path(R.home(), "bin", "texi2dvi"),
+                               "--pdf", bft, ">",
+                               paste(bf, ".stdout", sep="")))
+            if(yy>0)
+                return(paste("Error: running texi2dvi on", bft, "failed"))
+
+        }
+        f <- list.files()
+        f <- f[!(f %in% c(pdfs, origfiles))]
+        unlink(f)
+    }
+    invisible(NULL)
+}
+        
+                      
+                     
+
+        
+            
+        
