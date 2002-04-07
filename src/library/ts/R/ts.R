236b3b3a78a92905e0b3bfd0f79aeecab897372a
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2002-04-07 11:10:05 +0000

    make arima.sim S+ compatible
    git-svn-id: https://svn.r-project.org/R/trunk@19209 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/ts/R/ts.R
+++ src/library/ts/R/ts.R
@@ -14,17 +14,31 @@ ts.plot <- function(..., gpars = list())
     do.call("plot.ts", c(list(sers, plot.type = "single"), gpars))
 }
 
-arima.sim <- function(n, model, rand.gen = rnorm,
+arima.sim <- function(model, n, rand.gen = rnorm,
                       innov = rand.gen(n, ...), n.start = NA, ...)
 {
-    minroots <- min(Mod(polyroot(c(1, -model$ar))))
-    if (minroots <= 1) stop("ar part of model is not stationary")
+    if(!is.list(model)) stop("`model' must be list")
+    nm <- names(model)
     p <- length(model$ar)
+    if(p) {
+        minroots <- min(Mod(polyroot(c(1, -model$ar))))
+        if(minroots <= 1) stop("ar part of model is not stationary")
+    }
     q <- length(model$ma)
-    if(is.na(n.start)) n.start <- p + q + ceiling(6/log(minroots))
+    if(is.na(n.start)) n.start <- p + q +
+        ifelse(p > 0, ceiling(6/log(minroots)), 0)
     if(n.start < p + q) stop("burn-in must be as long as ar + ma")
-    x <- ts(c(rnorm(n.start), innov[1:n]), start = 1 - n.start)
+    d <- 0
+    if(!is.null(ord <- model$order)) {
+        if(length(ord) != 3) stop("`model$order' must be of length 3")
+        d <- ord[2]
+        if(d != round(d) || d < 0)
+            stop("number of differences must be a positive integer")
+    }
+    x <- ts(c(rand.gen(n.start, ...), innov[1:n]), start = 1 - n.start)
     if(length(model$ma)) x <- filter(x, c(1, model$ma), sides = 1)
     if(length(model$ar)) x <- filter(x, model$ar, method = "recursive")
-    as.ts(x[-(1:n.start)])
+    x <- x[-(1:n.start)]
+    if(d > 0) x <- diffinv(x, differences = d)
+    else as.ts(x)
 }
