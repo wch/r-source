078232a4fc0c65fd72d845642e34eed5496e7700
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2002-04-18 13:33:54 +0000

    glibc can supply inconsistent mon/mday/yday, Don't attempt to repair.
    git-svn-id: https://svn.r-project.org/R/trunk@19361 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/datetime.c
+++ src/main/datetime.c
@@ -620,6 +620,8 @@ static void glibc_fix(struct tm *tm, int *invalid)
 #endif
     tm0 = localtime(&t);
     if(tm->tm_year == NA_INTEGER) tm->tm_year = tm0->tm_year;
+    if(tm->tm_mon != NA_INTEGER && tm->tm_mday != NA_INTEGER) return;
+    /* at least one of the month and the day of the month is missing */
     if(tm->tm_yday != NA_INTEGER) {
 	/* since we have yday, let that take precedence over mon/mday */
 	int yday = tm->tm_yday, mon = 0;
