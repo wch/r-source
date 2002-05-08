fd3112ec485453cab0c5f0badad63b9fbc01a53e
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2002-05-08 21:14:34 +0000

    interface to pcre -- not yet operational!
    git-svn-id: https://svn.r-project.org/R/trunk@19602 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/grep.R
+++ src/library/base/R/grep.R
@@ -1,22 +1,22 @@
 grep <-
-    function(pattern, x, ignore.case=FALSE, extended=TRUE, value=FALSE)
+    function(pattern, x, ignore.case=FALSE, extended=TRUE, perl=FALSE, value=FALSE)
 {
-    .Internal(grep(pattern, x, ignore.case, extended, value))
+    .Internal(grep(pattern, x, ignore.case, extended, perl, value))
 }
 
 sub <-
-    function(pattern, replacement, x, ignore.case=FALSE, extended=TRUE)
+    function(pattern, replacement, x, ignore.case=FALSE, extended=TRUE, perl=FALSE)
 {
-    .Internal(sub(pattern, replacement, x, ignore.case, extended))
+    .Internal(sub(pattern, replacement, x, ignore.case, extended, perl))
 }
 
 gsub <-
-    function(pattern, replacement, x, ignore.case=FALSE, extended=TRUE)
+    function(pattern, replacement, x, ignore.case=FALSE, extended=TRUE, perl=FALSE)
 {
-    .Internal(gsub(pattern, replacement, x, ignore.case, extended))
+    .Internal(gsub(pattern, replacement, x, ignore.case, extended, perl))
 }
 
-regexpr <- function(pattern, text, extended=TRUE)
+regexpr <- function(pattern, text, extended=TRUE, perl=FALSE)
 {
-    .Internal(regexpr(pattern, text, extended))
+    .Internal(regexpr(pattern, text, extended, perl))
 }
