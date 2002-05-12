d4ff7ccfaec2298a46b9acf8d122f4edd95cd7a7
iacus <iacus@00db46b3-68df-0310-9c12-caf00c1e9a41>
2002-05-12 14:04:17 +0000

    quartz function to call the quartz device
    git-svn-id: https://svn.r-project.org/R/trunk@19655 00db46b3-68df-0310-9c12-caf00c1e9a41

---
+++ src/library/base/R/unix/quartz.R
@@ -0,0 +1,4 @@
+quartz <- function (display = "", width = 5, height = 5, pointsize = 12, 
+                    family="Helvetica", antialias = TRUE, autorefresh = TRUE)
+.Internal(Quartz(display, width, height, pointsize,family, antialias,autorefresh))
+
