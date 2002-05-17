266fc18a0445fb906d2107cf586aace2426acae9
tlumley <tlumley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2002-05-17 22:06:22 +0000

    clear inWarning flag with options(warn=2) (PR#1570)
    git-svn-id: https://svn.r-project.org/R/trunk@19732 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/errors.c
+++ src/main/errors.c
@@ -203,6 +203,7 @@ void warningcall(SEXP call, const char *format, ...)
 	va_start(ap, format);
 	Rvsnprintf(buf, BUFSIZE, format, ap);
 	va_end(ap);
+	inWarning=0; /* PR1570 */
 	errorcall(call, "(converted from warning) %s", buf);
     }
     else if(w == 1) {	/* print as they happen */
