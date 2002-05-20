e2cbc6234b6d47af6a64c66db2a98cdf504b4fd8
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2002-05-20 18:20:15 +0000

    Cosmetics.
    git-svn-id: https://svn.r-project.org/R/trunk@19771 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/Scripts.R
+++ src/library/base/R/Scripts.R
@@ -1,15 +1,18 @@
-.Script <- function (interpreter, script, args, ...) 
+.Script <- function(interpreter, script, args, ...)
 {
-    if (.Platform$OS.type == "windows") {
-        cmd <- paste(file.path(R.home(), "bin", "Rcmd"), file.path("..", 
-            "share", interpreter, script), args)
+    if(.Platform$OS.type == "windows") {
+        cmd <- paste(file.path(R.home(), "bin", "Rcmd"),
+                     file.path("..", "share", interpreter, script),
+                     args)
         system(cmd, invisible = TRUE)
     }
+    else if(.Platform$OS.type == "mac") {
+        .Internal(applescript(file.path(R.home(), "script"), "demo"))
+    }
     else
-     if(.Platform$OS.type == "mac"){
-     .Internal(applescript(file.path(R.home(),"script"),"demo"))
-     }
-     else system(paste(file.path(R.home(), "bin", "Rcmd"), interpreter, 
-         file.path(R.home(), "share", interpreter, script), args), 
-         ...)
+        system(paste(file.path(R.home(), "bin", "Rcmd"),
+                     interpreter, 
+                     file.path(R.home(), "share", interpreter, script),
+                     args), 
+               ...)
 }
