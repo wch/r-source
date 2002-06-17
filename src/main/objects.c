3b2f7d6561ce02755d32583665b78d16f237e367
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2002-06-17 07:28:22 +0000

    -Wall unsused variables
    git-svn-id: https://svn.r-project.org/R/trunk@20121 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/objects.c
+++ src/main/objects.c
@@ -1091,7 +1091,6 @@ argument to standardGeneric.
 */
 static SEXP get_this_generic(SEXP args)
 {
-    SEXP rval = NULL,t;
     RCNTXT *cptr;
     /* a second argument to the call, if any, is taken as the function */
     if(CDR(args) != R_NilValue)
