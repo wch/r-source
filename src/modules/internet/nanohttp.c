a37d7ce22543b10ad4bd68eeae86954c7b2b640e
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2002-06-19 09:21:09 +0000

    proxyPort was being set to zero if not specified
    git-svn-id: https://svn.r-project.org/R/branches/R-1-5-patches@20166 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/modules/internet/nanohttp.c
+++ src/modules/internet/nanohttp.c
@@ -366,9 +366,9 @@ RxmlNanoHTTPScanProxy(const char *URL)
         xmlFree(proxy);
 	proxy = NULL;
     }
-    if (proxyPort != 0) {
+    /*if (proxyPort != 0) {
 	proxyPort = 0;
-    }
+	}*/
     if (URL == NULL)
 	RxmlMessage(0, "Removing HTTP proxy info");
     else
