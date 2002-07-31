45c2c288a232dae5113d0b496c1bbecc9681ae92
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2002-07-31 15:19:47 +0000

    drop levels in interaction case too
    git-svn-id: https://svn.r-project.org/R/trunk@20689 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/split.R
+++ src/library/base/R/split.R
@@ -2,10 +2,8 @@ split <- function(x, f) UseMethod("split")
 
 split.default <- function(x, f)
 {
-    if (is.list(f))
-        f <- interaction(f)
-    else
-        f <- factor(f)                  # drop extraneous levels
+    if (is.list(f)) f <- interaction(f)
+    f <- factor(f)                  # drop extraneous levels
     if (is.null(attr(x, "class")) && is.null(names(x)))
         return(.Internal(split(x, f)))
     ## else
