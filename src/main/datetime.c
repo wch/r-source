729df027f1c65bc7916129c2e904c29cef7b6d34
iacus <iacus@00db46b3-68df-0310-9c12-caf00c1e9a41>
2002-08-04 13:44:55 +0000

    patched mktime on Mac, still doens't take into account for TZ
    git-svn-id: https://svn.r-project.org/R/trunk@20748 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/datetime.c
+++ src/main/datetime.c
@@ -55,6 +55,11 @@
 
 #include "Defn.h"
 
+#if (defined(Macintosh) & defined(__MRC__))
+#define mktime POSIXMakeTime
+#endif
+
+ 
 #ifndef HAVE_WORKING_STRPTIME
 /* Substitute based on glibc code. */
 # include "Rstrptime.h"
