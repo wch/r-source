8d4313f1c851975b5974305fff987effe758609b
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2002-08-21 14:42:21 +0000

    better fix for grid() for log coord -- will works when par("[xy]axp") does...
    git-svn-id: https://svn.r-project.org/R/trunk@20953 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/grid.R
+++ src/library/base/R/grid.R
@@ -3,13 +3,15 @@ grid <- function (nx=NULL, ny=NULL, col="lightgray", lty="dotted")
     if (is.null(nx)|| nx >= 1) {
         axp <- par("xaxp")
         if(is.null(nx)) nx <- axp[3]
-	abline(v = seq(axp[1],axp[2],len=1+nx),
-               col = col, lty = lty, untf = TRUE)
+        at <- if(par("xlog")) 10^seq(log10(axp[1]), log10(axp[2]), len= 1+nx)
+            else seq(axp[1],axp[2],len=1+nx)
+	abline(v = at, col = col, lty = lty)
     }
     if (is.null(ny)|| ny >= 1) {
         axp <- par("yaxp")
         if(is.null(ny)) ny <- axp[3]
-	abline(h = seq(axp[1],axp[2],len=1+ny),
-               col = col, lty = lty, untf = TRUE)
+        at <- if(par("ylog")) 10^seq(log10(axp[1]), log10(axp[2]), len= 1+ny)
+            else seq(axp[1],axp[2],len=1+ny)
+	abline(h = at, col = col, lty = lty)
     }
 }
