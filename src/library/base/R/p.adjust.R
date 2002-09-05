7852ae38ca7019395c8360d8beb68cb664448b78
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2002-09-05 21:44:30 +0000

    Two new p.adjust() methods, contributed by Gordon Smyth.
    git-svn-id: https://svn.r-project.org/R/trunk@21185 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/p.adjust.R
+++ src/library/base/R/p.adjust.R
@@ -1,25 +1,45 @@
-p.adjust.methods<- c("holm", "hochberg", "bonferroni","none")
+p.adjust.methods <-
+    c("holm", "hochberg", "hommel", "bonferroni", "fdr", "none")
 
 p.adjust <- function(p, method = p.adjust.methods, n = length(p)) {
+    ## Methods 'Hommel' and 'FDR' and speed improvements contributed by
+    ## Gordon Smyth <smyth@wehi.edu.au>.
+
+    if (n == 1) return(p)
     method <- match.arg(method)
-    if ( n == 1 ) return(p)
-    switch (method,
-            hochberg = {
-                r <- rank(p)
-                index <- order(p)
-                qi <- p*(n+1-r)
-                for (i in (length(p)-1):1)
-                    qi[index[i]] <- min(qi[index[i]], qi[index[i+1]])
-                qi
-            },
-            holm = {
-                r <- rank(p)
-                index <- order(p)
-                qi <- p*(n+1-r)
-                for (i in 2:length(p))
-                    qi[index[i]] <- max(qi[index[i]], qi[index[i-1]])
-                pmin(qi, 1)
-            },
-            bonferroni = pmin(n * p, 1),
-            none = p)
+    switch(method,
+           holm = {
+               i <- 1:n
+               o <- order(p)
+               ro <- order(o)
+               pmin(1, cummax( (n - i + 1) * p[o] ))[ro]
+           },
+           hochberg = {
+               i <- n:1
+               o <- order(p, decreasing = TRUE)
+               ro <- order(o)
+               pmin(1, cummin( (n - i + 1) * p[o] ))[ro]
+           },
+           hommel = {
+               i <- 1:n
+               s <- sort(p, index = TRUE)
+               p <- s$x
+               ro <- order(s$ix)
+               q <- pa <- rep( min(n*p/(1:n)), n)
+               for (j in (n-1):2) {
+                   q1 <- min(j*p[(n-j+2):n]/(2:j))
+                   q[1:(n-j+1)] <- pmin( j*p[1:(n-j+1)], q1)
+                   q[(n-j+2):n] <- q[n-j+1]
+                   pa <- pmax(pa,q)
+               }
+               pmax(pa,p)[ro]
+           },
+           fdr = {
+               i <- n:1
+               o <- order(p, decreasing = TRUE)
+               ro <- order(o)
+               pmin(1, cummin( n / i * p[o] ))[ro]
+           },
+           bonferroni = pmin(n * p, 1),
+           none = p)
 }
