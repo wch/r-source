d003c584e760d0d284cae72de814a6d0c3bcbaf8
pd <pd@00db46b3-68df-0310-9c12-caf00c1e9a41>
2002-09-22 21:16:23 +0000

    parse(text=character(0)), clarify documentation
    git-svn-id: https://svn.r-project.org/R/trunk@21338 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/parse.R
+++ src/library/base/R/parse.R
@@ -3,6 +3,8 @@
 parse <- function(file = "", n = NULL, text = NULL, prompt = "?",
                   white = FALSE)
 {
+    if (!is.null(text) && length(as.character(text)) == 0)
+        return(expression())
     if(is.character(file))
         if(file == "") file <- stdin()
         else {
