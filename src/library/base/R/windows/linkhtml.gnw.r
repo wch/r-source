5b741a3b238301381618226a7baf5de827022220
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2002-10-21 16:36:05 +0000

    change format of packages.html on Windows
    git-svn-id: https://svn.r-project.org/R/trunk@21618 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/windows/linkhtml.gnw.r
+++ src/library/base/R/windows/linkhtml.gnw.r
@@ -16,32 +16,45 @@ make.packages.html <- function(lib.loc=.libPaths())
     f.hd <- file.path(R.home(), "doc/html/packages-head.html")
     if(!file.create(f.tg)) {
         warning("cannot update HTML package index")
-        return()
+        return(FALSE)
     }
     file.append(f.tg, f.hd)
     out <- file(f.tg, open="a")
-    cat('<table align="center" summary="R Package list">\n', file=out)
-    pg <- sort(.packages(all.available = TRUE, lib.loc = lib.loc))
+    cat('<table width=\"100%\" summary="R Package list">\n',
+        file=out)
     rh <- gsub("\\\\", "/", R.home())
     drive <- substring(rh, 1, 2)
-    for (i in  pg) {
-        title <- package.description(i, field="Title")[1]
-        if (is.na(title)) title <- "-- Title is missing --"
-        f.t <- c(i, title)
-        lib <- system.file(package=i)
+    for (lib in lib.loc) {
+        pg <- sort(.packages(all.available = TRUE, lib.loc = lib))
+        ## use relative indexing for .Library
         if(is.na(pmatch(rh, lib))) {
-            if(substring(lib, 2, 2) != ":") lib <- paste(drive, lib, sep="")
+            libname <- gsub("/", "\\\\", lib)
+            if(substring(lib, 2, 2) != ":")
+                lib <- paste(drive, lib, sep="")
             lib <- paste("file:///", lib, sep="")
-        } else
-            lib <- gsub(rh, "../..", lib)
-        cat('<tr align="left" valign="top">\n',
-            "<td><a href=\"", lib, "/html/00Index.html\">",
-            f.t[1], "</a></td><td>", paste(f.t[-1], collapse=" "),
-            "</td></tr>\n", file=out, sep="")
+        } else {
+            lib <- "../../library"
+            libname <- "the standard library"
+        }
+        if(length(lib.loc) > 1)
+            cat("<p><h3>Packages in ", libname, "</h3>\n",
+                sep = "", file = out)
+        if(libname != "the standard library")
+            cat("<p>Cross-links from this library to other libraries may not work.\n\n",file = out)
+        cat("<p>\n<table width=\"100%\">\n", file = out)
+        for (i in  pg) {
+            title <- package.description(i, field="Title")[1]
+            if (is.na(title)) title <- "-- Title is missing --"
+            cat('<tr align="left" valign="top">\n',
+                '<td width="25%"><a href="', lib, '/', i,
+                '/html/00Index.html">', i, "</a></td><td>", title,
+                "</td></tr>\n", file=out, sep="")
+        }
+        cat("</table>\n\n", file=out)
     }
-    cat("</table>\n</body></html>\n", file=out)
+    cat("</body></html>\n", file=out)
     close(out)
-    invisible(pg)
+    invisible(TRUE)
 }
 
 make.search.html <- function(lib.loc=.Library)
