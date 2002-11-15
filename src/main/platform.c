3f7c8e6420df876333dca9c76d2fc73808b897e5
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2002-11-15 21:56:52 +0000

    -Wall on platforms without symlink
    git-svn-id: https://svn.r-project.org/R/trunk@21912 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/platform.c
+++ src/main/platform.c
@@ -357,10 +357,13 @@ SEXP do_fileremove(SEXP call, SEXP op, SEXP args, SEXP rho)
 
 SEXP do_filesymlink(SEXP call, SEXP op, SEXP args, SEXP rho)
 {
-    SEXP f1, f2, ans;
-    int i, n, n1, n2;
+    SEXP f1, f2;
+    int n, n1, n2;
+#ifdef HAVE_SYMLINK
+    SEXP ans;
+    int i;
     char from[PATH_MAX], to[PATH_MAX], *p;
-
+#endif
     checkArity(op, args);
     f1 = CAR(args); n1 = length(f1);
     f2 = CADR(args); n2 = length(f2);
