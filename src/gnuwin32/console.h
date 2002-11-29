4e63de91a477993263c9e6c8b657b7f3f8a53868
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2002-11-29 10:43:45 +0000

    move across usability enhancements from R-devel
    git-svn-id: https://svn.r-project.org/R/branches/R-1-6-patches@22100 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/console.h
+++ src/gnuwin32/console.h
@@ -1,7 +1,7 @@
 /*
  *  R : A Computer Language for Statistical Data Analysis
  *  file console.h
- *  Copyright (C) 1998--1999  Guido Masarotto and Brian Ripley
+ *  Copyright (C) 1998--2002  Guido Masarotto and Brian Ripley
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -50,3 +50,5 @@ void consoleclear(control c);
 
 extern int setWidthOnResize;
 int consolecols(console c);
+void pagerbclose(control m);
+
