0da4811e3ba412a7e808677f6a0de105d9c59c9e
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2002-12-15 21:54:34 +0000

    Add functions .saveRDA() and .loadRDA() as a preliminary interface to serialization.
    git-svn-id: https://svn.r-project.org/R/trunk@22329 00db46b3-68df-0310-9c12-caf00c1e9a41

---
+++ src/library/base/R/serialize.R
@@ -0,0 +1,38 @@
+.saveRDA <-
+function(object, file = "", ascii = FALSE, version = NULL,
+         compress = FALSE, refhook = NULL) 
+{
+    if(is.character(file)) {
+        if(file == "") stop("'file' must be non-empty string")
+        mode <- if(ascii) "w" else "wb"
+        if(compress && capabilities("libz")) con <- gzfile(file, mode)
+        else con <- file(file, mode)
+        on.exit(close(con))
+    }
+    else if (inherits(file, "connection")) {
+        con <- file
+        if(missing(ascii)) 
+            if(summary(con)$text == "text") 
+                ascii <- TRUE
+            else ascii <- FALSE
+    }        
+    else stop("bad 'file' argument")
+    invisible(.Internal(serializeToConn(object, con, ascii, version,
+                                        refhook)))
+}
+
+.readRDA <-
+function(file, refhook = NULL) 
+{
+    if(is.character(file)) {
+        if(capabilities("libz")) 
+            con <- gzfile(file, "rb")
+        else
+            con <- file(file, "rb")
+        on.exit(close(con))
+    }
+    else if (inherits(file, "connection")) 
+        con <- file
+    else stop("bad 'file' argument")
+    .Internal(unserializeFromConn(con, refhook))
+}
