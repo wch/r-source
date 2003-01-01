6659817bc45fcfc9334c5bc441a5341f03eaed0b
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-01-01 21:39:37 +0000

    remove call to etime which HP-UX does not have in a PIC library
    git-svn-id: https://svn.r-project.org/R/trunk@22561 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/modules/lapack/dlapack0.f
+++ src/modules/lapack/dlapack0.f
@@ -25707,40 +25707,6 @@ CIBM           PREFER SCALAR
 *     End of DRSCL
 *
       END
-      DOUBLE PRECISION FUNCTION DSECND( )
-*
-*  -- LAPACK auxiliary routine (version 3.0) --
-*     Univ. of Tennessee, Univ. of California Berkeley, NAG Ltd.,
-*     Courant Institute, Argonne National Lab, and Rice University
-*     September 30, 1994
-*
-*  Purpose
-*  =======
-*
-*  DSECND returns the user time for a process in seconds.
-*  This version gets the time from the system function ETIME.
-*
-* =====================================================================
-*
-*     .. Local Scalars ..
-      REAL               T1
-*     ..
-*     .. Local Arrays ..
-      REAL               TARRAY( 2 )
-*     ..
-*     .. External Functions ..
-      REAL               ETIME
-      EXTERNAL           ETIME
-*     ..
-*     .. Executable Statements ..
-*
-      T1 = ETIME( TARRAY )
-      DSECND = TARRAY( 1 )
-      RETURN
-*
-*     End of DSECND
-*
-      END
       SUBROUTINE DTGEX2( WANTQ, WANTZ, N, A, LDA, B, LDB, Q, LDQ, Z,
      $                   LDZ, J1, N1, N2, WORK, LWORK, INFO )
 *
