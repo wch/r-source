bf32552adc79d4a58f599220362825e9184146f8
tlumley <tlumley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-01-22 01:08:34 +0000

    pass y down to plot.function in plot (rpeng@stat.ucla.edu)
    git-svn-id: https://svn.r-project.org/R/trunk@22770 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/methods/R/plot.R
+++ src/library/methods/R/plot.R
@@ -2,8 +2,8 @@ plot <- function (x, y, ...)
 {
     if (is.null(attr(x, "class")) && is.function(x)) {
         if ("ylab" %in% names(list(...))) 
-            plot.function(x, ...)
-        else plot.function(x, ylab = paste(deparse(substitute(x)), 
+            plot.function(x,y, ...)
+        else plot.function(x,y, ylab = paste(deparse(substitute(x)), 
                               "(x)"), ...)
     }
     else UseMethod("plot")
