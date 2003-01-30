294ee512851379f3253dc05454abb526e39a137b
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-01-30 21:01:02 +0000

    missing any in tests
    git-svn-id: https://svn.r-project.org/R/trunk@22914 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/distn.R
+++ src/library/base/R/distn.R
@@ -33,24 +33,24 @@ rcauchy <-
 
 dgamma <- function(x, shape, rate = 1, scale = 1/rate, log = FALSE)
 {
-    if(shape <= 0) stop("shape must be strictly positive")
+    if(any(shape <= 0)) stop("shape must be strictly positive")
     .Internal(dgamma(x, shape, scale, log))
 }
 pgamma <- function(q, shape, rate = 1, scale = 1/rate,
                    lower.tail = TRUE, log.p = FALSE)
 {
-    if(shape <= 0) stop("shape must be strictly positive")
+    if(any(shape <= 0)) stop("shape must be strictly positive")
     .Internal(pgamma(q, shape, scale, lower.tail, log.p))
 }
 qgamma <- function(p, shape, rate = 1, scale = 1/rate,
                    lower.tail = TRUE, log.p = FALSE)
 {
-    if(shape <= 0) stop("shape must be strictly positive")
+    if(any(shape <= 0)) stop("shape must be strictly positive")
     .Internal(qgamma(p, shape, scale, lower.tail, log.p))
 }
 rgamma <- function(n, shape, rate = 1, scale = 1/rate)
 {
-    if(shape <= 0) stop("shape must be strictly positive")
+    if(any(shape <= 0)) stop("shape must be strictly positive")
     .Internal(rgamma(n, shape, scale))
 }
 
