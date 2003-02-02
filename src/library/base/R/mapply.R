c62bfc10863dc69197ad37c5d8554cd4448f41ec
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-02-02 16:57:30 +0000

    missing PACKAGE=
    git-svn-id: https://svn.r-project.org/R/trunk@22976 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/mapply.R
+++ src/library/base/R/mapply.R
@@ -1,24 +1,23 @@
-mapply<-function(FUN,..., MoreArgs=NULL, SIMPLIFY=TRUE, USE.NAMES=TRUE){
+mapply<-function(FUN,..., MoreArgs=NULL, SIMPLIFY=TRUE, USE.NAMES=TRUE)
+{
     FUN <- match.fun(FUN)
-    dots<-list(...)
-    
-    answer<-.Call("do_mapply", FUN, dots,  MoreArgs, environment())
+    dots <- list(...)
 
-    if (USE.NAMES && length(dots) && is.character(dots[[1]]) && is.null(names(answer)))
-        names(answer) <- dots[[1]]
+    answer<-.Call("do_mapply", FUN, dots, MoreArgs, environment(),
+                  PACKAGE="base")
+
+    if (USE.NAMES && length(dots) && is.character(dots[[1]]) &&
+        is.null(names(answer))) names(answer) <- dots[[1]]
     if (SIMPLIFY && length(answer) &&
         length(common.len <- unique(unlist(lapply(answer, length)))) == 1) {
-      if (common.len == 1)
+        if (common.len == 1)
             unlist(answer, recursive = FALSE)
         else if (common.len > 1)
-            array(unlist(answer, recursive = FALSE), dim = c(common.len,
-                max(sapply(dots,length))), dimnames = list(names(answer[[1]]),
-                names(answer)))
+            array(unlist(answer, recursive = FALSE),
+                  dim = c(common.len, max(sapply(dots,length))),
+                  dimnames = list(names(answer[[1]]), names(answer)))
         else answer
     }
     else answer
-
-    
-    
 }
 
