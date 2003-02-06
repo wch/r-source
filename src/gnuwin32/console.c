0e212069d3c4b7dbd1490b2e423ef5dd64c59636
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-02-06 08:26:35 +0000

    add page up/down shortcut keys to the internal pager
    git-svn-id: https://svn.r-project.org/R/trunk@23057 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/console.c
+++ src/gnuwin32/console.c
@@ -761,6 +761,10 @@ FBEGIN
     if (st == -1) return;
     if (p->kind == PAGER) {
 	if(k == 'q' || k == 'Q') pagerbclose(c);
+	if(k == ' ') setfirstvisible(c, NEWFV + ROWS);
+	if(k == '-') setfirstvisible(c, NEWFV - ROWS);
+	if(k == 'F' - 'A' + 1) setfirstvisible(c, NEWFV + ROWS);
+	if(k == 'B' - 'A' + 1) setfirstvisible(c, NEWFV - ROWS);
 	return;
     }
     storekey(c, k);
