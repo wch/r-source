4cc7e285a07cafbffda21b13db038be179bb3942
bates <bates@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-02-06 16:37:51 +0000

    datax argument added to qqnorm and qqline
    git-svn-id: https://svn.r-project.org/R/trunk@23061 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/qqnorm.R
+++ src/library/base/R/qqnorm.R
@@ -3,24 +3,32 @@ qqnorm <- function(y, ...) UseMethod("qqnorm")
 qqnorm.default <-
     function(y, ylim, main="Normal Q-Q Plot",
 	     xlab="Theoretical Quantiles", ylab="Sample Quantiles",
-	     plot.it=TRUE, ...)
+	     plot.it=TRUE, datax = FALSE, ...)
 {
     y <- y[!is.na(y)]
     if(0 == (n <- length(y))) stop("y is empty")
     if (missing(ylim)) ylim <- range(y)
     x <- qnorm(ppoints(n))[order(order(y))]
     if(plot.it)
-	plot(x, y, main= main, xlab= xlab, ylab= ylab, ylim= ylim, ...)
-    invisible(list(x = x, y = y))
+        if (datax)
+            plot(y, x, main= main, xlab= ylab, ylab=xlab, xlim = ylim, ...)
+        else
+            plot(x, y, main= main, xlab= xlab, ylab= ylab, ylim= ylim, ...)
+    invisible(if(datax) list(x = y, y = x) else list(x = x, y = y))
 }
 
 ## Splus also has qqnorm.aov(), qqnorm.aovlist(), qqnorm.maov() ...
 
-qqline <- function(y, ...)
+qqline <- function(y, datax = FALSE, ...)
 {
     y <- quantile(y[!is.na(y)],c(0.25, 0.75))
     x <- qnorm(c(0.25, 0.75))
-    slope <- diff(y)/diff(x)
-    int <- y[1]-slope*x[1]
+    if (datax) {
+        slope <- diff(x)/diff(y)
+        int <- x[1] - slope*y[1]
+    } else {
+        slope <- diff(y)/diff(x)
+        int <- y[1]-slope*x[1]
+    }
     abline(int, slope, ...)
 }
