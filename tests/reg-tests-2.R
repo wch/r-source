2f5aa2d9a1602ed35abf15a452db46532a3163e0
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-02-07 13:08:23 +0000

    move PR390 test to reg-test-2, sinc the postscript differs slightly on Solaris
    git-svn-id: https://svn.r-project.org/R/trunk@23074 00db46b3-68df-0310-9c12-caf00c1e9a41

--- tests/reg-tests-2.R
+++ tests/reg-tests-2.R
@@ -745,6 +745,50 @@ TEMP[,c(1,3)][TEMP[,c(1,3)]==1 & !is.na(TEMP[,c(1,3)])] < -10
 TEMP
 ##
 
+## moved from reg-plot.R as exact output depends on rounding error
+## PR 390 (axis for small ranges)
+
+relrange <- function(x) {
+    ## The relative range in EPS units
+    r <- range(x)
+    diff(r)/max(abs(r))/.Machine$double.eps
+}
+
+x <- c(0.12345678912345678,
+       0.12345678912345679,
+       0.12345678912345676)
+relrange(x) ## 1.0125
+plot(x) # `extra horizontal' ;  +- ok on Solaris; label off on Linux
+
+y <- c(0.9999563255363383973418,
+       0.9999563255363389524533,
+       0.9999563255363382863194)
+## The relative range number:
+relrange(y) ## 3.000131
+plot(y)# once gave infinite loop on Solaris [TL];  y-axis too long
+
+## Comments: The whole issue was finally deferred to main/graphics.c l.1944
+##    error("relative range of values is too small to compute accurately");
+## which is not okay.
+
+set.seed(101)
+par(mfrow = c(3,3))
+for(j.fac in 1e-12* c(10, 1, .7, .3, .2, .1, .05, .03, .01)) {
+##           ====
+    #set.seed(101) # or don't
+    x <- pi + jitter(numeric(101), f = j.fac)
+    rrtxt <- paste("rel.range =", formatC(relrange(x), dig = 4),"* EPS")
+    cat("j.f = ", format(j.fac)," ;  ", rrtxt,"\n",sep="")
+    plot(x, type = "l", main = rrtxt)
+    cat("par(\"usr\")[3:4]:", formatC(par("usr")[3:4], wid = 10),"\n",
+        "par(\"yaxp\") :   ", formatC(par("yaxp"), wid = 10),"\n\n", sep="")
+}
+par(mfrow = c(1,1))
+## The warnings from inside GScale() will differ in their  relrange() ...
+## >> do sloppy testing
+## 2003-02-03 hopefully no more.  BDR
+## end of PR 390
+
 
 ## print/show dispatch
 hasMethods <- .isMethodsDispatchOn()
