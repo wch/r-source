c6195424c2450d3cfd366a54b5af8a137b6816a4
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-03-15 17:13:43 +0000

    table(*, exclude=NULL) for factors {fix to M.Friendly's improvement}
    git-svn-id: https://svn.r-project.org/R/trunk@23654 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/table.R
+++ src/library/base/R/table.R
@@ -1,25 +1,22 @@
 table <- function (..., exclude = c(NA, NaN),
-   dnn = list.names(...), deparse.level = 1)
+		   dnn = list.names(...), deparse.level = 1)
 {
     list.names <- function(...) {
-        l <- as.list(substitute(list(...)))[-1]
-        nm <- names(l)
-        fixup <- if (is.null(nm))
-            seq(along = l)
-        else nm == ""
-        dep <- sapply(l[fixup], function(x)
+	l <- as.list(substitute(list(...)))[-1]
+	nm <- names(l)
+	fixup <- if (is.null(nm)) seq(along = l) else nm == ""
+	dep <- sapply(l[fixup], function(x)
 	    switch (deparse.level + 1,
-		"",
-		if (is.symbol(x)) as.character(x) else "",
-		deparse(x)[1]
-	    )
-        )
-        if (is.null(nm))
-            dep
-        else {
-            nm[fixup] <- dep
-            nm
-        }
+		    "", ## 0
+		    if (is.symbol(x)) as.character(x) else "", ## 1
+		    deparse(x)[1]) ## 2
+		      )
+	if (is.null(nm))
+	    dep
+	else {
+	    nm[fixup] <- dep
+	    nm
+	}
     }
 
     args <- list(...)
@@ -29,9 +26,9 @@ table <- function (..., exclude = c(NA, NaN),
 	args <- args[[1]]
 	if (length(dnn) != length(args))
 	    dnn <- if (!is.null(argn <- names(args)))
-	         argn
+		 argn
 	    else
-                 paste(dnn[1],1:length(args),sep='.')
+		 paste(dnn[1], 1:length(args), sep = '.')
     }
     bin <- 0
     lens <- NULL
@@ -42,15 +39,15 @@ table <- function (..., exclude = c(NA, NaN),
 	if (is.null(lens)) lens <- length(a)
 	else if (length(a) != lens)
 	    stop("all arguments must have the same length")
-	if (is.factor(a)){
-            levels<-levels(a)
-            levels<-levels[!(levels %in% exclude)]
-            cat <- factor(a,levels=levels)
-	}else
-	    cat <- factor(a, exclude = exclude)
-	nl <- length(l <- levels(cat))
+	if (is.factor(a)) { ## R 1.7.0: allow `excludes' also for factor arg
+	    ll <- levels(a)
+	    cat <- factor(a, levels = ll[!(ll %in% exclude)],
+			  exclude = if(is.null(exclude)) NULL else NA)
+	} else
+	cat <- factor(a, exclude = exclude)
+	nl <- length(ll <- levels(cat))
 	dims <- c(dims, nl)
-	dn <- c(dn, list(l))
+	dn <- c(dn, list(ll))
 	## requiring   all(unique(as.integer(cat)) == 1:nlevels(cat))  :
 	bin <- bin + pd * (as.integer(cat) - 1)
 	pd <- pd * nl
@@ -63,11 +60,12 @@ table <- function (..., exclude = c(NA, NaN),
     y
 }
 
-print.table <- function(x, digits = getOption("digits"), quote = FALSE,
-                        na.print = "", ...)
+print.table <-
+					# na.print = "" (why is this good)?
+function(x, digits = getOption("digits"), quote = FALSE, na.print = "", ...)
 {
     print.default(unclass(x), digits = digits, quote = quote,
-                  na.print = na.print, ...)
+		  na.print = na.print, ...)
 }
 
 
@@ -80,21 +78,19 @@ summary.table <- function(object, ...)
     y <- list(n.vars = n.vars,
 	      n.cases = n.cases)
     if(n.vars > 1) {
-        m <- vector("list", length = n.vars)
-        relFreqs <- object / n.cases
-        for(k in seq(along = m)) {
-            m[[k]] <- apply(relFreqs, k, sum)
-        }
-        expected <- apply(do.call("expand.grid", m), 1, prod) * n.cases
-        statistic <- sum((c(object) - expected)^2 / expected)
-        parameter <-
-            prod(sapply(m, length)) - 1 - sum(sapply(m, length) - 1)
-        y <- c(y, list(statistic = statistic,
-                       parameter = parameter,
-                       approx.ok = all(expected >= 5),
-                       p.value = pchisq(statistic, parameter,
-                       lower.tail = FALSE),
-                       call = attr(object, "call")))
+	m <- vector("list", length = n.vars)
+	relFreqs <- object / n.cases
+	for(k in 1:n.vars)
+	    m[[k]] <- apply(relFreqs, k, sum)
+	expected <- apply(do.call("expand.grid", m), 1, prod) * n.cases
+	statistic <- sum((c(object) - expected)^2 / expected)
+	parameter <-
+	    prod(sapply(m, length)) - 1 - sum(sapply(m, length) - 1)
+	y <- c(y, list(statistic = statistic,
+		       parameter = parameter,
+		       approx.ok = all(expected >= 5),
+		       p.value = pchisq(statistic, parameter, lower.tail=FALSE),
+		       call = attr(object, "call")))
     }
     class(y) <- "summary.table"
     y
@@ -106,22 +102,19 @@ function(x, digits = max(1, getOption("digits") - 3), ...)
     if(!inherits(x, "summary.table"))
 	stop("x must inherit from class `summary.table'")
     if(!is.null(x$call)) {
-        cat("Call: "); print(x$call)
+	cat("Call: "); print(x$call)
     }
     cat("Number of cases in table:", x$n.cases, "\n")
     cat("Number of factors:", x$n.vars, "\n")
     if(x$n.vars > 1) {
-        cat("Test for independence of all factors:\n")
-        ch <- x$statistic
-        cat("\tChisq = ",
-            format(round(ch, max(0, digits - log10(ch)))),
-            ", df = ",
-            x$parameter,
-            ", p-value = ",
-            format.pval(x$p.value, digits, eps = 0),
-            "\n", sep = "")
-        if(!x$approx.ok)
-            cat("\tChi-squared approximation may be incorrect\n")
+	cat("Test for independence of all factors:\n")
+	ch <- x$statistic
+	cat("\tChisq = ",	format(round(ch, max(0, digits - log10(ch)))),
+	    ", df = ",		x$parameter,
+	    ", p-value = ",	format.pval(x$p.value, digits, eps = 0),
+	    "\n", sep = "")
+	if(!x$approx.ok)
+	    cat("\tChi-squared approximation may be incorrect\n")
     }
     invisible(x)
 }
@@ -130,7 +123,7 @@ as.data.frame.table <- function(x, row.names = NULL, optional = FALSE, ...)
 {
     x <- as.table(x)
     data.frame(do.call("expand.grid", dimnames(x)), Freq = c(x),
-               row.names = row.names)
+	       row.names = row.names)
 }
 
 is.table <- function(x) inherits(x, "table")
@@ -138,40 +131,40 @@ as.table <- function(x, ...) UseMethod("as.table")
 as.table.default <- function(x, ...)
 {
     if(is.table(x))
-        return(x)
+	return(x)
     else if(is.array(x) || is.numeric(x)) {
-        x <- as.array(x)
-        if(any(dim(x) == 0))
-            stop("cannot coerce into a table")
-        ## Try providing dimnames where missing.
-        dnx <- dimnames(x)
-        if(is.null(dnx))
-            dnx <- vector("list", length(dim(x)))
-        for(i in which(sapply(dnx, is.null)))
-            dnx[[i]] <- LETTERS[seq(length = dim(x)[i])]
-        dimnames(x) <- dnx
-        class(x) <- c("table", oldClass(x))
-        return(x)
+	x <- as.array(x)
+	if(any(dim(x) == 0))
+	    stop("cannot coerce into a table")
+	## Try providing dimnames where missing.
+	dnx <- dimnames(x)
+	if(is.null(dnx))
+	    dnx <- vector("list", length(dim(x)))
+	for(i in which(sapply(dnx, is.null)))
+	    dnx[[i]] <- LETTERS[seq(length = dim(x)[i])]
+	dimnames(x) <- dnx
+	class(x) <- c("table", oldClass(x))
+	return(x)
     }
     else
-        stop("cannot coerce into a table")
+	stop("cannot coerce into a table")
 }
 
 prop.table <- function(x, margin = NULL)
 {
     if(length(margin))
-        sweep(x, margin, margin.table(x, margin), "/")
+	sweep(x, margin, margin.table(x, margin), "/")
     else
-    	x / sum(x)
+	x / sum(x)
 }
 
 margin.table <- function(x, margin = NULL)
 {
     if(!is.array(x)) stop("x is not an array")
     if (length(margin)) {
-        z <- apply(x, margin, sum)
-        dim(z) <- dim(x)[margin]
-        dimnames(z) <- dimnames(x)[margin]
+	z <- apply(x, margin, sum)
+	dim(z) <- dim(x)[margin]
+	dimnames(z) <- dimnames(x)[margin]
     }
     else return(sum(x))
     class(z) <- oldClass(x) # avoid adding "matrix"
@@ -180,16 +173,16 @@ margin.table <- function(x, margin = NULL)
 
 r2dtable <- function(n, r, c) {
     if(length(n) == 0 || (n < 0) || is.na(n))
-        stop("invalid argument 'n'")
+	stop("invalid argument 'n'")
     if((length(r) <= 1) || any(r < 0) || any(is.na(r)))
-        stop("invalid argument 'r'")
+	stop("invalid argument 'r'")
     if((length(c) <= 1) || any(c < 0) || any(is.na(c)))
-        stop("invalid argument 'c'")
+	stop("invalid argument 'c'")
     if(sum(r) != sum(c))
-        stop("arguments 'r' and 'c' must have the same sums")
+	stop("arguments 'r' and 'c' must have the same sums")
     .Call("R_r2dtable",
-          as.integer(n),
-          as.integer(r),
-          as.integer(c),
-          PACKAGE = "base")
+	  as.integer(n),
+	  as.integer(r),
+	  as.integer(c),
+	  PACKAGE = "base")
 }
