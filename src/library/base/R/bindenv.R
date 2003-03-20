eb4b51a138dd5c3a71e929f1f2f8daddbf892c86
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-03-20 07:17:31 +0000

    add unlockBinding
    git-svn-id: https://svn.r-project.org/R/trunk@23710 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/bindenv.R
+++ src/library/base/R/bindenv.R
@@ -23,3 +23,8 @@ bindingIsActive <- function(sym, env) {
     if (is.character(sym)) sym <- as.name(sym)
     .Internal(bindingIsActive(sym, env))
 }
+
+unlockBinding <- function(sym, env) {
+    if (is.character(sym)) sym <- as.name(sym)
+    .Internal(unlockBinding(sym, env))
+}
