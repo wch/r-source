c2e9fe52f29b5ad075109294a19803794741de6d
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-03-27 08:16:00 +0000

    use local() in a couple of menus
    git-svn-id: https://svn.r-project.org/R/trunk@23787 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/rui.c
+++ src/gnuwin32/rui.c
@@ -315,14 +315,14 @@ static void menupkgupdatebioc(control m)
 static void menupkginstallbioc(control m) {
     if (!ConsoleAcceptCmd) return;
     consolecmd(RConsole,
-	       "{a<- CRAN.packages(CRAN=getOption(\"BIOC\"))\ninstall.packages(select.list(a[,1],,TRUE), .libPaths()[1], available=a, CRAN=getOption(\"BIOC\"))}");
+	       "local({a<- CRAN.packages(CRAN=getOption(\"BIOC\"))\ninstall.packages(select.list(a[,1],,TRUE), .libPaths()[1], available=a, CRAN=getOption(\"BIOC\"))})");
 }
 
 static void menupkginstallcran(control m)
 {
     if (!ConsoleAcceptCmd) return;
     consolecmd(RConsole,
-	       "{a <- CRAN.packages()\ninstall.packages(select.list(a[,1],,TRUE), .libPaths()[1], available=a)}");
+	       "local({a <- CRAN.packages()\ninstall.packages(select.list(a[,1],,TRUE), .libPaths()[1], available=a)})");
 /*    show(RConsole); */
 }
 
