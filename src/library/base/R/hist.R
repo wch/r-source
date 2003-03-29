3ebff43219d3903a4c663b4e19cd114dfe336a40
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-03-29 21:12:19 +0000

    use $density, not deprecated $intensities (PR#2696)
    git-svn-id: https://svn.r-project.org/R/trunk@23811 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/hist.R
+++ src/library/base/R/hist.R
@@ -2,53 +2,53 @@ hist <- function(x, ...) UseMethod("hist")
 
 hist.default <-
     function (x, breaks = "Sturges", freq = NULL,
-              probability = !freq, include.lowest= TRUE,
-              right= TRUE, density = NULL, angle = 45,
-              col = NULL, border = NULL,
-              main = paste("Histogram of" , xname),
-              xlim = range(breaks), ylim = NULL,
-              xlab = xname, ylab,
-              axes = TRUE, plot = TRUE, labels = FALSE, nclass = NULL, ...)
+	      probability = !freq, include.lowest= TRUE,
+	      right= TRUE, density = NULL, angle = 45,
+	      col = NULL, border = NULL,
+	      main = paste("Histogram of" , xname),
+	      xlim = range(breaks), ylim = NULL,
+	      xlab = xname, ylab,
+	      axes = TRUE, plot = TRUE, labels = FALSE, nclass = NULL, ...)
 {
     if (!is.numeric(x))
-        stop("`x' must be numeric")
+	stop("`x' must be numeric")
     xname <- deparse(substitute(x))
     n <- length(x <- x[!is.na(x)])
     use.br <- !missing(breaks)
     if(use.br) {
-        if(!missing(nclass))
-            warning("`nclass' not used when `breaks' specified")
+	if(!missing(nclass))
+	    warning("`nclass' not used when `breaks' specified")
     }
     else if(!is.null(nclass) && length(nclass) == 1)
-        breaks <- nclass
+	breaks <- nclass
     use.br <- use.br && (nB <- length(breaks)) > 1
     if(use.br)
-        breaks <- sort(breaks)
-    else {                              # construct vector of breaks
-        if(!include.lowest) {
-            include.lowest <- TRUE
-            warning("include.lowest ignored as `breaks' is not a vector")
-        }
-        if(is.character(breaks)) {
-            breaks <- match.arg(tolower(breaks),
-                                c("sturges", "fd",
-                                  "freedman-diaconis", "scott"))
-            breaks <- switch(breaks,
-                             sturges = nclass.Sturges(x),
-                             "freedman-diaconis" =,
-                             fd = nclass.FD(x),
-                             scott = nclass.scott(x),
-                             stop("Unknown breaks algorithm"))
-        } else if(is.function(breaks)) {
-            breaks <- breaks(x)
-        }
-        if(!is.numeric(breaks) || is.na(breaks) || breaks < 2)
-            stop("invalid number of breaks")
-        breaks <- pretty (range(x), n = breaks, min.n = 1)
-        nB <- length(breaks)
-        if(nB <= 1) ##-- Impossible !
-            stop(paste("hist.default: pretty() error, breaks=",
-                       format(breaks)))
+	breaks <- sort(breaks)
+    else {				# construct vector of breaks
+	if(!include.lowest) {
+	    include.lowest <- TRUE
+	    warning("include.lowest ignored as `breaks' is not a vector")
+	}
+	if(is.character(breaks)) {
+	    breaks <- match.arg(tolower(breaks),
+				c("sturges", "fd",
+				  "freedman-diaconis", "scott"))
+	    breaks <- switch(breaks,
+			     sturges = nclass.Sturges(x),
+			     "freedman-diaconis" =,
+			     fd = nclass.FD(x),
+			     scott = nclass.scott(x),
+			     stop("Unknown breaks algorithm"))
+	} else if(is.function(breaks)) {
+	    breaks <- breaks(x)
+	}
+	if(!is.numeric(breaks) || is.na(breaks) || breaks < 2)
+	    stop("invalid number of breaks")
+	breaks <- pretty (range(x), n = breaks, min.n = 1)
+	nB <- length(breaks)
+	if(nB <= 1) ##-- Impossible !
+	    stop(paste("hist.default: pretty() error, breaks=",
+		       format(breaks)))
     }
 
     ## Do this *before* adding fuzz or logic breaks down...
@@ -56,11 +56,11 @@ hist.default <-
     h <- diff(breaks)
     equidist <- !use.br || diff(range(h)) < 1e-7 * mean(h)
     if (!use.br && any(h <= 0))
-        stop("not strictly increasing `breaks'.")
+	stop("not strictly increasing `breaks'.")
     if (is.null(freq)) {
-        freq <- if(!missing(probability)) !as.logical(probability) else equidist
+	freq <- if(!missing(probability)) !as.logical(probability) else equidist
     } else if(!missing(probability) && any(probability == freq))
-        stop("`probability' is an alias for `!freq', however they differ.")
+	stop("`probability' is an alias for `!freq', however they differ.")
 
     ## Fuzz to handle cases where points are "effectively on"
     ## the boundaries
@@ -80,75 +80,77 @@ hist.default <-
     ## With the fuzz adjustment above, the "right" and "include"
     ## arguments are really irrelevant
     counts <- .C("bincount",
-                 x,
-                 n,
-                 fuzzybreaks,
-                 nB,
-                 counts = integer(nB - 1),
-                 right = as.logical(right),
-                 include= as.logical(include.lowest), naok = FALSE,
-                 NAOK = FALSE, DUP = FALSE, PACKAGE = "base") $counts
+		 x,
+		 n,
+		 fuzzybreaks,
+		 nB,
+		 counts = integer(nB - 1),
+		 right = as.logical(right),
+		 include= as.logical(include.lowest), naok = FALSE,
+		 NAOK = FALSE, DUP = FALSE, PACKAGE = "base") $counts
     if (any(counts < 0))
-        stop("negative `counts'. Internal Error in C-code for \"bincount\"")
+	stop("negative `counts'. Internal Error in C-code for \"bincount\"")
     if (sum(counts) < n)
-        stop("some `x' not counted; maybe `breaks' do not span range of `x'")
+	stop("some `x' not counted; maybe `breaks' do not span range of `x'")
     dens <- counts/(n*h)
     mids <- 0.5 * (breaks[-1] + breaks[-nB])
     r <- structure(list(breaks = breaks, counts = counts,
-                        intensities = dens,
+			intensities = dens,
 			density = dens, mids = mids,
-                        xname = xname, equidist = equidist),
-                   class="histogram")
+			xname = xname, equidist = equidist),
+		   class="histogram")
     if (plot) {
-        plot(r, freq = freq, col = col, border = border,
-             angle = angle, density = density,
-             main = main, xlim = xlim, ylim = ylim, xlab = xlab, ylab = ylab,
-             axes = axes, labels = labels, ...)
-        invisible(r)
+	plot(r, freq = freq, col = col, border = border,
+	     angle = angle, density = density,
+	     main = main, xlim = xlim, ylim = ylim, xlab = xlab, ylab = ylab,
+	     axes = axes, labels = labels, ...)
+	invisible(r)
     }
     else r
 }
 
 plot.histogram <-
     function (x, freq = equidist, density = NULL, angle = 45,
-              col = NULL, border = par("fg"), lty = NULL,
-              main = paste("Histogram of", x$xname), sub = NULL,
-              xlab = x$xname, ylab,
-              xlim = range(x$breaks), ylim = NULL,
-              axes = TRUE, labels = FALSE, add = FALSE, ...)
+	      col = NULL, border = par("fg"), lty = NULL,
+	      main = paste("Histogram of", x$xname), sub = NULL,
+	      xlab = x$xname, ylab,
+	      xlim = range(x$breaks), ylim = NULL,
+	      axes = TRUE, labels = FALSE, add = FALSE, ...)
 {
     equidist <-
-        if(is.logical(x$equidist)) x$equidist
-        else { h <- diff(x$breaks) ; diff(range(h)) < 1e-7 * mean(h) }
+	if(is.logical(x$equidist)) x$equidist
+	else { h <- diff(x$breaks) ; diff(range(h)) < 1e-7 * mean(h) }
     if(freq && !equidist)
-        warning("the AREAS in the plot are wrong -- rather use `freq=FALSE'!")
+	warning("the AREAS in the plot are wrong -- rather use `freq=FALSE'!")
 
-    y <- if (freq) x$counts else x$intensities
+    y <- if (freq) x$counts else { ## x$density -- would be enough, but
+	## for back compatibility
+	y <- x$density; if(is.null(y)) x$intensities else y}
     nB <- length(x$breaks)
     if(is.null(y) || 0 == nB) stop("`x' is wrongly structured")
 
     if(!add) {
-        if(is.null(ylim))
-            ylim <- range(y, 0)
-        if (missing(ylab))
-            ylab <- if (!freq) "Density" else "Frequency"
-        plot.new()
-        plot.window(xlim, ylim, "")     #-> ylim's default from 'y'
-        title(main = main, sub = sub, xlab = xlab, ylab = ylab, ...)
-        if(axes) {
-            axis(1, ...)
-            axis(2, ...)
-        }
+	if(is.null(ylim))
+	    ylim <- range(y, 0)
+	if (missing(ylab))
+	    ylab <- if (!freq) "Density" else "Frequency"
+	plot.new()
+	plot.window(xlim, ylim, "")	#-> ylim's default from 'y'
+	title(main = main, sub = sub, xlab = xlab, ylab = ylab, ...)
+	if(axes) {
+	    axis(1, ...)
+	    axis(2, ...)
+	}
     }
     rect(x$breaks[-nB], 0, x$breaks[-1], y,
-         col = col, border = border,
-         angle = angle, density = density, lty = lty)
+	 col = col, border = border,
+	 angle = angle, density = density, lty = lty)
     if((logl <- is.logical(labels) && labels) || is.character(labels))
-        text(x$mids, y,
-             labels = if(logl) {
-                 if(freq) x$counts else round(x$density,3)
-             } else labels,
-             adj = c(0.5, -0.5))
+	text(x$mids, y,
+	     labels = if(logl) {
+		 if(freq) x$counts else round(x$density,3)
+	     } else labels,
+	     adj = c(0.5, -0.5))
     invisible()
 }
 
