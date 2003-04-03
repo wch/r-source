cd209e56873239f2abdc16498ff8e85d03ed54d5
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-04-03 17:31:54 +0000

    Second attempt of getting better T/F error messages: assign a delayed stop(), but in .CheckExEnv as suggested by BDR.
    git-svn-id: https://svn.r-project.org/R/trunk@23867 00db46b3-68df-0310-9c12-caf00c1e9a41

--- share/perl/massage-Examples.pl
+++ share/perl/massage-Examples.pl
@@ -72,8 +72,10 @@ _EOF_
 if(!defined($ENV{'R_CHECK_WITH_T_N_F_AS_NULL'})
    || $ENV{'R_CHECK_WITH_T_N_F_AS_NULL'} ne "") {
     print <<_EOF_;
-	   assign("T", NULL, pos = 1)
-	   assign("F", NULL, pos = 1)
+	   assign("T", delay(stop("T used instead of TRUE")),
+		  pos = .CheckExEnv)
+	   assign("F", delay(stop("F used instead of FALSE")),
+		  pos = .CheckExEnv)
 _EOF_
 }
 print <<_EOF_;
