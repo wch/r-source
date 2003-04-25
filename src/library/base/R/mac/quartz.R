2257f486af60047bdcde273fa8be7bcbc92ace99
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-04-25 14:26:42 +0000

    remove more classic MacOS files
    git-svn-id: https://svn.r-project.org/R/trunk@24061 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/mac/quartz.R
+++
@@ -1,4 +0,0 @@
-quartz <- function (display = "", width = 5, height = 5, pointsize = 12, 
-                    family="Helvetica", antialias = TRUE, autorefresh = TRUE)
-.Internal(Quartz(display, width, height, pointsize,family, antialias,autorefresh))
-
