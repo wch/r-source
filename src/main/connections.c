5b9868440fc717f674ea83108c78a4a2e4c1e5ac
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-04-25 20:48:56 +0000

    r+/r+b connections were read-only, erroneously
    git-svn-id: https://svn.r-project.org/R/branches/R-1-7-patches@24076 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/connections.c
+++ src/main/connections.c
@@ -242,7 +242,8 @@ static Rboolean file_open(Rconnection con)
     con->isopen = TRUE;
     con->canwrite = (con->mode[0] == 'w' || con->mode[0] == 'a');
     con->canread = !con->canwrite;
-    if(mlen >= 2 && con->mode[1] == '+') con->canread = TRUE;
+    if(mlen >= 2 && con->mode[1] == '+')
+	con->canread = con->canwrite = TRUE;
     this->last_was_write = !con->canread;
     this->rpos = 0;
     if(con->canwrite) this->wpos = ftell(fp);
