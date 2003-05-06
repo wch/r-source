cdaaee5a4952a24c9af16458f1d2de7ce068f10b
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-05-06 19:11:47 +0000

    document range of pointsize, and up limit to 48
    git-svn-id: https://svn.r-project.org/R/branches/R-1-7-patches@24195 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/devga.c
+++ src/gnuwin32/devga.c
@@ -2215,7 +2215,7 @@ Rboolean GADeviceDriver(NewDevDesc *dd, char *display, double width,
     /* Font will load at first use  */
 
     ps = pointsize;
-    if (ps < 6 || ps > 24)
+    if (ps < 6 || ps > 48)
 	ps = 12;
     ps = 2 * (ps / 2);
     xd->fontface = -1;
