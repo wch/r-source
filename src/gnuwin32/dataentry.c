df368adf644d27c1ada0963aeaf6906a238bc3d0
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-05-11 16:44:33 +0000

    re-enable MDI toolbar
    git-svn-id: https://svn.r-project.org/R/branches/R-1-7-patches@24275 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/dataentry.c
+++ src/gnuwin32/dataentry.c
@@ -1542,6 +1542,8 @@ static void depopupact(control m)
 
 #define MCHECK(a) if (!(a)) {del(c);return NULL;}
 
+RECT *RgetMDIsize(); /* in rui.c */
+
 static dataeditor newdataeditor()
 {
     ConsoleData p;
@@ -1557,18 +1559,14 @@ static dataeditor newdataeditor()
 
     w = WIDTH ;
     h = HEIGHT;
-#ifdef USE_MDI
     if (ismdi()) {
 	RECT *pR = RgetMDIsize();
 	x = (pR->right - w) / 3; x = x > 20 ? x:20;
 	y = (pR->bottom - h) / 3; y = y > 20 ? y:20;
     } else {
-#endif
 	x = (devicewidth(NULL) - w) / 3;
 	y = (deviceheight(NULL) - h) / 3 ;
-#ifdef USE_MDI
     }
-#endif
     c = (dataeditor) newwindow(" Data Editor", rect(x, y, w, h),
 			       Document | StandardWindow | Menubar |
 			       VScrollbar | HScrollbar | TrackMouse);
@@ -1585,7 +1583,6 @@ static dataeditor newdataeditor()
     BORDERY = (HEIGHT - ROWS*FH) / 2;
     gsetcursor(c, ArrowCursor);
     setbackground(c, consolebg);
-#ifdef USE_MDI
     if (ismdi() && (RguiMDI & RW_TOOLBAR)) {
 	/* blank toolbar to stop windows jumping around */
         int btsize = 24;
@@ -1594,15 +1591,12 @@ static dataeditor newdataeditor()
         MCHECK(tb = newtoolbar(btsize + 4));
 	gsetcursor(tb, ArrowCursor);
     }
-#endif
     MCHECK(gpopup(depopupact, DePopup));
     MCHECK(m = newmenubar(demenuact));
     MCHECK(newmenu("File"));
 /*    MCHECK(m = newmenuitem("-", 0, NULL));*/
     MCHECK(m = newmenuitem("Close", 0, declose));
-#ifdef USE_MDI
     newmdimenu();
-#endif
     MCHECK(newmenu("Edit"));
     MCHECK(m = newmenuitem("Copy  \tCTRL+C", 0, de_copy));
     MCHECK(m = newmenuitem("Paste \tCTRL+V", 0, de_paste));
