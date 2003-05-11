83363a15d60ba7eafca0899aba5fb23f1f30010c
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-05-11 11:26:37 +0000

    Have get_exclude_patterns() include old-style data and vignette indices.
    git-svn-id: https://svn.r-project.org/R/trunk@24267 00db46b3-68df-0310-9c12-caf00c1e9a41

--- share/perl/R/Utils.pm
+++ share/perl/R/Utils.pm
@@ -145,15 +145,14 @@ sub get_exclude_patterns {
     ## Return list of file patterns excluded by R CMD build and check.
     ## Kept here so that we ensure that the lists are in sync, but not
     ## exported.
+    ## <FIXME>
+    ## Has Unix-style '/' path separators hard-coded.
     my @exclude_patterns = ("^.Rbuildignore\$", "^.DS_Store\$",
 			    "\~\$", "\\.swp\$", "\\.bak\$",
 			    "^.*/\\.#[^/]*\$", "^.*/#[^/]*#\$",
-			    "^TITLE\$");
-    ## <FIXME>
-    ## Add
-    ##   "^data/00Index\$"
-    ##   "^inst/doc/00Index.dcf\$"
-    ## once 1.7 is out ...
+			    "^TITLE\$", "^data/00Index\$",
+			    "^inst/doc/00Index.dcf\$"
+			    );
     ## </FIXME>
     @exclude_patterns;
 }
