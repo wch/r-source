c2bd4d57de9ba42a2234cfba22514f03ab392778
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-05-27 18:29:05 +0000

    add a namespace
    git-svn-id: https://svn.r-project.org/R/trunk@24486 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tcltk/R/unix/zzz.R
+++ src/library/tcltk/R/unix/zzz.R
@@ -1,4 +1,4 @@
-.First.lib <- function(lib, pkg) {
+.onLoad <- function(lib, pkg) {
     ## Use local=FALSE to allow easy loading of Tcl extensions
     library.dynam("tcltk", pkg, lib, local=FALSE)
     .C("tcltk_init", PACKAGE="tcltk")
@@ -9,7 +9,7 @@
 ###    options(pager=if (is.null(userpager)) tkpager else userpager)
 }
 
-.Last.lib <- function(libpath) {
+.onUnload <- function(libpath) {
 ###    options(pager=NULL)
     .C("delTcl", PACKAGE="tcltk")
 ###    dyn.unload(file.path(libpath, "libs",
--- src/library/tcltk/R/windows/zzz.R
+++ src/library/tcltk/R/windows/zzz.R
@@ -1,4 +1,4 @@
-.First.lib <- function(lib, pkg)
+.onLoad <- function(lib, pkg)
 {
     if(nchar(Sys.getenv("MY_TCLTK"))) {
         library.dynam("tcltk", pkg, lib)
@@ -19,16 +19,10 @@
     invisible(addTclPath(extra))
 }
 
-.Last.lib <- function(libpath) {
+.onUnload <- function(libpath) {
     if(is.loaded(symbol.C("tcltk_end"))) {
         .C("tcltk_end", PACKAGE="tcltk")
         dyn.unload(file.path(libpath, "libs", "tcltk.dll"))
-        ## <NOTE>
-        ## Versions of R prior to 1.4.0 had .Dyn.libs in .AutoloadEnv
-        ## (and did not always ensure getting it from there).
-        ## Until 1.6.0, we consistently used the base environment.
-        ## Now we have a dynamic variable instead.
-        ## </NOTE>
         .Dyn.libs <- .dynLibs()
         num <- match("tcltk", .Dyn.libs)
         .dynLibs(.Dyn.libs[-num])
