ef2320853593b7033b90b64b2a8ad0a79aedf480
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-05-28 13:57:29 +0000

    looks like Tcl/Tk 8.4.1 doesn't like to be re-initialized, so don't unload
    git-svn-id: https://svn.r-project.org/R/branches/R-1-7-patches@24505 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tcltk/R/windows/zzz.R
+++ src/library/tcltk/R/windows/zzz.R
@@ -22,15 +22,11 @@
 .Last.lib <- function(libpath) {
     if(is.loaded(symbol.C("tcltk_end"))) {
         .C("tcltk_end", PACKAGE="tcltk")
-        dyn.unload(file.path(libpath, "libs", "tcltk.dll"))
-        ## <NOTE>
-        ## Versions of R prior to 1.4.0 had .Dyn.libs in .AutoloadEnv
-        ## (and did not always ensure getting it from there).
-        ## Until 1.6.0, we consistently used the base environment.
-        ## Now we have a dynamic variable instead.
-        ## </NOTE>
-        .Dyn.libs <- .dynLibs()
-        num <- match("tcltk", .Dyn.libs)
-        .dynLibs(.Dyn.libs[-num])
+## unloading the DLL used to work, but it seems Tcl/Tk 8.4.1 does
+## not like being reinitialized
+#         dyn.unload(file.path(libpath, "libs", "tcltk.dll"))
+#         .Dyn.libs <- .dynLibs()
+#         num <- match("tcltk", .Dyn.libs)
+#         .dynLibs(.Dyn.libs[-num])
     }
 }
