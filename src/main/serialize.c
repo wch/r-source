0100fd309816bc5dec9846e4cb6d62dfa23dd5c3
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-06-04 19:17:13 +0000

    branch update
    git-svn-id: https://svn.r-project.org/R/trunk@24618 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/serialize.c
+++ src/main/serialize.c
@@ -1022,6 +1022,11 @@ static SEXP ReadItem (SEXP ref_table, R_inpstream_t stream)
 	    SET_FRAME(s, ReadItem(ref_table, stream));
 	    SET_TAG(s, ReadItem(ref_table, stream));
 	    SET_ATTRIB(s, ReadItem(ref_table, stream));
+	    if (ATTRIB(s) != R_NilValue &&
+		getAttrib(s, R_ClassSymbol) != R_NilValue)
+		/* We don't write out the object bit for environments,
+		   so reconstruct it here if needed. */
+		SET_OBJECT(s, 1);
 	    R_RestoreHashCount(s);
 #ifdef ENVIRONMENT_LOCKING
 	    if (locked) R_LockEnvironment(s, FALSE);
