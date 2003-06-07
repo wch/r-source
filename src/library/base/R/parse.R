036090a44ca476a620c1fb413259ad27127b6c52
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-06-07 21:08:46 +0000

    Deprecated unimplemented argument 'white' of parse().
    git-svn-id: https://svn.r-project.org/R/branches/R-1-7-patches@24638 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/parse.R
+++ src/library/base/R/parse.R
@@ -1,9 +1,14 @@
-## we don't use white; it's for compatibility
-
 parse <- function(file = "", n = NULL, text = NULL, prompt = "?",
                   white = FALSE)
 {
-    if (!is.null(text) && length(as.character(text)) == 0)
+    ## <NOTE>
+    ## This used to have:
+    ##   we don't use white; it's for compatibility.
+    ## We now think it is a bad idea to have it.
+    if(!missing(white))
+        warning("argument 'white' is unimplemented and deprecated")
+    ## </NOTE>
+    if(!is.null(text) && length(as.character(text)) == 0)
         return(expression())
     if(is.character(file))
         if(file == "") file <- stdin()
