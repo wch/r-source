66329b81f92aa76f23112b2b5d3694b028eb5b51
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-06-12 12:05:18 +0000

    remove plot.mts
    git-svn-id: https://svn.r-project.org/R/trunk@24703 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/ts.R
+++ src/library/base/R/ts.R
@@ -400,16 +400,53 @@ function (x, y = NULL, type = "l", xlim = NULL, ylim = NULL,
 	  lty = par("lty"), lwd = par("lwd"),
 	  axes = TRUE, frame.plot = axes, ann = par("ann"),
 	  main = NULL, plot.type = c("multiple", "single"),
-	  xy.labels = n <= 150, xy.lines = do.lab, panel=lines, nc, ...)
+	  xy.labels = (n <= 150), xy.lines = do.lab, panel = lines,
+          oma = c(6, 0, 5, 0), nc, ...)
 {
+    plot.type <- match.arg(plot.type)
+    nser <- NCOL(x)
+
+    if(plot.type == "multiple" && nser > 1) {
+        addmain <- function(main, cex.main=par("cex.main"),
+                            font.main=par("font.main"),
+                            col.main=par("col.main"), ...)
+            mtext(main, 3, 3, cex=cex.main, font=font.main, col=col.main, ...)
+
+        panel <- match.fun(panel)
+        nser <- NCOL(x)
+        if(nser > 10) stop("Can't plot more than 10 series")
+        if(is.null(main)) main <- deparse(substitute(x))
+        nm <- colnames(x)
+        if(is.null(nm)) nm <- paste("Series", 1:nser)
+        if(missing(nc)) nc <- if(nser >  4) 2 else 1
+        oldpar <- par("mar", "oma", "mfcol")
+        on.exit(par(oldpar))
+        par(mar = c(0, 5.1, 0, 2.1), oma = oma)
+        nr <- ceiling(nser / nc)
+        par(mfcol = c(nr, nc))
+        for(i in 1:nser) {
+            plot(x[, i], axes = FALSE, xlab="", ylab="",
+                 log = log, col = col, bg = bg, pch = pch, ann = ann,
+                 type = "n", ...)
+            panel(x[, i], col = col, bg = bg, pch = pch, type=type, ...)
+            box()
+            axis(2, xpd=NA)
+            mtext(nm[i], 2, 3)
+            if(i%%nr==0 || i==nser) axis(1, xpd=NA)
+        }
+        if(ann) {
+            mtext(xlab, 1, 3, ...)
+            if(!is.null(main)) {
+                par(mfcol=c(1,1))
+                addmain(main, ...)
+            }
+        }
+	return(invisible())
+    }
+    ## end of multiple plot section
+
     xlabel <- if (!missing(x)) deparse(substitute(x))## else NULL
     ylabel <- if (!missing(y)) deparse(substitute(y))
-    plot.type <- match.arg(plot.type)
-    if(plot.type == "multiple" && NCOL(x) > 1) {
-	m <- match.call()
-	m[[1]] <- as.name("plot.mts")
-	return(eval(m, parent.frame()))
-    }
     x <- as.ts(x)
     if(!is.null(y)) {
 	## want ("scatter") plot of y ~ x
@@ -496,57 +533,6 @@ function (x, y = NULL, type = "l", xlim = NULL, ylim = NULL,
 lines.ts <- function(x, ...)
     lines.default(time(as.ts(x)), x, ...)
 
-
-plot.mts <- function (x, plot.type = c("multiple", "single"), panel = lines,
-                      log = "", col = par("col"),  bg = NA, pch = par("pch"),
-                      cex = par("cex"), lty = par("lty"), lwd = par("lwd"),
-                      ann = par("ann"),  xlab = "Time", type = "l", main=NULL,
-                      oma=c(6, 0, 5, 0), nc, ...)
-{
-    addmain <- function(main, cex.main=par("cex.main"),
-                        font.main=par("font.main"),
-                        col.main=par("col.main"), ...)
-    {
-        mtext(main, 3, 3, cex=cex.main, font=font.main, col=col.main, ...)
-    }
-    plot.type <- match.arg(plot.type)
-    panel <- match.fun(panel)
-    nser <- NCOL(x)
-    if(plot.type == "single" || nser == 1) {
-        m <- match.call()
-        m[[1]] <- as.name("plot.ts")
-        m$plot.type <- "single"
-        return(eval(m, parent.frame()))
-    }
-    if(nser > 10) stop("Can't plot more than 10 series")
-    if(is.null(main)) main <- deparse(substitute(x))
-    nm <- colnames(x)
-    if(is.null(nm)) nm <- paste("Series", 1:nser)
-    if(missing(nc)) nc <- if(nser >  4) 2 else 1
-    oldpar <- par("mar", "oma", "mfcol")
-    on.exit(par(oldpar))
-    par(mar = c(0, 5.1, 0, 2.1), oma = oma)
-    nr <- ceiling(nser / nc)
-    par(mfcol = c(nr, nc))
-    for(i in 1:nser) {
-        plot(x[, i], axes = FALSE, xlab="", ylab="",
-             log = log, col = col, bg = bg, pch = pch, ann = ann,
-             type = "n", ...)
-        panel(x[, i], col = col, bg = bg, pch = pch, type=type, ...)
-        box()
-        axis(2, xpd=NA)
-        mtext(nm[i], 2, 3)
-        if(i%%nr==0 || i==nser) axis(1, xpd=NA)
-    }
-    if(ann) {
-        mtext(xlab, 1, 3, ...)
-        if(!is.null(main)) {
-            par(mfcol=c(1,1))
-            addmain(main, ...)
-        }
-    }
-    invisible()
-}
 
 window.default <- function(x, start = NULL, end = NULL,
                            frequency = NULL, deltat = NULL,
