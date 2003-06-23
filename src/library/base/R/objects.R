d2f15e58aa428a5ef56183eb2bcedf36dbf7edb5
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-06-23 05:54:11 +0000

    methods("$") was failing
    git-svn-id: https://svn.r-project.org/R/trunk@24876 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/objects.R
+++ src/library/base/R/objects.R
@@ -90,7 +90,7 @@ methods <- function (generic.function, class)
 	name <- paste(".", class, "$", sep = "")
     }
     else stop("must supply generic.function or class")
-    keep <- grep(gsub("([.[])", "\\\\\\1", name), an)
+    keep <- grep(gsub("([.[$])", "\\\\\\1", name), an)
     res <- an[keep]; info <- info[keep,]
     keep <- ! res %in% S3MethodsStopList
     res <- res[keep]; info <- info[keep,]
@@ -170,7 +170,7 @@ getS3method <-  function(f, class, optional = FALSE)
         else .BaseNamespaceEnv
         S3Table <- get(".__S3MethodsTable__.", envir = defenv)
         S3reg <- ls(S3Table)
-        if(length(grep(gsub("([.[])", "\\\\\\1", method), S3reg)))
+        if(length(grep(gsub("([.[$])", "\\\\\\1", method), S3reg)))
             return(get(method, envir = S3Table))
     }
     if(optional) NULL
