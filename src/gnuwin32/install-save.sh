f6a429152c4d7d17b75242d3a9b8030013e42442
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-06-26 17:39:45 +0000

    Added support for packages with name spaces.
    git-svn-id: https://svn.r-project.org/R/trunk@24950 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/install-save.sh
+++ src/gnuwin32/install-save.sh
@@ -11,20 +11,28 @@ export R_SAVE_IMAGE
 
 if ${R_SAVE_IMAGE}; then
     echo "  save image"
-    if test -f NAMESPACE; then
-      echo "ERROR: cannot use --save if package has NAMESPACE yet"
-	exit 1
-    fi
     save_image_defaults="list(compress=TRUE, safe=FALSE)"
+    code_file="${lib}/${pkg}/R/${pkg}"
+    rda_file="${lib}/${pkg}/R/all.rda"
+    if test -f NAMESPACE; then
+        code_cmd="echo saveNamespaceImage(\"${pkg}\", \"${rda_file}\", \"${lib}\")"
+        loader_file=nsrdaload.R
+        R_SAVE_EXE="${R_HOME}/bin/Rterm --slave --no-site-file --no-init-file"
+    else
+        code_cmd="cat ${code_file}"
+        loader_file=firstlib.R
+        R_SAVE_EXE="${R_HOME}/bin/Rterm --slave --save --no-site-file --no-init-file"
+    fi
     (echo "options(save.image.defaults=${save_image_defaults})"; \
       if test -s R_PROFILE.R; then cat R_PROFILE.R; fi; \
       echo "invisible(.libPaths(c(\"${lib}\", .libPaths())))"; \
-      cat ${lib}/${pkg}/R/${pkg}) | \
-      ${R_HOME}/bin/Rterm --slave --save --no-site-file --no-init-file \
+      ${code_cmd}) | ${R_SAVE_EXE} \
         || (echo "Execution of package source for ${pkg} failed"; exit 1)
-    mv .RData ${lib}/${pkg}/R/all.rda
-    mv ${lib}/${pkg}/R/${pkg} ${lib}/${pkg}/R/${pkg}.R
-    cat ${R_HOME}/share/R/firstlib.R > ${lib}/${pkg}/R/${pkg}
+    if test ! -f NAMESPACE; then
+        mv .RData ${rda_file}
+    fi
+    mv "${code_file}" "${code_file}.R"
+    cat "${R_HOME}/share/R/${loader_file}" > "${code_file}"
     ## if install.R is non-empty, arrange to evaluate the R code it
     ## contains after the package source (maybe for some kind of
     ## cleanup).
