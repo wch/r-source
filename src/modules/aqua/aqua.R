2f5ae7f808c30570cf4781181a797641141a3ee6
iacus <iacus@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-07-09 22:56:18 +0000

    *** empty log message ***
    git-svn-id: https://svn.r-project.org/R/trunk@25145 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/modules/aqua/aqua.R
+++ src/modules/aqua/aqua.R
@@ -1,9 +1,15 @@
 
 # Setup coming from src/modules/aqua/aqua.R
 # 
+# sets the quartz device as default
+if(.Platform$GUI == "AQUA")
+ options(device = "quartz")
+
 # sets initial current working directory to user's root
 setwd("~/")
+
 # this is to allow g77 compiler to work
 Sys.putenv("PATH" = paste(Sys.getenv("PATH"),":/usr/local/bin",sep=""))
+
 
 
