21102c7e45a6af0edb20c7cfebebc3791c834c2e
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-07-20 07:26:45 +0000

    ISO C does not allow tokens after #ifdef
    git-svn-id: https://svn.r-project.org/R/trunk@25270 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/unix/dlfcn-darwin.c
+++ src/unix/dlfcn-darwin.c
@@ -29,6 +29,10 @@ OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
 #ifdef __APPLE_CC__
 
 
@@ -867,4 +871,4 @@ to darwin-development AT lists DOT apple DOT com and slightly modified
 	return 1;
 }
 
-#endif __APPLE_CC__
+#endif /* __APPLE_CC__ */
