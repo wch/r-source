09692550e8205f757fa4194b926c7fa9234e1ea4
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-07-22 19:39:42 +0000

    Replaced strange onintr usage by error().
    git-svn-id: https://svn.r-project.org/R/trunk@25317 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/graphics.c
+++ src/main/graphics.c
@@ -2345,7 +2345,11 @@ void GCheckState(DevDesc *dd)
     if(Rf_gpptr(dd)->state == 0)
 	error("plot.new has not been called yet");
     if (!Rf_gpptr(dd)->valid)
+#ifdef OLD
 	onintr();
+#else
+        error("invalid graphics state");
+#endif
 }
 
 /*-------------------------------------------------------------------
