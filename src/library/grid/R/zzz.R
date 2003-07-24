ec567c0b84bf075ef7e02ed004f2b170d224f5d6
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-07-24 08:50:03 +0000

    only need to library.dynam in one place
    git-svn-id: https://svn.r-project.org/R/trunk@25346 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/grid/R/zzz.R
+++ src/library/grid/R/zzz.R
@@ -1,17 +1,17 @@
 .onLoad <- function(lib, pkg) {
-  library.dynam( "grid", pkg, lib )
-  .Call("L_initGrid", PACKAGE="grid")
-  .grid.loaded <<- TRUE
+    library.dynam( "grid", pkg, lib )
+    .Call("L_initGrid", PACKAGE="grid")
+    .grid.loaded <<- TRUE
 }
 
-.onUnload <-function(libpath) {
-  if (.grid.loaded) {
-    # Kill all existing devices to avoid replay
-    # of display list which tries to run grid code
-    # Not very friendly to other registered graphics systems
-    # but its safety first for now
-    graphics.off()
-    .Call("L_killGrid", PACKAGE="grid")
-  }
+.onUnload <- function(libpath) {
+    if (.grid.loaded) {
+        ## Kill all existing devices to avoid replay
+        ## of display list which tries to run grid code
+        ## Not very friendly to other registered graphics systems
+        ## but its safety first for now
+        graphics.off()
+        .Call("L_killGrid", PACKAGE="grid")
+    }
     library.dynam.unload("grid", libpath)
 }
