58f65d9035b5281c9f44099c56d2044999a4f101
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-07-28 16:57:07 +0000

    add copyright statements make R_gridEvalEnv extern is all but one place
    git-svn-id: https://svn.r-project.org/R/trunk@25414 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/grid/src/grid.h
+++ src/library/grid/src/grid.h
@@ -1,3 +1,24 @@
+/*
+ *  R : A Computer Language for Statistical Data Analysis
+ *  Copyright (C) 2001-3 Paul Murrell
+ *                2003 The R Development Core Team
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  A copy of the GNU General Public License is available via WWW at
+ *  http://www.gnu.org/copyleft/gpl.html.  You can also obtain it by
+ *  writing to the Free Software Foundation, Inc., 59 Temple Place,
+ *  Suite 330, Boston, MA  02111-1307  USA.
+ */
+
 #include <Rinternals.h>
 #include <Rgraphics.h>  
 #include <Rmath.h>
@@ -143,7 +164,11 @@ typedef struct {
 } LViewportContext;
 
 /* Evaluation environment */
+#ifndef GRID_MAIN
+extern SEXP R_gridEvalEnv;
+#else
 SEXP R_gridEvalEnv;
+#endif
 
 /* Functions called by R code
  * (from all over the place)
