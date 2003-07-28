8421bad27dc762d7c8f35652b924558d75571d02
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-07-28 16:50:40 +0000

    Move R_PKGS_* variables to 'share/make/vars.mk', so that they are also available at run-time. Rename R_PKGS_RECOMMENDED to R_PKGS_RECOMMENDED_SOURCES. Rename R_PKGS_RECOMMENDED_EXPANDED to R_PKGS_RECOMMENDED.
    git-svn-id: https://svn.r-project.org/R/trunk@25400 00db46b3-68df-0310-9c12-caf00c1e9a41

---
+++ share/make/vars.mk
@@ -0,0 +1,7 @@
+## Static (i.e., not determined by configure) Make variables to be
+## shared between 'Makeconf' and 'etc/Makeconf'.
+
+R_PKGS_BASE = base ctest eda grid lqs methods modreg mva nls splines stepfun tcltk tools ts
+R_PKGS_RECOMMENDED_SOURCES = survival boot cluster foreign lattice KernSmooth mgcv nlme rpart VR
+R_PKGS_RECOMMENDED = survival boot cluster foreign lattice KernSmooth mgcv nlme rpart MASS nnet spatial class
+
