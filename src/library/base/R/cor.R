0398efff234c996bf516cee88953853a88865c9d
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-08-22 07:12:14 +0000

    cov2cor()
    git-svn-id: https://svn.r-project.org/R/trunk@25842 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/cor.R
+++ src/library/base/R/cor.R
@@ -7,3 +7,23 @@ cor <- function (x, y=NULL, use="all.obs")
         stop("supply both x and y or a matrix-like x")
     .Internal(cor(x, y, na.method))
 }
+
+cov2cor <- function(V)
+{
+    ## Purpose: Covariance matrix |--> Correlation matrix -- efficiently
+    ## ----------------------------------------------------------------------
+    ## Arguments: V: a covariance matrix (i.e. symmetric and positive definite)
+    ## ----------------------------------------------------------------------
+    ## Author: Martin Maechler, Date: 12 Jun 2003, 11:50
+    p <- (d <- dim(V))[1]
+    if(!is.numeric(V) || length(d) != 2 || p != d[2])
+        stop("`V' is not a square numeric matrix")
+    Is <- sqrt(1/diag(V)) # diag( 1/sigma_i )
+    if(any(!is.finite(Is)))
+        warning("diagonal has non-finite entries")
+    r <- V # keep dimnames
+    r[] <- Is * V * rep(Is, each = p)
+    ##  == D %*% V %*% D  where D = diag(Is)
+    r[cbind(1:p,1:p)] <- 1 # exact in diagonal
+    r
+}
