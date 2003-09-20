c4bbc98bdb992fd8c61a285048796e33f8b94ba5
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-09-20 21:36:28 +0000

    mkdir -p and ln -s are not portable.
    git-svn-id: https://svn.r-project.org/R/trunk@26332 00db46b3-68df-0310-9c12-caf00c1e9a41

--- share/sh/help-links.sh
+++ share/sh/help-links.sh
@@ -1,49 +1,50 @@
 #! /bin/sh
 
-USER_R_HOME=$1/.R; shift
-rm -rf ${USER_R_HOME}
+USER_R_HOME="${1}/.R"; shift
+rm -rf "${USER_R_HOME}"
 
-dirs="${USER_R_HOME} ${USER_R_HOME}/doc ${USER_R_HOME}/doc/html ${USER_R_HOME}/doc/html/search ${USER_R_HOME}/library"
-for d in ${dirs}; do
-    mkdir -p ${d}
+for d in "${USER_R_HOME}" "${USER_R_HOME}/doc" \
+         "${USER_R_HOME}/doc/html" "${USER_R_HOME}/doc/html/search" \
+	 "${USER_R_HOME}/library"; do \
+  "${R_HOME}"/bin/mkinstalldirs "${d}" >/dev/null
 done
 
 for f in AUTHORS COPYING THANKS; do
-  if test -f ${R_HOME}/${f}; then
-    ln -s ${R_HOME}/${f} ${USER_R_HOME}/${f}
+  if test -f "${R_HOME}/${f}"; then
+    ${LN_S} "${R_HOME}/${f}" "${USER_R_HOME}/${f}"
   fi
 done
 
-if test -d ${R_HOME}/doc/manual; then
-  ln -s ${R_HOME}/doc/manual ${USER_R_HOME}/doc/manual
+if test -d "${R_HOME}/doc/manual"; then
+  ${LN_S} "${R_HOME}/doc/manual" "${USER_R_HOME}/doc/manual"
 fi
 
-for f in ${R_HOME}/doc/html/*; do
-    if test -f $f; then
-	ln -s ${f} ${USER_R_HOME}/doc/html
-    fi
+for f in "${R_HOME}"/doc/html/*; do
+  if test -f "${f}"; then
+    ${LN_S} "${f}" "${USER_R_HOME}/doc/html"
+  fi
 done
-# we are going to recreate this in R code
-rm -f ${USER_R_HOME}/doc/html/packages.html
-# this needs to be copied for OS X 
-rm -f ${USER_R_HOME}/doc/html/index.html
-cp ${R_HOME}/doc/html/index.html ${USER_R_HOME}/doc/html
+## we are going to recreate this in R code
+rm -f "${USER_R_HOME}/doc/html/packages.html"
+## this needs to be copied for OS X 
+rm -f "${USER_R_HOME}/doc/html/index.html"
+cp "${R_HOME}/doc/html/index.html}" "${USER_R_HOME}/doc/html"
 
-# class files must be copied for mozilla to work
-for f in ${R_HOME}/doc/html/search/*.class; do
-    if test -f $f; then
-	cp ${f} ${USER_R_HOME}/doc/html/search
-    fi
+## class files must be copied for Mozilla to work
+for f in "${R_HOME}"/doc/html/search/*.class; do
+  if test -f "${f}"; then
+    cp "${f}" "${USER_R_HOME}/doc/html/search"
+  fi
 done
-for f in ${R_HOME}/doc/html/search/*.html; do
-    if test -f $f; then
-	ln -s ${f} ${USER_R_HOME}/doc/html/search
-    fi
+for f in "${R_HOME}"/doc/html/search/*.html; do
+  if test -f "${f}"; then
+    ${LN_S} "${f}" "${USER_R_HOME}/doc/html/search"
+  fi
 done
-# we are going to recreate this in R code
-rm -f ${USER_R_HOME}/doc/html/search/index.txt
+## we are going to recreate this in R code
+rm -f "${USER_R_HOME}/doc/html/search/index.txt"
 
-ln -s ${R_HOME}/doc/html/R.css ${USER_R_HOME}/library
+${LN_S} "${R_HOME}/doc/html/R.css" "${USER_R_HOME}/library"
 
 ### Local Variables: ***
 ### mode: sh ***
