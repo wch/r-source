c334f812bfc926ab3ee5ef347e44f2ec3a90097b
tlumley <tlumley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-09-22 15:31:56 +0000

    error instead of segfault for quartz at command-line
    git-svn-id: https://svn.r-project.org/R/trunk@26368 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/unix/quartz.R
+++ src/library/base/R/unix/quartz.R
@@ -1,4 +1,7 @@
 quartz <- function (display = "", width = 5, height = 5, pointsize = 12, 
-                    family="Helvetica", antialias = TRUE, autorefresh = TRUE)
-.Internal(Quartz(display, width, height, pointsize,family, antialias,autorefresh))
-
+                    family="Helvetica", antialias = TRUE, autorefresh = TRUE){
+  if (.Platform$GUI=="AQUA")
+    .Internal(Quartz(display, width, height, pointsize,family, antialias,autorefresh))
+  else
+    stop("quartz() currently works only under RAqua")
+}
