27318b7936553d2c3f96615dba7710174978a9d3
martyn <martyn@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-11-17 09:36:38 +0000

    Corrected toolbar print button
    git-svn-id: https://svn.r-project.org/R/branches/R-1-8-patches@27098 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/modules/gnome/devGNOME.c
+++ src/modules/gnome/devGNOME.c
@@ -402,7 +402,7 @@ static void toolbar_print_cb (GtkWidget *widget, gpointer data)
     NewDevDesc *dd = (NewDevDesc*) data;
     gchar *cmd;
 
-    cmd = g_strdup_printf("dev.cur(%d)\ndev.print()",
+    cmd = g_strdup_printf("dev.set(%d)\ndev.print()",
 			  RDeviceNumber(dd));
     R_gtk_terminal_run(cmd);
     g_free(cmd);
