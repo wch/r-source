9993b5bd1a8e8ece12f885f53eaf8c4e2c06b23c
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-11-20 09:14:51 +0000

    (C) R Foundation; and GPL with URL -- in one of the alphabetically first files
    git-svn-id: https://svn.r-project.org/R/branches/R-1-8-patches@27166 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/arithmetic.c
+++ src/main/arithmetic.c
@@ -2,6 +2,7 @@
  *  R : A Computer Language for Statistical Data Analysis
  *  Copyright (C) 1995, 1996, 1997  Robert Gentleman and Ross Ihaka
  *  Copyright (C) 1998--2003	    The R Development Core Team.
+ *  Copyright (C) 2003		    The R Foundation
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -13,9 +14,10 @@
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *  A copy of the GNU General Public License is available via WWW at
+ *  http://www.gnu.org/copyleft/gpl.html.  You can also obtain it by
+ *  writing to the Free Software Foundation, Inc., 59 Temple Place,
+ *  Suite 330, Boston, MA  02111-1307  USA.
  */
 
 #ifdef HAVE_CONFIG_H
@@ -1068,7 +1070,7 @@ static SEXP math2(SEXP sa, SEXP sb, double (*f)(), SEXP lcall)
     if (!isNumeric(sa) || !isNumeric(sb))
 	errorcall(lcall, R_MSG_NONNUM_MATH);
 
-    /* for 0-length a we want the attributes of a, not those of b 
+    /* for 0-length a we want the attributes of a, not those of b
        as no recycling will occur */
 #define SETUP_Math2				\
     na = LENGTH(sa);				\
