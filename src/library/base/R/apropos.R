b82c52e139437c8c89f05830d59acf6e26e35790
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-11-29 10:42:49 +0000

    find was pretty broken: see NEWS
    git-svn-id: https://svn.r-project.org/R/trunk@27268 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/apropos.R
+++ src/library/base/R/apropos.R
@@ -20,26 +20,37 @@ apropos <- function (what, where = FALSE, mode = "any")
 find <- function(what, mode = "any", numeric. = FALSE, simple.words=TRUE) {
     if(!is.character(what))
 	what <- as.character(substitute(what))
-    if(simple.words)
-	what <- gsub("([.[])", "\\\\\\1", paste("^",what,"$", sep=""))
+    if(length(what) > 1) {
+        warning("elements of 'what' after the first will be ignored")
+        what <- what[1]
+    }
+#   would need to escape at least + * | as well
+#     if(simple.words)
+# 	what <- gsub("([.[])", "\\\\\\1", paste("^",what,"$", sep=""))
     len.s <- length(sp <- search())
     ind <- logical(len.s)
     if((check.mode <- mode != "any"))
 	nam <- character(len.s)
     for (i in 1:len.s) {
-	ll <- length(li <- ls(pos = i, pattern = what, all.names = TRUE))
-	ind[i] <- ll > 0
-	if(ll >= 2) warning(paste(ll, "occurrences in", sp[i]))
-	if(check.mode && ind[i]) nam[i] <- li[1]
+        if(simple.words) {
+            li <- ls(pos = i, all.names = TRUE)
+            found <- what %in% ls(pos = i, all.names = TRUE)
+            if(found && check.mode)
+                found <- exists(what, where = i, mode = mode, inherits=FALSE)
+            ind[i] <- found
+        } else {
+            li <- ls(pos = i, pattern = what, all.names = TRUE)
+            ll <- length(li)
+            if(ll > 0 && check.mode) {
+                mode.ok <- sapply(li, exists, where = i, mode = mode,
+                                  inherits = FALSE)
+                ll <- sum(mode.ok)
+                if(ll >= 2) warning(paste(ll, "occurrences in", sp[i]))
+            }
+            ind[i] <- ll > 0
+        }
     }
     ## found name in  search()[ ind ]
-
-    ii <- which(ind)
-    if(check.mode && any(ind)) {
-	mode.ok <- sapply(ii, function(i) exists(nam[i], where = i,
-						 mode = mode, inherits=FALSE))
-	ii <- ii[mode.ok]
-    }
-    if(numeric.) structure(ii, names=sp[ii]) else sp[ii]
+    if(numeric.) structure(which(ind), names=sp[ind]) else sp[ind]
 }
 
