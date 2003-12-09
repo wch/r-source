b98b6e219f1758a8f9a2f7ecfd8cbbc1f8ce0850
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-12-09 21:04:54 +0000

    reduce eda to stub
    git-svn-id: https://svn.r-project.org/R/trunk@27473 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/eda/R/zzz.R
+++ src/library/eda/R/zzz.R
@@ -1,4 +1,9 @@
 .noGenerics <- TRUE
 
-.onUnload <- function(libpath)
-    library.dynam.unload("eda", libpath)
+.First.lib <- function(lib, pkg)
+{
+    have.stats <- "package:stats" %in% search()
+    if(!have.stats) require("stats")
+    warning("package ", sQuote("eda"), " has been merged into ",
+            sQuote("stats"), call. = FALSE)
+}
