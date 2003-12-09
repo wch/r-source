8751c34e0384ef1380db53c295151ec6997b2c89
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-12-09 15:07:34 +0000

    make ctest into a stub package
    git-svn-id: https://svn.r-project.org/R/trunk@27457 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/ctest/R/zzz.R
+++ src/library/ctest/R/zzz.R
@@ -1,4 +1,9 @@
 .noGenerics <- TRUE
 
-.onUnload <- function(libpath)
-    library.dynam.unload("ctest", libpath)
+.First.lib <- function(lib, pkg)
+{
+    have.stats <- "package:stats" %in% search()
+    if(!have.stats) require("stats")
+    warning("package ", sQuote("ctest"), " has been merged into ",
+            sQuote("stats"), call. = FALSE)
+}
