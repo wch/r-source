fb182100170484518b1e36fff32e4758f8c82db0
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-12-11 08:24:51 +0000

    stub .First.lib
    git-svn-id: https://svn.r-project.org/R/trunk@27502 00db46b3-68df-0310-9c12-caf00c1e9a41

---
+++ src/library/nls/R/zzz.R
@@ -0,0 +1,9 @@
+.noGenerics <- TRUE
+
+.First.lib <- function(lib, pkg)
+{
+    have.stats <- "package:stats" %in% search()
+    if(!have.stats) require("stats")
+    warning("package ", sQuote("nls"), " has been merged into ",
+            sQuote("stats"), call. = FALSE)
+}
