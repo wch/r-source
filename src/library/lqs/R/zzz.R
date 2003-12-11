95e1c89878fcb66cfb1464105c38a6985ce40684
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-12-11 07:42:37 +0000

    lqs -> stub
    git-svn-id: https://svn.r-project.org/R/trunk@27500 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/lqs/R/zzz.R
+++ src/library/lqs/R/zzz.R
@@ -1,4 +1,13 @@
 .noGenerics <- TRUE
 
-.onUnload <- function(libpath)
-    library.dynam.unload("lqs", libpath)
+.First.lib <- function(pkg, lib)
+{
+    cat("Package 'lqs' has been moved back to package 'MASS'\n")
+    have.VR <- "package:MASS" %in% search()
+    if(!have.VR) {
+        if(require(MASS, quietly=TRUE))
+            cat("Package 'MASS' has now been loaded\n")
+        else
+            cat("Package 'MASS' seems to be missing from this R installation\n")
+    }
+}
