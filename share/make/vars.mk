e11c3413af26dbbdc5ebcfdc65515294d9d4a73e
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-12-16 13:50:22 +0000

    Kurt suggests tools earlier
    git-svn-id: https://svn.r-project.org/R/trunk@27624 00db46b3-68df-0310-9c12-caf00c1e9a41

--- share/make/vars.mk
+++ share/make/vars.mk
@@ -1,7 +1,7 @@
 ## Static (i.e., not determined by configure) Make variables to be
 ## shared between 'Makeconf' and 'etc/Makeconf'.
 
-R_PKGS_BASE = base utils graphics stats tools methods grid mle splines stepfun tcltk
+R_PKGS_BASE = base tools utils graphics stats methods grid mle splines stepfun tcltk
 R_PKGS_STUBS = ctest eda lqs modreg mva nls ts
 R_PKGS_RECOMMENDED_SOURCES = survival boot cluster foreign lattice KernSmooth mgcv nlme rpart VR
 R_PKGS_RECOMMENDED = survival boot cluster foreign lattice KernSmooth mgcv nlme rpart MASS class nnet spatial
