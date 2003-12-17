150cdea3f61bcddd6a479a9ac36d63211ee92697
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-12-17 20:36:44 +0000

    stepfun is now a stub
    git-svn-id: https://svn.r-project.org/R/trunk@27657 00db46b3-68df-0310-9c12-caf00c1e9a41

--- share/make/vars.mk
+++ share/make/vars.mk
@@ -1,8 +1,8 @@
 ## Static (i.e., not determined by configure) Make variables to be
 ## shared between 'Makeconf' and 'etc/Makeconf'.
 
-R_PKGS_BASE = base tools utils graphics stats methods grid mle splines stepfun tcltk
-R_PKGS_STUBS = ctest eda lqs modreg mva nls ts
+R_PKGS_BASE = base tools utils graphics stats methods grid mle splines tcltk
+R_PKGS_STUBS = ctest eda lqs modreg mva nls stepfun ts
 R_PKGS_RECOMMENDED_SOURCES = survival boot cluster foreign lattice KernSmooth mgcv nlme rpart VR
 R_PKGS_RECOMMENDED = survival boot cluster foreign lattice KernSmooth mgcv nlme rpart MASS class nnet spatial
 
