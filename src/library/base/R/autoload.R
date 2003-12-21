7328b95cfa9c51970dcd9ef4a417fd71f8414c59
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-12-21 10:20:29 +0000

    reset in autoloader could fail if setMethod came into the picture
    git-svn-id: https://svn.r-project.org/R/trunk@27732 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/autoload.R
+++ src/library/base/R/autoload.R
@@ -1,11 +1,12 @@
-autoload <- function(name, package, ...)
+autoload <- function(name, package, reset=FALSE, ...)
 {
-    if (exists(name, envir = .GlobalEnv, inherits = FALSE))
+    if (!reset && exists(name, envir = .GlobalEnv, inherits = FALSE))
 	stop("Object with that name already exists")
     m <- match.call()
     m[[1]] <- as.name("list")
     newcall <- eval(m, parent.frame())
     newcall <- as.call(c(as.name("autoloader"), newcall))
+    newcall$reset <- NULL
     if (is.na(match(package, .Autoloaded)))
 	assign(".Autoloaded", c(package, .Autoloaded), env =.AutoloadEnv)
     assign(name, do.call("delay", list(newcall)), env = .AutoloadEnv)
@@ -23,7 +24,7 @@ autoloader <- function (name, package, ...)
     ## load the package
     eval(m, .GlobalEnv)
     ## reset the autoloader
-    autoload(name, package, ...)
+    autoload(name, package, reset = TRUE, ...)
     ## reevaluate the object
     where <- match(paste("package", package, sep = ":"), search())
     if (exists(name, where = where, inherits = FALSE))
