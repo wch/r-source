06c516eed4c6fa6fab4b293de75b46eb9da3577b
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-12-26 16:52:23 +0000

    all ports have gzfilethese days
    git-svn-id: https://svn.r-project.org/R/trunk@27790 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/serialize.R
+++ src/library/base/R/serialize.R
@@ -1,51 +1,37 @@
 .saveRDS <-
 function(object, file = "", ascii = FALSE, version = NULL,
-         compress = FALSE, refhook = NULL) 
+         compress = FALSE, refhook = NULL)
 {
     if(is.character(file)) {
         if(file == "") stop("'file' must be non-empty string")
         mode <- if(ascii) "w" else "wb"
-        if(compress && capabilities("libz")) con <- gzfile(file, mode)
-        else con <- file(file, mode)
+        con <- if(compress) gzfile(file, mode) else file(file, mode)
         on.exit(close(con))
     }
     else if (inherits(file, "connection")) {
         con <- file
-        if(missing(ascii)) 
-            if(summary(con)$text == "text") 
-                ascii <- TRUE
-            else ascii <- FALSE
-    }        
+        if(missing(ascii)) ascii <- summary(con)$text == "text"
+    }
     else stop("bad 'file' argument")
-    invisible(.Internal(serializeToConn(object, con, ascii, version,
-                                        refhook)))
+    invisible(.Internal(serializeToConn(object, con, ascii, version, refhook)))
 }
 
 .readRDS <-
-function(file, refhook = NULL) 
+function(file, refhook = NULL)
 {
     if(is.character(file)) {
-        if(capabilities("libz")) 
-            con <- gzfile(file, "rb")
-        else
-            con <- file(file, "rb")
+        con <- gzfile(file, "rb")
         on.exit(close(con))
-    }
-    else if (inherits(file, "connection")) 
-        con <- file
+    } else if (inherits(file, "connection")) con <- file
     else stop("bad 'file' argument")
     .Internal(unserializeFromConn(con, refhook))
 }
 
 serialize <- function(object, connection, ascii = FALSE, refhook = NULL) {
     if (! is.null(connection)) {
-        if (!inherits(connection, "connection")) 
+        if (!inherits(connection, "connection"))
             stop("`connection' must be a connection")
-        if (missing(ascii))
-            if (summary(connection)$text == "text")
-                ascii <- TRUE
-            else
-                ascii <- FALSE
+        if (missing(ascii)) ascii <- summary(connection)$text == "text"
     }
     if (! ascii && inherits(connection, "sockconn"))
         .Call("R_serializeb", object, connection, refhook, PACKAGE="base")
@@ -55,7 +41,7 @@ serialize <- function(object, connection, ascii = FALSE, refhook = NULL) {
 }
 
 unserialize <- function(connection, refhook = NULL) {
-    if (! is.character(connection) && !inherits(connection, "connection")) 
+    if (! is.character(connection) && !inherits(connection, "connection"))
         stop("`connection' must be a connection")
     .Call("R_unserialize", connection, refhook, PACKAGE="base")
 }
