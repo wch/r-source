550b59453b666d64fcb878aed90349902b47a86f
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2004-01-01 17:38:03 +0000

    alpha order
    git-svn-id: https://svn.r-project.org/R/trunk@27853 00db46b3-68df-0310-9c12-caf00c1e9a41

--- share/make/vars.mk
+++ share/make/vars.mk
@@ -2,7 +2,7 @@
 ## shared between 'Makeconf' and 'etc/Makeconf'.
 
 R_PKGS_BASE = base tools utils graphics stats methods grid splines stats4 tcltk
-R_PKGS_STUBS = ctest eda lqs modreg mle mva nls stepfun ts
+R_PKGS_STUBS = ctest eda lqs mle modreg mva nls stepfun ts
 R_PKGS_RECOMMENDED_SOURCES = survival boot cluster foreign KernSmooth lattice mgcv nlme rpart VR
 R_PKGS_RECOMMENDED = survival boot cluster foreign KernSmooth lattice mgcv nlme rpart MASS class nnet spatial
 
