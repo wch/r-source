6b8277a11f81be057a0e41719f1a9bf7cf91ff82
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2004-01-05 10:20:45 +0000

    update copyright dates
    git-svn-id: https://svn.r-project.org/R/trunk@27895 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/src/distance.c
+++ src/library/stats/src/distance.c
@@ -3,7 +3,7 @@
  *  Copyright (C) 1995, 1996  Robert Gentleman and Ross Ihaka
  *  Copyright (C) 1998, 2001  Robert Gentleman, Ross Ihaka and the
  *			      R Development Core Team
- *  Copyright (C) 2002	      The R Foundation
+ *  Copyright (C) 2002, 2004  The R Foundation
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
