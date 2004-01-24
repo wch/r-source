37271cdbdcd7e5d82c79bdb536ef305d93b644ad
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2004-01-24 14:15:47 +0000

    add head() and tail(), improved from code by Pat Burns
    git-svn-id: https://svn.r-project.org/R/trunk@28111 00db46b3-68df-0310-9c12-caf00c1e9a41

---
+++ src/library/utils/R/head.R
@@ -0,0 +1,33 @@
+### placed in the public domain 2002
+### Patrick Burns patrick@burns-stat.com
+
+head <- function(x, ...) UseMethod("head")
+
+head.default <- function(x, n=6)
+{
+    ans <- x[seq(len=min(n, length(x)))]
+    if(length(dim(x)) == 1) array(ans, n, list(names(ans))) else ans
+}
+
+head.data.frame <- head.matrix <- function(x, n=6)
+    x[seq(len=min(n, nrow(x))), , drop=FALSE]
+
+head.function <- function(x, n=6) head(deparse(x), n=n)
+
+tail <- function(x, ...) UseMethod("tail")
+
+tail.default <- function(x, n=6)
+{
+    xlen <- length(x)
+    n <- min(n, xlen)
+    ans <- x[seq(to=xlen, length=n)]
+    if(length(dim(x)) == 1) array(ans, n, list(names(ans))) else ans
+}
+
+tail.data.frame <- tail.matrix <- function(x, n=6)
+{
+    nrx <- nrow(x)
+    x[seq(to=nrx, length=min(n, nrx)), , drop=FALSE]
+}
+
+tail.function <- function(x, n=6) tail(deparse(x), n=n)
