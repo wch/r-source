18f1f390a2db4c8e27966df46f1567fa176d1461
pd <pd@00db46b3-68df-0310-9c12-caf00c1e9a41>
2004-03-08 00:53:44 +0000

    coef(summary.mle) doc + variable capture issue in replicate()
    git-svn-id: https://svn.r-project.org/R/trunk@28559 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/replicate.R
+++ src/library/base/R/replicate.R
@@ -1,3 +1,3 @@
 replicate <- function(n, expr, simplify = TRUE) 
         sapply(integer(n), 
-           eval.parent(substitute(function(x)expr)), simplify = simplify)
+           eval.parent(substitute(function(...)expr)), simplify = simplify)
