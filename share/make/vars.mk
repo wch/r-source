a1eddb1157f423d97dc8f18479d5e265cbc6bb10
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2004-03-15 17:42:35 +0000

    mgcv links to nlme
    git-svn-id: https://svn.r-project.org/R/trunk@28643 00db46b3-68df-0310-9c12-caf00c1e9a41

--- share/make/vars.mk
+++ share/make/vars.mk
@@ -3,6 +3,7 @@
 
 R_PKGS_BASE = base tools utils graphics stats methods grid splines stats4 tcltk
 R_PKGS_STUBS = ctest eda lqs mle modreg mva nls stepfun ts
-R_PKGS_RECOMMENDED_SOURCES = survival boot cluster foreign KernSmooth lattice mgcv nlme rpart VR
-R_PKGS_RECOMMENDED = survival boot cluster foreign KernSmooth lattice mgcv nlme rpart MASS class nnet spatial
+# mgcv links to nlme, boot used to link to survival
+R_PKGS_RECOMMENDED_SOURCES = survival boot cluster foreign KernSmooth lattice nlme mgcv rpart VR
+R_PKGS_RECOMMENDED = survival boot cluster foreign KernSmooth lattice nlme mgcvrpart MASS class nnet spatial
 
