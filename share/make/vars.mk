16e5178f864e87f63bfd01be7bb539b70770e189
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2004-03-17 11:35:30 +0000

    Typo in most recent change.
    git-svn-id: https://svn.r-project.org/R/trunk@28674 00db46b3-68df-0310-9c12-caf00c1e9a41

--- share/make/vars.mk
+++ share/make/vars.mk
@@ -5,5 +5,5 @@ R_PKGS_BASE = base tools utils graphics stats methods grid splines stats4 tcltk
 R_PKGS_STUBS = ctest eda lqs mle modreg mva nls stepfun ts
 # mgcv links to nlme, boot used to link to survival
 R_PKGS_RECOMMENDED_SOURCES = survival boot cluster foreign KernSmooth lattice nlme mgcv rpart VR
-R_PKGS_RECOMMENDED = survival boot cluster foreign KernSmooth lattice nlme mgcvrpart MASS class nnet spatial
+R_PKGS_RECOMMENDED = survival boot cluster foreign KernSmooth lattice nlme mgcv rpart MASS class nnet spatial
 
