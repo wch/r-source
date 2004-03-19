b377bc1f3dcf3b0c2c5fc6a717594c9e9c7b0733
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2004-03-19 18:02:05 +0000

    add csh and cmd.exe quoting
    git-svn-id: https://svn.r-project.org/R/trunk@28722 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/utils/R/utils.R
+++ src/library/utils/R/utils.R
@@ -1,5 +1,23 @@
-shQuote <- function(string, type = "sh") {
-    if(type != "sh")
-        stop("currently only type = \"sh\" is supported")
-    paste('"', gsub('(["$`\\])', "\\\\\\1", string), '"', sep="")
+shQuote <- function(string, type = c("sh", "csh", "cmd"))
+{
+    cshquote <- function(x) {
+        xx <- strsplit(x, "'", fixed = TRUE)[[1]]
+        paste(paste("'", xx, "'", sep = ""), collapse="\"'\"")
+    }
+    type <- match.arg(type)
+    if(type == "cmd") {
+        paste('"', gsub('"', '\\\\"', string), '"', sep = "")
+    } else {
+        if(!length(string)) return('')
+        has_single_quote <- grep("'", string)
+        if(!length(has_single_quote))
+            return(paste("'", string, "'", sep = ""))
+        if(type == "sh")
+            paste('"', gsub('(["$`\\])', "\\\\\\1", string), '"', sep="")
+        else {
+            if(!length(grep('([$`])', string))) {
+                paste('"', gsub('(["!\\])', "\\\\\\1", string), '"', sep="")
+            } else sapply(string, cshquote)
+        }
+    }
 }
