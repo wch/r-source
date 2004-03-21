9835a648e3ef80c6bf9c23e8a46aac361549c329
pd <pd@00db46b3-68df-0310-9c12-caf00c1e9a41>
2004-03-21 09:43:00 +0000

    negative index bug + doc gzfile fix in NEWS
    git-svn-id: https://svn.r-project.org/R/trunk@28762 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/subscript.c
+++ src/main/subscript.c
@@ -251,13 +251,15 @@ static SEXP negativeSubscript(SEXP s, int ns, int nx)
 {
     SEXP indx;
     int stretch = 0;
-    int i;
+    int i, ix;
     PROTECT(indx = allocVector(INTSXP, nx));
     for (i = 0; i < nx; i++)
 	INTEGER(indx)[i] = 1;
-    for (i = 0; i < ns; i++)
-	if (INTEGER(s)[i] != 0)
-	    INTEGER(indx)[-INTEGER(s)[i] - 1] = 0;
+    for (i = 0; i < ns; i++) {
+	ix = INTEGER(s)[i]; 
+	if (ix != 0 && ix != NA_INTEGER && -ix <= nx)
+	    INTEGER(indx)[-ix - 1] = 0;
+    }
     s = logicalSubscript(indx, nx, nx, &stretch);
     UNPROTECT(1);
     return s;
