cb24d0a78ac8d02008d05e140f70de209510c4d2
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2004-03-23 17:38:00 +0000

    2nd part of qgamma() improvements
    git-svn-id: https://svn.r-project.org/R/trunk@28808 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/nmath/qgamma.c
+++ src/nmath/qgamma.c
@@ -3,7 +3,7 @@
  *  Copyright (C) 1998 Ross Ihaka
  *  Copyright (C) 2000 The R Development Core Team
  *  Copyright (C) 2004 The R Foundation
- *  based on AS91 (C) 1979 Royal Statistical Society
+ *  based on AS 91 (C) 1979 Royal Statistical Society
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -40,16 +40,82 @@
 #include "nmath.h"
 #include "dpq.h"
 
-double qgamma(double p, double alpha, double scale, int lower_tail, int log_p)
-/*			shape = alpha */
+#ifdef DEBUG_qgamma
+# define DEBUG_q
+# include <R_ext/Print.h>
+#endif
+
+double qchisq_appr(double p, double nu, double g/* = log Gamma(nu/2) */,
+		   int lower_tail, int log_p, double tol /* EPS1 */)
 {
 #define C7	4.67
 #define C8	6.66
 #define C9	6.73
 #define C10	13.32
 
+    double p_, alpha, a, c, ch, p1;
+    double p2, q, t, x;
+
+    /* test arguments and initialise */
+
+#ifdef IEEE_754
+    if (ISNAN(p) || ISNAN(nu))
+	return p + nu;
+#endif
+    R_Q_P01_check(p);
+    if (nu <= 0) ML_ERR_return_NAN;
+
+    p_ = R_DT_qIv(p);/* lower_tail prob (in any case) */
+
+    alpha = 0.5 * nu;/* = [pq]gamma() shape */
+    c = alpha-1;
+
+    if(nu < (-1.24)*(p1 = R_DT_log(p))) {	/* for small chi-squared */
+
+	ch = exp((log(alpha) + p1 + g)/alpha + M_LN2);
+#ifdef DEBUG_qgamma
+	REprintf(" small chi-sq., ch0 = %g\n", ch);
+#endif
+
+    } else if(nu > 0.32) {	/*  using Wilson and Hilferty estimate */
+
+	x = qnorm(p, 0, 1, lower_tail, log_p);
+	p1 = 2./(9*nu);
+	ch = nu*pow(x*sqrt(p1) + 1-p1, 3);
+
+#ifdef DEBUG_qgamma
+	REprintf(" nu > .32: Wilson-Hilferty; x = %7g\n", x);
+#endif
+	/* approximation for p tending to 1: */
+	if( ch > 2.2*nu + 6 )
+	    ch = -2*(R_DT_Clog(p) - c*log(0.5*ch) + g);
+
+    } else { /* 1.24*(-log(p)) <= nu <= 0.32 */
+
+	ch = 0.4;
+	a = R_DT_Clog(p) + g + c*M_LN2;
+#ifdef DEBUG_qgamma
+	REprintf(" nu <= .32: a = %7g\n", a);
+#endif
+	do {
+	    q = ch;
+	    p1 = 1. / (1+ch*(C7+ch));
+	    p2 = ch*(C9+ch*(C8+ch));
+	    t = -0.5 +(C7+2*ch)*p1 - (C9+ch*(C10+3*ch))/p2;
+	    ch -= (1- exp(a+0.5*ch)*p2*p1)/t;
+	} while(fabs(q - ch) > tol * fabs(ch));
+    }
+
+    return ch;
+}
+
+double qgamma(double p, double alpha, double scale, int lower_tail, int log_p)
+/*			shape = alpha */
+{
 #define EPS1 1e-2
-#define EPS2 5e-7/* final precision */
+#define EPS2 5e-7/* final precision of AS 91 */
+#define EPS_N 1e-15/* precision of Newton step / iterations */
+
 #define MAXIT 1000/* was 20 */
 
 #define pMIN 1e-100   /* was 0.000002 = 2e-6 */
@@ -60,9 +126,9 @@ double qgamma(double p, double alpha, double scale, int lower_tail, int log_p)
 	i2520 = 1./ 2520.,
 	i5040 = 1./ 5040;
 
-    double p_, a, b, c, ch, g, p1, v;
+    double p_, a, b, c, g, ch, ch0, p1;
     double p2, q, s1, s2, s3, s4, s5, s6, t, x;
-    int i;
+    int i, max_it_Newton = 1;
 
     /* test arguments and initialise */
 
@@ -73,127 +139,118 @@ double qgamma(double p, double alpha, double scale, int lower_tail, int log_p)
     R_Q_P01_check(p);
     if (alpha <= 0) ML_ERR_return_NAN;
 
-    /* FIXME: This (cutoff to {0, +Inf}) is far from optimal when log_p: */
     p_ = R_DT_qIv(p);/* lower_tail prob (in any case) */
-    v = 2*alpha;/* = 'nu' == df	 in qchisq() */
-
-    c = alpha-1;
-    g = lgammafn(alpha);/* log Gamma(v/2) */
-
-
-/*----- Phase I : Starting Approximation */
 
 #ifdef DEBUG_qgamma
     REprintf("qgamma(p=%7g, alpha=%7g, scale=%7g, l.t.=%2d, log_p=%2d): ",
 	     p,alpha,scale, lower_tail, log_p);
 #endif
+    g = lgammafn(alpha);/* log Gamma(v/2) */
 
-    if(v < (-1.24)*R_DT_log(p)) {	/* for small chi-squared */
+    /*----- Phase I : Starting Approximation */
+    ch = qchisq_appr(p, /* nu= 'df' =  */ 2*alpha, /* lgamma(nu/2)= */ g,
+		     lower_tail, log_p, /* tol= */ EPS1);
+    if(!R_FINITE(ch)) {
+	/* forget about all iterations! */
+	max_it_Newton = 0; goto END;
+    }
+    if(ch < EPS2) {/* Corrected according to AS 91; MM, May 25, 1999 */
+	max_it_Newton = 20;
+	goto END;/* and do Newton steps */
+    }
 
-#ifdef DEBUG_qgamma
-	REprintf(" small chi-sq.\n");
-#endif
-	/* FIXME: Improve this "if (log_p)" :
-	 *	  (A*exp(b)) ^ 1/al */
-	ch = pow(p_* alpha*exp(g+alpha*M_LN2), 1/alpha);
-	if(ch < EPS2) {/* Corrected according to AS 91; MM, May 25, 1999 */
-	    goto END;
-	}
-
-    } else if(v > 0.32) {	/*  using Wilson and Hilferty estimate */
-
-	x = qnorm(p, 0, 1, lower_tail, log_p);
-	p1 = 2./(9*v);
-	ch = v*pow(x*sqrt(p1) + 1-p1, 3);
-
-#ifdef DEBUG_qgamma
-	REprintf(" v > .32: Wilson-Hilferty; x = %7g\n", x);
-#endif
-	/* starting approximation for p tending to 1 */
-
-	if( ch > 2.2*v + 6 )
-	    ch = -2*(R_DT_Clog(p) - c*log(0.5*ch) + g);
-
-    } else { /* for v <= 0.32 */
-
-	ch = 0.4;
-	a = R_DT_Clog(p) + g + c*M_LN2;
-#ifdef DEBUG_qgamma
-	REprintf(" v <= .32: a = %7g\n", a);
-#endif
-	do {
-	    q = ch;
-	    p1 = 1. / (1+ch*(C7+ch));
-	    p2 = ch*(C9+ch*(C8+ch));
-	    t = -0.5 +(C7+2*ch)*p1 - (C9+ch*(C10+3*ch))/p2;
-	    ch -= (1- exp(a+0.5*ch)*p2*p1)/t;
-	} while(fabs(q - ch) > EPS1*fabs(ch));
+    /* FIXME: This (cutoff to {0, +Inf}) is far from optimal
+     * -----  when log_p or !lower_tail : */
+    if(p_ > pMAX || p_ < pMIN) {
+	/* did return ML_POSINF or 0.;	much better: */
+	max_it_Newton = 20;
+	goto END;/* and do Newton steps */
     }
 
 #ifdef DEBUG_qgamma
     REprintf("\t==> ch = %10g:", ch);
 #endif
 
-    if(p_ > pMAX || p_ < pMIN)
-	/* did return ML_POSINF or 0.;	much better: */
-	goto END;/* and do 1 a Newton step -- FIXME: do more than one */
-
 /*----- Phase II: Iteration
  *	Call pgamma() [AS 239]	and calculate seven term taylor series
  */
-    for( i=1 ; i <= MAXIT ; i++ ) {
+    c = alpha-1;
+    s6 = (120+c*(346+127*c)) * i5040; /* used below, is "const" */
+
+    ch0 = ch;/* save initial approx. */
+    for(i=1; i <= MAXIT; i++ ) {
 	q = ch;
 	p1 = 0.5*ch;
 	p2 = p_ - pgamma(p1, alpha, 1, /*lower_tail*/TRUE, /*log_p*/FALSE);
+#ifdef DEBUG_qgamma
+	if(i == 1) Rprintf(" Ph.II iter; ch=%g, p2=%g\n", ch, p2);
+	if(i >= 2) Rprintf("     it=%d,  ch=%g, p2=%g\n", i, p2);
+#endif
 #ifdef IEEE_754
 	if(!R_FINITE(p2))
 #else
 	if(errno != 0)
 #endif
-		return ML_NAN;
+	    { ch = ch0; max_it_Newton = 27; goto END; }/*was  return ML_NAN;*/
 
 	t = p2*exp(alpha*M_LN2+g+p1-c*log(ch));
 	b = t/ch;
 	a = 0.5*t - b*c;
-	s1 = (210+a*(140+a*(105+a*(84+a*(70+60*a))))) * i420;
-	s2 = (420+a*(735+a*(966+a*(1141+1278*a)))) * i2520;
-	s3 = (210+a*(462+a*(707+932*a))) * i2520;
-	s4 = (252+a*(672+1182*a)+c*(294+a*(889+1740*a))) * i5040;
-	s5 = (84+2264*a+c*(1175+606*a)) * i2520;
-	s6 = (120+c*(346+127*c)) * i5040;
+	s1 = (210+ a*(140+a*(105+a*(84+a*(70+60*a))))) * i420;
+	s2 = (420+ a*(735+a*(966+a*(1141+1278*a)))) * i2520;
+	s3 = (210+ a*(462+a*(707+932*a))) * i2520;
+	s4 = (252+ a*(672+1182*a) + c*(294+a*(889+1740*a))) * i5040;
+	s5 = (84+2264*a + c*(1175+606*a)) * i2520;
+
 	ch += t*(1+0.5*t*s1-b*c*(s1-b*(s2-b*(s3-b*(s4-b*(s5-b*s6))))));
 	if(fabs(q - ch) < EPS2*ch)
 	    goto END;
     }
-/* no convergence in MAXIT iterations */
+/* no convergence in MAXIT iterations -- but we add Newton now... */
 #ifdef DEBUG_q
     MATHLIB_WARNING3("qgamma(%g) not converged in %d iterations; rel.ch=%g\n",
 		     p, MAXIT, ch/fabs(q - ch));
-#else
-    ML_ERROR(ME_PRECISION);/* does nothing in R ! */
 #endif
+/* was
+ *    ML_ERROR(ME_PRECISION);
+ * does nothing in R !*/
 
 END:
 /* PR# 2214 :	 From: Morten Welinder <terra@diku.dk>, Fri, 25 Oct 2002 16:50
    --------	 To: R-bugs@biostat.ku.dk     Subject: qgamma precision
 
-   * With a final Newton step, double accuracy, e.g. for (p= 7e-4; df= 0.9)
+   * With a final Newton step, double accuracy, e.g. for (p= 7e-4; nu= 0.9)
    *
-   * Improved (MM): only if rel.Err > 1e-15;
-   *		    also for lower_tail = FALSE	 or log_p = TRUE
-*/
+   * Improved (MM): - only if rel.Err > EPS_N (= 1e-15);
+   *		    - also for lower_tail = FALSE	 or log_p = TRUE
+   * 		    - optionally *iterate* Newton
+   */
     x = 0.5*scale*ch;
-    p_ = pgamma(x, alpha, scale, lower_tail, log_p);
-    if(fabs(p1 = p_ - p) >= fabs(1e-15*p) &&
-       (g = dgamma(x, alpha, scale, FALSE)) != 0)
-    {
-	/* delta x = f(x)/f'(x);
-	 * if(log_p) f(x) := log P(x) - p; f'(x) = d/dx log P(x) = P' / P
-	 * ==> f(x)/f'(x) = f*P / P' = f*exp(p_) / P' (since p_ = log P(x))*/
-	t = log_p ? p1*exp(p_)/g : p1/g ;/* = "delta x" */
-	t = lower_tail ? x - t : x + t;
-	if (fabs(pgamma (t, alpha, scale, lower_tail, log_p) - p) < fabs(p1))
-	    x = t;
+
+    if(max_it_Newton)
+	p_ = pgamma(x, alpha, scale, lower_tail, log_p);
+    for(i = 1; i <= max_it_Newton; i++) {
+	p1 = p_ - p;
+#ifdef DEBUG_qgamma
+	if(i == 2) Rprintf(" p=%g, N{it=%d}, d{p}=%g\n", p, i, p_ - p);
+	if(i >= 3) Rprintf("         it=%d,  d{p}=%g\n", i, p_ - p);
+#endif
+	if(fabs(p1) < fabs(EPS_N * p) ||
+	   (g = dgamma(x, alpha, scale, FALSE)) == 0) {
+	    break;
+	}
+	else {
+	    /* delta x = f(x)/f'(x);
+	     * if(log_p) f(x) := log P(x) - p; f'(x) = d/dx log P(x) = P' / P
+	     * ==> f(x)/f'(x) = f*P / P' = f*exp(p_) / P' (since p_ = log P(x))
+	     */
+	    t = log_p ? p1*exp(p_)/g : p1/g ;/* = "delta x" */
+	    t = lower_tail ? x - t : x + t;
+	    p_ = pgamma (t, alpha, scale, lower_tail, log_p);
+	    if (fabs(p_ - p) >= fabs(p1))/* no improvement */
+		break;
+	    else x = t;
+	}
     }
     return x;
 }
