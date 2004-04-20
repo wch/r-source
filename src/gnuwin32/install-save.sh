f7c9df9d756a384511bca5a56b5f45c68a0207d4
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2004-04-20 14:37:54 +0000

    branch update
    git-svn-id: https://svn.r-project.org/R/trunk@29085 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/install-save.sh
+++ src/gnuwin32/install-save.sh
@@ -11,14 +11,15 @@ case $1 in
 esac
 export R_SAVE_IMAGE
 
-if test "x${BUILD}" = "xCROSS"; then
-R_DEFAULT_PACKAGES=NULL
+## R_START_PACKAGES is set to NULL during bootstrapping
+R_DEFAULT_PACKAGES=${R_START_PACKAGES}
 export R_DEFAULT_PACKAGES
+if test "x${BUILD}" = "xCROSS"; then
 R_EXE="${RX_EXE} --slave --no-site-file --no-init-file"
 ## we have to install into a temporary dir to avoid libpath problems
 lib1=/tmp/RtmpWin
 else
-R_EXE="${R_HOME}/bin/Rterm --slave --no-site-file --no-init-file R_DEFAULT_PACKAGES=NULL"
+R_EXE="${R_HOME}/bin/Rterm --slave --no-site-file --no-init-file"
 lib1=${lib}
 fi
 
