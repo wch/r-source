611abb18987c09f5ae2e3b3b06bb43fc786e5763
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2004-06-09 13:08:39 +0000

    start splitting up src/unix/sys-common.c into smaller components
    git-svn-id: https://svn.r-project.org/R/trunk@29747 00db46b3-68df-0310-9c12-caf00c1e9a41

---
+++ src/main/Renviron.c
@@ -0,0 +1,191 @@
+/*
+  R : A Computer Language for Statistical Data Analysis
+  Copyright (C) 1997-2004   Robert Gentleman, Ross Ihaka
+                            and the R Development Core Team
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or (at
+  your option) any later version.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
+  U.S.A.
+ */
+
+/* ------------------- process .Renviron files in C -----------------
+ *  Formally part of ../unix/sys-common.c. 
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+#include <Defn.h> /* for PATH_MAX */
+
+
+/* remove leading and trailing space */
+static char *rmspace(char *s)
+{
+    int   i;
+
+    for (i = strlen(s) - 1; i >= 0 && isspace((int)s[i]); i--) s[i] = '\0';
+    for (i = 0; isspace((int)s[i]); i++);
+    return s + i;
+}
+
+/* look for ${FOO:-bar} constructs, recursively */
+static char *findterm(char *s)
+{
+    char *p, *q;
+
+    if(!strlen(s)) return "";
+    if(strncmp(s, "${", 2)) return s;
+    /* found one, so remove leading ${ and final } */
+    if(s[strlen(s) - 1] != '}') return "";
+    s[strlen(s) - 1] = '\0';
+    s += 2;
+    p = strchr(s, '-');
+    if(!p) return "";
+    q = p + 1; /* start of value */
+    if(p - s > 1 && *(p-1) == ':') *(p-1) = '\0'; else *p = '\0';
+    s = rmspace(s);
+    if(!strlen(s)) return "";
+    p = getenv(s);
+    if(p && strlen(p)) return p; /* variable was set and non-empty */
+    return findterm(q);
+}
+
+static void Putenv(char *a, char *b)
+{
+    char *buf, *value, *p, *q, quote='\0';
+    int inquote = 0;
+
+    buf = (char *) malloc((strlen(a) + strlen(b) + 2) * sizeof(char));
+    if(!buf) R_Suicide("allocation failure in reading Renviron");
+    strcpy(buf, a); strcat(buf, "=");
+    value = buf+strlen(buf);
+
+    /* now process the value */
+    for(p = b, q = value; *p; p++) {
+	/* remove quotes around sections, preserve \ inside quotes */
+	if(!inquote && (*p == '"' || *p == '\'')) {
+	    inquote = 1;
+	    quote = *p;
+	    continue;
+	}
+	if(inquote && *p == quote && *(p-1) != '\\') {
+	    inquote = 0;
+	    continue;
+	}
+	if(!inquote && *p == '\\') {
+	    if(*(p+1) == '\n') p++;
+	    else if(*(p+1) == '\\') *q++ = *p;
+	    continue;
+	}
+	if(inquote && *p == '\\' && *(p+1) == quote) continue;
+	*q++ = *p;
+    }
+    *q = '\0';
+    putenv(buf);
+    /* no free here: storage remains in use */
+}
+
+
+#define BUF_SIZE 255
+#define MSG_SIZE 2000
+static int process_Renviron(char *filename)
+{
+    FILE *fp;
+    char *s, *p, sm[BUF_SIZE], *lhs, *rhs, msg[MSG_SIZE+50];
+    int errs = 0;
+
+    if (!filename || !(fp = fopen(filename, "r"))) return 0;
+    snprintf(msg, MSG_SIZE+50, 
+	     "\n   File %s contains invalid line(s)", filename);
+
+    while(fgets(sm, BUF_SIZE, fp)) {
+        sm[BUF_SIZE-1] = '\0';
+	s = rmspace(sm);
+	if(strlen(s) == 0 || s[0] == '#') continue;
+	if(!(p = strchr(s, '='))) {
+	    errs++;
+	    if(strlen(msg) < MSG_SIZE) {
+		strcat(msg, "\n      "); strcat(msg, s);
+	    }
+	    continue;
+	}
+	*p = '\0';
+	lhs = rmspace(s);
+	rhs = findterm(rmspace(p+1));
+	/* set lhs = rhs */
+	if(strlen(lhs) && strlen(rhs)) Putenv(lhs, rhs);
+    }
+    fclose(fp);
+    if (errs) {
+	strcat(msg, "\n   They were ignored\n");
+	R_ShowMessage(msg);
+    }
+    return 1;
+}
+
+
+/* try system Renviron: R_HOME/etc/Renviron.  Unix only. */
+void process_system_Renviron()
+{
+    char buf[PATH_MAX];
+
+    if(strlen(R_Home) + strlen("/etc/Renviron") > PATH_MAX - 1) {
+	R_ShowMessage("path to system Renviron is too long: skipping");
+	return;
+    }
+    strcpy(buf, R_Home);
+    strcat(buf, "/etc/Renviron");
+    if(!process_Renviron(buf))
+	R_ShowMessage("cannot find system Renviron");
+}
+
+/* try site Renviron: R_ENVIRON, then R_HOME/etc/Renviron.site. */
+void process_site_Renviron ()
+{
+    char buf[PATH_MAX], *p = getenv("R_ENVIRON");
+
+    if(p && strlen(p)) {
+	process_Renviron(p);
+	return;
+    }
+    if(strlen(R_Home) + strlen("/etc/Renviron.site") > PATH_MAX - 1) {
+	R_ShowMessage("path to Renviron.site is too long: skipping");
+	return;
+    }
+    snprintf(buf, PATH_MAX, "%s/etc/Renviron.site", R_Home);
+    process_Renviron(buf);
+}
+
+/* try user Renviron: ./.Renviron, then ~/.Renviron */
+void process_user_Renviron()
+{
+    char *s;
+
+    if(process_Renviron(".Renviron")) return;
+#ifdef Unix
+    s = R_ExpandFileName("~/.Renviron");
+#endif
+#ifdef Win32
+    {
+	char buf[1024];
+	/* R_USER is not necessarily set yet, so we have to work harder */
+	s = getenv("R_USER");
+	if(!s) s = getenv("HOME");
+	if(!s) return;
+	snprintf(buf, 1024, "%s/.Renviron", s);
+	s = buf;
+    }
+#endif
+    process_Renviron(s);
+}
---
+++ src/main/sysutils.c
@@ -0,0 +1,276 @@
+/*
+  R : A Computer Language for Statistical Data Analysis
+  Copyright (C) 1995-1996   Robert Gentleman and Ross Ihaka
+  Copyright (C) 1997-2004   Robert Gentleman, Ross Ihaka
+                            and the R Development Core Team
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or (at
+  your option) any later version.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
+  U.S.A.
+ */
+
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+#include <Defn.h>
+
+/*
+  See ../unix/system.txt for a description of some of these functions.
+  Formally part of ../unix/sys-common.c.
+ */
+
+/* The __APPLE__ and __APPLE_CC__ defines are for OS X */
+
+/*
+ * FILESYSTEM INTERACTION
+ */
+
+/*
+ * This call provides a simple interface to the "stat" system call.
+ */
+
+#ifdef HAVE_STAT
+#include <sys/types.h>
+#ifdef HAVE_SYS_STAT_H
+# include <sys/stat.h>
+#endif
+
+Rboolean R_FileExists(char *path)
+{
+    struct stat sb;
+    return stat(R_ExpandFileName(path), &sb) == 0;
+}
+
+double R_FileMtime(char *path)
+{
+    struct stat sb;
+    if (stat(R_ExpandFileName(path), &sb) != 0)
+	error("cannot determine file modification time of %s", path);
+    return sb.st_mtime;
+}
+#else
+Rboolean R_FileExists(char *path)
+{
+    error("file existence is not available on this system");
+}
+
+double R_FileMtime(char *path)
+{
+    error("file modification time is not available on this system");
+    return 0.0; /* not reached */
+}
+#endif
+
+    /*
+     *  Unix file names which begin with "." are invisible.
+     */
+
+Rboolean R_HiddenFile(char *name)
+{
+    if (name && name[0] != '.') return 0;
+    else return 1;
+}
+
+
+FILE *R_fopen(const char *filename, const char *mode)
+{
+    return(filename ? fopen(filename, mode) : NULL );
+}
+
+/*
+ *  SYSTEM INFORMATION
+ */
+
+          /* The location of the R system files */
+
+char *R_HomeDir()
+{
+    return getenv("R_HOME");
+}
+
+
+SEXP do_interactive(SEXP call, SEXP op, SEXP args, SEXP rho)
+{
+    SEXP rval;
+
+    rval=allocVector(LGLSXP, 1);
+    LOGICAL(rval)[0]= (R_Interactive) ? 1 : 0;
+    return rval;
+}
+
+SEXP do_tempdir(SEXP call, SEXP op, SEXP args, SEXP env)
+{
+    SEXP  ans;
+
+    PROTECT(ans = allocVector(STRSXP, 1));
+    SET_STRING_ELT(ans, 0, mkChar(R_TempDir));
+    UNPROTECT(1);
+    return (ans);
+}
+
+
+SEXP do_tempfile(SEXP call, SEXP op, SEXP args, SEXP env)
+{
+    SEXP  ans, pattern, tempdir;
+    char *tn, *td, *tm;
+    int i, n1, n2, slen;
+
+    checkArity(op, args);
+    pattern = CAR(args); n1 = length(pattern);
+    tempdir = CADR(args); n2 = length(tempdir);
+    if (!isString(pattern))
+        errorcall(call, "invalid filename pattern");
+    if (!isString(tempdir))
+        errorcall(call, "invalid tempdir");
+    if (n1 < 1)
+	errorcall(call, "no patterns");
+    if (n2 < 1)
+	errorcall(call, "no tempdir");
+    slen = (n1 > n2) ? n1 : n2;
+    PROTECT(ans = allocVector(STRSXP, slen));
+    for(i = 0; i < slen; i++) {
+	tn = CHAR( STRING_ELT( pattern , i%n1 ) );
+	td = CHAR( STRING_ELT( tempdir , i%n2 ) );
+	/* try to get a new file name */
+	tm = R_tmpnam(tn, td);
+	SET_STRING_ELT(ans, i, mkChar(tm));
+	if(tm) free(tm);
+    }
+    UNPROTECT(1);
+    return (ans);
+}
+
+#ifdef HAVE_POPEN
+FILE *R_popen(char *command, char *type)
+{
+    FILE *fp;
+#ifdef __APPLE_CC__
+    /* Luke recommends this to fix PR#1140 */
+    sigset_t ss;
+    sigaddset(&ss, SIGPROF);
+    sigprocmask(SIG_BLOCK, &ss,  NULL);
+    fp = popen(command, type);
+    sigprocmask(SIG_UNBLOCK, &ss, NULL);
+#else
+    fp = popen(command, type);
+#endif
+    return fp;
+}
+#endif /* HAVE_POPEN */
+
+int R_system(char *command)
+{
+    int val;
+#ifdef __APPLE_CC__
+    /* Luke recommends this to fix PR#1140 */
+    sigset_t ss;
+    sigaddset(&ss, SIGPROF);
+    sigprocmask(SIG_BLOCK, &ss,  NULL);
+    val = system(command);
+    sigprocmask(SIG_UNBLOCK, &ss, NULL);
+#else
+    val = system(command);
+#endif
+    return val;
+}
+
+#ifdef Win32
+# include <windows.h>
+#elif defined(__APPLE__)
+# include <crt_externs.h>
+# define environ (*_NSGetEnviron())
+#else
+extern char ** environ;
+#endif
+
+SEXP do_getenv(SEXP call, SEXP op, SEXP args, SEXP env)
+{
+    int i, j;
+    char *s;
+    SEXP ans;
+
+    checkArity(op, args);
+
+    if (!isString(CAR(args)))
+	errorcall(call, "wrong type for argument");
+
+    i = LENGTH(CAR(args));
+    if (i == 0) {
+#ifdef Win32
+	char *envir, *e;
+	envir = (char *) GetEnvironmentStrings();
+	for (i = 0, e = envir; strlen(e) > 0; i++, e += strlen(e)+1);
+	PROTECT(ans = allocVector(STRSXP, i));
+	for (i = 0, e = envir; strlen(e) > 0; i++, e += strlen(e)+1)
+	    SET_STRING_ELT(ans, i, mkChar(e));
+	FreeEnvironmentStrings(envir);
+#else
+	char **e;
+	for (i = 0, e = environ; *e != NULL; i++, e++);
+	PROTECT(ans = allocVector(STRSXP, i));
+	for (i = 0, e = environ; *e != NULL; i++, e++)
+	    SET_STRING_ELT(ans, i, mkChar(*e));
+#endif
+    } else {
+	PROTECT(ans = allocVector(STRSXP, i));
+	for (j = 0; j < i; j++) {
+	    s = getenv(CHAR(STRING_ELT(CAR(args), j)));
+	    if (s == NULL)
+		SET_STRING_ELT(ans, j, mkChar(""));
+	    else
+		SET_STRING_ELT(ans, j, mkChar(s));
+	}
+    }
+    UNPROTECT(1);
+    return (ans);
+}
+
+#ifdef HAVE_PUTENV
+static int Rputenv(char *str)
+{
+    char *buf;
+    buf = (char *) malloc((strlen(str) + 1) * sizeof(char));
+    if(!buf) return 1;
+    strcpy(buf, str);
+    putenv(buf);
+    /* no free here: storage remains in use */
+    return 0;
+}
+#endif
+
+
+SEXP do_putenv(SEXP call, SEXP op, SEXP args, SEXP env)
+{
+#ifdef HAVE_PUTENV
+    int i, n;
+    SEXP ans, vars;
+
+    checkArity(op, args);
+
+    if (!isString(vars =CAR(args)))
+	errorcall(call, "wrong type for argument");
+
+    n = LENGTH(vars);
+    PROTECT(ans = allocVector(LGLSXP, n));
+    for (i = 0; i < n; i++) {
+	LOGICAL(ans)[i] = Rputenv(CHAR(STRING_ELT(vars, i))) == 0;
+    }
+    UNPROTECT(1);
+    return ans;
+#else
+    error("`putenv' is not available on this system");
+    return R_NilValue; /* -Wall */
+#endif
+}
