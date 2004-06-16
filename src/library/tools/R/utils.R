6e2d9d2d47f90070e74cebd8ac371cd7dba0d80b
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2004-06-16 09:02:28 +0000

    merged from main
    git-svn-id: https://svn.r-project.org/R/branches/rawtype@29862 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/utils.R
+++ src/library/tools/R/utils.R
@@ -229,6 +229,17 @@ function(nsInfo)
 function()
     c("Ops", "Math", "Summary", "Complex")
 
+### ** .get_standard_Rd_keywords
+
+.get_standard_Rd_keywords <-
+function()
+{
+    lines <- readLines(file.path(R.home(), "doc", "KEYWORDS.db"))
+    lines <- grep("^.*\\\|([^:]*):.*", lines, value = TRUE)
+    lines <- sub("^.*\\\|([^:]*):.*", "\\1", lines)
+    lines
+}
+
 ### ** .is_primitive
 
 .is_primitive <-
