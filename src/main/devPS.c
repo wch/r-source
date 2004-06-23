81b5c18be96c1733df37d9c3a4ccec3bf7035e09
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2004-06-23 06:36:52 +0000

    branch update
    git-svn-id: https://svn.r-project.org/R/trunk@29983 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/devPS.c
+++ src/main/devPS.c
@@ -1,7 +1,7 @@
 /*
  *  R : A Computer Language for Statistical Data Analysis
  *  Copyright (C) 1995, 1996  Robert Gentleman and Ross Ihaka
- *  Copyright (C) 1998--2003  Robert Gentleman, Ross Ihaka and the
+ *  Copyright (C) 1998--2004  Robert Gentleman, Ross Ihaka and the
  *                            R Development Core Team
  *
  *  This program is free software; you can redistribute it and/or modify
