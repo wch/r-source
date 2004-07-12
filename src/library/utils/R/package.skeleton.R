291a14b260f6374b8fe6b211c333f0fa9ce4a6db
murdoch <murdoch@00db46b3-68df-0310-9c12-caf00c1e9a41>
2004-07-12 13:17:26 +0000

    package.skeleton() creates legal DESCRIPTION
    git-svn-id: https://svn.r-project.org/R/branches/R-1-9-patches@30265 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/utils/R/package.skeleton.R
+++ src/library/utils/R/package.skeleton.R
@@ -36,7 +36,7 @@ function(name = "anRpackage", list, environment = .GlobalEnv,
     ## DESCRIPTION
     cat("Creating DESCRIPTION ...\n")
     description <- file(file.path(path, name, "DESCRIPTION"), "wt")
-    cat("Package: the_name_of_the_package\n",
+    cat("Package: ", name, "\n",
         "Title: What the package does\n",
         "Version: 1.0\n",
         "Author: Who wrote it\n",
