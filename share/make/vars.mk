db77d41a9714ae0385202f88fb30f36bb8ef3703
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2004-08-04 10:01:13 +0000

    no longer need survival before boot
    git-svn-id: https://svn.r-project.org/R/trunk@30502 00db46b3-68df-0310-9c12-caf00c1e9a41

--- share/make/vars.mk
+++ share/make/vars.mk
@@ -3,7 +3,7 @@
 
 R_PKGS_BASE = base tools utils graphics stats datasets methods grid splines stats4 tcltk
 R_PKGS_STUBS = ctest eda lqs mle modreg mva nls stepfun ts
-# mgcv links to nlme, boot used to link to survival
-R_PKGS_RECOMMENDED_SOURCES = survival boot cluster foreign KernSmooth lattice nlme mgcv rpart VR
-R_PKGS_RECOMMENDED = survival boot cluster foreign KernSmooth lattice nlme mgcv rpart MASS class nnet spatial
+# mgcv links to nlme
+R_PKGS_RECOMMENDED_SOURCES = boot cluster foreign KernSmooth lattice nlme mgcv rpart survival VR
+R_PKGS_RECOMMENDED = boot cluster foreign KernSmooth lattice nlme mgcv rpart survival MASS class nnet spatial
 
