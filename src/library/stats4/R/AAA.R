fc6a5b597c73dd21603bfc75b6f355b26a6b9091
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2004-08-11 10:52:31 +0000

    make work correctly from R_DEFAULT_PACKAGES=NULL install.R is not longer required on Windows
    git-svn-id: https://svn.r-project.org/R/trunk@30606 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats4/R/AAA.R
+++ src/library/stats4/R/AAA.R
@@ -1,2 +1,8 @@
-require(methods)
+### dumping for standard packages is with R_DEFAULT_PACKAGES=NULL
+require(graphics); require(stats); require(methods)
 
+.onLoad <- function(lib, pkg) {
+    require(graphics, save=FALSE) || stop("package 'graphics' is missing")
+    require(stats, save=FALSE) || stop("package 'graphics' is missing")
+    require(methods, save=FALSE) || stop("package 'graphics' is missing")
+}
