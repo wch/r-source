f589d818e5518aaee7e98be5b3282127484e908c
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2004-08-11 18:11:24 +0000

    tidy up Makefile, as does have NAMESPACE make use of new facilities to preload methods
    git-svn-id: https://svn.r-project.org/R/trunk@30619 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats4/R/AAA.R
+++
@@ -1,8 +0,0 @@
-### dumping for standard packages is with R_DEFAULT_PACKAGES=NULL
-require(graphics); require(stats); require(methods)
-
-.onLoad <- function(lib, pkg) {
-    require(graphics, save=FALSE) || stop("package 'graphics' is missing")
-    require(stats, save=FALSE) || stop("package 'graphics' is missing")
-    require(methods, save=FALSE) || stop("package 'graphics' is missing")
-}
