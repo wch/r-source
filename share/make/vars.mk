925740d5920db1fdd7340d2f798004a41fa325fb
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2004-08-18 14:13:54 +0000

    mgcv currently Depends on MASS as well as nlme
    git-svn-id: https://svn.r-project.org/R/trunk@30750 00db46b3-68df-0310-9c12-caf00c1e9a41

--- share/make/vars.mk
+++ share/make/vars.mk
@@ -3,7 +3,7 @@
 
 R_PKGS_BASE = base tools utils grDevices graphics stats datasets methods grid splines stats4 tcltk
 R_PKGS_STUBS = ctest eda lqs mle modreg mva nls stepfun ts
-# mgcv links to nlme
-R_PKGS_RECOMMENDED_SOURCES = boot cluster foreign KernSmooth lattice nlme mgcv rpart survival VR
+# mgcv requires nlme and MASS
+R_PKGS_RECOMMENDED_SOURCES = VR boot cluster foreign KernSmooth lattice nlme mgcv rpart survival
 R_PKGS_RECOMMENDED = boot cluster foreign KernSmooth lattice nlme mgcv rpart survival MASS class nnet spatial
 
