4820683fb194cf9048a4bc4139163312d2c7a523
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2004-08-28 21:06:15 +0000

    Eliminate internal .shell_quote() in favor of shQuote() (now in base).
    git-svn-id: https://svn.r-project.org/R/trunk@30908 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/Scripts.R
+++ src/library/base/R/Scripts.R
@@ -1,13 +1,6 @@
 .Script <-
 function(interpreter, script, args, ...)
 {
-    ## <FIXME>
-    ## Keep in sync with tools:::.shell_quote, or do something smarter
-    ## eventually (such as using utils::shQuote) ...
-    .shell_quote <- sQuote
-    ## Also, what about Windows?
-    ## </FIXME>
-    
     if(.Platform$OS.type == "windows") {
         cmd <- paste(file.path(R.home(), "bin", "Rcmd"),
                      file.path("..", "share", interpreter, script),
@@ -15,10 +8,10 @@ function(interpreter, script, args, ...)
         system(cmd, invisible = TRUE)
     }
     else
-        system(paste(.shell_quote(file.path(R.home(), "bin", "Rcmd")),
+        system(paste(shQuote(file.path(R.home(), "bin", "Rcmd")),
                      interpreter,
-                     .shell_quote(file.path(R.home(), "share",
-                                            interpreter, script)),
+                     shQuote(file.path(R.home(), "share",
+                                       interpreter, script)),
                      args),
                ...)
 }
--- src/library/tools/R/utils.R
+++ src/library/tools/R/utils.R
@@ -150,9 +150,9 @@ function(file, pdf = FALSE, clean = FALSE,
             texi2dvi <- "texi2dvi"
     }
 
-    yy <- system(paste(.shell_quote(texi2dvi),
+    yy <- system(paste(shQuote(texi2dvi),
                        quiet, pdf, clean,
-                       .shell_quote(file)))
+                       shQuote(file)))
     if(yy > 0) stop(paste("running texi2dvi on", file, "failed"))
 }
 
@@ -468,21 +468,6 @@ function(dfile)
     if(inherits(db, "try-error"))
         stop(paste("file", sQuote(dfile), "is not in valid DCF format"))
     db
-}
-
-### ** .shell_quote
-
-.shell_quote <-
-function(x)
-{
-    ## Quote elements of a character vector x for passing them to a
-    ## (Bourne) shell.
-    ## Currently only does simple single quoting (so that embedded
-    ## whitespace is taken care of).
-    ## <FIXME>
-    ## We already have utils::shQuote() which is much better ...
-    sQuote(x)
-    ## </FIXME>
 }
 
 ### ** .source_assignments
