9ff14c237db9d0322460ae089566627611adc6cf
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2004-08-30 13:44:11 +0000

    :: and ::: were not being deparsed correctly if promises
    git-svn-id: https://svn.r-project.org/R/trunk@30936 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/deparse.c
+++ src/main/deparse.c
@@ -992,15 +992,21 @@ static void deparse2buff(SEXP s, LocalParseData *d)
 		    break;
 		}
 		else {
+		    SEXP val = R_NilValue; /* -Wall */
+		    if (isSymbol(CAR(s))) {
+			val = SYMVALUE(CAR(s));
+			if (TYPEOF(val) == PROMSXP)
+			    val = eval(val, R_NilValue);
+		    }
 		    if ( isSymbol(CAR(s))
-		      && TYPEOF(SYMVALUE(CAR(s))) == CLOSXP
+		      && TYPEOF(val) == CLOSXP
 		      && streql(CHAR(PRINTNAME(CAR(s))), "::") ){ /*  :: is special case */
 		    	deparse2buff(CADR(s), d);
 		    	print2buff("::", d);
 			deparse2buff(CADDR(s), d);
 		    }
 		    else if ( isSymbol(CAR(s))
-		      && TYPEOF(SYMVALUE(CAR(s))) == CLOSXP
+		      && TYPEOF(SYMVALUE(val)) == CLOSXP
 		      && streql(CHAR(PRINTNAME(CAR(s))), ":::") ){ /*  ::: is special case */
 		    	deparse2buff(CADR(s), d);
 		    	print2buff(":::", d);
