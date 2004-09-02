ecdb28437f55c86cb5fcbbe5d722f096cae637c8
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2004-09-02 11:02:46 +0000

    cannot assume share/make/vars.mk is installed
    git-svn-id: https://svn.r-project.org/R/trunk@31007 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/utils.R
+++ src/library/tools/R/utils.R
@@ -289,16 +289,17 @@ function()
 
 ### ** .get_standard_package_names
 
+## we cannot assume that file.path(R.home(), "share", "make", "vars.mk")
+## is installed, as it is not on Windows
 .get_standard_package_names <-
-function()
-{
+local({
     lines <- readLines(file.path(R.home(), "share", "make", "vars.mk"))
     lines <- grep("^R_PKGS_[[:upper:]]+ *=", lines, value = TRUE)
     out <- strsplit(sub("^R_PKGS_[[:upper:]]+ *= *", "", lines), " +")
     names(out) <-
         tolower(sub("^R_PKGS_([[:upper:]]+) *=.*", "\\1", lines))
-    out
-}
+    eval(substitute(function() {out}, list(out=out)), envir=NULL)
+})
 
 ### ** .is_primitive
 
