7eb3a52e6f1ca53d76e65a521cc86ebfcebf9ed4
murrell <murrell@00db46b3-68df-0310-9c12-caf00c1e9a41>
2004-09-19 21:09:38 +0000

    Bug fix from Uwe Ligges
    git-svn-id: https://svn.r-project.org/R/trunk@31203 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/plotmath.c
+++ src/main/plotmath.c
@@ -1693,7 +1693,8 @@ static BBOX RenderAccent(SEXP expr, int draw, mathContext *mc, R_GE_gcontext *gc
     }
     bodyBBox = CombineOffsetBBoxes(bodyBBox, 0, accentBBox, 0,
 				   xoffset, yoffset);
-    PMoveTo(savedX + width, savedY, mc);
+    if (draw)
+	PMoveTo(savedX + width, savedY, mc);
     return bodyBBox;
 }
 
