0a6e6b808d8f6e3987cb4f5a1c7f4c6226c04f7c
(no author) <(no author)@00db46b3-68df-0310-9c12-caf00c1e9a41>
2004-09-23 11:51:15 +0000

    process events called in Cocoa GUI. substituted || with && in R_PRocessEvents
    git-svn-id: https://svn.r-project.org/R/trunk@31222 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/unix/aqua.c
+++ src/unix/aqua.c
@@ -408,7 +408,7 @@ void R_ProcessEvents(void)
     EventTargetRef theTarget;
     bool	conv = false;
 
-    if(!useaqua || !useCocoa){
+    if(!useaqua && !useCocoa){
 		if (R_interrupts_pending)
 			onintr();
 		return;
