57ffb78458ddb42488007d59d429d618629b13b6
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2004-10-23 07:47:06 +0000

    move approx{fun} from base to stats
    git-svn-id: https://svn.r-project.org/R/trunk@31570 00db46b3-68df-0310-9c12-caf00c1e9a41

---
+++ src/library/base/R/findInt.R
@@ -0,0 +1,30 @@
+### This is a `variant' of  approx( method = "constant" ) :
+findInterval <- function(x, vec, rightmost.closed = FALSE, all.inside = FALSE)
+{
+    ## Purpose: gives back the indices of  x in vec;  vec[] sorted
+    ## -------------------------------------------------------------------------
+    ## Author: Martin Maechler, Date:  4 Jan 2002, 10:16
+
+    if(any(is.na(vec)))
+	stop(sQuote("vec")," contains NAs")
+    if(is.unsorted(vec))
+	stop(sQuote("vec")," must be sorted non-decreasingly")
+    ## deal with NA's in x:
+    if(has.na <- any(ix <- is.na(x)))
+	x <- x[!ix]
+    nx <- length(x)
+    index <- integer(nx)
+    .C("find_interv_vec",
+       xt = as.double(vec), n = as.integer(length(vec)),
+       x  = as.double(x),  nx = as.integer(nx),
+       as.logical(rightmost.closed),
+       as.logical(all.inside),
+       index, DUP = FALSE, NAOK = TRUE, # NAOK: 'Inf' only
+       PACKAGE = "base")
+    if(has.na) {
+	ii <- as.integer(ix)
+	ii[ix] <- NA
+	ii[!ix] <- index
+	ii
+    } else index
+}
