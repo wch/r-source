d5cc8de1c1c74b133fc569e9d53da76c61b58d00
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2004-10-30 09:16:04 +0000

    suppress -Wall warning
    git-svn-id: https://svn.r-project.org/R/trunk@31685 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/objects.c
+++ src/main/objects.c
@@ -339,7 +339,7 @@ int usemethod(char *generic, SEXP obj, SEXP call, SEXP args,
 
 SEXP do_usemethod(SEXP call, SEXP op, SEXP args, SEXP env)
 {
-    SEXP ans, generic, obj;
+    SEXP ans, generic = R_NilValue /* -Wall */, obj;
     SEXP callenv, defenv;
     int nargs;
     RCNTXT *cptr;
@@ -361,7 +361,7 @@ SEXP do_usemethod(SEXP call, SEXP op, SEXP args, SEXP env)
     if (nargs)
 	PROTECT(generic = eval(CAR(args), env));
     else 
-	errorcall(call, "there must be a first argument");
+	errorcall(call, "There must be a first argument");
 
     if (nargs > 2)  /* R-lang says there should be a warning */
 	warningcall(call, "Arguments after the first two are ignored");
