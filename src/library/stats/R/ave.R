16d579176f5d80c968593ca9c7401e0d008e462e
pd <pd@00db46b3-68df-0310-9c12-caf00c1e9a41>
2004-10-30 14:28:46 +0000

    split<- and ave fixes
    git-svn-id: https://svn.r-project.org/R/trunk@31686 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/R/ave.R
+++ src/library/stats/R/ave.R
@@ -1,15 +1,10 @@
 ave <- function (x, ..., FUN = mean)
 {
     n <- length(l <- list(...))
-    x[] <- if (n) {
-        g <- 1
-        nlv <- 1
-        for (i in 1:n) {
-            l[[i]] <- li <- factor(l[[i]])# maybe dropping levels
-            g <- g + nlv * (as.numeric(li) - 1)
-            nlv <- nlv * length(levels(li))
-        }
-        unlist(lapply(split(x, g), FUN))[g]
-    } else FUN(x)
+    if (n) {
+	g <- interaction(...)
+	split(x,g) <- lapply(split(x, g), FUN)
+    } else 
+        x[] <- FUN(x)
     x
 }
