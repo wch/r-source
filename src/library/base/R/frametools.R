a004e4b594015d184c6cc5ca1fa16a18c8f32666
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2004-11-06 16:14:10 +0000

    add matrix method for subset()
    git-svn-id: https://svn.r-project.org/R/trunk@31804 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/frametools.R
+++ src/library/base/R/frametools.R
@@ -1,5 +1,4 @@
-subset.data.frame <-
-    function (x, subset, select, drop = FALSE, ...)
+subset.data.frame <- function (x, subset, select, drop = FALSE, ...)
 {
     if(missing(subset))
 	r <- TRUE
@@ -18,16 +17,23 @@ subset.data.frame <-
     x[r, vars, drop = drop]
 }
 
-subset<-
-    function(x, ...)
-    UseMethod("subset")
+subset<- function(x, ...) UseMethod("subset")
 
-subset.default <-
-    function(x, subset, ...)
-    x[subset & !is.na(subset)]
+subset.default <- function(x, subset, ...) x[subset & !is.na(subset)]
 
-transform.data.frame <-
-    function (x, ...)
+subset.matrix <- function(x, subset, select, drop = FALSE, ...)
+{
+    if(missing(select))
+	vars <- TRUE
+    else {
+	nl <- as.list(1:ncol(x))
+	names(nl) <- colnames(x)
+	vars <- eval(substitute(select), nl, parent.frame())
+    }
+    x[subset & !is.na(subset), vars, drop = drop]
+}
+
+transform.data.frame <- function (x, ...)
 {
     e <- eval(substitute(list(...)), x, parent.frame())
     tags <- names(e)
@@ -42,19 +48,15 @@ transform.data.frame <-
     else x
 }
 
-transform <-
-    function(x,...)
-    UseMethod("transform")
+transform <- function(x,...) UseMethod("transform")
 
 ## Actually, I have no idea what to transform(), except dataframes.
 ## The default converts its argument to a dataframe and transforms
 ## that. This is probably marginally useful at best. --pd
-transform.default <-
-    function(x,...)
+transform.default <- function(x,...)
     transform.data.frame(data.frame(x),...)
 
-stack.data.frame <-
-    function(x, select, ...)
+stack.data.frame <- function(x, select, ...)
 {
     if (!missing(select)) {
 	nl <- as.list(1:ncol(x))
@@ -67,12 +69,9 @@ stack.data.frame <-
                ind = factor(rep.int(names(x), lapply(x, length))))
 }
 
-stack <-
-    function(x, ...)
-    UseMethod("stack")
+stack <- function(x, ...) UseMethod("stack")
 
-stack.default <-
-    function(x, ...)
+stack.default <- function(x, ...)
 {
     x <- as.list(x)
     x <- x[unlist(lapply(x, is.vector))]
@@ -80,8 +79,7 @@ stack.default <-
                ind = factor(rep.int(names(x), lapply(x, length))))
 }
 
-unstack.data.frame <-
-    function(x, form = formula(x), ...)
+unstack.data.frame <- function(x, form = formula(x), ...)
 {
     form <- as.formula(form)
     if (length(form) < 3)
@@ -92,12 +90,9 @@ unstack.data.frame <-
     data.frame(res)
 }
 
-unstack <-
-    function(x, ...)
-    UseMethod("unstack")
+unstack <- function(x, ...) UseMethod("unstack")
 
-unstack.default <-
-    function(x, form, ...)
+unstack.default <- function(x, form, ...)
 {
     x <- as.list(x)
     form <- as.formula(form)
