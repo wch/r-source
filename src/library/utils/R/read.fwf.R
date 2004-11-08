a92b69661e89d6493b969048911e4f32ae613b26
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2004-11-08 08:34:29 +0000

    fix PR#7350 formatting issues in read.fwf
    git-svn-id: https://svn.r-project.org/R/trunk@31817 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/utils/R/read.fwf.R
+++ src/library/utils/R/read.fwf.R
@@ -1,76 +1,74 @@
 read.fwf <-
 function(file, widths, header = FALSE, sep = "\t", as.is = FALSE,
-         skip = 0, row.names, col.names, n = -1, buffersize=2000, ...)
+         skip = 0, row.names, col.names, n = -1, buffersize = 2000, ...)
 {
     doone <- function(x) {
         x <- substring(x, first, last)
-        x[nchar(x)==0] <- as.character(NA)
+        x[nchar(x) == 0] <- as.character(NA)
         x
     }
 
     if (is.list(widths)) {
-      recordlength<-length(widths)
-      widths<-do.call("c",widths)
-    } else recordlength<-1
+        recordlength <- length(widths)
+        widths <- do.call("c",widths)
+    } else recordlength <- 1
 
-    drop<- (widths<0)
-    widths<-abs(widths)
+    drop<- (widths < 0)
+    widths <- abs(widths)
 
 
-    buffersize<-(buffersize %/% recordlength)* recordlength
+    buffersize <- (buffersize %/% recordlength) * recordlength
 
     FILENAME <- tempfile("Rfwf.")
     on.exit(unlink(FILENAME))
-    FILE<-file(FILENAME,"a")
+    FILE <- file(FILENAME,"a")
     on.exit(close(FILE),add=TRUE)
 
     if (is.character(file)) {
-      file <- file(file,"r")
-      on.exit(close(file),add=TRUE)
+        file <- file(file, "r")
+        on.exit(close(file), add=TRUE)
     } else if (!isOpen(file)) {
-      file <- open(file,"r")
-      on.exit(close(file),add=TRUE)
+        file <- open(file, "r")
+        on.exit(close(file), add=TRUE)
     }
 
-    if (skip)
-      readLines(file,n=skip)
-    if (header){
-      headerline<-readLines(file,n=1)
-      cat(FILE,headerline,"\n")
+    if (skip) readLines(file, n=skip)
+    if (header) {
+        headerline <- readLines(file, n=1)
+        cat(FILE, headerline, "\n")
     }
 
     repeat({
-      if (n==-1)
-        thisblock<-buffersize
-      else
-        thisblock<-min(buffersize,n)
+        if (n == 0) break
+        if (n == -1)
+            thisblock <- buffersize
+        else
+            thisblock <- min(buffersize,n)
 
-      raw <- readLines(file, n=thisblock)
-      nread<-length(raw)
-      if (recordlength>1 &&  nread %% recordlength){
-        raw<-raw[1:(nread-nread %% recordlength)]
-        warning("Last record incomplete, ",nread %% recordlength," lines discarded")
-      }
-      if (recordlength>1){
-        raw<-matrix(raw,nrow=recordlength)
-        raw<-apply(raw,2,paste,collapse="")
-      }
+        raw <- readLines(file, n = thisblock)
+        nread <- length(raw)
+        if (recordlength > 1 &&  nread %% recordlength) {
+            raw<-raw[1:(nread-nread %% recordlength)]
+            warning("Last record incomplete, ", nread %% recordlength,
+                    " lines discarded")
+        }
+        if (recordlength > 1) {
+            raw <- matrix(raw, nrow=recordlength)
+            raw <- apply(raw, 2, paste, collapse="")
+        }
 
-      st <- c(1, 1+cumsum(widths))
-      first <- st[-length(st)][!drop]
-      last <- cumsum(widths)[!drop]
-      cat(file = FILE, sapply(raw, doone),
-          sep = c(rep(sep, length.out = length(first)-1), "\n"))
+        st <- c(1, 1+cumsum(widths))
+        first <- st[-length(st)][!drop]
+        last <- cumsum(widths)[!drop]
+        cat(file = FILE, sapply(raw, doone),
+            sep = c(rep(sep, length.out = length(first)-1), "\n"))
 
-      if (length(raw)<thisblock)
-        break
-      if (n>0)
-        n<- n - length(raw)
+        if (length(raw) < thisblock) break
+        if (n > 0) n <- n - length(raw)
     })
 
     close(FILE)
-    FILE<-file(FILENAME,"r")
+    FILE <- file(FILENAME,"r")
     read.table(file = FILE, header = header, sep = sep, as.is = as.is,
-	       skip = skip, row.names = row.names, col.names =
-               col.names, quote="", ...)
+	       row.names = row.names, col.names = col.names, quote = "", ...)
 }
