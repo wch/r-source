3d778fc16d23b933efb66abcbd0560f4ae0c309a
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2004-11-11 18:35:34 +0000

    update to match src/modules/lapack/Lapack.h
    git-svn-id: https://svn.r-project.org/R/trunk@31876 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/include/R_ext/Lapack.h
+++ src/include/R_ext/Lapack.h
@@ -2330,11 +2330,17 @@ void
 F77_NAME(dgesc2)(int *n, double *a, int *lda, 
 	double *rhs, int *ipiv, int *jpiv, double *scale);
  
+/* DGESDD - compute the singular value decomposition (SVD); of a   */
+/* real M-by-N matrix A, optionally computing the left and/or      */
+/* right singular vectors.  If singular vectors are desired, it uses a */
+/* divide-and-conquer algorithm.                                   */
 void
-F77_NAME(dgesdd)(char *jobz, int *m, int *n, double *
-	a, int *lda, double *s, double *u, int *ldu, 
-	double *vt, int *ldvt, double *work, int *lwork, 
-	int *iwork, int *info);
+F77_NAME(dgesdd)(const char *jobz,
+		 const int *m, const int *n,
+		 double *a, const int *lda, double *s,
+		 double *u, const int *ldu,
+		 double *vt, const int *ldvt,
+		 double *work, const int *lwork, int *iwork, int *info);
  
 void
 F77_NAME(dgetc2)(int *n, double *a, int *lda, int 
