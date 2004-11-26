7d58fe221ee97509fb505f076bb8319a4497d983
leisch <leisch@00db46b3-68df-0310-9c12-caf00c1e9a41>
2004-11-26 14:52:24 +0000

    new print and edit methods for vignette objects
    git-svn-id: https://svn.r-project.org/R/trunk@32054 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/utils/R/vignette.R
+++ src/library/utils/R/vignette.R
@@ -20,36 +20,35 @@ function(topic, package = NULL, lib.loc = NULL)
     if(!missing(topic)) {
         topic <- topic[1]               # Just making sure ...
         vignettes <- as.character(unlist(vignettes))
-        idx <-
-            which(tools::file_path_sans_ext(basename(vignettes)) == topic)
-        if(length(idx)) {
-            f <- sub("\\.[[:alpha:]]+$", ".pdf", vignettes[idx])
-            f <- f[tools::file_test("-f", f)]
-            if(length(f) > 1) {
-                ## <FIXME>
-                ## Should really offer a menu to select from.
-                f <- f[1]
-                warning(paste("vignette ", sQuote(topic),
-                              " found more than once,\n",
-                              "using the one found in ",
-                              sQuote(dirname(f)),
-                              sep = ""),
-                        call. = FALSE)
-                ## </FIXME>
+        vidx <- (tools::file_path_sans_ext(basename(vignettes)) == topic)
+        if(any(vidx)) {
+            
+            pdf <- sub("\\.[[:alpha:]]+$", ".pdf", vignettes)
+            pidx <- tools::file_test("-f", pdf)
+            ok <- vidx & pidx
+
+            if(any(ok)){
+                idx <- min(which(ok))
+                if(sum(ok)>1){
+                    ## <FIXME>
+                    ## Should really offer a menu to select from.
+                    warning(paste("vignette ", sQuote(topic),
+                                  " found more than once,\n",
+                                  "using the one found in ",
+                                  sQuote(dirname(pdf[idx])),
+                                  sep = ""),
+                            call. = FALSE)
+                    ## </FIXME>
+                }
+
+                z <- list(file=vignettes[idx], pdf=pdf[idx])
             }
-            if(length(f)) {
-                ## <FIXME>
-                ## Should really abstract this into a BioC style
-                ## openPDF() along the lines of browseURL() ...
-                if(.Platform$OS == "windows")
-                    shell.exec(f)
-                else
-                    system(paste(Sys.getenv("R_PDFVIEWER"), f, "&"))
-                ## </FIXME>
+            else{
+                z <- list(file=vignettes[vidx][1], pdf=character(0))
             }
-            else
-                warning(paste("vignette", sQuote(topic), "has no PDF"),
-                        call. = FALSE)
+            z$topic <- topic
+            class(z) <- "vignette"
+            return(z)
         }
         else
             warning(paste("vignette", sQuote(topic), "*not* found"),
@@ -97,4 +96,29 @@ function(topic, package = NULL, lib.loc = NULL)
         class(y) <- "packageIQR"
         return(y)
     }
+}
+
+print.vignette <- function(x, ...){
+
+    if(length(x$pdf)){
+        ## <FIXME>
+        ## Should really abstract this into a BioC style
+        ## openPDF() along the lines of browseURL() ...
+        if(.Platform$OS == "windows")
+            shell.exec(x$pdf)
+        else
+            system(paste(getOption("pdfviewer"), x$pdf, "&"))
+        ## </FIXME>
+    }
+    else{
+        warning(paste("vignette", sQuote(x$topic), "has no PDF"),
+                call. = FALSE)
+    }
+}
+
+edit.vignette <- function(name, ...){
+
+    f <- paste(tempfile(name$topic), ".R", sep="")
+    Stangle(name$file, output=f, quiet=TRUE)
+    file.edit(file=f, ...)
 }
