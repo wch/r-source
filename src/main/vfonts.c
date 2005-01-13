548903c408c254dbe8505d8bc59d00110086181b
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2005-01-13 17:56:36 +0000

    suppress warning
    git-svn-id: https://svn.r-project.org/R/trunk@32620 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/vfonts.c
+++ src/main/vfonts.c
@@ -94,7 +94,7 @@ double GVStrWidth (const unsigned char *s, int typeface, int fontindex,
     if(utf8locale && !utf8strIsASCII(str)) {
 	buff = alloca(strlen(str)+1); /* Output string cannot be longer */
 	if(!buff) error("allocation failure in GVStrWidth");
-	utf8toLatin1(s, buff);
+	utf8toLatin1(s,(char *)buff);
 	str = buff;
     }
 #endif
@@ -131,7 +131,7 @@ double GVStrHeight (const unsigned char *s, int typeface, int fontindex,
     if(utf8locale && !utf8strIsASCII(str)) {
 	buff = alloca(strlen(str)+1); /* Output string cannot be longer */
 	if(!buff) error("allocation failure in GVStrHeight");
-	utf8toLatin1(s, buff);
+	utf8toLatin1(s, (char *)buff);
 	str = buff;
     }
 #endif
