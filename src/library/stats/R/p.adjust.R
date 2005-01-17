db96da450f801bb32644c8d40538c5a36e1cd565
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2005-01-17 23:44:01 +0000

    next round of p.adjust() adjustments
    git-svn-id: https://svn.r-project.org/R/trunk@32692 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/R/p.adjust.R
+++ src/library/stats/R/p.adjust.R
@@ -1,59 +1,63 @@
 p.adjust.methods <-
-    c("holm", "hochberg", "hommel", "bonferroni", "fdr", "none")
+    c("holm", "hochberg", "hommel", "bonferroni", "yh", "bh", "fdr", "none")
 
 p.adjust <-
-    function(p, method = p.adjust.methods, n = length(p), na.rm = FALSE)
+    function(p, method = p.adjust.methods, n = length(p))
 {
-    ## Methods 'Hommel' and 'FDR' and speed improvements contributed by
-    ## Gordon Smyth <smyth@wehi.edu.au>  (w/o NA treatment yet)
+    ## Methods 'Hommel', 'BH', 'YH' and speed improvements contributed by
+    ## Gordon Smyth <smyth@wehi.edu.au>.
 
     method <- match.arg(method)
-    if(na.rm && any(ina <- is.na(p))) {
-        pp <- p
-        p <- pp[!ina] # for n's default
-        pp[!ina] <- p.adjust(p, method = method, n = n, na.rm = FALSE)
-        return(pp)
-    }
-    if (n <= 1) return(p)
-    switch(method,
-           bonferroni = pmin(1, n * p),
-           holm = {
+    if(method == "fdr") method <- "bh"
+    p0 <- p
+    if(all(nna <- !is.na(p))) nna <- TRUE
+    p <- as.vector(p[nna])
+ ## n <- length(p)
+    if (n <= 1) return(p0)
+    if (n == 2 && method == "hommel") method <- "hochberg"
+    
+    p0[nna] <- 
+      switch(method,
+             bonferroni = pmin(1, n * p),
+             holm = {
                i <- 1:n
                o <- order(p)
                ro <- order(o)
                pmin(1, cummax( (n - i + 1) * p[o] ))[ro]
-           },
-           hochberg = ,
-           fdr = {
-               i <- n:1
-               o <- order(p, decreasing = TRUE, na.last = FALSE)# NA's first
-               ro <- order(o)
-               p <- (if(method == "fdr") n / i else n - i + 1) * p[o]
-               p <- if(has.na <- is.na(p[1])) {
-                   nna <- sum(is.na(p))
-                   c(rep.int(NA, nna), cummin(p[(nna+1):n]))
-               } else cummin(p)
-               pmin(1, p)[ro]
-           },
-           hommel = { ## needs n-1 >= 2, i.e. n > 2 in for() below
-               if (n <= 2) return(p)
+             },
+             hommel = { ## needs n-1 >= 2, i.e. n > 2 in for() below
                i <- 1:n
                o <- order(p)
-               p <- p[o] # NA's last
+               p <- p[o]
                ro <- order(o)
-               q <- pa <- rep.int( min(n*p/(1:n), na.rm=TRUE), n)
-               ok.p <- !is.na(p)
+               q <- pa <- rep.int( min(n*p/(1:n)), n)
                for (j in (n-1):2) {
-                   qq <- j*p[1:(n-j+1)]
-                   if(ok.p[n-j+2]) {
-                       q1 <- min(j*p[(n-j+2):n]/(2:j), na.rm=TRUE)
-                       qq <- pmin(qq, q1)
-                   }
-                   q[1:(n-j+1)] <- qq
-                   q[(n-j+2):n] <- q[n-j+1]
-                   pa <- pmax(pa,q)
+                 q1 <- min(j*p[(n-j+2):n]/(2:j))
+                 q[1:(n-j+1)] <- pmin( j*p[1:(n-j+1)], q1)
+                 q[(n-j+2):n] <- q[n-j+1]
+                 pa <- pmax(pa,q)
                }
-               pmin(1, pmax(pa,p)[ro])
-           },
-           none = p)
+               pmax(pa,p)[ro]
+             },
+             hochberg = {
+               i <- n:1
+               o <- order(p, decreasing = TRUE)
+               ro <- order(o)
+               pmin(1, cummin( (n - i + 1) * p[o] ))[ro]
+             },
+             bh = {
+               i <- n:1
+               o <- order(p, decreasing = TRUE)
+               ro <- order(o)
+               pmin(1, cummin( n / i * p[o] ))[ro]
+             },
+             yh = {
+               i <- n:1
+               o <- order(p, decreasing = TRUE)
+               ro <- order(o)
+               q <- sum(1/(1:n))
+               pmin(1, cummin(q * n / i * p[o]))[ro]
+             },
+             none = p)
+    p0
 }
