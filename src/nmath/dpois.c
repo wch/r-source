40176d05a441b6a78094fb0784f86bc8d4a47e51
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2005-01-21 09:40:57 +0000

    dpois(*, lambda=Inf)
    git-svn-id: https://svn.r-project.org/R/trunk@32759 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/nmath/dpois.c
+++ src/nmath/dpois.c
@@ -37,7 +37,11 @@
 
 double dpois_raw(double x, double lambda, int give_log)
 {
+    /*       x >= 0 "integer",
+        lambda >= 0
+    */
     if (lambda == 0) return( (x == 0) ? R_D__1 : R_D__0 );
+    if (!R_finite(lambda)) return R_D__0;
     if (x == 0) return( R_D_exp(-lambda) );
     if (x < 0)  return( R_D__0 );
 
@@ -53,7 +57,9 @@ double dpois(double x, double lambda, int give_log)
 
     if (lambda < 0) ML_ERR_return_NAN;
     R_D_nonint_check(x);
-    if (x < 0 || !R_FINITE(x)) return R_D__0;
+    if (x < 0 || !R_FINITE(x))
+	return R_D__0;
+
     x = R_D_forceint(x);
 
     return( dpois_raw(x,lambda,give_log) );
