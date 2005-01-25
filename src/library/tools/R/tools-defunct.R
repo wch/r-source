8c2b4780d7da3b64ceffeeb2204cd464b87b588d
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2005-01-25 09:26:41 +0000

    Make functions which were deprecated in 2.0.0 defunct in 2.1.0, and schedule for complete removal in 2.2.0.
    git-svn-id: https://svn.r-project.org/R/trunk@32824 00db46b3-68df-0310-9c12-caf00c1e9a41

---
+++ src/library/tools/R/tools-defunct.R
@@ -0,0 +1,39 @@
+## <entry>
+## Deprecated in 2.0.0.
+## Defunct in 2.1.0.
+filePathAsAbsolute <-
+function(x)
+    .Defunct(package = "tools")
+## </entry>
+
+## <entry>
+## Deprecated in 2.0.0.
+## Defunct in 2.1.0.
+filePathSansExt <-
+function(x)
+    .Defunct(package = "tools")
+## </entry>
+
+## <entry>
+## Deprecated in 2.0.0.
+## Defunct in 2.1.0.
+fileTest <-
+function(op, x, y)
+    .Defunct(package = "tools")    
+## </entry>
+
+## <entry>
+## Deprecated in 2.0.0.
+## Defunct in 2.1.0.
+listFilesWithExts <-
+function(dir, exts, all.files = FALSE, full.names = TRUE)
+    .Defunct(package = "tools")    
+## </entry>
+
+## <entry>
+## Deprecated in 2.0.0.
+## Defunct in 2.1.0.
+listFilesWithType <-
+function(dir, type, all.files = FALSE, full.names = TRUE)
+    .Defunct(package = "tools")    
+## </entry>
