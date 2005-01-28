0e54eb7771448359157786584004d33f62a75e57
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2005-01-28 21:15:24 +0000

    use R_Home
    git-svn-id: https://svn.r-project.org/R/trunk@32914 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/main.c
+++ src/main/main.c
@@ -452,7 +452,7 @@ void setup_Rmainloop(void)
 #ifdef ENABLE_NLS
     setlocale(LC_MESSAGES,"");
     textdomain(PACKAGE);
-    strcpy(localedir, getenv("R_HOME")); strcat(localedir, "/share/locale");
+    strcpy(localedir, R_Home); strcat(localedir, "/share/locale");
     bindtextdomain(PACKAGE, localedir);
 #endif
 #endif
