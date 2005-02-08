e55c2ee98a1a1c3627bfdcf17df7c8784dec9743
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2005-02-08 15:22:10 +0000

    improve messages
    git-svn-id: https://svn.r-project.org/R/trunk@33101 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/arithmetic.c
+++ src/main/arithmetic.c
@@ -516,7 +516,7 @@ static SEXP integer_unary(ARITHOP_TYPE code, SEXP s1)
 	}
 	return ans;
     default:
-	error(_("illegal unary operator"));
+	error(_("invalid unary operator"));
     }
     return s1;			/* never used; to keep -Wall happy */
 }
@@ -535,7 +535,7 @@ static SEXP real_unary(ARITHOP_TYPE code, SEXP s1, SEXP lcall)
 	    REAL(ans)[i] = -REAL(s1)[i];
 	return ans;
     default:
-	errorcall(lcall, _("illegal unary operator"));
+	errorcall(lcall, _("invalid unary operator"));
     }
     return s1;			/* never used; to keep -Wall happy */
 }
@@ -1129,7 +1129,7 @@ SEXP do_Math2(SEXP call, SEXP op, SEXP args, SEXP env)
 
     checkArity(op, args);
     if (length(CADR(args)) == 0)
-	errorcall(call, _("illegal second argument of length 0"));
+	errorcall(call, _("invalid second argument of length 0"));
     return do_math2(call, op, args, env);
 }
 
