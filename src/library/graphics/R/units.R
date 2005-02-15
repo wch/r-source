fba82819f7cd804697043f33287471e02457b681
murrell <murrell@00db46b3-68df-0310-9c12-caf00c1e9a41>
2005-02-15 00:38:25 +0000

    Shifted xinch(), yinch(), xyinch() from grDevices to graphics
    git-svn-id: https://svn.r-project.org/R/trunk@33188 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/graphics/R/units.R
+++ src/library/graphics/R/units.R
@@ -1,5 +1,3 @@
-cm <- function(x) 2.54*x
-
 xinch <- function(x=1, warn.log=TRUE) {
     if(warn.log && par("xlog")) warning("x log scale:  xinch() is non-sense")
     x * diff(par("usr")[1:2])/par("pin")[1]
