71a4ad5b3dc4cee4e2eb73d73705b682e6d2b8af
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2005-02-15 11:57:01 +0000

    vectorize sprintf
    git-svn-id: https://svn.r-project.org/R/trunk@33194 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/sprintf.c
+++ src/main/sprintf.c
@@ -16,7 +16,7 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * Written by Jonathan Rougier, email J.C.Rougier@durham.ac.uk
+ * Originally written by Jonathan Rougier, email J.C.Rougier@durham.ac.uk
 */
 
 /* <UTF8> char here is handled as a whole string */
@@ -36,200 +36,216 @@
 */
 SEXP do_sprintf(SEXP call, SEXP op, SEXP args, SEXP env)
 {
-    int i, nargs, cnt = 0, v;
+    int i, nargs, cnt, v;
     char *formatString;
-    char fmt[MAXLINE+1], bit[MAXLINE+1], outputString[MAXLINE+1] = "";
+    char fmt[MAXLINE+1], bit[MAXLINE+1], outputString[MAXLINE+1];
     size_t n, cur, chunk;
 
     SEXP format, ans, this, a[100];
+    int ns, maxlen, lens[100], nthis;
 
     /* grab the format string */
 
     nargs = length(args);
     format = CAR(args);
-    if (!isString(format) || LENGTH(format) != 1)
-	errorcall(call, _("'fmt' is not a character string of length 1"));
-    formatString = CHAR(STRING_ELT(format, 0));
-    n = strlen(formatString);
-    if (n > MAXLINE)
-	errorcall(call, _("'fmt' length exceeds maximal buffer length %d"),
-		  MAXLINE);
+    if (!isString(format) || LENGTH(format) == 0)
+	errorcall(call, _("'fmt' is not a non-empty character vector"));
     args = CDR(args); nargs--;
-    if(nargs >=100)
+    if(nargs >= 100)
 	errorcall(call, _("only 100 arguments are allowed"));
 
     /* record the args for later re-ordering */
     for(i = 0; i < nargs; i++, args = CDR(args)) a[i] = CAR(args);
 
-    /* process the format string */
-    for (cur = 0; cur < n; cur += chunk) {
+    maxlen = LENGTH(format);
 
-	if (formatString[cur] == '%') { /* handle special format command */
-
-	    if (cur < n - 1 && formatString[cur + 1] == '%') {
-		/* take care of %% in the format */
-		chunk = 2;
-		strcpy(bit, "%");
-	    }
-	    else {
-		/* recognise selected types from Table B-1 of K&R */
-
-		chunk = strcspn(formatString + cur, "disfeEgGx") + 1;
-		if (cur + chunk > n)
-		    errorcall(call, _("unrecognised format at end of string"));
-
-		strncpy(fmt, formatString + cur, chunk);
-		fmt[chunk] = '\0';
-
-		this = NULL;
-		/* now look for %n$ or %nn$ form */
-		if (strlen(fmt) > 3 && fmt[1] >= '1' && fmt[1] <= '9') {
-		    v = fmt[1] - '0';
-		    if(fmt[2] == '$') {
-			if(v > nargs)
-			    errorcall(call, _("reference to non-existent argument %d"), v);
-			this = a[v-1];
-			memmove(fmt+1, fmt+3, strlen(fmt)-2);
-		    } else if(fmt[2] >= '1' && fmt[2] <= '9' 
-			      && fmt[3] == '$') {
-			v = 10*v + fmt[2] - '0';
-			if(v > nargs)
-			    errorcall(call, _("reference to non-existent argument %d"), v);
-			this = a[v-1];
-			memmove(fmt+1, fmt+4, strlen(fmt)-3);
-		    }
-		}
-		
-		if(!this) {
-		    if (cnt >= nargs) errorcall(call, _("too few arguments"));
-		    this = a[cnt++];
-		}
-
-		if (LENGTH(this) < 1)
-		    errorcall(call, _("zero-length argument"));
-
-		/* Now let us see if some minimal coercion would be sensible.
-		   Do not need to PROTECT as no R allocation takes place.
-		 */
-		switch(tolower(fmt[strlen(fmt) - 1])) {
-		case 'd':
-		case 'i':
-		    if(TYPEOF(this) == REALSXP) {
-			double r = REAL(this)[0];
-			if((double)((int) r) == r)
-			    this = coerceVector(this, INTSXP);
-		    }
-		    break;
-		case 'e':
-		case 'f':
-		case 'g':
-		    if(TYPEOF(this) == INTSXP || TYPEOF(this) == LGLSXP)
-			this = coerceVector(this, REALSXP);
-		    break;
-		case 's':
-		    this = coerceVector(this, STRSXP);
-		    break;
-		default:
-		    break;
-		}
-
-		switch(TYPEOF(this)) {
-		case LGLSXP:
-		{
-		    int x = LOGICAL(this)[0];
-		    if (strcspn(fmt, "di") >= strlen(fmt))
-			error("%s", 
-			      _("use format %d or %i for logical objects"));
-		    if (x == NA_LOGICAL) {
-			fmt[strlen(fmt)-1] = 's';
-			sprintf(bit, fmt, "NA");
-		    } else
-			sprintf(bit, fmt, x);
-		    break;
-		}
-		case INTSXP:
-		{
-		    int x = INTEGER(this)[0];
-		    if (strcspn(fmt, "dix") >= strlen(fmt))
-			error("%s",
-			      _("use format %d, %i or %x for integer objects"));
-		    if (x == NA_INTEGER) {
-			fmt[strlen(fmt)-1] = 's';
-			sprintf(bit, fmt, "NA");
-		    } else
-			sprintf(bit, fmt, x);
-		    break;
-		}
-		case REALSXP:
-		{
-		    double x = REAL(this)[0];
-		    if (strcspn(fmt, "feEgG") >= strlen(fmt))
-			error("%s", 
-			      _("use format %f, %e or %g for numeric objects"));
-		    if (R_FINITE(x)) {
-			sprintf(bit, fmt, x);
-		    } else {
-			char *p = strchr(fmt, '.');
-			if (p) {
-			    *p++ = 's'; *p ='\0';
-			} else
-			    fmt[strlen(fmt)-1] = 's';
-			if (ISNA(x)) {
-			    if (strcspn(fmt, " ") < strlen(fmt))
-				sprintf(bit, fmt, " NA");
-			    else
-				sprintf(bit, fmt, "NA");
-			} else if (ISNAN(x)) {
-			    if (strcspn(fmt, " ") < strlen(fmt))
-				sprintf(bit, fmt, " NaN");
-			    else
-				sprintf(bit, fmt, "NaN");
-			} else if (x == R_PosInf) {
-			    if (strcspn(fmt, "+") < strlen(fmt))
-				sprintf(bit, fmt, "+Inf");
-			    else if (strcspn(fmt, " ") < strlen(fmt))
-				sprintf(bit, fmt, " Inf");
-			    else
-				sprintf(bit, fmt, "Inf");
-			} else if (x == R_NegInf)
-			    sprintf(bit, fmt, "-Inf");
-		    }
-		    break;
-		}
-		case STRSXP:
-		    /* NA_STRING will be printed as `NA' */
-		    if (strcspn(fmt, "s") >= strlen(fmt))
-			error("%s", _("use format %s for character objects"));
-		    if(strlen(CHAR(STRING_ELT(this, 0))) > MAXLINE)
-			warning(_("Likely truncation of character string"));
-		    snprintf(bit, MAXLINE, fmt, CHAR(STRING_ELT(this, 0)));
-		    bit[MAXLINE] = '\0';
-		    break;
-		default:
-		    errorcall(call, _("unsupported type"));
-		    break;
-		}
-	    }
-	}
-	else { /* not '%' : handle string part */
-
-	    chunk = strcspn(formatString + cur, "%");
-	    strncpy(bit, formatString + cur, chunk);
-	    bit[chunk] = '\0';
-	}
-
-	if (strlen(outputString) + strlen(bit) > MAXLINE)
-	    errorcall(call, _("String length exceeds buffer size of %d"), 
-		      MAXLINE);
-	strcat(outputString, bit);
+    for(i = 0; i < nargs; i++) {
+	lens[i] = LENGTH(a[i]);
+	if(lens[i] == 0)
+	    errorcall(call, _("zero-length argument"));
+	if(maxlen < lens[i]) maxlen = lens[i];
+    }
+    if(maxlen % LENGTH(format))
+	errorcall(call, _("arguments cannot be recycled to the same length"));
+    for(i = 0; i < nargs; i++) {
+	if(maxlen % lens[i])
+	    errorcall(call, _("arguments cannot be recycled to the same length"));
     }
 
-    /* no longer makes sense with out-of-order args 
-       if (cnt < nargs) warning(_("Unused arguments")); */
+    PROTECT(ans = allocVector(STRSXP, maxlen));
+    for(ns = 0; ns < maxlen; ns++) {
+	cnt = 0;
+	outputString[0] = '\0';
+	formatString = CHAR(STRING_ELT(format, ns % LENGTH(format)));
+	n = strlen(formatString);
+	if (n > MAXLINE)
+	    errorcall(call, _("'fmt' length exceeds maximal buffer length %d"),
+		      MAXLINE);
+ 	/* process the format string */
+	for (cur = 0; cur < n; cur += chunk) {
 
-    /* return outputString as SEXP */
-    PROTECT(ans = allocVector(STRSXP, 1));
-    SET_STRING_ELT(ans, 0, mkChar(outputString));
+	    if (formatString[cur] == '%') { /* handle special format command */
+
+		if (cur < n - 1 && formatString[cur + 1] == '%') {
+		    /* take care of %% in the format */
+		    chunk = 2;
+		    strcpy(bit, "%");
+		}
+		else {
+		    /* recognise selected types from Table B-1 of K&R */
+
+		    chunk = strcspn(formatString + cur, "disfeEgGx") + 1;
+		    if (cur + chunk > n)
+			errorcall(call, _("unrecognised format at end of string"));
+
+		    strncpy(fmt, formatString + cur, chunk);
+		    fmt[chunk] = '\0';
+
+		    nthis = -1;
+		    /* now look for %n$ or %nn$ form */
+		    if (strlen(fmt) > 3 && fmt[1] >= '1' && fmt[1] <= '9') {
+			v = fmt[1] - '0';
+			if(fmt[2] == '$') {
+			    if(v > nargs)
+				errorcall(call, _("reference to non-existent argument %d"), v);
+			    nthis = v-1;
+			    memmove(fmt+1, fmt+3, strlen(fmt)-2);
+			} else if(fmt[2] >= '1' && fmt[2] <= '9' 
+				  && fmt[3] == '$') {
+			    v = 10*v + fmt[2] - '0';
+			    if(v > nargs)
+				errorcall(call, _("reference to non-existent argument %d"), v);
+			    nthis = v-1;
+			    memmove(fmt+1, fmt+4, strlen(fmt)-3);
+			}
+		    }
+		
+		    if(nthis < 0) {
+			if (cnt >= nargs) errorcall(call, _("too few arguments"));
+			nthis = cnt++;
+		    }
+		    this = a[nthis];
+
+		    /* Now let us see if some minimal coercion would be sensible.
+		       Do not need to PROTECT as no R allocation takes place.
+		    */
+		    switch(tolower(fmt[strlen(fmt) - 1])) {
+		    case 'd':
+		    case 'i':
+			if(TYPEOF(this) == REALSXP) {
+			    double r = REAL(this)[0];
+			    if((double)((int) r) == r)
+				this = coerceVector(this, INTSXP);
+			}
+			break;
+		    case 'e':
+		    case 'f':
+		    case 'g':
+			if(TYPEOF(this) == INTSXP || TYPEOF(this) == LGLSXP)
+			    this = coerceVector(this, REALSXP);
+			break;
+		    case 's':
+			this = coerceVector(this, STRSXP);
+			break;
+		    default:
+			break;
+		    }
+
+		    switch(TYPEOF(this)) {
+		    case LGLSXP:
+		    {
+			int x = LOGICAL(this)[ns % lens[nthis]];
+			if (strcspn(fmt, "di") >= strlen(fmt))
+			    error("%s", 
+				  _("use format %d or %i for logical objects"));
+			if (x == NA_LOGICAL) {
+			    fmt[strlen(fmt)-1] = 's';
+			    sprintf(bit, fmt, "NA");
+			} else
+			    sprintf(bit, fmt, x);
+			break;
+		    }
+		    case INTSXP:
+		    {
+			int x = INTEGER(this)[ns % lens[nthis]];
+			if (strcspn(fmt, "dix") >= strlen(fmt))
+			    error("%s",
+				  _("use format %d, %i or %x for integer objects"));
+			if (x == NA_INTEGER) {
+			    fmt[strlen(fmt)-1] = 's';
+			    sprintf(bit, fmt, "NA");
+			} else
+			    sprintf(bit, fmt, x);
+			break;
+		    }
+		    case REALSXP:
+		    {
+			double x = REAL(this)[ns % lens[nthis]];
+			if (strcspn(fmt, "feEgG") >= strlen(fmt))
+			    error("%s", 
+				  _("use format %f, %e or %g for numeric objects"));
+			if (R_FINITE(x)) {
+			    sprintf(bit, fmt, x);
+			} else {
+			    char *p = strchr(fmt, '.');
+			    if (p) {
+				*p++ = 's'; *p ='\0';
+			    } else
+				fmt[strlen(fmt)-1] = 's';
+			    if (ISNA(x)) {
+				if (strcspn(fmt, " ") < strlen(fmt))
+				    sprintf(bit, fmt, " NA");
+				else
+				    sprintf(bit, fmt, "NA");
+			    } else if (ISNAN(x)) {
+				if (strcspn(fmt, " ") < strlen(fmt))
+				    sprintf(bit, fmt, " NaN");
+				else
+				    sprintf(bit, fmt, "NaN");
+			    } else if (x == R_PosInf) {
+				if (strcspn(fmt, "+") < strlen(fmt))
+				    sprintf(bit, fmt, "+Inf");
+				else if (strcspn(fmt, " ") < strlen(fmt))
+				    sprintf(bit, fmt, " Inf");
+				else
+				    sprintf(bit, fmt, "Inf");
+			    } else if (x == R_NegInf)
+				sprintf(bit, fmt, "-Inf");
+			}
+			break;
+		    }
+		    case STRSXP:
+			/* NA_STRING will be printed as `NA' */
+			if (strcspn(fmt, "s") >= strlen(fmt))
+			    error("%s", _("use format %s for character objects"));
+			if(strlen(CHAR(STRING_ELT(this, ns % lens[nthis])))
+			   > MAXLINE)
+			    warning(_("Likely truncation of character string"));
+			snprintf(bit, MAXLINE, fmt, 
+				 CHAR(STRING_ELT(this, ns % lens[nthis])));
+			bit[MAXLINE] = '\0';
+			break;
+		    default:
+			errorcall(call, _("unsupported type"));
+			break;
+		    }
+		}
+	    }
+	    else { /* not '%' : handle string part */
+
+		chunk = strcspn(formatString + cur, "%");
+		strncpy(bit, formatString + cur, chunk);
+		bit[chunk] = '\0';
+	    }
+
+	    if (strlen(outputString) + strlen(bit) > MAXLINE)
+		errorcall(call, _("String length exceeds buffer size of %d"), 
+			  MAXLINE);
+	    strcat(outputString, bit);
+	}
+	SET_STRING_ELT(ans, ns, mkChar(outputString));
+    }
+
     UNPROTECT(1);
     return ans;
 }
