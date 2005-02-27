cc4b9d892690c404083b0ce5325a1f371d94b22c
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2005-02-27 21:26:28 +0000

    Improve print method for DLLInfoList objects.
    git-svn-id: https://svn.r-project.org/R/trunk@33381 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/dynload.R
+++ src/library/base/R/dynload.R
@@ -153,11 +153,11 @@ print.DLLInfo <- function(x, ...)
 
 print.DLLInfoList <- function(x, ...)
 {
-    m <- data.frame(Filename = sapply(x, function(x) x[["path"]]),
-                    "Dynamic Lookup" =
-                    sapply(x, function(x) x[["dynamicLookup"]]))
-    print(m, ...)
+    if(length(x)) {
+        m <- data.frame(Filename = sapply(x, function(x) x[["path"]]),
+                        "Dynamic Lookup" =
+                        sapply(x, function(x) x[["dynamicLookup"]]))
+        print(m, ...)
+    }
     invisible(x)
 }
-
-
