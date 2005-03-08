674cc238d0e703fa549be66a2e86177684d302c2
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2005-03-08 21:28:10 +0000

    amend last change
    git-svn-id: https://svn.r-project.org/R/trunk@33542 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/attrib.c
+++ src/main/attrib.c
@@ -478,7 +478,7 @@ SEXP R_data_class2 (SEXP obj)
 	else
 	    class0 = mkChar("array");
     }
-    PROTECT(class0); 
+    PROTECT(class0);
     switch(t = TYPEOF(obj)) {
     case CLOSXP: case SPECIALSXP: case BUILTINSXP:
 	class = mkChar("function");
@@ -489,10 +489,9 @@ SEXP R_data_class2 (SEXP obj)
 	    PROTECT(value = allocVector(STRSXP, 2));
 	    SET_STRING_ELT(value, 0, type2str(t));
 	    SET_STRING_ELT(value, 1, mkChar("numeric"));
-	    UNPROTECT(1);
+	    UNPROTECT(2);
 	}
 	else {
-	    PROTECT(class0);
 	    PROTECT(value = allocVector(STRSXP, 3));
 	    SET_STRING_ELT(value, 0, class0);	
 	    SET_STRING_ELT(value, 1, type2str(t));
