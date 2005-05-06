5d98a52234fc6ede6224e0cb02439b28f7544f73
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2005-05-06 09:53:18 +0000

    Add \special Rd markup. New function .Rd_transform_command().
    git-svn-id: https://svn.r-project.org/R/trunk@34191 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/Rd.R
+++ src/library/tools/R/Rd.R
@@ -689,37 +689,54 @@ function(txt)
 {
     txt <- get_Rd_section(txt, "examples")
     if(length(txt) != 1) return(character())
+    
     txt <- gsub("\\\\l?dots", "...", txt)
     txt <- gsub("\\\\%", "%", txt)
 
-    ## Now try removing \dontrun{}.
-    ## Simple version of R::Rdconv::undefine_command().
-    out <- character()
-    pattern <- "\\\\dontrun\\{"
-    while((pos <- regexpr(pattern, txt)) != -1) {
-        out <- c(out, substring(txt, 1, pos - 1))
-        txt <- substring(txt, pos + attr(pos, "match.length") - 1)
-        if((pos <- delimMatch(txt)) == -1)
-            stop("unclosed \\dontrun")
-        txt <- substring(txt, pos + attr(pos, "match.length"))
-    }
-    txt <- paste(c(out, txt), collapse = "")
-    ## Now try removing \dontshow{} and \testonly{}.
-    ## Simple version of R::Rdconv::replace_command().
-    out <- character()
-    pattern <- "\\\\(testonly|dontshow)\\{"
-    while((pos <- regexpr(pattern, txt)) != -1) {
-        out <- c(out, substring(txt, 1, pos - 1))
-        txt <- substring(txt, pos + attr(pos, "match.length") - 1)
-        if((pos <- delimMatch(txt)) == -1)
-            stop("unclosed \\dontshow or \\testonly")
-        out <- c(out,
-                 substring(txt, 2, pos + attr(pos, "match.length") - 2))
-        txt <- substring(txt, pos + attr(pos, "match.length"))
-    }
-    paste(c(out, txt), collapse = "")
+    ## Simple version of [Perl] R::Rdconv::drop_full_command().    
+    txt <- .Rd_transform_command(txt, "dontrun", function(u) NULL)
+    ## Simple version of [Perl] R::Rdconv::undefine_command().
+    ## <FIXME>
+    ## It would be nice to handle \dontshow and \testonly at once (as
+    ## earlier versions did).  Could be achieved by modifying
+    ## .Rd_transform_command to work on a sequence of commands ...
+    ## </FIXME>
+    txt <- .Rd_transform_command(txt, "dontshow", function(u) u)
+    txt <- .Rd_transform_command(txt, "testonly", function(u) u)
+    txt
 }
 
+### .Rd_transform_command
+
+.Rd_transform_command <-
+function(txt, cmd, FUN)
+{
+    ## In Rd text, replace markup of the form \cmd{something} by the
+    ## result of applying FUN to something.  Covers several separate
+    ## functions in the R::Rdconv Perl code:
+    ##   drop_full_command      FUN = function(u) NULL
+    ##   undefine_command       FUN = function(u) u
+    ##   replace_command        FUN = function(u) sprintf("Bef%sAft", u)
+    ## Currently, optional arguments to \cmd are not supported.
+
+    if(length(txt) != 1) return(character())
+    
+    out <- character()
+    pattern <- sprintf("\\\\%s\\{", cmd)
+    while((pos <- regexpr(pattern, txt)) != -1) {
+        out <- c(out, substring(txt, 1, pos - 1))
+        txt <- substring(txt, pos + attr(pos, "match.length") - 1)
+        if((pos <- delimMatch(txt)) == -1)
+            stop(sprintf("unclosed \\%s", cmd))
+        out <- c(out,
+                 FUN(substring(txt, 2,
+                               pos + attr(pos, "match.length") - 2)))
+        txt <- substring(txt, pos + attr(pos, "match.length"))
+    }
+    
+    paste(c(out, txt), collapse = "")
+}
+    
 ### .apply_Rd_filter_to_Rd_db
 
 .apply_Rd_filter_to_Rd_db <-
