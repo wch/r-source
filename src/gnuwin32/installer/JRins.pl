485d88ef9bf3e6966f42935799136a92fba674bb
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2005-05-10 16:46:21 +0000

    move to Inno Setup 5.1.x for better internationalization support
    git-svn-id: https://svn.r-project.org/R/branches/R-2-1-patches@34264 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/installer/JRins.pl
+++ src/gnuwin32/installer/JRins.pl
@@ -21,7 +21,7 @@
 use Cwd;
 use File::Find;
 
-my $fn, $component, $mini, $path;
+my $fn, $component, $path;
 my $startdir=cwd();
 my $RVER;
 my $RW=$ARGV[0];
@@ -38,14 +38,6 @@ $RVER =~ s/\n.*$//;
 $RVER =~ s/Under .*$/Pre-release/;
 
 open insfile, "> R.iss" || die "Cannot open R.iss\n";
-open minifile,"> Rsmall.iss" || die "Cannot open Rsmall.iss\n";
-
-print minifile <<END;
-[Setup]
-OutputBaseFilename=miniR
-DiskSpanning=yes
-END
-
 print insfile <<END;
 [Setup]
 OutputBaseFilename=${RW}
@@ -76,53 +68,69 @@ my $lines2=<<END;
 
 [Languages]
 Name: en; MessagesFile: "compiler:Default.isl"
+Name: br; MessagesFile: "compiler:Languages\\BrazilianPortuguese.isl"
+Name: ca; MessagesFile: "compiler:Languages\\Catalan.isl"
 Name: cz; MessagesFile: "compiler:Languages\\Czech.isl"
+Name: dk; MessagesFile: "compiler:Languages\\Danish.isl"
 Name: nl; MessagesFile: "compiler:Languages\\Dutch.isl"
 Name: fr; MessagesFile: "compiler:Languages\\French.isl"
 Name: de; MessagesFile: "compiler:Languages\\German.isl"
+Name: hu; MessagesFile: "compiler:Languages\\Hungarian.isl"
+Name: it; MessagesFile: "compiler:Languages\\Italian.isl"
 Name: no; MessagesFile: "compiler:Languages\\Norwegian.isl"
 Name: po; MessagesFile: "compiler:Languages\\Polish.isl"
+Name: pt; MessagesFile: "compiler:Languages\\Portuguese.isl"
 Name: ru; MessagesFile: "compiler:Languages\\Russian.isl"
 Name: sl; MessagesFile: "compiler:Languages\\Slovenian.isl"
+Name: chs; MessagesFile: "ChineseSimp.isl"
+Name: ja; MessagesFile: "Japanese.isl"
 
 
 [CustomMessages]
 en.regentries=Registry entries:
 en.associate=&Associate R with .RData files
-en.dcom=&Register R path for use by the (D)COM 
+en.dcom=&Register R path for use by the (D)COM server
+en.user=User installation
+en.compact=Minimal user installation
+en.full=Full installation
+en.CJK=Chinese/Japanese/Korean installation
+en.custom=Custom installation
 
-de.regentries= Einträge in der Windows-Registrierung:
-de.associate=&Verknüpfe R mit .RData Dateien
-de.dcom=&Registriere den R Pfad für den (D)COM Server
+br.regentries=Entradas no registro:
+br.associate=&Associar arquivos .RData ao R
+br.dcom=&Registrar o caminho do R para uso pelo servidor (D)COM
+br.user=Instalação modo usuário
+br.compact=Instalação modo usuário mínima
+br.full=Instalação completa
+br.CJK=Instalação em chinês/japonês/coreano
+br.custom=Instalação personalizada
 
-cz.regentries=Registry entries:
-cz.associate=&Associate R with .RData files
-cz.dcom=&Register R path for use by the (D)COM 
-
-nl.regentries=Registry entries:
-nl.associate=&Associate R with .RData files
-nl.dcom=&Register R path for use by the (D)COM 
+dk.regentries=Indgange i registreringsdatabasen:
+dk.associate=&Associere R med .Rdata filer
+dk.dcom=&Registrer R til brug for (D)COM server
+dk.user=Brugerinstallation
+dk.compact=Minimal brugerinstallation
+dk.full=Luld installation
+dk.CJK=Kinesisk/Japansk/Koreansk installation
+dk.custom=Brugerdefinet installation
 
 fr.regentries=Entrées dans le registre:
 fr.associate=&Associer R avec les fichiers .RData
 fr.dcom=&Enregistrer R pour l'utiliser avec '(D)COM server'
+fr.user=Installation utilisateur
+fr.compact=Installation utilisateur minimale
+fr.full=Installation utilisateur complète
+fr.CJK=Installation chinois/japonais/coréen
+fr.custom=Installation personnalisée
 
-no.regentries=Registry entries:
-no.associate=&Associate R with .RData files
-no.dcom=&Register R path for use by the (D)COM 
-
-po.regentries=Registry entries:
-po.associate=&Associate R with .RData files
-po.dcom=&Register R path for use by the (D)COM 
-
-ru.regentries=Registry entries:
-ru.associate=&Associate R with .RData files
-ru.dcom=&Register R path for use by the (D)COM 
-
-sl.regentries=Registry entries:
-sl.associate=&Associate R with .RData files
-sl.dcom=&Register R path for use by the (D)COM 
-
+de.regentries= Einträge in der Windows-Registrierung:
+de.associate=&Verknüpfe R mit .RData Dateien
+de.dcom=&Registriere den R Pfad für den (D)COM Server
+de.user=Benutzerinstallation
+de.compact=Minimale Installation
+de.full=Vollständige Installation
+de.CJK=Chinesische/Japanische/Koreanische Installation
+de.custom=Benutzerdefinierte Installation
 
 [Tasks]
 Name: "desktopicon"; Description: {cm:CreateDesktopIcon}; GroupDescription: {cm:AdditionalIcons}; MinVersion: 4,4
@@ -159,11 +167,11 @@ print insfile <<END;
 Name: "{group}\\R $RVER Help"; Filename: "{app}\\doc\\html\\Rwin.html"; Components: html
 
 [Types]
-Name: "user"; Description: "User installation"
-Name: "compact"; Description: "Minimal user installation"
-Name: "full"; Description: "Full installation"
-Name: "CJK"; Description: "Chinese/Japanese/Korean installation";
-Name: "custom"; Description: "Custom installation"; Flags: iscustom
+Name: "user"; Description: {cm:user}
+Name: "compact"; Description: {cm:compact}
+Name: "full"; Description: {cm:full}
+Name: "CJK"; Description: {cm:CJK}
+Name: "custom"; Description: {cm:custom}; Flags: iscustom
 
 [Components]
 Name: "main"; Description: "Main Files"; Types: user compact full CJK custom; Flags: fixed
@@ -178,21 +186,6 @@ Name: "mbcs"; Description: "Version for East Asian languages"; Types: CJK custom
 Name: "latex"; Description: "Latex Help Files"; Types: full custom
 Name: "refman"; Description: "PDF Reference Manual"; Types: full custom
 Name: "Rd"; Description: "Source Files for Help Pages"; Types: full custom
-
-[Files]
-END
-
-print minifile $lines;
-print minifile $lines2;
-print minifile <<END;
-
-[Types]
-Name: "compact"; Description: "Minimal user installation"
-Name: "custom"; Description: "Custom installation"; Flags: iscustom
-
-[Components]
-Name: "main"; Description: "Main Files"; Types: compact custom; Flags: fixed
-Name: "manuals"; Description: "On-line (PDF) Manuals"; Types: custom
 
 [Files]
 END
@@ -216,12 +209,10 @@ $path="${SRCDIR}";chdir($path);
 find(\&listFiles, ".");
 
 close insfile;
-close minifile;
 
 sub listFiles {
     $fn = $File::Find::name;
     $fn =~ s+^./++;
-    my $mini = 1;
     my $newname = "";
     if (!(-d $_)) {
 	$fn =~ s+/+\\+g;
@@ -231,16 +222,11 @@ sub listFiles {
 	$dir =~ s/\\$//;
 	$_ = $fn;
 	
-	if (m/^library\\tcltk/ || m/^MD5/ || m/^bin\\md5check.exe/) {
-	    $mini = 0;
-	}
 	if ($_ eq "bin\\Rchtml.dll" 
 	    || m/^library\\[^\\]*\\chtml/) {
 	    $component = "chtml";
-	    $mini = 0;
 	} elsif ($_ eq "doc\\html\\logo.jpg") {
 	    $component = "html devel";
-	    $mini = 0;
 	} elsif ($_ eq "doc\\manual\\R-FAQ.html"
 		 || $_ eq "doc\\html\\rw-FAQ.html"
 		 || $_ eq "share\\texmf\\Sweave.sty") {
@@ -251,24 +237,16 @@ sub listFiles {
 		 || m/^library\\[^\\]*\\CONTENTS/
 		 || $_ eq "library\\R.css") {
 	    $component = "html";
-	    $mini = 0;
 	} elsif ($_ eq "doc\\manual\\refman.pdf") {
 	    $component = "refman";
-	    $mini = 0;
 	} elsif (m/^doc\\manual/ && $_ ne "doc\\manual\\R-FAQ.pdf") {
 	    $component = "manuals";
-	    if (m/R-admin.pdf/ || m/R-exts.pdf/ || m/R-lang.pdf/) {
-		$mini = 0;
-	    }
 	} elsif (m/^library\\[^\\]*\\latex/) {
 	    	$component = "latex";
-	    	$mini = 0;
 	} elsif (m/^library\\[^\\]*\\man/) {
 	    	$component = "Rd";
-	    	$mini = 0;
 	} elsif (m/^Tcl/) {
 	    $component = "tcl";
-	    $mini = 0;
 	} elsif (exists($develfiles{$_})
 		 || m/^doc\\KEYWORDS/
 		 || m/^src\\gnuwin32/
@@ -292,21 +270,16 @@ sub listFiles {
 		 || m/^bin\\SHLIB/
 		 || m/^lib\\/) {
 	    $component = "devel";
-	    $mini = 0;
 	} elsif (m/^library\\grid\\doc/
 		 || $_ eq "library\\survival\\survival.ps.gz") {
 	    $component = "libdocs";
-	    $mini = 0;
 	} elsif ($_ eq "modules\\iconv.dll") {
 	    $component = "main";
-	    $mini = 0;
 	} elsif (m/^share\\locale/ 
 		 || m/^library\\[^\\]*\\po/) { # needs iconv
 	    $component = "trans";
-	    $mini = 0;
 	} elsif ($_ eq "bin\\Rmbcs.dll") {
 	    $component = "mbcs";
-	    $mini = 0;
 	    $newname = "R.dll";
 	} else {
 	    $component = "main";
@@ -316,8 +289,5 @@ sub listFiles {
 	$lines="Source: \"$path\\$fn\"; DestDir: \"{app}$dir\"; DestName: \"$newname\"; Flags: ignoreversion; Components: $component\n" if $newname ne "";
 
 	print insfile $lines;
-	if ($mini) {
-	    print minifile $lines;
-	}
     }
 }
