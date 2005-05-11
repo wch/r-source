1e785097d1b3ecc5c584bc598510d4731e2112f1
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2005-05-11 17:37:42 +0000

    remove some unused code
    git-svn-id: https://svn.r-project.org/R/branches/R-2-1-patches@34295 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/options.c
+++ src/main/options.c
@@ -1,7 +1,7 @@
 /*
  *  R : A Computer Language for Statistical Data Analysis
  *  Copyright (C) 1995, 1996  Robert Gentleman and Ross Ihaka
- *  Copyright (C) 1998-2000   The R Development Core Team.
+ *  Copyright (C) 1998-2005   The R Development Core Team.
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -272,10 +272,6 @@ void InitOptions(void)
     SETCAR(v, allocVector(LGLSXP, 1));
     LOGICAL(CAR(v))[0] = R_KeepSource;
     v = CDR(v);
-
-    SET_TAG(v, install("error.messages"));
-    SETCAR(v, allocVector(LGLSXP, 1));
-    LOGICAL(CAR(v))[0] = 1;
 
     SET_TAG(v, install("warnings.length"));
     SETCAR(v, allocVector(INTSXP, 1));
