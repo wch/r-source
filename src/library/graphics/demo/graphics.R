ca65afa773ed4c144b596608de58f8c399f04ffc
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2005-05-19 13:27:57 +0000

    use min()  {& other cosmetic, more white space}
    git-svn-id: https://svn.r-project.org/R/trunk@34421 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/graphics/demo/graphics.R
+++ src/library/graphics/demo/graphics.R
@@ -13,15 +13,15 @@ opar <- par(ask = interactive() &&
 
 
 x <- rnorm(50)
-opar <- c(opar, par(bg="white"))
-plot(x, ann=FALSE, type="n")
-abline(h=0, col=gray(.90))
-lines(x, col="green4", lty="dotted")
-points(x, bg="limegreen", pch=21)
-title(main="Simple Use of Color In a Plot",
-      xlab="Just a Whisper of a Label",
-      col.main="blue", col.lab=gray(.8),
-      cex.main=1.2, cex.lab=1.0, font.main=4, font.lab=3)
+opar <- c(opar, par(bg = "white"))
+plot(x, ann = FALSE, type = "n")
+abline(h = 0, col = gray(.90))
+lines(x, col = "green4", lty = "dotted")
+points(x, bg = "limegreen", pch = 21)
+title(main = "Simple Use of Color In a Plot",
+      xlab = "Just a Whisper of a Label",
+      col.main = "blue", col.lab = gray(.8),
+      cex.main = 1.2, cex.lab = 1.0, font.main = 4, font.lab = 3)
 
 
 ## A little color wheel.	 This code just plots equally spaced hues in
@@ -31,10 +31,11 @@ title(main="Simple Use of Color In a Plot",
 ## the RGB primaries.  On the other hand on the SGI Indy at work the
 ## effect is near perfect.
 
-par(bg="gray")
-pie(rep(1,24), col=rainbow(24), radius=0.9)
-title(main="A Sample Color Wheel", cex.main=1.4, font.main=3)
-title(xlab="(Use this as a test of monitor linearity)", cex.lab=0.8, font.lab=3)
+par(bg = "gray")
+pie(rep(1,24), col = rainbow(24), radius = 0.9)
+title(main = "A Sample Color Wheel", cex.main = 1.4, font.main = 3)
+title(xlab = "(Use this as a test of monitor linearity)",
+      cex.lab = 0.8, font.lab = 3)
 
 
 ## We have already confessed to having these.  This is just showing off X11
@@ -44,9 +45,9 @@ pie.sales <- c(0.12, 0.3, 0.26, 0.16, 0.04, 0.12)
 names(pie.sales) <- c("Blueberry", "Cherry",
 		      "Apple", "Boston Cream", "Other", "Vanilla Cream")
 pie(pie.sales,
-    col=c("purple","violetred1","green3","cornsilk","cyan","white"))
-title(main="January Pie Sales", cex.main=1.8, font.main=1)
-title(xlab="(Don't try this at home kids)", cex.lab=0.8, font.lab=3)
+    col = c("purple","violetred1","green3","cornsilk","cyan","white"))
+title(main = "January Pie Sales", cex.main = 1.8, font.main = 1)
+title(xlab = "(Don't try this at home kids)", cex.lab = 0.8, font.lab = 3)
 
 
 ## Boxplots:  I couldn't resist the capability for filling the "box".
@@ -89,8 +90,8 @@ points(x, pch=21, bg="lightcyan", cex=1.25)
 axis(2, col.axis="blue", las=1)
 axis(1, at=1:12, lab=month.abb, col.axis="blue")
 box()
-title(main="The Level of Interest in R", font.main=4, col.main="red")
-title(xlab="1996", col.lab="red")
+title(main= "The Level of Interest in R", font.main=4, col.main="red")
+title(xlab= "1996", col.lab="red")
 
 
 ## A filled histogram, showing how to change the font used for the
@@ -107,7 +108,7 @@ title(main="1000 Normal Random Variates", font.main=3)
 
 pairs(iris[1:4], main="Edgar Anderson's Iris Data", font.main=4, pch=19)
 pairs(iris[1:4], main="Edgar Anderson's Iris Data", pch=21,
-      bg=c("red", "green3", "blue")[unclass(iris$Species)])
+      bg = c("red", "green3", "blue")[unclass(iris$Species)])
 
 
 ## Contour plotting
@@ -115,31 +116,31 @@ pairs(iris[1:4], main="Edgar Anderson's Iris Data", pch=21,
 
 x <- 10*1:nrow(volcano)
 y <- 10*1:ncol(volcano)
-l <- pretty(range(volcano), 10)
-par(bg="lightcyan")
+lev <- pretty(range(volcano), 10)
+par(bg = "lightcyan")
 pin <- par("pin")
 xdelta <- diff(range(x))
 ydelta <- diff(range(y))
 xscale <- pin[1]/xdelta
 yscale <- pin[2]/ydelta
-scale <- if(xscale < yscale) xscale else yscale
-xadd <- 0.5*(pin[1]/scale-xdelta)
-yadd <- 0.5*(pin[2]/scale-ydelta)
+scale <- min(xscale, yscale)
+xadd <- 0.5*(pin[1]/scale - xdelta)
+yadd <- 0.5*(pin[2]/scale - ydelta)
 plot(numeric(0), numeric(0),
-     xlim=range(x)+c(-1,1)*xadd, ylim=range(y)+c(-1,1)*yadd,
-     type="n", ann=FALSE)
+     xlim = range(x)+c(-1,1)*xadd, ylim = range(y)+c(-1,1)*yadd,
+     type = "n", ann = FALSE)
 usr <- par("usr")
 rect(usr[1], usr[3], usr[2], usr[4], col="green3")
-contour(x, y, volcano, levels=l, col="yellow", lty="solid", add=TRUE)
+contour(x, y, volcano, levels = lev, col="yellow", lty="solid", add=TRUE)
 box()
-title("A Topographic Map of Maunga Whau",font=4)
-title(xlab="Meters North", ylab="Meters West", font=3)
+title("A Topographic Map of Maunga Whau", font= 4)
+title(xlab = "Meters North", ylab = "Meters West", font= 3)
 mtext("10 Meter Contour Spacing", side=3, line=0.35, outer=FALSE,
-      at=mean(par("usr")[1:2]), cex=0.7, font=3)
+      at = mean(par("usr")[1:2]), cex=0.7, font=3)
 
 ## Conditioning plots
 
 par(bg="cornsilk")
-coplot(lat ~ long | depth, data=quakes, pch=21, bg="green3")
+coplot(lat ~ long | depth, data = quakes, pch = 21, bg = "green3")
 
 par(opar)
