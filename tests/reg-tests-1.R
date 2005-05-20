bccf531b73855f4e5dd18ff66bdc4221ccc681d4
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2005-05-20 12:13:45 +0000

    port r34442 from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-2-1-patches@34443 00db46b3-68df-0310-9c12-caf00c1e9a41

--- tests/reg-tests-1.R
+++ tests/reg-tests-1.R
@@ -3644,6 +3644,18 @@ duplicated(x)
 unique(x)
 ## unique failed in 2.1.0
 
+## prog.aovlist on data with row.names
+N <- c(0,1,0,1,1,1,0,0,0,1,1,0,1,1,0,0,1,0,1,0,1,1,0,0)
+P <- c(1,1,0,0,0,1,0,1,1,1,0,0,0,1,0,1,1,0,0,1,0,1,1,0)
+K <- c(1,0,0,1,0,1,1,0,0,1,0,1,0,1,1,0,0,0,1,1,1,0,1,0)
+yield <- c(49.5,62.8,46.8,57.0,59.8,58.5,55.5,56.0,62.8,55.8,69.5,
+  55.0, 62.0,48.8,45.5,44.2,52.0,51.5,49.8,48.8,57.2,59.0,53.2,56.0)
+npk <- data.frame(block=gl(6,4), N=factor(N), P=factor(P),
+		  K=factor(K), yield=yield)
+row.names(npk) <- letters[2:25]
+npk.aovE <- aov(yield ~  N*P*K + Error(block), npk)
+pr <- proj(npk.aovE)
+## failed in 2.1.1
 
 ### end of tests added in 2.1.0 patched ###
 
