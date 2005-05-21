a03ea80227b8b876a6675067dca3d8159dc01bb5
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2005-05-21 09:16:29 +0000

    update for Inno Setup 5.1.3
    git-svn-id: https://svn.r-project.org/R/trunk@34458 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/front-ends/md5check.c
+++ src/gnuwin32/front-ends/md5check.c
@@ -1,6 +1,6 @@
 /*
  *  R : A Computer Language for Statistical Data Analysis
- *  Copyright (C) 2004  R Development Core Team
+ *  Copyright (C) 2004-5  R Development Core Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -42,8 +42,8 @@ void read_unist_file(char* fname)
     }
     fseek(fp, 320, SEEK_SET);
     fread(&Version, 4, 1, fp);
-    /* 4.2.x is 15, 5.0.6 is 33 */
-    if(Version > 33)
+    /* 4.2.x is 15, 5.0.6 is 33 5.1.3 is 42*/
+    if(Version > 42)
 	fprintf(stderr, "Version %d of the uninst format is new and may not be supported\n", Version);
     fread(&NumRecs, 4, 1, fp);
     fread(&EndOffset, 4, 1, fp);
