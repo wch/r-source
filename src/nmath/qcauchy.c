ca6e9404bb5e824b2cd55e7357a0c2fb697d9013
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2005-06-01 09:46:55 +0000

    ported r34525 (qcauchy, PR#7902) from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-2-1-patches@34526 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/nmath/qcauchy.c
+++ src/nmath/qcauchy.c
@@ -2,6 +2,9 @@
  *  Mathlib : A C Library of Special Functions
  *  Copyright (C) 1998 Ross Ihaka
  *  Copyright (C) 2000 The R Development Core Team
+ *  Copyright (C) 2005 The R Foundation
+ *
+ *  This version is based on a suggestion by Morten Welinder.
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -13,9 +16,8 @@
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA.
+ *  A copy of the GNU General Public License is available via WWW at
+ *  http://www.gnu.org/copyleft/gpl.html.
  *
  *  DESCRIPTION
  *
@@ -32,10 +34,26 @@ double qcauchy(double p, double location, double scale,
     if (ISNAN(p) || ISNAN(location) || ISNAN(scale))
 	return p + location + scale;
 #endif
-    if(!R_FINITE(p) || !R_FINITE(location) || !R_FINITE(scale))
-	ML_ERR_return_NAN;
     R_Q_P01_check(p);
-    if (scale <= 0) ML_ERR_return_NAN;
+    if (scale <= 0 || !R_FINITE(scale)) {
+	if (scale == 0) return location;
+	/* else */ ML_ERR_return_NAN;
+    }
 
-    return location + scale * tan(M_PI * (R_DT_qIv(p) - 0.5));
+    if (log_p) {
+	if (p > -1) {
+	    /* when ep := exp(p),
+	     * tan(pi*ep)= -tan(pi*(-ep))= -tan(pi*(-ep)+pi) = -tan(pi*(1-ep)) =
+	     *		 = -tan(pi*(-expm1(p))
+	     * for p ~ 0, exp(p) ~ 1, tan(~0) may be better than tan(~pi).
+	     */
+	    if (p == 0.) /* needed, since 1/tan(-0) = -Inf  for some arch. */
+		return location + (lower_tail ? scale : -scale) * ML_POSINF;
+	    lower_tail = !lower_tail;
+	    p = -expm1(p);
+	} else
+	    p = exp(p);
+    }
+    return location + (lower_tail ? -scale : scale) / tan(M_PI * p);
+    /*	-1/tan(pi * p) = -cot(pi * p) = tan(pi * (p - 1/2))  */
 }
