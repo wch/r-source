22c1e7d3cc1204bc8fb0346ff1f0589bd4bb7f3b
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2005-06-21 17:49:05 +0000

    runmed(*, -1)
    git-svn-id: https://svn.r-project.org/R/trunk@34796 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/R/runmed.R
+++ src/library/stats/R/runmed.R
@@ -25,6 +25,7 @@ runmed <- function(x, k, endrule = c("median","keep","constant"),
 {
     n <- length(x)
     k <- as.integer(k)
+    if(k < 0) stop("'k' must be positive")
     if(k%%2 == 0)
         warning("'k' must be odd!  Changing 'k' to ",
                 k <- as.integer(1+ 2*(k %/% 2)))
