fb9a0cec9bd267aaf748ca4a361ad0438474a6ce
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2005-07-24 14:26:26 +0000

    Factor out .build_repository_package_db(). Use message() for verbose diagnostics. Use NULL default value for 'fields'.
    git-svn-id: https://svn.r-project.org/R/trunk@35027 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/writePACKAGES.R
+++ src/library/tools/R/writePACKAGES.R
@@ -1,75 +1,16 @@
 write_PACKAGES <-
-    function(dir, fields,
-             type = c("source", "mac.binary", "win.binary"),
-             verbose = FALSE)
+function(dir, fields = NULL,
+         type = c("source", "mac.binary", "win.binary"),
+         verbose = FALSE)
 {
-    if(missing(type) && .Platform$OS.type == "windows") type <- "win.binary"
+    if(missing(type) && .Platform$OS.type == "windows")
+        type <- "win.binary"
     type <- match.arg(type)
-    files <- list.files(dir,
-                        pattern = switch(type,
-                        "source" = "\\.tar\\.gz$",
-                        "mac.binary" = "\\.tgz$",
-                        "win.binary" = "\\.zip$")
-                        )
-    if(length(files)) {
-        ## Standard set of fields required to build a
-        ## repository's PACKAGES file:
-        if(missing(fields))
-            fields <- c("Package", "Bundle", "Priority", "Version",
-                        "Depends", "Suggests", "Imports", "Contains")
-        packages <- sapply(strsplit(files, "_"), "[", 1)
-        files <- file.path(dir, files)
-        desc <- vector(length(files), mode = "list")
-        ## many (roughly length(files))
-        ## warnings are *expected*, hence suppressed
-        op <- options(warn = -1)
-        if(verbose) cat(gettext("Processing packages:\n"))
-        if(type == "win.binary") {
-            for(i in seq(along = files)) {
-                if(verbose) cat("  ", files[i], "\n", sep ="")
-                ## for bundles:
-                con <- unz(files[i], "DESCRIPTION")
-                temp <- try(read.dcf(con, fields = fields), silent = TRUE)
-                if(inherits(temp, "try-error")) {
-                    close(con)
-                    ## for regular packages:
-                    con <- unz(files[i], file.path(packages[i], "DESCRIPTION"))
-                    temp <- try(read.dcf(con, fields = fields), silent = TRUE)
-                    if(inherits(temp, "try-error")) {
-                        close(con)
-                        next
-                    }
-                }
-                desc[[i]] <- temp
-                close(con)
-            }
-        } else {
-            dir <- file_path_as_absolute(dir)
-            files <- list.files(dir,
-                                pattern=switch(type,
-                                "source" = "\\.tar\\.gz$",
-                                "mac.binary" = "\\.tgz$"),
-                                full.names=TRUE)
-            cwd <- getwd()
-            td <- tempfile("PACKAGES")
-            if(!dir.create(td)) stop("unable to create ", td)
-            on.exit(unlink(td, recursive = TRUE))
-            setwd(td)
-            for(i in seq(along = files)) {
-                if(verbose) cat("  ", files[i], "\n", sep ="")
-                p <- file.path(packages[i], "DESCRIPTION")
-                temp <- try(system(paste("tar zxf", files[i], p)))
-                if(!inherits(temp, "try-error")) {
-                    temp <- try(read.dcf(p, fields = fields), silent = TRUE)
-                    if(!inherits(temp, "try-error"))
-                        desc[[i]] <- temp
-                }
-                unlink(packages[i], recursive = TRUE)
-            }
-            setwd(cwd)
-        }
-        if(verbose) cat(gettext("done\n"))
-        options(op)                  # change warning level back again
+
+    desc <- .build_repository_package_db(dir, fields, type, verbose)
+
+    if(length(desc)) {
+        fields <- colnames(desc[[1]])
         desc <- matrix(unlist(desc), ncol = length(fields), byrow = TRUE)
         colnames(desc) <- fields
         ## bundles do not have a Package entry in the DESCRIPTION,
@@ -77,12 +18,13 @@ write_PACKAGES <-
         noPack <- is.na(desc[,"Package"])
         desc[noPack, "Package"] <- desc[noPack, "Bundle"]
 
-        ## Writing PACKAGES file from matrix desc linewise in order to omit
-        ## NA entries appropriately:
+        ## Writing PACKAGES file from matrix desc linewise in order to
+        ## omit NA entries appropriately:
         out <- file(file.path(dir, "PACKAGES"), "wt")
         outgz <- gzfile(file.path(dir, "PACKAGES.gz"), "wt")
         for(i in seq(length = nrow(desc))){
-            desci <- desc[i, !(is.na(desc[i, ]) | (desc[i, ] == "")), drop = FALSE]
+            desci <- desc[i, !(is.na(desc[i, ]) | (desc[i, ] == "")),
+                          drop = FALSE]
             write.dcf(desci, file = out)
             write.dcf(desci, file = outgz)
         }
@@ -92,3 +34,79 @@ write_PACKAGES <-
     }
     else invisible(0)
 }
+
+.build_repository_package_db <-
+function(dir, fields = NULL,
+         type = c("source", "mac.binary", "win.binary"),
+         verbose = getOption("verbose"))
+{
+    type <- match.arg(type)
+
+    package_pattern <- switch(type,
+                              "source" = "\\.tar\\.gz$",
+                              "mac.binary" = "\\.tgz$",
+                              "win.binary" = "\\.zip$")
+    files <- list.files(dir, pattern = package_pattern)
+
+    if(!length(files))
+        return(list())
+    
+    ## Standard set of fields required to build a repository's PACKAGES
+    ## file:
+    if(is.null(fields))
+        fields <- c("Package", "Bundle", "Priority", "Version",
+                    "Depends", "Suggests", "Imports", "Contains")
+    packages <- sapply(strsplit(files, "_"), "[", 1)
+    files <- file.path(dir, files)
+    db <- vector(length(files), mode = "list")
+    ## Many (roughly length(files)) warnings are *expected*, hence
+    ## suppressed.
+    op <- options(warn = -1)
+    on.exit(options(op))
+    if(verbose) message("Processing packages:")
+    if(type == "win.binary") {
+        for(i in seq(along = files)) {
+            if(verbose) message(paste(" ", files[i]))
+            ## for bundles:
+            con <- unz(files[i], "DESCRIPTION")
+            temp <- try(read.dcf(con, fields = fields), silent = TRUE)
+            if(inherits(temp, "try-error")) {
+                close(con)
+                ## for regular packages:
+                con <- unz(files[i], file.path(packages[i], "DESCRIPTION"))
+                temp <- try(read.dcf(con, fields = fields), silent = TRUE)
+                if(inherits(temp, "try-error")) {
+                    close(con)
+                    next
+                }
+            }
+            db[[i]] <- temp
+            close(con)
+        }
+    } else {
+        dir <- file_path_as_absolute(dir)
+        files <- list.files(dir, pattern = package_pattern,
+                            full.names = TRUE)
+        cwd <- getwd()
+        td <- tempfile("PACKAGES")
+        if(!dir.create(td)) stop("unable to create ", td)
+        on.exit(unlink(td, recursive = TRUE), add = TRUE)
+        setwd(td)
+        for(i in seq(along = files)) {
+            if(verbose) message(paste(" ", files[i]))
+            p <- file.path(packages[i], "DESCRIPTION")
+            temp <- try(system(paste("tar zxf", files[i], p)))
+            if(!inherits(temp, "try-error")) {
+                temp <- try(read.dcf(p, fields = fields), silent = TRUE)
+                if(!inherits(temp, "try-error"))
+                    db[[i]] <- temp
+            }
+            unlink(packages[i], recursive = TRUE)
+        }
+        setwd(cwd)
+    }
+    if(verbose) message("done")
+
+    db
+}
+    
