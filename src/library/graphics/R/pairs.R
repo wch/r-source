27056dd0025e26152caa594e75ea283979fe91a9
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2005-07-28 09:16:47 +0000

    remove a couple of \synopses
    git-svn-id: https://svn.r-project.org/R/trunk@35062 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/graphics/R/pairs.R
+++ src/library/graphics/R/pairs.R
@@ -20,25 +20,31 @@ function(formula, data = NULL, ..., subset, na.action = stats::na.pass)
 #################################################
 
 pairs.default <-
-function (x, labels, panel = points, ..., main = NULL, oma = NULL,
-          font.main = par("font.main"), cex.main = par("cex.main"),
+function (x, labels, panel = points, ...,
           lower.panel = panel, upper.panel = panel,
           diag.panel = NULL, text.panel = textPanel,
           label.pos = 0.5 + has.diag/3,
           cex.labels = NULL, font.labels = 1,
-          row1attop = TRUE, gap=1)
+          row1attop = TRUE, gap = 1)
 {
     textPanel <-
         function(x = 0.5, y = 0.5, txt, cex, font)
             text(x, y, txt, cex = cex, font = font)
 
-    localAxis <- function(side, xpd, bg, col=NULL, ...) {
+    localAxis <- function(side, xpd, bg, col=NULL, main, ...) {
       ## Explicitly ignore any color argument passed in as
       ## it was most likely meant for the data points and
       ## not for the axis.
       axis(side, xpd = NA, ...)
     }
 
+    localPlot <- function(..., main, oma, font.main, cex.main) plot(...)
+    localLowerPanel <- function(..., main, oma, font.main, cex.main)
+        lower.panel(...)
+    localUpperPanel <- function(..., main, oma, font.main, cex.main)
+        upper.panel(...)
+
+    dots <- list(...); nmdots <- names(dots)
     if (!is.matrix(x)) x <- data.matrix(x)
     if (!is.numeric(x)) stop("non-numeric argument to 'pairs'")
     panel <- match.fun(panel)
@@ -62,6 +68,8 @@ function (x, labels, panel = points, ..., main = NULL, oma = NULL,
         if (is.null(labels)) labels <- paste("var", 1:nc)
     }
     else if(is.null(labels)) has.labs <- FALSE
+    oma <- if("oma" %in% nmdots) dots$oma else NULL
+    main <- if("main" %in% nmdots) dots$main else NULL
     if (is.null(oma)) {
         oma <- c(4, 4, 4, 4)
         if (!is.null(main)) oma[3] <- 6
@@ -71,8 +79,8 @@ function (x, labels, panel = points, ..., main = NULL, oma = NULL,
 
     for (i in if(row1attop) 1:nc else nc:1)
         for (j in 1:nc) {
-            plot(x[, j], x[, i], xlab = "", ylab = "",
-                 axes = FALSE, type = "n", ...)
+            localPlot(x[, j], x[, i], xlab = "", ylab = "",
+                      axes = FALSE, type = "n", ...)
             if(i == j || (i < j && has.lower) || (i > j && has.upper) ) {
                 box()
                 if(i == 1  && (!(j %% 2) || !has.upper || !has.lower ))
@@ -96,15 +104,18 @@ function (x, labels, panel = points, ..., main = NULL, oma = NULL,
                                    cex = cex.labels, font = font.labels)
                     }
                 } else if(i < j)
-                    lower.panel(as.vector(x[, j]), as.vector(x[, i]), ...)
+                    localLowerPanel(as.vector(x[, j]), as.vector(x[, i]), ...)
                 else
-                    upper.panel(as.vector(x[, j]), as.vector(x[, i]), ...)
+                    localUpperPanel(as.vector(x[, j]), as.vector(x[, i]), ...)
                 if (any(par("mfg") != mfg))
                     stop("the 'panel' function made a new plot")
             } else par(new = FALSE)
 
         }
-    if (!is.null(main))
+    if (!is.null(main)) {
+        font.main <- if("font.main" %in% nmdots) dots$font.main else par("font.main")
+        cex.main <- if("cex.main" %in% nmdots) dots$cex.main else par("cex.main")
         mtext(main, 3, 3, TRUE, 0.5, cex = cex.main, font = font.main)
+    }
     invisible(NULL)
 }
