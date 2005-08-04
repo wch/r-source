f69ae76889119e098b50d33933dc2b0cceb8ca29
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2005-08-04 07:05:26 +0000

    make gcFirst=TRUE the default in system.time correct what happens(and what is documented) when proc.time is not supported.
    git-svn-id: https://svn.r-project.org/R/trunk@35142 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/time.R
+++ src/library/base/R/time.R
@@ -1,17 +1,21 @@
-system.time <- function(expr, gcFirst = FALSE) {
+system.time <- function(expr, gcFirst = TRUE)
+{
     if(!exists("proc.time")) return(rep(as.numeric(NA), 5))
     loc.frame <- parent.frame()
     if(gcFirst)  gc(FALSE)
-    on.exit(cat("Timing stopped at:", proc.time() - time, "\n"))
     expr <- substitute(expr)
     time <- proc.time()
+    ## need on.exit after 'time' has been set:
+    ## on some systems proc.time throws an error.
+    on.exit(cat("Timing stopped at:", proc.time() - time, "\n"))
     eval(expr, envir = loc.frame)
     new.time <- proc.time()
     on.exit()
-    if(length(new.time) == 3)	new.time <- c(new.time, 0, 0)
-    if(length(time) == 3)	time	 <- c(	  time, 0, 0)
+    ## this cannot happen
+    ## if(length(new.time) == 3)	new.time <- c(new.time, 0, 0)
+    ## if(length(time) == 3)	time	 <- c(	  time, 0, 0)
     new.time - time
 }
 unix.time <- system.time
 
-date <- function().Internal(date())
+date <- function() .Internal(date())
