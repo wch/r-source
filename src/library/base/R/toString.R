1e7c6c0f80a446a9409ef5783343f94f6628308c
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2005-08-14 07:13:06 +0000

    correct description, allow NULL and 0 for width (like format)
    git-svn-id: https://svn.r-project.org/R/trunk@35279 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/toString.R
+++ src/library/base/R/toString.R
@@ -1,18 +1,14 @@
 #functions to convert their first argument to strings
-toString <- function(x, ...)
-    UseMethod("toString")
+toString <- function(x, ...) UseMethod("toString")
 
-toString.default <- function(x, width, ...)
+toString.default <- function(x, width = NULL, ...)
 {
     string <- paste(x, collapse=", ")
-    if( missing(width) )
-        return( string )
-    if( width <= 0 )
-        stop("'width' must be positive")
-    if(nchar(string, type="c") > width) {
+    if( missing(width) || is.null(width) || width == 0) return(string)
+    if( width < 0 ) stop("'width' must be positive")
+    if(nchar(string, type = "w") > width) {
         width <- max(6, width) ## Leave something!
-        string <- paste(strtrim(string, width-4), "....", sep = "")
+        string <- paste(strtrim(string, width - 4), "....", sep = "")
     }
     string
 }
-
