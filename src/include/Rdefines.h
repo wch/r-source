e8d7ff4fc9d6664543dd6e5b0a91a744952c7681
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2005-08-20 18:40:53 +0000

    correct copyright date
    git-svn-id: https://svn.r-project.org/R/trunk@35337 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/include/Rdefines.h
+++ src/include/Rdefines.h
@@ -1,6 +1,6 @@
 /*
  *  R : A Computer Language for Statistical Data Analysis
- *  Copyright (C) 1999-2001 The R Development Core Team.
+ *  Copyright (C) 1999-2005 The R Development Core Team.
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as published by
