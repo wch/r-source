fb30f8e3cf45025dbc1286f307cf4f294f1db7fe
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2005-09-09 21:14:24 +0000

    yet another round
    git-svn-id: https://svn.r-project.org/R/trunk@35540 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/methods/R/cbind-rbind.R
+++ src/library/methods/R/cbind-rbind.R
@@ -1,73 +1,102 @@
 
 ### Experimental {similar c() code: MM's ~/R/MM/Pkg-ex/methods/cbind-etc.R}
 
+## NOTE: We rely on
+##
+## o   dim(.) working reliably for all arguments of cbind2()
+## o   All cbind2() methods are assumed to
+##	       1) correctly set rownames
+##	       2) correctly set colnames for *matrix*(like) arguments
+
 ## Note that this
 ## 1) is namespace-hidden usually,
 ## 2) cbind / rbind are almost never called in 'methods' itself
 ## hence, the following has almost no effect unless ``activated'' (see below)
+
 cbind <- function(..., deparse.level = 1)
 {
     na <- nargs() - !missing(deparse.level)
     deparse.level <- as.integer(deparse.level)
     stopifnot(0 <= deparse.level, deparse.level <= 2)
 
+    argl <- list(...)
+    ## remove trailing 'NULL's:
+    while(na > 0 && is.null(argl[[na]])) { argl <- argl[-na]; na <- na - 1 }
+    if(na == 0) return(NULL)
     if(na <= 1)
-        ## FIXME(?) what if  ..1  is an ``object''?
-        ## -------  in that case  call  cbind2(<object>, NULL) ?
-        return(.Internal(cbind(deparse.level, ...)))
+	## FIXME(?) if	..1  is an ``object'', {once we can test this reliably!}
+	## -------  call  cbind2(..1, NULL) ?
+	return(.Internal(cbind(deparse.level, ...)))
 
-    ## else :
-    larg <- as.list(sys.call()[-1])
-    ism1 <- !is.null(d1 <- dim(..1)) && length(d1) == 2
+    ## else :  na >= 2
+
+    if(deparse.level) {
+	symarg <- as.list(sys.call()[-1])[1:na] # the unevaluated arguments
+	## For default 'deparse.level = 1', cbind(a, b) has to give *names*!
+	Nms <- function(i) { # possibly 'deparsed' names of argument  i
+	    if(is.null(r <- names(symarg[i])) || r == "") {
+		if(is.symbol(r <- symarg[[i]]) || deparse.level == 2)
+		    deparse(r)		# else NULL
+	    } else r
+	}
+    }
     if(na == 2) {
-        r <- ..2
-    } else { ## na >= 3 arguments: recurse but need care
-        ## determine nrow(<result>)  for e.g.,  cbind(diag(2), 1, 2)
-        ## only when the last two argument have *no* dim attribute:
-        rest <- list(...)[-1]
-        nrs <- lapply(rest, nrow) # of length (na - 1)
-        if(identical(nrs[(na-2):(na-1)], list(NULL,NULL))) {
-            nr <- max(nrs[!sapply(nrs, is.null)])
-            ## use 1-column `matrix'; but no special name
-            rest[[na - 1]] <- cbind(rest[[na - 1]], deparse.level = 0)
-        }
-        r <- do.call(cbind, c(rest, list(deparse.level=deparse.level)))
+	r <- ..2
+	fix.na <- FALSE
+    }
+    else { ## na >= 3 arguments: -- RECURSION -- with care
+	## determine nrow(<result>)  for e.g.,	cbind(diag(2), 1, 2)
+	## only when the last two argument have *no* dim attribute:
+	nrs <- unname(lapply(argl, nrow)) # of length na
+	iV <- sapply(nrs, is.null)# is 'vector'
+	fix.na <- identical(nrs[(na-1):na], list(NULL,NULL))
+	if(fix.na) {
+	    ## "fix" last argument, using 1-column `matrix' of proper nrow():
+	    nr <- max(if(all(iV)) sapply(argl, length) else unlist(nrs[!iV]))
+	    argl[[na]] <- cbind(rep(argl[[na]], length.out = nr),
+				deparse.level = 0)
+	    ## and since it's a 'matrix' now, cbind() below may not name it
+	}
+	## need to pass argl, the evaluated arg list to do.call();
+	## OTOH, these may have lost their original 'symbols'
+	if(deparse.level) {
+	    if(fix.na)
+		fix.na <- !is.null(Nna <- Nms(na))
+	    if(!is.null(nmi <- names(argl))) iV <- iV & (nmi == "")
+	    ## attach `symbols' to argl[-1] for 'vectors'[iV]
+	    ii <- if(fix.na) # need to fix later ([na] is 'matrix')
+		2:(na-1) else 2:na
+	    if(any(iV[ii])) {
+		for(i in ii[iV[ii]])
+		    if (!is.null(nmi <- Nms(i))) names(argl)[i] <- nmi
+	    }
+	}
+	r <- do.call(cbind, c(argl[-1], list(deparse.level=deparse.level)))
     }
 
-    ism2 <- !is.null(d2 <- dim( r )) && length(d2) == 2
+    d2 <- dim(r)
     r <- cbind2(..1, r)
-    ## NOTE: We rely on
-    ##
-    ## o   dim(.) working reliably for all arguments
-    ## o   All cbind2() methods are assumed to
-    ##	       1) correctly set rownames
-    ##	       2) correctly set colnames for *matrix*(like) arguments
-
-    if(ism1 && ism2 || deparse.level == 0)# two matrices or no 'deparse naming'
+    if(deparse.level == 0)
+	return(r)
+    ism1 <- !is.null(d1 <- dim(..1)) && length(d1) == 2
+    ism2 <- !is.null(d2)	     && length(d2) == 2 && !fix.na
+    if(ism1 && ism2) ## two matrices
 	return(r)
 
-    ## else -- all the rest: Setting colnames correctly
+    ## else -- Setting colnames correctly
     ##	       when one was not a matrix [needs some diligence!]
-
     Ncol <- function(x) {
 	d <- dim(x); if(length(d) == 2) d[2] else as.integer(length(x) > 0) }
-    ## For default 'deparse.level = 1',	  cbind(a, b) has to give *names*!
-    Nms <- function(i) { # possibly 'deparsed' names of argument  i
-	if(is.null(r <- names(larg[i])) || r == "") {
-	    if(is.symbol(r <- larg[[i]]) || deparse.level == 2)
-		deparse(r) # else NULL
-	} else r
-    }
-
     nn1 <- !is.null(N1 <- if((l1 <- Ncol(..1)) && !ism1) Nms(1)) # else NULL
     nn2 <- !is.null(N2 <- if(na == 2 && Ncol(..2) && !ism2) Nms(2))
-    if(nn1 || nn2) {
+    if(nn1 || nn2 || fix.na) {
 	if(is.null(colnames(r)))
 	    colnames(r) <- rep.int("", ncol(r))
-        setN <- function(i0, nams)
-            colnames(r)[i0+1] <<- if(is.null(nams)) "" else nams
-	if(nn1) setN(0,	 N1)
-	if(nn2) setN(l1, N2)
+	setN <- function(i, nams)
+            colnames(r)[i] <<- if(is.null(nams)) "" else nams
+	if(nn1) setN(1,	 N1)
+	if(nn2) setN(1+l1, N2)
+	if(fix.na) setN(ncol(r), Nna)
     }
     r
 }
@@ -79,11 +108,14 @@ bind_activation <- function(on = TRUE) {
     if(on) {
 	utils::assignInNamespace(".__H__.cbind", base::cbind, ns = base.ns)
 	utils::assignInNamespace("cbind", cbind, ns = base.ns)
+##	utils::assignInNamespace(".__H__.rbind", base::rbind, ns = base.ns)
+##	utils::assignInNamespace("rbind", rbind, ns = base.ns)
     }
     else { # turn it off
 	if(!inherits(try(getFromNamespace(".__H__.cbind", ns = base.ns),
 			 silent = TRUE), "try-error"))
 	    utils::assignInNamespace("cbind", base::.__H__.cbind, ns = base.ns)
+##	    utils::assignInNamespace("rbind", base::.__H__.rbind, ns = base.ns)
     }
 }
 
