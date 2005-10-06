946d86426ceb4ff8bae5bdbed6852abcdae30713
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2005-10-06 19:18:08 +0000

    S-PLUS compatibility update to arima.sim
    git-svn-id: https://svn.r-project.org/R/trunk@35765 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/R/ts.R
+++ src/library/stats/R/ts.R
@@ -732,7 +732,8 @@ ts.plot <- function(..., gpars = list())
 }
 
 arima.sim <- function(model, n, rand.gen = rnorm,
-                      innov = rand.gen(n, ...), n.start = NA, ...)
+                      innov = rand.gen(n, ...), n.start = NA,
+                      start.innov = rand.gen(n.start, ...), ...)
 {
     if(!is.list(model)) stop("'model' must be list")
     p <- length(model$ar)
@@ -753,7 +754,10 @@ arima.sim <- function(model, n, rand.gen = rnorm,
         if(d != round(d) || d < 0)
             stop("number of differences must be a positive integer")
     }
-    x <- ts(c(rand.gen(n.start, ...), innov[1:n]), start = 1 - n.start)
+    if(!missing(start.innov) && length(start.innov) < n.start)
+        stop(gettextf("'start.innov' is too short: need %d points", n.start),
+             domain = NA)
+    x <- ts(c(start.innov[1:n.start], innov[1:n]), start = 1 - n.start)
     if(length(model$ma)) x <- filter(x, c(1, model$ma), sides = 1)
     if(length(model$ar)) x <- filter(x, model$ar, method = "recursive")
     if(n.start > 0) x <- x[-(1:n.start)]
