37bfb40e6e11d916513007ae28c574d625633061
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2005-10-08 10:50:22 +0000

    update on Nakama patch
    git-svn-id: https://svn.r-project.org/R/trunk@35810 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/grDevices/src/devPS.c
+++ src/library/grDevices/src/devPS.c
@@ -5410,7 +5410,7 @@ static void PDF_endfile(PDFDesc *pd)
 			"  /Type /Font\n"
 			"  /Subtype /Type0\n" 
 			"  /Name /%s_%d\n"
-			"  /BaseFont /%s\n"
+			"  /BaseFont /%s%s\n"
 			"  /DescendantFonts [\n"
 			"    <<\n"
 			"      /Type /Font\n"
@@ -5427,6 +5427,7 @@ static void PDF_endfile(PDFDesc *pd)
 			fontlist->cidfamily->fxname,             /* /Name       */
 			i+1,                                     /* - face      */
 			CIDResource[cid_id].pdffontname,         /* /BaseFont   */
+			boldslant(i),                            /* - boldslant */
 			CIDResource[cid_id].pdffontname,         /* /BaseFont   */
 			boldslant(i),                            /* - boldslant */
 			CIDResource[cid_id].pdfresource,         /* Resource    */
