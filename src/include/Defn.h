fc022043b9f92e5e0a414c89d353cda04d797d91
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2005-10-19 07:16:39 +0000

    change default expressions limit to 1000
    git-svn-id: https://svn.r-project.org/R/branches/R-2-2-patches@35939 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/include/Defn.h
+++ src/include/Defn.h
@@ -500,7 +500,7 @@ LibExtern int	R_Visible;	    /* Value visibility flag */
 LibExtern int	R_EvalDepth	INI_as(0);	/* Evaluation recursion depth */
 extern int	R_BrowseLevel	INI_as(0);	/* how deep the browser is */
 
-extern int	R_Expressions	INI_as(5000);	/* options(expressions) */
+extern int	R_Expressions	INI_as(1000);	/* options(expressions) */
 extern Rboolean	R_KeepSource	INI_as(FALSE);	/* options(keep.source) */
 extern int	R_UseNamespaceDispatch INI_as(TRUE);
 extern int	R_WarnLength	INI_as(1000);	/* Error/warning max length */
