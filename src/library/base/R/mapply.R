ca7fc589621553b95bc4e5f9cc825709ec330fc2
murdoch <murdoch@00db46b3-68df-0310-9c12-caf00c1e9a41>
2005-11-02 18:53:49 +0000

    Added Vectorize function
    git-svn-id: https://svn.r-project.org/R/trunk@36144 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/mapply.R
+++ src/library/base/R/mapply.R
@@ -21,3 +21,30 @@ mapply<-function(FUN,..., MoreArgs=NULL, SIMPLIFY=TRUE, USE.NAMES=TRUE)
     else answer
 }
 
+Vectorize <- function(FUN, vectorize.args = arg.names, SIMPLIFY = TRUE, 
+		USE.NAMES = TRUE) {
+    arg.names <- as.list(formals(FUN))
+    arg.names[["..."]] <- NULL
+    arg.names <- names(arg.names)
+
+    vectorize.args <- as.character(vectorize.args)
+
+    if (!length(vectorize.args)) return(FUN)
+
+    if (!all(vectorize.args %in% arg.names)) 
+    	stop("must specify formal argument names to vectorize")
+
+    FUNV <- function() { # will set the formals below
+        args <- lapply(as.list(match.call())[-1], eval, parent.frame())
+        names <- if(is.null(names(args))) character(length(args)) 
+        	 else names(args)
+        dovec <- names %in% vectorize.args
+        do.call("mapply", c(FUN = FUN,
+                            args[dovec],
+                            MoreArgs = list(args[!dovec]),
+                            SIMPLIFY = SIMPLIFY,
+                            USE.NAMES = USE.NAMES))
+    }
+    formals(FUNV) <- formals(FUN)
+    FUNV
+}
