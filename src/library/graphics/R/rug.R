722d941dddead2c6f052993fcf3ca602d909e040
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2005-11-05 09:20:05 +0000

    use inline pars not global pars.
    git-svn-id: https://svn.r-project.org/R/trunk@36192 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/graphics/R/rug.R
+++ src/library/graphics/R/rug.R
@@ -1,15 +1,9 @@
-rug <- function(x, ticksize = 0.03, side = 1, lwd = 0.5, col,
+rug <- function(x, ticksize = 0.03, side = 1, lwd = 0.5, col = par("fg"),
 		quiet = getOption("warn") < 0, ...)
 {
     x <- as.vector(x)
     ok <- is.finite(x)
     x <- x[ok]
-    oldtick <- par(tck = ticksize)
-    on.exit(par(oldtick))
-    if( !missing(col) ) {
-	oldcol <- par(fg = col)
-	on.exit(par(oldcol), add=TRUE)
-    }
     if(!quiet) {
 	u <- par("usr")
 	u <- if (side %% 2 == 1) {
@@ -20,5 +14,6 @@ rug <- function(x, ticksize = 0.03, side = 1, lwd = 0.5, col,
 	if(any(x < u[1] | x > u[2]))
 	    warning("some values will be clipped")
     }
-    Axis(side = side, at = x, labels = FALSE, lwd = lwd, ...)
+    Axis(side = side, at = x, labels = FALSE, lwd = lwd, col = col,
+         tck = ticksize, ...)
 }
