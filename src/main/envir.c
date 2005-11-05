044db6f4336fa6e907aae6eafddca6aca01ee7d5
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2005-11-05 09:15:45 +0000

    as.environment(NULL) is supposed to have been deprecated, not an error.
    git-svn-id: https://svn.r-project.org/R/trunk@36190 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/envir.c
+++ src/main/envir.c
@@ -2623,6 +2623,9 @@ SEXP do_as_environment(SEXP call, SEXP op, SEXP args, SEXP rho)
 	return matchEnvir(call, CHAR(asChar(arg)));
     case REALSXP: case INTSXP:
 	return do_pos2env(call, op, args, rho);
+    case NILSXP:
+	warning(_("using 'as.environment(NULL)' is deprecated"));
+	return R_BaseEnv;
     default:
 	errorcall(call, _("invalid object for as.environment"));
 	return R_NilValue;	/* -Wall */
