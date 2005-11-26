25bcc0f9428e415a34e4083c33df8183db25859f
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2005-11-26 11:15:48 +0000

    revert last change
    git-svn-id: https://svn.r-project.org/R/trunk@36485 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/src/md5.h
+++ src/library/tools/src/md5.h
@@ -143,11 +143,10 @@ extern int md5_stream __P ((FILE *stream, void *resblock));
    digest.  */
 extern void *md5_buffer __P ((const char *buffer, size_t len, void *resblock));
 
-#idef UNUSED
 /* The following is from gnupg-1.0.2's cipher/bithelp.h.  */
 /* Rotate a 32 bit integer by n bytes */
 #if defined __GNUC__ && defined __i386__
-static inline md5_uint32
+static md5_uint32
 rol(md5_uint32 x, int n)
 {
   __asm__("roll %%cl,%0"
@@ -157,7 +156,6 @@ rol(md5_uint32 x, int n)
 }
 #else
 # define rol(x,n) ( ((x) << (n)) | ((x) >> (32-(n))) )
-#endif
 #endif
 
 #endif /* R_MD5_H */
