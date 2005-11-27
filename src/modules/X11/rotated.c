c809d363086eeb9648f9dc5176800836416b9958
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2005-11-27 07:43:10 +0000

    typo
    git-svn-id: https://svn.r-project.org/R/trunk@36513 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/modules/X11/rotated.c
+++ src/modules/X11/rotated.c
@@ -42,7 +42,7 @@
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
-#if i!defined(strdup) && defined(HAVE_DECL_STRDUP) && !HAVE_DECL_STRDUP
+#if !defined(strdup) && defined(HAVE_DECL_STRDUP) && !HAVE_DECL_STRDUP
 extern char *strdup(const char *s1);
 #endif
 
