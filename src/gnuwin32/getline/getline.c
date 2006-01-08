a24b3692a30ecb43fb99b8df83b9cd97336dc521
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2006-01-08 12:05:55 +0000

    make ^C send a signal even during input
    git-svn-id: https://svn.r-project.org/R/trunk@37024 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/getline/getline.c
+++ src/gnuwin32/getline/getline.c
@@ -255,7 +255,7 @@ gl_char_init()			/* turn off input echo */
        Win32OutputStream = GetStdHandle(STD_OUTPUT_HANDLE);	
    }
    GetConsoleMode(Win32InputStream,&OldWin32Mode);
-   SetConsoleMode(Win32InputStream, 0);
+   SetConsoleMode(Win32InputStream, ENABLE_PROCESSED_INPUT); /* So ^C works */
    AltIsDown = 0;
 #endif      
    
