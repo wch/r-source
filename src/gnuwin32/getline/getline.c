d23d2b9373df350c8c8a007810f9912cdf9893b7
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2006-01-20 12:03:37 +0000

    transposition was broken in non-MBCS locales
    git-svn-id: https://svn.r-project.org/R/trunk@37137 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/getline/getline.c
+++ src/gnuwin32/getline/getline.c
@@ -884,31 +884,30 @@ gl_transpose(void)
 	    mbstate_t mb_st;
 
 	    mbs_init(&mb_st);
-	    for (i = 0; i < gl_pos ;) {
+	    for (i = 0; i < gl_pos;) {
 		l_len = mbrlen(gl_buf+i, MB_CUR_MAX, &mb_st);
 		i += l_len;
 	    }
 	    mbs_init(&mb_st);
 	    r_len = mbrlen(gl_buf+gl_pos, MB_CUR_MAX, &mb_st);
-	    for (i=0; i<r_len; i++) {
-		for(j=0; j<l_len; j++) {
+	    for (i = 0; i < r_len; i++) {
+		for(j = 0; j < l_len; j++) {
 		    c = gl_buf[gl_pos+i-j];
 		    gl_buf[gl_pos+i-j] = gl_buf[gl_pos+i-j-1];
 		    gl_buf[gl_pos+i-j-1] = (char)c;
 		}
 	    }
 	    gl_extent = l_len + r_len;
-	    gl_fixup(gl_prompt,
-		     gl_pos - l_len,
-		     gl_pos + (r_len - l_len)); 
+	    gl_fixup(gl_prompt, gl_pos - l_len, gl_pos + (r_len - l_len)); 
+	} else
+#endif  /* SUPPORT_MBCS */
+	{
+	    c = gl_buf[gl_pos-1];
+	    gl_buf[gl_pos-1] = gl_buf[gl_pos];
+	    gl_buf[gl_pos] = (char) c;
+	    gl_extent = 2;
+	    gl_fixup(gl_prompt, gl_pos-1, gl_pos);
 	}
-#else  /* SUPPORT_MBCS */
-	c = gl_buf[gl_pos-1];
-	gl_buf[gl_pos-1] = gl_buf[gl_pos];
-	gl_buf[gl_pos] = (char) c;
-	gl_extent = 2;
-	gl_fixup(gl_prompt, gl_pos-1, gl_pos);
-#endif /* SUPPORT_MBCS */
     } else
 	gl_beep();
 }
