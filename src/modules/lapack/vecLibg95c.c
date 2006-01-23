a9106a11958a2f606b68ae36503400cb9eca91b9
urbaneks <urbaneks@00db46b3-68df-0310-9c12-caf00c1e9a41>
2006-01-23 06:49:06 +0000

    Fix fn names in vecLib ABI fix for gfortran
    git-svn-id: https://svn.r-project.org/R/trunk@37143 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/modules/lapack/vecLibg95c.c
+++ src/modules/lapack/vecLibg95c.c
@@ -1,24 +1,21 @@
+#ifdef HAVE_CONFIG_H
 #include <config.h>
+#endif
+
 #include <vecLib/vecLib.h>
 
-#ifdef HAVE_F77_UNDERSCORE
-# define F77_SYMBOL(x)   x ## _
-#else
-# define F77_SYMBOL(x)   x
-#endif                                                                                                         
-
-void F77_SYMBOL(rcblas_cdotu_sub)(const int *N, const void *X, const int *incX,
+void F77_FUNC_(rcblas_cdotu_sub,)(const int *N, const void *X, const int *incX,
                        const void *Y, const int *incY, void *dotu) 
 { cblas_cdotu_sub(*N, X, *incX, Y, *incY, dotu); };
 
-void F77_SYMBOL(rcblas_cdotc_sub)(const int *N, const void *X, const int *incX,
+void F77_FUNC_(rcblas_cdotc_sub,)(const int *N, const void *X, const int *incX,
                        const void *Y, const int *incY, void *dotc)
 { cblas_cdotc_sub(*N, X, *incX, Y, *incY, dotc); };
 
-void F77_SYMBOL(rcblas_zdotu_sub)(const int *N, const void *X, const int *incX,
+void F77_FUNC_(rcblas_zdotu_sub,)(const int *N, const void *X, const int *incX,
                        const void *Y, const int *incY, void *dotu)
 { cblas_zdotu_sub(*N, X, *incX, Y, *incY, dotu); };
 
-void F77_SYMBOL(rcblas_zdotc_sub)(const int *N, const void *X, const int *incX,
+void F77_FUNC_(rcblas_zdotc_sub,)(const int *N, const void *X, const int *incX,
                        const void *Y, const int *incY, void *dotc)
 { cblas_zdotc_sub(*N, X, *incX, Y, *incY, dotc); };
