58ea9fb319c7e6707a9effde175a468d53d81d3d
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2006-02-01 18:23:30 +0000

    fix df(0, >=< 2, *)
    git-svn-id: https://svn.r-project.org/R/trunk@37235 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/nmath/df.c
+++ src/nmath/df.c
@@ -44,7 +44,8 @@ double df(double x, double m, double n, int give_log)
 	return x + m + n;
 #endif
     if (m <= 0 || n <= 0) ML_ERR_return_NAN;
-    if (x <= 0.) return(R_D__0);
+    if (x < 0.)  return(R_D__0);
+    if (x == 0.) return(m > 2 ? R_D__0 : (m == 2 ? R_D__1 : ML_POSINF));
     if (!R_FINITE(m) && !R_FINITE(n)) { /* both +Inf */
 	if(x == 1.) return ML_POSINF;
 	/* else */  return R_D__0;
