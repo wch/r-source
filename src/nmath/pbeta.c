60c12728566eca4f4703bb0b2784224d00d10b49
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2006-03-12 08:07:43 +0000

    move to toms708 for pbeta in all cases
    git-svn-id: https://svn.r-project.org/R/trunk@37517 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/nmath/pbeta.c
+++ src/nmath/pbeta.c
@@ -1,7 +1,7 @@
 /*
  *  Mathlib : A C Library of Special Functions
  *  Copyright (C) 1998 Ross Ihaka
- *  Copyright (C) 2000, 2005 The R Development Core Team
+ *  Copyright (C) 2000-2006 The R Development Core Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -30,142 +30,28 @@
  *	( = The incomplete beta ratio I_x(p,q) ).
  *
  *  NOTES
- *
- *	This routine is a translation into C of a Fortran subroutine
- *	by W. Fullerton of Los Alamos Scientific Laboratory.
- *
- *  REFERENCE
- *
- *	Bosten and Battiste (1974).
- *	Remark on Algorithm 179, CACM 17, p153, (1974).
+ * 
+ *      As from R 2.3.0, a wrapper for TOMS708
  */
 
 #include "nmath.h"
 #include "dpq.h"
 
-/* This is called from	qbeta(.) in a root-finding loop --- be FAST! */
-
 attribute_hidden
 double pbeta_raw(double x, double pin, double qin, int lower_tail)
 {
-    double ans, c, finsum, p, ps, p1, q, term, xb, xi, y;
-    int n, i, ib, swap_tail;
-
-    const static double eps = .5*DBL_EPSILON;
-    const static double sml = DBL_MIN;
-    const double lneps = log(eps);
-    const double lnsml = log(sml);
-
-    /* Switch to TOMS 708 if p or q is large */
-    if (pin > 15 || qin > 15) {
-	double x1 = 1 - x, w, wc;
-	int ierr;
-	bratio(pin, qin, x, x1, &w, &wc, &ierr);
-	if(ierr)
-	    MATHLIB_WARNING(_("pbeta_raw() -> bratio() gave error code %d"),
-			    ierr);
-	return lower_tail ? w : wc;
-    }
-
-    /* swap tails if x is greater than the mean */
-    if (pin / (pin + qin) < x) {
-	swap_tail = 1;
-	y = 1 - x;
-	p = qin;
-	q = pin;
-    }
-    else {
-	swap_tail = 0;
-	y = x;
-	p = pin;
-	q = qin;
-    }
-
-    if ((p + q) * y / (p + 1) < eps) {
-
-	/* tail approximation */
-
-	xb = p * log(fmax2(y, sml)) - log(p) - lbeta(p, q);
-	if (xb > lnsml && y != 0) {
-	    ans = (swap_tail == lower_tail) ? -expm1(xb) : exp(xb);
-	} else {
-	    ans = (swap_tail == lower_tail) ? 1. : 0;
-	}
-    } else {
-	/* evaluate the infinite sum first.  term will equal */
-	/* y^p / beta(ps, p) * (1 - ps)-sub-i * y^i / fac(i) */
-
-	/* Ly := log(y) */
-	double Ly = swap_tail ? log1p(-x) : log(y);
-
-	ps = q - floor(q);
-	xb = p * Ly;
-	if (ps == 0)
-	    ps = 1; /*==> lbeta(ps,p)= log Beta(1,p) = log(1/p) = -log(p) */
-	else
-	    xb -= (lbeta(ps, p) + log(p));
-	ans = 0;
-	if (xb >= lnsml) {
-	    ans = exp(xb);
-	    term = ans * p;
-	    if (ps != 1) {
-		n = fmax2(lneps/Ly, 4.0);
-		for(i=1 ; i <= n ; i++) {
-		    xi = i;
-		    term *= (xi - ps) * y / xi;
-		    ans += term / (p + xi);
-		}
-	    }
-	}
-
-	/* now evaluate the finite sum, maybe. */
-
-	if (q > 1) {
-
-	    double liy;/* == log(1-y) */
-	    if(swap_tail) {
-		c = 1./x;/* == 1/(1 - y) */
-		liy = log(x);
-	    }
-	    else {
-		c = 1./(1. - y);
-		liy = log1p(-y);
-	    }
-	    xb = p * Ly + q * liy - lbeta(p, q) - log(q);
-	    ib = fmax2(xb / lnsml, 0.0);
-	    term = exp(xb - ib * lnsml);
-	    p1 = q * c / (p + q - 1);
-
-	    finsum = 0;
-	    n = q;
-	    if (q == n)
-		n--;
-	    for(i= 1; i <= n; i++) {
-		if (p1 <= 1 && term / eps <= finsum)
-		    break;
-		xi = i;
-		term = (q - xi + 1) * c * term / (p + q - xi);
-		if (term > 1) {
-		    ib--;
-		    term *= sml;
-		}
-		if (ib == 0)
-		    finsum += term;
-	    }
-	    ans += finsum;
-	}
-	if (swap_tail == lower_tail)
-	    ans = 1 - ans;
-	ans = fmax2(fmin2(ans, 1.), 0.);
-    }
-    return ans;
+    double x1 = 1 - x, w, wc;
+    int ierr;
+    bratio(pin, qin, x, x1, &w, &wc, &ierr);
+    if(ierr)
+	MATHLIB_WARNING(_("pbeta_raw() -> bratio() gave error code %d"), ierr);
+    return lower_tail ? w : wc;
 } /* pbeta_raw() */
 
 double pbeta(double x, double pin, double qin, int lower_tail, int log_p)
 {
 #ifdef IEEE_754
-    if (ISNAN(x) || ISNAN(pin) || ISNAN(qin))
-	return x + pin + qin;
+    if (ISNAN(x) || ISNAN(pin) || ISNAN(qin)) return x + pin + qin;
 #endif
 
     if (pin <= 0 || qin <= 0) ML_ERR_return_NAN;
