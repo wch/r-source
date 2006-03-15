259d8074ae1edce221f9bbdf36f4c0f847279c5d
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2006-03-15 17:23:23 +0000

    confint(*, level = 0.999) should not say "100%"
    git-svn-id: https://svn.r-project.org/R/trunk@37544 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/R/confint.R
+++ src/library/stats/R/confint.R
@@ -1,16 +1,24 @@
 confint <- function(object, parm, level = 0.95, ...) UseMethod("confint")
 
+format.perc <- function(probs, digits)
+    ## Not yet exported, maybe useful in other contexts:
+    ## quantile.default() sometimes uses a version of it
+    paste(format(100 * probs, trim = TRUE, scientific = FALSE, digits = digits),
+	  "%")
+
 confint.lm <- function(object, parm, level = 0.95, ...)
 {
     cf <- coef(object)
     pnames <- names(cf)
-    if(missing(parm)) parm <- seq(along=pnames)
-    else if(is.character(parm))  parm <- match(parm, pnames, nomatch = 0)
+    if(missing(parm))
+	parm <- seq(along=pnames)
+    else if(is.character(parm))
+	parm <- match(parm, pnames, nomatch = 0)
     a <- (1 - level)/2
     a <- c(a, 1 - a)
-    pct <- paste(round(100*a, 1), "%")
+    pct <- format.perc(a, 3)
     ci <- array(NA, dim = c(length(parm), 2),
-                dimnames = list(pnames[parm], pct))
+		dimnames = list(pnames[parm], pct))
     ses <- sqrt(diag(vcov(object)))[parm]
     fac <- qt(a, object$df.residual)
     ci[] <- cf[parm] + ses %o% fac
@@ -28,15 +36,15 @@ confint.default <- function (object, parm, level = 0.95, ...)
     cf <- coef(object)
     pnames <- names(cf)
     if (missing(parm))
-        parm <- seq(along = pnames)
+	parm <- seq(along = pnames)
     else if (is.character(parm))
-        parm <- match(parm, pnames, nomatch = 0)
+	parm <- match(parm, pnames, nomatch = 0)
     a <- (1 - level)/2
     a <- c(a, 1 - a)
-    pct <- paste(round(100*a, 1), "%")
+    pct <- format.perc(a, 3)
     fac <- qnorm(a)
     ci <- array(NA, dim = c(length(parm), 2),
-                dimnames = list(pnames[parm], pct))
+		dimnames = list(pnames[parm], pct))
     ses <- sqrt(diag(vcov(object)))[parm]
     ci[] <- cf[parm] + ses %o% fac
     ci
