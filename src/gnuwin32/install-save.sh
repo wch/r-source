be3dfc5e1589b4e9e3069caaa24dc6a798d7d8fa
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2006-04-10 15:03:23 +0000

    make defunct install.R and R_PROFILE.R
    git-svn-id: https://svn.r-project.org/R/trunk@37712 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/install-save.sh
+++ src/gnuwin32/install-save.sh
@@ -6,11 +6,14 @@ BUILD=$5
 RX_EXE=$6
 DPKG=$7
 
-case $1 in
-    CHECK|'') if test -r install.R; then R_SAVE_IMAGE=true; else R_SAVE_IMAGE=false; fi;;
-    *) R_SAVE_IMAGE=$1;;
-esac
+R_SAVE_IMAGE=$1
 export R_SAVE_IMAGE
+if test -r install.R; then 
+    echo "WARNING: use of install.R is no longer supported"
+fi
+if test -r R_PROFILE.R; then 
+    echo "WARNING: use of R_PROFILE.R is no longer supported"
+fi
 
 ## R_START_PACKAGES is set to NULL during bootstrapping
 R_DEFAULT_PACKAGES=${R_START_PACKAGES}
@@ -51,7 +54,6 @@ if ${R_SAVE_IMAGE}; then
         R_SAVE_EXE="--save"
     fi
     (echo "options(save.image.defaults=${save_image_defaults})"; \
-      if test -s R_PROFILE.R; then cat R_PROFILE.R; fi; \
       echo "invisible(.libPaths(c(.Library,\"${lib}\",.libPaths()))); .getRequiredPackages()"; \
       ${code_cmd}) | ${R_EXE} ${R_SAVE_EXE}
     if test ${?} -ne 0; then
@@ -68,10 +70,4 @@ if ${R_SAVE_IMAGE}; then
     rm "${code_file}"
     # mv "${code_file}" "${code_file}.R"
     cat "${R_HOME}/share/R/${loader_file}" > "${code_file}"
-    ## if install.R is non-empty, arrange to evaluate the R code it
-    ## contains after the package source (maybe for some kind of
-    ## cleanup).
-    if test -s install.R; then
-      cat install.R >> ${lib}/${pkg}/R/${pkg}
-    fi
 fi
