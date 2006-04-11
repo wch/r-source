da48fee32d897ce04baef11b63bac4eca6bc529b
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2006-04-11 13:43:10 +0000

    enhance history, fix PR#8761
    git-svn-id: https://svn.r-project.org/R/trunk@37723 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/utils/R/history.R
+++ src/library/utils/R/history.R
@@ -1,28 +1,32 @@
-loadhistory <- function(file=".Rhistory")
+loadhistory <- function(file = ".Rhistory")
     invisible(.Internal(loadhistory(file)))
 
-savehistory <- function(file=".Rhistory")
+savehistory <- function(file = ".Rhistory")
     invisible(.Internal(savehistory(file)))
 
-history <- function(max.show=25, reverse=FALSE)
+history <- function(max.show = 25, reverse = FALSE, pattern, ...)
 {
     file1 <- tempfile("Rrawhist")
     savehistory(file1)
-    rawhist <- scan(file1, what = "", quiet=TRUE, sep="\n")
+    rawhist <- readLines(file1)
     unlink(file1)
+    if(!missing(pattern))
+        rawhist <- unique(grep(pattern, rawhist, value = TRUE, ...))
     nlines <- length(rawhist)
-    inds <- max(1, nlines-max.show):nlines
-    if(reverse) inds <- rev(inds)
+    if(nlines) {
+        inds <- max(1, nlines-max.show):nlines
+        if(reverse) inds <- rev(inds)
+    } else inds <- integer(0)
     file2 <- tempfile("hist")
-    write(rawhist[inds], file2)
-    file.show(file2, title="R History", delete.file=TRUE)
+    writeLines(rawhist[inds], file2)
+    file.show(file2, title = "R History", delete.file = TRUE)
 }
 
-timestamp <- function(stamp=date(), prefix="##------ ", suffix=" ------##",
-		      quiet=FALSE)
+timestamp <- function(stamp = date(), prefix = "##------ ",
+                      suffix = " ------##", quiet = FALSE)
 {
-    stamp <- paste(prefix, stamp, suffix, sep="")
+    stamp <- paste(prefix, stamp, suffix, sep = "")
     .Internal(addhistory(stamp))
-    if (!quiet) cat(stamp, sep="\n")
+    if (!quiet) cat(stamp, sep = "\n")
     invisible(stamp)
 }
