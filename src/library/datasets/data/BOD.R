8051e3f3ca5250609930409fd6463629cadf58c5
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2006-04-17 07:29:10 +0000

    use data.frame or at least not encode character row.names
    git-svn-id: https://svn.r-project.org/R/trunk@37798 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/datasets/data/BOD.R
+++ src/library/datasets/data/BOD.R
@@ -4,6 +4,6 @@
   structure(list(Time = c(1, 2, 3, 4, 5, 7),
                  demand = c(8.3, 10.3, 19, 16, 15.6, 19.8)),
             .Names = c("Time", "demand"),
-            row.names = c("1", "2", "3", "4", "5", "6"),
+            row.names = c(NA, 6),
             class = "data.frame",
             reference = "A1.4, p. 270")
