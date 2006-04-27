61df4cb869ec38e54e3ed5d719a321bde4b27ac0
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2006-04-27 11:58:00 +0000

    remove Windows special-casing
    git-svn-id: https://svn.r-project.org/R/trunk@37931 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/modules/internet/sock.h
+++ src/modules/internet/sock.h
@@ -16,16 +16,15 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
+ *  MA 02110-1301  USA
  */
 
 /* POSIX says ssize_t is defined in unistd.h, but apparently on 
-  RH9 it is not.  Dominick Samperi, R-devel, 2006-04-27 */
+   RH9 it is not.  (Dominick Samperi, R-devel, 2006-04-27)
+   So include sys/types.h and remove Win32 special casing.
+*/
 #include <sys/types.h>
-
-#if defined Win32 && !defined _SSIZE_T_
-typedef int ssize_t;
-#endif
 
 typedef unsigned short Sock_port_t;
 
