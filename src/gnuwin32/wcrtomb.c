091b60b6f4b6029dc8c7b0d5723887da79dc1935
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2006-05-17 08:45:13 +0000

    port r38095 from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-2-3-patches@38096 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/wcrtomb.c
+++ src/gnuwin32/wcrtomb.c
@@ -27,34 +27,33 @@ unsigned int get_cp_from_locale (void)
 static int
 __wcrtomb_cp (char *dst, wchar_t wc, const unsigned int cp,
 	      const unsigned int mb_max)
-{       
-  if (cp == 0)
-    {
-      *dst = (char) wc;
-      return 1;
-    }
-  else
-    {
-      int invalid_char = 0;
-   
-      int size = WideCharToMultiByte(get_cp_from_locale(),
-				     0 /* Is this correct flag? */,
-				     &wc, 1, dst, mb_max,
-				     NULL, &invalid_char);
-      if (size == 0 || invalid_char)  
-        {
-          errno = EILSEQ;
-          return -1;
+{
+    if (cp == 0) { /* C locale */
+	if (wc > 255) {
+	    errno = EILSEQ;
+	    return -1;
+	}
+	*dst = (char) wc;
+	return 1;
+    } else {
+	int invalid_char = 0;
+
+	int size = WideCharToMultiByte(cp, 0 /* Is this correct flag? */,
+				       &wc, 1, dst, mb_max,
+				       NULL, &invalid_char);
+	if (size == 0 || invalid_char) {
+	    errno = EILSEQ;
+	    return -1;
         }
-      return size;
+	return size;
     }
 }
 
 size_t
 wcrtomb (char *dst, wchar_t wc, mbstate_t *ps)
 {
-  char byte_bucket [MB_LEN_MAX];
-  char* tmp_dst = dst ? dst : byte_bucket;      
-  return (size_t)__wcrtomb_cp (tmp_dst, wc, get_cp_from_locale (),
-			       MB_CUR_MAX);
+    char byte_bucket [MB_LEN_MAX];
+    char* tmp_dst = dst ? dst : byte_bucket;
+    return (size_t)__wcrtomb_cp (tmp_dst, wc, get_cp_from_locale (),
+				 MB_CUR_MAX);
 }
