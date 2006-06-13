6974e3150820da6d4ed2297cb174128b43ae02de
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2006-06-13 20:44:22 +0000

    port r38346 (except regression tests) from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-2-3-patches@38347 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/nmath/qunif.c
+++ src/nmath/qunif.c
@@ -1,7 +1,7 @@
 /*
  *  Mathlib : A C Library of Special Functions
  *  Copyright (C) 1998 Ross Ihaka
- *  Copyright (C) 2000 The R Development Core Team
+ *  Copyright (C) 2000-2006 The R Development Core Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -32,7 +32,9 @@ double qunif(double p, double a, double b, int lower_tail, int log_p)
 	return p + a + b;
 #endif
     R_Q_P01_check(p);
-    if (b <= a ) ML_ERR_return_NAN;
+    if (!R_FINITE(a) || !R_FINITE(b)) ML_ERR_return_NAN;
+    if (b < a) ML_ERR_return_NAN;
+    if (b == a) return a;
 
     return a + R_DT_qIv(p) * (b - a);
 }
