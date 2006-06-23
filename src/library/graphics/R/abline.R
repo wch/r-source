c212d0164aac1d40d3910aa763f3dce979b1d600
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2006-06-23 11:09:00 +0000

    eliminate \synopsis
    git-svn-id: https://svn.r-project.org/R/trunk@38398 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/graphics/R/abline.R
+++ src/library/graphics/R/abline.R
@@ -1,15 +1,18 @@
-abline <-
-    function(a=NULL, b=NULL, h=NULL, v=NULL, reg=NULL, coef=NULL,
-	     untf=FALSE, col=par("col"), lty=par("lty"), lwd=par("lwd"), ...)
+abline <- function(a = NULL, b = NULL, h = NULL, v = NULL, reg = NULL,
+                   coef = NULL, untf = FALSE, ...)
 {
+    int_abline <- function(a, b, h, v, untf, col = par("col"),
+                           lty = par("lty"), lwd = par("lwd"), ...)
+        .Internal(abline(a, b, h, v, untf, col, lty, lwd, ...))
+
     if(!is.null(reg)) a <- reg
+    ## this should probably be is.object(a) || is.list(a)
     if(!is.null(a) && is.list(a)) {
-	temp <- as.vector(coefficients(a))
+	temp <- as.vector(coef(a))
 	if(length(temp) == 1) {
 	    a <- 0
 	    b <- temp
-	}
-	else {
+	} else {
 	    a <- temp[1]
 	    b <- temp[2]
 	}
@@ -18,6 +21,6 @@ abline <-
 	a <- coef[1]
 	b <- coef[2]
     }
-    .Internal(abline(a, b, h, v, untf, col, lty, lwd, ...))
+    int_abline(a=a, b=b, h=h, v=v, untf=untf, ...)
     invisible()
 }
