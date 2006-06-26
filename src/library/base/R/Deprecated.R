ce9126d4cd158642f9280819936342207089e3a7
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2006-06-26 12:05:02 +0000

    cosmetic simplification
    git-svn-id: https://svn.r-project.org/R/trunk@38415 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/Deprecated.R
+++ src/library/base/R/Deprecated.R
@@ -1,14 +1,15 @@
 ###----- NOTE:	../man/base-deprecated.Rd   must be synchronized with this file!
 ###		-------------------------
-.Deprecated <- function(new, package=NULL) {
+.Deprecated <- function(new, package = NULL) {
     msg <- gettextf("'%s' is deprecated.\n",
-                    as.character(sys.call(sys.parent())[[1]]))
+		    as.character(sys.call(sys.parent())[[1]]))
     if(!missing(new))
-        msg <- c(msg, gettextf("Use '%s' instead.\n", new))
-    if(!is.null(package))
-        msg <- c(msg,
-                 gettextf("See help(\"Deprecated\") and help(\"%s-deprecated\").", package))
-    else msg <- c(msg, gettext("See help(\"Deprecated\")"))
+	msg <- c(msg, gettextf("Use '%s' instead.\n", new))
+    msg <- c(msg,
+	     if(!is.null(package))
+	     gettextf("See help(\"Deprecated\") and help(\"%s-deprecated\").",
+		      package)
+	     else gettext("See help(\"Deprecated\")"))
     warning(paste(msg, collapse=""), call. = FALSE, domain = NA)
 }
 
