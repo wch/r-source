7c4eb61e856b252e33205c9121f8264af642109d
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2006-06-26 12:14:01 +0000

    update copyright date
    git-svn-id: https://svn.r-project.org/R/trunk@38418 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/optim.c
+++ src/main/optim.c
@@ -1,6 +1,6 @@
 /*
  *  R : A Computer Language for Statistical Data Analysis
- *  Copyright (C) 1999-2004  the R Development Core Team
+ *  Copyright (C) 1999-2006  the R Development Core Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
