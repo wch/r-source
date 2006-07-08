993ace03bd540ad3231a435ca42d59b08ed7d7c5
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2006-07-08 07:59:20 +0000

    some optimizations over REAL()
    git-svn-id: https://svn.r-project.org/R/trunk@38514 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/src/family.c
+++ src/library/stats/src/family.c
@@ -23,7 +23,7 @@ static R_INLINE double x_d_omx(double x) {
 
 /** 
  * Evaluate x/(1 + x). An inline function is used so that x is
- * evaluated once only. 
+ * evaluated once only. [but inlining is optional!]
  * 
  * @param x input
  * 
@@ -35,11 +35,12 @@ SEXP logit_link(SEXP mu)
 {
     int i, n = LENGTH(mu);
     SEXP ans = PROTECT(duplicate(mu));
+    double *rans = REAL(ans), *rmu=REAL(mu);
 
     if (!n || !isReal(mu))
 	error(_("Argument %s must be a nonempty numeric vector"), "mu");
     for (i = 0; i < n; i++)
-	REAL(ans)[i] = log(x_d_omx(REAL(mu)[i]));
+	rans[i] = log(x_d_omx(rmu[i]));
     UNPROTECT(1);
     return ans;
 }
@@ -48,14 +49,15 @@ SEXP logit_linkinv(SEXP eta)
 {
     SEXP ans = PROTECT(duplicate(eta));
     int i, n = LENGTH(eta);
+    double *rans = REAL(ans), *reta = REAL(eta);
 
     if (!n || !isReal(eta))
 	error(_("Argument %s must be a nonempty numeric vector"), "eta");
     for (i = 0; i < n; i++) {
-	double etai = REAL(eta)[i];
-
-	REAL(ans)[i] = x_d_opx((etai < MTHRESH) ? DOUBLE_EPS :
-			       ((etai > THRESH) ? INVEPS : exp(etai)));
+	double etai = reta[i], tmp;
+	tmp = (etai < MTHRESH) ? DOUBLE_EPS :
+	    ((etai > THRESH) ? INVEPS : exp(etai));
+	rans[i] = x_d_opx(tmp);
     }
     UNPROTECT(1);
     return ans;
@@ -65,14 +67,15 @@ SEXP logit_mu_eta(SEXP eta)
 {
     SEXP ans = PROTECT(duplicate(eta));
     int i, n = LENGTH(eta);
+    double *rans = REAL(ans), *reta = REAL(eta);
 
     if (!n || !isReal(eta))
 	error(_("Argument %s must be a nonempty numeric vector"), "eta");
     for (i = 0; i < n; i++) {
-	double etai = REAL(eta)[i];
+	double etai = reta[i];
 	double opexp = 1 + exp(etai);
 
-	REAL(ans)[i] = (etai > THRESH || etai < MTHRESH) ? DOUBLE_EPS :
+	rans[i] = (etai > THRESH || etai < MTHRESH) ? DOUBLE_EPS :
 	    exp(etai)/(opexp * opexp);
     }
     UNPROTECT(1);
@@ -89,12 +92,16 @@ SEXP binomial_dev_resids(SEXP y, SEXP mu, SEXP wt)
 {
     int i, n = LENGTH(y), lmu = LENGTH(mu), lwt = LENGTH(wt), nprot = 1;
     SEXP ans;
-    double mui, yi;
+    double mui, yi, *rmu, *ry, *rwt, *rans;
 
     if (!isReal(y)) {y = PROTECT(coerceVector(y, REALSXP)); nprot++;}
+    ry = REAL(y);
     ans = PROTECT(duplicate(y));
+    rans = REAL(ans);
     if (!isReal(mu)) {mu = PROTECT(coerceVector(mu, REALSXP)); nprot++;}
     if (!isReal(wt)) {wt = PROTECT(coerceVector(wt, REALSXP)); nprot++;}
+    rmu = REAL(mu);
+    rwt = REAL(wt);
     if (lmu != n && lmu != 1)
 	error(_("argument %s must be a numeric vector of length 1 or length %d"),
 	      "mu", n);
@@ -104,16 +111,16 @@ SEXP binomial_dev_resids(SEXP y, SEXP mu, SEXP wt)
     /* Written separately to avoid an optimization bug on Solaris cc */
     if(lmu > 1) {
 	for (i = 0; i < n; i++) {
-	    mui = REAL(mu)[i];
-	    yi = REAL(y)[i];
-	    REAL(ans)[i] = 2 * REAL(wt)[lwt > 1 ? i : 0] *
+	    mui = rmu[i];
+	    yi = ry[i];
+	    rans[i] = 2 * rwt[lwt > 1 ? i : 0] *
 		(y_log_y(yi, mui) + y_log_y(1 - yi, 1 - mui));
 	}
     } else {
-	mui = REAL(mu)[0];
+	mui = rmu[0];
 	for (i = 0; i < n; i++) {
-	    yi = REAL(y)[i];
-	    REAL(ans)[i] = 2 * REAL(wt)[lwt > 1 ? i : 0] *
+	    yi = ry[i];
+	    rans[i] = 2 * rwt[lwt > 1 ? i : 0] *
 		(y_log_y(yi, mui) + y_log_y(1 - yi, 1 - mui));
 	}
     }
