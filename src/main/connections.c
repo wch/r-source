2f7b93fb7f62e4609884769fd088f6cf1b9e9ef1
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2006-07-16 15:19:33 +0000

    unlock binding in the right place
    git-svn-id: https://svn.r-project.org/R/trunk@38625 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/connections.c
+++ src/main/connections.c
@@ -1842,6 +1842,7 @@ static void outtext_close(Rconnection con)
     SEXP tmp;
     int idx = ConnIndex(con);
 
+    R_unLockBinding(this->namesymbol, VECTOR_ELT(OutTextData, idx));
     if(strlen(this->lastline) > 0) {
 	PROTECT(tmp = lengthgets(this->data, ++this->len));
 	SET_STRING_ELT(tmp, this->len - 1, mkChar(this->lastline));
@@ -1850,7 +1851,6 @@ static void outtext_close(Rconnection con)
 	this->data = tmp;
 	UNPROTECT(1);
     }
-    R_unLockBinding(this->namesymbol, VECTOR_ELT(OutTextData, idx));
     /* moved to _destroy 
     SET_VECTOR_ELT(OutTextData, idx, R_NilValue);
     */
