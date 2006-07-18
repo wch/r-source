c9b8d7dcda3979684eda86c5426ec313780f4f1b
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2006-07-18 09:32:52 +0000

    small optimization
    git-svn-id: https://svn.r-project.org/R/trunk@38634 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/unlist.R
+++ src/library/base/R/unlist.R
@@ -1,6 +1,7 @@
 unlist <- function(x, recursive=TRUE, use.names=TRUE)
 {
-    if(length(x) > 0 && all(lapply(x, is.factor))) { # so will not be recursing
+    if(length(x) > 0 && is.factor(x[[1]]) && all(lapply(x, is.factor))) {
+        ## so will not be recursing
         lv <- unique(.Internal(unlist(lapply(x, levels), FALSE, FALSE)))
         nm <- if(use.names) names(.Internal(unlist(x, FALSE, use.names)))
         res <- .Internal(unlist(lapply(x, as.character), FALSE, FALSE))
