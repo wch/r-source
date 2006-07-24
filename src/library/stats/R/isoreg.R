32740e049d4fe1f49a62f4979ce740990c0331ac
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2006-07-24 08:18:13 +0000

    added lines.isoreg()
    git-svn-id: https://svn.r-project.org/R/trunk@38691 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/R/isoreg.R
+++ src/library/stats/R/isoreg.R
@@ -32,6 +32,16 @@ print.isoreg <- function(x, digits = getOption("digits"), ...) {
   invisible(x)
 }
 
+lines.isoreg <- function(x, col = "red", lwd = 1.5,
+			 do.points = FALSE, cex = 1.5, pch = 13, ...)
+{
+    xx <- if(x$isOrd) x$x else x$x[x$ord]
+    lines (xx, x$yf, col = col, lwd = lwd, type = "S")
+    if(do.points)
+	points(xx[x$iKnots], x$yf[x$iKnots], col = col, cex = cex, pch = pch)
+    invisible()
+}
+
 plot.isoreg <-
     function(x, plot.type = c("single", "row.wise", "col.wise"),
 	     main = paste("Isotonic regression", deparse(x$call)),
