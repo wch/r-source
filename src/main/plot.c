3163b1f04ab80104af94565d53f73b7efa197c71
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2006-08-01 07:31:50 +0000

    use capitalized forms of (un)protect use PROTECT_WITH_INDEX in GetTextArg (although it looks unnecessary)
    git-svn-id: https://svn.r-project.org/R/trunk@38749 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/plot.c
+++ src/main/plot.c
@@ -452,19 +452,20 @@ GetTextArg(SEXP call, SEXP spec, SEXP *ptxt,
     int i, n, col, font, colspecd;
     double cex;
     SEXP txt, nms;
+    PROTECT_INDEX pi;
 
     txt	  = R_NilValue;
     cex	  = NA_REAL;
     col	  = R_TRANWHITE;
     colspecd = 0;
     font  = NA_INTEGER;
-    PROTECT(txt);
+    /* It doesn't look as if this protection is needed */
+    PROTECT_WITH_INDEX(txt, &pi);
 
     switch (TYPEOF(spec)) {
     case LANGSXP:
     case SYMSXP:
-	UNPROTECT(1);
-	PROTECT(txt = coerceVector(spec, EXPRSXP));
+	REPROTECT(txt = coerceVector(spec, EXPRSXP), pi);
 	break;
     case VECSXP:
 	if (length(spec) == 0) {
@@ -472,16 +473,12 @@ GetTextArg(SEXP call, SEXP spec, SEXP *ptxt,
 	}
 	else {
 	    nms = getAttrib(spec, R_NamesSymbol);
-	    if (nms==R_NilValue){ /* PR#1939 */
+	    if (nms == R_NilValue){ /* PR#1939 */
 	       txt = VECTOR_ELT(spec, 0);
-	       if (TYPEOF(txt) == LANGSXP ||TYPEOF(txt) == SYMSXP ) {
-		    UNPROTECT(1);
-		    PROTECT(txt = coerceVector(txt, EXPRSXP));
-	       }
-	       else if (!isExpression(txt)) {
-		    UNPROTECT(1);
-		    PROTECT(txt = coerceVector(txt, STRSXP));
-	       }
+	       if (TYPEOF(txt) == LANGSXP || TYPEOF(txt) == SYMSXP )
+		    REPROTECT(txt = coerceVector(txt, EXPRSXP), pi);
+	       else if (!isExpression(txt))
+		    REPROTECT(txt = coerceVector(txt, STRSXP), pi);
 	    } else {
 	       n = length(nms);
 	       for (i = 0; i < n; i++) {
@@ -500,14 +497,10 @@ GetTextArg(SEXP call, SEXP spec, SEXP *ptxt,
 		}
 		else if (!strcmp(CHAR(STRING_ELT(nms, i)), "")) {
 		    txt = VECTOR_ELT(spec, i);
-		    if (TYPEOF(txt) == LANGSXP || TYPEOF(txt)==SYMSXP) {
-			UNPROTECT(1);
-			PROTECT(txt = coerceVector(txt, EXPRSXP));
-		    }
-		    else if (!isExpression(txt)) {
-			UNPROTECT(1);
-			PROTECT(txt = coerceVector(txt, STRSXP));
-		    }
+		    if (TYPEOF(txt) == LANGSXP || TYPEOF(txt) == SYMSXP)
+			REPROTECT(txt = coerceVector(txt, EXPRSXP), pi);
+		    else if (!isExpression(txt))
+			REPROTECT(txt = coerceVector(txt, STRSXP), pi);
 		}
 		else errorcall(call, _("invalid graphics parameter"));
 	       }
@@ -519,7 +512,7 @@ GetTextArg(SEXP call, SEXP spec, SEXP *ptxt,
 	txt = spec;
 	break;
     default:
-	txt = coerceVector(spec, STRSXP);
+	REPROTECT(txt = coerceVector(spec, STRSXP), pi);
 	break;
     }
     UNPROTECT(1);
@@ -2617,8 +2610,9 @@ SEXP attribute_hidden do_title(SEXP call, SEXP op, SEXP args, SEXP env)
 	cex = Rf_gpptr(dd)->cexmain;
 	col = Rf_gpptr(dd)->colmain;
 	font = Rf_gpptr(dd)->fontmain;
+	/* GetTextArg may coerce, so protect the result */
 	GetTextArg(call, Main, &Main, &col, &cex, &font);
-	protect(Main);	
+	PROTECT(Main);	
 	Rf_gpptr(dd)->col = col;
 	Rf_gpptr(dd)->cex = Rf_gpptr(dd)->cexbase * cex;
 	Rf_gpptr(dd)->font = font;
@@ -2660,14 +2654,15 @@ SEXP attribute_hidden do_title(SEXP call, SEXP op, SEXP args, SEXP env)
 			  adjy, 0.0, dd);
 	  }
 	}
-	unprotect(1);
+	UNPROTECT(1);
     }
     if (sub != R_NilValue) {
 	cex = Rf_gpptr(dd)->cexsub;
 	col = Rf_gpptr(dd)->colsub;
 	font = Rf_gpptr(dd)->fontsub;
+	/* GetTextArg may coerce, so protect the result */
 	GetTextArg(call, sub, &sub, &col, &cex, &font);
-	protect(sub);
+	PROTECT(sub);
 	Rf_gpptr(dd)->col = col;
 	Rf_gpptr(dd)->cex = Rf_gpptr(dd)->cexbase * cex;
 	Rf_gpptr(dd)->font = font;
@@ -2694,14 +2689,15 @@ SEXP attribute_hidden do_title(SEXP call, SEXP op, SEXP args, SEXP env)
 		    GMtext(CHAR(string), 1, vpos, where, hpos, 0, 0.0, dd);
 	    }
 	}
-	unprotect(1);
+	UNPROTECT(1);
     }
     if (xlab != R_NilValue) {
 	cex = Rf_gpptr(dd)->cexlab;
 	col = Rf_gpptr(dd)->collab;
 	font = Rf_gpptr(dd)->fontlab;
+	/* GetTextArg may coerce, so protect the result */
 	GetTextArg(call, xlab, &xlab, &col, &cex, &font);
-	protect(xlab);
+	PROTECT(xlab);
 	Rf_gpptr(dd)->cex = Rf_gpptr(dd)->cexbase * cex;
 	Rf_gpptr(dd)->col = col;
 	Rf_gpptr(dd)->font = font;
@@ -2728,14 +2724,15 @@ SEXP attribute_hidden do_title(SEXP call, SEXP op, SEXP args, SEXP env)
 		    GMtext(CHAR(string), 1, vpos + i, where, hpos, 0, 0.0, dd);
 	    }
 	}
-	unprotect(1);
+	UNPROTECT(1);
     }
     if (ylab != R_NilValue) {
 	cex = Rf_gpptr(dd)->cexlab;
 	col = Rf_gpptr(dd)->collab;
 	font = Rf_gpptr(dd)->fontlab;
+	/* GetTextArg may coerce, so protect the result */
 	GetTextArg(call, ylab, &ylab, &col, &cex, &font);
-	protect(ylab);
+	PROTECT(ylab);
 	Rf_gpptr(dd)->cex = Rf_gpptr(dd)->cexbase * cex;
 	Rf_gpptr(dd)->col = col;
 	Rf_gpptr(dd)->font = font;
@@ -2762,7 +2759,7 @@ SEXP attribute_hidden do_title(SEXP call, SEXP op, SEXP args, SEXP env)
 		    GMtext(CHAR(string), 2, vpos - i, where, hpos, 0, 0.0, dd);
 	    }
 	}
-	unprotect(1);
+	UNPROTECT(1);
     }
     GMode(0, dd);
     GRestorePars(dd);
