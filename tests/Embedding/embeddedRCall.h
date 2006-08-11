d37268d5ebbf32c58a45a1e5af3c53209ec2c929
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2006-08-11 17:04:42 +0000

    aargh, only tested on Windows
    git-svn-id: https://svn.r-project.org/R/trunk@38843 00db46b3-68df-0310-9c12-caf00c1e9a41

--- tests/Embedding/embeddedRCall.h
+++ tests/Embedding/embeddedRCall.h
@@ -3,7 +3,7 @@
 
 #include <R.h>
 #include <Rinternals.h>
-#include <RDevices.h> /* for KillAllDevices */
+#include <Rdevices.h> /* for KillAllDevices */
 
 int eval_R_command(const char *funcName, int argc, char *argv[]);
 void init_R(int argc, char **argv);
