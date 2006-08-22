718347f6d8631013a4d8411cfdd0bb7c5f4800c9
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2006-08-22 08:15:17 +0000

    preserve object bit when class would be preserved.
    git-svn-id: https://svn.r-project.org/R/trunk@38951 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/character.c
+++ src/main/character.c
@@ -240,6 +240,8 @@ SEXP attribute_hidden do_substr(SEXP call, SEXP op, SEXP args, SEXP env)
 	Free(buff);
     }
     SET_ATTRIB(s, duplicate(ATTRIB(x)));
+    /* This copied the class, if any */
+    SET_OBJECT(s, OBJECT(x));
     UNPROTECT(1);
     return s;
 }
@@ -722,6 +724,8 @@ SEXP attribute_hidden do_abbrev(SEXP call, SEXP op, SEXP args, SEXP env)
     }
     if(warn) warningcall(call, _("abbreviate used with non-ASCII chars"));
     SET_ATTRIB(ans, duplicate(ATTRIB(x)));
+    /* This copied the class, if any */
+    SET_OBJECT(ans, OBJECT(x));
     UNPROTECT(1);
     return(ans);
 }
@@ -1171,6 +1175,8 @@ SEXP attribute_hidden do_gsub(SEXP call, SEXP op, SEXP args, SEXP env)
     }
     if (!fixed_opt) regfree(&reg);
     SET_ATTRIB(ans, duplicate(ATTRIB(vec)));
+    /* This copied the class, if any */
+    SET_OBJECT(ans, OBJECT(vec));
     UNPROTECT(1);
     return ans;
 }
@@ -1588,6 +1594,8 @@ SEXP attribute_hidden do_tolower(SEXP call, SEXP op, SEXP args, SEXP env)
             }
         }
     SET_ATTRIB(y, duplicate(ATTRIB(x)));
+    /* This copied the class, if any */
+    SET_OBJECT(y, OBJECT(x));
     UNPROTECT(1);
     return(y);
 }
@@ -1914,6 +1922,8 @@ SEXP attribute_hidden do_chartr(SEXP call, SEXP op, SEXP args, SEXP env)
         }
 
     SET_ATTRIB(y, duplicate(ATTRIB(x)));
+    /* This copied the class, if any */
+    SET_OBJECT(y, OBJECT(x));
     UNPROTECT(1);
     return(y);
 }
@@ -2450,6 +2460,8 @@ SEXP attribute_hidden do_strtrim(SEXP call, SEXP op, SEXP args, SEXP env)
     }
     if(len > 0) DeallocBuffer(&cbuff);
     SET_ATTRIB(s, duplicate(ATTRIB(x)));
+    /* This copied the class, if any */
+    SET_OBJECT(s, OBJECT(x));
     UNPROTECT(2);
     return s;
 }
