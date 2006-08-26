eb098320751542f1ac6039b083217a12377243bf
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2006-08-26 08:11:47 +0000

    COPYRIGHTS says this is GPL, not LGPL
    git-svn-id: https://svn.r-project.org/R/trunk@38988 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/include/Rinlinedfuns.h
+++ src/include/Rinlinedfuns.h
@@ -4,8 +4,8 @@
  *  Copyright (C) 1999-2006   The R Development Core Team.
  *
  *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU Lesser General Public License as published by
- *  the Free Software Foundation; either version 2.1 of the License, or
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
  *  (at your option) any later version.
  *
  *  This program is distributed in the hope that it will be useful,
@@ -13,7 +13,7 @@
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU Lesser General Public License for more details.
  *
- *  You should have received a copy of the GNU Lesser General Public License
+ *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
  */
