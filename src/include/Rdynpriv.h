c87492e74bad96104394a8bde00ec7afc10dd519
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2006-08-26 08:09:12 +0000

    COPYRIGHTS says this is GPL, not LGPL
    git-svn-id: https://svn.r-project.org/R/trunk@38987 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/include/Rdynpriv.h
+++ src/include/Rdynpriv.h
@@ -3,8 +3,8 @@
  *  Copyright (C) 2001-6  The R Development Core Team.
  *
  *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU Lesser General Public License as published by
- *  the Free Software Foundation; either version 2.1 of the License, or
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
  *  (at your option) any later version.
  *
  *  This program is distributed in the hope that it will be useful,
