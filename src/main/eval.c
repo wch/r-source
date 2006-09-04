1c68393ad6ec50edf7cc00eac0044848a0b7fb93
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2006-09-04 12:44:16 +0000

    typo
    git-svn-id: https://svn.r-project.org/R/trunk@39094 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/eval.c
+++ src/main/eval.c
@@ -1444,13 +1444,13 @@ SEXP attribute_hidden evalList(SEXP el, SEXP rho, SEXP op)
 	    tail = CDR(tail);
 	    SET_TAG(tail, CreateTag(TAG(el)));
 	} else if (CDR(el) == R_NilValue) {
-	    warningcall_immediate(R_Nilvalue,
+	    warningcall_immediate(R_NilValue,
                "a final empty element has been omitted");
 	    PrintArgs(orig, op);
 	} else if(streql(PRIMNAME(op), "c")
 		  || streql(PRIMNAME(op), "list")) {
 	    /* temporarily special-case c() and list() */
-	    warningcall_immediate(R_Nilvalue,
+	    warningcall_immediate(R_NilValue,
 	        "an element is empty and has been omitted");
 	    PrintArgs(orig, op);
 	} else { /* It was a missing element */
