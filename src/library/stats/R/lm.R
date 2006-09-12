1b5a5fcbc37e8ce50c4987ca9098f217f10a86fb
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2006-09-12 10:58:38 +0000

    Ported r39269 from trunk.
    git-svn-id: https://svn.r-project.org/R/branches/R-2-4-branch@39271 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/R/lm.R
+++ src/library/stats/R/lm.R
@@ -426,12 +426,12 @@ residuals.lm <-
 
 simulate.lm <- function(object, nsim = 1, seed = NULL, ...)
 {
-    if(!exists(".Random.seed", envir = .GlobalEnv))
-        runif(1) # initialize the RNG if necessary
+    if(!exists(".Random.seed", envir = .GlobalEnv, inherits = FALSE))
+        runif(1)                       # initialize the RNG if necessary
     if(is.null(seed))
-        RNGstate <- .Random.seed
+        RNGstate <- get(".Random.seed", envir = .GlobalEnv)
     else {
-        R.seed <- .Random.seed
+        R.seed <- get(".Random.seed", envir = .GlobalEnv)
 	set.seed(seed)
         RNGstate <- structure(seed, kind = as.list(RNGkind()))
         on.exit(assign(".Random.seed", R.seed, envir = .GlobalEnv))
