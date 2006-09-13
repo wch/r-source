f4e4bf1bdbb77c8c2b67f0cc587ffaf5b7ef7c34
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2006-09-13 17:12:07 +0000

    Add arguments 'namespace' and 'code_files'. Only generate one top-level 'Read-and-delete-me' file. Make work for arbitrary (non-attached) environments.
    git-svn-id: https://svn.r-project.org/R/trunk@39299 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/utils/R/package.skeleton.R
+++ src/library/utils/R/package.skeleton.R
@@ -1,6 +1,7 @@
 package.skeleton <-
     function(name = "anRpackage", list, environment = .GlobalEnv,
-	     path = ".", force = FALSE)
+	     path = ".", force = FALSE, namespace = FALSE,
+             code_files = character())
 {
     safe.dir.create <- function(path)
     {
@@ -9,11 +10,24 @@ package.skeleton <-
 	    stop(gettextf("cannot create directory '%s'", path), domain = NA)
     }
 
-    if(missing(list))
-	list<-ls(env=environment)
+    if(!is.character(code_files))
+        stop("'code_files' should be a character vector")
+    use_code_files <- length(code_files) > 0
+
+    if(missing(list)) {
+        if(use_code_files) {
+            environment <- new.env()
+            for(cf in code_files)
+                sys.source(cf, envir = environment)
+        }
+        list <- ls(env = environment)
+    }
 
     if(!is.character(list))
 	stop("'list' should be a character vector naming R objects")
+
+    if(!is.logical(namespace) || (length(namespace) != 1))
+        stop("'namespace' should be a single logical")
 
     ## we need to test in the C locale
     curLocale <- Sys.getlocale("LC_CTYPE")
@@ -21,7 +35,7 @@ package.skeleton <-
     if(Sys.setlocale("LC_CTYPE", "C") != "C")
         warning("cannot turn off locale-specific chars via LC_CTYPE")
 
-    have <- sapply(list, exists)
+    have <- sapply(list, exists, envir = environment)
     if(any(!have))
 	warning(sprintf(ngettext(sum(!have),
 				 "object '%s' not found",
@@ -32,20 +46,20 @@ package.skeleton <-
     if(!length(list))
 	stop("no R objects specified or available")
 
-    cat("Creating directories ...\n")
+    message("Creating directories ...")
     ## Make the directories
-    if(file.exists(file.path(path,name)) && !force)
-	stop(gettextf("directory '%s' already exists", name), domain = NA)
+    dir <- file.path(path, name)    
+    if(file.exists(dir) && !force)
+	stop(gettextf("directory '%s' already exists", dir), domain = NA)
 
-    safe.dir.create(file.path(path, name))
-    safe.dir.create(file.path(path, name, "man"))
-    safe.dir.create(file.path(path, name, "src"))
-    safe.dir.create(file.path(path, name, "R"))
-    safe.dir.create(file.path(path, name, "data"))
+    safe.dir.create(dir)
+    safe.dir.create(code_dir <- file.path(dir, "R"))
+    safe.dir.create(docs_dir <- file.path(dir, "man"))
+    safe.dir.create(data_dir <- file.path(dir, "data"))
 
     ## DESCRIPTION
-    cat("Creating DESCRIPTION ...\n")
-    description <- file(file.path(path, name, "DESCRIPTION"), "wt")
+    message("Creating DESCRIPTION ...")
+    description <- file(file.path(dir, "DESCRIPTION"), "wt")
     cat("Package: ", name, "\n",
 	"Type: Package\n",
 	"Title: What the package does (short line)\n",
@@ -58,81 +72,96 @@ package.skeleton <-
 	file = description, sep = "")
     close(description)
 
-    ## READMEs
-    cat("Creating Read-and-delete-mes ...\n")
+    ## NAMESPACE
+    ## <NOTE>
+    ## For the time being, we export all non-internal objects we come up
+    ## with.  S3 methods, or S4 classes and methods are not handled.
+    ## </NOTE>
+    if(namespace) {
+        message("Creating NAMESPACE ...")
+        out <- file(file.path(dir, "NAMESPACE"), "wt")
+        writeLines("exportPattern(\"^[[:alpha:]]+\")", out)
+        close(out)
+    }
 
-    ## src/Read-and-delete-me
-    readme <- file(file.path(path, name, "src", "Read-and-delete-me"), "wt")
-    cat("Put C/C++/Fortran code here.",
-	"If you have compiled code, add a .First.lib() function",
-	"in the 'R' subdirectory to load it;",
-	"or if you use a NAMESPACE, an .OnLoad() function instead.\n",
-	file = readme, sep = "\n")
-    close(readme)
-
-    ## man/Read-and-delete-me
-    readme <- file(file.path(path, name, "man", "Read-and-delete-me"), "wt")
-    cat("Edit these help files.\n",
-	"You may want to combine the help files for multiple functions.\n",
-	file = readme, sep = "")
-    close(readme)
-
-    readme <- file(file.path(path, name, "Read-and-delete-me"), "wt")
-    cat("1. Put any C/C++/Fortran code in 'src'\n",
-	"2. If you have compiled code, add a .First.lib() function in 'R'\n",
-	"   to load the shared library\n",
-	"3. Edit the help file skeletons in 'man'\n",
-	"4. Run R CMD build to create the index files\n",
-	"5. Run R CMD check to check the package\n",
-	"6. Run R CMD build to make the package file\n",
-	"\n\nRead \"Writing R Extensions\" for more information.\n",
-	file = readme, sep = "")
-    close(readme)
+    ## Read-and-delete-me
+    message("Creating Read-and-delete-me ...")
+    out <- file(file.path(dir, "Read-and-delete-me"), "wt")
+    msg <-
+    c("* Edit the help file skeletons in 'man', possibly combining help files for multiple functions.",
+      if(namespace)
+      "* Edit the exports in 'NAMESPACE', and add necessary imports.",
+      "* Put any C/C++/Fortran code in 'src'.",
+      if(namespace)
+      "* If you have compiled code, add a useDynLib() directive to 'NAMESPACE'."
+      else
+      "* If you have compiled code, add a .First.lib() function in 'R' to load the shared library.",
+      "* Run R CMD build to build the package tarball.",
+      "* Run R CMD check to check the package tarball.",
+      "",
+      "Read \"Writing R Extensions\" for more information.")
+    writeLines(strwrap(msg, exdent = 2), out)
+    close(out)
 
     internalObjInds <- grep("^\\.", list)
     internalObjs <- list[internalObjInds]
     if(any(internalObjInds))
 	list <- list[-internalObjInds]
 
-    ## Some object names may not be valid file names, especially replacement
-    ## function names. And if we start changing them they may collide.
-    list0 <- gsub("[[:cntrl:]\"*/:<>?\\|]", "_", list)
-    wrong <- grep("^(con|prn|aux|clock\\$|nul|lpt[1-3]|com[1-4])(\\..*|)$",
-		  list0)
-    if(length(wrong)) list0[wrong] <- paste("zz", list0[wrong], sep="")
-    ok <- grep("^[[:alnum:]]", list0)
-    if(length(ok) < length(list0))
-        list0[-ok] <- paste("z", list0[-ok], sep="")
-    ## now on Windows lower/uppercase will collide too
-    list1 <- tolower(list0)
-    list2 <- make.unique(list1, sep="_")
-    changed <- (list2 != list1)
-    list0[changed] <- list2[changed]
+    if(!use_code_files) {
+        ## Some object names may not be valid file names, especially
+        ## replacement function names.  And if we start changing them
+        ## they may collide.
+        ## <NOTE>
+        ## If we use given code files, we could still check whether
+        ## these file are valid across platforms ...
+        ## </NOTE>
+        list0 <- gsub("[[:cntrl:]\"*/:<>?\\|]", "_", list)
+        wrong <- grep("^(con|prn|aux|clock\\$|nul|lpt[1-3]|com[1-4])(\\..*|)$",
+                      list0)
+        if(length(wrong))
+            list0[wrong] <- paste("zz", list0[wrong], sep="")
+        ok <- grep("^[[:alnum:]]", list0)
+        if(length(ok) < length(list0))
+            list0[-ok] <- paste("z", list0[-ok], sep="")
+        ## now on Windows lower/uppercase will collide too
+        list1 <- tolower(list0)
+        list2 <- make.unique(list1, sep="_")
+        changed <- (list2 != list1)
+        list0[changed] <- list2[changed]
+    } else {
+        list0 <- list
+    }
     names(list0) <- list
 
     ## Dump the items in 'data' or 'R'
-    cat("Saving functions and data ...\n")
-    if(any(internalObjInds))
-	dump(internalObjs,
-	     file = file.path(path, name, "R",
-			      paste(name, "-internal.R", sep = "")))
-    for(item in list){
-	if(is.function(get(item)))
-	    dump(item,
-		 file = file.path(path, name, "R",
-				  paste(list0[item], "R", sep = ".")))
-	else # we cannot guarantee this is a valid file name
-	    try(save(list = item,
-		     file = file.path(path, name, "data",
-				      paste(item, "rda", sep = "."))))
+    if(!use_code_files) {
+        message("Saving functions and data ...")
+        if(any(internalObjInds))
+            dump(internalObjs,
+                 file = file.path(code_dir,
+                                  sprintf("%s-internal.R", name)))
+        for(item in list){
+            if(is.function(get(item, envir = environment)))
+                dump(item,
+                     file = file.path(code_dir,
+                                      sprintf("%s.R", list0[item])))
+            else # we cannot guarantee this is a valid file name
+                try(save(list = item,
+                         file = file.path(data_dir,
+                                          sprintf("%s.rda", item))))
+        }
+    } else {
+        message("Copying code files ...")
+        file.copy(code_files, code_dir)
     }
 
     ## Make help file skeletons in 'man'
-    cat("Making help files ...\n")
+    message("Making help files ...")
     if(any(internalObjInds)) {
-	Rdfile <- file(file.path(path, name, "man",
-				 paste(name, "-internal.Rd", sep = "")),
-		       "wt")
+	Rdfile <- file(file.path(docs_dir,
+                                 sprintf("%s-internal.Rd", name)),
+                       "wt")
 	cat("\\name{", name, "-internal}\n",
 	    "\\title{Internal ", name, " objects}\n",
 	    file = Rdfile, sep = "")
@@ -145,34 +174,33 @@ package.skeleton <-
 	    file = Rdfile, sep = "")
 	close(Rdfile)
     }
-    ## Redirect output so that we do not see the partially inappropriate
-    ## messages from prompt().
-    outFile <- tempfile()
-    outConn <- file(outFile, "w")
-    sink(outConn, type = "output")
-    yy <- try({ promptPackage(name, filename = file.path(path, name, "man",
-                                                   paste(name, "package.Rd", sep="-")),
-                              lib.loc = path);
-                sapply(list,
-		     function(item) {
-			 prompt(name = item,
-				filename = file.path(path, name, "man",
-				paste(list0[item], "Rd", sep=".")))
-		     })})
-    sink(type = "output")
-    close(outConn)
-    unlink(outFile)
+    ## Suppress partially inappropriate messages from prompt().
+    yy <- try(suppressMessages({
+        promptPackage(name,
+                      filename =
+                      file.path(docs_dir,
+                                sprintf("%s-package.Rd", name)),
+                      lib.loc = path)
+        sapply(list,
+               function(item) {
+                   prompt(get(item, envir = environment),
+                          name = item,
+                          filename =
+                          file.path(docs_dir,
+                                    sprintf("%s.Rd", list0[item])))
+               })
+    }))
     if(inherits(yy, "try-error"))
 	stop(yy)
 
     ## Now we may have created an empty data or R directory
-    Rdir <- file.path(path, name, "R")
-    if(length(list.files(Rdir)) == 0) unlink(Rdir, recursive=TRUE)
-    datadir <- file.path(path, name, "data")
-    if(length(list.files(datadir)) == 0) unlink(datadir, recursive=TRUE)
+    if(length(list.files(code_dir)) == 0)
+        unlink(code_dir, recursive = TRUE)
+    if(length(list.files(data_dir)) == 0)
+        unlink(data_dir, recursive = TRUE)
 
-    cat("Done.\n")
-    cat(paste("Further steps are described in",
-	      file.path(path, name, "Read-and-delete-me"),
-	      "\n"))
+    message("Done.")
+    message(gettextf("Further steps are described in '%s'.",
+                     file.path(dir, "Read-and-delete-me")),
+            domain = NA)
 }
