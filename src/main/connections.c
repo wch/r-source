46d47b6f74653c73387c1d5fb870b1c327a83cd6
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2006-09-13 06:37:25 +0000

    typo
    git-svn-id: https://svn.r-project.org/R/branches/R-2-4-branch@39290 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/connections.c
+++ src/main/connections.c
@@ -1839,7 +1839,7 @@ static void outtext_close(Rconnection con)
     int idx = ConnIndex(con);
     SEXP tmp, env = VECTOR_ELT(OutTextData, idx);
 
-    if(!this->namesymbol &&
+    if(this->namesymbol &&
        findVarInFrame3(env, this->namesymbol, FALSE) != R_UnboundValue)
 	R_unLockBinding(this->namesymbol, env);
     if(strlen(this->lastline) > 0) {
