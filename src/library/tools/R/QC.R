feb742e71571e979f6a82c6889a8c1605380ef3c
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2006-09-29 22:59:49 +0000

    Handle standard evaluation in library()/require().
    git-svn-id: https://svn.r-project.org/R/trunk@39551 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/QC.R
+++ src/library/tools/R/QC.R
@@ -3734,13 +3734,29 @@ function(package, dir, lib.loc = NULL)
         if(is.call(e) || is.expression(e)) {
             Call <- deparse(e[[1]])[1]
             if(length(e) >= 2) pkg <- deparse(e[[2]])
-            if(Call %in% c("library", "requires")) {
+            if(Call %in% c("library", "require")) {
                 ## Zelig has library()
                 if(length(e) >= 2) {
                     pkg <- sub('^"(.*)"$', '\\1', pkg)
+                    ## <NOTE>
+                    ## Using code analysis, we really don't know which
+                    ## package was called if character.only = TRUE and
+                    ## the package argument is not a string constant.
+                    ## (Btw, what if character.only is given a value
+                    ## which is an expression evaluating to TRUE?)
+                    dunno <- FALSE
+                    pos <- which(!is.na(pmatch(names(e),
+                                               "character.only")))
+                    if(length(pos)
+                       && identical(e[[pos]], TRUE)
+                       && !identical(class(e[[2]]), "character"))
+                        dunno <- TRUE
+                    ## <NOTE>
+                    ## </NOTE>
                     ## <FIXME> could be inside substitute or a variable
                     ## and is in e.g. R.oo
-                    if(! pkg %in% c(depends_suggests, common_names))
+                    if(! dunno
+                       && ! pkg %in% c(depends_suggests, common_names))
                         bad_exprs <<- c(bad_exprs, pkg)
                 }
             } else if(Call %in%  "::") {
