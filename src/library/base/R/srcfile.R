60e41bdd45c8d2c5a907af4f46b1415e2a3b13bf
murdoch <murdoch@00db46b3-68df-0310-9c12-caf00c1e9a41>
2006-10-22 18:34:04 +0000

    Added docs; cosmetic changes to make check happy.
    git-svn-id: https://svn.r-project.org/R/branches/djm-source@39700 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/srcfile.R
+++ src/library/base/R/srcfile.R
@@ -1,61 +1,64 @@
 # a srcfile is a file with a timestamp
 
 srcfile <- function(filename) {
-  stopifnot(is.character(filename), length(filename) == 1)  
-  
-  e <- new.env(parent=emptyenv())
+    stopifnot(is.character(filename), length(filename) == 1)  
 
-  e$wd <- getwd()
-  e$filename <- filename
-  e$timestamp <- file.info(filename)[1,"mtime"]
-  
-  class(e) <- "srcfile"
-  return(e)
+    e <- new.env(parent=emptyenv())
+
+    e$wd <- getwd()
+    e$filename <- filename
+    e$timestamp <- file.info(filename)[1,"mtime"]
+
+    class(e) <- "srcfile"
+    return(e)
 }
 
-print.srcfile <- function(srcfile, ...) {
-  cat(srcfile$filename, "\n")
-  invisible(srcfile)
+print.srcfile <- function(x, ...) {
+    cat(x$filename, "\n")
+    invisible(x)
 }
 
-open.srcfile <- function(srcfile, line) {
-  
-  oldline <- srcfile$line
-  if (!is.null(oldline) && oldline > line) close(srcfile)
-  
-  conn <- srcfile$conn
-  if (is.null(conn)) {
-    olddir <- setwd(srcfile$wd)
-    on.exit(setwd(olddir))   
-    timestamp <- file.info(srcfile$filename)[1,"mtime"]
-    if (timestamp != srcfile$timestamp) warning("Timestamp of '",srcfile$filename,"' has changed", call.=FALSE)
-    srcfile$conn <- conn <- file(srcfile$filename, open="rt")
-    srcfile$line <- 1
-    oldline <- 1
-  } else if (!isOpen(conn)) {
-    open(conn, open="rt")
-    srcfile$line <- 1
-    oldline <- 1
-  }
-  if (oldline < line) {
-    readLines(conn, line-oldline)
-    srcfile$line <- line
-  }
-  invisible(conn)
+open.srcfile <- function(con, line, ...) {
+
+    srcfile <- con
+
+    oldline <- srcfile$line
+    if (!is.null(oldline) && oldline > line) close(srcfile)
+
+    conn <- srcfile$conn
+    if (is.null(conn)) {
+	olddir <- setwd(srcfile$wd)
+	on.exit(setwd(olddir))   
+	timestamp <- file.info(srcfile$filename)[1,"mtime"]
+	if (timestamp != srcfile$timestamp) warning("Timestamp of '",srcfile$filename,"' has changed", call.=FALSE)
+	srcfile$conn <- conn <- file(srcfile$filename, open="rt")
+	srcfile$line <- 1
+	oldline <- 1
+    } else if (!isOpen(conn)) {
+	open(conn, open="rt")
+	srcfile$line <- 1
+	oldline <- 1
+    }
+    if (oldline < line) {
+	readLines(conn, line-oldline)
+	srcfile$line <- line
+    }
+    invisible(conn)
 }
 
-close.srcfile <- function(srcfile) {
-  conn <- srcfile$conn
-  if (is.null(conn)) return()
-  else {
-    close(conn)
-    rm(list=c("conn", "line"), envir=srcfile)
-  }
+close.srcfile <- function(con, ...) {
+    srcfile <- con
+    conn <- srcfile$conn
+    if (is.null(conn)) return()
+    else {
+	close(conn)
+	rm(list=c("conn", "line"), envir=srcfile)
+    }
 }
 
 .isOpen <- function(srcfile) {
-  conn <- srcfile$conn
-  return( !is.null(conn) && isOpen(conn) )
+    conn <- srcfile$conn
+    return( !is.null(conn) && isOpen(conn) )
 }
 
 getSrcLines <- function(srcfile, first, last) {
@@ -72,17 +75,17 @@ getSrcLines <- function(srcfile, first, last) {
 # all are inclusive
 
 srcref <- function(srcfile, lloc) {
-  stopifnot(inherits(srcfile, "srcfile"), length(lloc) == 4)
-  structure(as.integer(lloc), srcfile=srcfile, class="srcref")
+    stopifnot(inherits(srcfile, "srcfile"), length(lloc) == 4)
+    structure(as.integer(lloc), srcfile=srcfile, class="srcref")
 }
   
 as.character.srcref <- function(srcref) {
-  srcfile <- attr(srcref, "srcfile")
-  lines <- getSrcLines(srcfile, srcref[1], srcref[3])  
-  lines[length(lines)] <- substring(lines[length(lines)], 1, srcref[4])
-  lines[1] <- substring(lines[1], srcref[2])
-  lines
+    srcfile <- attr(srcref, "srcfile")
+    lines <- getSrcLines(srcfile, srcref[1], srcref[3])  
+    lines[length(lines)] <- substring(lines[length(lines)], 1, srcref[4])
+    lines[1] <- substring(lines[1], srcref[2])
+    lines
 }
 
-print.srcref <- function(srcref, ...) 
-  cat(as.character(srcref), sep="\n")
+print.srcref <- function(x, ...) 
+    cat(as.character(x), sep="\n")
