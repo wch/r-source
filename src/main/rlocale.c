7aa42226ae963900b2d75e0b1fae547262d5bb32
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2006-11-14 11:26:30 +0000

    r39866 was unsafe, because of #undefs in the header?
    git-svn-id: https://svn.r-project.org/R/trunk@39888 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/rlocale.c
+++ src/main/rlocale.c
@@ -63,7 +63,7 @@ void Ri18n_iswctype(void)
 
 #else /* SUPPORT_MBCS */
 
-#include <R_ext/rlocale.h>
+/* #include <R_ext/rlocale.h> unsafe */
 #include "rlocale_data.h"
 
 #include <wctype.h>
@@ -211,6 +211,7 @@ static const char UNICODE[] = "UCS-4BE";
 #else
 static const char UNICODE[] = "UCS-4LE";
 #endif
+char *locale2charset(const char *locale);
 
 #define ISWFUNC(ISWNAME) static int Ri18n_isw ## ISWNAME (wint_t wc) \
 {	                                                             \
