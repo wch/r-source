c87312e624ace50ad0a6538d2f70718671ac2d6a
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2006-11-16 13:16:24 +0000

    add test for time, and error if we have nothing at all
    git-svn-id: https://svn.r-project.org/R/trunk@39918 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/mkdtemp.c
+++ src/main/mkdtemp.c
@@ -139,8 +139,10 @@ gen_tempname (char *tmpl)
     gettimeofday (&tv, NULL);
     random_time_bits = ((uint64_t) tv.tv_usec << 16) ^ tv.tv_sec;
   }
-# else
+# elif HAVE_TIME
   random_time_bits = time (NULL);
+# else
+# error RANDOM_BITS or gettimeofday or time is required
 # endif
 #endif
   value += random_time_bits ^ getpid ();
