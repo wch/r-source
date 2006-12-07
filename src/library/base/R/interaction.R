818e761519fbb183fd2a1f7b9297f82310109e21
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2006-12-07 10:33:46 +0000

    interaction( lex.order )
    git-svn-id: https://svn.r-project.org/R/trunk@40133 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/interaction.R
+++ src/library/base/R/interaction.R
@@ -1,6 +1,6 @@
 ### This is almost like the Primitive ":" for factors
 ### (that has no "drop = TRUE") --- it's not used anywhere in "standard R"
-interaction <- function(..., drop = FALSE, sep = ".")
+interaction <- function(..., drop = FALSE, sep = ".", lex.order = FALSE)
 {
     args <- list(...)
     narg <- length(args)
@@ -8,15 +8,27 @@ interaction <- function(..., drop = FALSE, sep = ".")
 	args <- args[[1]]
 	narg <- length(args)
     }
-    ans <- 0
-    lvs <- NULL
     for(i in narg:1) {
         f <- args[[i]]
 	if (!is.factor(f)) f <- factor(f)
 	l <- levels(f)
-	ans <- ans * length(l) + as.integer(f) - 1
-	lvs <- if (i == narg) l	else as.vector(outer(l, lvs, paste, sep=sep))
+        if1 <- as.integer(f) - 1
+        if(i != narg) {
+            if(lex.order) {
+                ll <- length(lvs)
+                ans <- ans + ll * if1
+                lvs <- ## as.vector(t(outer(l, lvs, paste, sep=sep)))
+                    paste(rep(l, each= ll), rep(lvs, length(l)), sep=sep)
+            } else {
+                ans <- ans * length(l) + if1
+                lvs <- ## as.vector(outer(l, lvs, paste, sep=sep))
+                    paste(rep(l, length(lvs)), rep(lvs, each = length(l)), sep=sep)
+            }
+        } else {
+            ans <- if1
+            lvs <- l
+        }
     }
-    ans <- structure(as.integer(ans+1), levels=lvs, class="factor")
-    ans[ ,drop=drop]
+    ans <- structure(as.integer(ans+1), levels=lvs, class = "factor")
+    ans[ , drop=drop]
 }
