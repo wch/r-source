5b4e015911bb4d4e8b5fc82e02b7154346ce221f
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2006-12-19 04:18:28 +0000

    revert r40242
    git-svn-id: https://svn.r-project.org/R/trunk@40246 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/graphics/R/abline.R
+++ src/library/graphics/R/abline.R
@@ -5,27 +5,19 @@ abline <- function(a = NULL, b = NULL, h = NULL, v = NULL, reg = NULL,
                            lty = par("lty"), lwd = par("lwd"), ...)
         .Internal(abline(a, b, h, v, untf, col, lty, lwd, ...))
 
-    if(!is.null(reg)) {
-        if(!is.null(a)) warning("'a' is overridden by 'reg'")
-        a <- reg
-    }
-    if(is.object(a) || is.list(a)) {
-        ## was  (!is.null(a) && is.list(a))
-	coefa <- as.vector(coef(a))
-	if (length(coefa) > 2)
-	    warning("only using the first two of ", length(coefa),
-		    "regression coefficients")
-	has.int <- !is.null(ii <- attr(terms(a), "intercept")) && as.logical(ii)
-	if (!has.int) {
+    if(!is.null(reg)) a <- reg
+    ## this should probably be is.object(a) || is.list(a)
+    if(!is.null(a) && is.list(a)) {
+	temp <- as.vector(coef(a))
+	if(length(temp) == 1) {
 	    a <- 0
-	    b <- coefa[1]
+	    b <- temp
 	} else {
-	    a <- coefa[1]
-	    b <- if (length(coefa) >= 2) coefa[2] else 0
+	    a <- temp[1]
+	    b <- temp[2]
 	}
     }
     if(!is.null(coef)) {
-	if(!is.null(a)) warning("'a' and 'b' are overridden by 'coef'")
 	a <- coef[1]
 	b <- coef[2]
     }
