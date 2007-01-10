eccbf26251c5b71d09b017d9eb113dd86a462690
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2007-01-10 07:38:30 +0000

    missing break
    git-svn-id: https://svn.r-project.org/R/trunk@40422 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/summary.c
+++ src/main/summary.c
@@ -327,7 +327,7 @@ static Rboolean cprod(Rcomplex *x, int n, Rcomplex *value, Rboolean narm)
 
 SEXP attribute_hidden do_summary(SEXP call, SEXP op, SEXP args, SEXP env)
 {
-    SEXP ans, a, stmp, scum = NA_STRING;
+    SEXP ans, a, stmp = NA_STRING /* -Wall */, scum = NA_STRING;
     double tmp = 0.0, s;
     Rcomplex z, ztmp, zcum={0.0, 0.0} /* -Wall */;
     int itmp = 0, icum=0, int_a, real_a, empty, warn = 0 /* dummy */;
@@ -509,7 +509,7 @@ SEXP attribute_hidden do_summary(SEXP call, SEXP op, SEXP args, SEXP env)
 			} else if(
 			    (iop == 2 && tmp < zcum.r) ||
 			    (iop == 3 && tmp > zcum.r))	zcum.r = tmp;
-		    } else {
+		    } else if(ans_type == STRSXP) {
 			if(empty) scum = stmp;
 			else {
 			    if(int_a)
@@ -667,18 +667,18 @@ SEXP attribute_hidden do_summary(SEXP call, SEXP op, SEXP args, SEXP env)
     switch(ans_type) {
     case INTSXP:   INTEGER(ans)[0] = icum;break;
     case REALSXP:  REAL(ans)[0] = zcum.r; break;
-    case CPLXSXP:  COMPLEX(ans)[0].r = zcum.r; COMPLEX(ans)[0].i = zcum.i;
-    case STRSXP:   SET_STRING_ELT(ans, 0, scum);
+    case CPLXSXP:  COMPLEX(ans)[0].r = zcum.r; COMPLEX(ans)[0].i = zcum.i;break;
+    case STRSXP:   SET_STRING_ELT(ans, 0, scum); break;
     }
     return ans;
 
-na_answer: /* only INTSXP case used */
+na_answer: /* only INTSXP case curently used */
     ans = allocVector(ans_type, 1);
     switch(ans_type) {
     case INTSXP:	INTEGER(ans)[0] = NA_INTEGER; break;
     case REALSXP:	REAL(ans)[0] = NA_REAL; break;
-    case CPLXSXP:	COMPLEX(ans)[0].r = COMPLEX(ans)[0].i = NA_REAL;
-    case STRSXP:        SET_STRING_ELT(ans, 0, NA_STRING);
+    case CPLXSXP:	COMPLEX(ans)[0].r = COMPLEX(ans)[0].i = NA_REAL; break;
+    case STRSXP:        SET_STRING_ELT(ans, 0, NA_STRING); break;
     }
     return ans;
 
