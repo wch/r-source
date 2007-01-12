fef5f8556d3ec6bc9a7cd40adb91b1821bb4421c
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2007-01-12 13:51:49 +0000

    minuscule patch from Jeffrey Horner
    git-svn-id: https://svn.r-project.org/R/trunk@40453 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/modules/internet/internet.c
+++ src/modules/internet/internet.c
@@ -167,7 +167,7 @@ static Rconnection in_R_newurl(char *description, const char * const mode)
 
     new = (Rconnection) malloc(sizeof(struct Rconn));
     if(!new) error(_("allocation of url connection failed"));
-    new->class = (char *) malloc(strlen("file") + 1);
+    new->class = (char *) malloc(strlen("url") + 1);
     if(!new->class) {
 	free(new);
 	error(_("allocation of url connection failed"));
@@ -287,7 +287,7 @@ static SEXP in_do_download(SEXP call, SEXP op, SEXP args, SEXP env)
     UNPROTECT(1);
     if(TYPEOF(sheaders) == NILSXP)
         headers = NULL;
-    else 
+    else
         headers = CHAR(STRING_ELT(sheaders, 0));
 #ifdef Win32
     if (!pbar.wprog) {
@@ -295,7 +295,7 @@ static SEXP in_do_download(SEXP call, SEXP op, SEXP args, SEXP env)
 		      Titlebar | Centered);
 	setbackground(pbar.wprog, dialog_bg());
 	pbar.l_url = newlabel(" ", rect(10, 15, 520, 25), AlignCenter);
-	pbar.pb = newprogressbar(rect(20, 50, 500, 20), 0, 1024, 1024, 1);	    	
+	pbar.pb = newprogressbar(rect(20, 50, 500, 20), 0, 1024, 1024, 1);
     }
 #endif
     if(strncmp(url, "file://", 7) == 0) {
@@ -304,7 +304,7 @@ static SEXP in_do_download(SEXP call, SEXP op, SEXP args, SEXP env)
 	size_t n;
 	int nh = 7;
 #ifdef Win32
-	/* on Windows we have file:///d:/path/to 
+	/* on Windows we have file:///d:/path/to
 	   whereas on Unix it is file:///path/to */
 	if (strlen(url) > 9 && url[7] == '/' && url[9] == ':') nh = 8;
 #endif
@@ -395,7 +395,7 @@ static SEXP in_do_download(SEXP call, SEXP op, SEXP args, SEXP env)
 #ifdef Win32
 	    R_FlushConsole();
 	    endcontext(&(pbar.cntxt));
-	    doneprogressbar(&pbar);	    
+	    doneprogressbar(&pbar);
 #endif
 	    if (total > 0 && total != nbytes)
 		warning(_("downloaded length %d != reported length %d"),
@@ -444,7 +444,7 @@ static SEXP in_do_download(SEXP call, SEXP op, SEXP args, SEXP env)
 			 R_NilValue, R_NilValue, R_NilValue);
 	    pbar.cntxt.cend = &doneprogressbar;
 	    pbar.cntxt.cenddata = &pbar;
-	    
+
 #endif
 	    while ((len = in_R_FTPRead(ctxt, buf, sizeof(buf))) > 0) {
 		size_t res = fwrite(buf, 1, len, out);
@@ -477,7 +477,7 @@ static SEXP in_do_download(SEXP call, SEXP op, SEXP args, SEXP env)
 #ifdef Win32
 	    R_FlushConsole();
 	    endcontext(&(pbar.cntxt));
-	    doneprogressbar(&pbar);	    
+	    doneprogressbar(&pbar);
 #endif
 	    if (total > 0 && total != nbytes)
 		warning(_("downloaded length %d != reported length %d"),
@@ -629,7 +629,7 @@ InternetCallback(HINTERNET hInternet, DWORD context, DWORD Status,
 }
 #endif /* USE_WININET_ASYNC */
 
-static void *in_R_HTTPOpen(const char *url, const char *headers, 
+static void *in_R_HTTPOpen(const char *url, const char *headers,
                            const int cacheOK)
 {
     WIctxt  wictxt;
@@ -717,7 +717,7 @@ static void *in_R_HTTPOpen(const char *url, const char *headers,
 	    warning(_("InternetOpenUrl failed: '%s'"), buf);
 	    return NULL;
 	} else {
-	    FormatMessage( 
+	    FormatMessage(
 		FORMAT_MESSAGE_FROM_HMODULE,
 		GetModuleHandle("wininet.dll"),
 		err1,
@@ -868,7 +868,7 @@ static void *in_R_FTPOpen(const char *url)
 	    warning(_("InternetOpenUrl failed: '%s'"), buf);
 	    return NULL;
 	} else {
-	    FormatMessage( 
+	    FormatMessage(
 		FORMAT_MESSAGE_FROM_HMODULE,
 		GetModuleHandle("wininet.dll"),
 		err1,
@@ -895,7 +895,7 @@ static void in_R_FTPClose(void *ctx)
 #endif
 
 #ifndef HAVE_INTERNET
-static void *in_R_HTTPOpen(const char *url, const char *headers, 
+static void *in_R_HTTPOpen(const char *url, const char *headers,
                            const int cacheOK)
 {
     return NULL;
@@ -935,7 +935,7 @@ void RxmlMessage(int level, const char *format, ...)
 
     clevel = asInteger(GetOption(install("internet.info"), R_BaseEnv));
     if(clevel == NA_INTEGER) clevel = 2;
-    
+
     if(level < clevel) return;
 
     va_start(ap, format);
