a11e7c34bc237d27411e7c6440f321b25120c6b9
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2007-01-15 08:41:42 +0000

    tidy up and begin merging zip.file.extract
    git-svn-id: https://svn.r-project.org/R/trunk@40478 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/utils/R/unix/zip.R
+++ src/library/utils/R/unix/zip.R
@@ -1,20 +1,20 @@
-zip.file.extract <- function(file, zipname = "R.zip")
+zip.file.extract <- function(file, zipname = "R.zip",
+                             unzip = getOption("unzip"))
 {
-    ## somewhat system-specific.
-    unzip <- getOption("unzip")
+    if(!is.character(unzip) || length(unzip) != 1)
+        stop("'unzip' must be a single character string")
     if(!nchar(unzip)) unzip <- "internal"
     path <- dirname(file)
     topic <- basename(file)
     if(file.exists(file.path(path, zipname))) {
         tmpd <- tempdir()
         if(unzip != "internal") {
-            if(!system(paste(unzip, "-o",
-                             file.path(path, zipname), topic, "-d", tmpd,
-                             " > /dev/null")))
+            if(!system(paste(unzip, "-oq",
+                             shQuote(file.path(path, zipname)), topic,
+                             "-d", tmpd, " > /dev/null")))
                 file <- file.path(tmpd, topic)
         } else {
-            rc <- .Internal(int.unzip(file.path(path, zipname),
-                                      topic, tmpd))
+            rc <- .Internal(int.unzip(file.path(path, zipname), topic, tmpd))
             if (rc == 0)
                 file <- file.path(tmpd, topic)
         }
--- src/library/utils/R/windows/zip.R
+++ src/library/utils/R/windows/zip.R
@@ -1,14 +1,19 @@
-zip.file.extract <- function(file, zipname = "R.zip")
+zip.file.extract <- function(file, zipname = "R.zip",
+                             unzip = getOption("unzip"))
 {
+    if(!is.character(unzip) || length(unzip) != 1)
+        stop("'unzip' must be a single character string")
+    if(!nchar(unzip)) unzip <- "internal"
     path <- dirname(file)
     topic <- basename(file)
     if(file.exists(file.path(path, zipname))) {
         tmpd <- tempdir()
-        if((unzip <- getOption("unzip")) != "internal") {
-            if(!system(paste(unzip, ' -oq "',
-                             file.path(path, zipname), '" ', topic,
-                             " -d ", tmpd, sep=""), invisible = TRUE))
-                file <- file.path(tmpd, topic)
+        if(unzip != "internal") {
+            cmd <- paste(unzip, "-oq", shQuote(file.path(path, zipname)),
+                         topic, " -d ", tmpd)
+            res <- if(.Platform$OS.type == "windows")
+                system(cmd, invisible = TRUE) else system(cmd)
+            if(!res) file <- file.path(tmpd, topic)
         } else {
             rc <- .Internal(int.unzip(file.path(path, zipname), topic, tmpd))
             if (rc == 0)
