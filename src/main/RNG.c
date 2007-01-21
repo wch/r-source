4e7399577f4f28b11d7fa8fb19b3447f5604c5f3
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2007-01-21 12:51:34 +0000

    port 40538:40 from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-2-4-branch@40541 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/RNG.c
+++ src/main/RNG.c
@@ -566,6 +566,13 @@ static double MT_genrand()
     return ( (double)y * 2.3283064365386963e-10 ); /* reals: [0,1)-interval */
 }
 
+/* 
+   The following code was taken from earlier versions of
+   http://www-cs-faculty.stanford.edu/~knuth/programs/rng.c-old
+   http://www-cs-faculty.stanford.edu/~knuth/programs/rng.c
+*/
+
+
 #define long Int32
 #define Void void
 #define void static void
@@ -653,12 +660,12 @@ void ran_start(seed)    /* do this before using ran_array */
 /* after calling ran_start, get new randoms by, e.g., "x=ran_arr_next()" */
 
 #define QUALITY 1009 /* recommended quality level for high-res use */
-static long ran_arr_buf[QUALITY];
-static long ran_arr_sentinel=(long)-1;
-static long *ran_arr_ptr=&ran_arr_sentinel; /* the next random number, or -1 */
+long ran_arr_buf[QUALITY];
+long ran_arr_sentinel=-1;
+long *ran_arr_ptr=&ran_arr_sentinel; /* the next random number, or -1 */
 
 #define ran_arr_next() (*ran_arr_ptr>=0? *ran_arr_ptr++: ran_arr_cycle())
-static long ran_arr_cycle()
+long ran_arr_cycle()
 {
   ran_array(ran_arr_buf,QUALITY);
   ran_arr_buf[100]=-1;
