945fe7ef60fd8531a3e92fa69cc31a43bc045e90
tlumley <tlumley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2007-03-06 22:38:03 +0000

    add "profmem" to capabilities()
    git-svn-id: https://svn.r-project.org/R/trunk@40818 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/platform.c
+++ src/main/platform.c
@@ -1327,8 +1327,8 @@ SEXP attribute_hidden do_capabilities(SEXP call, SEXP op, SEXP args, SEXP rho)
 	        break;
 	    }
 #endif
-    PROTECT(ans = allocVector(LGLSXP, 11));
-    PROTECT(ansnames = allocVector(STRSXP, 11));
+    PROTECT(ans = allocVector(LGLSXP, 12));
+    PROTECT(ansnames = allocVector(STRSXP, 12));
 
     SET_STRING_ELT(ansnames, i, mkChar("jpeg"));
 #ifdef HAVE_JPEG
@@ -1427,6 +1427,13 @@ SEXP attribute_hidden do_capabilities(SEXP call, SEXP op, SEXP args, SEXP rho)
 
     SET_STRING_ELT(ansnames, i, mkChar("NLS"));
 #ifdef ENABLE_NLS
+    LOGICAL(ans)[i++] = TRUE;
+#else
+    LOGICAL(ans)[i++] = FALSE;
+#endif
+
+    SET_STRING_ELT(ansnames, i, mkChar("profmem"));
+#ifdef R_MEMORY_PROFILING
     LOGICAL(ans)[i++] = TRUE;
 #else
     LOGICAL(ans)[i++] = FALSE;
