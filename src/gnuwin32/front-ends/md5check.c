862a3199d40830c42db9a9187a5b44f8dcc54ebb
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2007-03-18 12:43:05 +0000

    update
    git-svn-id: https://svn.r-project.org/R/trunk@40853 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/front-ends/md5check.c
+++ src/gnuwin32/front-ends/md5check.c
@@ -42,8 +42,8 @@ void read_unist_file(char* fname)
     }
     fseek(fp, 320, SEEK_SET);
     fread(&Version, 4, 1, fp);
-    /* 4.2.x is 15, 5.0.6 is 33 5.1.3 is 42*/
-    if(Version > 42)
+    /* 4.2.x is 15, 5.0.6 is 33 5.1.3 is 42 5.1.11 is 46 */
+    if(Version > 46)
 	fprintf(stderr, "Version %d of the uninst format is new and may not be supported\n", Version);
     fread(&NumRecs, 4, 1, fp);
     fread(&EndOffset, 4, 1, fp);
