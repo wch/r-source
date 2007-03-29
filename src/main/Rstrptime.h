e74ce89068ac808dcc7ecd2ddbb7b28f1ba9c9c7
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2007-03-29 22:43:02 +0000

    bug fix in ansari.test avoid symbol clash in AIX 5.3 headers
    git-svn-id: https://svn.r-project.org/R/trunk@40951 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/Rstrptime.h
+++ src/main/Rstrptime.h
@@ -198,7 +198,7 @@ static wchar_t w_ab_month_name[][10] =
 static wchar_t w_am_pm[][4] = {L"AM", L"PM"};
 
 /* Need case-insensitive version */
-static int wcsncasecmp(const wchar_t *cs1, const wchar_t *s2)
+static int Rwcsncasecmp(const wchar_t *cs1, const wchar_t *s2)
 {
     size_t i, n = wcslen(cs1);
     const wchar_t *a = cs1, *b = s2;
@@ -209,7 +209,7 @@ static int wcsncasecmp(const wchar_t *cs1, const wchar_t *s2)
 }
 
 #define w_match_string(cs1, s2) \
-  (wcsncasecmp ((cs1), (s2)) ? 0 : ((s2) += wcslen (cs1), 1))
+  (Rwcsncasecmp ((cs1), (s2)) ? 0 : ((s2) += wcslen (cs1), 1))
 
 #define w_recursive(new_fmt) \
   (*(new_fmt) != '\0'							      \
