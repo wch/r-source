8c5c09de453d1be5f8bbb0a7935340b54ad36b4f
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2007-03-29 11:38:45 +0000

    port r40939 from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-2-5-branch@40940 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/sets.R
+++ src/library/base/R/sets.R
@@ -1,12 +1,24 @@
-union <- function(x, y) unique(c(x, y))
+union <- function(x, y) unique(c(as.vector(x), as.vector(y)))
 
-intersect <- function(x, y) unique(y[match(x, y, 0)])
+intersect <- function(x, y)
+{
+    y <- as.vector(y)
+    unique(y[match(as.vector(x), y, 0)])
+}
 
 setdiff <- function(x, y)
+{
+    x <- as.vector(x)
+    y <- as.vector(y)
     unique(if(length(x) || length(y)) x[match(x, y, 0) == 0] else x)
-
+}
 ## Faster versions, see R-devel, Jan.4-6, 2000;  optimize later...
-setequal <- function(x, y) all(c(match(x, y, 0) > 0, match(y, x, 0) > 0))
+setequal <- function(x, y)
+{
+    x <- as.vector(x)
+    y <- as.vector(y)
+    all(c(match(x, y, 0) > 0, match(y, x, 0) > 0))
+}
 
 ##  same as %in% ( ./match.R ) but different arg names:
 is.element <- function(el, set) match(el, set, 0) > 0
