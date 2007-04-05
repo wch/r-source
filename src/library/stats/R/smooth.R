55e4fcee1fc106813a3f01938a32fc6b24a58024
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2007-04-05 16:20:35 +0000

    fix overwriting bug in smooth.c
    git-svn-id: https://svn.r-project.org/R/trunk@41063 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/R/smooth.R
+++ src/library/stats/R/smooth.R
@@ -24,7 +24,7 @@ smooth <- function(x, kind = c("3RS3R", "3RSS", "3RSR", "3R", "3", "S"),
               y = double(n),
               n, iend,
               iter = integer(1),
-              # DUP=FALSE,
+              DUP=FALSE,
 	      PACKAGE = "stats")[c("y","iter")]
 
     if(any(kind == c("R", "S"))) { # `iter' really was `changed'
