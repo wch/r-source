483a21dc401435867e2dafd667e06042e6915509
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2007-04-15 07:23:40 +0000

    fixes for PR#8876 and PR#7951
    git-svn-id: https://svn.r-project.org/R/trunk@41164 00db46b3-68df-0310-9c12-caf00c1e9a41

--- tests/primitives.R
+++ tests/primitives.R
@@ -53,7 +53,6 @@ stopifnot(S4generic)
 # functions which are listed but not primitive
 extraS4 <- c('all', 'any', 'gamma', 'lgamma', 'log', 'log10', 'max',
              'min', 'prod', 'range', 'round', 'signif', 'sum',
-             'trace', 'untrace',
-             'as.complex', 'as.integer', 'as.logical', 'as.raw', 'as.numeric')
+             'trace', 'untrace')
 ff4[!ff4 %in% c(ff, extraS4)]
 stopifnot(ff4 %in% c(ff, extraS4))
