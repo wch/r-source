4ddf1f6dcf9ae970488c629ba5c28a8d966ccbce
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2007-04-26 09:11:54 +0000

    ifdef for use on Windows
    git-svn-id: https://svn.r-project.org/R/trunk@41325 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/options.c
+++ src/main/options.c
@@ -281,7 +281,9 @@ void attribute_hidden InitOptions(void)
     /* value from Rf_initialize_R */
     SET_TAG(v, install("rl_word_breaks"));
     SETCAR(v, mkString(" \t\n\"\\'`><=+-*%;,|&{()}"));
+#ifdef HAVE_RL_COMPLETION_MATCHES
     set_rl_word_breaks(" \t\n\"\\'`><=+-*%;,|&{()}");
+#endif
 
     SET_SYMVALUE(install(".Options"), val);
     UNPROTECT(1);
@@ -511,7 +513,9 @@ SEXP attribute_hidden do_options(SEXP call, SEXP op, SEXP args, SEXP rho)
 	    else if (streql(CHAR(namei), "rl_word_breaks")) {
 		if (TYPEOF(argi) != STRSXP || LENGTH(argi) != 1)
 		    errorcall(call, _("rl_word_breaks parameter invalid"));
+#ifdef HAVE_RL_COMPLETION_MATCHES
 		set_rl_word_breaks(CHAR(STRING_ELT(argi, 0)));
+#endif
 		SET_VECTOR_ELT(value, i, SetOption(tag, duplicate(argi)));
 	    }
 	    else {
