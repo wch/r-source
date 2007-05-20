1be75d6ce14f69a88957ddc8f60f9c557604930a
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2007-05-20 13:29:38 +0000

    bug fixes to qr auxiliaries
    git-svn-id: https://svn.r-project.org/R/trunk@41643 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/solve.R
+++ src/library/base/R/solve.R
@@ -2,15 +2,18 @@ solve.qr <- function(a, b, ...)
 {
     if( !is.qr(a) )
 	stop("this is the \"qr\" method for the generic function solve()")
-    nc <- ncol(a$qr)
+    nc <- ncol(a$qr); nr <- nrow(a$qr)
+    if( a$rank != min(nc, nr) )
     if( a$rank != nc )
 	stop("singular matrix 'a' in 'solve'")
     if( missing(b) ) {
-	if( nc != nrow(a$qr) )
+	if( nc != nr )
 	    stop("only square matrices can be inverted")
 	b <- diag(1, nc)
     }
-    return(qr.coef(a, b))
+    res <- qr.coef(a, b)
+    res[is.na(res)] <- 0
+    res
 }
 
 solve.default <-
@@ -74,14 +77,16 @@ qr.solve <- function(a, b, tol = 1e-7)
 {
     if( !is.qr(a) )
 	a <- qr(a, tol = tol)
-    nc <- ncol(a$qr)
-    if( a$rank != nc )
+    nc <- ncol(a$qr); nr <- nrow(a$qr)
+    if( a$rank != min(nc, nr) )
 	stop("singular matrix 'a' in solve")
     if( missing(b) ) {
-	if( nc != nrow(a$qr) )
+	if( nc != nr )
 	    stop("only square matrices can be inverted")
 	b <- diag(1, nc)
     }
-    return(qr.coef(a, b))
+    res <- qr.coef(a, b)
+    res[is.na(res)] <- 0
+    res
 }
 
