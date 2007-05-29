7152a793b79d32f3f83221549436898379bbde40
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2007-05-29 20:04:08 +0000

    change rl_word_breaks default for rcompgen 0.1_13
    git-svn-id: https://svn.r-project.org/R/trunk@41752 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/options.c
+++ src/main/options.c
@@ -285,8 +285,8 @@ void attribute_hidden InitOptions(void)
 #ifdef HAVE_RL_COMPLETION_MATCHES
     /* value from Rf_initialize_R */
     SET_TAG(v, install("rl_word_breaks"));
-    SETCAR(v, mkString(" \t\n\"\\'`><=+-*%;,|&{()}"));
-    set_rl_word_breaks(" \t\n\"\\'`><=+-*%;,|&{()}");
+    SETCAR(v, mkString(" \t\n\"\\'`><=%;,|&{()}"));
+    set_rl_word_breaks(" \t\n\"\\'`><=%;,|&{()}");
 #endif
 
     SET_SYMVALUE(install(".Options"), val);
