9751fce8760ee9d02faa2ffde8215c5648a5247b
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2007-05-30 19:38:41 +0000

    drop extraneous sort(.)
    git-svn-id: https://svn.r-project.org/R/trunk@41756 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/R/ecdf.R
+++ src/library/stats/R/ecdf.R
@@ -7,7 +7,7 @@ ecdf <- function (x)
     x <- sort(x) # drops NAs
     n <- length(x)
     if(n < 1) stop("'x' must have 1 or more non-missing values")
-    vals <- sort(unique(x))
+    vals <- unique(x)
     rval <- approxfun(vals, cumsum(tabulate(match(x, vals)))/n,
 		      method = "constant", yleft = 0, yright = 1, f = 0,
                       ties = "ordered")
@@ -23,7 +23,7 @@ print.ecdf <- function (x, digits= getOption("digits") - 2, ...)
     print(attr(x, "call"), ...)
     n <- length(xx <- eval(expression(x), envir = environment(x)))
     i1 <- 1:min(3,n)
-    i2 <- if(n>=4) max(4,n-1):n else integer(0)
+    i2 <- if(n >= 4) max(4,n-1):n else integer(0)
     cat(" x[1:",n,"] = ", numform(xx[i1]),
 	if(n>3) ", ", if(n>5) " ..., ", numform(xx[i2]), "\n", sep = "")
     invisible(x)
