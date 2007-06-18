0f3dd63558c1e8bac331c5dfba4795b482822358
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2007-06-18 04:30:23 +0000

    update
    git-svn-id: https://svn.r-project.org/R/branches/R-2-5-branch@41984 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/fixed/h/config.h
+++ src/gnuwin32/fixed/h/config.h
@@ -784,13 +784,13 @@
 #define PACKAGE_NAME "R"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "R 2.5.0"
+#define PACKAGE_STRING "R 2.5.1"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "R"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "2.5.0"
+#define PACKAGE_VERSION "2.5.1"
 
 /* Define if <inttypes.h> exists and defines unusable PRI* macros. */
 /* #undef PRI_MACROS_BROKEN */
@@ -889,7 +889,7 @@
 #define VALGRIND_LEVEL 0
 
 /* Version number of package */
-#define VERSION "2.5.0"
+#define VERSION "2.5.1"
 
 /* Define to 1 if your processor stores words with the most significant byte
    first (like Motorola and SPARC, unlike Intel and VAX). */
