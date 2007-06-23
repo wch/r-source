f483028c0451c626da295ed7503ef6b80796a3fa
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2007-06-23 07:23:12 +0000

    avoid setting .Random.seed (which we do not recoomend to users)
    git-svn-id: https://svn.r-project.org/R/trunk@42035 00db46b3-68df-0310-9c12-caf00c1e9a41

--- tests/demos2.R
+++ tests/demos2.R
@@ -1,6 +1,6 @@
 #### Run all demos for which we do not wish to diff the output
 .ptime <- proc.time()
-.Random.seed <- c(0,rep(7654, 3))
+set.seed(123)
 
 demos <- c("Hershey", "Japanese", "lm.glm", "nlm", "plotmath")
 
