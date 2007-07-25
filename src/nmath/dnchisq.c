7c6eee48c63481cda429c8e36acc52c52a031ff6
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2007-07-25 09:51:32 +0000

    change reference to GPL
    git-svn-id: https://svn.r-project.org/R/trunk@42310 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/nmath/dnchisq.c
+++ src/nmath/dnchisq.c
@@ -16,8 +16,7 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, a copy is available at
- *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
- *  USA.
+ *  http://www.r-project.org/Licenses/
  *
  *  DESCRIPTION
  *
