b7ac6cc18d44fc4b207daa16cb6a49b7f3532aa8
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2007-07-25 09:31:46 +0000

    change reference to GPL
    git-svn-id: https://svn.r-project.org/R/trunk@42306 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/front-ends/bdx_com.c
+++ src/gnuwin32/front-ends/bdx_com.c
@@ -13,9 +13,8 @@
  *  Library General Public License for more details.
  * 
  *  You should have received a copy of the GNU Library General Public
- *  License along with this library; if not, write to the Free
- *  Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- *  MA 02110-1301, USA.
+ *  License along with this library; if not, a copy is available at
+ *  http://www.r-project.org/Licenses/
  *
  *  Conversion functions from VARIANT to BDX and vice versa.
  *
