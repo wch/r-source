3a4c017586eff5e97a84b2e9d33e23b3ae7451eb
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2007-07-25 06:40:52 +0000

    start to move address of copy of GPL add copyright to files missing it
    git-svn-id: https://svn.r-project.org/R/trunk@42298 00db46b3-68df-0310-9c12-caf00c1e9a41

--- share/perl/R/Vars.pm
+++ share/perl/R/Vars.pm
@@ -1,3 +1,20 @@
+## Copyright (C) 2002-2005 R Development Core Team
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2, or (at your option)
+## any later version.
+##
+## This program is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+## General Public License for more details.
+##
+## A copy of the GNU General Public License is available at
+## http://www.r-project.org/Licenses/
+
+## Send any bug reports to r-bugs@r-project.org.
+
 package R::Vars;
 
 =head1 NAME
