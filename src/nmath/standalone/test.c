956a5ac1a2e107d1eba3417adb3de75dbb7b918a
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2007-08-03 07:43:35 +0000

    update Windows tests
    git-svn-id: https://svn.r-project.org/R/trunk@42406 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/nmath/standalone/test.c
+++ src/nmath/standalone/test.c
@@ -1,6 +1,6 @@
 /*
  *  Mathlib : A C Library of Special Functions
- *  Copyright (C) 2000-6  The R Development Core Team
+ *  Copyright (C) 2000-7  The R Development Core Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -22,6 +22,14 @@
 #include <Rmath.h>
 
 #include <stdio.h>
+typedef enum {
+    BUGGY_KINDERMAN_RAMAGE,
+    AHRENS_DIETER,
+    BOX_MULLER,
+    USER_NORM,
+    INVERSION,
+    KINDERMAN_RAMAGE
+} N01type;
 
 int
 main(int argc, char** argv)
@@ -29,5 +37,12 @@ main(int argc, char** argv)
 /* something to force the library to be included */
     qnorm(0.7, 0.0, 1.0, 0, 0);
     printf("*** loaded '%s'\n", argv[0]);
+    set_seed(123, 456);
+    N01_kind = AHRENS_DIETER;
+    printf("one normal %f\n", norm_rand());
+    set_seed(123, 456);
+    N01_kind = BOX_MULLER;
+    printf("normal via BM %f\n", norm_rand());
+    
     return 0;
 }
