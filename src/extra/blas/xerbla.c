eed02faa956342774e12e21bfe3f839cbee2be21
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2007-08-21 20:41:43 +0000

    final tweaks for cygwin
    git-svn-id: https://svn.r-project.org/R/trunk@42601 00db46b3-68df-0310-9c12-caf00c1e9a41

---
+++ src/extra/blas/xerbla.c
@@ -0,0 +1,14 @@
+/* It seems Cygwin does not allow mutually dependent dlls, so fake xerbla */
+
+#include <S.h>
+
+/* Fortran-callable error routine for lapack */
+
+void F77_NAME(xerbla)(const char *srname, int *info)
+{
+   /* srname is not null-terminated.  It should be 6 characters. */
+    char buf[7];
+    strncpy(buf, srname, 6);
+    buf[6] = '\0';
+    printf("BLAS/LAPACK routine '%6s' gave error code %d", buf, -(*info));
+}
