8befe0caa6686fed30602f61248dc3f2d1b669d8
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2007-09-14 17:19:37 +0000

    fix PR#9908
    git-svn-id: https://svn.r-project.org/R/trunk@42847 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/datetime.R
+++ src/library/base/R/datetime.R
@@ -698,6 +698,7 @@ julian <- function(x, ...) UseMethod("julian")
 
 julian.POSIXt <- function(x, origin = as.POSIXct("1970-01-01", tz="GMT"), ...)
 {
+    origin <- as.POSIXct(origin)
     if(length(origin) != 1) stop("'origin' must be of length one")
     res <- difftime(as.POSIXct(x), origin, units = "days")
     structure(res, "origin" = origin)
