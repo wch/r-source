3cfa00ff0f78a4825bea5add0ea41943e8b33af0
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2007-10-01 07:41:36 +0000

    fix thinko
    git-svn-id: https://svn.r-project.org/R/branches/R-2-6-branch@43027 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/envir.c
+++ src/main/envir.c
@@ -3231,7 +3231,7 @@ void attribute_hidden InitStringHash()
 
 #define CHARENCCMP(a, b) ( \
                          ((IS_UTF8(a) == IS_UTF8(b)) && \
-                          (IS_UTF8(a) == IS_UTF8(b))))
+                          (IS_LATIN1(a) == IS_LATIN1(b))))
 
 /* Resize a hash table with char* based keys.  The new table will be
    sized according to HASHTABLEGROWTHRATE. */
