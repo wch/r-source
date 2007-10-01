92257ed1340b83cd0f166a16cd9529bf80743dbc
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2007-10-01 15:22:10 +0000

    seems libiconv and glibc have different endiannesss for UCS-2
    git-svn-id: https://svn.r-project.org/R/trunk@43036 00db46b3-68df-0310-9c12-caf00c1e9a41

--- tests/reg-tests-3.R
+++ tests/reg-tests-3.R
@@ -88,10 +88,10 @@ print(1.001, digits=16)
 
 ## iconv to UCS-2
 x <- "A test string"
-y <- try(iconv(x, "", "UCS-2"))
+y <- try(iconv(x, "", "UCS-2BE"))
 if(!inherits(y, "try-error")) {
     print(y)
-    z <- iconv(y, "UCS-2", "")
+    z <- iconv(y, "UCS-2BE", "")
     stopifnot(identical(x, z))
 }
 ## works in R >= 2.6.0
