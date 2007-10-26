40437eaca0db9548e7ab340c060b61e316da9c1e
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2007-10-26 06:04:17 +0000

    more efficiency tweaks
    git-svn-id: https://svn.r-project.org/R/trunk@43278 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/eval.c
+++ src/main/eval.c
@@ -926,10 +926,25 @@ static SEXP assignCall(SEXP op, SEXP symbol, SEXP fun,
 
 static R_INLINE Rboolean asLogicalNoNA(SEXP s, SEXP call)
 {
-    Rboolean cond = asLogical(s);
+    Rboolean cond = NA_LOGICAL;
+
     if (length(s) > 1)
 	warningcall(call,
 		    _("the condition has length > 1 and only the first element will be used"));
+    if (length(s) > 0) {
+	/* inline common cases for efficiency */
+	switch(TYPEOF(s)) {
+	case LGLSXP: 
+	    cond = LOGICAL(s)[0];
+	    break;
+	case INTSXP:
+	    cond = INTEGER(s)[0]; /* relies on NA_INTEGER == NA_LOGICAL */
+	    break;
+	default:
+	    cond = asLogical(s);
+	}
+    }
+
     if (cond == NA_LOGICAL) {
 	char *msg = length(s) ? (isLogical(s) ?
 				 _("missing value where TRUE/FALSE needed") :
