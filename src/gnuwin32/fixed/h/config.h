b5fa84b5ca98fcdc37f4fc4fe4f455a3df4a68e7
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2007-11-08 08:52:52 +0000

    compute the version at install time.
    git-svn-id: https://svn.r-project.org/R/trunk@43390 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/fixed/h/config.h
+++ src/gnuwin32/fixed/h/config.h
@@ -791,13 +791,13 @@
 #define PACKAGE_NAME "R"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "R 2.7.0"
+#define PACKAGE_STRING "R @VERSION@"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "R"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "2.7.0"
+#define PACKAGE_VERSION "@VERSION@"
 
 /* Define if <inttypes.h> exists and defines unusable PRI* macros. */
 /* #undef PRI_MACROS_BROKEN */
@@ -926,7 +926,7 @@
 #define VALGRIND_LEVEL 0
 
 /* Version number of package */
-#define VERSION "2.7.0"
+#define VERSION "@VERSION@"
 
 /* Define to 1 if your processor stores words with the most significant byte
    first (like Motorola and SPARC, unlike Intel and VAX). */
