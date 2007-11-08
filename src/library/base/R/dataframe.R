f65e3c44f7c470cff74ae3ebf33ad2d11427322e
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2007-11-08 09:19:10 +0000

    exclude empty char indices
    git-svn-id: https://svn.r-project.org/R/trunk@43391 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/dataframe.R
+++ src/library/base/R/dataframe.R
@@ -707,6 +707,7 @@ data.frame <-
         if(any(is.na(j)))
             stop("missing values are not allowed in subscripted assignments of data frames")
 	if(is.character(j)) {
+            if("" %in% j) stop("column name \"\" cannot match any column")
 	    jj <- match(j, names(x))
 	    nnew <- sum(is.na(jj))
 	    if(nnew > 0L) {
@@ -917,10 +918,10 @@ data.frame <-
 	stop("non-existent rows not allowed")
 
     if(is.character(j)) {
+        if("" %in% j) stop("column name \"\" cannot match any column")
 	jseq <- match(j, names(x))
 	if(any(is.na(jseq)))
-	    stop("replacing element in non-existent column: ",
-                 j[is.na(jseq)])
+	    stop("replacing element in non-existent column: ", j[is.na(jseq)])
     }
     else if(is.logical(j) || min(j) < 0L)
 	jseq <- seq_along(x)[j]
