da407bd0ed3af0b1d95293d18daf833022999c9a
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2007-11-10 09:46:01 +0000

    add missing copyright headers
    git-svn-id: https://svn.r-project.org/R/branches/R-2-6-branch@43419 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tcltk/src/tcltk_win.c
+++ src/library/tcltk/src/tcltk_win.c
@@ -1,3 +1,22 @@
+/*
+ *  R : A Computer Language for Statistical Data Analysis
+ *  Copyright (C) 2000--2007  The R Development Core Team
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, a copy is available at
+ *  http://www.r-project.org/Licenses/
+ */
+
 #include <tcl.h>
 #define WIN32_LEAN_AND_MEAN 1
 #include <windows.h>
