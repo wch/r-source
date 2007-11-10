df1f5edc8d7218ab0d8b94425a97b951e68f4814
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2007-11-10 09:39:12 +0000

    adjust Windows make for tcltk_unix.c add long-missing copyright notices
    git-svn-id: https://svn.r-project.org/R/trunk@43418 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tcltk/src/tcltk_win.c
+++ src/library/tcltk/src/tcltk_win.c
@@ -1,5 +1,25 @@
+/*
+ *  R : A Computer Language for Statistical Data Analysis
+ *  Copyright (C) 2000--2007  The R Development Core Team
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, a copy is available at
+ *  http://www.r-project.org/Licenses/
+ */
+
+#include "tcltk.h"
+
 #include <R_ext/Boolean.h>
-#include <tcl.h>
 #define WIN32_LEAN_AND_MEAN 1
 #include <windows.h>
 
@@ -7,8 +27,6 @@ Rboolean R_ToplevelExec(void (*fun)(void *), void *data);
 
 void tcltk_init();
 static int Tcl_lock = 0; /* reentrancy guard */
-
-extern __declspec(dllimport) void (* R_tcldo)();
 
 static void TclSpinLoop(void *data)
 {
@@ -24,6 +42,8 @@ static void _R_tcldo(void)
     }
 }
 
+/* import from src/gnuwin32/system.c */
+extern __declspec(dllimport) void (* R_tcldo)();
 static void (* old_R_tcldo)();
 
 void tcltk_start()
