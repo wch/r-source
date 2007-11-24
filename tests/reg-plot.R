50beb0c3607a39183b7262f6ea2b08bc581f5a17
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2007-11-24 13:56:33 +0000

    plot.ts(x,y) label fix
    git-svn-id: https://svn.r-project.org/R/trunk@43526 00db46b3-68df-0310-9c12-caf00c1e9a41

--- tests/reg-plot.R
+++ tests/reg-plot.R
@@ -127,3 +127,5 @@ op <- par(mar=c(5,4.123,4,2)+0.1)
 plot(gamma, yla = expression(Gamma(x)), n=11, yaxt="n")
 par(op)
 
+## plot.ts(x, y) could get the labels wrong in R <= 2.6.0:
+x <- ts(1:5);x1 <- lag(x, 2); plot(x1, x, axes=FALSE)
