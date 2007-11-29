76ea33730d06aa07a907de2fd0033fb22d2f66a0
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2007-11-29 16:56:59 +0000

    colnames() for data frames no longer accesses row names
    git-svn-id: https://svn.r-project.org/R/trunk@43561 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/matrix.R
+++ src/library/base/R/matrix.R
@@ -65,6 +65,8 @@ rownames <- function(x, do.NULL = TRUE, prefix = "row")
 
 colnames <- function(x, do.NULL = TRUE, prefix = "col")
 {
+    if(is.data.frame(x) && do.NULL)
+	return(names(x))
     dn <- dimnames(x)
     if(!is.null(dn[[2]]))
 	dn[[2]]
