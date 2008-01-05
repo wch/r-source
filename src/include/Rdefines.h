bf0287f67630009396bd7d6ea37beb3449cb1e23
jmc <jmc@00db46b3-68df-0310-9c12-caf00c1e9a41>
2008-01-05 20:15:41 +0000

    add macro SET_ATTR for consistency to the GET_*, SET_* macros
    git-svn-id: https://svn.r-project.org/R/trunk@43874 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/include/Rdefines.h
+++ src/include/Rdefines.h
@@ -127,6 +127,7 @@
 #define GET_LEVELS(x)       	getAttrib(x, R_LevelsSymbol)
 #define GET_TSP(x)       	getAttrib(x, R_TspSymbol)
 #define GET_NAMES(x)		getAttrib(x, R_NamesSymbol)
+#define SET_ATTR(x, what, n)     	setAttrib(x, what, n)
 #define SET_CLASS(x, n)     	setAttrib(x, R_ClassSymbol, n)
 #define SET_DIM(x, n)     	setAttrib(x, R_DimSymbol, n)
 #define SET_DIMNAMES(x, n)     	setAttrib(x, R_DimNamesSymbol, n)
