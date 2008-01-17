a2c83fcaddcd674a56b16b626f4924cce07bb4fb
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2008-01-17 17:51:09 +0000

    optimization for pch="."
    git-svn-id: https://svn.r-project.org/R/trunk@44029 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/grid/src/grid.c
+++ src/library/grid/src/grid.c
@@ -23,6 +23,7 @@
 #include "grid.h"
 #include <math.h>
 #include <float.h>
+#include <string.h>
 
 /* NOTE:
  * The extensive use of L or L_ prefixes dates back to when this 
@@ -2993,7 +2994,11 @@ SEXP L_points(SEXP x, SEXP y, SEXP pch, SEXP size)
 	    symbolSize = toDeviceWidth(symbolSize, GE_INCHES, dd);
 	    if (R_FINITE(symbolSize)) {
 	        if (isString(pch)) {
-		    ipch = Rf_string_to_pch(STRING_ELT(pch, i % npch));
+		    /* special case for efficiency: pch = "." */
+		    if(strcmp(CHAR(STRING_ELT(pch, i % npch)), ".") == 0)
+			ipch = 46;
+		    else
+			ipch = Rf_string_to_pch(STRING_ELT(pch, i % npch));
 		} else if (isInteger(pch)) {
 		    ipch = INTEGER(pch)[i % npch];
 		} else if (isReal(pch)) {
