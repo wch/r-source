ee0f2e56af896fee8e52e71647a6dfda7e0b4466
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2008-01-19 07:33:01 +0000

    widechar version of dir(), update omments
    git-svn-id: https://svn.r-project.org/R/trunk@44047 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/dir.c
+++ src/gnuwin32/dir.c
@@ -1,7 +1,6 @@
 /*
  *  R : A Computer Language for Statistical Data Analysis
- *  Copyright (C) 1995, 1996  Robert Gentleman and Ross Ihaka
- *  Copyright (C) 1998, 2001-8 The R Development Core Team
+ *  Copyright (C) 2001-8 The R Development Core Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -18,7 +17,7 @@
  *  http://www.r-project.org/Licenses/
  */
 
-/* A version of code in platform.c for Unix-alikes */
+/* A widechar version of code in platform.c for Unix-alikes */
 
 #ifdef HAVE_CONFIG_H
 # include <config.h>
@@ -27,54 +26,60 @@
 #include <Defn.h>
 #include <sys/stat.h>
 #include <dirent.h>
+#include <wchar.h>
+
+static Rboolean attribute_hidden R_HiddenWFile(const wchar_t *name)
+{
+    if (name && name[0] != L'.') return 0;
+    else return 1;
+}
 
 #define CBUFSIZE 2*PATH_MAX+1
-static SEXP filename(const char *dir, const char *file)
+static SEXP filename(const wchar_t *dir, const wchar_t *file)
 {
     SEXP ans;
-    char cbuf[CBUFSIZE];
+    char cbuf[2*CBUFSIZE];
+    wchar_t wb[CBUFSIZE];
+    int ienc= UTF8_MASK;
+
     if (dir) {
-	switch (dir[strlen(dir)-1])
+	switch (dir[wcslen(dir)-1])
 	{
-	case '/':
-	case '\\':
+	case L'/':
+	case L'\\':
 	case ':':
-	{
-	    snprintf(cbuf, CBUFSIZE, "%s%s", dir, file);
-	    ans = mkChar(cbuf);
+	    wcscpy(wb, dir); wcscat(wb, file);
 	    break;
-	}
 	default:
-	    snprintf(cbuf, CBUFSIZE, "%s/%s", dir, file);
-	    ans = mkChar(cbuf);
+	    wcscpy(wb, dir); wcscat(wb, L"/"); wcscat(wb, file);
     	}
-    } else {
-	snprintf(cbuf, CBUFSIZE, "%s", file);
-        ans = mkChar(cbuf);
-    }
+    } else wcscpy(wb, file);
+    wcstoutf8(cbuf, wb, wcslen(wb)+1);
+    if (utf8strIsASCII(cbuf)) ienc = 0;
+    ans = mkCharEnc(cbuf, ienc);
     return ans;
 }
 
-static void count_files(const char *dnp, int *count,
+static void count_files(const wchar_t *dnp, int *count,
 			Rboolean allfiles, Rboolean recursive)
 {
-    DIR *dir;
-    struct dirent *de;
-    char p[PATH_MAX];
+    _WDIR *dir;
+    struct _wdirent *de;
+    wchar_t p[PATH_MAX];
     struct _stati64 sb;
 
-    if (strlen(dnp) >= PATH_MAX)  /* should not happen! */
+    if (wcslen(dnp) >= PATH_MAX)  /* should not happen! */
 	error(_("directory/folder path name too long"));
-    if ((dir = opendir(dnp)) == NULL) {
+    if ((dir = _wopendir(dnp)) == NULL) {
 	warning(_("list.files: '%s' is not a readable directory"), dnp);
     } else {
-	while ((de = readdir(dir))) {
-	    if (allfiles || !R_HiddenFile(de->d_name)) {
+	while ((de = _wreaddir(dir))) {
+	    if (allfiles || !R_HiddenWFile(de->d_name)) {
 		if(recursive) {
-		    snprintf(p, PATH_MAX, "%s/%s", dnp, de->d_name);
-		    _stati64(p, &sb);
+		    wcscpy(p, dnp); wcscat(p, L"/"); wcscat(p, de->d_name);
+		    _wstati64(p, &sb);
 		    if((sb.st_mode & S_IFDIR) > 0) {
-			if (strcmp(de->d_name, ".") && strcmp(de->d_name, ".."))
+			if (wcscmp(de->d_name, L".") && wcscmp(de->d_name, L".."))
 				count_files(p, count, allfiles, recursive);
 			continue;
 		    }
@@ -82,32 +87,31 @@ static void count_files(const char *dnp, int *count,
 		(*count)++;
 	    }
 	}
-	closedir(dir);
+	_wclosedir(dir);
     }
 }
 
-static void list_files(const char *dnp, const char *stem, int *count, SEXP ans,
+static void list_files(const wchar_t *dnp, const wchar_t *stem, int *count, SEXP ans,
 		       Rboolean allfiles, Rboolean recursive)
 {
-    DIR *dir;
-    struct dirent *de;
-    char p[PATH_MAX], stem2[PATH_MAX];
+    _WDIR *dir;
+    struct _wdirent *de;
+    wchar_t p[PATH_MAX], stem2[PATH_MAX];
     struct _stati64 sb;
 
-    if ((dir = opendir(dnp)) != NULL) {
-	while ((de = readdir(dir))) {
-	    if (allfiles || !R_HiddenFile(de->d_name)) {
+    if ((dir = _wopendir(dnp)) != NULL) {
+	while ((de = _wreaddir(dir))) {
+	    if (allfiles || !R_HiddenWFile(de->d_name)) {
 		if(recursive) {
-		    snprintf(p, PATH_MAX, "%s/%s", dnp, de->d_name);
-		    _stati64(p, &sb);
+		    wcscpy(p, dnp); wcscat(p, L"/"); wcscat(p, de->d_name);
+		    _wstati64(p, &sb);
 		    if((sb.st_mode & S_IFDIR) > 0) {
-			if (strcmp(de->d_name, ".") && 
-			    strcmp(de->d_name, "..")) {
-			    if(stem)
-			    	snprintf(stem2, PATH_MAX, "%s/%s", stem,
-				         de->d_name);
-			    else
-			    	strcpy(stem2, de->d_name);
+			if (wcscmp(de->d_name, L".") && wcscmp(de->d_name, L"..")) {
+			    if(stem) {
+				wcscpy(stem2, stem);
+				wcscat(stem2, L"/"); 
+				wcscat(stem2, de->d_name);
+			    } else wcscpy(stem2, de->d_name);
 			    list_files(p, stem2, count, ans, allfiles, recursive);
 		    	}
 			continue;
@@ -116,7 +120,7 @@ static void list_files(const char *dnp, const char *stem, int *count, SEXP ans,
 		SET_STRING_ELT(ans, (*count)++, filename(stem, de->d_name));
 	    }
 	}
-	closedir(dir);
+	_wclosedir(dir);
     }
 }
 
@@ -125,7 +129,7 @@ SEXP attribute_hidden do_listfiles(SEXP call, SEXP op, SEXP args, SEXP rho)
     SEXP d, ans;
     Rboolean allfiles, fullnames, recursive;
     int i, count, ndir;
-    const char *dnp;
+    const wchar_t *dnp;
 
     checkArity(op, args);
     d = CAR(args);  args = CDR(args);
@@ -137,13 +141,13 @@ SEXP attribute_hidden do_listfiles(SEXP call, SEXP op, SEXP args, SEXP rho)
     ndir = length(d);
     count = 0;
     for (i = 0; i < ndir ; i++) {
-	dnp = R_ExpandFileName(translateChar(STRING_ELT(d, i)));
+	dnp = filenameToWchar(STRING_ELT(d, i), TRUE);
 	count_files(dnp, &count, allfiles, recursive);
     }
     PROTECT(ans = allocVector(STRSXP, count));
     count = 0;
     for (i = 0; i < ndir ; i++) {
-	dnp = R_ExpandFileName(translateChar(STRING_ELT(d, i)));
+	dnp = filenameToWchar(STRING_ELT(d, i), TRUE);
 	if (fullnames)
 	    list_files(dnp, dnp, &count, ans, allfiles, recursive);
 	else
