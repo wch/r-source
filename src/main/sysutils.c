f3631b51782c8852a04bdd4e03f94c24945cf6f2
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2008-01-26 05:50:36 +0000

    need to invalidate cached encodings
    git-svn-id: https://svn.r-project.org/R/trunk@44162 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/sysutils.c
+++ src/main/sysutils.c
@@ -1155,6 +1155,11 @@ const char *reEnc(const char *x, int ce_in, int ce_out)
 {
     return x;
 }
+
+void attribute_hidden
+invalidate_cached_recodings(void)
+{
+}
 #endif
 
 /* moved from src/unix/sys-unix.c and src/gnuwin32/extra.c */
