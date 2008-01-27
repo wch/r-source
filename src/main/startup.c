dfb645c7356f833bb51450e00b324dda5de3c467
murdoch <murdoch@00db46b3-68df-0310-9c12-caf00c1e9a41>
2008-01-27 15:22:52 +0000

    In Windows, a command-line specified workspace failed to open it if the Rprofile or Rprofile.site file changed the working directory.
    git-svn-id: https://svn.r-project.org/R/trunk@44187 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/startup.c
+++ src/main/startup.c
@@ -105,9 +105,12 @@ FILE *R_OpenSiteFile(void)
 
 	/* Saving and Restoring the Global Environment */
 
+#ifndef Win32
 static char workspace_name[100] = ".RData";
 
-#ifdef Win32
+#else
+static char workspace_name[MAX_PATH] = ".RData";
+
 void set_workspace_name(const char *fn)
 {
     strcpy(workspace_name, fn);
