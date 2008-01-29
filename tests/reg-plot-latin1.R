7727d91db64f22e1770ab95ce765ecc734c5d97a
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2008-01-29 11:39:10 +0000

    add separate test for example(text)
    git-svn-id: https://svn.r-project.org/R/trunk@44228 00db46b3-68df-0310-9c12-caf00c1e9a41

---
+++ tests/reg-plot-latin1.R
@@ -0,0 +1,6 @@
+postscript(file = "reg-plot-latin1.ps",
+           encoding = "ISOLatin1",
+           width = 7, height = 7)
+library(graphics) # to be sure
+example(text)     # has Latin-1 examples
+q("no")
