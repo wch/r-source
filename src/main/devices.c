6f5e9fe311a9f9bf7efb224ffe4d75ad612154cf
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2008-01-31 11:06:57 +0000

    need Rf_
    git-svn-id: https://svn.r-project.org/R/trunk@44270 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/devices.c
+++ src/main/devices.c
@@ -370,7 +370,7 @@ int ndevNumber(NewDevDesc *dd)
 
 
 /* Incorrectly declared old version */
-int devNumber(DevDesc *dd)
+int Rf_devNumber(DevDesc *dd)
 {
     return ndevNumber((NewDevDesc *) dd);
 }
