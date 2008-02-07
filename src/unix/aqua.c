ff1bc33c03112e6083088fe1271708daa4704e23
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2008-02-07 08:41:51 +0000

    last? NewDevDesc pointer
    git-svn-id: https://svn.r-project.org/R/trunk@44368 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/unix/aqua.c
+++ src/unix/aqua.c
@@ -1,6 +1,6 @@
 /* 
  *  R : A Computer Language for Statistical Data Analysis
- *  Copyright (C) 1999-2007 The R Development Core Team
+ *  Copyright (C) 1999-2008 The R Development Core Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -72,7 +72,7 @@ QuartzFunctions_t *getQuartzAPI(void) {
 }
 
 /* obsolete Quartz Cocoa call */
-Rboolean CocoaInnerQuartzDevice(NewDevDesc *dd,char *display,
+Rboolean CocoaInnerQuartzDevice(pDevDesc dd,char *display,
 				double width,double height,
 				double pointsize,char *family,
 				Rboolean antialias,
