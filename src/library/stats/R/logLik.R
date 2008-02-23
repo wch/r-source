01310c0bebf8aa0399593d5b48f8d52968c35097
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2008-02-23 20:31:13 +0000

    print.logLik() also correctly display *several* logLik values
    git-svn-id: https://svn.r-project.org/R/trunk@44590 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/R/logLik.R
+++ src/library/stats/R/logLik.R
@@ -18,7 +18,7 @@ logLik <- function(object, ...) UseMethod("logLik")
 
 print.logLik <- function(x, digits = getOption("digits"), ...)
 {
-    cat("'log Lik.' ",format(c(x), digits=digits),
+    cat("'log Lik.' ", paste(format(c(x), digits=digits), collapse=", "),
         " (df=",format(attr(x,"df")),")\n",sep="")
     invisible(x)
 }
