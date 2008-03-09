0f1723672a79c25628e4befe33aec50706c4985f
murdoch <murdoch@00db46b3-68df-0310-9c12-caf00c1e9a41>
2008-03-09 15:03:18 +0000

    Bring up to date with Windows Rgui interface change
    git-svn-id: https://svn.r-project.org/R/trunk@44706 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/unix/edit.c
+++ src/unix/edit.c
@@ -36,7 +36,7 @@
 #include <stdio.h>
 #ifdef Win32
 # include "run.h"
-int Rgui_Edit(char *filename, char *title, int modal);
+int Rgui_Edit(char *filename, int enc, char *title, int modal);
 #endif
 
 #ifdef Unix
@@ -149,7 +149,7 @@ SEXP attribute_hidden do_edit(SEXP call, SEXP op, SEXP args, SEXP rho)
 	    title = R_alloc(strlen(filename)+1, sizeof(char));
 	    strcpy(title, filename);
 	}
-	Rgui_Edit(filename, title, 1);
+	Rgui_Edit(filename, CE_NATIVE, title, 1);
     }
     else {
 	/* Quote path if necessary */
