d520b9c5ba9824d30d5efe2dd1af65f98bff40ff
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2008-04-01 16:08:20 +0000

    add text fallback in tkProgressBar
    git-svn-id: https://svn.r-project.org/R/trunk@45023 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tcltk/R/utils.R
+++ src/library/tcltk/R/utils.R
@@ -90,33 +90,48 @@ tk_select.list <-
 tkProgressBar <- function(title = "R progress bar", label = "",
                           min = 0, max = 1, initial = 0, width = 300)
 {
+    useText <- FALSE
     have_ttk <- as.character(tcl("info", "tclversion")) >= "8.5"
-    if(!have_ttk)
-        if(as.character(tclRequire("BWidget")) == "FALSE")
-            stop("either Tk >= 8.5 or BWidget is required")
+    if(!have_ttk && as.character(tclRequire("BWidget")) == "FALSE")
+        useText <- TRUE
+
 
     .win <- tktoplevel()
     .val <- initial
-    .tkval <- tclVar(0)
     .killed <- FALSE
 
     tkwm.geometry(.win, sprintf("%dx80", width+40))
     tkwm.title(.win, title)
     fn <- tkfont.create(family="helvetica", size=12)
 
-    .lab <- tklabel(.win, text=label, font = fn)
-    tkpack(.lab, side="top")
-    tkpack(tklabel(.win, text="", font = fn), side="bottom")
+    if(useText) {
+        .lab <- tklabel(.win, text=label, font=fn, padx=20)
+        tkpack(.lab, side = "left")
+        fn2 <- tkfont.create(family="helvetica", size=16)
+       .vlab <- tklabel(.win, text="0%", font=fn2, padx=20)
+        tkpack(.vlab, side = "right")
+        up <- function(value) {
+            if(!is.finite(value) || value < min || value > max) return()
+            .val <<- value
+             tkconfigure(.vlab,
+                         text=sprintf("%d%%",
+                         round(100*(value - min)/(max - min))))
+        }
+    } else {
+        .lab <- tklabel(.win, text=label, font=fn, pady=10)
+       .tkval <- tclVar(0)
+        tkpack(.lab, side="top")
+        tkpack(tklabel(.win, text="", font = fn), side="bottom")
 
-    pBar <- if(have_ttk) ttkprogressbar(.win, length=width, variable=.tkval) else tkwidget(.win, "ProgressBar", width=width, variable=.tkval)
-    tkpack(pBar, side="bottom")
-
-    getVal <- function() .val
-    up <- function(value) {
-        if(!is.finite(value) || value < min || value > max) return()
-        .val <<- value
-        tclvalue(.tkval) <<- 100*(value - min)/(max - min)
+        pBar <- if(have_ttk) ttkprogressbar(.win, length=width, variable=.tkval) else tkwidget(.win, "ProgressBar", width=width, variable=.tkval)
+        tkpack(pBar, side="bottom")
+        up <- function(value) {
+            if(!is.finite(value) || value < min || value > max) return()
+            .val <<- value
+            tclvalue(.tkval) <<- 100*(value - min)/(max - min)
+        }
     }
+    getVal <- function() .val
     kill <- function() if(!.killed) {tkdestroy(.win); .killed <<- TRUE}
     title <- function(title) tkwm.title(.win, title)
     lab <- function(label) tkconfigure(.lab, text=label)
