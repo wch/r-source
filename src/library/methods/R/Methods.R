3c2f67eb29cb0b3cf0d8339fa9c8faf5b567d3d4
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2008-04-02 10:23:51 +0000

    Fix getGeneric() calls with 'where' as second unnamed argument.
    git-svn-id: https://svn.r-project.org/R/trunk@45032 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/methods/R/Methods.R
+++ src/library/methods/R/Methods.R
@@ -1229,7 +1229,7 @@ setGenericImplicit <- function(name, where = topenv(parent.frame()), restore = T
                          name), domain = NA)
         return(FALSE)
     }
-    generic <- getGeneric(name, where)
+    generic <- getGeneric(name, where = where)
     if(restore)
         removeMethods(name, where)
     else
@@ -1350,7 +1350,10 @@ findMethods <- function(f, where, classes = character(), inherited = FALSE) {
         f <- fdef@generic
     }
     else if(is.character(f) && length(f) == 1) {
-        fdef <- getGeneric(f, where)
+        fdef <- if(missing(where))
+            getGeneric(f)
+        else 
+            getGeneric(f, where = where)
     }
     else if(!is(f, "function"))
         stop(gettextf("argument \"f\" must be a generic function or a single character string; got an object of class \"%s\"", class(f)), domain = NA)
