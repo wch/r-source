05d83d409d1181c5b60f3619610bbba0615ab021
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2008-04-14 12:36:25 +0000

    fix rcmd.exe to set TEXINPUTS like R CMD
    git-svn-id: https://svn.r-project.org/R/trunk@45321 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/front-ends/rcmdfn.c
+++ src/gnuwin32/front-ends/rcmdfn.c
@@ -1,6 +1,6 @@
 /*
  *  R : A Computer Language for Statistical Data Analysis
- *  Copyright (C) 2000-4  R Development Core Team
+ *  Copyright (C) 2000-8  R Development Core Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -76,7 +76,7 @@ int rcmdfn (int cmdarg, int argc, char **argv)
        set R_SHARE_DIR as env variable
        set PATH to include R_HOME\bin
        set PERL5LIB to %R_SHARE_DIR%/perl;%Perl5LIB%
-       set TEXINPUTS to %R_SHARE_DIR%/texmf;%TEXINPUTS%
+       set TEXINPUTS to .;%R_SHARE_DIR%/texmf;%TEXINPUTS%
        set HOME if unset
        launch %R_HOME%\bin\$*
      */
@@ -274,7 +274,7 @@ int rcmdfn (int cmdarg, int argc, char **argv)
 	if ( (p = getenv("PERL5LIB")) ) strcat(PERL5LIB, p);
 	putenv(PERL5LIB);
 
-	strcpy(TEXINPUTS, "TEXINPUTS=");
+	strcpy(TEXINPUTS, "TEXINPUTS=.;");
 	strcat(TEXINPUTS, RHome); strcat(TEXINPUTS, "\\share\\texmf;");
 	if ( (p = getenv("TEXINPUTS")) ) strcat(TEXINPUTS, p);
 	putenv(TEXINPUTS);
