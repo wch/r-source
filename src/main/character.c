ba7863fbd6625c587e182afe050acf48fc256b43
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2008-04-18 09:44:37 +0000

    back out substr changes for now
    git-svn-id: https://svn.r-project.org/R/trunk@45373 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/character.c
+++ src/main/character.c
@@ -179,11 +179,10 @@ SEXP attribute_hidden do_nchar(SEXP call, SEXP op, SEXP args, SEXP env)
     return s;
 }
 
-static int substr(char *buf, const char *str, int ienc, int sa, int so)
+static void substr(char *buf, const char *str, int ienc, int sa, int so)
 {
 /* Store the substring	str [sa:so]  into buf[] */
     int i, j, used;
-    char *b0 = buf;
 
     if (ienc == CE_UTF8) {
 	for (i = 0; i < so; i++) {
@@ -208,7 +207,6 @@ static int substr(char *buf, const char *str, int ienc, int sa, int so)
 	    for (str += (sa - 1), i = sa; i <= so; i++) *buf++ = *str++;
     }
     *buf = '\0';
-    return (buf - b0);
 }
 
 SEXP attribute_hidden do_substr(SEXP call, SEXP op, SEXP args, SEXP env)
@@ -244,17 +242,16 @@ SEXP attribute_hidden do_substr(SEXP call, SEXP op, SEXP args, SEXP env)
 	    }
 	    ienc = getCharCE(el);
 	    ss = CHAR(el);
-	    slen = LENGTH(el);
+	    slen = strlen(ss); /* FIXME -- should handle embedded nuls */
 	    buf = R_AllocStringBuffer(slen+1, &cbuff);
 	    if (start < 1) start = 1;
 	    if (start > stop || start > slen) {
 		buf[0] = '\0';
-		slen = 0;
 	    } else {
 		if (stop > slen) stop = slen;
-		slen = substr(buf, ss, ienc, start, stop);
+		substr(buf, ss, ienc, start, stop);
 	    }
-	    SET_STRING_ELT(s, i, mkCharLenCE(buf, slen, ienc));
+	    SET_STRING_ELT(s, i, mkCharCE(buf, ienc));
 	}
 	R_FreeStringBufferL(&cbuff);
     }
@@ -264,28 +261,26 @@ SEXP attribute_hidden do_substr(SEXP call, SEXP op, SEXP args, SEXP env)
     return s;
 }
 
-static int
-substrset(char *buf, int blen, const char *const str, int slen, 
-	  cetype_t ienc, int sa, int so)
+static void 
+substrset(char *buf, const char *const str, cetype_t ienc, int sa, int so)
 {
-    /* Replace the substring buf[sa:so] by str[], 
-       or as much as str provides */
-    int i, in = 0, out = 0, adj = 0;
+    /* Replace the substring buf[sa:so] by str[] */
+    int i, in = 0, out = 0;
 
     if (ienc == CE_UTF8) {
 	for (i = 1; i < sa; i++) buf += utf8clen(*buf);
 	for (i = sa; i <= so; i++) {
 	    in +=  utf8clen(str[in]);
 	    out += utf8clen(buf[out]);
-	    if (in >= slen) break;
+	    if (!str[in]) break;
 	}
-	if (in != out) memmove(buf+in, buf+out, blen-out+1);
-	adj = in - out;
+	if (in != out) memmove(buf+in, buf+out, strlen(buf+out)+1);
 	memcpy(buf, str, in);
     } else if (ienc == CE_LATIN1) {
-	in = slen;
+	in = strlen(str);
 	out = so - sa + 1;
 	memcpy(buf + sa - 1, str, (in < out) ? in : out);
+	memcpy(buf + sa - 1, str, so - sa + 1);
     } else {
 #ifdef SUPPORT_MBCS
 	/* This cannot work for stateful encodings */
@@ -295,20 +290,18 @@ substrset(char *buf, int blen, const char *const str, int slen,
 	    for (i = sa; i <= so; i++) {
 		in += Mbrtowc(NULL, str+in, MB_CUR_MAX, NULL);
 		out += Mbrtowc(NULL, buf+out, MB_CUR_MAX, NULL);
-		if (in >= slen) break;
+		if (!str[in]) break;
 	    }
-	    if (in != out) memmove(buf+in, buf+out, blen-out+1);
-	    adj = in - out;
+	    if (in != out) memmove(buf+in, buf+out, strlen(buf+out)+1);
 	    memcpy(buf, str, in);
 	} else
 #endif
 	{
-	    in = slen;
+	    in = strlen(str);
 	    out = so - sa + 1;
 	    memcpy(buf + sa - 1, str, (in < out) ? in : out);
 	}
     }
-    return blen + adj;
 }
 
 SEXP attribute_hidden do_substrgets(SEXP call, SEXP op, SEXP args, SEXP env)
@@ -350,34 +343,32 @@ SEXP attribute_hidden do_substrgets(SEXP call, SEXP op, SEXP args, SEXP env)
 	    }
 	    ienc = getCharCE(el);
 	    ss = CHAR(el);
-	    slen = LENGTH(el);
+	    slen = strlen(ss);
 	    if (start < 1) start = 1;
 	    if (stop > slen) stop = slen; /* SBCS optimization */
 	    if (start > stop) {
 		/* just copy element across */
 		SET_STRING_ELT(s, i, STRING_ELT(x, i));
 	    } else {
-		int ienc2 = ienc, vlen, olen;
+		int ienc2 = ienc;
 		v_ss = CHAR(v_el);
-		vlen = LENGTH(v_el);
 		/* is the value in the same encoding? */
 		venc = getCharCE(v_el);
 		if (venc != ienc && !strIsASCII(v_ss)) {
 		    ss = translateChar(el);
 		    slen = strlen(ss);
 		    v_ss = translateChar(v_el);
-		    vlen = strlen(v_ss);
 		    ienc2 = CE_NATIVE;
 		}
 #ifdef SUPPORT_MBCS
 		/* might expand under MBCS */
-		buf = R_AllocStringBuffer(slen+vlen, &cbuff);
+		buf = R_AllocStringBuffer(slen+strlen(v_ss), &cbuff);
 #else
 		buf = R_AllocStringBuffer(slen, &cbuff);
 #endif
-		memcpy(buf, ss, slen+1);
-		olen = substrset(buf, slen, v_ss, vlen, ienc2, start, stop);
-		SET_STRING_ELT(s, i, mkCharLenCE(buf, olen, ienc2));
+		strcpy(buf, ss);
+		substrset(buf, v_ss, ienc2, start, stop);
+		SET_STRING_ELT(s, i, mkCharCE(buf, ienc2));
 	    }
 	}
 	R_FreeStringBufferL(&cbuff);
