de3ff726b95c5f8ec5ce8d0960114e36ec936ba1
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2008-04-24 20:05:57 +0000

    port r45491 from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-2-7-branch@45492 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/console.c
+++ src/gnuwin32/console.c
@@ -1059,7 +1059,7 @@ void consolepaste(control c)
 	p->needredraw = 1;
 	REDRAW;
      }
-    if (p->kind == PAGER) return;;
+    if (p->kind == PAGER) return;
     if ( OpenClipboard(NULL) &&
 	 (hglb = GetClipboardData(CF_UNICODETEXT)) &&
 	 (pc = (wchar_t *) GlobalLock(hglb)))
@@ -1592,7 +1592,32 @@ static void draweditline(control c)
     }
 }
 
-/* Here multibyte characters will be entered as individual bytes */
+/* This needs to convert the nul-terminated wchar_t string 'in' to a
+   sensible strinf in buf[len].  It must not be empty, as R will
+   interpret that as EOF, and it should end in \n, as 'in' should do.
+
+   Our strategy is to convert character by character to the current
+   Windows locale, using \uxxxx escapes for invalid characters.
+*/
+
+static void wcstobuf(char *buf, int len, const wchar_t *in)
+{
+    int used, tot = 0;
+    char *p = buf, tmp[7];
+    const wchar_t *wc = in;
+    
+    for(; wc; wc++, p+=used, tot+=used) {
+	if(tot >= len - 2) break;
+	used = wctomb(p, *wc);
+	if (used < 0) {
+	    snprintf(tmp, 7, "\\u%x", *wc);
+	    used = strlen(tmp);
+	    memcpy(p, tmp, used);
+	}
+    }
+    *p++ = '\n'; *p = '\0';
+}
+
 int consolereads(control c, const char *prompt, char *buf, int len,
 		 int addtohistory)
 {
@@ -1734,8 +1759,9 @@ int consolereads(control c, const char *prompt, char *buf, int len,
 			cur_line[max_pos + 1] = L'\0';
 		    } else
 			cur_line[max_pos] = L'\0';
-		    /* just to be safe */
-		    sprintf(buf, "%ls", cur_line);
+		    wcstobuf(buf, len, cur_line);
+		    //sprintf(buf, "%ls", cur_line);
+		    //if(strlen(buf) == 0) strcpy(buf, "invalid input\n");
 		    p->r = -1;
 		    cur_line[max_pos] = L'\0';
 		    if (max_pos && addtohistory) wgl_histadd(cur_line);
