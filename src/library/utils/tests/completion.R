76a263aaa2741a79c13023f0cee017d5b98fe757
deepayan <deepayan@00db46b3-68df-0310-9c12-caf00c1e9a41>
2008-05-27 20:20:06 +0000

    added a set of tests for completion
    git-svn-id: https://svn.r-project.org/R/trunk@45799 00db46b3-68df-0310-9c12-caf00c1e9a41

---
+++ src/library/utils/tests/completion.R
@@ -0,0 +1,29 @@
+
+## test some typical completion attempts
+
+testLine <- function(line, cursor = nchar(line))
+{
+    str(utils:::.win32consoleCompletion(line, cursor))
+}
+
+testLine("")
+
+testLine("lib")
+testLine("data(")
+testLine("data(US")
+testLine("data(US", 3)
+
+testLine("?INS")
+
+testLine("utils::data")
+testLine("utils:::.show_help_on_topic_")
+testLine("utils::.show_help_on_topic_")
+
+testLine("update(")
+
+testLine("version$m")
+testLine("nchar(version[")
+
+## testLine("")
+## testLine("")
+## testLine("")
