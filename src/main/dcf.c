a93ee2f0dd40b4bb24c0130adf8f142117f72374
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2008-05-30 19:19:46 +0000

    Make read.dcf() always stop on invalid DCF.
    git-svn-id: https://svn.r-project.org/R/trunk@45818 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/dcf.c
+++ src/main/dcf.c
@@ -1,6 +1,6 @@
 /*
  *  R : A Computer Language for Statistical Data Analysis
- *  Copyright (C) 2001-7   The R Development Core Team.
+ *  Copyright (C) 2001-8   The R Development Core Team.
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -16,7 +16,6 @@
  *  along with this program; if not, a copy is available at
  *  http://www.r-project.org/Licenses/
  */
-
 
 #ifdef HAVE_CONFIG_H
 # include <config.h>
@@ -98,7 +97,8 @@ SEXP attribute_hidden do_readDCF(SEXP call, SEXP op, SEXP args, SEXP env)
 		   record. */
 		if(lastm == -1 && !field_skip) {
 		    line[20] = '\0';
-		    warning("Found continuation line starting '%s ...' at begin of record.", line);
+		    error(_("Found continuation line starting '%s ...' at begin of record."),
+			  line);
 		    continue;
 		}
 		if(lastm >= 0) {
@@ -179,8 +179,9 @@ SEXP attribute_hidden do_readDCF(SEXP call, SEXP op, SEXP args, SEXP env)
 				       mkChar(line + regmatch[0].rm_eo));
 		    }
 		} else {
+		    /* Must be a regular line with no tag ... */
 		    line[20] = '\0';
-		    warning("Line starting '%s ...' is malformed!", line);
+		    error(_("Line starting '%s ...' is malformed!"), line);
 		}
 	    }
 	}
