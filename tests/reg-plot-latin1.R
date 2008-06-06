09b674911a3f384d78fe2a32480c0178f44ce1cb
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2008-06-06 08:03:22 +0000

    Allow testing latin-1 plotting to fail gracefully in locales which   do not support latin-1 characters. Do package testing on Windows in current locale, not in C (for consistency   with Unix).
    git-svn-id: https://svn.r-project.org/R/trunk@45867 00db46b3-68df-0310-9c12-caf00c1e9a41

--- tests/reg-plot-latin1.R
+++ tests/reg-plot-latin1.R
@@ -1,6 +1,6 @@
 postscript(file = "reg-plot-latin1.ps",
            encoding = "ISOLatin1",
-           width = 7, height = 7)
+           width = 7, height = 7, paper = "a4")
 library(graphics) # to be sure
 example(text)     # has Latin-1 examples
 q("no")
