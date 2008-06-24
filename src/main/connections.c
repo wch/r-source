ddedd33beb1b82dbd6ce2da493e7a3bdb0e90729
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2008-06-24 12:49:27 +0000

    port r45979 from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-2-7-branch@45980 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/connections.c
+++ src/main/connections.c
@@ -3676,7 +3676,7 @@ SEXP attribute_hidden do_clearpushback(SEXP call, SEXP op, SEXP args, SEXP env)
 
 /* ------------------- sink functions  --------------------- */
 
-/* Switch output to connection number icon, or popd stack if icon < 0
+/* Switch output to connection number icon, or pop stack if icon < 0
  */
 
 static Rboolean
@@ -3697,10 +3697,17 @@ switch_or_tee_stdout(int icon, int closeOnExit, int tee)
 	SinkConsClose[R_SinkNumber] = 0;
     } else if(icon >= 3) {
 	Rconnection con = getConnection(icon); /* checks validity */
+	if(!con->canwrite)
+	    error(_("connection is not writable"));
 	toclose = 2*closeOnExit;
 	if(!con->isopen) {
 	    if(!con->open(con)) error(_("cannot open the connection"));
 	    toclose = 1;
+	    /* Once open, writability may have changed */
+	    if(!con->canwrite) {
+		con->close(con);
+		error(_("connection is not writable"));
+	    }
 	}
 	R_OutputCon = SinkCons[++R_SinkNumber] = icon;
 	SinkConsClose[R_SinkNumber] = toclose;
@@ -3728,7 +3735,7 @@ switch_or_tee_stdout(int icon, int closeOnExit, int tee)
 /* This is not only used by cat(), but is in a public
    header, so we need a wrapper
 
-   Mo, Rconnections.h is not public and not installed.
+   No, Rconnections.h is not public and not installed.
 */
 
 Rboolean attribute_hidden switch_stdout(int icon, int closeOnExit)
