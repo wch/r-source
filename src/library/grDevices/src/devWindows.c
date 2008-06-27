21d3273e29dbd6e56ab58178ee9ca52f600e67b8
murdoch <murdoch@00db46b3-68df-0310-9c12-caf00c1e9a41>
2008-06-27 13:47:26 +0000

    Port r46007 from trunk.
    git-svn-id: https://svn.r-project.org/R/branches/R-2-7-branch@46008 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/grDevices/src/devWindows.c
+++ src/library/grDevices/src/devWindows.c
@@ -1659,6 +1659,7 @@ setupScreenDevice(pDevDesc dd, gadesc *xd, double w, double h,
     setredraw(xd->gawin, HelpExpose);
     setmousedown(xd->gawin, HelpMouseClick);
     setmousemove(xd->gawin, HelpMouseMove);
+    setmousedrag(xd->gawin, HelpMouseMove);
     setmouseup(xd->gawin, HelpMouseUp);
     setkeydown(xd->gawin, NHelpKeyIn);
     setkeyaction(xd->gawin, CHelpKeyIn);
