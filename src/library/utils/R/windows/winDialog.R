208e8afec78fe12a2e41e33350121d79a66e16b0
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2008-07-03 13:02:55 +0000

    add interactivity check to winDialog[String]
    git-svn-id: https://svn.r-project.org/R/trunk@46027 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/utils/R/windows/winDialog.R
+++ src/library/utils/R/windows/winDialog.R
@@ -17,6 +17,8 @@
 winDialog <- function(type = c("ok", "okcancel", "yesno", "yesnocancel"),
                        message)
 {
+    if (!interactive())
+        stop("winDialog() cannot be used non-interactively")
     type <- match.arg(type)
     res <- .Internal(winDialog(type, message))
     if(res == 10) return(invisible(NULL))
@@ -24,7 +26,11 @@ winDialog <- function(type = c("ok", "okcancel", "yesno", "yesnocancel"),
 }
 
 winDialogString <- function(message, default)
+{
+    if (!interactive())
+        stop("winDialogString() cannot be used non-interactively")
     .Internal(winDialogString(message, default))
+}
 
 winMenuDel <- function(menuname)
     invisible(.Internal(winMenuDel(menuname, NULL)))
