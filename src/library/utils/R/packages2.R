411118856387287298e4d10fd28230c80ae68795
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2008-07-18 15:59:15 +0000

    install.packages():   rationalize OS-switch inside if(missing(pkgs) ...) ...
    git-svn-id: https://svn.r-project.org/R/trunk@46079 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/utils/R/packages2.R
+++ src/library/utils/R/packages2.R
@@ -79,28 +79,23 @@ install.packages <-
     }
 
     if(missing(pkgs) || !length(pkgs)) {
-        if(.Platform$OS.type == "windows" || .Platform$GUI == "AQUA") {
-            if(is.null(available))
-                available <- available.packages(contriburl = contriburl,
-                                                method = method)
-            if(NROW(available)) {
-                a <- explode_bundles(available)
-                pkgs <- implode_bundles(select.list(a, multiple = TRUE, title = "Packages"))
-            }
-            if(!length(pkgs)) stop("no packages were specified")
-        } else if(.Platform$OS.type == "unix" &&
-                  capabilities("tcltk") && capabilities("X11")) {
-            if(is.null(available))
-                available <- available.packages(contriburl = contriburl,
-                                                method = method)
-            if(NROW(available)) {
-                a <- explode_bundles(available)
-                pkgs <- implode_bundles(tcltk::tk_select.list(a, multiple = TRUE,
-                                              title ="Packages"))
-            }
-            if(!length(pkgs)) stop("no packages were specified")
-        } else
-            stop("no packages were specified")
+	if(.Platform$OS.type == "windows" || .Platform$GUI == "AQUA") {
+	    SelectList <- select.list
+	} else if(.Platform$OS.type == "unix" &&
+		  capabilities("tcltk") && capabilities("X11")) {
+	    SelectList <- tcltk::tk_select.list
+	} else
+	    stop("no packages were specified")
+
+	if(is.null(available))
+	    available <- available.packages(contriburl = contriburl,
+					    method = method)
+	if(NROW(available)) {
+	    a <- explode_bundles(available)
+	    pkgs <- implode_bundles(SelectList(a, multiple = TRUE,
+					       title = "Packages"))
+	}
+	if(!length(pkgs)) stop("no packages were specified")
     }
 
     if(missing(lib) || is.null(lib)) {
