7dc4deed04ad24eedf2c2f7eff3edac361d6565f
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2008-07-28 14:08:13 +0000

    add box.col function to legend() layout issues in NEWS
    git-svn-id: https://svn.r-project.org/R/trunk@46137 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/graphics/R/legend.R
+++ src/library/graphics/R/legend.R
@@ -17,7 +17,7 @@
 legend <-
 function(x, y = NULL, legend, fill=NULL, col = par("col"), lty, lwd, pch,
 	 angle = 45, density = NULL, bty = "o", bg = par("bg"),
-         box.lwd = par("lwd"), box.lty = par("lty"),
+         box.lwd = par("lwd"), box.lty = par("lty"), box.col = par("fg"),
 	 pt.bg = NA, cex = 1, pt.cex = cex, pt.lwd = lwd,
 	 xjust = 0, yjust = 1, x.intersp = 1, y.intersp = 1, adj = c(0, 0.5),
 	 text.width = NULL, text.col = par("col"),
@@ -193,7 +193,7 @@ function(x, y = NULL, legend, fill=NULL, col = par("col"), lty, lwd, pch,
 	if(trace)
 	    catn("  rect2(",left,",",top,", w=",w,", h=",h,", ...)",sep="")
 	rect2(left, top, dx = w, dy = h, col = bg, density = NULL,
-              lwd = box.lwd, lty = box.lty)
+              lwd = box.lwd, lty = box.lty, border = box.col)
     }
 
     ## (xt[],yt[]) := `current' vectors of (x/y) legend text
