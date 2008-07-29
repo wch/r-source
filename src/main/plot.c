46e30f5fceae57375911bbc03533ea118b08dff8
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2008-07-29 15:26:20 +0000

    port r46156 from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-2-7-branch@46157 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/plot.c
+++ src/main/plot.c
@@ -959,11 +959,7 @@ SEXP attribute_hidden do_axis(SEXP call, SEXP op, SEXP args, SEXP env)
      * The values in the par value "mgp" are interpreted
      * relative to this value. */
     line = asReal(CAR(args));
-    if (!R_FINITE(line)) {
-	/* Except that here mgp values are not relative to themselves */
-	line = gpptr(dd)->mgp[2];
-	lineoff = line;
-    }
+    /* defer processing until after in-line pars */
     args = CDR(args);
 
     /* Optional argument: "pos" */
@@ -972,7 +968,7 @@ SEXP attribute_hidden do_axis(SEXP call, SEXP op, SEXP args, SEXP env)
     /* are interpreted relative to this value. */
 
     pos = asReal(CAR(args));
-    if (!R_FINITE(pos)) pos = NA_REAL; else lineoff = 0;
+    /* defer processing until after in-line pars */
     args = CDR(args);
 
     /* Optional argument: "outer" */
@@ -1050,6 +1046,14 @@ SEXP attribute_hidden do_axis(SEXP call, SEXP op, SEXP args, SEXP env)
 	nint = dpptr(dd)->lab[1];
 	break;
     }
+
+    /* Deferred processing */
+    if (!R_FINITE(line)) {
+	/* Except that here mgp values are not relative to themselves */
+	line = gpptr(dd)->mgp[2];
+	lineoff = line;
+    }
+    if (!R_FINITE(pos)) pos = NA_REAL; else lineoff = 0;
 
     /* Determine the tickmark positions.  Note that these may fall */
     /* outside the plot window. We will clip them in the code below. */
