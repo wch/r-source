b6667ab0ccce9bcaff9f247426ef790706ac9508
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2008-08-08 05:23:29 +0000

    fix for PR#11988
    git-svn-id: https://svn.r-project.org/R/trunk@46258 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/platform.c
+++ src/main/platform.c
@@ -1270,7 +1270,7 @@ void R_CleanTempDir(void)
     }
 }
 #else
-static int R_unlink(char *name, int recursive)
+static int R_unlink(const char *name, int recursive)
 {
     if (streql(name, ".") || streql(name, "..")) return 0;
     if (!R_FileExists(name)) return 0;
@@ -1354,9 +1354,10 @@ SEXP attribute_hidden do_unlink(SEXP call, SEXP op, SEXP args, SEXP env)
 SEXP attribute_hidden do_unlink(SEXP call, SEXP op, SEXP args, SEXP env)
 {
     SEXP  fn;
-    int i, j, nfiles, res, failures = 0, recursive;
+    int i, nfiles, failures = 0, recursive;
     const char *names;
 #if defined(HAVE_GLOB)
+    int j, res;
     glob_t globbuf;
 #endif
 
@@ -1387,7 +1388,8 @@ SEXP attribute_hidden do_unlink(SEXP call, SEXP op, SEXP args, SEXP env)
 		globfree(&globbuf);
 	    } else failures++;
 #else /* HAVE_GLOB */
-	    failures += R_unlink(names, recursive);
+	        failures += R_unlink(names, recursive);
+	    } else failures++;
 #endif
 	}
     }
