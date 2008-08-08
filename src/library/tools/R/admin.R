905755388611e6d690a2880e557d7195dedbcbee
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2008-08-08 16:20:23 +0000

    port r46266:7 from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-2-7-branch@46268 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/admin.R
+++ src/library/tools/R/admin.R
@@ -705,7 +705,7 @@ function(dir, packages)
         if(is.na(tmp)) return(invisible())
         pkgs <- tmp
     }
-    pkgs <- strsplit(pkgs[1L], ",[:blank]*")[[1L]]
+    pkgs <- strsplit(pkgs[1L], ",[[:blank:]]*")[[1L]]
     paths <- .find.package(pkgs, lib.loc, quiet=TRUE)
     if(length(paths))
         cat(paste(paste('-I"', paths, '/include"', sep=""), collapse=" "))
