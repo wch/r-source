9177f8e8f307068469f0189ac0b650d27ade696a
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2008-08-21 09:24:28 +0000

    median(<named NA>) fix
    git-svn-id: https://svn.r-project.org/R/trunk@46395 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/R/median.R
+++ src/library/stats/R/median.R
@@ -19,6 +19,7 @@ median <- function(x, na.rm=FALSE) UseMethod("median")
 median.default <- function(x, na.rm = FALSE)
 {
     if(is.factor(x) || mode(x) != "numeric") stop("need numeric data")
+    if(length(names(x))) names(x) <- NULL # for e.g., c(x = NA_real_)
     if(na.rm) x <- x[!is.na(x)] else if(any(is.na(x))) return(x[FALSE][NA])
     n <- length(x)
     if (n == 0L) return(x[FALSE][NA])
