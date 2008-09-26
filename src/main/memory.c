60bc8a4b686e38378f31cd8cdc2a4f1acd913959
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2008-09-26 17:03:06 +0000

    Bumped up NAMED for expresions in mkPromise and substitute to ensure code does not get accidentally modified.
    git-svn-id: https://svn.r-project.org/R/trunk@46573 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/memory.c
+++ src/main/memory.c
@@ -1839,6 +1839,10 @@ SEXP attribute_hidden mkPROMISE(SEXP expr, SEXP rho)
 #if VALGRIND_LEVEL > 2
     VALGRIND_MAKE_READABLE(s,sizeof(*s));
 #endif
+    /* precaution to ensure code does not get modified via
+       substitute() and the like */ 
+    if (NAMED(expr) < 2) SET_NAMED(expr, 2);
+
     s->sxpinfo = UnmarkedNodeTemplate.sxpinfo;
     TYPEOF(s) = PROMSXP;
     PRCODE(s) = expr;
