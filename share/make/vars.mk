df7bbc4bf1b3531a54e9118233b40419559792c2
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2008-09-29 12:03:55 +0000

    Matrix: now a recommended package
    git-svn-id: https://svn.r-project.org/R/trunk@46577 00db46b3-68df-0310-9c12-caf00c1e9a41

--- share/make/vars.mk
+++ share/make/vars.mk
@@ -1,9 +1,15 @@
 ## Static (i.e., not determined by configure) Make variables to be
-## shared between 'Makeconf' and 'etc/Makeconf'.
+## shared between '../../Makeconf' and '../../etc/Makeconf'
+## *and* grepped from '../../m4/R.m4'
 
 R_PKGS_BASE = base tools utils grDevices graphics stats datasets methods grid splines stats4 tcltk
 R_PKGS_STUBS = ctest eda lqs mle modreg mva nls stepfun ts
-# mgcv requires nlme and MASS
-R_PKGS_RECOMMENDED_SOURCES = VR boot cluster codetools foreign KernSmooth lattice nlme mgcv rpart survival
-R_PKGS_RECOMMENDED = boot cluster codetools foreign KernSmooth lattice nlme mgcv rpart survival MASS class nnet spatial
 
+# FIXME: cannot use common make variable,
+# R_PKGS_RECOMMENDED_common = boot cluster codetools foreign KernSmooth lattice Matrix nlme mgcv rpart survival
+# R_PKGS_RECOMMENDED_SOURCES = VR $(R_PKGS_RECOMMENDED_common)
+# R_PKGS_RECOMMENDED = $(R_PKGS_RECOMMENDED_common) MASS class nnet spatial
+#  as ../../m4/R.m4  greps the *_SOURCES line
+R_PKGS_RECOMMENDED_SOURCES = VR boot cluster codetools foreign KernSmooth lattice Matrix nlme mgcv rpart survival
+R_PKGS_RECOMMENDED =            boot cluster codetools foreign KernSmooth lattice Matrix nlme mgcv rpart survival MASS class nnet spatial
+# mgcv requires nlme and MASS
