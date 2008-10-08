905c022d438ef895e38d0d16ba7bc1103c11dade
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2008-10-08 13:57:56 +0000

    port r46654/60 from R-2-8-branch
    git-svn-id: https://svn.r-project.org/R/trunk@46661 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/R/ecdf.R
+++ src/library/stats/R/ecdf.R
@@ -68,8 +68,8 @@ print.summary.ecdf <- function(x, ...)
 ## and use ./KS-confint.R 's  code !!!
 
 plot.ecdf <- function(x, ..., ylab="Fn(x)", verticals = FALSE,
-		      col.01line = "gray70")
+		      col.01line = "gray70", pch = 19)
 {
-    plot.stepfun(x, ..., ylab = ylab, verticals = verticals)
-    abline(h=c(0,1), col = col.01line, lty=2)
+    plot.stepfun(x, ..., ylab = ylab, verticals = verticals, pch = pch)
+    abline(h = c(0,1), col = col.01line, lty = 2)
 }
