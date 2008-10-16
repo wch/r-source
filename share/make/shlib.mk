923e58afd33c54434903d5e3702515f428ed3889
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2008-10-16 09:50:00 +0000

    AIX fixes.
    git-svn-id: https://svn.r-project.org/R/trunk@46725 00db46b3-68df-0310-9c12-caf00c1e9a41

--- share/make/shlib.mk
+++ share/make/shlib.mk
@@ -2,11 +2,10 @@
 
 include $(R_HOME)/etc${R_ARCH}/Makeconf
 
-.PHONY: shlib-clean
-
 $(SHLIB): $(OBJECTS)
 	$(SHLIB_LINK) -o $@ $(OBJECTS) $(ALL_LIBS)
 
+.PHONY: shlib-clean
 shlib-clean:
 	@rm -rf .libs _libs
 	@rm -f $(OBJECTS)
