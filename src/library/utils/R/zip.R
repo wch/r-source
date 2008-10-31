8833953b44d6b81fa688304b4b270de1cb7cebde
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2008-10-31 16:32:37 +0000

    zip.file.extract(*, dir=tempdir())
    git-svn-id: https://svn.r-project.org/R/trunk@46800 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/utils/R/zip.R
+++ src/library/utils/R/zip.R
@@ -15,7 +15,7 @@
 #  http://www.r-project.org/Licenses/
 
 zip.file.extract <- function(file, zipname = "R.zip",
-                             unzip = getOption("unzip"))
+			     unzip = getOption("unzip"), dir = tempdir())
 {
     if(!is.character(unzip) || length(unzip) != 1)
         stop("'unzip' must be a single character string")
@@ -23,19 +23,18 @@ zip.file.extract <- function(file, zipname = "R.zip",
     path <- dirname(file)
     topic <- basename(file)
     if(file.exists(file.path(path, zipname))) {
-        tmpd <- tempdir()
         if(unzip != "internal") {
             cmd <- paste(unzip, "-oq", shQuote(file.path(path, zipname)),
-                         topic, " -d ", tmpd)
+                         topic, " -d ", dir)
             ## there is an unzip clone about that does not respect -q, so
             ## use redirection here.
             res <- if(.Platform$OS.type == "windows")
                 system(cmd, invisible = TRUE) else system(paste(cmd, "> /dev/null"))
-            if(!res) file <- file.path(tmpd, topic)
+            if(!res) file <- file.path(dir, topic)
         } else {
-            rc <- .Internal(int.unzip(file.path(path, zipname), topic, tmpd))
+            rc <- .Internal(int.unzip(file.path(path, zipname), topic, dir))
             if (rc == 0)
-                file <- file.path(tmpd, topic)
+                file <- file.path(dir, topic)
         }
     }
     file
