6b37b61a9d47a8b08dbcd3a305b8acb0f7a60234
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2008-12-11 08:09:36 +0000

    deprecated in 2.4.0 -> defunct
    git-svn-id: https://svn.r-project.org/R/trunk@47140 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/R/family.R
+++ src/library/stats/R/family.R
@@ -46,93 +46,84 @@ power <- function(lambda = 1)
 ## added valideta(eta) function..
 make.link <- function (link)
 {
-    ## first two deprecated in 2.4.0
-    if (is.character(link) && length(grep("^power", link) > 0)) {
-        ## this seems intended to catch calls like "power(3)"
-        warning('calling make.link("power(z)") is deprecated', domain = NA)
-        return(eval(parse(text = link)))
-    } else if(!is.character(link) && !is.na(lambda <- as.numeric(link))) {
-        warning('calling make.link(number) is deprecated', domain = NA)
-        return(power(lambda))
-    } else
-        switch(link,
-               "logit" = {
-                   linkfun <- function(mu)
-                       .Call("logit_link", mu, PACKAGE="stats")
-                   linkinv <- function(eta)
-                       .Call("logit_linkinv", eta, PACKAGE="stats")
-                   mu.eta <- function(eta)
-                       .Call("logit_mu_eta", eta, PACKAGE="stats")
-                   valideta <- function(eta) TRUE
-               },
-               "probit" = {
-                   linkfun <- function(mu) qnorm(mu)
-                   linkinv <- function(eta) {
-                       thresh <- - qnorm(.Machine$double.eps)
-                       eta <- pmin(pmax(eta, -thresh), thresh)
-                       pnorm(eta)
-                   }
-                   mu.eta <- function(eta)
-                       pmax(dnorm(eta),.Machine$double.eps)
-                   valideta <- function(eta) TRUE
-               },
-               "cauchit" = {
-         	   linkfun <- function(mu) qcauchy(mu)
-         	   linkinv <- function(eta) {
-                       thresh <- -qcauchy(.Machine$double.eps)
-                       eta <- pmin(pmax(eta, -thresh), thresh)
-                       pcauchy(eta)
-         	   }
-         	   mu.eta <- function(eta)
-                       pmax(dcauchy(eta), .Machine$double.eps)
-         	   valideta <- function(eta) TRUE
- 		},
-               "cloglog" = {
-                   linkfun <- function(mu) log(-log(1 - mu))
-                   linkinv <- function(eta)
-                       pmax(pmin(-expm1(-exp(eta)), 1 - .Machine$double.eps),
-                            .Machine$double.eps)
-                   mu.eta <- function(eta) {
-                       eta <- pmin(eta, 700)
-                       pmax(exp(eta) * exp(-exp(eta)), .Machine$double.eps)
-                   }
-                   valideta <- function(eta) TRUE
-               },
-               "identity" = {
-                   linkfun <- function(mu) mu
-                   linkinv <- function(eta) eta
-                   mu.eta <- function(eta) rep.int(1, length(eta))
-                   valideta <- function(eta) TRUE
-               },
-               "log" = {
-                   linkfun <- function(mu) log(mu)
-                   linkinv <- function(eta)
-                       pmax(exp(eta), .Machine$double.eps)
-                   mu.eta <- function(eta)
-                       pmax(exp(eta), .Machine$double.eps)
-                   valideta <- function(eta) TRUE
-               },
-               "sqrt" = {
-                   linkfun <- function(mu) sqrt(mu)
-                   linkinv <- function(eta) eta^2
-                   mu.eta <- function(eta) 2 * eta
-                   valideta <- function(eta) all(eta>0)
-               },
-               "1/mu^2" = {
-                   linkfun <- function(mu) 1/mu^2
-                   linkinv <- function(eta) 1/sqrt(eta)
-                   mu.eta <- function(eta) -1/(2 * eta^1.5)
-                   valideta <- function(eta) all(eta>0)
-               },
-               "inverse" = {
-                   linkfun <- function(mu) 1/mu
-                   linkinv <- function(eta) 1/eta
-                   mu.eta <- function(eta) -1/(eta^2)
-                   valideta <- function(eta) all(eta!=0)
-               },
-               ## else :
-               stop(sQuote(link), " link not recognised")
-               )# end switch(.)
+    switch(link,
+           "logit" = {
+               linkfun <- function(mu)
+                   .Call("logit_link", mu, PACKAGE="stats")
+               linkinv <- function(eta)
+                   .Call("logit_linkinv", eta, PACKAGE="stats")
+               mu.eta <- function(eta)
+                   .Call("logit_mu_eta", eta, PACKAGE="stats")
+               valideta <- function(eta) TRUE
+           },
+           "probit" = {
+               linkfun <- function(mu) qnorm(mu)
+               linkinv <- function(eta) {
+                   thresh <- - qnorm(.Machine$double.eps)
+                   eta <- pmin(pmax(eta, -thresh), thresh)
+                   pnorm(eta)
+               }
+               mu.eta <- function(eta)
+                   pmax(dnorm(eta),.Machine$double.eps)
+               valideta <- function(eta) TRUE
+           },
+           "cauchit" = {
+               linkfun <- function(mu) qcauchy(mu)
+               linkinv <- function(eta) {
+                   thresh <- -qcauchy(.Machine$double.eps)
+                   eta <- pmin(pmax(eta, -thresh), thresh)
+                   pcauchy(eta)
+               }
+               mu.eta <- function(eta)
+                   pmax(dcauchy(eta), .Machine$double.eps)
+               valideta <- function(eta) TRUE
+           },
+           "cloglog" = {
+               linkfun <- function(mu) log(-log(1 - mu))
+               linkinv <- function(eta)
+                   pmax(pmin(-expm1(-exp(eta)), 1 - .Machine$double.eps),
+                        .Machine$double.eps)
+               mu.eta <- function(eta) {
+                   eta <- pmin(eta, 700)
+                   pmax(exp(eta) * exp(-exp(eta)), .Machine$double.eps)
+               }
+               valideta <- function(eta) TRUE
+           },
+           "identity" = {
+               linkfun <- function(mu) mu
+               linkinv <- function(eta) eta
+               mu.eta <- function(eta) rep.int(1, length(eta))
+               valideta <- function(eta) TRUE
+           },
+           "log" = {
+               linkfun <- function(mu) log(mu)
+               linkinv <- function(eta)
+                   pmax(exp(eta), .Machine$double.eps)
+               mu.eta <- function(eta)
+                   pmax(exp(eta), .Machine$double.eps)
+               valideta <- function(eta) TRUE
+           },
+           "sqrt" = {
+               linkfun <- function(mu) sqrt(mu)
+               linkinv <- function(eta) eta^2
+               mu.eta <- function(eta) 2 * eta
+               valideta <- function(eta) all(eta>0)
+           },
+           "1/mu^2" = {
+               linkfun <- function(mu) 1/mu^2
+               linkinv <- function(eta) 1/sqrt(eta)
+               mu.eta <- function(eta) -1/(2 * eta^1.5)
+               valideta <- function(eta) all(eta>0)
+           },
+           "inverse" = {
+               linkfun <- function(mu) 1/mu
+               linkinv <- function(eta) 1/eta
+               mu.eta <- function(eta) -1/(eta^2)
+               valideta <- function(eta) all(eta!=0)
+           },
+           ## else :
+           stop(sQuote(link), " link not recognised")
+           )# end switch(.)
     structure(list(linkfun = linkfun, linkinv = linkinv,
                    mu.eta = mu.eta, valideta = valideta, name = link),
               class="link-glm")
