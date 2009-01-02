6e756977a19141e6cdbc7f63f54d5114d43d24bd
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2009-01-02 07:49:58 +0000

    more cleaning up after versioned installs
    git-svn-id: https://svn.r-project.org/R/trunk@47434 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/utils/R/packages2.R
+++ src/library/utils/R/packages2.R
@@ -408,7 +408,7 @@ install.packages <-
         }
         if(!is.null(tmpd) && is.null(destdir))
             cat("\n", gettextf("The downloaded packages are in\n\t%s",
-                               normalizePath(tmpd)), "\n", sep = "")
+                               sQuote(normalizePath(tmpd))), "\n", sep = "")
         ## update packages.html on Unix only if .Library was installed into
         libs_used <- unique(update[, 2L])
         if(.Platform$OS.type == "unix" && .Library %in% libs_used)
