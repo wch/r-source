8a39cc09390915a842ab7fa5d17b08e35067b372
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2009-01-10 16:17:54 +0000

    compress .afm files on all platforms
    git-svn-id: https://svn.r-project.org/R/trunk@47536 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/grDevices/src/devPS.c
+++ src/library/grDevices/src/devPS.c
@@ -57,9 +57,7 @@ static void mbcsToSbcs(const char *in, char *out, const char *encoding, int enc)
 extern int errno;
 #endif
 
-#ifdef Win32
-# define USE_GZIO
-#endif
+#define USE_GZIO
 
 #ifdef USE_GZIO
 #include <zlib.h>
