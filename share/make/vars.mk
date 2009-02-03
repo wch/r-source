5f73db947ea918153f3fe4db210d1f8b0a5a280a
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2009-02-03 09:15:06 +0000

    move Matrix to the end as it is liable to fail
    git-svn-id: https://svn.r-project.org/R/trunk@47822 00db46b3-68df-0310-9c12-caf00c1e9a41

--- share/make/vars.mk
+++ share/make/vars.mk
@@ -10,6 +10,6 @@ R_PKGS_STUBS = ctest eda lqs mle modreg mva nls stepfun ts
 # R_PKGS_RECOMMENDED_SOURCES = VR $(R_PKGS_RECOMMENDED_common)
 # R_PKGS_RECOMMENDED = $(R_PKGS_RECOMMENDED_common) MASS class nnet spatial
 #  as ../../m4/R.m4  greps the *_SOURCES line
-R_PKGS_RECOMMENDED_SOURCES = VR boot cluster codetools foreign KernSmooth lattice Matrix nlme mgcv rpart survival
-R_PKGS_RECOMMENDED =            boot cluster codetools foreign KernSmooth lattice Matrix nlme mgcv rpart survival MASS class nnet spatial
+R_PKGS_RECOMMENDED_SOURCES = VR boot cluster codetools foreign KernSmooth lattice nlme mgcv rpart survival Matrix
+R_PKGS_RECOMMENDED =            boot cluster codetools foreign KernSmooth lattice nlme mgcv rpart survival MASS class nnet spatial Matrix
 # mgcv requires nlme and MASS
