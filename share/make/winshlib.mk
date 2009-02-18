3ebd185c0745bdc7cb8dd185bd7df5ff7f827f18
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2009-02-18 13:32:50 +0000

    tweak
    git-svn-id: https://svn.r-project.org/R/trunk@47953 00db46b3-68df-0310-9c12-caf00c1e9a41

--- share/make/winshlib.mk
+++ share/make/winshlib.mk
@@ -3,7 +3,13 @@
 include $(R_HOME)/etc${R_ARCH}/Makeconf
 
 all: before $(SHLIB) after
+
+## do it this way as packages might add dependencies to this target
 $(SHLIB): $(OBJECTS)
+	@echo EXPORTS > tmp.def
+	@$(NM) $^ | $(SED) -n 's/^.* [BCDRT] _/ /p' >> tmp.def
+	$(SHLIB_LD) -shared $(DLLFLAGS) -o $@ tmp.def $(OBJECTS) $(ALL_LIBS)
+	@$(RM) tmp.def
 
 .PHONY: all before after shlib-clean
 shlib-clean:
