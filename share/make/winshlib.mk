2ed3d36747a725af26b2fd4a9631b203806ac591
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2009-02-18 15:35:04 +0000

    reinstate support for pkg=win.def
    git-svn-id: https://svn.r-project.org/R/trunk@47954 00db46b3-68df-0310-9c12-caf00c1e9a41

--- share/make/winshlib.mk
+++ share/make/winshlib.mk
@@ -4,12 +4,18 @@ include $(R_HOME)/etc${R_ARCH}/Makeconf
 
 all: before $(SHLIB) after
 
+BASE = $(shell basename $(SHLIB) .dll)
+
 ## do it this way as packages might add dependencies to this target
 $(SHLIB): $(OBJECTS)
-	@echo EXPORTS > tmp.def
-	@$(NM) $^ | $(SED) -n 's/^.* [BCDRT] _/ /p' >> tmp.def
-	$(SHLIB_LD) -shared $(DLLFLAGS) -o $@ tmp.def $(OBJECTS) $(ALL_LIBS)
-	@$(RM) tmp.def
+	@if test -e "$(BASE)-win.def"; then \
+	  $(SHLIB_LD) -shared $(DLLFLAGS) -o $@ $(BASE)-win.def $(OBJECTS) $(ALL_LIBS); \
+	else \
+	  echo EXPORTS > tmp.def; \
+	  $(NM) $^ | $(SED) -n 's/^.* [BCDRT] _/ /p' >> tmp.def; \
+	  $(SHLIB_LD) -shared $(DLLFLAGS) -o $@ tmp.def $(OBJECTS) $(ALL_LIBS); \
+	  $(RM) tmp.def; \
+	fi
 
 .PHONY: all before after shlib-clean
 shlib-clean:
