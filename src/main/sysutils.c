93abf3eb87e9bcfeb84eb15d565901b343fe598e
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2009-02-26 09:10:15 +0000

    port r48006 from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-2-8-branch@48010 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/sysutils.c
+++ src/main/sysutils.c
@@ -382,7 +382,8 @@ static int Rwputenv(const wchar_t *nm, const wchar_t *val)
     wchar_t *buf;
     buf = (wchar_t *) malloc((wcslen(nm) + wcslen(val) + 2) * sizeof(wchar_t));
     if(!buf) return 1;
-    wsprintfW(buf, L"%s=%s", nm, val);
+    /* previously wsprintfW, which had a limit of 1024 chars */
+    wcscpy(buf, nm); wcscat(buf, L"="); wcscat(buf, val);
     if(_wputenv(buf)) return 1;
     /* no free here: storage remains in use */
     return 0;
