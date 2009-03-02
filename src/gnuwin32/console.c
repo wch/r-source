406ca845973539b39213a11afb554cb8f30eb996
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2009-03-02 12:43:24 +0000

    tidy up and document change marking completion lines as user text.
    git-svn-id: https://svn.r-project.org/R/trunk@48039 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/console.c
+++ src/gnuwin32/console.c
@@ -830,8 +830,8 @@ static void storekey(control c, int k)
     if (p->wipe_completion) {
 	p->wipe_completion = 0;
 	checkpointpos(p->lbuf, 0);
-	/* maek current line as user input */
-	USER(NUMLINES-1) = max_pos;
+	/* mark whole of current line as user input */
+	USER(NUMLINES-1) = 0;
 	p->needredraw = 1;
 	REDRAW;
     }
