5deaa539dea739313e207587ed1b97630e0555c1
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2009-03-02 12:56:42 +0000

    GNUish version failed for Uwe with parallel make
    git-svn-id: https://svn.r-project.org/R/trunk@48040 00db46b3-68df-0310-9c12-caf00c1e9a41

--- share/make/winshlib.mk
+++ share/make/winshlib.mk
@@ -2,22 +2,25 @@
 
 include $(R_HOME)/etc${R_ARCH}/Makeconf
 
+## NB, not parallel-safe but Matrix currently uses this
 all: before $(SHLIB) after
 
 BASE = $(shell basename $(SHLIB) .dll)
 
-## do it with explict rules as packages might add dependencies to this target
-## and do it GNUishly to get commands echoed
-ifeq ($(wildcard $(BASE)-win.def),$(BASE)-win.def)
+## do it with explicit rules as packages might add dependencies to this target
+## (attempts to do this GNUishly failed for parallel makes,
+## but we do want the link targets echoed)
 $(SHLIB): $(OBJECTS)
-	$(SHLIB_LD) -shared $(DLLFLAGS) -o $@ $(BASE)-win.def $(OBJECTS) $(ALL_LIBS)
-else
-$(SHLIB): $(OBJECTS)
-	@echo EXPORTS > tmp.def
-	@$(NM) $^ | $(SED) -n 's/^.* [BCDRT] _/ /p' >> tmp.def
-	$(SHLIB_LD) -shared $(DLLFLAGS) -o $@ tmp.def $(OBJECTS) $(ALL_LIBS)
-	@$(RM) tmp.def
-endif
+	@if test -e "$(BASE)-win.def"; then \
+	  echo $(SHLIB_LD) -shared $(DLLFLAGS) -o $@ $(BASE)-win.def $(OBJECTS) $(ALL_LIBS); \
+	  $(SHLIB_LD) -shared $(DLLFLAGS) -o $@ $(BASE)-win.def $(OBJECTS) $(ALL_LIBS); \
+	else \
+	  echo EXPORTS > tmp.def; \
+	  $(NM) $^ | $(SED) -n 's/^.* [BCDRT] _/ /p' >> tmp.def; \
+	  echo $(SHLIB_LD) -shared $(DLLFLAGS) -o $@ tmp.def $(OBJECTS) $(ALL_LIBS); \
+	  $(SHLIB_LD) -shared $(DLLFLAGS) -o $@ tmp.def $(OBJECTS) $(ALL_LIBS); \
+	  $(RM) tmp.def; \
+	fi
 
 .PHONY: all before after shlib-clean
 shlib-clean:
