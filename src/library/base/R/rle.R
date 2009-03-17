76404ac9f0c9e6ce22eba8bf3b46d184a14c66d0
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2009-03-17 11:04:42 +0000

    replace rep() by rep.int() in inverse.rle()
    git-svn-id: https://svn.r-project.org/R/trunk@48135 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/rle.R
+++ src/library/base/R/rle.R
@@ -42,5 +42,5 @@ inverse.rle <- function(x, ...)
     if(is.null(le <- x$lengths) ||
        is.null(v  <- x$values) || length(le) != length(v))
         stop("invalid 'rle' structure")
-    rep(v, le)
+    rep.int(v, le)
 }
