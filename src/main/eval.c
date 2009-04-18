7ddb0a9bb097916e20f349c7d5392b1af8606b29
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2009-04-18 16:50:23 +0000

    typo
    git-svn-id: https://svn.r-project.org/R/trunk@48350 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/eval.c
+++ src/main/eval.c
@@ -2323,7 +2323,7 @@ void R_initialize_bcode(void)
   R_SubsetSym = R_BracketSymbol; /* "[" */
   R_SubassignSym = install("[<-");
   R_CSym = install("c");
-  R_Subset2Sym = R_Bracket2Symbol /* "[[" */
+  R_Subset2Sym = R_Bracket2Symbol; /* "[[" */
   R_Subassign2Sym = install("[[<-");
 
   FakeCall0 = CONS(R_NilValue, R_NilValue);
