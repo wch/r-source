950275f7b7032f542e0d8861e3fba247d9cd041c
murdoch <murdoch@00db46b3-68df-0310-9c12-caf00c1e9a41>
2009-05-19 13:50:43 +0000

    Warn if invalid encoding is used in parse()
    git-svn-id: https://svn.r-project.org/R/trunk@48572 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/source.c
+++ src/main/source.c
@@ -210,11 +210,11 @@ SEXP attribute_hidden do_parse(SEXP call, SEXP op, SEXP args, SEXP env)
     if(streql(encoding, "latin1")) {
 	known_to_be_latin1 = TRUE;
 	allKnown = FALSE;
-    }
-    if(streql(encoding, "UTF-8"))  {
+    } else if(streql(encoding, "UTF-8"))  {
 	known_to_be_utf8 = TRUE;
 	allKnown = FALSE;
-    }
+    } else if(!streql(encoding, "unknown")) 
+    	warning(_("argument '%s = \"%s\"' will be ignored"), "encoding", encoding);
 
     if (prompt == R_NilValue)
 	PROTECT(prompt);
