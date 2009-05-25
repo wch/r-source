d73e4798b2ff712c4f0dbe76135c814814688271
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2009-05-25 13:55:42 +0000

    ported r48608 [qbinom(), PR#13711] from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-2-9-branch@48609 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/nmath/qbinom.c
+++ src/nmath/qbinom.c
@@ -1,8 +1,8 @@
 /*
  *  Mathlib : A C Library of Special Functions
  *  Copyright (C) 1998 Ross Ihaka
- *  Copyright (C) 2000-2006 The R Development Core Team
- *  Copyright (C) 2003-2004 The R Foundation
+ *  Copyright (C) 2000-2009 The R Development Core Team
+ *  Copyright (C) 2003-2009 The R Foundation
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -33,7 +33,7 @@
 #include "nmath.h"
 #include "dpq.h"
 
-static double 
+static double
 do_search(double y, double *z, double p, double n, double pr, double incr)
 {
     if(*z >= p) {
@@ -42,10 +42,12 @@ do_search(double y, double *z, double p, double n, double pr, double incr)
 	REprintf("\tnew z=%7g >= p = %7g  --> search to left (y--) ..\n", z,p);
 #endif
 	for(;;) {
+	    double newz;
 	    if(y == 0 ||
-	       (*z = pbinom(y - incr, n, pr, /*l._t.*/TRUE, /*log_p*/FALSE)) < p)
+	       (newz = pbinom(y - incr, n, pr, /*l._t.*/TRUE, /*log_p*/FALSE)) < p)
 		return y;
 	    y = fmax2(0, y - incr);
+	    *z = newz;
 	}
     }
     else {		/* search to the right */
@@ -58,7 +60,7 @@ do_search(double y, double *z, double p, double n, double pr, double incr)
 	       (*z = pbinom(y, n, pr, /*l._t.*/TRUE, /*log_p*/FALSE)) >= p)
 		return y;
 	}
-    }    
+    }
 }
 
 
