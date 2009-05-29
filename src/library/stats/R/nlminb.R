66368ac80c3295645617594ec3f1f97443c8a019
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2009-05-29 21:52:59 +0000

    more useful messages from nlminb() in some cases
    git-svn-id: https://svn.r-project.org/R/trunk@48688 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/R/nlminb.R
+++ src/library/stats/R/nlminb.R
@@ -73,9 +73,18 @@ nlminb <-
 
     ans <- list(par = get(".par", envir = rho))
     ans$objective <- v[10L]
-    ans$convergence <- as.integer(if (iv[1L] %in% 3L:6L) 0L else 1L)
-    ans$message <-
-        switch(as.character(iv[1L]),
+    ans$convergence <- as.integer(if (iv[1] %in% 3L:6L) 0L else 1L)
+
+    ans$message <- if (19 <= iv[1] && iv[1] <= 43) {
+	if(any(B <- iv[1] == cpos))
+	    sprintf("'control' component '%s' = %g, is out of range",
+		    names(cpos)[B], v[iv[1]])
+	else
+	    sprintf("V[IV[1]] = V[%d] = %g is out of range (see PORT docu.)",
+		    iv[1], v[iv[1]])
+    }
+    else
+	switch(as.character(iv[1]),
                "3" = "X-convergence (3)",
                "4" = "relative convergence (4)",
                "5" = "both X-convergence and relative convergence (5)",
@@ -93,7 +102,7 @@ nlminb <-
                "65" = "gr cannot be computed at initial par (65)")
     if (is.null(ans$message))
         ans$message <-
-            paste("See PORT documentation.  Code (", iv[1L], ")", sep = "")
+            paste("See PORT documentation.  Code (", iv[1], ")", sep = "")
     ans$iterations <- iv[31]
     ans$evaluations <- c("function" = iv[6], gradient = iv[30])
     ans
