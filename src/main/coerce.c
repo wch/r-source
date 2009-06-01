853b333a8049dfe89845c5bec11f59f0fd2b5d23
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2009-06-01 15:06:41 +0000

    dropTrailing0(): possible speedup & improve comments
    git-svn-id: https://svn.r-project.org/R/trunk@48699 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/coerce.c
+++ src/main/coerce.c
@@ -297,9 +297,7 @@ SEXP attribute_hidden StringFromInteger(int x, int *warn)
 
 static const char* dropTrailing0(char *s, char cdec)
 {
-    /* Note: Argument 's' is modified which can be unsafe, depending on how
-     * this is used.  It is ok however, in the context of filtering an
-     * Encode*() value into mkChar(): */
+    /* Note that  's'  is modified */
     char *p = s;
     for (p = s; *p; p++) {
 	if(*p == cdec) {
@@ -307,8 +305,9 @@ static const char* dropTrailing0(char *s, char cdec)
 	    while ('0' <= *p  &&  *p <= '9')
 		if(*(p++) != '0')
 		    replace = p;
-	    while((*(replace++) = *(p++)))
-		;
+	    if(replace != p)
+		while((*(replace++) = *(p++)))
+		    ;
 	    break;
 	}
     }
@@ -320,7 +319,11 @@ SEXP attribute_hidden StringFromReal(double x, int *warn)
     int w, d, e;
     formatReal(&x, 1, &w, &d, &e, 0);
     if (ISNA(x)) return NA_STRING;
-    else return mkChar(dropTrailing0((char *)EncodeReal(x, w, d, e, OutDec),
+    else
+	/* Note that we recast EncodeReal()'s value to possibly modify it
+	 * destructively; this is harmless here (in a sequential
+	 * environment), as mkChar() creates a copy */
+	return mkChar(dropTrailing0((char *)EncodeReal(x, w, d, e, OutDec),
 				     OutDec));
 }
 
