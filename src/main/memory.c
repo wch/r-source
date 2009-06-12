47d61e17a56aff3642e8772a7f73678336f193a5
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2009-06-12 16:05:17 +0000

    Ported r48759 (added PROTECT call in RunFinalizers) from trunk.
    git-svn-id: https://svn.r-project.org/R/branches/R-2-9-branch@48760 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/memory.c
+++ src/main/memory.c
@@ -1109,7 +1109,12 @@ static Rboolean RunFinalizers(void)
 		    R_weak_refs = next;
 		else
 		    SET_WEAKREF_NEXT(last, next);
+		/* The value of 'next' is protected to make is safe
+		   for thsis routine to be called recursively from a
+		   gc triggered by a finalizer. */
+		PROTECT(next);
 		R_RunWeakRefFinalizer(s);
+		UNPROTECT(1);
 	    }
 	    endcontext(&thiscontext);
 	    R_ToplevelContext = saveToplevelContext;
