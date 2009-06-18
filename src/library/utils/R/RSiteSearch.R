25d4d4e9a16816e9bf90f8becedb4f29e7b092aa
murdoch <murdoch@00db46b3-68df-0310-9c12-caf00c1e9a41>
2009-06-18 06:26:43 +0000

    Port r48792 from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-2-9-branch@48793 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/utils/R/RSiteSearch.R
+++ src/library/utils/R/RSiteSearch.R
@@ -14,7 +14,7 @@
 #  A copy of the GNU General Public License is available at
 #  http://www.r-project.org/Licenses/
 
-RSiteSearch <- function(string, restrict = c("Rhelp02a", "functions", "docs"),
+RSiteSearch <- function(string, restrict = c("Rhelp08", "functions", "views"),
 			format = c("normal", "short"),
 			sortby = c("score", "date:late", "date:early",
 			"subject", "subject:descending",
@@ -27,7 +27,8 @@ RSiteSearch <- function(string, restrict = c("Rhelp02a", "functions", "docs"),
     mpp <- paste0("max=", matchesPerPage)
     format <- paste0("result=", match.arg(format))
 
-    restrictVALS <- c("Rhelp02a", "Rhelp01", "functions", "docs", "R-devel")
+    restrictVALS <- c("Rhelp08", "Rhelp01", "Rhelp02", "functions", "views",
+                      "R-devel", "R-sig-mixed-models")
     restr <- match.arg(restrict, choices = restrictVALS, several.ok = TRUE)
     restr <- paste(paste0("idxname=", restr), collapse = "&")
 
