eab33b9fc5f082aa084a8288453763feec1d2106
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2009-07-01 17:52:56 +0000

    port r48882:3 from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-2-9-branch@48884 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/utils/R/packages2.R
+++ src/library/utils/R/packages2.R
@@ -206,6 +206,9 @@ install.packages <-
 	    a <- explode_bundles(available)
 	    pkgs <- implode_bundles(SelectList(a, multiple = TRUE,
 					       title = "Packages"))
+            ## avoid duplicate entries in menus, since the latest will
+            ## be picked up
+            pkgs <- unique(pkgs)
 	}
 	if(!length(pkgs)) stop("no packages were specified")
     }
