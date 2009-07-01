32b0653b71de2c77175ca3d862ca6537346b5a79
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2009-07-01 06:38:10 +0000

    fix PR#13790
    git-svn-id: https://svn.r-project.org/R/trunk@48879 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/grDevices/R/unix/x11.R
+++ src/library/grDevices/R/unix/x11.R
@@ -105,7 +105,7 @@ png <- function(filename = "Rplot%03d.png",
     height <-
         switch(units, "in"=res, "cm"=res/2.54, "mm"=res/25.4, "px"=1) * height
     width <-
-        switch(units, "in"=res, "cm"=res/2.54, "mm"=1/25.4, "px"=1) * width
+        switch(units, "in"=res, "cm"=res/2.54, "mm"=res/25.4, "px"=1) * width
     new <- list(...)
     if(missing(type)) type <- getOption("bitmapType")
     type <- match.arg(type)
@@ -144,7 +144,7 @@ jpeg <- function(filename = "Rplot%03d.jpeg",
     height <-
         switch(units, "in"=res, "cm"=res/2.54, "mm"=res/25.4, "px"=1) * height
     width <-
-        switch(units, "in"=res, "cm"=res/2.54, "mm"=1/25.4, "px"=1) * width
+        switch(units, "in"=res, "cm"=res/2.54, "mm"=res/25.4, "px"=1) * width
     new <- list(...)
     if(!missing(type)) new$type <- match.arg(type)
     antialiases <- get("antialiases", envir = .X11env)
@@ -181,7 +181,7 @@ tiff <- function(filename = "Rplot%03d.tiff",
     height <-
         switch(units, "in"=res, "cm"=res/2.54, "mm"=res/25.4, "px"=1) * height
     width <-
-        switch(units, "in"=res, "cm"=res/2.54, "mm"=1/25.4, "px"=1) * width
+        switch(units, "in"=res, "cm"=res/2.54, "mm"=res/25.4, "px"=1) * width
     new <- list(...)
     type <- if(!missing(type)) match.arg(type) else getOption("bitmapType")
     antialiases <- get("antialiases", envir = .X11env)
@@ -218,7 +218,7 @@ bmp <- function(filename = "Rplot%03d.bmp",
     height <-
         switch(units, "in"=res, "cm"=res/2.54, "mm"=res/25.4, "px"=1) * height
     width <-
-        switch(units, "in"=res, "cm"=res/2.54, "mm"=1/25.4, "px"=1) * width
+        switch(units, "in"=res, "cm"=res/2.54, "mm"=res/25.4, "px"=1) * width
     new <- list(...)
     type <- if(!missing(type)) match.arg(type) else getOption("bitmapType")
     antialiases <- get("antialiases", envir = .X11env)
