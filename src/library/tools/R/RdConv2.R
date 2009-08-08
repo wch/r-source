a63c530cf260e73ecade0e237a5d5ae14189e4d7
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2009-08-08 15:05:41 +0000

    workaround empty \url{} entries
    git-svn-id: https://svn.r-project.org/R/trunk@49111 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/RdConv2.R
+++ src/library/tools/R/RdConv2.R
@@ -614,7 +614,8 @@ Rd2HTML <-
                "\\link" = writeLink(tag, block),
                "\\email" = of0('<a href="mailto:', block[[1L]], '">', htmlify(block[[1L]]), '</a>'),
                ## FIXME: encode, not htmlify
-               "\\url" = of0('<a href="', block[[1L]], '">', block[[1L]], '</a>'),
+               ## watch out for empty URLs (TeachingDemos has one)
+               "\\url" = if(length(block)) of0('<a href="', block[[1L]], '">', block[[1L]], '</a>'),
                "\\Sexpr"= { of1("\\Sexpr")  # This is only here if processing didn't get it...
 	                    option <- attr(block, "Rd_option")
 	                    if (!is.null(option)) of0("[", option, "]")
