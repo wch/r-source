f54d51f8ac2075c5186b3d707360a53aa12a3fad
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2009-08-11 07:01:39 +0000

    cleanup, remove svn:executable
    git-svn-id: https://svn.r-project.org/R/trunk@49153 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/Sweavetools.R
+++ src/library/tools/R/Sweavetools.R
@@ -1,20 +1,19 @@
-SweaveTeXFilter <-
-function(ifile, encoding = "unknown") {
-    
+SweaveTeXFilter <- function(ifile, encoding = "unknown")
+{
     lines <- readLines(ifile, encoding = encoding, warn = FALSE)
-    
-    TEXT <- 1
-    CODE <- 0
-    
+
+    TEXT <- 1L
+    CODE <- 0L
+
     recs <- rbind( data.frame(line=grep("^@", lines), type=TEXT),
                    data.frame(line=grep("^<<(.*)>>=.*", lines), type=CODE))
     recs <- recs[order(recs$line),]
-    last <- 0
+    last <- 0L
     state <- TEXT
-    for (i in seq.int(1, nrow(recs))) {
+    for (i in seq_len(nrow(recs))) {
     	line <- recs$line[i]
     	if (state == CODE)
-    	    lines[(last+1):line] <- ""
+    	    lines[(last+1L):line] <- ""
     	else
     	    lines[line] <- ""
     	state <- recs$type[i]
