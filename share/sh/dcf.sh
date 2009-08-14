d28a1443869d76e857e94b5135e51ac291141fc0
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2009-08-14 07:51:37 +0000

    dcf.sh is unused
    git-svn-id: https://svn.r-project.org/R/branches/R-2-9-branch@49220 00db46b3-68df-0310-9c12-caf00c1e9a41

--- share/sh/dcf.sh
+++
@@ -1,16 +0,0 @@
-get_dcf_field () {
-  ## Get one field including all continuation lines from a DCF file.
-  ## Usage:
-  ##   get_dcf_field FIELD FILE
-  ws="[ 	]"		# space and tab
-  ${SED-sed} -n "/^${1}:/,/^[^ ]/p" ${2} | \
-    ${SED-sed} -n "1s/^${1}:${ws}*//p; /^${ws}/p" | \
-    ${SED-sed} "/^${ws}/s/^${ws}*//; s/[ 	]*$//"
-    ## (Strip leading tag [first match], delete all additional tag
-    ## lines, and remove leading whitespace from continuation lines.)
-}
-
-### Local Variables: ***
-### mode: sh ***
-### sh-indentation: 2 ***
-### End: ***
