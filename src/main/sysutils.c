9e9863704938fac93798d9c4549dd6e047e4f175
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2009-08-24 08:46:28 +0000

    conditionalize Windows update
    git-svn-id: https://svn.r-project.org/R/trunk@49392 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/sysutils.c
+++ src/main/sysutils.c
@@ -1476,7 +1476,9 @@ SEXP attribute_hidden do_glob(SEXP call, SEXP op, SEXP args, SEXP env)
     SEXP x, ans;
     int i, n, res, dirmark;
     glob_t globbuf;
+#ifdef Win32
     R_StringBuffer cbuff = {NULL, 0, MAXELTSIZE};
+#endif
 
     checkArity(op, args);
     if (!isString(x = CAR(args)))
