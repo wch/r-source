e5cfda1039fff6b2a5e5b7a175b78fe7c73e53d0
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2009-08-24 08:09:22 +0000

    updates
    git-svn-id: https://svn.r-project.org/R/trunk@49385 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/sysutils.c
+++ src/main/sysutils.c
@@ -1476,6 +1476,7 @@ SEXP attribute_hidden do_glob(SEXP call, SEXP op, SEXP args, SEXP env)
     SEXP x, ans;
     int i, n, res, dirmark;
     glob_t globbuf;
+    R_StringBuffer cbuff = {NULL, 0, MAXELTSIZE};
 
     checkArity(op, args);
     if (!isString(x = CAR(args)))
