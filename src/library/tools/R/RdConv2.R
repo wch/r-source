4dfe6e529a313c1ce24ac101c29f18a9a8ddc6cc
murdoch <murdoch@00db46b3-68df-0310-9c12-caf00c1e9a41>
2009-08-26 13:04:34 +0000

    Corrected version of r49440
    git-svn-id: https://svn.r-project.org/R/trunk@49451 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/RdConv2.R
+++ src/library/tools/R/RdConv2.R
@@ -307,7 +307,6 @@ prepare_Rd <-
              options = RweaveRdDefaults,
              stage2 = TRUE, stage3 = TRUE, ..., msglevel = 0)
 {
-    Rdfile <- "not known"
     if (is.character(Rd)) {
         Rdfile <- Rd
         ## do it this way to get info in internal warnings
@@ -317,6 +316,8 @@ prepare_Rd <-
         Rdfile <- summary(Rd)
         Rd <- parse_Rd(Rd, encoding = encoding, ...)
     } else Rdfile <- attr(Rd, "Rdfile")
+    if (is.null(Rdfile) && !is.null(srcref <- attr(Rd, "srcref")))
+    	Rdfile <- attr(srcref, "srcfile")$filename
     pratt <- attr(Rd, "prepared")
     if (is.null(pratt)) pratt <- 0L
     if ("build" %in% stages)
@@ -404,7 +405,7 @@ prepare2_Rd <- function(Rd, Rdfile)
     if (any(bad)) {
         for(s in which(bad))
             warnRd(Rd[[s]], Rdfile, "Section ",
-                   sections[Rd[[s]]], " is unrecognized and will be dropped")
+                   sections[s], " is unrecognized and will be dropped")
         drop <- drop | bad
     }
     Rd <- Rd[!drop]
