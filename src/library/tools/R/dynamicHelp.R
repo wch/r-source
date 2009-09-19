0bd2b47f2e75f8d7e646691ec61bbe9c556ca8f8
murdoch <murdoch@00db46b3-68df-0310-9c12-caf00c1e9a41>
2009-09-19 13:25:03 +0000

    Dynamic help should show links.
    git-svn-id: https://svn.r-project.org/R/trunk@49759 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/dynamicHelp.R
+++ src/library/tools/R/dynamicHelp.R
@@ -270,7 +270,7 @@ httpd <- function(path, query, ...)
 	    outfile <- tempfile("Rhttpd")
 	    temp <- tools::Rd2HTML(tools:::fetchRdDB(RdDB, helpdoc),
                                    out = outfile, package = dirpath,
-                                   dynamic = TRUE)
+                                   dynamic = TRUE, no_links = FALSE)
 	    on.exit(unlink(outfile))
 	    return(list(payload = paste(readLines(temp), collapse = "\n")))
 	} else {
