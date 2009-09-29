c1564d91424fe7265a0fa7a2e2dc462c97b8037d
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2009-09-29 11:31:19 +0000

    port r49871:3 from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-2-10-branch@49874 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/writePACKAGES.R
+++ src/library/tools/R/writePACKAGES.R
@@ -41,11 +41,13 @@ function(dir = ".", fields = NULL,
                                              unpacked)
 
         if(length(desc)) {
+            Files <- names(desc)
             fields <- names(desc[[1L]])
             desc <- matrix(unlist(desc), ncol = length(fields), byrow = TRUE)
             colnames(desc) <- fields
             bundle <- !is.na(desc[,"Bundle"])
             desc[bundle, "Package"] <- desc[bundle, "Bundle"]
+            desc <- cbind(desc, File = Files)
             if(latestOnly) desc <- .remove_stale_dups(desc)
 
             ## Standardize licenses or replace by NA.
@@ -105,6 +107,7 @@ function(dir, fields = NULL,
     fields <- unique(c(.get_standard_repository_db_fields(), fields))
     packages <- sapply(strsplit(files, "_", fixed = TRUE), "[", 1L)
     db <- vector(length(files), mode = "list")
+    names(db) <- files
     ## Many (roughly length(files)) warnings are *expected*, hence
     ## suppressed.
     op <- options(warn = -1)
@@ -169,10 +172,6 @@ function(dir, fields = NULL,
     }
     if(verbose) message("done")
 
-    ## Note that we cannot simply do
-    ##   names(db) <- sub("_.*", "", basename(files))
-    ## as repositories could hold multiple versions of packages.
-
     db
 }
 
@@ -189,7 +188,7 @@ function(dir, fields = NULL, verbose = getOption("verbose"))
     for(i in seq_along(paths)) {
         if(verbose) message(paste(" ", basename(paths[i])))
         temp <- tryCatch(read.dcf(file.path(paths[i], "DESCRIPTION"),
-                             fields = fields)[1L, ],
+                                  fields = fields)[1L, ],
                          error = identity)
         if(!inherits(temp, "error"))
             db[[i]] <- temp
