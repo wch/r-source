247396eaa19f34ed76d04c5ab60297ce59c79ac6
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2009-10-09 09:16:45 +0000

    clean up after ourselves -- especially in the source dir
    git-svn-id: https://svn.r-project.org/R/trunk@50001 00db46b3-68df-0310-9c12-caf00c1e9a41

--- tests/reg-tests-1.R
+++ tests/reg-tests-1.R
@@ -5188,6 +5188,7 @@ if(.Platform$OS.type == "unix") {
     print(installed.packages(lib.loc= "myLib", priority= "NA"))
     options(op)
     unlink("myLib", recursive=TRUE)
+    unlink(file.path(pkgSrcPath, "pkgB", "R"), recursive = TRUE)
 }
 unlink("myTst", recursive=TRUE)
 
