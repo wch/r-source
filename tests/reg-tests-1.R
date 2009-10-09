386314ddf3be1ddb21ab6e9bcb3da5bc7a86e015
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2009-10-09 12:27:59 +0000

    revert r50002
    git-svn-id: https://svn.r-project.org/R/branches/R-2-10-branch@50006 00db46b3-68df-0310-9c12-caf00c1e9a41

--- tests/reg-tests-1.R
+++ tests/reg-tests-1.R
@@ -5184,7 +5184,6 @@ if(.Platform$OS.type == "unix") {
     print(installed.packages(lib.loc= "myLib", priority= "NA"))
     options(op)
     unlink("myLib", recursive=TRUE)
-    unlink(file.path(pkgSrcPath, "pkgB", "R"), recursive = TRUE)
 }
 unlink("myTst", recursive=TRUE)
 
