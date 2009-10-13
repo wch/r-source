7d04599a1a509f42755aaba7e594305faffca851
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2009-10-13 11:25:57 +0000

    port r50054 from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-2-10-branch@50055 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/admin.R
+++ src/library/tools/R/admin.R
@@ -852,7 +852,7 @@ function(dir)
 
 checkRdaFiles <- function(paths)
 {
-    if(length(paths) == 1L && file.info(paths)$isdir)
+    if(length(paths) == 1L && isTRUE(file.info(paths)$isdir))
         paths <- Sys.glob(c(file.path(paths, "*.rda"),
                             file.path(paths, "*.RData")))
     res <- data.frame(size = NA_real_, ASCII = NA,
@@ -886,7 +886,7 @@ resaveRdaFiles <- function(paths,
                            compress = c("auto", "gzip", "bzip2", "xz"),
                            compression_level)
 {
-    if(length(paths) == 1L && file.info(paths)$isdir)
+    if(length(paths) == 1L && isTRUE(file.info(paths)$isdir))
         paths <- Sys.glob(c(file.path(paths, "*.rda"),
                             file.path(paths, "*.RData")))
     compress <- match.arg(compress)
