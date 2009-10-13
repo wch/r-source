ff0d31222809f76deccc9f54b5aceffefc853a4d
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2009-10-13 11:25:15 +0000

    protect against non-existent path
    git-svn-id: https://svn.r-project.org/R/trunk@50054 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/admin.R
+++ src/library/tools/R/admin.R
@@ -799,7 +799,7 @@ function(dir)
 
 checkRdaFiles <- function(paths)
 {
-    if(length(paths) == 1L && file.info(paths)$isdir)
+    if(length(paths) == 1L && isTRUE(file.info(paths)$isdir))
         paths <- Sys.glob(c(file.path(paths, "*.rda"),
                             file.path(paths, "*.RData")))
     res <- data.frame(size = NA_real_, ASCII = NA,
@@ -833,7 +833,7 @@ resaveRdaFiles <- function(paths,
                            compress = c("auto", "gzip", "bzip2", "xz"),
                            compression_level)
 {
-    if(length(paths) == 1L && file.info(paths)$isdir)
+    if(length(paths) == 1L && isTRUE(file.info(paths)$isdir))
         paths <- Sys.glob(c(file.path(paths, "*.rda"),
                             file.path(paths, "*.RData")))
     compress <- match.arg(compress)
