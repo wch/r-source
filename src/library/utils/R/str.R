3cfdf620ad6186bcfbb0cc615a5a889da62af69d
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2009-10-23 07:45:12 +0000

    print.ls_str() : do not stop on error, as we cannot catch "missingness" 	       when the error message is translated (LC_MESSAGES)
    git-svn-id: https://svn.r-project.org/R/trunk@50180 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/utils/R/str.R
+++ src/library/utils/R/str.R
@@ -546,9 +546,9 @@ print.ls_str <- function(x, max.level = 1, give.attr = FALSE,
 
 	o <- tryCatch(get(nam, envir = E, mode = M), error = function(e)e)
 	if(inherits(o, "error")) {
-	    if(length(grep("missing|not found", o$message)))
-		cat("<missing>\n")
-	    else stop(o$message)
+	    cat(## FIXME: only works with "C" (or English) LC_MESSAGES locale!
+		if(length(grep("missing|not found", o$message)))
+		"<missing>" else o$message, "\n", sep='')
 	}
 	else
 	    do.call(str, c(list(o), strargs))
