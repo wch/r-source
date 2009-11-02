0e41933def427290939d6f89c47fb322c71be31f
murrell <murrell@00db46b3-68df-0310-9c12-caf00c1e9a41>
2009-11-02 20:00:11 +0000

    fix for compiler warnings
    git-svn-id: https://svn.r-project.org/R/trunk@50294 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/modules/X11/cairoX11.c
+++ src/modules/X11/cairoX11.c
@@ -282,20 +282,24 @@ static void Cairo_Raster(unsigned int *raster, int w, int h,
                          Rboolean interpolate,
                          const pGEcontext gc, pDevDesc dd)
 {
+    char *vmax = vmaxget();
     int i;
     cairo_surface_t *image;
+    unsigned char *imageData;
     pX11Desc xd = (pX11Desc) dd->deviceSpecific;
 
+    imageData = (unsigned char *) R_alloc(4*w*h, sizeof(unsigned char));
     /* The R ABGR needs to be converted to a Cairo ARGB */
     for (i=0; i<w*h; i++) {
-        raster[i] = ((((raster[i])>>24)&255)<<24 |
-                     ((raster[i])&255)<<16 | 
-                     (((raster[i])>>8)&255)<<8 |
-                     (((raster[i])>>16)&255));
+        imageData[i*4 + 3] = R_ALPHA(raster[i]);
+        imageData[i*4 + 2] = R_RED(raster[i]);
+        imageData[i*4 + 1] = R_GREEN(raster[i]);
+        imageData[i*4 + 0] = R_BLUE(raster[i]);
     }
-    image = cairo_image_surface_create_for_data(raster, 
+    image = cairo_image_surface_create_for_data(imageData, 
                                                 CAIRO_FORMAT_ARGB32,
-                                                w, h, 4*w);
+                                                w, h, 
+                                                4*w);
 
     cairo_save(xd->cc);
 
@@ -322,6 +326,8 @@ static void Cairo_Raster(unsigned int *raster, int w, int h,
 
     cairo_restore(xd->cc);
     cairo_surface_destroy(image);
+
+    vmaxset(vmax);
 }
 
 static SEXP Cairo_Cap(pDevDesc dd)
@@ -332,7 +338,7 @@ static SEXP Cairo_Cap(pDevDesc dd)
     cairo_format_t format;
     unsigned char *screenData;
     SEXP dim, raster = R_NilValue;
-    int *rint;
+    unsigned int *rint;
 
     screen = cairo_surface_reference(cairo_get_target(xd->cc));
     width = cairo_image_surface_get_width(screen);
@@ -352,7 +358,7 @@ static SEXP Cairo_Cap(pDevDesc dd)
 
     /* Copy each byte of screen to an R matrix. 
      * The Cairo RGB24 needs to be converted to an R ABGR32 */
-    rint = INTEGER(raster);
+    rint = (unsigned int *) INTEGER(raster);
     for (i=0; i<size; i++) {
         /* First byte is "black" - WTF?
          * Anyway, I consequently start at byte 2 
