b6a9c9831afacf37db6627d8dc630dae3b0ccaa5
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2009-11-20 11:04:33 +0000

    port r50507 from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-2-10-branch@50508 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/Rdynload.c
+++ src/main/Rdynload.c
@@ -627,9 +627,11 @@ addDLL(char *dpath, char *DLLname, HINSTANCE handle)
     LoadedDLL[CountDLL].numCSymbols = 0;
     LoadedDLL[CountDLL].numCallSymbols = 0;
     LoadedDLL[CountDLL].numFortranSymbols = 0;
+    LoadedDLL[CountDLL].numExternalSymbols = 0;
     LoadedDLL[CountDLL].CSymbols = NULL;
     LoadedDLL[CountDLL].CallSymbols = NULL;
     LoadedDLL[CountDLL].FortranSymbols = NULL;
+    LoadedDLL[CountDLL].ExternalSymbols = NULL;
     CountDLL++;
 
     return(ans);
