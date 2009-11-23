cb625a46193b271c9e00b6eb4211300ce09a395d
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2009-11-23 22:46:39 +0000

    another pchisq(., ncp > 80) case
    git-svn-id: https://svn.r-project.org/R/trunk@50552 00db46b3-68df-0310-9c12-caf00c1e9a41

--- tests/d-p-q-r-tests.R
+++ tests/d-p-q-r-tests.R
@@ -665,7 +665,9 @@ stopifnot(plnorm(-1:0, lower.tail=FALSE, log.p=TRUE) == 0,
 
 ## pchisq(df=0) was wrong in 2.7.1
 stopifnot(pchisq(c(-1,0,1), df=0) == c(0,1,1),
-          pchisq(c(-1,0,1), df=0, lower.tail=FALSE) == c(1,0,0))
+          pchisq(c(-1,0,1), df=0, lower.tail=FALSE) == c(1,0,0),
+	  ## for ncp >= 80, gave values >= 1 in 2.10.0
+	  pchisq(500:700, 1.01, ncp = 80) <= 1)
 
 ## dnbinom for extreme  size and/or mu :
 mu <- 20
