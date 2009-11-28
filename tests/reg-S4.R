e52c181fd839a422b839d2d559961098d1a4beba
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2009-11-28 11:28:13 +0000

    clean up, remove some tests of anti-the-documentation ops
    git-svn-id: https://svn.r-project.org/R/trunk@50592 00db46b3-68df-0310-9c12-caf00c1e9a41

--- tests/reg-S4.R
+++ tests/reg-S4.R
@@ -446,17 +446,9 @@ stopifnot(identical(m,			as(nf, "matrix")),
 	  identical(matrix(1:6,ncol=2), as(n3, "matrix")))
 ## partly failed at times in pre-2.8.0
 
-if("S4_subsettable" == TRUE) { ## now (2.9.0) not thought to be a good idea
-## "[" subsetting of "simple S4" classes:
-for(bcl in c("list","integer","numeric")) {
-    setClass("C", contains= bcl)
-    x <- new("C", 1:3); x <- x[2:3]
-    stopifnot(is(x, "C"), is(rep(x, 3), "C"), is(rep.int(x, 2), "C"))
-}
-## used to drop the class in 2.8.0 and earlier
-}
-
-##From "Michael Lawrence" <....@fhcrc.org>  To r-devel@r-project, 25 Nov 2008:
+## From "Michael Lawrence" <....@fhcrc.org>  To r-devel@r-project, 25 Nov 2008:
+## NB: setting a generic on order() is *not* the approved method
+## -- set xtfrm() methods instead
 setGeneric("order", signature="...",
 	   function (..., na.last=TRUE, decreasing=FALSE)
 	   standardGeneric("order"))
@@ -465,10 +457,8 @@ setGeneric("rbind", function(..., deparse.level=1)
 	   standardGeneric("rbind"), signature = "...")
 stopifnot(identical(rbind(1), matrix(1,1,1)))
 ## gave Error in .Method( .... in R 2.8.0
-## NB: setting a generic on order() is *not* the approved method
-## -- set xtfrm() methods instead
 
-## median.default( <simple S4> )
+## median( <simple S4> )
 ## FIXME: if we use "C" instead of "L", this fails because of caching
 setClass("L", contains = "list")
 ## {simplistic, just for the sake of testing here} :
@@ -479,27 +469,20 @@ setMethod("Summary", "L",
 	  function(x, ..., na.rm=FALSE) {x <- unlist(x); callNextMethod()})
 setMethod("[", signature(x="L", i="ANY", j="missing",drop="missing"),
           function(x,i,j,drop) new(class(x), x@.Data[i]))
-## This example requires a method for sort(), now that class "L"
-## inherits S3 methods for "list"; i.e., sort.list
-setMethod("sort", signature = "L", function(x, decreasing = FALSE, ...)
-          sort.L(x, decreasing, ...))
 ## defining S4 methods for sort() has no effect on calls to
 ## sort() from functions in a name space; e.g., median.default.
 ## but setting an xtfrm() method works.
 setMethod("xtfrm", "L", function(x) xtfrm(unlist(x@.Data)))
-## Alternatively, set an S3 method
-# sort.L <- function(x, ...) { x@.Data <- as.list(sort(unlist(x@.Data), ...)); x}
-
-## NB: median is documented to use mean(), but was incorrectly changed
-## to use sum() in 2.8.1.  So we need an S3 mean method:
+## median is documented to use mean(), so we need an S3 mean method:
+## An S4 method will not do because of the long-standing S4 scoping bug.
 mean.L <- function(x, ...) new("L", mean(unlist(x@.Data), ...))
 x <- new("L", 1:3); x2 <- x[-2]
 stopifnot(unlist(x2) == (1:3)[-2],
 	  is(mx <- median(x), "L"), mx == 2,
-	  identical(mx, quantile(x, 0.5, names=FALSE)),
 	  ## median of two
 	  median(x2) == x[2])
-## median.default(x) was too stringent on x
+## NB: quantile() is not said to work on such an object, and only does so
+## for order statistics (so should not be tested, but was in earlier versions).
 
 ## Buglet in as() generation for class without own slots
 setClass("SIG", contains="signature")
