d294efa3b7e46987176f6d467ef87ec37e1c1a22
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2009-12-14 14:55:20 +0000

    qpois(1,0) [PR#14135]
    git-svn-id: https://svn.r-project.org/R/trunk@50733 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/nmath/qpois.c
+++ src/nmath/qpois.c
@@ -1,7 +1,7 @@
 /*
  *  Mathlib : A C Library of Special Functions
  *  Copyright (C) 1998 Ross Ihaka
- *  Copyright (C) 2000-2006 The R Development Core Team
+ *  Copyright (C) 2000-2009 The R Development Core Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -33,7 +33,7 @@
 #include "nmath.h"
 #include "dpq.h"
 
-static double 
+static double
 do_search(double y, double *z, double p, double lambda, double incr)
 {
     if(*z >= p) {
@@ -64,11 +64,10 @@ double qpois(double p, double lambda, int lower_tail, int log_p)
 #endif
     if(!R_FINITE(lambda))
 	ML_ERR_return_NAN;
-
-    R_Q_P01_boundaries(p, 0, ML_POSINF);
-
     if(lambda < 0) ML_ERR_return_NAN;
     if(lambda == 0) return 0;
+
+    R_Q_P01_boundaries(p, 0, ML_POSINF);
 
     mu = lambda;
     sigma = sqrt(lambda);
