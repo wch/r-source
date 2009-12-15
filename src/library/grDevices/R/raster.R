dfc29409a7577887f072db38ff8456df0f357965
murrell <murrell@00db46b3-68df-0310-9c12-caf00c1e9a41>
2009-12-15 03:11:50 +0000

    added more methods for as.raster()
    git-svn-id: https://svn.r-project.org/R/trunk@50738 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/grDevices/R/raster.R
+++ src/library/grDevices/R/raster.R
@@ -8,34 +8,66 @@ is.raster <- function(x) {
     inherits(x, "raster")
 }
 
-as.raster <- function(x) {
+as.raster <- function(x, ...) {
     UseMethod("as.raster")
 }
 
-as.raster.raster<- function(x) {
+as.raster.raster<- function(x, ...) {
     x
 }
 
-# FIXME:  method for vectors, with row and col args
+as.raster.logical <- function(x, max=1, ...) {
+    as.raster(matrix(x, ...), max)
+}
 
-# FIXME:  character matrix assumed to be colour names
-#         (just pass through)
-#         numeric matrix assumed to be greyscales
-#         (convert using grey())
-#         3D numeric array assumed to be RGB
-#         (convert using rgb())
-as.raster.matrix <- function(x) {
-    # Transpose ...
-    r <- t(x)
-    # ... but retain original dimensions
+as.raster.numeric <- as.raster.logical 
+
+as.raster.character <- as.raster.logical
+
+as.raster.matrix <- function(x, max=1, ...) {
+    if (is.character(x)) {
+        # Assume to be color names
+        r <- t(x)
+    } else if (is.numeric(x) || is.logical(x)) {
+        # Assume greyscale or b&w values
+        # Use rgb() to allow for different 'max' value
+        tx <- t(x)
+        r <- rgb(tx, tx, tx, max=max)
+    } else {
+        stop("A raster matrix must be character, or numeric, or logical")
+    }
+    # Transposed, but retain original dimensions
     dim(r) <- dim(x)
     class(r) <- "raster"
     r
 }
 
+as.raster.array <- function(x, max=1, ...) {
+    if (!is.numeric(x))
+        stop("A raster array must be numeric")
+    if (length(dim(x)) != 3) {
+        stop("A raster array must have exactly 3 dimensions")
+    }
+    if (dim(x)[3] == 3) {
+        r <- rgb(t(x[,,1]), t(x[,,2]), t(x[,,3]), max=max)
+    } else if (dim(x)[3] == 4) {
+        r <- rgb(t(x[,,1]), t(x[,,2]), t(x[,,3]), t(x[,,4]), max=max)
+    } else {
+        stop("A raster array must have exactly 3 or 4 planes")
+    }
+    dim(r) <- dim(x)[1:2]
+    class(r) <- "raster"
+    r
+}
+
 # For completeness ...
-raster <- function(x) {
-    as.raster(x)
+raster <- function(x, ...) {
+    as.raster(x, ...)
+}
+
+print.raster <- function(x, ...) {
+    dim <- dim(x)
+    print(matrix(x, nrow=dim[1], ncol=dim[2], byrow=TRUE))
 }
 
 
