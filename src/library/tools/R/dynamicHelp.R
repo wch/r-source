11ef15893b0855b4ae32274e3638d714901f44cc
murdoch <murdoch@00db46b3-68df-0310-9c12-caf00c1e9a41>
2009-12-16 13:11:11 +0000

    Fix MIME type for R.css in dynamic help.
    git-svn-id: https://svn.r-project.org/R/trunk@50755 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/dynamicHelp.R
+++ src/library/tools/R/dynamicHelp.R
@@ -118,7 +118,8 @@ httpd <- function(path, query, ...)
                    sep = ""))
 
     if (grepl("R\\.css$", path))
-        return(list(file = file.path(R.home("doc"), "html", "R.css")))
+        return(list(file = file.path(R.home("doc"), "html", "R.css"),
+                    "content-type" = "text/css"))
     else if(path == "/favicon.ico")
         return(list(file = file.path(R.home("doc"), "html", "favicon.ico")))
     else if(path == "/NEWS") {
