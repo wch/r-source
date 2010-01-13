b045a15a0adb0d80a042cc3e50e56518c81b1f3b
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2010-01-13 09:51:09 +0000

    convert help files to UTF-8
    git-svn-id: https://svn.r-project.org/R/trunk@50971 00db46b3-68df-0310-9c12-caf00c1e9a41

--- tests/reg-plot-latin1.R
+++ tests/reg-plot-latin1.R
@@ -2,5 +2,5 @@ postscript(file = "reg-plot-latin1.ps",
            encoding = "ISOLatin1",
            width = 7, height = 7, paper = "a4")
 library(graphics) # to be sure
-example(text)     # has Latin-1 examples
+example(text)     # has examples that need to he plotted in latin-1
 q("no")
