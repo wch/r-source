dd66df51fa43d18614e0539387b6f6e24e1a0c81
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2010-01-31 15:27:06 +0000

    simplify
    git-svn-id: https://svn.r-project.org/R/trunk@51071 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/R/vcov.R
+++ src/library/stats/R/vcov.R
@@ -18,22 +18,20 @@ vcov <- function(object, ...) UseMethod("vcov")
 
 ## The next three have to call the method, as classes which
 ## inherit from "glm" need not have summary methods which
-## inherit from "summary.glm".
-vcov.glm <- function(object, ...)
-{
-    so <- summary.glm(object, corr=FALSE, ...)
-    so$dispersion * so$cov.unscaled
-}
+## inherit from "summary.glm", and similarly for "lm" and "mlm"
+
+## Allow for 'dispersion' to be passed down (see the help for vcov)
+vcov.glm <- function(object, ...) summary.glm(object, ...)$cov.scaled
 
 vcov.lm <- function(object, ...)
 {
-    so <- summary.lm(object, corr=FALSE)
+    so <- summary.lm(object)
     so$sigma^2 * so$cov.unscaled
 }
 
 vcov.mlm <- function(object, ...)
 {
-    so <- summary.mlm(object, corr=FALSE)[[1L]]
+    so <- summary.mlm(object)[[1L]]
     kronecker(estVar(object), so$cov.unscaled, make.dimnames = TRUE)
 }
 
