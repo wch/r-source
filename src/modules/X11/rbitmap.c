c85a269cb4d4c1ab680a5403ba2437176f72f19a
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2010-02-01 14:35:32 +0000

    port r51087 from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-2-10-branch@51088 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/modules/X11/rbitmap.c
+++ src/modules/X11/rbitmap.c
@@ -1,6 +1,6 @@
 /*
  *  R : A Computer Language for Statistical Data Analysis
- *  Copyright (C) 1999-2008  Guido Masarotto and the R Development Core Team
+ *  Copyright (C) 1999-2010  Guido Masarotto and the R Development Core Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -70,7 +70,11 @@
 static void my_png_error(png_structp png_ptr, png_const_charp msg)
 {
     R_ShowMessage((char *) msg);
+#if PNG_LIBPNG_VER < 10400
     longjmp(png_ptr->jmpbuf,1);
+#else
+    longjmp(png_jmpbuf(png_ptr),1);
+#endif
 }
 
 static void my_png_warning(png_structp png_ptr, png_const_charp msg)
@@ -124,7 +128,12 @@ int R_SaveAsPng(void  *d, int width, int height,
     /* Set error handling.  REQUIRED if you aren't supplying your own
      * error handling functions in the png_create_write_struct() call.
      */
-    if (setjmp(png_ptr->jmpbuf)) {
+#if PNG_LIBPNG_VER < 10400
+    if (setjmp(png_ptr->jmpbuf))
+#else
+    if (setjmp(png_jmpbuf(png_ptr)))
+#endif
+{
 	/* If we get here, we had a problem writing the file */
 	free(scanline);
 	png_destroy_write_struct(&png_ptr, &info_ptr);
