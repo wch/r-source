ee121f2f4bdab029b4ca9ab94ffb4dcf213c1b59
falcon <falcon@00db46b3-68df-0310-9c12-caf00c1e9a41>
2010-02-10 18:48:37 +0000

    Fix slow gsub(pat, rep, s, useBytes=TRUE, fixed=TRUE)
    We were using strcat in a way that repeatedly copied the entire string
    for each match instead of incrementally adding to the buffer.
    
    git-svn-id: https://svn.r-project.org/R/trunk@51118 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/grep.c
+++ src/main/grep.c
@@ -1214,11 +1214,13 @@ SEXP attribute_hidden do_gsub(SEXP call, SEXP op, SEXP args, SEXP env)
 		cbuf = u = Calloc(ns + nr*(replen - patlen) + 1, char);
 		*u = '\0';
 		do {
-		    nr = strlen(u);
-		    strncat(u, s, st); u[nr+st] = '\0'; s += st+patlen;
-		    strcat(u, srep);
+                    strncpy(u, s, st);
+                    u += st;
+                    s += st+patlen;
+		    strncpy(u, srep, replen);
+                    u += replen;
 		} while(global && (st = fgrep_one_bytes(spat, s, useBytes, use_UTF8)) >= 0);
-		strcat(u, s);
+		strcpy(u, s);
 		if (useBytes)
 		    SET_STRING_ELT(ans, i, mkChar(cbuf));
 		else if (use_UTF8)
