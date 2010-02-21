0cc837c149bdc60c5c9c8cdb511eb8f19567ee21
pd <pd@00db46b3-68df-0310-9c12-caf00c1e9a41>
2010-02-21 10:27:52 +0000

    subassign with length zero (PR#14217)
    git-svn-id: https://svn.r-project.org/R/trunk@51162 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/subassign.c
+++ src/main/subassign.c
@@ -1524,6 +1524,8 @@ do_subassign2_dflt(SEXP call, SEXP op, SEXP args, SEXP rho)
     
     stretch = 0;
     if (isVector(x)) {
+	if (!isVectorList(x) && LENGTH(y) == 0)
+	    error(_("replacement has length zero"));
 	if (!isVectorList(x) && LENGTH(y) > 1)
 	    error(_("more elements supplied than there are to replace"));
 	if (nsubs == 0 || CAR(subs) == R_MissingArg)
