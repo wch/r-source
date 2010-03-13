208f456686caed1e3677a3653adaee85d070ea8d
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2010-03-13 09:46:24 +0000

    clean up layout
    git-svn-id: https://svn.r-project.org/R/trunk@51264 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/match.c
+++ src/main/match.c
@@ -202,8 +202,7 @@ SEXP attribute_hidden matchArgs(SEXP formals, SEXP supplied, SEXP call)
     int fargused[arg_i];
     memset(fargused, 0, sizeof(fargused));
 
-    for(b = supplied; b != R_NilValue; b=CDR(b))
-	SET_ARGUSED(b, 0);
+    for(b = supplied; b != R_NilValue; b = CDR(b)) SET_ARGUSED(b, 0);
 
     PROTECT(actuals);
 
@@ -225,8 +224,7 @@ SEXP attribute_hidden matchArgs(SEXP formals, SEXP supplied, SEXP call)
 		    if (ARGUSED(b) == 2)
 			error(_("argument %d matches multiple formal arguments"), i);
 		    SETCAR(a, CAR(b));
-		    if(CAR(b) != R_MissingArg)
-			SET_MISSING(a, 0);	/* not missing this arg */
+		    if(CAR(b) != R_MissingArg) SET_MISSING(a, 0);
 		    SET_ARGUSED(b, 2);
 		    fargused[arg_i] = 2;
 		}
@@ -253,8 +251,7 @@ SEXP attribute_hidden matchArgs(SEXP formals, SEXP supplied, SEXP call)
 		/* Record where ... value goes */
 		dots = a;
 		seendots = 1;
-	    }
-	    else {
+	    } else {
 		i = 1;
 		for (b = supplied; b != R_NilValue; b = CDR(b)) {
 		    if (ARGUSED(b) != 2 && TAG(b) != R_NilValue &&
@@ -271,8 +268,7 @@ SEXP attribute_hidden matchArgs(SEXP formals, SEXP supplied, SEXP call)
 					CHAR(PRINTNAME(TAG(f))) );
 			}
 			SETCAR(a, CAR(b));
-			if (CAR(b) != R_MissingArg)
-			    SET_MISSING(a, 0);       /* not missing this arg */
+			if (CAR(b) != R_MissingArg) SET_MISSING(a, 0);
 			SET_ARGUSED(b, 1);
 			fargused[arg_i] = 1;
 		    }
@@ -303,26 +299,22 @@ SEXP attribute_hidden matchArgs(SEXP formals, SEXP supplied, SEXP call)
 	    seendots = 1;
 	    f = CDR(f);
 	    a = CDR(a);
-	}
-	else if (CAR(a) != R_MissingArg) {
+	} else if (CAR(a) != R_MissingArg) {
 	    /* Already matched by tag */
 	    /* skip to next formal */
 	    f = CDR(f);
 	    a = CDR(a);
-	}
-	else if (ARGUSED(b) || TAG(b) != R_NilValue) {
+	} else if (ARGUSED(b) || TAG(b) != R_NilValue) {
 	    /* This value used or tagged , skip to next value */
 	    /* The second test above is needed because we */
 	    /* shouldn't consider tagged values for positional */
 	    /* matches. */
 	    /* The formal being considered remains the same */
 	    b = CDR(b);
-	}
-	else {
+	} else {
 	    /* We have a positional match */
 	    SETCAR(a, CAR(b));
-	    if(CAR(b) != R_MissingArg)
-		SET_MISSING(a, 0);
+	    if(CAR(b) != R_MissingArg) SET_MISSING(a, 0);
 	    SET_ARGUSED(b, 1);
 	    b = CDR(b);
 	    f = CDR(f);
@@ -333,29 +325,26 @@ SEXP attribute_hidden matchArgs(SEXP formals, SEXP supplied, SEXP call)
     if (dots != R_NilValue) {
 	/* Gobble up all unused actuals */
 	SET_MISSING(dots, 0);
-	i=0;
-	for(a=supplied; a!=R_NilValue ; a=CDR(a) )
-	    if(!ARGUSED(a)) i++;
+	i = 0;
+	for(a = supplied; a != R_NilValue ; a = CDR(a)) if(!ARGUSED(a)) i++;
 
 	if (i) {
 	    a = allocList(i);
 	    SET_TYPEOF(a, DOTSXP);
-	    f=a;
-	    for(b=supplied;b!=R_NilValue;b=CDR(b))
+	    f = a;
+	    for(b = supplied; b != R_NilValue; b = CDR(b))
 		if(!ARGUSED(b)) {
 		    SETCAR(f, CAR(b));
 		    SET_TAG(f, TAG(b));
-		    f=CDR(f);
+		    f = CDR(f);
 		}
 	    SETCAR(dots, a);
 	}
-    }
-    else {
+    } else {
 	/* Check that all arguments are used */
 	SEXP unused = R_NilValue, last = R_NilValue;
 	for (b = supplied; b != R_NilValue; b = CDR(b))
-	    /* Uncomment to allow unmatched empty args, as done < 2.4.0 */
-	    if (!ARGUSED(b)/* && CAR(b) != R_MissingArg) */) {
+	    if (!ARGUSED(b)) {
 		if(last == R_NilValue) {
 		    PROTECT(unused = CONS(CAR(b), R_NilValue));
 		    SET_TAG(unused, TAG(b));
@@ -369,20 +358,18 @@ SEXP attribute_hidden matchArgs(SEXP formals, SEXP supplied, SEXP call)
 
 	if(last != R_NilValue) {
             /* show bad arguments in call without evaluating them */
-            SEXP unusedForError = R_NilValue, last = R_NilValue ;
+            SEXP unusedForError = R_NilValue, last = R_NilValue;
+
             for(b = unused ; b != R_NilValue ; b = CDR(b)) {
-                SEXP tagB = TAG(b) ;
-                SEXP carB = CAR(b) ;
-                if (TYPEOF(carB) == PROMSXP) {
-                    carB = PREXPR(carB) ;
-                }
+                SEXP tagB = TAG(b), carB = CAR(b) ;
+                if (TYPEOF(carB) == PROMSXP) carB = PREXPR(carB) ;
                 if (last == R_NilValue) {
                     PROTECT(last = CONS(carB, R_NilValue));
                     SET_TAG(last, tagB);
-                    unusedForError = last ;
+                    unusedForError = last;
                 } else {
                     SETCDR(last, CONS(carB, R_NilValue));
-                    last = CDR(last) ;
+                    last = CDR(last);
                     SET_TAG(last, tagB);
                 }
             }
