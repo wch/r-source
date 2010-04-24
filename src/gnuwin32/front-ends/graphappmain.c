fd906781e20daf284aa6cb7e12127e46e9e80563
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2010-04-24 15:49:53 +0000

    allow MinGW-w64 32-bit build
    git-svn-id: https://svn.r-project.org/R/trunk@51816 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/front-ends/graphappmain.c
+++ src/gnuwin32/front-ends/graphappmain.c
@@ -15,7 +15,7 @@ WinMain (HINSTANCE Instance, HINSTANCE PrevInstance, LPSTR CmdLine,
 {
     extern void AppMain(int argc, char **argv);
 
-#ifdef WIN64
+#ifdef W64
     extern int __argc;
     extern char **__argv;
 
