f708d13a8640fed77c92307d4055d5100a315950
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2010-05-11 06:23:00 +0000

    argh, vars.mk is not just used as a Makefile
    git-svn-id: https://svn.r-project.org/R/trunk@51973 00db46b3-68df-0310-9c12-caf00c1e9a41

--- share/make/vars.mk
+++ share/make/vars.mk
@@ -3,7 +3,6 @@
 
 R_PKGS_BASE = base tools utils grDevices graphics stats datasets methods grid splines stats4 tcltk
 
-R_PKGS_RECOMMENDED =  boot cluster codetools foreign KernSmooth lattice mgcv nlme rpart survival MASS class nnet spatial Matrix
 R_PKGS_RECOMMENDED =  MASS lattice Matrix nlme survival boot cluster codetools foreign KernSmooth rpart class nnet spatial mgcv
 # there are dependencies in src/library/Recommended/Makefile*
 # which this order respects
