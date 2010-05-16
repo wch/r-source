4077bf0baae6e40ec733d35e83b78646f9682ed4
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2010-05-16 09:00:56 +0000

    sort out PR#14263 -- *not* part of 2.11.0
    git-svn-id: https://svn.r-project.org/R/branches/R-2-11-branch@52008 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/dataframe.R
+++ src/library/base/R/dataframe.R
@@ -906,8 +906,14 @@ data.frame <-
                 }
             }
 	}
-    else if(p > 0L) for(jjj in p:1L) { # we might delete columns with NULL
-	jj <- jseq[jjj]
+    else if(p > 0L)
+      for(jjj in p:1L) { # we might delete columns with NULL
+        ## ... and for that reason, we'd better ensure that jseq is increasing!
+        o <- order(jseq)
+        jseq <- jseq[o]
+        jvseq <- jvseq[o]
+
+        jj <- jseq[jjj]
         v <- value[[ jvseq[[jjj]] ]]
         ## This is consistent with the have.i case rather than with
         ## [[<- and $<- (which throw an error).  But both are plausible.
