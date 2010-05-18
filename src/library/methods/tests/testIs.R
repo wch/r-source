b9db311365c2751d8a3ce77583e907f63fd1c2b3
jmc <jmc@00db46b3-68df-0310-9c12-caf00c1e9a41>
2010-05-18 17:21:17 +0000

    test fro revision 52029
    git-svn-id: https://svn.r-project.org/R/trunk@52030 00db46b3-68df-0310-9c12-caf00c1e9a41

---
+++ src/library/methods/tests/testIs.R
@@ -0,0 +1,28 @@
+## test (non-conditional) explicit inheritance
+setClass("xy", representation(x="numeric", y="numeric"))
+
+setIs("xy", "complex",
+      coerce = function(from) complex(real = from@x, imaginary = from@y),
+      replace = function(from, value) {
+          from@x <- Re(value)
+          from@y <- Im(value)
+          from
+      })
+
+set.seed(124)
+x1 <- rnorm(10)
+y1 <- rnorm(10)
+cc <- complex(real = x1, imaginary=y1)
+xyc <- new("xy", x = x1, y = y1)
+stopifnot(identical(cc, as(xyc, "complex")))
+as(xyc, "complex") <- cc * 1i
+stopifnot(identical(xyc, new("xy", x = -y1, y = x1)))
+
+setGeneric("size", function(x)standardGeneric("size"))
+
+setMethod("size", "vector", function(x)length(x))
+
+## class "xy" should inherit the vector method through complex
+stopifnot(identical(size(xyc), length(x1)))
+removeClass("xy")
+removeGeneric("size")
