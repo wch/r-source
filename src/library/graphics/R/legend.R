4d36cadedbc884eda6bfed8892b8759b3e952975
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2010-06-14 19:00:36 +0000

    enhancement of legend()
    git-svn-id: https://svn.r-project.org/R/trunk@52278 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/graphics/R/legend.R
+++ src/library/graphics/R/legend.R
@@ -23,7 +23,7 @@ function(x, y = NULL, legend, fill=NULL, col = par("col"), border="black",
 	 text.width = NULL, text.col = par("col"),
 	 merge = do.lines && has.pch, trace = FALSE,
 	 plot = TRUE, ncol = 1, horiz = FALSE, title = NULL,
-	 inset = 0, xpd, title.col = text.col)
+	 inset = 0, xpd, title.col = text.col, title.adj = 0.5)
 {
     ## the 2nd arg may really be `legend'
     if(missing(legend) && !missing(y) &&
@@ -260,8 +260,8 @@ function(x, y = NULL, legend, fill=NULL, col = par("col"), border="black",
     xt <- xt + x.intersp * xchar
     if(plot) {
 	if (!is.null(title))
-            text2(left + w/2, top - ymax, labels = title,
-                  adj = c(0.5, 0), cex = cex, col = title.col)
+            text2(left + w*title.adj, top - ymax, labels = title,
+                  adj = c(title.adj, 0), cex = cex, col = title.col)
 
 	text2(xt, yt, labels = legend, adj = adj, cex = cex, col = text.col)
     }
