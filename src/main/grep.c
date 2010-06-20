d7e46310d9d5e1ce4c67d4b6f286a32433b16981
murdoch <murdoch@00db46b3-68df-0310-9c12-caf00c1e9a41>
2010-06-20 17:18:53 +0000

    Port r52340 from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-2-11-branch@52341 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/grep.c
+++ src/main/grep.c
@@ -328,8 +328,12 @@ SEXP attribute_hidden do_strsplit(SEXP call, SEXP op, SEXP args, SEXP env)
 		    }
 		    bufp = laststart;
 		}
-		if (*bufp)
-		    SET_STRING_ELT(t, ntok, markKnown(bufp, STRING_ELT(x, i)));
+		if (*bufp) {
+		    if (use_UTF8)
+		        SET_STRING_ELT(t, ntok, mkCharCE(bufp, CE_UTF8));
+		    else
+		    	SET_STRING_ELT(t, ntok, markKnown(bufp, STRING_ELT(x, i)));
+		}
 		vmaxset(vmax2);
 	    }
 	} else if (perl_opt) {
@@ -428,8 +432,12 @@ SEXP attribute_hidden do_strsplit(SEXP call, SEXP op, SEXP args, SEXP env)
 		    else
 			SET_STRING_ELT(t, j, markKnown(pt, STRING_ELT(x, i)));
 		}
-		if (*bufp)
-		    SET_STRING_ELT(t, ntok, markKnown(bufp, STRING_ELT(x, i)));
+		if (*bufp) {
+		    if (use_UTF8)
+		        SET_STRING_ELT(t, ntok, mkCharCE(bufp, CE_UTF8));
+		    else
+		    	SET_STRING_ELT(t, ntok, markKnown(bufp, STRING_ELT(x, i)));
+		}
 		vmaxset(vmax2);
 	    }
 	    pcre_free(re_pe);
