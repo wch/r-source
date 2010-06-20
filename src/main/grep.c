5ef765b6bcc1a5b53b6df6dc82aa5bb893b1e908
murdoch <murdoch@00db46b3-68df-0310-9c12-caf00c1e9a41>
2010-06-20 17:01:47 +0000

    Fix strsplit bug, and remove workaround in r52333.
    git-svn-id: https://svn.r-project.org/R/trunk@52340 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/grep.c
+++ src/main/grep.c
@@ -329,8 +329,12 @@ SEXP attribute_hidden do_strsplit(SEXP call, SEXP op, SEXP args, SEXP env)
 		    }
 		    bufp = laststart;
 		}
-		if (*bufp)
-		    SET_STRING_ELT(t, ntok, markKnown(bufp, STRING_ELT(x, i)));
+		if (*bufp) {
+		    if (use_UTF8)
+		        SET_STRING_ELT(t, ntok, mkCharCE(bufp, CE_UTF8));
+		    else
+		    	SET_STRING_ELT(t, ntok, markKnown(bufp, STRING_ELT(x, i)));
+		}
 		vmaxset(vmax2);
 	    }
 	} else if (perl_opt) {
@@ -429,8 +433,12 @@ SEXP attribute_hidden do_strsplit(SEXP call, SEXP op, SEXP args, SEXP env)
 		    else
 			SET_STRING_ELT(t, j, markKnown(pt, STRING_ELT(x, i)));
 		}
-		if (*bufp)
-		    SET_STRING_ELT(t, ntok, markKnown(bufp, STRING_ELT(x, i)));
+		if (*bufp) {
+		    if (use_UTF8)
+		        SET_STRING_ELT(t, ntok, mkCharCE(bufp, CE_UTF8));
+		    else
+		    	SET_STRING_ELT(t, ntok, markKnown(bufp, STRING_ELT(x, i)));
+		}
 		vmaxset(vmax2);
 	    }
 	    pcre_free(re_pe);
