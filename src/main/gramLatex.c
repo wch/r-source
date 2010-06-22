1d59da615a9f91046947bfd216904ee0a7d4fdca
murdoch <murdoch@00db46b3-68df-0310-9c12-caf00c1e9a41>
2010-06-22 16:50:36 +0000

    Correct handling of comments in LaTeX
    git-svn-id: https://svn.r-project.org/R/trunk@52359 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/gramLatex.c
+++ src/main/gramLatex.c
@@ -2409,6 +2409,7 @@ static int mkComment(int c)
     while ((c = xxgetc()) != '\n' && c != R_EOF);
     
     if (c == R_EOF) xxungetc(c);
+    else TEXT_PUSH(c);
     
     PROTECT(yylval = mkString2(stext,  bp - stext));
     if(stext != st0) free(stext);    
