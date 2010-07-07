cc5af8ccf6d63ae8aaa6d52e61d4d44e52aca2b5
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2010-07-07 18:50:15 +0000

    Make .Rd_get_xrefs() also extract \linkS4class.
    git-svn-id: https://svn.r-project.org/R/trunk@52476 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/Rd.R
+++ src/library/tools/R/Rd.R
@@ -663,12 +663,17 @@ function(x)
 {
     out <- matrix(character(), nrow = 0L, ncol = 2L)
     recurse <- function(e) {
-        if(identical(attr(e, "Rd_tag"), "\\link")) {
+        tag <- attr(e, "Rd_tag")
+        if(identical(tag, "\\link")) {
             val <- if(length(e)) { # mvbutils has empty links
                 arg <- as.character(e[[1L]])
                 opt <- attr(e, "Rd_option")
                 c(arg, if(is.null(opt)) "" else as.character(opt))
             } else c("", "")
+            out <<- rbind(out, val)
+        } else if(identical(tag, "\\linkS4class")) {
+            arg <- as.character(e[[1L]])
+            val <- c(arg, sprintf("=%s-class", arg))
             out <<- rbind(out, val)
         }
         if(is.list(e)) lapply(e, recurse)
