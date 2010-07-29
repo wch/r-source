8ce11722dccb929136562c4bb1410e8896fd0a77
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2010-07-29 08:07:58 +0000

    update
    git-svn-id: https://svn.r-project.org/R/trunk@52632 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/check.R
+++ src/library/tools/R/check.R
@@ -2272,9 +2272,9 @@ R_run_R <- function(cmd, Ropts, env)
     ## examples and tests are not.
     R_opts <- "--vanilla"
     R_opts2 <- "--vanilla --slave"
-    ## do run Renviron[.site]
-    R_opts3 <- "--no-site-file --no-init-file --no-restore"
-    R_opts4 <- "--no-site-file --no-init-file --no-restore --slave"
+    ## do run Renviron[.site] for some multiarch runs
+    R_opts3 <- "--no-site-file --no-init-file --no-restore --no-save --no-restore"
+    R_opts4 <- "--no-site-file --no-init-file --no-restore --no-save --no-restore --slave"
 
     msg_DESCRIPTION <- c("See the information on DESCRIPTION files",
                          " in the chapter 'Creating R packages'",
