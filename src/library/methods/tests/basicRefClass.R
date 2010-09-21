6fd2f7538225a6ae4faaf5ee7496e6329474fd4f
jmc <jmc@00db46b3-68df-0310-9c12-caf00c1e9a41>
2010-09-21 21:54:35 +0000

    simplification of ReferenceMethods to eliminate special arguments to setRefClass and generic referenceMethods
    git-svn-id: https://svn.r-project.org/R/trunk@52960 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/methods/tests/basicRefClass.R
+++ src/library/methods/tests/basicRefClass.R
@@ -1,10 +1,19 @@
 options(error = recover)
+## simple call, only field names
+fg <- setRefClass("foo", c("bar", "flag"))
+f1<- new("foo")
+f1$bar
+f1 <- fg$new(flag = "testing")
+f1$bar <- 1
+stopifnot(identical(f1$bar, 1))
+fg$methods(showAll = function() c(bar, flag))
+stopifnot(all.equal(f1$showAll(), c(1, "testing")))
+
 fg <- setRefClass("foo", list(bar = "numeric", flag = "character"),
-            fieldPrototypes = list(flag = "standard flag"),
-            refMethods = list(
+            methods = list(
             addToBar = function(incr) {
-                b = getBar() + incr
-                setBar(b)
+                b = bar + incr
+                bar <<- b
                 b
             }
             ))
@@ -18,14 +27,22 @@ f2 <- fg$new(bar = pi)
 ## identical does not return TRUE if *contents* of env are identical
 stopifnot(identical(ff$bar, f2$bar), identical(ff$flag, f2$flag))
 
+f2$flag <- "standard flag"
+stopifnot(identical(f2$flag, "standard flag"))
 
-stopifnot(identical(ff$flag, "standard flag"))
+## fg$lock("flag")
+## tryCatch(f2$flag <- "other", error = function(e)e)
+
+
+## add some accessor methods
+fg$accessors("bar")
 
 ff$setBar(1:3)
-stopifnot(identical(ff$bar, 1:3))
+stopifnot(identical(ff$getBar(), 1:3))
 
 ff$getBar()
 stopifnot(all.equal(ff$addToBar(1), 2:4))
+
 
 ## Add a method
 fg$methods(barTimes = function(x) {
@@ -50,11 +67,22 @@ stopifnot(is(tryCatch(setRefClass("foo2", list(b2 = "numeric", flag = "complex")
 setClass("ratedChar", contains = "character", representation(score = "numeric"))
 foo2 <- setRefClass("foo2", list(b2 = "numeric", flag = "ratedChar"),
             contains = "foo",
-            refMethods = list(addBoth = function(incr) {
+            methods = list(addBoth = function(incr) {
                 addToBar(incr) #uses inherited class method
-                setB2(getB2() + incr)
+                b2<<- b2 + incr
                 }))
+## now lock the flag field; should still allow one write
+foo2$lock("flag")
 f2 <- foo2$new(bar = -3, flag = as("ANY", "ratedChar"), b2 = ff$bar)
+## but not a second one
+stopifnot(is(tryCatch(f2$flag <- "Try again",
+         error = function(e)e), "error"))
+f22 <- foo2$new(bar = f2$bar)
+## same story if assignment follows the initialization
+f22$flag <- f2$flag
+stopifnot(is(tryCatch(f22$flag <- "Try again",
+         error = function(e)e), "error"))
+## Exporting superclass object
 f22 <- fg$new(bar = f2$bar, flag = f2$flag)
 f2e <- f2$export("foo")
 stopifnot(identical(f2e$bar, f22$bar), identical(f2e$flag, f22$flag),
@@ -64,17 +92,39 @@ stopifnot(identical(f2$flag,  as("ANY", "ratedChar")), identical(f2$bar, -3),
 f2$addBoth(-1)
 stopifnot(all.equal(f2$bar, -4), all.equal(f2$b2, 1:3+0))
 
-setRefClass("foo3", contains = "foo2",
-            refMethods = list(addBoth = function(incr) {
+## test callSuper()
+setRefClass("foo3", fields = list(flag2 = "ratedChar"),
+            contains = "foo2",
+            methods = list(addBoth = function(incr) {
                 callSuper(incr)
-                setFlag(as(paste(getFlag(), paste(incr, collapse = ", "), sep = "; "), "ratedChar"))
+                flag2 <<- as(paste(flag, paste(incr, collapse = ", "), sep = "; "), "ratedChar")
                 incr
             }))
 
+f2 <- foo2$new(bar = -3, flag = as("ANY", "ratedChar"), b2 =  1:3)
 f3 <- new("foo3")
 f3$import(f2)
 stopifnot(all.equal(f3$b2, f2$b2), all.equal(f3$bar, f2$bar), all.equal(f3$flag, f2$flag))
 f3$addBoth(1)
-stopifnot(all.equal(f3$bar, -3), all.equal(f3$b2, 2:4+0),
-          all.equal(f3$flag, as("ANY; 1", "ratedChar")))
+stopifnot(all.equal(f3$bar, -2), all.equal(f3$b2, 2:4+0),
+          all.equal(f3$flag2, as("ANY; 1", "ratedChar")))
 
+## but the import should have used up the one write for $flag
+stopifnot(is(tryCatch(f3$flag <- "Try again",
+         error = function(e)e), "error"))
+
+## a class with an initialize method, and an extra slot
+setOldClass(c("simple.list", "list"))
+fg4 <- setRefClass("foo4",
+            contains = "foo2",
+            methods = list(
+              initialize = function(...) {
+                  .self <- initFields(...)
+                  .self@made = R.version
+                  .self
+              }),
+            representation = list(made = "simple.list")
+            )
+
+f4 <- new("foo4", flag = "another test", bar = 1:3)
+stopifnot(identical(f4@made, R.version))
