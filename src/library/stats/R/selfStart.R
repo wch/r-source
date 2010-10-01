c3985ad72f08e0c01f455744ab082b925fbde889
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2010-10-01 22:14:15 +0000

    port r53111 from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-2-12-branch@53112 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/R/selfStart.R
+++ src/library/stats/R/selfStart.R
@@ -156,19 +156,17 @@ NLSstClosestX.sortedXyData <-
     function(xy, yval)
 {
     deviations <- xy$y - yval
+    if (any(deviations==0)) # PR#14384
+        return(xy$x[match(0, deviations)])
     if (any(deviations <= 0)) {
         dev1 <- max(deviations[deviations <= 0])
         lim1 <- xy$x[match(dev1, deviations)]
-        if (all(deviations <= 0)) {
-            return(lim1)
-        }
+        if (all(deviations <= 0)) return(lim1)
     }
     if (any(deviations >= 0)) {
         dev2 <- min(deviations[deviations >= 0])
         lim2 <- xy$x[match(dev2, deviations)]
-        if (all(deviations >= 0)) {
-            return(lim2)
-        }
+        if (all(deviations >= 0)) return(lim2)
     }
     dev1 <- abs(dev1)
     dev2 <- abs(dev2)
