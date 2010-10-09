86f5d2760aa916d33cd8dbd0125ebda01b050f58
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2010-10-09 09:08:27 +0000

    protect va_list when including C header in C++
    git-svn-id: https://svn.r-project.org/R/trunk@53253 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/include/R_ext/Print.h
+++ src/include/R_ext/Print.h
@@ -21,9 +21,13 @@
 #define R_EXT_PRINT_H_
 
 #ifdef  __cplusplus
-# include <cstdarg>
-# ifdef __SUNPRO_CC
+/* If the vprintf interface is defined at all in C++ it may only be
+   defined in namespace std. */
+# ifdef R_USE_C99_IN_CXX
+#  include <cstdarg>
+#  ifdef __SUNPRO_CC
 using _STLP_VENDOR_CSTD::va_list;
+#  endif
 # endif
 extern "C" {
 #else
@@ -32,8 +36,10 @@ extern "C" {
 
 void Rprintf(const char *, ...);
 void REprintf(const char *, ...);
+#if !defined(__cplusplus) || defined R_USE_C99_IN_CXX
 void Rvprintf(const char *, va_list);
 void REvprintf(const char *, va_list);
+#endif
 
 #ifdef  __cplusplus
 }
