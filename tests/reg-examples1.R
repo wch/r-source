9ba1e49839d84932f460e97f9b9a810387977b76
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2010-10-13 13:15:53 +0000

    split up reg-examples
    git-svn-id: https://svn.r-project.org/R/trunk@53294 00db46b3-68df-0310-9c12-caf00c1e9a41

--- tests/reg-examples1.R
+++ tests/reg-examples1.R
@@ -33,34 +33,8 @@ example(news)
 example(packageDescription)
 example(sessionInfo)
 
-## stats
-example(SSasympOrig)
-example(SSlogis)
-example(constrOptim)
-example(aov)
-example(family)
-example(glm)
-example(glm.control)
-# from extractAIC
-extractAIC(glm.D93)
-example(influence.measures)
-example(lm)
-example(ls.diag)
-example(model.tables)
-example(nlminb)
-example(optim)
-example(step)
-example(summary.manova)
-example(uniroot)
-
 ## datasets
 example(JohnsonJohnson)
-
-library(tools)
-example(Rdutils)
-example(fileutils)
-## results are location- and OS-specific
-example(parseLatex) # charset-specific
 
 ## grDevices
 if(.Platform$OS.type == "windows") {
@@ -69,3 +43,11 @@ if(.Platform$OS.type == "windows") {
     example(X11Fonts)
     example(quartzFonts)
 }
+
+library(tools)
+example(Rdutils)
+example(fileutils)
+## results are location- and OS-specific
+example(parseLatex) # charset-specific
+
+proc.time()
---
+++ tests/reg-examples2.R
@@ -0,0 +1,26 @@
+## For examples skipped in testing because they are 'random'
+
+set.seed(1)
+if(.Platform$OS.type == "windows") options(pager = "console")
+
+## stats
+example(SSasympOrig)
+example(SSlogis)
+example(constrOptim)
+example(aov)
+example(family)
+example(glm)
+example(glm.control)
+# from extractAIC
+extractAIC(glm.D93)
+example(influence.measures)
+example(lm)
+example(ls.diag)
+example(model.tables)
+example(nlminb)
+example(optim)
+example(step)
+example(summary.manova)
+example(uniroot)
+
+proc.time()
