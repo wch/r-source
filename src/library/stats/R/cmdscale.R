810bcd129a6ae393e8b985d5a9ad1e1bc11c77eb
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2010-10-15 19:44:21 +0000

    change incorrect return value if eig=TRUE, update refs
    git-svn-id: https://svn.r-project.org/R/branches/R-2-12-branch@53332 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/R/cmdscale.R
+++ src/library/stats/R/cmdscale.R
@@ -64,7 +64,7 @@ cmdscale <- function (d, k = 2, eig = FALSE, add = FALSE, x.ret = FALSE)
     dimnames(points) <- list(rn, NULL)
     if (eig || x.ret || add) {
         evalus <- e$values[-n]
-        list(points = points, eig = if(eig) ev, x = if(x.ret) x,
+        list(points = points, eig = if(eig) e$values, x = if(x.ret) x,
              ac = if(add) add.c else 0,
              GOF = sum(ev)/c(sum(abs(evalus)), sum(evalus[evalus > 0])))
     } else points
