f8d69b5aff8c6145c5b7ac74f8e2e8a0ba0a6feb
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2010-10-19 17:32:39 +0000

    close file if URL cannot be opened
    git-svn-id: https://svn.r-project.org/R/trunk@53369 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/modules/internet/internet.c
+++ src/modules/internet/internet.c
@@ -431,7 +431,6 @@ static SEXP in_do_download(SEXP call, SEXP op, SEXP args, SEXP env)
 #endif
 	    }
 	    in_R_HTTPClose(ctxt);
-	    fclose(out);
 	    if(!quiet) {
 #ifndef Win32
 		REprintf("\n");
@@ -455,6 +454,7 @@ static SEXP in_do_download(SEXP call, SEXP op, SEXP args, SEXP env)
 		warning(_("downloaded length %d != reported length %d"),
 			nbytes, total);
 	}
+	fclose(out);
 	R_Busy(0);
 	if (status == 1) error(_("cannot open URL '%s'"), url);
 
@@ -536,7 +536,6 @@ static SEXP in_do_download(SEXP call, SEXP op, SEXP args, SEXP env)
 #endif
 	    }
 	    in_R_FTPClose(ctxt);
-	    fclose(out);
 	    if(!quiet) {
 #ifndef Win32
 		REprintf("\n");
@@ -561,6 +560,7 @@ static SEXP in_do_download(SEXP call, SEXP op, SEXP args, SEXP env)
 			nbytes, total);
 	}
 	R_Busy(0);
+	fclose(out);
 	if (status == 1) error(_("cannot open URL '%s'"), url);
 #endif
 
