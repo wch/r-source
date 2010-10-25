37b4fb9e0414c02a11401a48365431e0a859e258
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2010-10-25 17:52:42 +0000

    port r53423 from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-2-12-branch@53424 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/dynload.c
+++ src/gnuwin32/dynload.c
@@ -167,7 +167,8 @@ static DL_FUNC getRoutine(DllInfo *info, char const *name)
 
 static void R_getDLLError(char *buf, int len)
 {
-    LPVOID lpMsgBuf;
+    LPSTR lpMsgBuf, p;
+    char *q;
     FormatMessage(
 	FORMAT_MESSAGE_ALLOCATE_BUFFER |
 	FORMAT_MESSAGE_FROM_SYSTEM |
@@ -175,12 +176,14 @@ static void R_getDLLError(char *buf, int len)
 	NULL,
 	GetLastError(),
 	MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-	(LPTSTR) &lpMsgBuf,
+	&lpMsgBuf,
 	0,
 	NULL
 	);
     strcpy(buf, "LoadLibrary failure:  ");
-    strcat(buf, lpMsgBuf);
+    q = buf + strlen(buf);
+    /* It seems that Win 7 returns error messages with CRLF terminators */
+    for (p = lpMsgBuf; *p; p++) if (*p != '\r') *q++ = *p;
     LocalFree(lpMsgBuf);
 }
 
