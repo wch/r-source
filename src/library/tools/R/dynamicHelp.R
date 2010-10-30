11bc6d9e4ad53a4f19f4c181239cfdd62c1b3c55
murdoch <murdoch@00db46b3-68df-0310-9c12-caf00c1e9a41>
2010-10-30 21:12:31 +0000

    r53482 added "hash=FALSE" where it isn't wanted
    git-svn-id: https://svn.r-project.org/R/trunk@53496 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/dynamicHelp.R
+++ src/library/tools/R/dynamicHelp.R
@@ -348,7 +348,7 @@ httpdPort <- 0L
 
 startDynamicHelp <- function(start=TRUE)
 {
-    env <- environment(hash = FALSE, startDynamicHelp) # small
+    env <- environment(startDynamicHelp) 
     if(nzchar(Sys.getenv("R_DISABLE_HTTPD"))) {
         unlockBinding("httpdPort", env)
         httpdPort <<- -1L
