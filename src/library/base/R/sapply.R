09dba9f6c95fd51c1b91a9486ff213b78a935db9
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2010-11-11 18:14:38 +0000

    ported r53535 [sapply(), mapply()] from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-2-12-branch@53563 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/sapply.R
+++ src/library/base/R/sapply.R
@@ -24,11 +24,15 @@ sapply <- function(X, FUN, ..., simplify = TRUE, USE.NAMES = TRUE)
        length(common.len <- unique(unlist(lapply(answer, length)))) == 1L) {
 	if(common.len == 1L)
 	    unlist(answer, recursive = FALSE)
-	else if(common.len > 1L)
-	    array(unlist(answer, recursive = FALSE),
-		  dim= c(common.len, length(X)),
-		  dimnames= if(!(is.null(n1 <- names(answer[[1L]])) &
-			         is.null(n2 <- names(answer)))) list(n1,n2))
+	else if(common.len > 1L) {
+	    ## make sure that array(*) will not call rep() {e.g. for 'call's}:
+	    r <- as.vector(unlist(answer, recursive = FALSE))
+	    if(prod(d <- c(common.len, length(X))) == length(r))
+		array(r, dim = d,
+		      dimnames= if(!(is.null(n1 <- names(answer[[1L]])) &
+				     is.null(n2 <- names(answer)))) list(n1,n2))
+	    else answer
+	}
 	else answer
     } else answer
 }
