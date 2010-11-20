9a3c86471e7ebde110e66516de44eda6d03c41a4
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2010-11-20 21:17:10 +0000

    IQR(*, type) -- correct version this time
    git-svn-id: https://svn.r-project.org/R/trunk@53644 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/R/quantile.R
+++ src/library/stats/R/quantile.R
@@ -113,6 +113,6 @@ quantile.default <-
     } else qs
 }
 
-IQR <- function (x, na.rm = FALSE)
+IQR <- function (x, na.rm = FALSE, type = 7)
     diff(quantile(as.numeric(x), c(0.25, 0.75), na.rm=na.rm, names = FALSE,
-		  type = 7))
+		  type = type))
