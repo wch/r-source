05fdfd8b1b4642add681764a39b9cb0668d3f2d8
murdoch <murdoch@00db46b3-68df-0310-9c12-caf00c1e9a41>
2010-11-29 15:49:09 +0000

    Port of r53703 from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-2-12-branch@53704 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/namespace.R
+++ src/library/base/R/namespace.R
@@ -1200,7 +1200,7 @@ parseNamespaceFile <- function(package, package.lib, mustExist = TRUE)
                        stop("too many 'S3method' directives", call. = FALSE)
                    S3methods[nS3, seq_along(spec)] <<- asChar(spec)
                },
-               warning(gettextf("unknown namespace directive: %s", deparse(e)),
+               warning(gettextf("unknown namespace directive: %s", deparse(e, nlines=1L)),
                     call. = FALSE, domain = NA)
                )
     }
