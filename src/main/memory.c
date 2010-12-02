091b5a5b0d7d436445079a9a5ca530960a60d5dc
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2010-12-02 13:48:10 +0000

    safer format on error
    git-svn-id: https://svn.r-project.org/R/trunk@53742 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/memory.c
+++ src/main/memory.c
@@ -2438,8 +2438,9 @@ void *R_chk_calloc(size_t nelem, size_t elsize)
 	return(NULL);
 #endif
     p = calloc(nelem, elsize);
-    if(!p) error(_("Calloc could not allocate (%u of %u) memory"),
-		 nelem, elsize);
+    if(!p) /* problem here is that we don't have a format for size_t. */
+	error(_("Calloc could not allocate memory (%.0f of %u bytes)"),
+	      (double) nelem, elsize);
     return(p);
 }
 
@@ -2448,7 +2449,9 @@ void *R_chk_realloc(void *ptr, size_t size)
     void *p;
     /* Protect against broken realloc */
     if(ptr) p = realloc(ptr, size); else p = malloc(size);
-    if(!p) error(_("Realloc could not re-allocate (size %u) memory"), size);
+    if(!p)
+	error(_("Realloc could not re-allocate memory (%.0f bytes)"), 
+	      (double) size);
     return(p);
 }
 
