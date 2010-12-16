fedeb2397315f4314ab17162ed2000da35ac8016
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2010-12-16 15:41:51 +0000

    Added R_ext/MathThreads.h to make R_num_math_threads and R_max_num_math_threads available to packages.
    git-svn-id: https://svn.r-project.org/R/trunk@53858 00db46b3-68df-0310-9c12-caf00c1e9a41

---
+++ src/include/R_ext/MathThreads.h
@@ -0,0 +1,35 @@
+/*
+ *  R : A Computer Language for Statistical Data Analysis
+ *  Copyright (C) 2000, 2001 The R Development Core Team.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation; either version 2.1 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, a copy is available at
+ *  http://www.r-project.org/Licenses/
+ */
+
+#ifndef R_EXT_MATHTHREADS_H_
+#define R_EXT_MATHTHREADS_H_
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
+#include <R_ext/libextern.h>
+LibExtern int R_num_math_threads;
+LibExtern int R_max_num_math_threads;
+
+#ifdef  __cplusplus
+}
+#endif
+
+#endif /* R_EXT_MATHTHREADS_H_ */
