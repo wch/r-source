428ae129e0d35554814afaebe01dd200ec208f03
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2011-01-13 22:12:21 +0000

    Added PROTECT calls for some constructed expressions used in C level eval() calls.
    git-svn-id: https://svn.r-project.org/R/trunk@53967 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/unix/sys-std.c
+++ src/unix/sys-std.c
@@ -735,6 +735,7 @@ R_custom_completion(const char *text, int start, int end)
 				       mkString(rl_line_buffer))),
 	startCall = PROTECT(lang2(RComp_assignStartSym, ScalarInteger(start))),
 	endCall = PROTECT(lang2(RComp_assignEndSym,ScalarInteger(end)));
+    SEXP filecompCall;
 
     /* Don't want spaces appended at the end.  Need to do this
        everytime, as readline>=6 resets it to ' ' */
@@ -745,9 +746,10 @@ R_custom_completion(const char *text, int start, int end)
     eval(endCall, rcompgen_rho);
     UNPROTECT(3);
     matches = rl_completion_matches(text, R_completion_generator);
-    infile = PROTECT(eval(lang1(RComp_getFileCompSym), rcompgen_rho));
+    filecompCall = PROTECT(lang1(RComp_getFileCompSym));
+    infile = PROTECT(eval(filecompCall, rcompgen_rho));
     if (!asLogical(infile)) rl_attempted_completion_over = 1;
-    UNPROTECT(1);
+    UNPROTECT(2);
     return matches;
 }
 
