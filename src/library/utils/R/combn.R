70fb4a5153a798a9b5008090070512a12b59db29
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2011-01-17 11:55:34 +0000

    ported r53995 [combn(n, 0)] from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-2-12-branch@54001 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/utils/R/combn.R
+++ src/library/utils/R/combn.R
@@ -32,8 +32,6 @@ combn <- function(x, m, FUN = NULL, simplify = TRUE, ...)
     stopifnot(length(m) == 1L)
     if(m < 0)
 	stop("m < 0")
-    if(m == 0)
-	return(if(simplify) vector(mode(x), 0L) else list())
     if(is.numeric(x) && length(x) == 1L && x > 0 && trunc(x) == x)
 	x <- seq_len(x)
     n <- length(x)
@@ -42,7 +40,7 @@ combn <- function(x, m, FUN = NULL, simplify = TRUE, ...)
     m <- as.integer(m)
     e <- 0
     h <- m
-    a <- 1L:m
+    a <- seq_len(m)
     nofun <- is.null(FUN)
     if(!nofun && !is.function(FUN))
 	stop("'FUN' must be a function or NULL")
@@ -82,24 +80,26 @@ combn <- function(x, m, FUN = NULL, simplify = TRUE, ...)
 	out[[1L]] <- r
     }
 
-    i <- 2L
-    nmmp1 <- n - m + 1L # using 1L to keep integer arithmetic
-    while(a[1L] != nmmp1) {
-	if(e < n - h) {
-	    h <- 1L
-	    e <- a[m]
-	    j <- 1L
+    if(m > 0) {
+	i <- 2L
+	nmmp1 <- n - m + 1L	 # using 1L to keep integer arithmetic
+	while(a[1L] != nmmp1) {
+	    if(e < n - h) {
+		h <- 1L
+		e <- a[m]
+		j <- 1L
+	    }
+	    else {
+		e <- a[m - h]
+		h <- h + 1L
+		j <- 1L:h
+	    }
+	    a[m - h + j] <- e + j
+	    r <- if(nofun) x[a] else FUN(x[a], ...)
+	    if(simplify) ##S if(use.arr)
+		out[, i] <- r else out[[i]] <- r
+	    i <- i + 1L
 	}
-	else {
-	    e <- a[m - h]
-	    h <- h + 1L
-	    j <- 1L:h
-	}
-	a[m - h + j] <- e + j
-	r <- if(nofun) x[a] else FUN(x[a], ...)
-	if(simplify) ##S if(use.arr)
-	    out[, i] <- r else out[[i]] <- r
-	i <- i + 1L
     }
     if(simplify) ##S if(use.arr)
 	array(out, dim.use) else out
