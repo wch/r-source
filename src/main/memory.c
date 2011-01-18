7abb31f472038a50aa90ddab93727ffa558c31a4
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2011-01-18 04:01:46 +0000

    Handle FREESXP marking of large vectors separately (temporary solution for now).
    git-svn-id: https://svn.r-project.org/R/trunk@54010 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/memory.c
+++ src/main/memory.c
@@ -846,6 +846,10 @@ static void ReleaseLargeFreeVectors(void)
 	    UNSNAP_NODE(s);
 	    R_LargeVallocSize -= size;
 	    R_GenHeap[LARGE_NODE_CLASS].AllocCount--;
+#ifdef PROTECTCHECK
+	    /*** eventually do this with the other FREESXP settings */
+	    TYPEOF(s) = FREESXP;
+#endif
 	    free(s);
 	}
 	s = next;
@@ -1476,7 +1480,7 @@ static void RunGenCollect(R_size_t size_needed)
     PROCESS_NODES();
 
 #ifdef PROTECTCHECK
-    for(i=1; i< NUM_NODE_CLASSES;i++){
+    for(i=1; i< NUM_SMALL_NODE_CLASSES;i++){
 	s = NEXT_NODE(R_GenHeap[i].New);
 	while (s != R_GenHeap[i].New) {
 	    SEXP next = NEXT_NODE(s);
@@ -1487,6 +1491,20 @@ static void RunGenCollect(R_size_t size_needed)
 	    }
 	    s = next;
 	}
+    }
+    s = NEXT_NODE(R_GenHeap[LARGE_NODE_CLASS].New);
+    while (s != R_GenHeap[i].New) {
+	SEXP next = NEXT_NODE(s);
+	if (TYPEOF(s) != NEWSXP) {
+#ifdef DODO
+	    /* can't to this yet -- need to first encode length for
+	       use in the release */
+	    TYPEOF(s) = FREESXP;
+#endif
+	    if (gc_inhibit_release)
+		FORWARD_NODE(s);
+	}
+	s = next;
     }
     if (gc_inhibit_release)
 	PROCESS_NODES();
