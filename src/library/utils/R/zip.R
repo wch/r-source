e11b9379b6fab9c3c0a2103af0414ce7ac8998db
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2011-01-24 10:24:55 +0000

    allow external unzip in unzip()
    git-svn-id: https://svn.r-project.org/R/trunk@54088 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/utils/R/zip.R
+++ src/library/utils/R/zip.R
@@ -43,13 +43,36 @@ zip.file.extract <- function(file, zipname = "R.zip",
 
 unzip <-
     function(zipfile, files = NULL, list = FALSE, overwrite = TRUE,
-             junkpaths = FALSE, exdir = ".")
+             junkpaths = FALSE, exdir = ".", unzip = "internal")
 {
-    if(!list && !missing(exdir))
-        dir.create(exdir, showWarnings = FALSE, recursive = TRUE)
-    res <- .Internal(unzip(zipfile, files, exdir, list, overwrite, junkpaths))
-    if(list) {
-        dates <- as.POSIXct(res[[3]], "%Y-%m-%d %H:%M",  tz="UTC")
-        data.frame(Name = res[[1]], Length = res[[2]], Date = dates)
-    } else invisible(attr(res, "extracted"))
+    if(identical(unzip, "internal")) {
+        if(!list && !missing(exdir))
+            dir.create(exdir, showWarnings = FALSE, recursive = TRUE)
+        res <- .Internal(unzip(zipfile, files, exdir, list, overwrite, junkpaths))
+        if(list) {
+            dates <- as.POSIXct(res[[3]], "%Y-%m-%d %H:%M",  tz="UTC")
+            data.frame(Name = res[[1]], Length = res[[2]], Date = dates)
+        } else invisible(attr(res, "extracted"))
+    } else {
+        if(!is.character(unzip) || length(unzip) != 1L || !nzchar(unzip))
+            stop("'unzip' must be a single character string")
+        if (list) {
+            res <- system2(unzip, c("-l", shQuote(zipfile)), stdout = TRUE,
+                           env = c("TZ=UTC"))
+            l <- length(res)
+            res2 <- res[-c(1,3, l-1, l)]
+            con <- textConnection(res2); on.exit(close(con))
+            z <- read.table(con, header=TRUE)
+            z[, "Date"] <- as.POSIXct(paste(z$Date, z$Time),
+                                      "%m-%d-%y %H:%M",  tz="UTC")
+            z[c("Name", "Length", "Date")]
+        } else {
+            args <- c("-oq", shQuote(zipfile))
+            if (length(files)) args <- c(args, shQuote(files))
+            if (exdir != ".") args <- c(args, "-d", shQuote(exdir))
+            ## there is an unzip clone about that does not respect -q
+            system2(unzip, args, stdout = NULL, stderr = NULL)
+            invisible(NULL)
+        }
+    }
 }
