8d5607b1910c955cdaf8a01e749237b78c513f40
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2011-02-18 10:55:18 +0000

    add a precaution if someone feeds in y, as dotchart did
    git-svn-id: https://svn.r-project.org/R/trunk@54474 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/graphics/R/lines.R
+++ src/library/graphics/R/lines.R
@@ -19,9 +19,9 @@ lines <- function(x, ...) UseMethod("lines")
 lines.default <- function(x, y=NULL, type="l", ...)
     plot.xy(xy.coords(x, y), type=type, ...)
 
-lines.table <- function (x, type = "h", lwd = 2,  ...)
+lines.table <- function (x, y = NULL, type = "h", lwd = 2,  ...)
 {
-     if (length(dim(x)) == 1L) {
+     if (!is.null(y) || length(dim(x)) == 1L) {
          nx <- dimnames(x)[[1L]]
          is.num <- suppressWarnings(!any(is.na(xx <- as.numeric(nx))))
          x0 <- if (is.num) xx else seq_along(x)
