df3ff444162c065837df4c95409c382f0f17fd7d
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2011-02-18 02:45:00 +0000

    port r54469:70 from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-2-12-branch@54471 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/check.R
+++ src/library/tools/R/check.R
@@ -1107,20 +1107,28 @@ R_runR <- function(cmd = NULL, Ropts = "", env = "",
         ## check src/Makevar*, src/Makefile* for correct use of BLAS_LIBS
         ## FLIBS is not needed on Windows, at least currently (as it is
         ## statically linked).
-        checkingLog(Log, "for portable use of $BLAS_LIBS")
+        checkingLog(Log, "for portable use of $(BLAS_LIBS) and $(LAPACK_LIBS)")
         makefiles <- Sys.glob(file.path("src",
                                         c("Makevars", "Makevars.in",
                                           "Makefile", "Makefile.win")))
         any <- FALSE
         for (f in makefiles) {
             lines <- readLines(f, warn = FALSE)
-            c1 <- grepl("PKG_LIBS", lines, useBytes = TRUE)
-            c2 <- grepl("$[{(]{0,1}BLAS_LIBS", lines, useBytes = TRUE)
-            c3 <- grepl("FLIBS", lines, useBytes = TRUE)
-            if (any(c1 && c2 && !c3)) {
+            c1 <- grepl("^[[:space:]]*PKG_LIBS", lines, useBytes = TRUE)
+            c2l <- grepl("\\$[{(]{0,1}LAPACK_LIBS", lines, useBytes = TRUE)
+            c2b <- grepl("\\$[{(]{0,1}BLAS_LIBS", lines, useBytes = TRUE)
+            c3 <- grepl("\\$[{(]{0,1}FLIBS", lines, useBytes = TRUE)
+            if (any(c1 & c2l & !c2b)) {
                 if (!any) warnLog()
                 any <- TRUE
-                printLog(Log, "apparently missing $(FLIBS) in ",
+                printLog(Log,
+                         "  apparently using $(LAPACK_LIBS) without $(BLAS_LIBS) in ",
+                         sQuote(f), "\n")
+            }
+            if (any(c1 & (c2b | c2l) & !c3)) {
+                if (!any) warnLog()
+                any <- TRUE
+                printLog(Log, "  apparently PKG_LIBS is missing $(FLIBS) in ",
                          sQuote(f), "\n")
             }
         }
