38ee6f1601c9d71ce98f37a7e713d5ce92c52713
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2011-02-18 11:00:10 +0000

    invert logic
    git-svn-id: https://svn.r-project.org/R/branches/R-2-12-branch@54476 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/graphics/R/lines.R
+++ src/library/graphics/R/lines.R
@@ -21,7 +21,7 @@ lines.default <- function(x, y=NULL, type="l", ...)
 
 lines.table <- function (x, y = NULL, type = "h", lwd = 2,  ...)
 {
-     if (!is.null(y) || length(dim(x)) == 1L) {
+     if (is.null(y) && length(dim(x)) == 1L) {
          nx <- dimnames(x)[[1L]]
          is.num <- suppressWarnings(!any(is.na(xx <- as.numeric(nx))))
          x0 <- if (is.num) xx else seq_along(x)
