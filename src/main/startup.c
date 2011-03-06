f6d488092e512a9acae92469421adbc68f56fc1a
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2011-03-06 17:33:44 +0000

    more on startup
    git-svn-id: https://svn.r-project.org/R/trunk@54680 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/startup.c
+++ src/main/startup.c
@@ -104,14 +104,18 @@ FILE *R_OpenSiteFile(void)
 	/* Saving and Restoring the Global Environment */
 
 #ifndef Win32
-static char workspace_name[100] = ".RData";
+static char workspace_name[1000] = ".RData";
 
+/*
+  set_workspace_name is in src/gnuwin32/system.c and used to implement
+  drag-and-drop on Windows.
+ */
 #else
 static char workspace_name[PATH_MAX] = ".RData";
 
 void set_workspace_name(const char *fn)
 {
-    strcpy(workspace_name, fn);
+    strncpy(workspace_name, fn, PATH_MAX);
 }
 #endif
 
