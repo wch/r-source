fa67e48db14a6e88d50ab82dac663bfec33a3d9d
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2011-03-09 07:30:13 +0000

    correct return value
    git-svn-id: https://svn.r-project.org/R/trunk@54710 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/platform.c
+++ src/main/platform.c
@@ -2119,7 +2119,7 @@ static int do_copy(const wchar_t* from, const wchar_t* name,
 	FILE *fp1 = NULL, *fp2 = NULL;
 	wchar_t buf[APPENDBUFSIZE];
 
-	nfail = 1;
+	nfail = 0;
 	nc = wcslen(to);
 	wsprintfW(dest, L"%ls%ls", to, name);
 	if (over || !R_WFileExists(dest)) { /* FIXME */
@@ -2264,7 +2264,7 @@ static int do_copy(const char* from, const char* name, const char* to,
 	FILE *fp1 = NULL, *fp2 = NULL;
 	char buf[APPENDBUFSIZE];
 
-	nfail = 1;
+	nfail = 0;
 	nc = strlen(to);
 	snprintf(dest, PATH_MAX, "%s%s", to, name);
 	if (over || !R_FileExists(dest)) {
