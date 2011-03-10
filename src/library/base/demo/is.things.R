d17909f89fa5469c2602d5acb74ca7a3f20844c7
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2011-03-10 07:23:21 +0000

    update
    git-svn-id: https://svn.r-project.org/R/trunk@54724 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/demo/is.things.R
+++ src/library/base/demo/is.things.R
@@ -1,12 +1,10 @@
 ## being a 'builtin' function is not the same as being in base
-## and what about objects starting with '.'?
 ls.base <- ls("package:base", all=TRUE)
 base.is.f <- sapply(ls.base, function(x) is.function(get(x)))
-bi <- ls.base[base.is.f]
 cat("\nNumber of base objects:\t\t", length(ls.base),
     "\nNumber of functions in base:\t", sum(base.is.f),
     "\n\t starting with 'is.' :\t  ",
-    length(is.bi <- bi[substring(bi,1,3) == "is."]), "\n", sep = "")
+    sum(grepl("^is\\.", ls.base[base.is.f])), "\n", sep = "")
 ## 0.14  : 31
 ## 0.50  : 33
 ## 0.60  : 34
