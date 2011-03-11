bcc330ca886d139981c59890813f7d22afbae699
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2011-03-11 14:04:36 +0000

    move compiler to the end of list: nothing depends on it
    git-svn-id: https://svn.r-project.org/R/trunk@54743 00db46b3-68df-0310-9c12-caf00c1e9a41

--- share/make/vars.mk
+++ share/make/vars.mk
@@ -1,7 +1,7 @@
 ## Static (i.e., not determined by configure) Make variables to be
 ## shared *and* grepped from m4/R.m4 and tools/*-recommended.
 
-R_PKGS_BASE = base tools utils compiler grDevices graphics stats datasets methods grid splines stats4 tcltk
+R_PKGS_BASE = base tools utils grDevices graphics stats datasets methods grid splines stats4 tcltk compiler
 
 R_PKGS_RECOMMENDED =  MASS lattice Matrix nlme survival boot cluster codetools foreign KernSmooth rpart class nnet spatial mgcv
 # there are dependencies in src/library/Recommended/Makefile*
