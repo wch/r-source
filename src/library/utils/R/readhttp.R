24487739c3d4f2512479fe852d40ad01dd114847
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2011-03-18 12:00:14 +0000

    be explicit about text mode
    git-svn-id: https://svn.r-project.org/R/trunk@54862 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/utils/R/readhttp.R
+++ src/library/utils/R/readhttp.R
@@ -18,7 +18,7 @@ url.show <-
     function (url,  title = url, file = tempfile(),
               delete.file = TRUE, method, ...)
 {
-    if (download.file(url, destfile = file, method = method))
+    if (download.file(url, destfile = file, method = method, mode = "w"))
         stop("transfer failure")
     file.show(file, delete.file = delete.file, title = title, ...)
 }
