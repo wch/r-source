7ddcf793479547d47913ed1cdc5537ca9a5064cf
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2011-03-18 11:40:53 +0000

    missing copyright header
    git-svn-id: https://svn.r-project.org/R/branches/R-2-13-branch@54861 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/utils/R/progressBar.R
+++ src/library/utils/R/progressBar.R
@@ -1,3 +1,19 @@
+#  File src/library/utils/R/progressBar.R
+#  Part of the R package, http://www.R-project.org
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  A copy of the GNU General Public License is available at
+#  http://www.r-project.org/Licenses/
+
 txtProgressBar <-
     function(min = 0, max = 1, initial = 0, char = "=",
              width = NA, title, label, style = 1)
