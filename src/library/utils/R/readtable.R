b50991b1436c3fd48ae20948f330f454c70c94aa
pd <pd@00db46b3-68df-0310-9c12-caf00c1e9a41>
2011-04-05 22:19:58 +0000

    text arg to read.table
    git-svn-id: https://svn.r-project.org/R/trunk@55311 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/utils/R/readtable.R
+++ src/library/utils/R/readtable.R
@@ -43,8 +43,12 @@ function(file, header = FALSE, sep = "", quote = "\"'", dec = ".",
          strip.white = FALSE, blank.lines.skip = TRUE,
          comment.char = "#", allowEscapes = FALSE, flush = FALSE,
          stringsAsFactors = default.stringsAsFactors(),
-         fileEncoding = "", encoding = "unknown")
+         fileEncoding = "", encoding = "unknown", text)
 {
+    if (missing(file) && !missing(text)) {
+	file <- textConnection(text)
+	on.exit(close(file))
+    }
     if(is.character(file)) {
         file <- if(nzchar(fileEncoding))
             file(file, "rt", encoding = fileEncoding) else file(file, "rt")
