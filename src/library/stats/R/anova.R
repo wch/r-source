a8734f8d30d5d9b7944e8aa48d0e64975f155fbf
pd <pd@00db46b3-68df-0310-9c12-caf00c1e9a41>
2011-04-08 13:55:48 +0000

    glm score tests: anova.glm/glmlist
    git-svn-id: https://svn.r-project.org/R/branches/R-glmscore@55382 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/R/anova.R
+++ src/library/stats/R/anova.R
@@ -17,13 +17,14 @@
 ## utility for anova.FOO(), FOO in "lmlist", "glm", "glmlist"
 ## depending on the ordering of the models this might get called with
 ## negative deviance and df changes.
-stat.anova <- function(table, test=c("Chisq", "F", "Cp"), scale, df.scale, n)
+stat.anova <- function(table, test=c("Rao","LRT","Chisq", "F", "Cp"), scale, df.scale, n)
 {
     test <- match.arg(test)
     dev.col <- match("Deviance", colnames(table))
-    if(is.na(dev.col)) dev.col <- match("Sum of Sq", colnames(table))
+    if (test=="Rao") dev.col <- match("Rao", colnames(table))
+    if (is.na(dev.col)) dev.col <- match("Sum of Sq", colnames(table))
     switch(test,
-	   "Chisq" = {
+	   "Rao"=,"LRT"=,"Chisq" = {
                dfs <- table[, "Df"]
                vals <- table[, dev.col]/scale * sign(dfs)
 	       vals[dfs %in% 0] <- NA
