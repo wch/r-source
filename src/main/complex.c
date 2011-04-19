ef0ebad535bdd68d92eb46f1cbd226fc80fa23ad
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2011-04-19 09:58:19 +0000

    port r55516 from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-2-13-branch@55517 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/complex.c
+++ src/main/complex.c
@@ -591,7 +591,8 @@ static Rboolean cmath1(double complex (*f)(double complex),
 	    y[i].r = NA_REAL; y[i].i = NA_REAL;
 	} else {
 	    SET_C99_COMPLEX(y, i, f(toC99(x + i)));
-	    if (ISNAN(y[i].r) || ISNAN(y[i].i)) naflag = TRUE;
+	    if ( (ISNAN(y[i].r) || ISNAN(y[i].i)) &&
+		!(ISNAN(x[i].r) || ISNAN(x[i].i)) ) naflag = TRUE;
 	}
     }
     return naflag;
@@ -713,7 +714,9 @@ SEXP attribute_hidden complex_math2(SEXP call, SEXP op, SEXP args, SEXP env)
 	    y[i].r = NA_REAL; y[i].i = NA_REAL;
 	} else {
 	    f(&y[i], &ai, &bi);
-	    if (ISNAN(y[i].r) || ISNAN(y[i].i)) naflag = TRUE;
+	    if ( (ISNAN(y[i].r) || ISNAN(y[i].i)) &&
+		 !(ISNAN(ai.r) || ISNAN(ai.i) || ISNAN(bi.r) || ISNAN(bi.i)) )
+		naflag = TRUE;
 	}
     }
     if (naflag)
