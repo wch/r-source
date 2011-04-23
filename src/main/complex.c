77ea16e7613f3e991b9a33fc9edecbefa0ca0fcb
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2011-04-23 15:43:27 +0000

    revert 0i^y changes, as too platform-dependent
    git-svn-id: https://svn.r-project.org/R/trunk@55618 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/complex.c
+++ src/main/complex.c
@@ -143,6 +143,7 @@ static R_INLINE double complex R_cpow_n(double complex X, int k)
      against the (slow) MSVCRT pow, and gets (0+0i)^Y as 0+0i for all Y.
 
   3) PPC Mac OS X crashes on powers of 0+0i (at least under Rosetta).
+  Really 0i^-1 should by Inf+NaNi, but getting that portably seems too hard.
 */
 
 static double complex mycpow (double complex X, double complex Y)
@@ -151,10 +152,7 @@ static double complex mycpow (double complex X, double complex Y)
     double yr = creal(Y), yi = cimag(Y); 
     int k;
     if (X == 0.0) {
-	if (yi == 0.0) {
-	    if (yr >= 0.0) Z = R_pow(0.0, yr); 
-	    else Z = R_pow(0.0, yr) + R_NaN*I;
-	} else Z = R_NaN + R_NaN*I;
+	if (yi == 0.0) Z = R_pow(0.0, yr); else Z = R_NaN + R_NaN*I;
     } else if (yi == 0.0 && yr == (k = (int) yr) && abs(k) <= 65536)
 	Z = R_cpow_n(X, k);
     else
