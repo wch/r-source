3b855cfc7cb1f2f9888869944932dabe71ae3bb7
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2011-05-13 09:21:27 +0000

    need to flush display for locator
    git-svn-id: https://svn.r-project.org/R/trunk@55876 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/modules/X11/devX11.c
+++ src/modules/X11/devX11.c
@@ -2507,6 +2507,7 @@ static Rboolean X11_Locator(double *x, double *y, pDevDesc dd)
     int done = 0;
 
     if (xd->type > WINDOW) return 0;
+    if (xd->buffered) Cairo_update(xd);
     R_ProcessX11Events((void*)NULL);	/* discard pending events */
     XDefineCursor(display, xd->window, cross_cursor);
     XSync(display, 1);
