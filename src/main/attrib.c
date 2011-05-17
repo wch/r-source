2941a1f98363082a218a4bff1a818a7f4ff58573
jmc <jmc@00db46b3-68df-0310-9c12-caf00c1e9a41>
2011-05-17 23:37:38 +0000

    add const declaration to avoid compile warning
    git-svn-id: https://svn.r-project.org/R/trunk@55946 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/attrib.c
+++ src/main/attrib.c
@@ -759,7 +759,7 @@ SEXP attribute_hidden do_namesgets(SEXP call, SEXP op, SEXP args, SEXP env)
     if (NAMED(CAR(args)) == 2)
 	SETCAR(args, duplicate(CAR(args)));
     if(IS_S4_OBJECT(CAR(args))) {
-	char *klass = CHAR(STRING_ELT(R_data_class(CAR(args), FALSE), 0));
+	const char *klass = CHAR(STRING_ELT(R_data_class(CAR(args), FALSE), 0));
 	if(getAttrib(CAR(args), R_NamesSymbol) == R_NilValue) {
 	    /* S4 class w/o a names slot or attribute */
 	    if(TYPEOF(CAR(args)) == S4SXP)
