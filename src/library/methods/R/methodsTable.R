70ea474f2654d97af3f0df5a06af703bbbad25b1
jmc <jmc@00db46b3-68df-0310-9c12-caf00c1e9a41>
2011-06-08 16:43:51 +0000

    install overriding methods in .AllMTable
    git-svn-id: https://svn.r-project.org/R/trunk@56089 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/methods/R/methodsTable.R
+++ src/library/methods/R/methodsTable.R
@@ -56,6 +56,10 @@
   signature <- generic@signature
   if(!exists(".SigLength", envir = fenv, inherits = FALSE))
      .setupMethodsTables(generic)
+  if(add)
+      allTable <- NULL # .AllMTable but only if required
+  else
+      allTable <- get(".AllMTable", envir = fenv)
   n <- get(".SigLength", envir = fenv)
   anySig <- rep("ANY", n)
   anyLabel <- .sigLabel(anySig)
@@ -88,11 +92,21 @@
         n <- ns
       }
     }
-    if(add)
-      assign(what, obj, envir = table)
+    if(add) {
+        if(exists(what, envir = table, inherits = FALSE)) {
+            ## must replace in .AllMTable also
+            if(is.null(allTable))
+                allTable <- get(".AllMTable", envir = fenv)
+            assign(what, obj, envir = allTable)
+        }
+        assign(what, obj, envir = table)
+    }
     else if(exists(what, envir = table, inherits = FALSE) &&
-            !all(obj@defined == "ANY") ) # remove methods, but not the default
-      remove(list = what, envir = table)
+            !all(obj@defined == "ANY") ) {
+        ## remove methods, but not the default
+        remove(list = what, envir = allTable)
+        remove(list = what, envir = table)
+    }
     ## else warning?
   }
   NULL
