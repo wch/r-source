99d1b36921b97ff4c75e73b08353f003a8a67992
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2011-07-03 11:06:45 +0000

    make 'make bytecode' the default -- not yet tested on Windows
    git-svn-id: https://svn.r-project.org/R/trunk@56252 00db46b3-68df-0310-9c12-caf00c1e9a41

--- share/make/vars.mk
+++ share/make/vars.mk
@@ -2,6 +2,8 @@
 ## shared *and* grepped from m4/R.m4 and tools/*-recommended.
 
 R_PKGS_BASE = base tools utils grDevices graphics stats datasets methods grid splines stats4 tcltk compiler
+## Those which can be installed initially compiled
+R_PKGS_BASE1 = utils grDevices graphics stats datasets methods grid splines stats4 tcltk
 
 R_PKGS_RECOMMENDED =  MASS lattice Matrix nlme survival boot cluster codetools foreign KernSmooth rpart class nnet spatial mgcv
 # there are dependencies in src/library/Recommended/Makefile*
