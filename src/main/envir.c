2990e5319cc7e82ab920158ae93f107cc36810b1
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2011-07-20 11:59:57 +0000

    typos in comments
    git-svn-id: https://svn.r-project.org/R/trunk@56443 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/envir.c
+++ src/main/envir.c
@@ -357,8 +357,8 @@ static SEXP DeleteItem(SEXP symbol, SEXP lst)
     if (lst != R_NilValue) {
 	SETCDR(lst, DeleteItem(symbol, CDR(lst)));
 	if (TAG(lst) == symbol) {
-	    SETCAR(lst, R_UnboundValue); /* in case binging is cached */
-	    LOCK_BINDING(lst);           /* in case binging is cached */
+	    SETCAR(lst, R_UnboundValue); /* in case binding is cached */
+	    LOCK_BINDING(lst);           /* in case binding is cached */
 	    lst = CDR(lst);
 	}
     }
@@ -747,8 +747,8 @@ static SEXP RemoveFromList(SEXP thing, SEXP list, int *found)
     }
     else if (TAG(list) == thing) {
 	*found = 1;
-	SETCAR(list, R_UnboundValue); /* in case binging is cached */
-	LOCK_BINDING(list);           /* in case binging is cached */
+	SETCAR(list, R_UnboundValue); /* in case binding is cached */
+	LOCK_BINDING(list);           /* in case binding is cached */
 	return CDR(list);
     }
     else {
@@ -757,8 +757,8 @@ static SEXP RemoveFromList(SEXP thing, SEXP list, int *found)
 	while (next != R_NilValue) {
 	    if (TAG(next) == thing) {
 		*found = 1;
-		SETCAR(next, R_UnboundValue); /* in case binging is cached */
-		LOCK_BINDING(next);           /* in case binging is cached */
+		SETCAR(next, R_UnboundValue); /* in case binding is cached */
+		LOCK_BINDING(next);           /* in case binding is cached */
 		SETCDR(last, CDR(next));
 		return list;
 	    }
