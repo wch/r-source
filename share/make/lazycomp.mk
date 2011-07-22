1e461df3cd5cefa5b2e390ce4a57b412c43f3ff8
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2011-07-22 08:59:49 +0000

    tweaks to the build process on Windows
    git-svn-id: https://svn.r-project.org/R/trunk@56471 00db46b3-68df-0310-9c12-caf00c1e9a41

--- share/make/lazycomp.mk
+++ share/make/lazycomp.mk
@@ -1,12 +1,17 @@
 ## ${R_HOME}/share/make/lazycomp.mk
-## Use for all packages except base, tools, datasets, methods
+## Used for all packages except base, tools, datasets, methods
 
 $(top_builddir)/library/$(pkg)/R/$(pkg).rdb: all.R
 	@$(INSTALL_DATA) all.R $(top_builddir)/library/$(pkg)/R/$(pkg)
-	@$(ECHO) "byte-compiling package '$(pkg)'"
-	@$(ECHO) "tools:::makeLazyLoading(\"$(pkg)\")" | \
+	@if test -n "$(R_NO_BASE_COMPILE)"; then \
+	 $(ECHO) "tools:::makeLazyLoading(\"$(pkg)\")" | \
+	  R_DEFAULT_PACKAGES=$(DEFPKGS) LC_ALL=C $(R_EXE) > /dev/null; \
+	else \
+	 $(ECHO) "byte-compiling package '$(pkg)'"; \
+	 $(ECHO) "tools:::makeLazyLoading(\"$(pkg)\")" | \
 	  R_COMPILE_PKGS=1 R_COMPILER_SUPPRESS_ALL=1 \
-	  R_DEFAULT_PACKAGES=$(DEFPKGS) LC_ALL=C $(R_EXE) > /dev/null
+	  R_DEFAULT_PACKAGES=$(DEFPKGS) LC_ALL=C $(R_EXE) > /dev/null; \
+	fi
 
 Rsimple: mkR mkRsimple
 Rlazy: mkR mkRsimple mklazy
