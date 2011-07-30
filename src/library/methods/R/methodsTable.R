c674ef84b177940491712f8bffe41ca1a84ecfd5
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2011-07-30 22:22:15 +0000

    less unhelpful error message
    git-svn-id: https://svn.r-project.org/R/trunk@56564 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/methods/R/methodsTable.R
+++ src/library/methods/R/methodsTable.R
@@ -388,7 +388,11 @@
     sig <- method@target
     pkgs <- packageSlot(sig)
     if( (length(pkgs) < length(as.character(sig))) || any(!nzchar(pkgs)))
-        stop("package slot missing from signature, cannot use with duplicate class names (the package may be out of date): class(es) ", paste(sig, collapse = ", "))
+        stop("package slot missing from signature for generic ",
+             sQuote(method@generic), "\n",
+             "and classes ", paste(sig, collapse = ", "), "\n",
+             "cannot use with duplicate class names (the package may need to be re-installed)",
+             call. = FALSE, domain = NA)
     paste(pkgs, collapse = "#")
 }
 
