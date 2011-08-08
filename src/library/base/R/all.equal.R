28072a1bff1cd81608ccd69b5594378db9507af1
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2011-08-08 17:36:08 +0000

    all.equal.factor predates character NAs
    git-svn-id: https://svn.r-project.org/R/trunk@56668 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/all.equal.R
+++ src/library/base/R/all.equal.R
@@ -132,26 +132,17 @@ all.equal.character <-
     else msg
 }
 
+## visible, so need to test both args
 all.equal.factor <- function(target, current, check.attributes = TRUE, ...)
 {
+    if(!inherits(target, "factor"))
+	return("'target' is not a factor")
     if(!inherits(current, "factor"))
 	return("'current' is not a factor")
     msg <-  if(check.attributes) attr.all.equal(target, current, ...)
-    class(target) <- class(current) <- NULL
-    nax <- is.na(target)
-    nay <- is.na(current)
-    n <- sum(nax != nay)
-    if(n > 1L)
-	msg <- c(msg, paste(n, "NA mismatches"))
-    else if (n == 1L)
-        msg <- c(msg, paste("1, NA mismatch"))
-    else {
-	target <- levels(target)[target[!nax]]
-	current <- levels(current)[current[!nay]]
-        n <- all.equal(target, current, check.attributes = check.attributes,
-                       ...)
-	if(is.character(n)) msg <- c(msg, n)
-    }
+    n <- all.equal(as.character(target), as.character(current),
+                   check.attributes = check.attributes, ...)
+    if(is.character(n)) msg <- c(msg, n)
     if(is.null(msg)) TRUE else msg
 }
 
