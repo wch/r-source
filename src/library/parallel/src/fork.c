494568e85e9457b425916f5545c2179ca34d3988
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2011-08-19 08:01:05 +0000

    tweak for Fedora
    git-svn-id: https://svn.r-project.org/R/trunk@56764 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/parallel/src/fork.c
+++ src/library/parallel/src/fork.c
@@ -606,7 +606,8 @@ SEXP mc_exit(SEXP sRes)
     if (is_master) error(_("mcexit can only be used in a child process"));
     if (master_fd != -1) { /* send 0 to signify that we're leaving */
 	unsigned int len = 0;
-	write(master_fd, &len, sizeof(len));
+	/* assign result for Fedora security settings */
+	ssize_t res = write(master_fd, &len, sizeof(len));
 	/* make sure the pipe is closed before we enter any waiting */
 	close(master_fd);
 	master_fd = -1;
