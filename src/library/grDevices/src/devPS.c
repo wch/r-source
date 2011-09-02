5244094c26b83c28ed99acd4d467d244df65737c
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2011-09-02 06:03:40 +0000

    add comments
    git-svn-id: https://svn.r-project.org/R/trunk@56881 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/grDevices/src/devPS.c
+++ src/library/grDevices/src/devPS.c
@@ -3964,8 +3964,13 @@ static void PS_writeRaster(unsigned int *raster, int w, int h,
      * image.  This will not work for larger images
      * (more than 10000 pixels, e.g., 100x100)
      * due to hard limits in the PostScript language.
-     * There is no support for semitransparent images.
+     * There is no support for semitransparent images, not even
+     * for transparent pixels (missing values in image(useRaster = TRUE) ).
+     * The latter could be added by using the level-3 feature of
+     * the 'image' operator with an image dictionary and ImageType 3:
+     * see PLRM 3rd ed section 4.10.6
      */
+
     /* Save graphics state */
     fprintf(pd->psfp, "gsave\n");
     /* set the colour space */
@@ -3976,9 +3981,7 @@ static void PS_writeRaster(unsigned int *raster, int w, int h,
     if (rot != 0.0)
 	fprintf(pd->psfp, "%.2f rotate\n", rot);
     /* scale */
-    fprintf(pd->psfp,
-	    "%.2f %.2f scale\n",
-	    width, height);
+    fprintf(pd->psfp, "%.2f %.2f scale\n", width, height);
     /* Begin image */
     /* Image characteristics */
     /* width height bitspercomponent matrix */
