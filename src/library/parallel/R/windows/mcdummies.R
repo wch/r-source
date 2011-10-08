ef5a67fe08e0472b604e0811eb76bd88d1510efd
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2011-10-08 20:19:00 +0000

    port r57190,3 from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-2-14-branch@57194 00db46b3-68df-0310-9c12-caf00c1e9a41

---
+++ src/library/parallel/R/windows/mcdummies.R
@@ -0,0 +1,35 @@
+#  File src/library/parallel/R/windows/mcdummies.R
+#  Part of the R package, http://www.R-project.org
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  A copy of the GNU General Public License is available at
+#  http://www.r-project.org/Licenses/
+
+mclapply <- function(X, FUN, ..., mc.preschedule = TRUE, mc.set.seed = TRUE,
+                     mc.silent = FALSE, mc.cores = 1L,
+                     mc.cleanup = TRUE, mc.allow.recursive = TRUE)
+{
+    cores <- as.integer(mc.cores)
+    if(cores < 1L) stop("'mc.cores' must be >= 1")
+    if(cores > 1L) stop("'mc.cores' > 1 is not supported on Windows")
+    lapply(X, FUN, ...)
+}
+
+pvec <- function(v, FUN, ..., mc.set.seed = TRUE, mc.silent = FALSE,
+                 mc.cores = 1L, mc.cleanup = TRUE)
+{
+    if (!is.vector(v)) stop("'v' must be a vector")
+    cores <- as.integer(mc.cores)
+    if(cores < 1L) stop("'mc.cores' must be >= 1")
+    if(cores > 1L) stop("'mc.cores' > 1 is not supported on Windows")
+    FUN(v, ...)
+}
