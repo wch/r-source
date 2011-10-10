d6bc6ccf4c2f4b64c1c306bcfcbcd389228ac079
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2011-10-10 15:40:12 +0000

    not using .Deprecated() for these, for now
    git-svn-id: https://svn.r-project.org/R/trunk@57208 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/R/sd.R
+++ src/library/stats/R/sd.R
@@ -8,24 +8,26 @@
 #
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
 #  GNU General Public License for more details.
 #
 #  A copy of the GNU General Public License is available at
 #  http://www.r-project.org/Licenses/
 
 ## in the future:
-sd <- function(x, na.rm=FALSE) 	sqrt(var(as.double(x), na.rm=na.rm))
+sd <- function(x, na.rm=FALSE)	sqrt(var(as.double(x), na.rm=na.rm))
 
 sd <- function(x, na.rm=FALSE) {
     if (is.matrix(x)) {
-	.Deprecated("apply(*, 2, sd)")
+	msg <- "sd(<matrix>) is deprecated.\n Use apply(*, 2, sd) instead."
+	warning(paste(msg, collapse = ""), call. = FALSE, domain = NA)
 	apply(x, 2, sd, na.rm=na.rm)
     }
     else if (is.vector(x))
 	sqrt(var(x, na.rm=na.rm))
     else if (is.data.frame(x)) {
-	.Deprecated("sapply(*, sd)")
+	msg <- "sd(<data.frame>) is deprecated.\n Use sapply(*, sd) instead."
+	warning(paste(msg, collapse = ""), call. = FALSE, domain = NA)
 	sapply(x, sd, na.rm=na.rm)
     }
     else
