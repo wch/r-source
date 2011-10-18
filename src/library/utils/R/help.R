78b0c8cdd58f3b9ef28d2bebd7469ef98441d72a
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2011-10-18 15:29:20 +0000

    clean up
    git-svn-id: https://svn.r-project.org/R/trunk@57306 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/utils/R/help.R
+++ src/library/utils/R/help.R
@@ -278,30 +278,17 @@ function(x, ...)
 }
 
 
-offline_help_helper <- function(texfile, type = "postscript", texinputs = NULL)
+offline_help_helper <- function(texfile, type, texinputs = NULL)
 {
-    PDF <- type == "pdf"
     ## Some systems have problems with texfile names like ".C.tex"
     tf <- tempfile("tex", tmpdir = ".", fileext = ".tex"); on.exit(unlink(tf))
     file.copy(texfile, tf)
-    tools::texi2dvi(tf, pdf = PDF, clean = TRUE, texinputs = texinputs)
-    ofile <- sub("tex$", if(PDF) "pdf" else "ps", tf)
-    if(!PDF) {
-        dfile <- sub("tex$", "dvi", texfile)
-        on.exit(unlink(dfile))
-        dvips <- getOption("dvipscmd", default = "dvips")
-        res <- system2(dvips, dfile, stdout = FALSE, stderr = FALSE)
-        if(res)
-            stop(gettextf("running %s failed", sQuote(dvips)), domain = NA)
-        if(!file.exists(ofile)) {
-            message(gettextf("%s produced no output file: sent to printer?",
-                             sQuote(dvips)), domain = NA)
-            return(invisible())
-        }
-    } else if(!file.exists(ofile))
-        stop(gettextf("creation of %s failed", sQuote(ofile)), domain = NA)
-    ofile2 <- sub("tex$", if(PDF) "pdf" else "ps", texfile)
-    if(file.copy(ofile, ofile2)) {
+    tools::texi2pdf(tf, clean = TRUE, texinputs = texinputs)
+    ofile <- sub("tex$", "pdf", tf)
+    ofile2 <- sub("tex$", "pdf", texfile)
+    if(!file.exists(ofile))
+        stop(gettextf("creation of %s failed", sQuote(ofile2)), domain = NA)
+    if(file.copy(ofile, ofile2, overwrite = TRUE)) {
         unlink(ofile)
         message("Saving help page to ", sQuote(basename(ofile2)))
     } else {
