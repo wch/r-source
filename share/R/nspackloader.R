3e4416fa759c6c3d18e43552af8f9e303fc38571
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2011-10-20 09:45:40 +0000

    port r57347 from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-2-14-branch@57348 00db46b3-68df-0310-9c12-caf00c1e9a41

--- share/R/nspackloader.R
+++ share/R/nspackloader.R
@@ -2,7 +2,7 @@ local({
     info <- loadingNamespaceInfo()
     ns <- .Internal(getRegisteredNamespace(as.name(info$pkgname)))
     if (is.null(ns))
-        stop("cannot find name space environment");
+        stop("cannot find namespace environment");
     barepackage <- sub("([^-]+)_.*", "\\1", info$pkgname)
     dbbase <- file.path(info$libname, info$pkgname, "R", barepackage)
     lazyLoad(dbbase, ns, filter = function(n) n != ".__NAMESPACE__.")
