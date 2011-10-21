wch/r-source

commit 8e4377949ea0c02762b5d9689f20b8501c7863e6

Allow Rd sections in \Sexpr, as originally intended but never supported.
git-svn-id: https://svn.r-project.org/R/trunk@57022 00db46b3-68df-0310-9c12-caf00c1e9a41

src/library/tools/R/RdConv2.R
@@ -263,17 +263,47 @@ processRdChunk <- function(code, stage, options, env, Rdfile)
 	    res <- as.character(err)   # The last value of the chunk
 	    tmpcon <- file()
 	    writeLines(res, tmpcon, useBytes = TRUE)
-	    res <- parse_Rd(tmpcon, fragment=TRUE)
+	    res <- suppressWarnings(try(parse_Rd(tmpcon, fragment=FALSE), silent=TRUE))
+	    if (inherits(res, "try-error")) {
+	    	seek(tmpcon, 0)
+	    	res <- parse_Rd(tmpcon, fragment=TRUE)
+	    }
+	    close(tmpcon)
 	    # Now remove that extra newline added by the writeLines
 	    last <- res[[length(res)]]
 	    if (attr(last, "Rd_tag") == "TEXT" && (len <- length(last)))
 	        res[[length(res)]][len] <- gsub("\\n$", "", last[len])
 	    flag <- getDynamicFlags(res)
-	    res <- tagged(res, "LIST")
-	    res <- setDynamicFlags(res, flag)
-	    close(tmpcon)
-	    res <- prepare_Rd(res, defines = .Platform$OS.type, options=options,
+	    # We may have multiple chunks now.  If they are in 
+	    # a section, we can wrap them in LIST, but at top
+	    # level we can't, so we disallow multiple sections.
+	    
+	    # First clear out the junk.
+	    tags <- RdTags(res)
+	    keep <- rep(TRUE, length(tags))
+	    for (i in seq_along(tags)) {
+	        if (tags[i] == "TEXT" && res[[i]] == "")
+	            keep[i] <- FALSE
+	    }
+	    res <- res[keep]
+	    tags <- tags[keep]
+	    if (length(res) > 1) {
+	    	is_section <- !is.na(sectionOrder[tags])
+	    	if (!any(is_section)) 
+	    	    res <- tagged(res, "LIST")
+	    	else {
+	    	    if (sum(is_section) > 1) 
+	    		stop("Only one Rd section per \\Sexpr is supported.")
+	    	    res <- res[[which(is_section)]]
+	    	}
+	    } else if (length(res) == 1) res <- res[[1]]
+	    else res <- tagged("", "TEXT")
+	    
+	    if (is.list(res)) {
+	    	res <- setDynamicFlags(res, flag)
+	    	res <- prepare_Rd(res, defines = .Platform$OS.type, options=options,
 	                           stage2 = FALSE, stage3 = FALSE)
+	    }
 	} else if (options$results == "text")
 	    res <- tagged(err, "TEXT")
 	else if (length(res)) {

doc/manual/R-exts.texi
@@ -5053,8 +5053,12 @@ and include the printed results verbatim.  (Invisible results will not print.)
 
 @item @code{results=rd}
 The result is assumed to be a character vector containing markup to
-be passed to @code{parse_Rd(fragment=TRUE)}, with the result inserted in place.
-This could be used to insert computed aliases, for instance.
+be passed to @code{parse_Rd()}, with the result inserted in place.
+This could be used to insert computed aliases, for instance.  
+As of @R{} 2.13.1-patched, @code{parse_Rd()} is called first
+with @code{fragment=FALSE} to allow a single Rd section
+macro to be inserted.  If that fails, it is called again with
+@code{fragment=TRUE}, the older behavior.
 
 @item @code{results=hide}
 Insert no output.

commit 3438731c0f568100b6261148044ea5614d3131ab

Fix bug introduced in r57022, add regression tests
git-svn-id: https://svn.r-project.org/R/trunk@57370 00db46b3-68df-0310-9c12-caf00c1e9a41

src/library/tools/R/RdConv2.R
@@ -263,12 +263,13 @@ processRdChunk <- function(code, stage, options, env, Rdfile)
 	    res <- as.character(err)   # The last value of the chunk
 	    tmpcon <- file()
 	    writeLines(res, tmpcon, useBytes = TRUE)
-	    res <- suppressWarnings(try(parse_Rd(tmpcon, fragment=FALSE), silent=TRUE))
-	    if (inherits(res, "try-error")) {
-	    	seek(tmpcon, 0)
-	    	res <- parse_Rd(tmpcon, fragment=TRUE)
-	    }
-	    close(tmpcon)
+	    parseFragment <- function(cond) {
+	    	               seek(tmpcon, 0)
+	    	               parse_Rd(tmpcon, fragment=TRUE)
+	    	            }
+	    res <- tryCatch(parse_Rd(tmpcon, fragment=FALSE), 
+	    	            warning = parseFragment, error = parseFragment,
+	    	            finally = close(tmpcon))
 	    # Now remove that extra newline added by the writeLines
 	    last <- res[[length(res)]]
 	    if (attr(last, "Rd_tag") == "TEXT" && (len <- length(last)))

tests/Makefile.common
@@ -224,20 +224,20 @@ test-Segfault:
 test-Rd: $(srcdir)/testit.Rd $(srcdir)/ver20.Rd
 	@$(ECHO) "testing Rd conversion"
 	@$(RDCONV) -t txt $(srcdir)/testit.Rd > testit.txt
-	@diff -bw testit.txt $(srcdir)/testit.txt.save
 	@$(RDCONV) -t html $(srcdir)/testit.Rd > testit.html 2> /dev/null
-	@diff -bw testit.html $(srcdir)/testit.html.save
 	@$(RDCONV) -t latex $(srcdir)/testit.Rd > testit.tex
-	@diff -bw testit.tex $(srcdir)/testit.tex.save
 	@$(RDCONV) -t example $(srcdir)/testit.Rd > testit-Ex.R
+	@diff -bw testit.txt $(srcdir)/testit.txt.save
+	@diff -bw testit.html $(srcdir)/testit.html.save
+	@diff -bw testit.tex $(srcdir)/testit.tex.save
 	@diff -bw testit-Ex.R $(srcdir)/testit-Ex.R.save
 	@$(RDCONV) -t txt $(srcdir)/ver20.Rd > ver20.txt
-	@diff -bw ver20.txt $(srcdir)/ver20.txt.save
 	@$(RDCONV) -t html $(srcdir)/ver20.Rd > ver20.html
-	@diff -bw ver20.html $(srcdir)/ver20.html.save
 	@$(RDCONV) -t latex $(srcdir)/ver20.Rd > ver20.tex
-	@diff -bw ver20.tex $(srcdir)/ver20.tex.save
 	@$(RDCONV) -t example $(srcdir)/ver20.Rd > ver20-Ex.R
+	@diff -bw ver20.txt $(srcdir)/ver20.txt.save
+	@diff -bw ver20.html $(srcdir)/ver20.html.save
+	@diff -bw ver20.tex $(srcdir)/ver20.tex.save
 	@diff -bw ver20-Ex.R $(srcdir)/ver20-Ex.R.save
 
 test-DocFiles:

tests/testit.Rd
@@ -27,8 +27,10 @@ foo(\var{x}, \var{y})
   
   foo(\var{x}, \var{y}).
   
-  From gap: pedtodot.Rd -- n=split(m,par,"\034")
+  From gap: pedtodot.Rd -- n=split(m,par,"\034") 
   }
+  Yihui Xie's example:
+  \Sexpr[results=rd,stage=render]{"\\\\describe{\\\\item{def}{ghi}}"}
 }
 \examples{
 \\x

tests/testit.html.save
@@ -4,10 +4,9 @@
 <link rel="stylesheet" type="text/css" href="R.css">
 </head><body>
 
-<table width="100%" summary="page for testit {}"><tr><td>testit {}</td><td align="right">R Documentation</td></tr></table>
+<table width="100%" summary="page for testit"><tr><td>testit</td><td align="right">R Documentation</td></tr></table>
 
 <h2>An Rd Regression Test</h2>
-
 
 <h3>Usage</h3>
 
@@ -20,22 +19,19 @@ foo(<VAR>x</VAR>, <VAR>y</VAR>)
 
 <h3>Details</h3>
 
-<p>
-Escaped backslash <code>\x</code>.
+
+<p>Escaped backslash <code>\x</code>.
 </p>
-<p>
-<code>%{}</code>
+<p><code>%{}</code>
 </p>
-<p>
-a link and one to a known destination:
+<p>a link and one to a known destination:
 weighted.mean
 </p>
-<p>
-var in <VAR>text</VAR>.
+<p>var in <VAR>text</VAR>.
 </p>
-<p>
-<code>foo(<VAR>x</VAR>, <VAR>y</VAR>)</code>.
-<pre>
+<p><code>foo(<VAR>x</VAR>, <VAR>y</VAR>)</code>.
+</p>
+<PRE>
   Escaped backslash \x.
 
   %{}
@@ -44,9 +40,13 @@ var in <VAR>text</VAR>.
   
   foo(\var{x}, \var{y}).
   
-  From gap: pedtodot.Rd -- n=split(m,par,"\034")
-  </pre>
-</p>
+  From gap: pedtodot.Rd -- n=split(m,par,"\034") 
+  </PRE>
+Yihui Xie's example:
+<dl>
+<dt>def</dt><dd><p>ghi</p>
+</dd></dl>
+
 
 
 <h3>Examples</h3>
@@ -57,5 +57,5 @@ var in <VAR>text</VAR>.
 foo(<VAR>x</VAR>, <VAR>y</VAR>)
 </pre>
 
-<hr><div align="center">[]</div>
+
 </body></html>

tests/testit.tex.save
@@ -1,6 +1,6 @@
-\inputencoding{latin1}
+\inputencoding{utf8}
 \HeaderA{testit}{An Rd Regression Test}{testit}
-\aliasA{\{}{testit}{.Rlbrace.}
+\aliasB{\{}{testit}{.Rlbrace.}
 %
 \begin{Usage}
 \begin{verbatim}
@@ -30,8 +30,13 @@ var in \var{text}.
   
   foo(\bsl{}var\{x\}, \bsl{}var\{y\}).
   
-  From gap: pedtodot.Rd -- n=split(m,par,"\bsl{}034")
+  From gap: pedtodot.Rd -- n=split(m,par,"\bsl{}034") 
   \end{alltt}
+
+Yihui Xie's example:
+\begin{description}
+\item[def] ghi
+\end{description}
 
 \end{Details}
 %

tests/testit.txt.save
@@ -11,13 +11,13 @@ _D_e_t_a_i_l_s:
      Escaped backslash '\x'.
 
      '%{}'
-  
+
      a link and one to a known destination: weighted.mean
-  
+
      var in text.
-  
+
      'foo(x, y)'.
-  
+
      
      
        Escaped backslash \x.
@@ -28,8 +28,12 @@ _D_e_t_a_i_l_s:
        
        foo(\var{x}, \var{y}).
        
-       From gap: pedtodot.Rd -- n=split(m,par,"\034")
+       From gap: pedtodot.Rd -- n=split(m,par,"\034") 
        
+
+     Yihui Xie's example:
+
+     def ghi
 
 _E_x_a_m_p_l_e_s:
 

tests/ver20.html.save
@@ -1,86 +1,81 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-<html><head><title>R: Johnson &amp; Johnson,  $ _  ### ^ ~</title>
+<html><head><title>R: Johnson &amp; Johnson, $ _ ### ^ ~</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <link rel="stylesheet" type="text/css" href="R.css">
 </head><body>
 
-<table width="100%" summary="page for ver20 {}"><tr><td>ver20 {}</td><td align="right">R Documentation</td></tr></table>
+<table width="100%" summary="page for ver20"><tr><td>ver20</td><td align="right">R Documentation</td></tr></table>
 
 <h2>Johnson &amp; Johnson,  $ _  ### ^ ~</h2>
 
-
 <h3>Description</h3>
 
-<p>
-This is the description
+
+<p>This is the description
 </p>
 
 
 <h3>Arguments</h3>
 
+
 <table summary="R argblock">
 <tr valign="top"><td><code>foo</code></td>
 <td>
-item 1</td></tr>
+<p>item 1</p>
+</td></tr>
 <tr valign="top"><td><code>bar</code></td>
 <td>
-space, the item 2</td></tr>
+<p>space, the item 2</p>
+</td></tr>
 <tr valign="top"><td><code>bah</code></td>
 <td>
-newline, then item 3</td></tr>
+<p>newline, then item 3</p>
+</td></tr>
 </table>
 
 
 <h3>Details</h3>
 
-<p>
-<font face="Courier New,Courier" color="#666666"><b>R</b></font> <font face="Courier New,Courier" color="#666666"><b>R</b></font> \R <i>&alpha;&beta;&Gamma;</i>
+
+<p><font face="Courier New,Courier" color="#666666"><b>R</b></font> <font face="Courier New,Courier" color="#666666"><b>R</b></font> \R <i>&alpha;&beta;&Gamma;</i>
 </p>
-<p>
-In code, all that needs to be escaped is percent, backslash and unpaired
+<p>In code, all that needs to be escaped is percent, backslash and unpaired
 braces: <code>% \ {</code>. Unescaped: <code>$ # _ ^ ~ &lt; &gt; |</code>, and as a
 pair, <code>{...}</code>.  If we escape: <code> \_ \# \$ \&amp; { } \ </code>
 And some backslashes: <code>\ \ \\ \\</code> should
 give one, one, two, two backslashes.
 </p>
-<p>
-How about \ (that is backslash space) in text?
+<p>How about \ (that is backslash space) in text?
 </p>
-<p>
-Elsewhere, { } % \ need to be escaped, but not $ # _ ^ ~ &lt; &gt; | .
+<p>Elsewhere, { } % \ need to be escaped, but not $ # _ ^ ~ &lt; &gt; | .
 Let's try that again in <code>\samp</code>: <span class="samp"> { } % \ $ # _ ^ ~ &lt; &gt; |</span>.
 And if we do escape: \$ \# \_ , and with three backslashes, \\#.
 </p>
-<p>
-</p><p align="center"><i>a &lt; b &gt; c | d, x^y</i></p><p>
-</p><p align="center"><i>\$k &gt; \#k, x_y, x\_y</i></p><p>
-</p>
-<p>
+<p align="center"><i>a &lt; b &gt; c | d, x^y</i></p>
 
-group-weighting from $[N_iP_i(1-P_i)]$
+<p align="center"><i>\$k &gt; \#k, x_y, x\_y</i></p>
+
+
+<p>group-weighting from $[N_iP_i(1-P_i)]$
 </p>
-<p>
-<pre>
+<PRE>
     % a
     \
     \% c
     {
     \{
-  </pre>
-</p>
+  </PRE>
 
 
 <h3>URLs and emails</h3>
 
-<p>
-URLs need to be verbatim:  <a href="testit ~^_$#<>|">testit ~^_$#<>|</a>.
+
+<p>URLs need to be verbatim:  <a href="testit ~^_$#<>|">testit ~^_$#&lt;&gt;|</a>.
 </p>
-<p>
-Markus Kuhn, &ldquo;ASCII and Unicode quotation marks&rdquo;.
+<p>Markus Kuhn, &ldquo;ASCII and Unicode quotation marks&rdquo;.
 <a href="http://www.cl.cam.ac.uk/~mgk25/ucs/quotes.html">http://www.cl.cam.ac.uk/~mgk25/ucs/quotes.html</a>
 </p>
-<p>
-No escape is needed in <a href="mailto:Micah_Altman@harvard.edu">Micah_Altman@harvard.edu</a>
+<p>No escape is needed in <a href="mailto:Micah_Altman@harvard.edu">Micah_Altman@harvard.edu</a>
 </p>
 
 
@@ -90,5 +85,5 @@ No escape is needed in <a href="mailto:Micah_Altman@harvard.edu">Micah_Altman@ha
 "\\r\\n"
 </pre>
 
-<hr><div align="center">[]</div>
+
 </body></html>

tests/ver20.tex.save
@@ -1,4 +1,4 @@
-\inputencoding{latin1}
+\inputencoding{utf8}
 \HeaderA{ver20}{Johnson \& Johnson,  \$ \_  \#\#\# \textasciicircum{} \textasciitilde{}}{ver20}
 %
 \begin{Description}\relax

tests/ver20.txt.save
@@ -30,7 +30,7 @@ _D_e_t_a_i_l_s:
 
                              a < b > c | d, x^y                         
      
-                           \$k > \#k, x_y, x\_y                      
+                            \$k > \#k, x_y, x\_y                        
      
      group-weighting from $[N_iP_i(1-P_i)]$
 