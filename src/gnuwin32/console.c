6eae41fdc69ab6e94654d46235342e0facfc32e6
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2011-11-05 05:57:33 +0000

    port r57573,4 from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-2-14-branch@57575 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/console.c
+++ src/gnuwin32/console.c
@@ -279,10 +279,18 @@ static size_t enctowcs(wchar_t *wc, char *s, int n)
 static void xbufadds(xbuf p, const char *s, int user)
 {
     int n = strlen(s) + 1; /* UCS-2 must be shorter */
-    /* wchar_t tmp[n];  this might blow the stack */
-    wchar_t *tmp = (wchar_t*) R_alloc(n, sizeof(wchar_t));
-    enctowcs(tmp, (char *) s, n);
-    xbufaddxs(p, tmp, user);
+    if (n < 1000) {
+	wchar_t tmp[n];
+	enctowcs(tmp, (char *) s, n);
+	xbufaddxs(p, tmp, user);
+    } else {
+	/* very long line */
+	void *vmax = vmaxget();
+	wchar_t *tmp = (wchar_t*) R_alloc(n, sizeof(wchar_t));
+	enctowcs(tmp, (char *) s, n);
+	xbufaddxs(p, tmp, user);
+	vmaxset(vmax);
+    }
 }
 
 static void xbuffixl(xbuf p)
