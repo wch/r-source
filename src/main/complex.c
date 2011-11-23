81fba4d88659f6f596ca6dfa7d3b7a625a98f13c
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2011-11-23 22:42:05 +0000

    add comments about C1x
    git-svn-id: https://svn.r-project.org/R/trunk@57740 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/complex.c
+++ src/main/complex.c
@@ -71,6 +71,10 @@
 #endif
 
 
+/* 
+   Note: this could use the proposed C1x CMPLX() macro.
+   As could mycpow, z_tan and some of the substitutes.
+ */
 static R_INLINE double complex toC99(Rcomplex *x)
 {
 #if __GNUC__
@@ -144,6 +148,7 @@ static R_INLINE double complex R_cpow_n(double complex X, int k)
 
   3) PPC Mac OS X crashes on powers of 0+0i (at least under Rosetta).
   Really 0i^-1 should by Inf+NaNi, but getting that portably seems too hard.
+  (C1x's CMPLX will eventually be possible.)
 */
 
 static double complex mycpow (double complex X, double complex Y)
