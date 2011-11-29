dd00573af86553e064dea6fa80b99c77b6dc969b
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2011-11-29 10:25:33 +0000

    port r57761 from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-2-14-branch@57762 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/utils/R/SweaveDrivers.R
+++ src/library/utils/R/SweaveDrivers.R
@@ -616,7 +616,10 @@ RweaveEvalWithOpt <- function (expr, options)
     if (options$eval) {
         res <- try(withVisible(eval(expr, .GlobalEnv)), silent = TRUE)
         if (inherits(res, "try-error")) return(res)
-        if (options$print || (options$term && res$visible)) print(res$value)
+        if (options$print || (options$term && res$visible)) {
+            if (.isMethodsDispatchOn() && isS4(res$value))
+                methods:::show(res$value) else print(res$value)
+        }
     }
     res
 }
