01a061f3df34ab8738ba7f7c2f3dc86e8f22557e
murdoch <murdoch@00db46b3-68df-0310-9c12-caf00c1e9a41>
2011-12-30 01:18:20 +0000

    Port r58006 from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-2-14-branch@58007 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/scan.c
+++ src/main/scan.c
@@ -48,6 +48,7 @@ static R_INLINE int imin2(int x, int y)
 /* The size of vector initially allocated by scan */
 #define SCAN_BLOCKSIZE		1000
 /* The size of the console buffer */
+/* NB:  in Windows this also needs to be set in gnuwin32/getline/getline.c */
 #define CONSOLE_PROMPT_SIZE	256
 
 #define NO_COMCHAR 100000 /* won't occur even in Unicode */
