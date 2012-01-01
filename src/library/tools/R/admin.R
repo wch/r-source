1140fca4bc98500c9bdee26fdb7ac21bea8e4f98
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-01-01 09:24:46 +0000

    need immediate warnings (might be more than 50)
    git-svn-id: https://svn.r-project.org/R/trunk@58029 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/admin.R
+++ src/library/tools/R/admin.R
@@ -803,6 +803,7 @@ function(dir)
 
 .test_load_package <- function(pkg_name, lib)
 {
+    options(warn = 1)
     res <- try(suppressPackageStartupMessages(library(pkg_name, lib.loc = lib, character.only = TRUE, logical.return = TRUE)))
     if (inherits(res, "try-error") || !res)
         stop("loading failed", call. = FALSE)
