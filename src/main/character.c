ab08c3c4bad30294cb6a9d9eec0675c39fb54e9b
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-01-17 16:14:37 +0000

    port r58129 from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-2-14-branch@58130 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/character.c
+++ src/main/character.c
@@ -1299,9 +1299,12 @@ static int strtoi(SEXP s, int base)
     long res;
     char *endp;
 
+    /* strtol might return extreme values on error */
+    errno = 0;
+
     if(s == NA_STRING) return(NA_INTEGER);
     res = strtol(CHAR(s), &endp, base); /* ASCII */
-    if(*endp != '\0') res = NA_INTEGER;
+    if(errno || *endp != '\0') res = NA_INTEGER;
     if(res > INT_MAX || res < INT_MIN) res = NA_INTEGER;
     return(res);
 }
