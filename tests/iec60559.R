a56cb3dcad5f937a37a160fe746b175111490b4d
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-01-29 09:37:54 +0000

    add test for sqrt(-0)
    git-svn-id: https://svn.r-project.org/R/trunk@58223 00db46b3-68df-0310-9c12-caf00c1e9a41

--- tests/iec60559.R
+++ tests/iec60559.R
@@ -10,3 +10,9 @@ exp(c(-745:-740, -730, -720, -710:-705))
 
 # And because most libm pow() functions special-case integer powers.
 10^-(324:307-0.01)/10^0.01
+
+# IEC60559 mandates this, but C99/C11 do not.
+# Mingw-w64 did not do so in v 2.0.1
+x <- 0*(-1) # negative zero
+sqrt(x)
+identical(x, sqrt(x))
