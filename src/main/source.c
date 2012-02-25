efa23224e67d32c648b47b12864c9e5c29e482dd
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-02-25 08:51:03 +0000

    clean up from clang warning
    git-svn-id: https://svn.r-project.org/R/trunk@58489 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/source.c
+++ src/main/source.c
@@ -135,15 +135,15 @@ void attribute_hidden parseError(SEXP call, int linenum)
 	    error(_("%s%d:%d: %s"),
 		  filename, linenum, R_ParseErrorCol, R_ParseErrorMsg);
 	    break;
-	case 1: // clang says 'use of '%n' is discouraged'
-	    sprintf(buffer, "%d: %n", R_ParseContextLine, &width); 
+	case 1: // replaces use of %n
+	    width = sprintf(buffer, "%d: ", R_ParseContextLine); 
 	    error(_("%s%d:%d: %s\n%d: %s\n%*s"),
 		  filename, linenum, R_ParseErrorCol, R_ParseErrorMsg,
 		  R_ParseContextLine, CHAR(STRING_ELT(context, 0)), 
 		  width+R_ParseErrorCol, "^");
 	    break;
 	default:
-	    sprintf(buffer, "%d: %n", R_ParseContextLine, &width);
+	    width = sprintf(buffer, "%d:", R_ParseContextLine);
 	    error(_("%s%d:%d: %s\n%d: %s\n%d: %s\n%*s"),
 		  filename, linenum, R_ParseErrorCol, R_ParseErrorMsg,
 		  R_ParseContextLine-1, CHAR(STRING_ELT(context, len-2)),
