9a139ea0adce099bee144315c8e2c1145326dc8f
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-03-28 08:06:46 +0000

    allow for missingness passed down (even if it is a mistake)
    git-svn-id: https://svn.r-project.org/R/trunk@58858 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/seq.R
+++ src/library/base/R/seq.R
@@ -42,8 +42,8 @@ seq.default <-
 	warning(gettextf("extra argument(s) %s will be disregarded",
 			 paste(sQuote(names(list(...))), collapse = ", ")),
 		domain = NA)
-    if (length(from) != 1L) stop("'from' must be of length 1")
-    if (length(to) != 1L) stop("'to' must be of length 1")
+    if (!missing(from) && length(from) != 1L) stop("'from' must be of length 1")
+    if (!missing(to) && length(to) != 1L) stop("'to' must be of length 1")
     if(is.null(length.out))
 	if(missing(by))
 	    from:to
