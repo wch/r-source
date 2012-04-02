a41c34a386de2dd8ca8636f2c473c4055ae0e7a5
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-04-02 09:51:43 +0000

    not yet (needs write-barrier equivalent)
    git-svn-id: https://svn.r-project.org/R/branches/R-2-15-branch@58901 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/dotcode.c
+++ src/main/dotcode.c
@@ -425,7 +425,12 @@ static void *RObjToCPtr(SEXP s, int naok, int dup, int narg, int Fort,
 	break;
     case VECSXP:
 	if (Fort) error(_("invalid mode to pass to Fortran (arg %d)"), narg);
-	return (void*) DATAPTR(s);
+	if (!dup)
+	    error(_("lists must be duplicated in .C"));
+	n = length(s);
+	SEXP *lptr = (SEXP *) R_alloc(n, sizeof(SEXP));
+	for (int i = 0 ; i < n ; i++) lptr[i] = VECTOR_ELT(s, i);
+	ans = (void*) lptr;
 	break;
     case CLOSXP:
     case BUILTINSXP:
