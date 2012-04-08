2f8304c4705f4fb2a334cd0a8a2aabf3d027d107
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-04-08 07:09:13 +0000

    port r58926:7 from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-2-15-branch@58928 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/utils/R/iconv.R
+++ src/library/utils/R/iconv.R
@@ -76,7 +76,8 @@ localeToCharset <- function(locale = Sys.getlocale("LC_CTYPE"))
         ## Assume locales are like  en_US[.utf8[@euro]]
         x <- strsplit(locale, ".", fixed=TRUE)[[1L]]
         enc <- if(length(x) == 2) gsub("@.*$o", "", x[2L]) else ""
-        if(enc == "UTF-8") enc <- "utf8" # for AIX
+        # AIX uses UTF-8, OS X utf-8
+        if(toupper(enc) == "UTF-8") enc <- "utf8"
         if(nzchar(enc) && enc != "utf8") {
             enc <- tolower(enc)
             known <-
