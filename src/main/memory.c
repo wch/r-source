0de0c7ea867de2e9b0081fcd78c12c8020d35ef0
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-04-13 20:13:02 +0000

    Added initial experimental framework to support vectors with more than 2^31-1 elements.
    git-svn-id: https://svn.r-project.org/R/trunk@59005 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/memory.c
+++ src/main/memory.c
@@ -903,25 +903,25 @@ static R_INLINE R_size_t getVecSizeInVEC(SEXP s)
     R_size_t size;
     switch (TYPEOF(s)) {	/* get size in bytes */
     case CHARSXP:
-	size = LENGTH(s) + 1;
+	size = XLENGTH(s) + 1;
 	break;
     case RAWSXP:
-	size = LENGTH(s);
+	size = XLENGTH(s);
 	break;
     case LGLSXP:
     case INTSXP:
-	size = LENGTH(s) * sizeof(int);
+	size = XLENGTH(s) * sizeof(int);
 	break;
     case REALSXP:
-	size = LENGTH(s) * sizeof(double);
+	size = XLENGTH(s) * sizeof(double);
 	break;
     case CPLXSXP:
-	size = LENGTH(s) * sizeof(Rcomplex);
+	size = XLENGTH(s) * sizeof(Rcomplex);
 	break;
     case STRSXP:
     case EXPRSXP:
     case VECSXP:
-	size = LENGTH(s) * sizeof(SEXP);
+	size = XLENGTH(s) * sizeof(SEXP);
 	break;
     default:
 	register_bad_sexp_type(s, __LINE__);
@@ -939,7 +939,7 @@ static void ReleaseLargeFreeVectors(void)
 	    R_size_t size;
 #ifdef PROTECTCHECK
 	    if (TYPEOF(s) == FREESXP)
-		size = LENGTH(s);
+		size = XLENGTH(s);
 	    else
 		/* should not get here -- arrange for a warning/error? */
 		size = getVecSizeInVEC(s);
@@ -949,7 +949,14 @@ static void ReleaseLargeFreeVectors(void)
 	    UNSNAP_NODE(s);
 	    R_LargeVallocSize -= size;
 	    R_GenHeap[LARGE_NODE_CLASS].AllocCount--;
+#ifdef LONG_VECTOR_SUPPORT
+	    if (IS_LONG_VEC(s))
+		free(((char *) s) - sizeof(R_long_vec_hdr_t));
+	    else
+		free(s);
+#else
 	    free(s);
+#endif
 	}
 	s = next;
     }
@@ -1599,7 +1606,7 @@ static void RunGenCollect(R_size_t size_needed)
 		      calculating size */
 		if (CHAR(s) != NULL) {
 		    R_size_t size = getVecSizeInVEC(s);
-		    LENGTH(s) = size;
+		    SETLENGTH(s, size);
 		}
 		SETOLDTYPE(s, TYPEOF(s));
 		TYPEOF(s) = FREESXP;
@@ -2193,7 +2200,7 @@ SEXP attribute_hidden mkPROMISE(SEXP expr, SEXP rho)
 */
 #define intCHARSXP 73
 
-SEXP allocVector(SEXPTYPE type, R_len_t length)
+SEXP allocVector(SEXPTYPE type, R_xlen_t length)
 {
     SEXP s;     /* For the generational collector it would be safer to
 		   work in terms of a VECSEXP here, but that would
@@ -2239,14 +2246,17 @@ SEXP allocVector(SEXPTYPE type, R_len_t length)
 	    R_SmallVallocSize += alloc_size;
 	    ATTRIB(s) = R_NilValue;
 	    TYPEOF(s) = type;
-	    LENGTH(s) = length;
-	    TRUELENGTH(s) = 0;
+	    SET_SHORT_VEC_LENGTH(s, length);
+	    SET_SHORT_VEC_TRUELENGTH(s, 0);
 	    NAMED(s) = 0;
 	    return(s);
 	}
     }
 
-    if (length < 0 )
+    if (length > R_XLEN_T_MAX)
+	errorcall(R_GlobalContext->call,
+		  _("vector is too large")); /**** put length into message */
+    else if (length < 0 )
 	errorcall(R_GlobalContext->call,
 		  _("negative length vectors are not allowed"));
     /* number of vector cells to allocate */
@@ -2375,22 +2385,46 @@ SEXP allocVector(SEXPTYPE type, R_len_t length)
 	    s->sxpinfo = UnmarkedNodeTemplate.sxpinfo;
 	    SET_NODE_CLASS(s, node_class);
 	    R_SmallVallocSize += alloc_size;
+	    SET_SHORT_VEC_LENGTH(s, length);
 	}
 	else {
 	    Rboolean success = FALSE;
-	    s = NULL; /* initialize to suppress warning */
-	    if (size < (R_SIZE_T_MAX / sizeof(VECREC)) - sizeof(SEXPREC_ALIGN)) {
-		s = malloc(sizeof(SEXPREC_ALIGN) + size * sizeof(VECREC));
-		if (s == NULL) {
+	    R_size_t hdrsize = sizeof(SEXPREC_ALIGN);
+#ifdef LONG_VECTOR_SUPPORT
+	    if (length > R_SHORT_LEN_MAX)
+		hdrsize = sizeof(SEXPREC_ALIGN) + sizeof(R_long_vec_hdr_t);
+#endif
+	    void *mem = NULL; /* initialize to suppress warning */
+	    if (size < (R_SIZE_T_MAX / sizeof(VECREC)) - hdrsize) { /*** not sure this test is quite right -- why subtract the header? LT */
+		mem = malloc(hdrsize + size * sizeof(VECREC));
+		if (mem == NULL) {
 		    /* If we are near the address space limit, we
 		       might be short of address space.  So return
 		       all unused objects to malloc and try again. */
 		    R_gc_full(alloc_size);
-		    s = malloc(sizeof(SEXPREC_ALIGN) + size * sizeof(VECREC));
+		    mem = malloc(hdrsize + size * sizeof(VECREC));
 		}
-		if (s != NULL) success = TRUE;
+		if (mem != NULL) {
+#ifdef LONG_VECTOR_SUPPORT
+		    if (length > R_SHORT_LEN_MAX) {
+			s = (SEXP) (((char *) mem) + sizeof(R_long_vec_hdr_t));
+			SET_SHORT_VEC_LENGTH(s, R_LONG_VEC_TOKEN);
+			SET_LONG_VEC_LENGTH(s, length);
+			SET_LONG_VEC_TRUELENGTH(s, 0);
+		    }
+		    else {
+			s = mem;
+			SET_SHORT_VEC_LENGTH(s, length);
+		    }
+#else
+		    s = mem;
+		    SETLENGTH(s, length);
+#endif
+		    success = TRUE;
+		}
+		else s = NULL;
 #ifdef R_MEMORY_PROFILING
-		R_ReportAllocation(sizeof(SEXPREC_ALIGN) + size * sizeof(VECREC));
+		R_ReportAllocation(hdrsize + size * sizeof(VECREC));
 #endif
 	    }
 	    if (! success) {
@@ -2422,9 +2456,9 @@ SEXP allocVector(SEXPTYPE type, R_len_t length)
     }
     else {
 	GC_PROT(s = allocSExpNonCons(type));
+	SET_SHORT_VEC_LENGTH(s, length);
     }
-    LENGTH(s) = length;
-    TRUELENGTH(s) = 0;
+    SET_SHORT_VEC_TRUELENGTH(s, 0);
     NAMED(s) = 0;
 
     /* The following prevents disaster in the case */
@@ -2991,6 +3025,9 @@ int (LENGTH)(SEXP x) { return LENGTH(CHK(x)); }
 int (TRUELENGTH)(SEXP x) { return TRUELENGTH(CHK(x)); }
 void (SETLENGTH)(SEXP x, int v) { SETLENGTH(CHK(x), v); }
 void (SET_TRUELENGTH)(SEXP x, int v) { SET_TRUELENGTH(CHK(x), v); }
+R_xlen_t (XLENGTH)(SEXP x) { return XLENGTH(CHK(x)); }
+R_xlen_t (XTRUELENGTH)(SEXP x) { return XTRUELENGTH(CHK(x)); }
+int  (IS_LONG_VEC)(SEXP x) { return IS_LONG_VEC(CHK(x)); }
 
 const char *(R_CHAR)(SEXP x) {
     if(TYPEOF(x) != CHARSXP)
@@ -3467,3 +3504,10 @@ int Seql(SEXP a, SEXP b)
 }
 
 
+#ifdef LONG_VECTOR_SUPPORT
+R_len_t R_BadLongVector(SEXP x, const char *file, int line)
+{
+    error(_("long vectors not supported yet: %s:%d"), file, line);
+    return 0; /* not reached */
+}
+#endif
