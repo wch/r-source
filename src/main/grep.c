2f96077fc90b002704ed094a980d2d1f82853a37
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-04-13 19:26:22 +0000

    port r59000 and misc from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-2-15-branch@59001 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/grep.c
+++ src/main/grep.c
@@ -2406,7 +2406,8 @@ SEXP attribute_hidden do_regexpr(SEXP call, SEXP op, SEXP args, SEXP env)
     }
 
     if (PRIMVAL(op) == 0) { /* regexpr */
-	SEXP matchlen, capture_start = R_NilValue, capturelen = R_NilValue;
+	SEXP matchlen, capture_start, capturelen;
+	int *is, *il;
 	PROTECT(ans = allocVector(INTSXP, n));
 	/* Protect in case install("match.length") allocates */
 	PROTECT(matchlen = allocVector(INTSXP, n));
@@ -2427,8 +2428,9 @@ SEXP attribute_hidden do_regexpr(SEXP call, SEXP op, SEXP args, SEXP env)
 	    setAttrib(ans, install("capture.length"), capturelen);
 	    setAttrib(ans, install("capture.names"), capture_names);
 	    UNPROTECT(3);
-	}
-
+	    is = INTEGER(capture_start);
+	    il = INTEGER(capturelen);
+        } else is = il = NULL; /* not actually used */
 	vmax = vmaxget();
 	for (i = 0 ; i < n ; i++) {
 	    if (STRING_ELT(text, i) == NA_STRING) {
@@ -2473,15 +2475,13 @@ SEXP attribute_hidden do_regexpr(SEXP call, SEXP op, SEXP args, SEXP env)
 						 capture_count,
 						 INTEGER(ans) + i,
 						 INTEGER(matchlen) + i,
-						 INTEGER(capture_start) + i,
-						 INTEGER(capturelen) + i,
+						 is + i, il + i,
 						 s, n);
 		    } else {
 			INTEGER(ans)[i] = INTEGER(matchlen)[i] = -1;
 			for(int cn = 0; cn < capture_count; cn++) {
 			    int ind = i + cn*n;
-			    INTEGER(capture_start)[ind] = 
-				INTEGER(capturelen)[ind] = -1;
+			    is[ind] = il[ind] = -1;
 			}
 		    }
 		} else {
