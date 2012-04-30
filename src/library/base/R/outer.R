0d0cba93e07b800b434a62e47e3c8b8e63bf87d5
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-04-30 19:04:11 +0000

    trap a sllly user error, e.g. fun for FUN
    git-svn-id: https://svn.r-project.org/R/trunk@59290 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/outer.R
+++ src/library/base/R/outer.R
@@ -38,6 +38,7 @@ outer <- function (X, Y, FUN = "*", ...)
         if(!no.ny) ny <- list(names(Y))
     }
     if (is.character(FUN) && FUN=="*") {
+        if (length(list(...))) stop('using ... with FUN = "*" is an error')
         # this is for numeric vectors, so dropping attributes is OK
         robj <- as.vector(X) %*% t(as.vector(Y))
         dim(robj) <- c(dX, dY)
