62380cdb99015dcfcd9321845f3bd775d5dc3644
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-05-08 15:14:44 +0000

    Add option '--compact' to R CMD Sweave.
    git-svn-id: https://svn.r-project.org/R/trunk@59329 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/utils/R/Sweave.R
+++ src/library/utils/R/Sweave.R
@@ -421,6 +421,9 @@ SweaveHooks <- function(options, run = FALSE, envir = .GlobalEnv)
             "  --encoding=enc  default encoding 'enc' for file",
             "  --options=      comma-separated list of Sweave options",
             "  --pdf           convert to PDF document",
+            "  --compact=      try to compact PDF document:",
+            '                  "no" (default), "qpdf", "gs", "gs+pdf", "both"',
+            "  --compact       same as --compact=qpdf",
             "",
             "Report bugs at bugs.r-project.org .",
             sep = "\n")
@@ -435,6 +438,7 @@ SweaveHooks <- function(options, run = FALSE, envir = .GlobalEnv)
     file <- character()
     driver <- encoding <- options <- ""
     toPDF <- FALSE
+    compact <- Sys.getenv("_R_SWEAVE_COMPACT_PDF_", "no")
     while(length(args)) {
         a <- args[1L]
         if (a %in% c("-h", "--help")) {
@@ -459,6 +463,10 @@ SweaveHooks <- function(options, run = FALSE, envir = .GlobalEnv)
             options <- substr(a, 11, 1000)
         } else if (a == "--pdf") {
             toPDF <- TRUE
+        } else if (substr(a, 1, 10) == "--compact=") {
+            compact <- substr(a, 11, 1000)
+        } else if (a == "--compact") {
+            compact <- "qpdf"
         } else if (substr(a, 1, 1) == "-") {
             message("Warning: unknown option ", sQuote(a))
         } else file <- c(file, a)
@@ -477,10 +485,32 @@ SweaveHooks <- function(options, run = FALSE, envir = .GlobalEnv)
     }
     do.call(Sweave, args)
     if (toPDF) {
-        texfile <- sub("\\.[rsRS][[:alpha:]]+$", ".tex", file)
+        texfile <- basename(sub("\\.[rsRS][[:alpha:]]+$", ".tex", file))
         tools::texi2pdf(texfile, clean = TRUE)
         ofile <- sub("\\.tex$", ".pdf", texfile)
-        message("Created PDF document ", sQuote(basename(ofile)))
+        message("Created PDF document ", sQuote(ofile))
+        if(compact != "no") {
+            ## <NOTE>
+            ## Same code as used for --compact-vignettes in
+            ## .build_packages() ...
+            message("Compacting PDF document")
+            if(compact %in% c("gs", "gs+pdf", "both")) {
+                gs_cmd <- tools:::find_gs_cmd(Sys.getenv("R_GSCMD", ""))
+                gs_quality <- "ebook"
+            } else {
+                gs_cmd <- ""
+                gs_quality <- "none"
+            }
+            qpdf <- if(compact %in% c("qpdf", "gs+pdf", "both"))
+                Sys.which(Sys.getenv("R_QPDF", "qpdf"))
+            else ""
+            res <- tools::compactPDF(ofile, qpdf = qpdf,
+                                     gs_cmd = gs_cmd,
+                                     gs_quality = gs_quality)
+            res <- format(res, diff = 1e5)
+            if(length(res))
+                message(paste(format(res), collapse = "\n"))
+        }
     }
     do_exit()
 }
