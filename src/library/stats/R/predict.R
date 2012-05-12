22821510afe67634229b23e491849471b25b5db1
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-05-12 12:04:49 +0000

    rm historical relict
    git-svn-id: https://svn.r-project.org/R/trunk@59336 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/R/predict.R
+++ src/library/stats/R/predict.R
@@ -16,31 +16,3 @@
 
 predict <- function(object,...) UseMethod("predict")
 
-## This is not used anywhere anymore, is it ?
-## It would only work with objects very much like  "lm", would it?
-if(FALSE)
-predict.default <- function (object, ...) {
-    namelist <- list(...)
-    names(namelist) <- substitute(list(...))[-1L]
-    m <- length(namelist)
-    X <- as.matrix(namelist[[1L]])
-    if (m > 1)
-	for (i in (2:m)) X <- cbind(X, namelist[[i]])
-    if (object$intercept)
-	X <- cbind(rep(1, NROW(X)), X)
-    k <- NCOL(X)
-    n <- NROW(X)
-    if (length(object$coefficients) != k)
-	stop("wrong number of predictors")
-    predictor <- X %*% object$coefficients
-    ip <- numeric(n)
-    names(ip) <- paste0("P", 1L:n)
-    for (i in 1L:n)
-	ip[i] <- sum(X[i, ] * (object$covmat %*% X[i, ]))
-    stderr1 <- sqrt(ip)
-    stderr2 <- sqrt(object$rms^2 + ip)
-    tt <- qt(0.975, object$df)
-    predictor + tt * cbind(Predicted=0,
-                           "Conf lower"=-stderr1, "Conf upper"=stderr1,
-                           "Pred lower"=-stderr2, "Pred upper"=stderr2)
-}
