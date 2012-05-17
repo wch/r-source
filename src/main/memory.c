da5504df2ba6c77b4bbe64263cd63c7e3df0918a
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-05-17 10:09:42 +0000

    add some comments
    git-svn-id: https://svn.r-project.org/R/trunk@59348 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/memory.c
+++ src/main/memory.c
@@ -1988,6 +1988,7 @@ char *R_alloc(size_t nelem, int eltsize)
 	R_VStack = s;
 	return (char *) DATAPTR(s);
     }
+    /* One programmer has relied on this, but it is undocumented! */
     else return NULL;
 }
 
