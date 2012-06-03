0525f47591d4e63665034972b7ae05c8f65d8aef
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-06-03 19:42:54 +0000

    give 'info' in "exactly singular" error
    git-svn-id: https://svn.r-project.org/R/trunk@59503 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/modules/lapack/Lapack.c
+++ src/modules/lapack/Lapack.c
@@ -972,7 +972,8 @@ static SEXP modLa_dgesv(SEXP A, SEXP Bin, SEXP tolin)
 	error(_("argument %d of Lapack routine %s had invalid value"),
 	      -info, "dgesv");
     if (info > 0)
-	error(_("Lapack routine dgesv: system is exactly singular"));
+	error(_("Lapack routine %s: system is exactly singular: U[%d,%d] = 0"),
+	      "dgesv", info, info);
     anorm = F77_CALL(dlange)("1", &n, &n, REAL(A), &n, (double*) NULL);
     work = (double *) R_alloc(4*n, sizeof(double));
     F77_CALL(dgecon)("1", &n, avals, &n, &anorm, &rcond, work, ipiv, &info);
