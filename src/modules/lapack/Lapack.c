c66fd7864ccf6729fb09678f29661e2b73fb8e74
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-06-03 19:44:13 +0000

    dgesv: check 'tol' only when sensible
    git-svn-id: https://svn.r-project.org/R/trunk@59504 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/modules/lapack/Lapack.c
+++ src/modules/lapack/Lapack.c
@@ -974,12 +974,14 @@ static SEXP modLa_dgesv(SEXP A, SEXP Bin, SEXP tolin)
     if (info > 0)
 	error(_("Lapack routine %s: system is exactly singular: U[%d,%d] = 0"),
 	      "dgesv", info, info);
-    anorm = F77_CALL(dlange)("1", &n, &n, REAL(A), &n, (double*) NULL);
-    work = (double *) R_alloc(4*n, sizeof(double));
-    F77_CALL(dgecon)("1", &n, avals, &n, &anorm, &rcond, work, ipiv, &info);
-    if (rcond < tol)
-	error(_("system is computationally singular: reciprocal condition number = %g"),
-	      rcond);
+    if(tol > 0) {
+	anorm = F77_CALL(dlange)("1", &n, &n, REAL(A), &n, (double*) NULL);
+	work = (double *) R_alloc(4*n, sizeof(double));
+	F77_CALL(dgecon)("1", &n, avals, &n, &anorm, &rcond, work, ipiv, &info);
+	if (rcond < tol)
+	    error(_("system is computationally singular: reciprocal condition number = %g"),
+		  rcond);
+    }
     UNPROTECT(1);
     return B;
 }
