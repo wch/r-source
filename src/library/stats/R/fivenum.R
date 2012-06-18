2ea5f1fb6edb1e5fa48e8156a295880c7741bb9d
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-06-18 20:44:49 +0000

    Modified fivnum to work with large vectors and reduce computations when no NAs.
    git-svn-id: https://svn.r-project.org/R/trunk@59575 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/R/fivenum.R
+++ src/library/stats/R/fivenum.R
@@ -17,10 +17,12 @@
 fivenum <- function(x, na.rm=TRUE)
 {
     xna <- is.na(x)
-    if(na.rm) x <- x[!xna]
-    else if(any(xna)) return(rep.int(NA,5))
+    if(any(xna)) {
+        if(na.rm) x <- x[!xna]
+        else return(rep.int(NA,5))
+    }
     x <- sort(x)
-    n <- length(x)
+    n <- xlength(x)
     if(n == 0) rep.int(NA,5)
     else {
         n4 <- floor((n+3)/2) / 2
