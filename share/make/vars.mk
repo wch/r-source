5f4f30f51fa6ddabd1a2964ae2f64f3cc1dfcd2f
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-07-17 07:36:40 +0000

    add utility to make a translations package, update notes
    git-svn-id: https://svn.r-project.org/R/trunk@59858 00db46b3-68df-0310-9c12-caf00c1e9a41

--- share/make/vars.mk
+++ share/make/vars.mk
@@ -1,6 +1,8 @@
 ## Static (i.e., not determined by configure) Make variables to be
 ## shared *and* grepped from m4/R.m4 and tools/*-recommended.
 
+## There is also 'translations', but that is not Priority: base
+## so it can be handled by update.packages() in due course.
 R_PKGS_BASE = base tools utils grDevices graphics stats datasets methods grid splines stats4 tcltk compiler parallel
 ## Those which can be installed initially compiled (not base tools)
 R_PKGS_BASE1 = utils grDevices graphics stats datasets methods grid splines stats4 tcltk parallel
--- src/library/tools/R/translations.R
+++ src/library/tools/R/translations.R
@@ -313,3 +313,27 @@ update_RGui_po <- function(srcdir)
 
     invisible()
 }
+
+## make package out of current translations.
+make_translations_pkg <- function(srcdir, outDir = ".", append = "-1")
+{
+    src <- file.path(srcdir, "src/library/translations")
+    dest <- file.path(tempdir(), "translations")
+    dir.create(dest, FALSE)
+    file.copy(file.path(src, "inst"),  dest, recursive = TRUE)
+    lines <- readLines(file.path(src, "DESCRIPTION.in"))
+    ver <- getRversion()
+    lines <- gsub("@VERSION@", ver, lines, fixed = TRUE)
+    lines[2] <- paste0(lines[2], append)
+    ver <- unclass(getRversion())[[1]]
+    deps <- sprintf("Depends: R (>= %s.%d.0), R (< %d.%d.0)",
+                    ver[1], ver[2], ver[1], ver[2] + 1)
+    lines <- c(lines, deps)
+    writeLines(lines, file.path(dest, "DESCRIPTION"))
+    cmd <- file.path(R.home(), "bin", "R")
+    cmd <- paste(cmd, "CMD", "build", shQuote(dest))
+    if(system(cmd) != 0L) stop("R CMD build failed")
+    tarball <- Sys.glob(file.path(tempdir(), "translations_*.tar.gz"))
+    file.rename(tarball, file.path(outDir, basename(tarball)))
+    invisible()
+}
