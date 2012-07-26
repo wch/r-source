3e872f2776237904a27732765b870155be2638f1
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-07-26 10:36:36 +0000

    convert known encodings
    git-svn-id: https://svn.r-project.org/R/trunk@59969 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/strwrap.R
+++ src/library/base/R/strwrap.R
@@ -33,6 +33,12 @@ function(x, width = 0.9 * getOption("width"), indent = 0, exdent = 0,
     ## input need not be valid in this locale, e.g. from write.dcf
     ## but if x has UTF-8 encoding we want to preserve it, so
     if(all(Encoding(x) == "UTF-8")) UB <- FALSE
+    else {
+        ## Let's convert anything else with a marked encoding
+        ## to the current locale
+        enc <- Encoding(x) %in% c("latin1", "UTF-8")
+        if(length(enc)) x[enc] <- enc2native(x[enc])
+    }
     z <- lapply(strsplit(x, "\n[ \t\n]*\n", perl = TRUE, useBytes = UB),
                 strsplit, "[ \t\n]", perl = TRUE, useBytes = UB)
     ## Now z[[i]][[j]] is a character vector of all "words" in
