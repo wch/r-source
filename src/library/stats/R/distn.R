f125639c981a6aec90ef60b84b41af75a98004f4
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-07-27 13:28:44 +0000

    allow specifying rate and scale=1/rate
    git-svn-id: https://svn.r-project.org/R/trunk@60000 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/R/distn.R
+++ src/library/stats/R/distn.R
@@ -49,30 +49,47 @@ qcauchy <-
 rcauchy <-
     function(n, location=0, scale=1) .External(C_rcauchy, n, location, scale)
 
+## allow a fuzz of ca 20ulp here.
 dgamma <- function(x, shape, rate = 1, scale = 1/rate, log = FALSE)
 {
-    if(!missing(rate) && !missing(scale))
-       stop("specify 'rate' or 'scale' but not both")
+    if(!missing(rate) && !missing(scale)) {
+        if(abs(rate*scale - 1) < 1e-15)
+            warning("specify 'rate' or 'scale' but not both")
+        else
+            stop("specify 'rate' or 'scale' but not both")
+    }
     .External(C_dgamma, x, shape, scale, log)
 }
 pgamma <- function(q, shape, rate = 1, scale = 1/rate,
                    lower.tail = TRUE, log.p = FALSE)
 {
-    if(!missing(rate) && !missing(scale))
-       stop("specify 'rate' or 'scale' but not both")
+    if(!missing(rate) && !missing(scale)) {
+        if(abs(rate*scale - 1) < 1e-15)
+            warning("specify 'rate' or 'scale' but not both")
+        else
+            stop("specify 'rate' or 'scale' but not both")
+    }
     .External(C_pgamma, q, shape, scale, lower.tail, log.p)
 }
 qgamma <- function(p, shape, rate = 1, scale = 1/rate,
                    lower.tail = TRUE, log.p = FALSE)
 {
-    if(!missing(rate) && !missing(scale))
-       stop("specify 'rate' or 'scale' but not both")
+    if(!missing(rate) && !missing(scale)) {
+        if(abs(rate*scale - 1) < 1e-15)
+            warning("specify 'rate' or 'scale' but not both")
+        else
+            stop("specify 'rate' or 'scale' but not both")
+    }
     .External(C_qgamma, p, shape, scale, lower.tail, log.p)
 }
 rgamma <- function(n, shape, rate = 1, scale = 1/rate)
 {
-    if(!missing(rate) && !missing(scale))
-       stop("specify 'rate' or 'scale' but not both")
+    if(!missing(rate) && !missing(scale)) {
+        if(abs(rate*scale - 1) < 1e-15)
+            warning("specify 'rate' or 'scale' but not both")
+        else
+            stop("specify 'rate' or 'scale' but not both")
+    }
     .External(C_rgamma, n, shape, scale)
 }
 dlnorm <- function(x, meanlog=0, sdlog=1, log=FALSE)
