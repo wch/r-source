d0788475115ee000d5afd9ad83fb07fc54fbb47b
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-07-27 11:31:55 +0000

    revert this part to 8.30
    git-svn-id: https://svn.r-project.org/R/trunk@59993 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/extra/pcre/sljit/sljitUtils.c
+++ src/extra/pcre/sljit/sljitUtils.c
@@ -262,8 +262,9 @@ SLJIT_API_FUNC_ATTRIBUTE sljit_w SLJIT_CALL sljit_stack_resize(struct sljit_stac
 	}
 	aligned_new_limit = (new_limit + sljit_page_align) & ~sljit_page_align;
 	aligned_old_limit = (stack->limit + sljit_page_align) & ~sljit_page_align;
+// R change
 	if (aligned_new_limit < aligned_old_limit)
-		posix_madvise((void*)aligned_new_limit, aligned_old_limit - aligned_new_limit, POSIX_MADV_DONTNEED);
+		madvise((void*)aligned_new_limit, aligned_old_limit - aligned_new_limit, MADV_DONTNEED);
 	stack->limit = new_limit;
 	return 0;
 #endif
