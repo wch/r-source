661287917e952529a4548b0e724cff26afcbb33e
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-07-31 07:50:23 +0000

    tests were in wrong order
    git-svn-id: https://svn.r-project.org/R/branches/R-2-15-branch@60055 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/tabulate.R
+++ src/library/base/R/tabulate.R
@@ -22,10 +22,9 @@ tabulate <- function(bin, nbins = max(1L, bin, na.rm = TRUE))
     if (typeof(bin) != "integer") bin <- as.integer(bin)
     nb <- as.integer(length(bin))
     if (is.na(nb)) stop("invalid value of length(bin)")
-    nbins <- as.integer(nbins)
-    if (is.na(nbins)) stop("invalid value of nbins")
-    ## could remove once we have long vectors ....
     if (nbins > .Machine$integer.max)
         stop("attempt to make a table with >= 2^31 elements")
+    nbins <- as.integer(nbins)
+    if (is.na(nbins)) stop("invalid value of nbins")
     .Call("R_Tabulate", bin, nbins, PACKAGE = "base")
 }
