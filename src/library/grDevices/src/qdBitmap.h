1ac9638909a54ee398a888c46937fec3fa6bbc82
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-08-03 13:07:54 +0000

    more missing headers
    git-svn-id: https://svn.r-project.org/R/trunk@60106 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/grDevices/src/qdBitmap.h
+++ src/library/grDevices/src/qdBitmap.h
@@ -1,3 +1,25 @@
+/*
+ *  R : A Computer Language for Statistical Data Analysis
+ *  Copyright (C) 2007  The R Foundation
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, a copy is available at
+ *  http://www.r-project.org/Licenses/
+ *
+ *  Quartz Quartz device module header file
+ *
+ */
+
 #include <R.h>
 #include <R_ext/QuartzDevice.h>
 
