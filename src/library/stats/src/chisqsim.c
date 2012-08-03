0bafda12b28d1ab27df4f37487b66596ed61eb82
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-08-03 12:09:28 +0000

    add missing headers
    git-svn-id: https://svn.r-project.org/R/branches/R-2-15-branch@60101 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/src/chisqsim.c
+++ src/library/stats/src/chisqsim.c
@@ -1,3 +1,22 @@
+/*
+ *  R : A Computer Language for Statistical Data Analysis
+ *  Copyright (C) 2001-5   The R Core Team.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, a copy is available at
+ *  http://www.r-project.org/Licenses/
+ */
+
 #include <Rmath.h>
 #include <R_ext/Random.h>
 #include <R_ext/Applic.h>
