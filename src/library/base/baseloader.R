92ea26db9fb27729da004a5e455a42aed7331a31
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-08-05 09:59:41 +0000

    add some notices
    git-svn-id: https://svn.r-project.org/R/trunk@60153 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/baseloader.R
+++ src/library/base/baseloader.R
@@ -1,7 +1,24 @@
-## this should be kept in step with code in R/lazyload.R
-.Internal(eval(quote({
+#  File src/library/base/baseloader.R
+#  Part of the R package, http://www.R-project.org
 #
 #  Copyright (C) 1995-2012 The R Core Team
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  A copy of the GNU General Public License is available at
+#  http://www.r-project.org/Licenses/
+
+
+## this should be kept in step with code in R/lazyload.R
+.Internal(eval(quote({
 ..lazyLoad <- function(filebase, envir = parent.frame())
 {
     ##
--- src/library/base/makebasedb.R
+++ src/library/base/makebasedb.R
@@ -1,7 +1,23 @@
-local({
-    makeLazyLoadDB <- function(from, filebase, compress = TRUE, ascii = FALSE,
+#  File src/library/base/makebasedb.R
+#  Part of the R package, http://www.R-project.org
 #
 #  Copyright (C) 1995-2012 The R Core Team
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  A copy of the GNU General Public License is available at
+#  http://www.r-project.org/Licenses/
+
+local({
+    makeLazyLoadDB <- function(from, filebase, compress = TRUE, ascii = FALSE,
                                variables) {
 
         envlist <- function(e) {
