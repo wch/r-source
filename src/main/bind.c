a2b31be77d7726d2bc0139fcc7af2033287a124e
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-08-15 06:35:19 +0000

    typo
    git-svn-id: https://svn.r-project.org/R/trunk@60267 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/bind.c
+++ src/main/bind.c
@@ -26,7 +26,7 @@
 #endif
 
 #include <Defn.h>
-#include <R_ext/Prtutil.h> // for IndexWidth
+#include <R_ext/PrtUtil.h> // for IndexWidth
 #define imax2(x, y) ((x < y) ? y : x)
 
 #include "RBufferUtils.h"
