24a32d6164348d2512e8ff7367cb92ff5b8d949e
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-08-24 12:18:37 +0000

    typo
    git-svn-id: https://svn.r-project.org/R/trunk@60408 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/names.c
+++ src/main/names.c
@@ -992,7 +992,7 @@ FUNTAB R_FunTab[] =
 {"getSymbolInfo",do_getSymbolInfo,0,	11,	3,	{PP_FUNCALL, PREC_FN,	0}},
 {".isMethodsDispatchOn",do_S4on,0,	1,	-1,	{PP_FUNCALL, PREC_FN,	0}},
 {"lazyLoadDBfetch",do_lazyLoadDBfetch,0,1,	4,	{PP_FUNCALL, PREC_FN,	0}},
-{"lazyLoadDBflush",do_lazyLoadDBflush,0,1,	1,	{PP_FUNCALL, PREC_FN,	0}},
+{"lazyLoadDBflush",do_lazyLoadDBflush,0,11,	1,	{PP_FUNCALL, PREC_FN,	0}},
 {"getVarsFromFrame",do_getVarsFromFrame, 0,	11,	3,	{PP_FUNCALL, PREC_FN,	0}},
 {"lazyLoadDBinsertValue",do_lazyLoadDBinsertValue, 0,	11,	5,	{PP_FUNCALL, PREC_FN,	0}},
 
