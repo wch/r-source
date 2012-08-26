0220ae1630b9ca2203c8f5fb61ea6ab60a5dadd8
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-08-26 08:57:57 +0000

    tweak for r60421
    git-svn-id: https://svn.r-project.org/R/trunk@60428 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/modules/lapack/Lapack.c
+++ src/modules/lapack/Lapack.c
@@ -999,7 +999,7 @@ static SEXP La_dgesv(SEXP A, SEXP Bin, SEXP tolin)
 	    Bdn = allocVector(VECSXP, 2);
 	    if (!isNull(Adn)) SET_VECTOR_ELT(Bdn, 0, VECTOR_ELT(Adn, 1));
 	    if (!isNull(Bindn)) SET_VECTOR_ELT(Bdn, 1, VECTOR_ELT(Bindn, 1));
-	    if (!isNull(VECTOR_ELT(Bdn, 0)) && !isNull(VECTOR_ELT(Bdn, 1)))
+	    if (!isNull(VECTOR_ELT(Bdn, 0)) || !isNull(VECTOR_ELT(Bdn, 1)))
 		setAttrib(B, R_DimNamesSymbol, Bdn);
 	}
     } else {
