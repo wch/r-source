a8317cf51e2ea170b6c25d31ab4a07e96ed9541e
urbaneks <urbaneks@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-08-29 19:55:21 +0000

    work around vecLib requiring Accelerate in 10.8
    git-svn-id: https://svn.r-project.org/R/trunk@60485 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/modules/lapack/vecLibg95c.c
+++ src/modules/lapack/vecLibg95c.c
@@ -2,7 +2,13 @@
 #include <config.h>
 #endif
 
+#include <AvailabilityMacros.h> /* for MAC_OS_X_VERSION_10_* -- present on 10.2+ (according to Apple) */
+/* Since OS X 10.8 vecLib requires Accelerate to be included first (which in turn includes vecLib) */
+#if defined MAC_OS_X_VERSION_10_8 && MAC_OS_X_VERSION_MIN_REQUIRED >= 1040
+#include <Accelerate/Accelerate.h>
+#else
 #include <vecLib/vecLib.h>
+#endif
 
 void F77_FUNC_(rcblas_cdotu_sub,)(const int *N, const void *X, const int *incX,
                        const void *Y, const int *incY, void *dotu) 
