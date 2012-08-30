3ad9e64260d7e60d9967a60ef1ccd826bb9d3ed0
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-08-30 10:11:49 +0000

    take more of rowsums internal
    git-svn-id: https://svn.r-project.org/R/trunk@60496 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/rowsum.R
+++ src/library/base/R/rowsum.R
@@ -20,32 +20,20 @@ rowsum <- function(x, group, reorder = TRUE, ...) UseMethod("rowsum")
 
 rowsum.default <- function(x, group, reorder = TRUE, na.rm = FALSE, ...)
 {
-    if (!is.numeric(x))
-        stop("'x' must be numeric")
-    if (length(group) != NROW(x))
-        stop("incorrect length for 'group'")
-    if (any(is.na(group)))
-        warning("missing values for 'group'")
+    if (!is.numeric(x)) stop("'x' must be numeric")
+    if (length(group) != NROW(x)) stop("incorrect length for 'group'")
+    if (any(is.na(group))) warning("missing values for 'group'")
     ugroup <- unique(group)
     if (reorder) ugroup <- sort(ugroup, na.last = TRUE, method = "quick")
-
-    rval <- .Internal(rowsum_matrix(x, NCOL(x), group, ugroup, na.rm))
-
-    dimnames(rval) <- list(as.character(ugroup), dimnames(x)[[2L]])
-    rval
+    .Internal(rowsum_matrix(x, NCOL(x), group, ugroup, na.rm))
 }
 
 rowsum.data.frame <- function(x, group, reorder = TRUE, na.rm = FALSE, ...)
 {
     if (!is.data.frame(x)) stop("not a data frame") ## make MM happy
-    if (length(group) != NROW(x))
-        stop("incorrect length for 'group'")
-    if (any(is.na(group)))
-        warning("missing values for 'group'")
+    if (length(group) != NROW(x)) stop("incorrect length for 'group'")
+    if (any(is.na(group))) warning("missing values for 'group'")
     ugroup <- unique(group)
     if (reorder) ugroup <- sort(ugroup, na.last = TRUE, method = "quick")
-
-    rval <- .Internal(rowsum_df(x, NCOL(x), group, ugroup, na.rm))
-
-    as.data.frame(rval, row.names = as.character(ugroup))
+    .Internal(rowsum_df(x, group, ugroup, na.rm))
 }
