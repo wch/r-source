61f2b4d951e372bf109a87031d1d3a00148f9118
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-09-05 10:58:02 +0000

    cleaned up version
    git-svn-id: https://svn.r-project.org/R/branches/R-2-15-branch@60569 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/R/bandwidths.R
+++ src/library/stats/R/bandwidths.R
@@ -37,135 +37,99 @@ bw.nrd <- function (x)
     1.06 * min(sqrt(var(x)), h) * length(x)^(-1/5)
 }
 
-bw.SJ <- function(x, nb = 1000, lower = 0.1*hmax, upper = hmax,
+bw.SJ <- function(x, nb = 1000L, lower = 0.1*hmax, upper = hmax,
                   method = c("ste", "dpi"), tol = 0.1*lower)
 {
     if((n <- length(x)) < 2L) stop("need at least 2 data points")
+    n <- as.integer(n)
+    if (is.na(n)) stop("invalid length(x)")
     if(!is.numeric(x)) stop("invalid 'x'")
+    nb <- as.integer(nb)
+    if (is.na(nb) || nb <= 0L) stop("invalid 'nb'")
     storage.mode(x) <- "double"
+
     method <- match.arg(method)
 
-    fSD <- function(h, x, alph2, c1, n, d)
-        (c1/SDh(x, alph2 * h^(5/7), n, d))^(1/5) - h
-    SDh <- function(x, h, n, d)
-        .C(C_band_phi4_bin,
-           as.integer(n),
-           as.integer(length(x)),
-           as.double(d),
-           x,
-           as.double(h),
-           u = double(1))$u
-    TDh <- function(x, h, n, d)
-        .C(C_band_phi6_bin,
-           as.integer(n),
-           as.integer(length(x)),
-           as.double(d),
-           x,
-           as.double(h),
-           u = double(1))$u
+    fSD <- function(h) (c1/SDh(alph2 * h^(5/7)))^(1/5) - h
+    SDh <- function(h)
+        .C(C_band_phi4_bin, n, nb, as.double(d), cnt, as.double(h), u = double(1L))$u
+    TDh <- function(h)
+        .C(C_band_phi6_bin, n, nb, as.double(d), cnt, as.double(h), u = double(1L))$u
 
-    Z <- .C(C_band_den_bin,
-            as.integer(n),
-            as.integer(nb),
-            d = double(1),
-            x,
-            cnt = integer(nb))
+    Z <- .C(C_band_den_bin, n, nb, d = double(1L), x, cnt = integer(nb))
     d <- Z$d; cnt <- as.integer(Z$cnt)
     scale <- min(sd(x), IQR(x)/1.349)
     a <- 1.24 * scale * n^(-1/7)
     b <- 1.23 * scale * n^(-1/9)
     c1 <- 1/(2*sqrt(pi)*n)
-    TD  <- -TDh(cnt, b, n, d)
-    if(!is.finite(TD) || TD <= 0)
-        stop("sample is too sparse to find TD")
+    TD  <- -TDh(b)
+    if(!is.finite(TD) || TD <= 0) stop("sample is too sparse to find TD")
     if(method == "dpi")
-        res <- (c1/SDh(cnt, (2.394/(n * TD))^(1/7), n, d))^(1/5)
+        res <- (c1/SDh((2.394/(n * TD))^(1/7)))^(1/5)
     else {
         if(bnd.Miss <- missing(lower) || missing(upper)) {
             ## only used for  lower & upper  defaults :
             hmax <- 1.144 * scale * n^(-1/5)
-            ##              ^^^^^ used to be  sd(x)  which maybe way off
         }
-        alph2 <- 1.357*(SDh(cnt, a, n, d)/TD)^(1/7)
-        if(!is.finite(alph2))
-            stop("sample is too sparse to find alph2")
-        itry <- 1
-	while (fSD(lower, cnt, alph2, c1, n, d) *
-	       fSD(upper, cnt, alph2, c1, n, d) > 0) {
-	    if(itry > 99 || !bnd.Miss) # 1.2 ^ 99 = 69'014'979 .. enough
+        alph2 <- 1.357*(SDh(a)/TD)^(1/7)
+        if(!is.finite(alph2)) stop("sample is too sparse to find alph2")
+        itry <- 1L
+	while (fSD(lower) * fSD(upper) > 0) {
+	    if(itry > 99L || !bnd.Miss) # 1.2 ^ 99 = 69'014'979 .. enough
 		stop("no solution in the specified range of bandwidths")
-	    if(itry %% 2)
-		upper <- upper * 1.2
-	    else lower <- lower / 1.2
+	    if(itry %% 2) upper <- upper * 1.2 else lower <- lower / 1.2
 	    if(getOption("verbose"))
 		message(gettextf("increasing bw.SJ() search interval (%d) to [%.4g,%.4g]",
-			itry, lower, upper), domain = NA)
-	    itry <- itry + 1
+                                 itry, lower, upper), domain = NA)
+	    itry <- itry + 1L
 	}
-        res <- uniroot(fSD, c(lower, upper), tol=tol,
-                       x=cnt, alph2=alph2, c1=c1, n=n, d=d)$root
+        res <- uniroot(fSD, c(lower, upper), tol = tol)$root
     }
     res
 }
 
 
-bw.ucv <- function(x, nb = 1000, lower = 0.1*hmax, upper = hmax,
+bw.ucv <- function(x, nb = 1000L, lower = 0.1*hmax, upper = hmax,
                    tol = 0.1*lower)
 {
     if((n <- length(x)) < 2L) stop("need at least 2 data points")
+    n <- as.integer(n)
+    if (is.na(n)) stop("invalid length(x)")
     if(!is.numeric(x)) stop("invalid 'x'")
+    nb <- as.integer(nb)
+    if (is.na(nb) || nb <= 0L) stop("invalid 'nb'")
+    storage.mode(x) <- "double"
 
-    fucv <- function(h, x, n, d)
-        .C(C_band_ucv_bin,
-           as.integer(n),
-           as.integer(length(x)),
-           as.double(d),
-           x,
-           as.double(h),
-           u = double(1))$u
+    fucv <- function(h)
+        .C(C_band_ucv_bin, n, nb, as.double(d), cnt, as.double(h), u = double(1L))$u
 
     hmax <- 1.144 * sqrt(var(x)) * n^(-1/5)
-    storage.mode(x) <- "double"
-    Z <- .C(C_band_den_bin,
-            as.integer(n),
-            as.integer(nb),
-            d = double(1),
-            x,
-            cnt = integer(nb))
-    d <- Z$d; cnt <- as.integer(Z$cnt)
-    h <- optimize(fucv, c(lower, upper), tol=tol,
-                  x=cnt, n=n, d=d)$minimum
+    Z <- .C(C_band_den_bin, n, nb, d = double(1L), x, cnt = integer(nb))
+    d <- Z$d; cnt <- Z$cnt
+    h <- optimize(fucv, c(lower, upper), tol=tol)$minimum
     if(h < lower+tol | h > upper-tol)
         warning("minimum occurred at one end of the range")
     h
 }
 
-bw.bcv <- function(x, nb = 1000, lower = 0.1*hmax, upper = hmax,
+bw.bcv <- function(x, nb = 1000L, lower = 0.1*hmax, upper = hmax,
                    tol = 0.1*lower)
 {
     if((n <- length(x)) < 2L) stop("need at least 2 data points")
+    n <- as.integer(n)
+    if (is.na(n)) stop("invalid length(x)")
     if(!is.numeric(x)) stop("invalid 'x'")
+    nb <- as.integer(nb)
+    if (is.na(nb) || nb <= 0L) stop("invalid 'nb'")
+    storage.mode(x) <- "double"
 
-    fbcv <- function(h, x, n, d)
-        .C(C_band_bcv_bin,
-           as.integer(n),
-           as.integer(length(x)),
-           as.double(d),
-           x,
-           as.double(h),
-           u = double(1))$u
+    fbcv <- function(h)
+        .C(C_band_bcv_bin, n, nb, as.double(d), cnt, as.double(h), u = double(1L))$u
 
     hmax <- 1.144 * sqrt(var(x)) * n^(-1/5)
-    storage.mode(x) <- "double"
-    Z <- .C(C_band_den_bin,
-            as.integer(n),
-            as.integer(nb),
-            d = double(1),
-            x,
-            cnt = integer(nb))
-    d <- Z$d; cnt <- as.integer(Z$cnt)
-    h <- optimize(fbcv, c(lower, upper), tol=tol,
-                  x=cnt, n=n, d=d)$minimum
+    Z <- .C(C_band_den_bin, n, nb, d = double(1L), x, cnt = integer(nb))
+    d <- Z$d; cnt <- Z$cnt
+    h <- optimize(fbcv, c(lower, upper), tol = tol)$minimum
     if(h < lower+tol | h > upper-tol)
         warning("minimum occurred at one end of the range")
     h
