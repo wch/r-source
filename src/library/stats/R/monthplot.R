72b548d54a36af6f6a88fff151150dc8bdd7b6aa
murdoch <murdoch@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-09-15 19:48:07 +0000

    Allow control of monthplot reference line display.
    git-svn-id: https://svn.r-project.org/R/trunk@60727 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/R/monthplot.R
+++ src/library/stats/R/monthplot.R
@@ -52,7 +52,9 @@ monthplot.default <-
               ylab = deparse(substitute(x)),
               times = seq_along(x),
               phase = (times - 1L)%%length(labels) + 1L, base = mean,
-              axes = TRUE, type = c("l", "h"), box = TRUE, add = FALSE, ...)
+              axes = TRUE, type = c("l", "h"), box = TRUE, add = FALSE,
+              col = par("col"), lty = par("lty"), lwd = par("lwd"),
+              col.base = col, lty.base = lty, lwd.base = lwd, ...)
 {
     dots <- list(...); nmdots <- names(dots)
     type <- match.arg(type)
@@ -75,14 +77,17 @@ monthplot.default <-
         Call$xlab <- if("xlab" %in% nmdots) dots$xlab else ""
         if(box) Call$frame.plot <- TRUE
         Call$labels <- Call$times <- Call$phase <- Call$base <-
-            Call$type <- Call$box <- Call$add <- NULL
+            Call$type <- Call$box <- Call$add <- Call$col.base <- 
+            Call$lty.base <- Call$lwd.base <- NULL
         eval(Call)
         if (axes) {
             axis(1, at = 1L:f, labels = labels, ...)
             axis(2, ...)
         }
-        if (!is.null(base))
-            segments(1L:f - 0.45, means, 1L:f + 0.45, means)
+        if (!is.null(base)) {
+            segments(1L:f - 0.45, means, 1L:f + 0.45, means,
+                     col = col.base, lty = lty.base, lwd = lwd.base)
+        }
     }
     y <- as.numeric(times)
     scale <- 1 / diff(range(y, na.rm = TRUE)) * 0.9
@@ -90,9 +95,10 @@ monthplot.default <-
         sub <- phase == i
         if (type != "h")
             lines((y[sub] - min(y)) * scale - 0.45 + i, x[sub],
-                  type = type, ...)
+                  type = type, col = col, lty = lty, lwd = lwd, ...)
         else segments((y[sub] - min(y)) * scale - 0.45 + i, means[i],
-                      (y[sub] - min(y)) * scale - 0.45 + i, x[sub], ...)
+                      (y[sub] - min(y)) * scale - 0.45 + i, x[sub], 
+                      col = col, lty = lty, lwd = lwd, ...)
     }
     invisible()
 }
