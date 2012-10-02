3ac12761eaddaedb8980ca2ddab6d7fdcc649b4c
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-10-02 08:55:24 +0000

    typo
    git-svn-id: https://svn.r-project.org/R/branches/R-2-15-branch@60856 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/grid/R/components.R
+++ src/library/grid/R/components.R
@@ -174,7 +174,7 @@ grid.xaxis <- function(at=NULL, label=TRUE, main=TRUE,
     major <- make.xaxis.major(at, main)
     ticks <- make.xaxis.ticks(at, main)
     if (is.logical(label) && length(label) == 0)
-	stop("logical 'label' supplied on length 0")
+	stop("logical 'label' supplied of length 0")
     if (is.logical(label) && !label)
       labels <- NULL
     else
@@ -301,7 +301,7 @@ grid.yaxis <- function(at=NULL, label=TRUE, main=TRUE,
     major <- make.yaxis.major(at, main)
     ticks <- make.yaxis.ticks(at, main)
     if (is.logical(label) && length(label) == 0)
-	stop("logical 'label' supplied on length 0")
+	stop("logical 'label' supplied of length 0")
     if (is.logical(label) && !label)
       labels <- NULL
     else
