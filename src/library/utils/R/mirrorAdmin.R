c2b87bb8e2b62b47e039d3f3d361d94e8dc8890f
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-10-03 08:41:31 +0000

    Improve HTML generation.
    git-svn-id: https://svn.r-project.org/R/trunk@60864 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/utils/R/mirrorAdmin.R
+++ src/library/utils/R/mirrorAdmin.R
@@ -23,19 +23,21 @@ mirror2html <- function(mirrors = NULL, file="mirrors.html",
     if(is.null(mirrors)){
         mirrors <- getCRANmirrors(all=FALSE, local.only=TRUE)
     }
+    mirrors$Host <- gsub("&", "&amp;", mirrors$Host)
     z <- NULL
     if(file.exists(head)) z <- readLines(head)
     z <- c(z, "<dl>")
-    for(country in unique(mirrors$Country)){
-        m = mirrors[mirrors$Country==country,]
+    for(country in unique(mirrors$Country)) {
+        m <- mirrors[mirrors$Country == country,]
         z <- c(z, paste0("<dt>", country, "</dt>"),
                "<dd>",
-               "<table border=0 width=90%>")
-        for(k in 1L:nrow(m)){
+               sprintf("<table border=0 width=90%% summary=\"%s\">",
+                       country))
+        for(k in seq_len(nrow(m))) {
             z <- c(z, "<tr>",
                    "<td width=45%>",
-                   "<a href=\"", m$URL[k], "\" target=\"_top\">",
-                   m$URL[k], "</a>",
+                   sprintf("<a href=\"%s\" target=\"_top\">%s</a>",
+                           m$URL[k], m$URL[k]),
                    "</td>\n",
                    "<td>", m$Host[k], "</td>",
                    "</tr>")
