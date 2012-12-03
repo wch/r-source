dc2249bd996e2d5fd3375ebeedee23a1979d1a1a
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-12-03 10:24:05 +0000

    shorten line
    git-svn-id: https://svn.r-project.org/R/trunk@61193 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/files.R
+++ src/library/base/R/files.R
@@ -74,9 +74,10 @@ file.remove <- function(...)
 file.rename <- function(from, to)
     .Internal(file.rename(from, to))
 
-list.files <- function(path = ".", pattern = NULL, all.files = FALSE,
-                       full.names = FALSE, recursive = FALSE,
-		       ignore.case = FALSE, include.dirs = FALSE, no.. = FALSE)
+list.files <-
+    function(path = ".", pattern = NULL, all.files = FALSE,
+             full.names = FALSE, recursive = FALSE,
+             ignore.case = FALSE, include.dirs = FALSE, no.. = FALSE)
     .Internal(list.files(path, pattern, all.files, full.names,
 			 recursive, ignore.case, include.dirs, no..))
 
