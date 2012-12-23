f12717e7744fde84e66115101d99f12ab0ead4d6
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-12-23 12:49:24 +0000

    update windlgs
    git-svn-id: https://svn.r-project.org/R/branches/R-2-15-branch@61395 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/windlgs/R/windlgs.R
+++ src/gnuwin32/windlgs/R/windlgs.R
@@ -30,14 +30,14 @@ menu.ttest3 <- function() .Call("menu_ttest3")
 
 del.ttest <- function() winMenuDel("Statistics")
 
-.First.lib <- function(lib, pkg)
+.onAttach <- function(libname, pkgname)
 {
-    require(stats) # for t.test
-    library.dynam("windlgs", pkg, lib)
-    winMenuAdd("Statistics")
-    winMenuAdd("Statistics/Classical tests")
-    winMenuAddItem("Statistics/Classical tests", "t-test:1", "menu.ttest()")
-    winMenuAddItem("Statistics/Classical tests", "t-test:2", "menu.ttest2()")
-    winMenuAddItem("Statistics/Classical tests", "t-test:3", "menu.ttest3()")
-    cat("To remove the Statistics menu use del.ttest()\n")
+    if(interactive()) {
+        winMenuAdd("Statistics")
+        winMenuAdd("Statistics/Classical tests")
+        winMenuAddItem("Statistics/Classical tests", "t-test:1", "menu.ttest()")
+        winMenuAddItem("Statistics/Classical tests", "t-test:2", "menu.ttest2()")
+        winMenuAddItem("Statistics/Classical tests", "t-test:3", "menu.ttest3()")
+        cat("To remove the Statistics menu use del.ttest()\n")
+    }
 }
