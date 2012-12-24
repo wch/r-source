88df2effaf5972a19ed7a28d80edc843235a9cd4
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-12-24 20:31:35 +0000

    missing import (found when installing GroupSeq)
    git-svn-id: https://svn.r-project.org/R/trunk@61415 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tcltk/R/Tk.R
+++ src/library/tcltk/R/Tk.R
@@ -338,7 +338,7 @@ tktoplevel    <- function(parent = .TkRoot,...) {
                    rm(list = ID, envir = parent$env)
                tkbind(w, "<Destroy>","")
            })
-    process.events()
+    utils::process.events()
     w
 }
 ### ------ Window & Geometry managers, widget commands &c ------
