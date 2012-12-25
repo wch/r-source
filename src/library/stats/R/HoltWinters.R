c7a0ae146964e7eb59235198cc0110a12688d539
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-12-25 10:50:45 +0000

    remove some more trailing spaces
    git-svn-id: https://svn.r-project.org/R/trunk@61431 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/R/HoltWinters.R
+++ src/library/stats/R/HoltWinters.R
@@ -343,12 +343,12 @@ print.HoltWinters <- function (x, ...)
         if (is.logical(x$beta) && !x$beta) "without" else "with", "trend and",
         if (is.logical(x$gamma) && !x$gamma) "without" else
         paste0(if (is.logical(x$beta) && !x$beta) "with ", x$seasonal),
-        "seasonal component.\n")
-    cat("\nCall:\n", deparse (x$call), "\n\n")
+        "seasonal component.")
+    cat("\n\nCall:\n", deparse (x$call), "\n\n", sep = "")
     cat("Smoothing parameters:\n")
-    cat(" alpha: ", x$alpha, "\n")
-    cat(" beta : ", x$beta, "\n")
-    cat(" gamma: ", x$gamma, "\n\n")
+    cat(" alpha: ", x$alpha, "\n", sep = "")
+    cat(" beta : ", x$beta, "\n", sep = "")
+    cat(" gamma: ", x$gamma, "\n\n", sep = "")
 
     cat("Coefficients:\n")
     print(t(t(x$coefficients)))
