52c021003b6c2befec046b7a9c55172a787ea6bd
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-01-01 14:24:31 +0000

    more cleaning up, more informative message
    git-svn-id: https://svn.r-project.org/R/trunk@61506 00db46b3-68df-0310-9c12-caf00c1e9a41

--- share/make/vars.mk
+++ share/make/vars.mk
@@ -8,7 +8,6 @@ R_PKGS_BASE = base tools utils grDevices graphics stats datasets methods grid sp
 R_PKGS_BASE1 = utils grDevices graphics stats datasets methods grid splines stats4 tcltk parallel
 ## Those with standard R directories (not datasets, methods)
 R_PKGS_BASE2 = base tools utils grDevices graphics stats grid splines stats4 tcltk compiler parallel
-R_PKGS_refman = base datasets grDevices graphics grid methods parallel splines stats stats4 tcltk tools utils
 
 R_PKGS_RECOMMENDED =  MASS lattice Matrix nlme survival boot cluster codetools foreign KernSmooth rpart class nnet spatial mgcv
 # there are dependencies in src/library/Recommended/Makefile*
--- src/library/methods/R/SClasses.R
+++ src/library/methods/R/SClasses.R
@@ -955,10 +955,11 @@ inferProperties <- function(props, what) {
         if(!n)
             return(character())
         else if(is.null(propNames))
-            stop(gettextf("No %s name supplied", what),
+            stop(gettextf("No %s names supplied", what),
                  domain = NA, call. = FALSE)
         else if(!all(nzchar(propNames)))
-            stop(gettextf("All %s names must be nonempty", what),
+            stop(gettextf("All %s names must be nonempty in:\n(%s)", what,
+                          paste(sQuote(propNames), collapse = ", ")),
                  domain = NA, call. = FALSE)
         else if(any(duplicated(propNames))) # NB: not translatable because of plurals
             stop(gettextf("All %s names must be distinct in:\n(%s)", what,
