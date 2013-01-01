99d7e75b3e4397c7ddef610eb83eb9698ad3bcd0
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-01-01 08:07:46 +0000

    centralize package lists
    git-svn-id: https://svn.r-project.org/R/trunk@61500 00db46b3-68df-0310-9c12-caf00c1e9a41

--- share/make/vars.mk
+++ share/make/vars.mk
@@ -3,12 +3,12 @@
 
 ## There is also 'translations', but that is not Priority: base
 ## so it can be handled by update.packages() in due course.
-## If you add packages here, also update doc/manual/Makefile.{in,win}
 R_PKGS_BASE = base tools utils grDevices graphics stats datasets methods grid splines stats4 tcltk compiler parallel
 ## Those which can be installed initially compiled (not base tools)
 R_PKGS_BASE1 = utils grDevices graphics stats datasets methods grid splines stats4 tcltk parallel
 ## Those with standard R directories (not datasets, methods)
 R_PKGS_BASE2 = base tools utils grDevices graphics stats grid splines stats4 tcltk compiler parallel
+R_PKGS_refman = base datasets grDevices graphics grid methods parallel splines stats stats4 tcltk tools utils
 
 R_PKGS_RECOMMENDED =  MASS lattice Matrix nlme survival boot cluster codetools foreign KernSmooth rpart class nnet spatial mgcv
 # there are dependencies in src/library/Recommended/Makefile*
