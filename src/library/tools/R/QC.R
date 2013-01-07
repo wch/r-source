90118c499a8524d8c9128f6e330e4919c9aa17b2
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-01-07 14:31:10 +0000

    tweaks
    git-svn-id: https://svn.r-project.org/R/trunk@61578 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/QC.R
+++ src/library/tools/R/QC.R
@@ -1,7 +1,7 @@
 #  File src/library/tools/R/QC.R
 #  Part of the R package, http://www.R-project.org
 #
-#  Copyright (C) 1995-2012 The R Core Team
+#  Copyright (C) 1995-2013 The R Core Team
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -4166,8 +4166,8 @@ function(x, ...)
         }
         if(len <- length(xi$Warnings))
             writeLines(c(sprintf(ngettext(len,
-                                          "Warning in file '%s':",
-                                          "Warnings in file '%s':"),
+                                          "Warning in file %s:",
+                                          "Warnings in file %s:"),
                                  sQuote(xi$File)),
                          paste(" ", gsub("\n\n", "\n  ", xi$Warnings,
 					 perl = TRUE, useBytes = TRUE))))
