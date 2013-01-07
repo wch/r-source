2da6a104e2b8055f0d0321cd0bcd06421f446817
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-01-07 16:47:01 +0000

    replace sapply() by faster versions
    git-svn-id: https://svn.r-project.org/R/trunk@61582 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/funprog.R
+++ src/library/base/R/funprog.R
@@ -95,7 +95,7 @@ function(f, x, init, right = FALSE, accumulate = FALSE)
         ## If all results have length one, we can simplify.
         ## (Note that we do not simplify to arrays in case all results
         ## have a common length > 1.)
-        if(all(sapply(out, length) == 1L))
+	if(all(vapply(out, length, 1.) == 1L))
             out <- unlist(out, recursive = FALSE)
         out
     }
@@ -104,7 +104,7 @@ function(f, x, init, right = FALSE, accumulate = FALSE)
 Filter <-
 function(f, x)
 {
-    ind <- as.logical(sapply(x, f))
+    ind <- as.logical(unlist(lapply(x, f)))
     x[!is.na(ind) & ind]
 }
 
