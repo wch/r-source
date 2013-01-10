84ebac6237e836dcfb92fd6968609d1692d80fab
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-01-10 13:02:20 +0000

    more cleaning up
    git-svn-id: https://svn.r-project.org/R/trunk@61606 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/methods/R/zzz.R
+++ src/library/methods/R/zzz.R
@@ -1,7 +1,7 @@
 #  File src/library/methods/R/zzz.R
 #  Part of the R package, http://www.R-project.org
 #
-#  Copyright (C) 1995-2012 The R Core Team
+#  Copyright (C) 1995-2013 The R Core Team
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -17,8 +17,15 @@
 #  http://www.r-project.org/Licenses/
 
 utils::globalVariables(c(".possibleExtends", ".makeGeneric",
-                         ".newClassRepresentation", ".classGeneratorFunction",
-                         ".mergeClassDefSlots", "..mergeClassDefSlots"))
+                         ".newClassRepresentation",
+                         ".classGeneratorFunction",
+                         ".mergeClassDefSlots",
+                         "..mergeClassDefSlots", "..classEnv",
+                         "..addToMetaTable", "..extendsForS3",
+                         "..isPrototype", ".isSealedMethod",
+                         "..requirePackage", ".implicitGeneric",
+                         "..checkRequiredGenerics",
+                         "..methodPackageSlots"))
 
 ..onLoad  <-
   ## Initialize the methods package.  Called from .onLoad, first
