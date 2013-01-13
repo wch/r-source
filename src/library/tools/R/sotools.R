0841b5cd1ba12be676a4a8969ed32aa9f8ee0a6d
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-01-13 09:09:12 +0000

    port r61630 from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-2-15-branch@61631 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/sotools.R
+++ src/library/tools/R/sotools.R
@@ -1,7 +1,23 @@
+#  File src/library/tools/R/sotools.R
+#  Part of the R package, http://www.R-project.org
+#
+#  Copyright (C) 2011-2 The R Core Team
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  A copy of the GNU General Public License is available at
+#  http://www.r-project.org/Licenses/
+
 if(.Platform$OS.type == "windows") {
     read_symbols_from_dll <- function(f, rarch)
-#
-#  Copyright (C) 1995-2012 The R Core Team
     {
         ## reasonable to assume this on the path
         DLL_nm <- "objdump.exe"
