a6aca7d4a9f3a296e16265e96bfff72a1335cd8c
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-01-18 13:15:30 +0000

    strlen is size_t
    git-svn-id: https://svn.r-project.org/R/trunk@61677 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/eval.c
+++ src/main/eval.c
@@ -139,7 +139,7 @@ static SEXP GrowList(SEXP l, SEXP s)
 }
 
 static void lineprof(char* buf, SEXP srcref) {
-    int len;
+    size_t len;
     if (srcref && !isNull(srcref) && (len = strlen(buf)) < 1000) {
 	SEXP filename;
 	int fnum, line = asInteger(srcref);
@@ -161,7 +161,8 @@ static void doprof(int sig)  /* sig is ignored in Windows */
     RCNTXT *cptr;
     char buf[1100];
     unsigned long bigv, smallv, nodes;
-    int len, prevnum = R_Line_Profiling;
+    size_t len;
+    int prevnum = R_Line_Profiling;
     
     buf[0] = '\0';
     
