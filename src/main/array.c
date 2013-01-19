a4046b66f0f2a31b9eb04c83c5950a0a053b8fab
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-01-19 23:07:02 +0000

    ported r61696 [aperm(a, <char>) segfault] from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-2-15-branch@61697 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/array.c
+++ src/main/array.c
@@ -1073,27 +1073,30 @@ SEXP attribute_hidden do_aperm(SEXP call, SEXP op, SEXP args, SEXP rho)
     perm = CADR(args);
     if (length(perm) == 0) {
 	for (i = 0; i < n; i++) pp[i] = n-1-i;
-    } else if (isString(perm)) {
-	SEXP dna = getAttrib(a, R_DimNamesSymbol);
-	if (isNull(dna))
-	    error(_("'a' does not have named dimnames"));
-	SEXP dnna = getAttrib(dna, R_NamesSymbol);
-	if (isNull(dnna))
-	    error(_("'a' does not have named dimnames"));
-	for (i = 0; i < n; i++) {
-	    const char *this = translateChar(STRING_ELT(perm, i));
-	    for (j = 0; j < n; j++)
-		if (streql(translateChar(STRING_ELT(dnna, j)),
-			   this)) {pp[i] = j; break;}
-	    if (j >= n)
-		error(_("perm[%d] does not match a dimension name"), i+1);
-	}
     } else {
-	PROTECT(perm = coerceVector(perm, INTSXP));
-	if (length(perm) == n) {
+	if (LENGTH(perm) != n)
+	    error(_("'perm' is of wrong length %d (!= %d)"),
+		  LENGTH(perm), n);
+	if (isString(perm)) {
+	    SEXP dna = getAttrib(a, R_DimNamesSymbol);
+	    if (isNull(dna))
+		error(_("'a' does not have named dimnames"));
+	    SEXP dnna = getAttrib(dna, R_NamesSymbol);
+	    if (isNull(dnna))
+		error(_("'a' does not have named dimnames"));
+	    for (i = 0; i < n; i++) {
+		const char *this = translateChar(STRING_ELT(perm, i));
+		for (j = 0; j < n; j++)
+		    if (streql(translateChar(STRING_ELT(dnna, j)),
+			       this)) {pp[i] = j; break;}
+		if (j >= n)
+		    error(_("'perm[%d]' does not match a dimension name"), i+1);
+	    }
+	} else {
+	    PROTECT(perm = coerceVector(perm, INTSXP));
 	    for (i = 0; i < n; i++) pp[i] = INTEGER(perm)[i] - 1;
 	    UNPROTECT(1);
-	} else error(_("'perm' is of wrong length"));
+	}
     }
 
     int *iip = (int *) R_alloc((size_t) n, sizeof(int));
