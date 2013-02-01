fc08e6dc58d958cf5425f1e2db1c1ba87e4f739d
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-02-01 10:12:14 +0000

    update
    git-svn-id: https://svn.r-project.org/R/trunk@61806 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/license.R
+++ src/library/tools/R/license.R
@@ -1,7 +1,7 @@
 #  File src/library/tools/R/license.R
 #  Part of the R package, http://www.R-project.org
 #
-#  Copyright (C) 1995-2012 The R Core Team
+#  Copyright (C) 1995-2013 The R Core Team
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -230,9 +230,10 @@ list("Artistic-2.0" =
        "Artistic-2.0, see http://www.opensource.org/licenses/artistic-license-2.0.php"
        ),
 
-     "BSL-1.0" =
+     "BSL" =
      c("Boost Software License",
-       "Boost Software License 1.0"
+       "Boost Software License 1.0",
+       "BSL 1.0"
        ),
 
      "CeCILL-2" =
