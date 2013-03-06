16b16dbf1312e995af0fde7c0529c00291cb572c
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-03-06 18:18:41 +0000

    remove chol(LINPACK=TRUE, pivot=TRUE)
    git-svn-id: https://svn.r-project.org/R/trunk@62149 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/chol.R
+++ src/library/base/R/chol.R
@@ -23,47 +23,9 @@ chol.default <- function(x, pivot = FALSE, LINPACK = FALSE, tol = -1, ...)
     if (is.complex(x))
         stop("complex matrices not permitted at present")
 
-    if (!pivot && LINPACK) {
-        warning("LINPACK = TRUE, pivot = FALSE is defunct and will be ignored",
-                domain = NA)
-        LINPACK <- TRUE
-    }
+    if (LINPACK) warning("LINPACK = TRUE", domain = NA)
 
-    if(!LINPACK) return(.Internal(La_chol(as.matrix(x), pivot, tol)))
-
-    ## deprecated in R 3.0.0
-    warning("LINPACK = pivot = TRUE is deprecated", domain = NA)
-
-    if(is.matrix(x)) {
-	if(nrow(x) != ncol(x)) stop("non-square matrix in 'chol'")
-	n <- nrow(x)
-    } else {
-	if(length(x) != 1L) stop("non-matrix argument to 'chol'")
-	n <- 1L
-    }
-
-    ## sanity checks
-    n <- as.integer(n)
-    if(is.na(n)) stop("invalid nrow(x)")
-    if(n > 46340) stop("too large a matrix for LINPACK")
-
-    storage.mode(x) <- "double"
-
-    xx <- x
-    xx[lower.tri(xx)] <- 0
-    z <- .Fortran(.F_dchdc, x = xx, n, n, double(n), piv = integer(n),
-                  as.integer(pivot), rank = integer(1L), DUP = FALSE)
-    if (z$rank < n)
-        if(!pivot) stop("matrix not positive definite")
-        else warning("matrix not positive definite")
-    robj <- z$x
-    if (pivot) {
-        attr(robj, "pivot") <- z$piv
-        attr(robj, "rank") <- z$rank
-        if(!is.null(cn <- colnames(x)))
-            colnames(robj) <- cn[z$piv]
-    }
-    robj
+    .Internal(La_chol(as.matrix(x), pivot, tol))
 }
 
 chol2inv <- function(x, size = NCOL(x), LINPACK = FALSE)
--- src/main/registration.c
+++ src/main/registration.c
@@ -84,7 +84,6 @@ static R_CallMethodDef callMethods [] = {
 #define FDEF(name, n)  {#name, (DL_FUNC) &F77_SYMBOL(name), n, NULL}
 static R_FortranMethodDef fortranMethods[] = {
     /* LINPACK */
-    FDEF(dchdc, 7), // chol, deprecated
     FDEF(dqrcf, 8), // qr and auxiliaries
     FDEF(dqrdc2, 9),
     FDEF(dqrqty, 7),
