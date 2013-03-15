b431388da77fb6ff417d6f2713db1e16916a35ed
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-03-15 13:02:40 +0000

    port r62266 from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-3-0-branch@62267 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/grDevices/src/devWindows.c
+++ src/library/grDevices/src/devWindows.c
@@ -3706,6 +3706,13 @@ SEXP devga(SEXP args)
     R_CheckDeviceAvailable();
     BEGIN_SUSPEND_INTERRUPTS {
 	pDevDesc dev;
+	char type[100];
+	strcpy(type, "windows");
+	if (display[0]) {
+	    strncpy(type, display, 100);
+	    char *p = strchr(type, ':');
+	    if(p) *p = '\0';
+	}
 	/* Allocate and initialize the device driver data */
 	if (!(dev = (pDevDesc) calloc(1, sizeof(DevDesc)))) return 0;
 	GAsetunits(xpinch, ypinch);
@@ -3714,17 +3721,11 @@ SEXP devga(SEXP args)
 			    xpos, ypos, (Rboolean)buffered, psenv,
 			    restoreConsole, title, clickToConfirm,
 			    fillOddEven, family, quality)) {
-	    char type[100], *p;
 	    free(dev);
-	    if (display[0]) {
-		strncpy(type, display, 100);
-		p = strchr(type, ':');
-		if(p) *p = '\0';
-	    } else strcpy(type, "windows");
 	    error(_("unable to start %s() device"), type);
 	}
 	gdd = GEcreateDevDesc(dev);
-	GEaddDevice2(gdd, display[0] ? display : "windows");
+	GEaddDevice2(gdd, type);
     } END_SUSPEND_INTERRUPTS;
     vmaxset(vmax);
     return R_NilValue;
