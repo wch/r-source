5278724416c4fd9b6e61083d000ddf4e68ca2b59
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-03-18 11:50:46 +0000

    workaround for assumptions made by tkrplot about internals
    git-svn-id: https://svn.r-project.org/R/trunk@62300 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/grDevices/src/devWindows.c
+++ src/library/grDevices/src/devWindows.c
@@ -3710,8 +3710,11 @@ SEXP devga(SEXP args)
 	strcpy(type, "windows");
 	if (display[0]) {
 	    strncpy(type, display, 100);
-	    char *p = strchr(type, ':');
-	    if(p) *p = '\0';
+	    // Package tkrplot assumes the exact form here
+	    if(strncmp(display, "win.metafile", 12)) {
+		char *p = strchr(type, ':');
+		if(p) *p = '\0';
+	    }
 	}
 	/* Allocate and initialize the device driver data */
 	if (!(dev = (pDevDesc) calloc(1, sizeof(DevDesc)))) return 0;
