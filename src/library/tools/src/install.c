b0bae05ba4fe26abfb1ad0af9a0d356f75998245
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-04-12 19:48:26 +0000

    buffer underrun
    git-svn-id: https://svn.r-project.org/R/trunk@62555 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/src/install.c
+++ src/library/tools/src/install.c
@@ -139,7 +139,7 @@ SEXP codeFilesAppend(SEXP f1, SEXP f2)
 	    if (fwrite(buf, 1, APPENDBUFSIZE, fp1) != APPENDBUFSIZE)
 		goto append_error;
 	if (fwrite(buf, 1, nchar, fp1) != nchar) goto append_error;
-	if (buf[nchar - 1] != '\n')
+	if (!nchar || buf[nchar - 1] != '\n')
 	    if (fwrite("\n", 1, 1, fp1) != 1) goto append_error;
 	
 	status = 1;
