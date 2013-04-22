c4ec58bab98ec05e0501343c23b5ba1fb76a4062
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-04-22 12:04:14 +0000

    use explicit configure tests
    git-svn-id: https://svn.r-project.org/R/trunk@62636 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/include/Defn.h
+++ src/include/Defn.h
@@ -1249,10 +1249,10 @@ extern void *alloca(size_t);
 /* int_fast64_t is required by C99/C11
    Alternative would be to use intmax_t.
  */
-#ifdef int64_t
+#ifdef HAVE_INT64_T
 # define LONG_INT int64_t
 # define LONG_INT_MAX INT64_MAX
-#elif defined(int_fast64_t)
+#elif defined(HAVE_INT_FAST64_T)
 # define LONG_INT int_fast64_t
 # define LONG_INT_MAX INT_FAST64_MAX
 #else
