170d3ced98f60007d660c4d8e2c6362320f6380a
murdoch <murdoch@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-04-25 17:40:54 +0000

    PR#15288 fix:  summaryRprof only read the first chunk of profile data when line.profiling was active.
    git-svn-id: https://svn.r-project.org/R/trunk@62669 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/utils/R/summRprof.R
+++ src/library/utils/R/summRprof.R
@@ -1,7 +1,7 @@
 #  File src/library/utils/R/summRprof.R
 #  Part of the R package, http://www.R-project.org
 #
-#  Copyright (C) 1995-2012 The R Core Team
+#  Copyright (C) 1995-2013 The R Core Team
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -138,8 +138,6 @@ summaryRprof <-
            umem <- rowsum(c(new.umem, umem), c(names(new.utable), fnames))
 
        fnames <- sort(unique(c(fnames, names(new.utable))))
-
-       if (length(chunk) < chunksize) break
     })
 
     firstnum <- fcounts*sample.interval
