3bc5127b973a944a7225c7448fe846705d23f867
murdoch <murdoch@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-04-25 13:33:13 +0000

    Port r62663 from trunk.
    git-svn-id: https://svn.r-project.org/R/branches/R-3-0-branch@62664 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/R/splinefun.R
+++ src/library/stats/R/splinefun.R
@@ -48,7 +48,7 @@ splinefun <-
         m <- .Call(C_monoFC_m, m, Sx)
 
         ## Hermite spline with (x,y,m) :
-        return(splinefunH0(x = x, y = y, m = m, dx = dx))
+        return(splinefunH0(x0 = x, y0 = y, m = m, dx = dx))
     }
     ## else
     iMeth <- match(method, c("periodic", "natural", "fmm",
@@ -96,19 +96,19 @@ splinefun <-
 .splinefun <- function(x, z) .Call(C_SplineEval, x, z)
 
 ## hidden : The exported user function is splinefunH()
-splinefunH0 <- function(x, y, m, dx = x[-1L] - x[-length(x)])
+splinefunH0 <- function(x0, y0, m, dx = x0[-1L] - x0[-length(x0)])
 {
-    function(u, deriv=0, extrapol = c("linear","cubic"))
+    function(x, deriv=0, extrapol = c("linear","cubic"))
     {
 	extrapol <- match.arg(extrapol)
 	deriv <- as.integer(deriv)
-	if (deriv < 0 || deriv > 2)
-	    stop("'deriv' must be between 0 and 2")
-	i <- findInterval(u, x, all.inside = (extrapol == "cubic"))
+	if (deriv < 0 || deriv > 3)
+	    stop("'deriv' must be between 0 and 3")
+	i <- findInterval(x, x0, all.inside = (extrapol == "cubic"))
 	if(deriv == 0)
-	    interp <- function(u, i) {
+	    interp <- function(x, i) {
 		h <- dx[i]
-		t <- (u - x[i]) / h
+		t <- (x - x0[i]) / h
 		## Compute the 4 Hermite (cubic) polynomials h00, h01,h10, h11
 		t1 <- t-1
 		h01 <- t*t*(3 - 2*t)
@@ -116,47 +116,56 @@ splinefunH0 <- function(x, y, m, dx = x[-1L] - x[-length(x)])
 		tt1 <- t*t1
 		h10 <- tt1 * t1
 		h11 <- tt1 * t
-		y[i]  * h00 + h*m[i]  * h10 +
-		y[i+1]* h01 + h*m[i+1]* h11
+		y0[i]  * h00 + h*m[i]  * h10 +
+		y0[i+1]* h01 + h*m[i+1]* h11
 	    }
 	else if(deriv == 1)
-	    interp <- function(u, i) {
+	    interp <- function(x, i) {
 		h <- dx[i]
-		t <- (u - x[i]) / h
+		t <- (x - x0[i]) / h
 		## 1st derivative of Hermite polynomials h00, h01,h10, h11
 		t1 <- t-1
 		h01 <- -6*t*t1 # h00 = - h01
 		h10 <- (3*t - 1) * t1
 		h11 <- (3*t - 2) * t
-		(y[i+1] - y[i])/h * h01 + m[i] * h10 + m[i+1]* h11
+		(y0[i+1] - y0[i])/h * h01 + m[i] * h10 + m[i+1]* h11
 	    }
-	else ## deriv == 2
-	    interp <- function(u, i) {
+	else if (deriv == 2)
+	    interp <- function(x, i) {
 		h <- dx[i]
-		t <- (u - x[i]) / h
+		t <- (x - x0[i]) / h
 		## 2nd derivative of Hermite polynomials h00, h01,h10, h11
 		h01 <- 6*(1-2*t) # h00 = - h01
 		h10 <- 2*(3*t - 2)
 		h11 <- 2*(3*t - 1)
-		((y[i+1] - y[i])/h * h01 + m[i] * h10 + m[i+1]* h11) / h
+		((y0[i+1] - y0[i])/h * h01 + m[i] * h10 + m[i+1]* h11) / h
+	    }
+	else # deriv == 3
+	    interp <- function(x, i) {
+		h <- dx[i]
+		t <- (x - x0[i]) / h
+		## 3rd derivative of Hermite polynomials h00, h01,h10, h11
+		h01 <- -12 # h00 = - h01
+		h10 <- 6
+		h11 <- 6
+		((y0[i+1] - y0[i])/h * h01 + m[i] * h10 + m[i+1]* h11) / h
 	    }
 
-
 	if(extrapol == "linear" &&
-	   any(iXtra <- (iL <- (i == 0)) | (iR <- (i == (n <- length(x)))))) {
+	   any(iXtra <- (iL <- (i == 0)) | (iR <- (i == (n <- length(x0)))))) {
 	    ##	do linear extrapolation
-	    r <- u
-	    if(any(iL)) r[iL] <- if(deriv == 0) y[1L] + m[1L]*(u[iL] - x[1L]) else
+	    r <- x
+	    if(any(iL)) r[iL] <- if(deriv == 0) y0[1L] + m[1L]*(x[iL] - x0[1L]) else
 				  if(deriv == 1) m[1L] else 0
-	    if(any(iR)) r[iR] <- if(deriv == 0) y[n] + m[n]*(u[iR] - x[n]) else
+	    if(any(iR)) r[iR] <- if(deriv == 0) y0[n] + m[n]*(x[iR] - x0[n]) else
 				  if(deriv == 1) m[n] else 0
 	    ## For internal values, compute "as normal":
 	    ini <- !iXtra
-	    r[ini] <- interp(u[ini], i[ini])
+	    r[ini] <- interp(x[ini], i[ini])
 	    r
 	}
         else { ## use cubic Hermite polynomials, even for extrapolation
-            interp(u, i)
+            interp(x, i)
         }
 
     }
