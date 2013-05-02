3d54628e4902cd783d6f6ed3f009671503c4b483
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-05-02 15:45:17 +0000

    port r62702 from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-3-0-branch@62703 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/utils/R/objects.R
+++ src/library/utils/R/objects.R
@@ -1,7 +1,7 @@
 #  File src/library/utils/R/objects.R
 #  Part of the R package, http://www.R-project.org
 #
-#  Copyright (C) 1995-2012 The R Core Team
+#  Copyright (C) 1995-2013 The R Core Team
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -74,6 +74,7 @@ function(generic.function, class)
 {
     rbindSome <- function(df, nms, msg) {
         ## rbind.data.frame() -- dropping rows with duplicated names
+        nms <- unique(nms)
         n2 <- length(nms)
         dnew <- data.frame(visible = rep.int(FALSE, n2),
                            from    = rep.int(msg,   n2),
