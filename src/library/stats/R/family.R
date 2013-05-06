7fa002c1241dd087779fab0955a799f4c0f6c19e
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-05-06 12:48:17 +0000

    faster poisson() $ dev.resids(); use C version for binomial()$dev.resids  also in quasibinomial() and quasi()
    git-svn-id: https://svn.r-project.org/R/trunk@62720 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/R/family.R
+++ src/library/stats/R/family.R
@@ -153,7 +153,12 @@ poisson <- function (link = "log")
     variance <- function(mu) mu
     validmu <- function(mu) all(mu>0)
     dev.resids <- function(y, mu, wt)
-        2 * wt * (y * log(ifelse(y == 0, 1, y/mu)) - (y - mu))
+    { ## faster than  2 * wt * (y * log(ifelse(y == 0, 1, y/mu)) - (y - mu))
+	r <- mu*wt
+	p <- which(y > 0)
+	r[p] <- (wt * (y*log(y/mu) - (y - mu)))[p]
+	2*r
+    }
     aic <- function(y, n, mu, wt, dev) -2*sum(dpois(y, mu, log=TRUE)*wt)
     initialize <- expression({
 	if (any(y < 0))
@@ -209,7 +214,12 @@ quasipoisson <- function (link = "log")
     variance <- function(mu) mu
     validmu <- function(mu) all(mu>0)
     dev.resids <- function(y, mu, wt)
-	2 * wt * (y * log(ifelse(y == 0, 1, y/mu)) - (y - mu))
+    { ## faster than  2 * wt * (y * log(ifelse(y == 0, 1, y/mu)) - (y - mu))
+	r <- mu*wt
+	p <- which(y > 0)
+	r[p] <- (wt * (y*log(y/mu) - (y - mu)))[p]
+	2*r
+    }
     aic <- function(y, n, mu, wt, dev) NA
     initialize <- expression({
 	if (any(y < 0))
@@ -399,9 +409,7 @@ quasibinomial <- function (link = "logit")
     }
     variance <- function(mu) mu * (1 - mu)
     validmu <- function(mu) all(mu>0) && all(mu<1)
-    dev.resids <- function(y, mu, wt)
-	2 * wt * (y * log(ifelse(y == 0, 1, y/mu)) +
-		  (1 - y) * log(ifelse(y == 1, 1, (1 - y)/(1 - mu))))
+    dev.resids <- function(y, mu, wt) .Call(C_binomial_dev_resids, y, mu, wt)
     aic <- function(y, n, mu, wt, dev) NA
     initialize <- expression({
 	if (NCOL(y) == 1) {
@@ -574,9 +582,7 @@ quasi <- function (link = "identity", variance = "constant")
            "mu(1-mu)" = {
                varfun <- function(mu) mu * (1 - mu)
                validmu <- function(mu) all(mu>0) && all(mu<1)
-               dev.resids <- function(y, mu, wt)
-                   2 * wt * (y * log(ifelse(y == 0, 1, y/mu)) +
-                             (1 - y) * log(ifelse(y == 1, 1, (1 - y)/(1 - mu))))
+               dev.resids <- function(y, mu, wt) .Call(C_binomial_dev_resids, y, mu, wt)
                initialize <- expression({n <- rep.int(1, nobs)
                                          mustart <- pmax(0.001, pmin(0.999, y))})
            },
