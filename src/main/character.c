54e345d3f45402eb45264ef9cfe5df1876d6ea67
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-05-21 09:53:43 +0000

    tweak abbreviate
    git-svn-id: https://svn.r-project.org/R/trunk@62768 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/character.c
+++ src/main/character.c
@@ -559,9 +559,11 @@ SEXP attribute_hidden do_abbrev(SEXP call, SEXP op, SEXP args, SEXP env)
 	    SET_STRING_ELT(ans, i, NA_STRING);
 	else {
 	    s = translateChar(STRING_ELT(x, i));
-	    warn = warn | !strIsASCII(s);
-	    R_AllocStringBuffer(strlen(s), &cbuff);
-	    SET_STRING_ELT(ans, i, stripchars(s, minlen));
+	    if(strlen(s) > minlen) {
+		warn = warn | !strIsASCII(s);
+		R_AllocStringBuffer(strlen(s), &cbuff);
+		SET_STRING_ELT(ans, i, stripchars(s, minlen));
+	    } else SET_STRING_ELT(ans, i, mkChar(s));
 	}
 	vmaxset(vmax);
     }
