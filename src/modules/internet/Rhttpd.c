67fba877ff3fd7c7e9f4ddf988a6ba3756a131e6
urbaneks <urbaneks@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-05-22 01:45:05 +0000

    minor Rhttp update
    git-svn-id: https://svn.r-project.org/R/trunk@62770 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/modules/internet/Rhttpd.c
+++ src/modules/internet/Rhttpd.c
@@ -821,7 +821,7 @@ static void worker_input_handler(void *data) {
 		/* GET/HEAD or no content length mean no body */
 		if (c->method == METHOD_GET || c->method == METHOD_HEAD ||
 		    !(c->attr & CONTENT_LENGTH) || c->content_length == 0) {
-		    if (c->attr & CONTENT_LENGTH) {
+		    if ((c->attr & CONTENT_LENGTH) && c->content_length > 0) {
 			send_http_response(c, " 400 Bad Request (GET/HEAD with body)\r\n\r\n");
 			remove_worker(c);
 			return;
