9fbe6bc10326455dae0b08a1873b52d0e6363b9e
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-05-30 08:01:33 +0000

    new "safe" uniroot() =: unirootS()  [may change; see R-devel e-mail]
    git-svn-id: https://svn.r-project.org/R/trunk@62834 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/R/nlm.R
+++ src/library/stats/R/nlm.R
@@ -74,4 +74,64 @@ uniroot <- function(f, interval, ...,
     }
     list(root = val[1L], f.root = f(val[1L], ...),
          iter = iter, estim.prec = val[3L])
+}## uniroot()
+
+unirootS <- function(f, interval, ...,
+		     lower = min(interval), upper = max(interval),
+		     f.lower = f(lower, ...), f.upper = f(upper, ...),
+		     Sig = NULL, check.conv = FALSE,
+		     tol = .Machine$double.eps^0.25, maxiter = 1000, trace = 0)
+{
+    if (   is.null(Sig) && f.lower * f.upper > 0 ||
+	is.numeric(Sig) && (Sig*f.lower > 0 || Sig*f.upper < 0)) {
+	if(trace)
+	    cat(sprintf("search in [%g,%g]%s", lower, upper,
+			if(trace >= 2)"\n" else " ... "))
+	Delta <- function(u) 0.01* pmax(1e-7, abs(u))
+	## Two cases:
+	if(is.null(Sig)) {
+	    ## case 1)	'Sig' unspecified --> extend (lower, upper) at the same time
+	    delta <- Delta(c(lower,upper))
+	    while(isTRUE(f.lower*f.upper > 0) && any(iF <- is.finite(c(lower,upper)))) {
+		if(iF[1]) f.lower <- f(lower <- lower - delta[1])
+		if(iF[2]) f.upper <- f(upper <- upper + delta[2])
+		if(trace >= 2)
+		    cat(sprintf(" .. modified lower,upper: (%15g,%15g)\n",
+				lower,upper))
+		delta <- 2 * delta
+	    }
+	} else {
+	    ## case 2) 'Sig' specified --> typically change only *one* of lower, upper
+	    ## make sure we have Sig*f(lower) < 0 and Sig*f(upper) > 0:
+	    delta <- Delta(lower)
+	    while(isTRUE(Sig*f.lower > 0)) {
+		f.lower <- f(lower <- lower - delta)
+		if(trace) cat(sprintf(" .. modified lower: %g\n", lower))
+		delta <- 2 * delta
+	    }
+	    delta <- Delta(upper)
+	    while(isTRUE(Sig*f.upper < 0)) {
+		f.upper <- f(upper <- upper + delta)
+		if(trace) cat(sprintf(" .. modified upper: %g\n", upper))
+		delta <- 2 * delta
+	    }
+	}
+	if(trace && trace < 2)
+	    cat(sprintf("extended to [%g, %g]\n", lower, upper))
+    }
+    if(!isTRUE(f.lower * f.upper <= 0))
+	stop("did not succeed extending the interval endpoints for f(lower) * f(upper) <= 0")
+    if(check.conv) {
+	r <- tryCatch(uniroot(f, ..., lower=lower, upper=upper,
+			      f.lower=f.lower, f.upper=f.upper,
+			      tol=tol, maxiter=maxiter),
+		      warning = function(w)w)
+	if(inherits(r, "warning"))
+	    stop("convergence problem in zero finding: ", r$message)
+	else r
+    }
+    else
+	uniroot(f, ..., lower=lower, upper=upper,
+		f.lower=f.lower, f.upper=f.upper,
+		tol=tol, maxiter=maxiter)
 }
