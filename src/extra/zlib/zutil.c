e0369b6dda50b0b176cd46f21cf141731f8c7cc7
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-06-05 08:27:13 +0000

    port r62873 from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-3-0-branch@62874 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/extra/zlib/zutil.c
+++ src/extra/zlib/zutil.c
@@ -7,6 +7,7 @@
 
 #include "zutil.h"
 #ifndef Z_SOLO
+/* R change */
 //#  include "gzguts.h"
 #endif
 
@@ -14,7 +15,7 @@
 struct internal_state      {int dummy;}; /* for buggy compilers */
 #endif
 
-const char * const z_errmsg[10] = {
+z_const char * const z_errmsg[10] = {
 "need dictionary",     /* Z_NEED_DICT       2  */
 "stream end",          /* Z_STREAM_END      1  */
 "",                    /* Z_OK              0  */
