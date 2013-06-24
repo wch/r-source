89ca6601cb13ab34691deb3b33049321d42ebe50
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-06-24 12:03:07 +0000

    make R CMD build --no-vignettes defunct
    git-svn-id: https://svn.r-project.org/R/trunk@63033 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/build.R
+++ src/library/tools/R/build.R
@@ -819,7 +819,8 @@ get_exclude_patterns <- function()
         } else if (a == "--no-build-vignettes") {
             vignettes <- FALSE
         } else if (a == "--no-vignettes") { # pre-3.0.0 version
-            vignettes <- FALSE
+            stop("'--no-vignettes' is defunct:\n  use '--no-build-vignettes' instead",
+                 call. = FALSE, domain = NA)
         } else if (a == "--resave-data") {
             resave_data <- "best"
         } else if (a == "--no-resave-data") {
