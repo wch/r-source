c71110ea57d626e2ff3f316a2234a73c3d756ba5
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-06-24 10:55:05 +0000

    R CMD check --no-rebuild-vignettes is defunct
    git-svn-id: https://svn.r-project.org/R/trunk@63029 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/check.R
+++ src/library/tools/R/check.R
@@ -3512,8 +3512,9 @@ setRlibs <-
         } else if (a == "--no-build-vignettes") {
             do_build_vignettes  <- FALSE
         } else if (a == "--no-rebuild-vignettes") { # pre-3.0.0 version
-            do_build_vignettes  <- FALSE
-        } else if (a == "--no-vignettes") {
+            stop("'--no-rebuild-vignettes' is defunct: use '--no-build-vignettes' instead",
+                 call. = FALSE, domain = NA)
+      } else if (a == "--no-vignettes") {
             do_vignettes  <- FALSE
         } else if (a == "--no-manual") {
             do_manual  <- FALSE
