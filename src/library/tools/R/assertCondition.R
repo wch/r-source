2affed6d343247e33c4cf6183643799938e3fa75
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-07-02 12:53:13 +0000

    ported r63142-4 [assertCondition(..ignoreWarnings)] from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-3-0-branch@63145 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/assertCondition.R
+++ src/library/tools/R/assertCondition.R
@@ -1,18 +1,42 @@
-assertCondition <- function(expr, ..., verbose=getOption("verbose")) {
+#  File src/library/tools/R/assertCondition.R
+#  Part of the R package, http://www.R-project.org
+#
+#  Copyright (C) 2013 The R Core Team
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  A copy of the GNU General Public License is available at
+#  http://www.r-project.org/Licenses/
+
+assertCondition <- function(expr, ..., verbose=getOption("verbose"),
+			    ignoreWarning = is.na(match("warning", conds))) {
     fe <- function(e)e
     d.expr <- deparse(substitute(expr), width.cutoff = 30L)
     if(length(d.expr) > 1)
 	d.expr <- paste(d.expr[[1]], "...")
     conds <- if(nargs() > 1) c(...) else "condition"
     .Wanted <- paste(conds, collapse = " | ")
-    res <- tryCatch(expr, error = fe, warning = fe, finally = fe)
+    if(ignoreWarning)
+	res <- tryCatch(expr, error = fe, finally = fe)
+    else
+	res <- tryCatch(expr, error = fe, warning = fe, finally = fe)
     if(inherits(res, "condition")) {
-	if(any(ii <- class(res) %in% conds)) {
+	ii <- class(res) %in% conds
+	if(any(ii)) {
 	    if(verbose) cat(sprintf("Asserted %s: %s\n",
 				    class(res)[which.max(ii)],
 				    conditionMessage(res)))
 	    invisible(res)
-	} else
+	}
+	else
 	    stop(gettextf("Expected %s, got %s in evaluating %s",
 			 .Wanted, class(res)[[1]], d.expr))
     }
