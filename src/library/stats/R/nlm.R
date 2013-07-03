520c981ea5ba4e186e26934496880a7f99b0c947
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-07-03 08:47:22 +0000

    merged unirootS() into uniroot(), remaining back compatible; more to come (not the next couple of days, tough)
    git-svn-id: https://svn.r-project.org/R/trunk@63162 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/R/nlm.R
+++ src/library/stats/R/nlm.R
@@ -48,10 +48,16 @@ optimize <- function(f, interval, ...,
 ##nice to the English (or rather the Scots)
 optimise <- optimize
 
+## FIXME? have the  4 cases
+##    Sig \in {NULL,    -1 ,     0,     1  } -- with default 0 --->
+## extendInt  ( yes,  downX,    no,    upX ) -- with default "no"
+## crossing   (extend, down,   free,   up  ) -- with default "free"
 uniroot <- function(f, interval, ...,
-                    lower = min(interval), upper = max(interval),
-                    f.lower = f(lower, ...), f.upper = f(upper, ...),
-		    tol = .Machine$double.eps^0.25, maxiter = 1000)
+		    lower = min(interval), upper = max(interval),
+		    f.lower = f(lower, ...), f.upper = f(upper, ...),
+		    extendInt = c("no", "yes", "downX", "upX"),
+		    check.conv = FALSE,
+		    tol = .Machine$double.eps^0.25, maxiter = 1000, trace = 0)
 {
     if(!missing(interval) && length(interval) != 2L)
         stop("'interval' must be a vector of length 2")
@@ -59,32 +65,20 @@ uniroot <- function(f, interval, ...,
         stop("lower < upper  is not fulfilled")
     if(is.na(f.lower)) stop("f.lower = f(lower) is NA")
     if(is.na(f.upper)) stop("f.upper = f(upper) is NA")
-    if(f.lower * f.upper > 0)
-	stop("f() values at end points not of opposite sign")
-    val <- .External2(C_zeroin2, function(arg) f(arg, ...),
-                      lower, upper, f.lower, f.upper,
-                      tol, as.integer(maxiter))
-    iter <- as.integer(val[2L])
-    if(iter < 0) {
-        warning(sprintf(ngettext(maxiter,
-                                 "_NOT_ converged in %d iteration",
-                                 "_NOT_ converged in %d iterations"),
-                        maxiter), domain = NA)
-        iter <- maxiter
-    }
-    list(root = val[1L], f.root = f(val[1L], ...),
-         iter = iter, estim.prec = val[3L])
-}## uniroot()
-
-unirootS <- function(f, interval, ...,
-		     lower = min(interval), upper = max(interval),
-		     f.lower = f(lower, ...), f.upper = f(upper, ...),
-		     Sig = NULL, check.conv = FALSE,
-		     tol = .Machine$double.eps^0.25, maxiter = 1000, trace = 0)
-{
-    doX <- (   is.null(Sig) && f.lower * f.upper > 0 ||
-            is.numeric(Sig) && (Sig*f.lower > 0 || Sig*f.upper < 0))
-    if(doX) {
+    Sig <- switch(match.arg(extendInt),
+		  "yes" = NULL,
+		  "downX"= -1,
+		  "no"   =  0,
+		  "upX"  =  1,
+		  stop("invalid 'extendInt'; please report"))
+    ## protect against later   0 * Inf  |--> NaN :
+    min.at.least <- function(x) pmax.int(x, -.Machine$double.xmax)
+    max.at.most  <- function(x) pmin.int(x,  .Machine$double.xmax)
+    f.low. <- min.at.least(f.lower)
+    f.upp. <- max.at.most (f.upper)
+    doX <- (   is.null(Sig) && f.low. * f.upp. > 0 ||
+	    is.numeric(Sig) && (Sig*f.low. > 0 || Sig*f.upp. < 0))
+    if(doX) { ## extend the interval = [lower, upper]
 	if(trace)
 	    cat(sprintf("search in [%g,%g]%s", lower, upper,
 			if(trace >= 2)"\n" else " ... "))
@@ -94,12 +88,23 @@ unirootS <- function(f, interval, ...,
 	if(is.null(Sig)) {
 	    ## case 1)	'Sig' unspecified --> extend (lower, upper) at the same time
 	    delta <- Delta(c(lower,upper))
-	    while(isTRUE(f.lower*f.upper > 0) && any(iF <- is.finite(c(lower,upper)))) {
+	    while(isTRUE(f.lower*f.upper > 0) &&
+                  any(iF <- is.finite(c(lower,upper)))) {
 		if((it <- it + 1L) > maxiter)
 		    stop(gettextf("no sign change found in %d iterations", it-1),
 			 domain=NA)
-		if(iF[1]) f.lower <- f(lower <- lower - delta[1])
-		if(iF[2]) f.upper <- f(upper <- upper + delta[2])
+		if(iF[1]) {
+		    ol <- lower; of <- f.lower
+		    if(is.na(f.lower <- f(lower <- lower - delta[1], ...))) {
+			lower <- ol; f.lower <- of; delta[1] <- delta[1]/4
+		    }
+		}
+		if(iF[2]) {
+		    ol <- upper; of <- f.upper
+		    if(is.na(f.upper <- f(upper <- upper + delta[2], ...))) {
+			upper <- ol; f.upper <- of; delta[2] <- delta[2]/4
+		    }
+		}
 		if(trace >= 2)
 		    cat(sprintf(" .. modified lower,upper: (%15g,%15g)\n",
 				lower,upper))
@@ -113,7 +118,7 @@ unirootS <- function(f, interval, ...,
 		if((it <- it + 1L) > maxiter)
 		    stop(gettextf("no sign change found in %d iterations", it-1),
 			 domain=NA)
-		f.lower <- f(lower <- lower - delta)
+		f.lower <- f(lower <- lower - delta, ...)
 		if(trace >= 2) cat(sprintf(" .. modified lower: %g\n", lower))
 		delta <- 2 * delta
 	    }
@@ -122,7 +127,7 @@ unirootS <- function(f, interval, ...,
 		if((it <- it + 1L) > maxiter)
 		    stop(gettextf("no sign change found in %d iterations", it-1),
 			 domain=NA)
-		f.upper <- f(upper <- upper + delta)
+		f.upper <- f(upper <- upper + delta, ...)
 		if(trace >= 2) cat(sprintf(" .. modified upper: %g\n", upper))
 		delta <- 2 * delta
 	    }
@@ -130,21 +135,34 @@ unirootS <- function(f, interval, ...,
 	if(trace && trace < 2)
             cat(sprintf("extended to [%g, %g] in %d steps\n", lower, upper, it))
     }
-    if(!isTRUE(f.lower * f.upper <= 0))
+    if(!isTRUE(min.at.least(f.lower) * max.at.most(f.upper) <= 0))
 	stop(if(doX)
 	"did not succeed extending the interval endpoints for f(lower) * f(upper) <= 0"
 	     else "f() values at end points not of opposite sign")
+
     if(check.conv) {
-	r <- tryCatch(uniroot(f, ..., lower=lower, upper=upper,
-			      f.lower=f.lower, f.upper=f.upper,
-			      tol=tol, maxiter=maxiter),
-		      warning = function(w)w)
-	if(inherits(r, "warning"))
-	    stop("convergence problem in zero finding: ", r$message)
-	else r
+	val <- tryCatch(.External2(C_zeroin2, function(arg) f(arg, ...),
+				   lower, upper, f.lower, f.upper,
+				   tol, as.integer(maxiter)),
+			warning = function(w)w)
+	if(inherits(val, "warning"))
+	    stop("convergence problem in zero finding: ", conditionMessage(val))
+    } else {
+	val <- .External2(C_zeroin2, function(arg) f(arg, ...),
+			  lower, upper, f.lower, f.upper,
+			  tol, as.integer(maxiter))
     }
-    else
-	uniroot(f, ..., lower=lower, upper=upper,
-		f.lower=f.lower, f.upper=f.upper,
-		tol=tol, maxiter=maxiter)
-}
+    iter <- as.integer(val[2L])
+    if(iter < 0) {
+	(if(check.conv) stop else warning)(
+	    sprintf(ngettext(maxiter,
+			     "_NOT_ converged in %d iteration",
+			     "_NOT_ converged in %d iterations"),
+		    maxiter), domain = NA)
+	iter <- maxiter
+    }
+    if(doX) iter <- iter + it else it <- NA_integer_
+    list(root = val[1L], f.root = f(val[1L], ...),
+	 iter = iter, init.it = it, estim.prec = val[3L])
+}## uniroot()
+
