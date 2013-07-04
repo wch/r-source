723c385b7a6d28a67ad0667e248ca3f5778463ff
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-07-04 11:14:09 +0000

    assertCondition() tweaks (from jmc); add \link{}s to it
    git-svn-id: https://svn.r-project.org/R/trunk@63173 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/assertCondition.R
+++ src/library/tools/R/assertCondition.R
@@ -10,37 +10,66 @@
 #
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
 #  GNU General Public License for more details.
 #
 #  A copy of the GNU General Public License is available at
 #  http://www.r-project.org/Licenses/
 
-assertCondition <- function(expr, ..., verbose=getOption("verbose"),
-			    ignoreWarning = is.na(match("warning", conds))) {
+assertCondition <- function(expr, ..., verbose = getOption("verbose")) {
     fe <- function(e)e
+    getCondClasses <- function(expr) {
+	conds <- character()
+	lastCond <- NULL
+	tryCatch(withCallingHandlers(expr,
+				     warning = function(w) {
+					 conds <<- c(conds, class(w))
+					 lastCond <<- w
+					 invokeRestart("muffleWarning")
+				     },
+				     condition = function(cond) {
+					 conds <<- c(conds, class(cond))
+					 lastCond <<- cond}),
+		 error = function(e) {
+		     conds <<- c(conds, class(e))
+		     lastCond <<- e})
+	list(unique(conds), lastCond)
+    }
     d.expr <- deparse(substitute(expr), width.cutoff = 30L)
     if(length(d.expr) > 1)
 	d.expr <- paste(d.expr[[1]], "...")
-    conds <- if(nargs() > 1) c(...) else "condition"
-    .Wanted <- paste(conds, collapse = " | ")
-    if(ignoreWarning)
-	res <- tryCatch(expr, error = fe, finally = fe)
-    else
-	res <- tryCatch(expr, error = fe, warning = fe, finally = fe)
-    if(inherits(res, "condition")) {
-	ii <- class(res) %in% conds
-	if(any(ii)) {
-	    if(verbose) cat(sprintf("Asserted %s: %s\n",
-				    class(res)[which.max(ii)],
-				    conditionMessage(res)))
+    conds <- if(nargs() > 1) c(...) # else NULL
+    .Wanted <- (if(length(conds))
+		paste("expected", paste(conds, collapse = " | "))
+		else "any condition")
+    res <- getCondClasses(expr)
+    lastCond <- res[[2]]
+    res <- res[[1]]
+    if(length(res)) {
+	if(is.null(conds)) {
+	    if(verbose) cat(sprintf("Got condition \"%s\": %s \n",
+				    class(lastCond)[[1]],
+				    conditionMessage(lastCond)))
 	    invisible(res)
 	}
-	else
-	    stop(gettextf("Expected %s, got %s in evaluating %s",
-			 .Wanted, class(res)[[1]], d.expr))
+	else {
+	    ii <- res %in% conds
+	    if(any(ii)) {
+		if(verbose) {
+		    got <- res[which.max(ii)]
+		    text <- if(got %in% class(lastCond))
+			paste(":", conditionMessage(lastCond)) else ""
+		    cat(sprintf("Asserted %s %s\n", got, text))
+		}
+		invisible(res)
+	    }
+	    else
+		stop(gettextf("Got %s (%s) in evaluating %s; %s",
+			      class(lastCond)[[1]], conditionMessage(lastCond),
+			      d.expr, .Wanted))
+	}
     }
     else
-	stop(gettextf("Failed to get expected %s in evaluating %s",
-		     .Wanted, d.expr))
+	stop(gettextf("Failed to get %s in evaluating %s",
+		      .Wanted, d.expr))
 }
