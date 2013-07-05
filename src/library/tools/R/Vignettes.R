3a22d238d48cd34b6e6dd779a8a2fb12469fe26f
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-07-05 19:46:18 +0000

    Vignette engines can now register a text filter for spell checking.
    git-svn-id: https://svn.r-project.org/R/trunk@63195 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/Vignettes.R
+++ src/library/tools/R/Vignettes.R
@@ -1014,7 +1014,8 @@ vignetteEngine <- local({
         result
     }
 
-    setEngine <- function(name, package, pattern, weave, tangle) {
+    setEngine <- function(name, package, pattern, weave, tangle,
+                          text = NULL) {
         key <- engineKey(name, package)
         if (!is.null(package) && key[1L] != package)
             stop("Engine name ", sQuote(name), " and package ", sQuote(package), " do not match")
@@ -1040,7 +1041,9 @@ vignetteEngine <- local({
             else if (!is.character(pattern))
                 stop("Argument ", sQuote("pattern"), " must be a character vector or NULL and not ", sQuote(class(pattern)[1L]))
 
-            result <- list(name = key[2L], package = key[1L], pattern = pattern, weave = weave, tangle = tangle)
+            result <-
+                list(name = key[2L], package = key[1L], pattern = pattern,
+                     weave = weave, tangle = tangle, text = text)
             assign(rname, result, registry)
         }
 
@@ -1049,10 +1052,12 @@ vignetteEngine <- local({
 
     setEngine(name = "Sweave", package = "utils", pattern = NULL,
               weave = function(...) utils::Sweave(...),
-              tangle = function(...) utils::Stangle(...))
+              tangle = function(...) utils::Stangle(...),
+              text = "Sweave")
 
 
-    function(name, weave, tangle, pattern = NULL, package = NULL) {
+    function(name, weave, tangle, pattern = NULL, package = NULL,
+             text = NULL) {
         if (missing(weave)) { # we're getting the engine
             getEngine(name, package)
         } else { # we're setting a new engine
@@ -1061,7 +1066,9 @@ vignetteEngine <- local({
             }
             if (missing(package))
                 package <- utils::packageName(parent.frame())
-            result <- setEngine(name, package, pattern = pattern, weave = weave, tangle = tangle)
+            result <-
+                setEngine(name, package, pattern = pattern,
+                          weave = weave, tangle = tangle, text = text)
             invisible(result)
         }
     }
