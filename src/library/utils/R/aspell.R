e9cc27334d6e391e0c0de4428cfc38118e97d1e2
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-07-06 11:39:57 +0000

    Enhance aspell_package_vignettes() to support non-Sweave vignettes.
    git-svn-id: https://svn.r-project.org/R/trunk@63199 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/utils/R/aspell.R
+++ src/library/utils/R/aspell.R
@@ -147,7 +147,7 @@ function(files, filter, control = list(), encoding = "unknown",
                     domain = NA)
 
         lines <- if(is.null(filter))
-            readLines(file, encoding = enc)
+            readLines(file, encoding = enc, warn = FALSE)
         else {
             ## Assume that filter takes an input file (and additional
             ## arguments) and return a character vector.
@@ -586,24 +586,10 @@ function(dir,
 {
     dir <- tools::file_path_as_absolute(dir)
 
-    ## <FIXME>
-    ## This should be enhance to also work for non-Sweave package
-    ## vignettes, which requires that the respective engines provide
-    ## text filters.
-    ## </FIXME>
-
-    subdir <- file.path(dir, "vignettes")
-    if(!file_test("-d", subdir)) {
-        subdir <- file.path(dir, "inst", "doc")
-        if(!file_test("-d", subdir))
-            subdir <- character()
-    }
-    files <- if(length(subdir))
-        list.files(subdir,
-                   pattern = tools::vignetteEngine("Sweave")$pattern,
-                   full.names = TRUE)
-    else character()
-
+    vinfo <- tools::pkgVignettes(dir = dir)
+    files <- vinfo$docs
+    if(!length(files)) return(aspell(character()))
+    
     meta <- tools:::.get_package_metadata(dir, installed = FALSE)
     if(is.na(encoding <- meta["Encoding"]))
         encoding <- "unknown"
@@ -630,14 +616,24 @@ function(dir,
 
     program <- aspell_find_program(program)
 
-    aspell(files,
-           filter = "Sweave",
-           control =
-           c("-t",
-             aspell_control_package_vignettes[[names(program)]],
-             control),
-           program = program,
-           dictionaries = dictionaries)
+    files <- split(files, vinfo$engine)
+
+    do.call(rbind,
+            Map(function(files, engine) {
+                engine <- tools::vignetteEngine(engine)
+                aspell(files,
+                       filter = engine$aspell$filter,
+                       control =
+                       c(engine$aspell$control,
+                         aspell_control_package_vignettes[[names(program)]],
+                         control),
+                       program = program,
+                       dictionaries = dictionaries)
+            },
+                files,
+                names(files)
+                )
+            )
 }
 
 ## Spell-checking R files.
