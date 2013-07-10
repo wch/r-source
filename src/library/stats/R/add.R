8e2e8228ead6548827decba22dfe9ff2795afca6
pd <pd@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-07-10 11:48:40 +0000

    add1.glm double df divide
    git-svn-id: https://svn.r-project.org/R/branches/R-3-0-branch@63259 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/R/add.R
+++ src/library/stats/R/add.R
@@ -225,7 +225,7 @@ add1.glm <- function(object, scope, scale = 0, test=c("none", "Rao", "LRT",
 	dev <- table$Deviance
 	df <- table$Df
 	diff <- pmax(0, (dev[1L] - dev)/df)
-	Fs <- (diff/df)/(dev/(rdf-df))
+	Fs <- diff/(dev/(rdf-df))
 	Fs[df < .Machine$double.eps] <- NA
 	P <- Fs
 	nnas <- !is.na(Fs)
