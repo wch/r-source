c04460f9b52683d1333abdf90ef2525f1de01dd6
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-07-12 07:41:43 +0000

    port r63170, 63256, 63266 from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-3-0-branch@63267 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/graphics/R/stem.R
+++ src/library/graphics/R/stem.R
@@ -19,10 +19,10 @@
 stem <- function(x, scale = 1, width = 80, atom = 0.00000001)
 {
     if (!is.numeric(x) ) stop("'x' must be numeric")
-    x <- x[!is.na(x)]
+    x <- x[is.finite(x)]
     n <- as.integer(length(x))
     if (is.na(n)) stop("invalid length(x)")
-    if (n == 0) stop("no non-missing values")
+    if (n == 0) stop("no finite and non-missing values")
     if (scale <= 0) stop("'scale' must be positive") # unlike S
     .Call(C_StemLeaf, as.double(x), scale, width, atom)
     invisible(NULL)
