d7e5bdf8fc052b96f70fbcf78af96b966240a272
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-07-22 11:09:21 +0000

    back out r63377 -- causes problems in caschrono
    git-svn-id: https://svn.r-project.org/R/branches/R-3-0-branch@63380 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/R/arima.R
+++ src/library/stats/R/arima.R
@@ -192,17 +192,7 @@ arima <- function(x, order = c(0, 0, 0),
             S <- svd(na.omit(xreg))
             xreg <- xreg %*% S$v
         }
-        dx <- x
-        dxreg <- xreg
-        if(order[2L] > 0L) {
-            dx <- diff(dx, 1L, order[2L])
-            dxreg <- diff(dxreg, 1L, order[2L])
-        }
-        if(seasonal$period > 1L & seasonal$order[2L] > 0) {
-            dx <- diff(dx, seasonal$period, seasonal$order[2L])
-            dxreg <- diff(dxreg, seasonal$period, seasonal$order[2L])
-        }
-        fit <- lm(dx ~ dxreg - 1, na.action = na.omit)
+        fit <- lm(x ~ xreg - 1, na.action = na.omit)
         n.used <- sum(!is.na(resid(fit))) - length(Delta)
         init0 <- c(init0, coef(fit))
         ses <- summary(fit)$coefficients[, 2L]
