7a110cee74ef093725b231cff0b90d4d2264c6b6
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-07-29 08:00:41 +0000

    port r63445 from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-3-0-branch@63446 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/methods/src/methods_list_dispatch.c
+++ src/library/methods/src/methods_list_dispatch.c
@@ -1,6 +1,6 @@
 /*
  *  R : A Computer Language for Statistical Data Analysis
- *  Copyright (C) 2001-2009   The R Core Team.
+ *  Copyright (C) 2001-2013   The R Core Team.
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -30,7 +30,8 @@
 
 #include "RSMethods.h"
 #include "methods.h"
-#include "Rdefines.h"
+#include <Rinternals.h>
+#define STRING_VALUE(x)		CHAR(asChar(x))
 
 #if !defined(snprintf) && defined(HAVE_DECL_SNPRINTF) && !HAVE_DECL_SNPRINTF
 extern int snprintf (char *s, size_t n, const char *format, ...);
@@ -620,7 +621,7 @@ static SEXP do_dispatch(SEXP fname, SEXP ev, SEXP mlist, int firstTry,
     if(isNull(method)) {
       if(!firstTry)
 	error(_("no matching method for function '%s' (argument '%s', with class \"%s\")"),
-	      CHAR(asChar(fname)), CHAR(PRINTNAME(arg_sym)), class);
+	      EncodeChar(asChar(fname)), EncodeChar(PRINTNAME(arg_sym)), class);
       UNPROTECT(nprotect);
       return(R_NilValue);
     }
@@ -975,7 +976,7 @@ SEXP R_dispatchGeneric(SEXP fname, SEXP ev, SEXP fdef)
        mtable == R_UnboundValue)
 	error("generic \"%s\" seems not to have been initialized for table dispatch---need to have '.SigArgs' and '.AllMtable' assigned in its environment");
     nargs = asInteger(siglength);
-    PROTECT(classes = NEW_LIST(nargs)); nprotect++;
+    PROTECT(classes = allocVector(VECSXP, nargs)); nprotect++;
     if (nargs > LENGTH(sigargs))
 	error("'.SigArgs' is shorter than '.SigLength' says it should be");
     for(i = 0; i < nargs; i++) {
@@ -1051,20 +1052,16 @@ SEXP R_dispatchGeneric(SEXP fname, SEXP ev, SEXP fdef)
 
 SEXP R_set_method_dispatch(SEXP onOff)
 {
-    Rboolean value, prev; SEXP x;
-    prev = table_dispatch_on;
-    value = LOGICAL_VALUE(onOff);
+    Rboolean prev = table_dispatch_on, value = asLogical(onOff);
     if(value == NA_LOGICAL) /*  just return previous*/
 	value = prev;
     table_dispatch_on = value;
     if(value != prev) {
 	R_set_standardGeneric_ptr(
-	    (table_dispatch_on ? R_dispatchGeneric : R_standardGeneric)
-	    , Methods_Namespace);
+	    (table_dispatch_on ? R_dispatchGeneric : R_standardGeneric),
+	    Methods_Namespace);
 	R_set_quick_method_check(
 	    (table_dispatch_on ? R_quick_dispatch : R_quick_method_check));
     }
-    x = NEW_LOGICAL(1);
-    LOGICAL_DATA(x)[0] = prev;
-    return x;
+    return ScalarLogical(prev);
 }
