40bc55dfbb3c76160813a1223c22e63d53253e13
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-08-04 10:45:37 +0000

    ensure package has the fields use in the incoming test
    git-svn-id: https://svn.r-project.org/R/trunk@63491 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/QC.R
+++ src/library/tools/R/QC.R
@@ -5864,6 +5864,12 @@ function(dir)
     class(out) <- "check_package_CRAN_incoming"
 
     meta <- .get_package_metadata(dir, FALSE)
+    if(is.na(meta["Package"]))
+            stop("Package has no 'Package' field", call. = FALSE)
+    if(is.na(meta["License"]))
+        stop("Package has no 'License' field", call. = FALSE)
+    if(is.na(meta["Maintainer"]))
+        stop("Package has no 'Maintainer' field", call. = FALSE)
     info <- analyze_license(meta["License"])
     ## Use later to indicate changes from FOSS to non-FOSS licence.
     foss <- info$is_verified
@@ -5876,6 +5882,8 @@ function(dir)
     out$Maintainer <- meta["Maintainer"]
 
     ver <- meta["Version"]
+    if(is.na(ver))
+        stop("Package has no 'Version' field", call. = FALSE)
     if(grepl("(^|[.-])0[0-9]+", ver))
         out$version_with_leading_zeroes <- ver
 
