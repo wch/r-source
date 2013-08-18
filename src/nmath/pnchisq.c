00b8b5a1d4a6caec0c7a51cc0420f265ae0a83c8
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-08-18 05:30:06 +0000

    did not compile!
    git-svn-id: https://svn.r-project.org/R/trunk@63610 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/nmath/pnchisq.c
+++ src/nmath/pnchisq.c
@@ -136,7 +136,7 @@ pnchisq_raw(double x, double f, double theta,
        sqrt(DBL_EPSILON) * f2) {
 	/* evade cancellation error */
 	/* t = exp((1 - t)*(2 - t/(f2 + 1))) / sqrt(2*M_PI*(f2 + 1));*/
-        lt = (1 - t)*(2 - t/(f2 + 1)) - M_LN_SQRT_2PI - 0.5 * log(f2 + 1));
+        lt = (1 - t)*(2 - t/(f2 + 1)) - M_LN_SQRT_2PI - 0.5 * log(f2 + 1);
 #ifdef DEBUG_pnch
 	REprintf(" (case I) ==> ");
 #endif
