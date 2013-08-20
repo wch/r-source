aa670bf0da01d0026ff1589cc8655cd5bbab0b73
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-08-20 22:02:26 +0000

    Adjusted a comment and temprarily disabled fast REALSXP case in makeSubscript.
    git-svn-id: https://svn.r-project.org/R/trunk@63635 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/subscript.c
+++ src/main/subscript.c
@@ -27,7 +27,6 @@
 
  *  makeSubscript()   -- for "[" and "[<-" in ./subset.c and ./subassign.c,
  *			 and "[[<-" with a scalar in ./subassign.c
- *  vectorSubscript() -- for makeSubscript()   {currently unused externally}
  *  arraySubscript()  -- for "[i,j,..." and "[<-..." in ./subset.c, ./subassign.c
  */
 
@@ -871,6 +870,7 @@ makeSubscript(SEXP x, SEXP s, R_xlen_t *stretch, SEXP call)
 		return s;
 	    }
 	}
+#ifdef FAST_REAL_SUBSCRIPTS
 	else if (TYPEOF(s) == REALSXP) {
 	    double di = REAL(s)[0];
 	    if (1 <= di && di <= nx) {
@@ -882,6 +882,7 @@ makeSubscript(SEXP x, SEXP s, R_xlen_t *stretch, SEXP call)
 		return s;
 	    }
 	}
+#endif
     }
 
     PROTECT(s = duplicate(s));
