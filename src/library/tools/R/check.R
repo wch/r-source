6ba895d0e4a3378e08e9f65a3d07d11529298ee4
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-08-24 19:15:45 +0000

    turn replacing imports tests on with --as-cran
    git-svn-id: https://svn.r-project.org/R/branches/R-3-0-branch@63685 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/check.R
+++ src/library/tools/R/check.R
@@ -3782,6 +3782,7 @@ setRlibs <-
         Sys.setenv("_R_CHECK_NO_RECOMMENDED_" = "TRUE")
         Sys.setenv("_R_SHLIB_BUILD_OBJECTS_SYMBOL_TABLES_" = "TRUE")
         Sys.setenv("_R_CHECK_DOT_FIRSTLIB_" = "TRUE")
+        Sys.setenv("_R_CHECK_REPLACING_IMPORTS_" = "TRUE")
         R_check_vc_dirs <- TRUE
         R_check_executables_exclusions <- FALSE
         R_check_doc_sizes2 <- TRUE
