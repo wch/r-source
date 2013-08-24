a6f087842dc42af66f4a8f41204ef8d4fc48a525
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-08-24 17:59:59 +0000

    port filtering from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-3-0-branch@63683 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/check.R
+++ src/library/tools/R/check.R
@@ -3103,6 +3103,15 @@ setRlibs <-
                 if (!config_val_to_logical(check_imports_flag))
                     lines <- grep("Warning: replacing previous import", lines,
                                   fixed = TRUE, invert = TRUE, value = TRUE)
+                else {
+                    this <- unique(grep("Warning: replacing previous import",
+                                        lines, fixed = TRUE, value = TRUE))
+                    this <- grep(paste0(sQuote(pkgname), "$"), this,
+                                 value = TRUE)
+                    lines <- grep("Warning: replacing previous import", lines,
+                                  fixed = TRUE, invert = TRUE, value = TRUE)
+                    lines <- c(lines, this)
+                }
                 check_FirstLib_flag <-
                     Sys.getenv("_R_CHECK_DOT_FIRSTLIB_", "FALSE")
                 if (!config_val_to_logical(check_FirstLib_flag))
