2b0e99554bdefcbfa2343b4597790b42edff69f3
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-09-01 21:44:51 +0000

    make read/checkNEWS defunct
    git-svn-id: https://svn.r-project.org/R/trunk@63794 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/tools-defunct.R
+++ src/library/tools/R/tools-defunct.R
@@ -1,7 +1,7 @@
 #  File src/library/tools/R/tools-defunct.R
 #  Part of the R package, http://www.R-project.org
 #
-#  Copyright (C) 1995-2012 The R Core Team
+#  Copyright (C) 1995-2013 The R Core Team
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -19,3 +19,12 @@
 ## Defunct 2009-08-19
 ## Removed for 3.0.0
 ## Rd_parse <-function(file, text = NULL) .Defunct("parse_Rd")
+
+## Deprecated for 3.0.2
+## Defunct for 3.1.0 (and there is no such 'file')
+readNEWS <- function(file = file.path(R.home(), "NEWS"),
+                     trace = FALSE, chop = c("first", "1", "par1", "keepAll"))
+    .Defunct()
+
+checkNEWS <- function(file = file.path(R.home(), "NEWS"))
+    .Defunct()
