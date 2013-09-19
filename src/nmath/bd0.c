f932fe6edf97f50468d252819ec1293d6fe64206
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-09-19 18:31:39 +0000

    "last" tweak
    git-svn-id: https://svn.r-project.org/R/trunk@63954 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/nmath/bd0.c
+++ src/nmath/bd0.c
@@ -47,8 +47,9 @@ double attribute_hidden bd0(double x, double np)
 	if(fabs(s) < DBL_MIN) return s;
 	ej = 2*x*v;
 	v = v*v;
-	for (j = 1; j < 1000000; j++) { // Taylor series; no infinite loop
-	    ej *= v;// = v^(2j+1) 
+	for (j = 1; j < 1000; j++) { /* Taylor series; 1000: no infinite loop
+					as |v| < .1,  v^2000 is "zero" */
+	    ej *= v;// = v^(2j+1)
 	    s1 = s+ej/((j<<1)+1);
 	    if (s1 == s) /* last term was effectively 0 */
 		return s1 ;
