6d2e8c090f0fac6573e16ffcfb8163749f6b56bf
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-11-21 14:06:12 +0000

    print.table(x, zero.print = <non-"0">) now also has an effect for non-(integer valued) x
    git-svn-id: https://svn.r-project.org/R/trunk@64281 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/table.R
+++ src/library/base/R/table.R
@@ -133,9 +133,9 @@ function (x, digits = getOption("digits"), quote = FALSE, na.print = "",
     if(any(ina <- is.na(x)))
 	xx[ina] <- na.print
 
-    if(zero.print != "0" && any(i0 <- !ina & x == 0) && all(x == round(x)))
+    if(zero.print != "0" && any(i0 <- !ina & x == 0))
 	## MM thinks this should be an option for many more print methods...
-	xx[i0] <- sub("0", zero.print, xx[i0])
+	xx[i0] <- sub(if(all(x == round(x)))"0" else "0\\.0+", zero.print, xx[i0])
 
     ## Numbers get right-justified by format(), irrespective of 'justify'.
     ## We need to keep column headers aligned.
