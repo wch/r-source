5ef92e6f87d16e3f1b3e6b392d864aab3408c778
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-11-30 08:08:37 +0000

    use ftrunc where intended do not remap trunc in C++ code
    git-svn-id: https://svn.r-project.org/R/trunk@64353 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/nmath/pnorm.c
+++ src/nmath/pnorm.c
@@ -194,7 +194,7 @@ void pnorm_both(double x, double *cum, double *ccum, int i_tail, int log_p)
 	temp = (xnum + c[7]) / (xden + d[7]);
 
 #define do_del(X)							\
-	xsq = trunc(X * SIXTEN) / SIXTEN;				\
+	xsq = ftrunc(X * SIXTEN) / SIXTEN;				\
 	del = (X - xsq) * (X + xsq);					\
 	if(log_p) {							\
 	    *cum = (-xsq * xsq * 0.5) + (-del * 0.5) + log(temp);	\
