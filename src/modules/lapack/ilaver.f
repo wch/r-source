258f77e62bedfd2cca3c7a630ce815e37d34d4b1
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-12-08 18:22:20 +0000

    correct version of file for this LAPACK
    git-svn-id: https://svn.r-project.org/R/branches/R-3-0-branch@64415 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/modules/lapack/ilaver.f
+++ src/modules/lapack/ilaver.f
@@ -41,25 +41,25 @@
 *> \author Univ. of Colorado Denver 
 *> \author NAG Ltd. 
 *
-*> \date September 2012
+*> \date November 2011
 *
 *> \ingroup auxOTHERauxiliary
 *
 *  =====================================================================
       SUBROUTINE ILAVER( VERS_MAJOR, VERS_MINOR, VERS_PATCH )
 *
-*  -- LAPACK computational routine (version 3.5.0) --
+*  -- LAPACK computational routine (version 3.4.1) --
 *  -- LAPACK is a software package provided by Univ. of Tennessee,    --
 *  -- Univ. of California Berkeley, Univ. of Colorado Denver and NAG Ltd..--
-*     November 2013
+*     November 2011
 *
 *  =====================================================================
 *
       INTEGER VERS_MAJOR, VERS_MINOR, VERS_PATCH
 *  =====================================================================
       VERS_MAJOR = 3
-      VERS_MINOR = 5
-      VERS_PATCH = 0
+      VERS_MINOR = 4
+      VERS_PATCH = 1
 *  =====================================================================
 *
       RETURN
