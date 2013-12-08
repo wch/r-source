bb0ea6f40774c19939130ed1af077453054ae192
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-12-08 18:00:05 +0000

    port r64410 from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-3-0-branch@64413 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/modules/lapack/Lapack.c
+++ src/modules/lapack/Lapack.c
@@ -29,6 +29,9 @@
 
 #include "Lapack.h"
 
+extern void F77_NAME(ilaver)(int *major, int *minor, int *patch);
+
+
 /* NB: the handling of dims is odd here.  Most are coerced to be
  * integers (which dimgets currently guarantees), but a couple were
  * used unchecked. 
@@ -1340,6 +1343,15 @@ static SEXP mod_do_lapack(SEXP call, SEXP op, SEXP args, SEXP env)
 	a4 = CAR(args); args = CDR(args);
 	a5 = CAR(args); args = CDR(args);
 	ans = La_svd_cmplx(a1, a2, a3, a4, a5, CAR(args));
+	break;
+    }
+    case 1000:
+    {
+	int major, minor, patch;
+	char str[20];
+	F77_CALL(ilaver)(&major, &minor, &patch);
+	snprintf(str, 20, "%d.%d.%d", major, minor, patch);
+	ans = mkString(str);
 	break;
     }
     }
