dff24092d87bc67d6b7c3861ae6385860606fac9
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2013-12-24 10:31:37 +0000

    ldouble overflows are undefined, so handle ourselves
    git-svn-id: https://svn.r-project.org/R/trunk@64511 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/summary.c
+++ src/main/summary.c
@@ -25,6 +25,8 @@
 #include <Defn.h>
 #include <Internal.h>
 
+#include <float.h> // for DBL_MAX
+
 #define R_MSG_type	_("invalid 'type' (%s) of argument")
 #define imax2(x, y) ((x < y) ? y : x)
 
@@ -118,7 +120,9 @@ static Rboolean rsum(double *x, R_xlen_t n, double *value, Rboolean narm)
 	    s += x[i];
 	}
     }
-    *value = (double) s;
+    if(s > DBL_MAX) *value = R_PosInf;
+    else if (s < -DBL_MAX) *value = R_NegInf;
+    else *value = (double) s;
 
     return updated;
 }
@@ -318,7 +322,9 @@ static Rboolean rprod(double *x, R_xlen_t n, double *value, Rboolean narm)
 	    s *= x[i];
 	}
     }
-    *value = (double) s;
+    if(s > DBL_MAX) *value = R_PosInf;
+    else if (s < -DBL_MAX) *value = R_NegInf;
+    else *value = (double) s;
 
     return updated;
 }
