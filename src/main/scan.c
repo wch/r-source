5772ecd06628b1d92680f60a71ccbe778985ba6e
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2014-01-07 20:57:57 +0000

    faulty merge
    git-svn-id: https://svn.r-project.org/R/branches/R-3-0-branch@64692 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/scan.c
+++ src/main/scan.c
@@ -970,7 +970,7 @@ SEXP attribute_hidden do_scan(SEXP call, SEXP op, SEXP args, SEXP rho)
     if (!data.ttyflag && !data.wasopen)
 	data.con->close(data.con);
     if (data.quoteset[0]) free(data.quoteset);
-    if (!skipNul && data.embedWarn) 
+    if (data.embedWarn) 
 	warning(_("embedded nul(s) found in input"));
     return ans;
 }
