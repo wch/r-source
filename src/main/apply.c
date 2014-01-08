e060060619a5040713b10e92d1662c4d8e75958c
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2014-01-08 09:26:23 +0000

    code cleanup: - make use of C99 features - move comments to just above the code they refer to
    git-svn-id: https://svn.r-project.org/R/trunk@64696 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/apply.c
+++ src/main/apply.c
@@ -27,60 +27,57 @@
 /* .Internal(lapply(X, FUN)) */
 
 /* This is a special .Internal, so has unevaluated arguments.  It is
-   called from a closure wrapper, so X and FUN are promises. */
+   called from a closure wrapper, so X and FUN are promises. 
+
+   FUN must be unevaluated for use in e.g. bquote .
+*/
 SEXP attribute_hidden do_lapply(SEXP call, SEXP op, SEXP args, SEXP rho)
 {
-    SEXP R_fcall, ans, names, X, XX, FUN;
-    R_xlen_t i, n;
     PROTECT_INDEX px;
 
     checkArity(op, args);
-    PROTECT_WITH_INDEX(X = CAR(args), &px);
-    PROTECT(XX = eval(CAR(args), rho));
-    FUN = CADR(args);  /* must be unevaluated for use in e.g. bquote */
-    n = xlength(XX);
-    if (n == NA_INTEGER) error(_("invalid length"));
+    SEXP X, XX, FUN;
+    PROTECT_WITH_INDEX(X =CAR(args), &px);
+    XX = PROTECT(eval(CAR(args), rho));
+    R_xlen_t n = xlength(XX);  // a vector, so will be valid.
+    FUN = CADR(args);
     Rboolean realIndx = n > INT_MAX;
 
-    PROTECT(ans = allocVector(VECSXP, n));
-    names = getAttrib(XX, R_NamesSymbol);
+    SEXP ans = PROTECT(allocVector(VECSXP, n));
+    SEXP names = getAttrib(XX, R_NamesSymbol);
     if(!isNull(names)) setAttrib(ans, R_NamesSymbol, names);
 
+    /* Build call: FUN(XX[[<ind>]], ...) */
+
+    /* Notice that it is OK to have one arg to LCONS do memory
+       allocation and not PROTECT the result (LCONS does memory
+       protection of its args internally), but not both of them,
+       since the computation of one may destroy the other */
+    
+    SEXP ind = PROTECT(allocVector(realIndx ? REALSXP : INTSXP, 1));
+    SEXP tmp;
     /* The R level code has ensured that XX is a vector.
        If it is atomic we can speed things up slightly by
        using the evaluated version.
     */
-    {
-	SEXP ind, tmp;
-	/* Build call: FUN(XX[[<ind>]], ...) */
+    if(isVectorAtomic(XX))
+	tmp = PROTECT(tmp = LCONS(R_Bracket2Symbol,
+				  LCONS(XX, LCONS(ind, R_NilValue))));
+    else
+	tmp = PROTECT(LCONS(R_Bracket2Symbol,
+			    LCONS(X, LCONS(ind, R_NilValue))));
+    SEXP R_fcall = PROTECT(LCONS(FUN,
+				 LCONS(tmp, LCONS(R_DotsSymbol, R_NilValue))));
 
-	/* Notice that it is OK to have one arg to LCONS do memory
-	   allocation and not PROTECT the result (LCONS does memory
-	   protection of its args internally), but not both of them,
-	   since the computation of one may destroy the other */
-
-	PROTECT(ind = allocVector(realIndx ? REALSXP : INTSXP, 1));
-	if(isVectorAtomic(XX))
-	    PROTECT(tmp = LCONS(R_Bracket2Symbol,
-				LCONS(XX, LCONS(ind, R_NilValue))));
-	else
-	    PROTECT(tmp = LCONS(R_Bracket2Symbol,
-				LCONS(X, LCONS(ind, R_NilValue))));
-	PROTECT(R_fcall = LCONS(FUN,
-				LCONS(tmp, LCONS(R_DotsSymbol, R_NilValue))));
-
-	for(i = 0; i < n; i++) {
-	    if (realIndx) REAL(ind)[0] = (double)(i + 1);
-	    else INTEGER(ind)[0] = (int)(i + 1);
-	    tmp = eval(R_fcall, rho);
-	    if (NAMED(tmp))
-		tmp = duplicate(tmp);
-	    SET_VECTOR_ELT(ans, i, tmp);
-	}
-	UNPROTECT(3);
+    for(R_xlen_t i = 0; i < n; i++) {
+	if (realIndx) REAL(ind)[0] = (double)(i + 1);
+	else INTEGER(ind)[0] = (int)(i + 1);
+	tmp = eval(R_fcall, rho);
+	if (NAMED(tmp)) tmp = duplicate(tmp);
+	SET_VECTOR_ELT(ans, i, tmp);
     }
 
-    UNPROTECT(3); /* X, XX, ans */
+    UNPROTECT(6);
     return ans;
 }
 
