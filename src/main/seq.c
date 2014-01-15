da75a338642890a722ab717c3c5efd45e94cd455
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2014-01-15 23:47:43 +0000

    rep.int in rep2 could create shared substructure without setting NAMED.
    git-svn-id: https://svn.r-project.org/R/trunk@64789 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/seq.c
+++ src/main/seq.c
@@ -236,6 +236,7 @@ static SEXP rep2(SEXP s, SEXP ncopy)
 	    SEXP elt = duplicate(VECTOR_ELT(s, i));
 	    for (j = 0; j < INTEGER(t)[i]; j++)
 		SET_VECTOR_ELT(a, n++, elt);
+	    if (j > 1) SET_NAMED(elt, 2);
 	}
 	break;
     case RAWSXP:
