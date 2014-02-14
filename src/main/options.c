b46ce83bff2e5a1f0c4a92368bd7e43ad38be846
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2014-02-14 06:16:06 +0000

    change comment
    git-svn-id: https://svn.r-project.org/R/trunk@64993 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/options.c
+++ src/main/options.c
@@ -341,8 +341,11 @@ SEXP attribute_hidden do_options(SEXP call, SEXP op, SEXP args, SEXP rho)
     options = SYMVALUE(Options());
 
     /* This code is not re-entrant and people have used it in
-       finalizers.  So GC here to reduce nasty surprises */
-    //R_gc();
+       finalizers.
+
+       If a re-entrancy lock needs to be added, note that it
+       would apply to R_SetOption* too.
+    */
 
     if (args == R_NilValue) {
 	/* This is the zero argument case.
