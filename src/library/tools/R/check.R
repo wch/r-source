6b7913888501befb5330e7e2607132776353f175
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2014-02-20 18:21:43 +0000

    port r65049,50 from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-3-0-branch@65051 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/check.R
+++ src/library/tools/R/check.R
@@ -2937,7 +2937,9 @@ setRlibs <-
                 ## known false positives
                 for(fp in  c("foreign/tests/datefactor.dta",
                              "msProcess/inst/data[12]/.*.txt",
-                             "WMBrukerParser/inst/Examples/C3ValidationExtractSmall/RobotRun1/2-100kDa/0_B1/1/1SLin/fid") )
+                             "WMBrukerParser/inst/Examples/C3ValidationExtractSmall/RobotRun1/2-100kDa/0_B1/1/1SLin/fid",
+                             "bayeslife/inst/ex-data/bayesLife.output/predictions/traj_country104.rda" # file 5.16
+                             ) )
                     known <- known | grepl(fp, pexecs)
                 execs <- execs[!known]
             }
