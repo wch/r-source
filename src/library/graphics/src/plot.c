17473cfa0a90c284cb7b2162b930b8823a681662
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2014-03-23 20:57:10 +0000

    misuse of LENGTH
    git-svn-id: https://svn.r-project.org/R/trunk@65265 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/graphics/src/plot.c
+++ src/library/graphics/src/plot.c
@@ -1829,9 +1829,7 @@ SEXP C_raster(SEXP args)
 
     raster = CAR(args); args = CDR(args);
     n = LENGTH(raster);
-    if (n <= 0) {
-        error(_("Empty raster"));  
-    }
+    if (n <= 0) error(_("Empty raster"));  
     dim = getAttrib(raster, R_DimSymbol);
 
     vmax = vmaxget();
@@ -2527,19 +2525,19 @@ SEXP C_title(SEXP args)
 
     Main = sub = xlab = ylab = R_NilValue;
 
-    if (CAR(args) != R_NilValue && LENGTH(CAR(args)) > 0)
+    if (CAR(args) != R_NilValue && length(CAR(args)) > 0)
 	Main = CAR(args);
     args = CDR(args);
 
-    if (CAR(args) != R_NilValue && LENGTH(CAR(args)) > 0)
+    if (CAR(args) != R_NilValue && length(CAR(args)) > 0)
 	sub = CAR(args);
     args = CDR(args);
 
-    if (CAR(args) != R_NilValue && LENGTH(CAR(args)) > 0)
+    if (CAR(args) != R_NilValue && length(CAR(args)) > 0)
 	xlab = CAR(args);
     args = CDR(args);
 
-    if (CAR(args) != R_NilValue && LENGTH(CAR(args)) > 0)
+    if (CAR(args) != R_NilValue && length(CAR(args)) > 0)
 	ylab = CAR(args);
     args = CDR(args);
 
