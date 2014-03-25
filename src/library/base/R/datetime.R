4246ace3acae70fa6ec57dc8782792647fbf2272
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2014-03-25 12:57:43 +0000

    updates of anyNA change
    git-svn-id: https://svn.r-project.org/R/trunk@65282 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/datetime.R
+++ src/library/base/R/datetime.R
@@ -397,7 +397,7 @@ as.list.POSIXct <- function(x, ...)
 }
 
 is.na.POSIXlt <- function(x) is.na(as.POSIXct(x))
-anyNA.POSIXlt <- function(x) anyNA(as.POSIXct(x))
+anyNA.POSIXlt <- function(x, recursive = FALSE) anyNA(as.POSIXct(x))
 
 ## <FIXME> check the argument validity
 ## This is documented to remove the timezone
