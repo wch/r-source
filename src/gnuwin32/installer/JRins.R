bd81740a2b040b41823f2fa5acee65b7afe12b95
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2014-05-08 14:26:02 +0000

    reduce SVN in installer version
    git-svn-id: https://svn.r-project.org/R/trunk@65549 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/installer/JRins.R
+++ src/gnuwin32/installer/JRins.R
@@ -28,7 +28,9 @@
 
     Rver <- readLines("../../../VERSION")[1L]
     Rver <- sub("Under .*$", "Pre-release", Rver)
+    ## This is now over 2^16, so truncate
     SVN <- sub("Revision: ", "", readLines("../../../SVN-REVISION"))[1L]
+    SVN <- as.character(as.numeric(SVN) - 50000L)
     Rver0 <- paste(sub(" .*$", "", Rver), SVN, sep = ".")
 
 
