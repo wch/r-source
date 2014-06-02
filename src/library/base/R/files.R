81a56486adc5290052b2dcd99245234a500a76bb
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2014-06-02 06:19:09 +0000

    port small performance tweak from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-3-1-branch@65816 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/files.R
+++ src/library/base/R/files.R
@@ -1,7 +1,7 @@
 #  File src/library/base/R/files.R
 #  Part of the R package, http://www.R-project.org
 #
-#  Copyright (C) 1995-2013 The R Core Team
+#  Copyright (C) 1995-2014 The R Core Team
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -128,10 +128,11 @@ file.copy <- function(from, to,
     	okay[okay] <- file.create(to[okay])
     	if(any(okay)) {
             okay[okay] <- file.append(to[okay], from[okay])
-            if(copy.mode)
-                Sys.chmod(to[okay], file.info(from[okay])$mode, TRUE)
-            if(copy.date)
-                Sys.setFileTime(to[okay], file.info(from[okay])$mtime)
+            if(copy.mode || copy.date) {
+                fi <- file.info(from[okay], extra_cols = FALSE)
+                if(copy.mode) Sys.chmod(to[okay], fi$mode, TRUE)
+                if(copy.date) Sys.setFileTime(to[okay], fi$mtime)
+            }
         }
     }
     okay
