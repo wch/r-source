b4cc3ba74b541e6654a31320e49d53f45283ca17
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2014-06-02 06:58:00 +0000

    mis-merge
    git-svn-id: https://svn.r-project.org/R/branches/R-3-1-branch@65818 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/files.R
+++ src/library/base/R/files.R
@@ -128,8 +128,8 @@ file.copy <- function(from, to,
     	okay[okay] <- file.create(to[okay])
     	if(any(okay)) {
             okay[okay] <- file.append(to[okay], from[okay])
-            if(copy.mode || copy.date) {
-                fi <- file.info(from[okay], extra_cols = FALSE)
+            if(copy.mode || copy.date) { # file.info call can be slow
+                fi <- file.info(from[okay])
                 if(copy.mode) Sys.chmod(to[okay], fi$mode, TRUE)
                 if(copy.date) Sys.setFileTime(to[okay], fi$mtime)
             }
