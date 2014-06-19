9f60f511dc2a7d3659ab576fee87aa69953031ce
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2014-06-19 15:46:40 +0000

    port doc tweaks from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-3-1-branch@65976 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/build.R
+++ src/library/tools/R/build.R
@@ -3,8 +3,6 @@
 #
 #  Copyright (C) 1995-2013 The R Core Team
 #
-# NB: also copyright date in Usage.
-#
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 2 of the License, or
