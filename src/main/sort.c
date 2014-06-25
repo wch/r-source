21fa9905f91e3a553e3b58d009106a6032101b99
murdoch <murdoch@00db46b3-68df-0310-9c12-caf00c1e9a41>
2014-06-25 02:53:52 +0000

    Potential protection error from Radford Neal:  "Fixed lack of protection bugs in the equal and greater functions in sort.c."
    git-svn-id: https://svn.r-project.org/R/trunk@66019 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/sort.c
+++ src/main/sort.c
@@ -653,11 +653,11 @@ static int equal(R_xlen_t i, R_xlen_t j, SEXP x, Rboolean nalast, SEXP rho)
     if (isObject(x) && !isNull(rho)) { /* so never any NAs */
 	/* evaluate .gt(x, i, j) */
 	SEXP si, sj, call;
-	si = ScalarInteger((int)i+1);
-	sj = ScalarInteger((int)j+1);
+	PROTECT(si = ScalarInteger((int)i+1));
+	PROTECT(sj = ScalarInteger((int)j+1));
 	PROTECT(call = lang4(install(".gt"), x, si, sj));
 	c = asInteger(eval(call, rho));
-	UNPROTECT(1);
+	UNPROTECT(3);
     } else {
 	switch (TYPEOF(x)) {
 	case LGLSXP:
@@ -691,11 +691,11 @@ static int greater(R_xlen_t i, R_xlen_t j, SEXP x, Rboolean nalast,
     if (isObject(x) && !isNull(rho)) { /* so never any NAs */
 	/* evaluate .gt(x, i, j) */
 	SEXP si, sj, call;
-	si = ScalarInteger((int)i+1);
-	sj = ScalarInteger((int)j+1);
+	PROTECT(si = ScalarInteger((int)i+1));
+	PROTECT(sj = ScalarInteger((int)j+1));
 	PROTECT(call = lang4(install(".gt"), x, si, sj));
 	c = asInteger(eval(call, rho));
-	UNPROTECT(1);
+	UNPROTECT(3);
     } else {
 	switch (TYPEOF(x)) {
 	case LGLSXP:
