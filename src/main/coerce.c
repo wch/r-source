8a7f0d69cd37ceda2b0095cdaf19aedae78e65ba
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2014-06-27 05:02:27 +0000

    Fixed missing NAMED adjustment in VectorToPaitList (PR15843).
    git-svn-id: https://svn.r-project.org/R/trunk@66045 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/coerce.c
+++ src/main/coerce.c
@@ -395,6 +395,8 @@ SEXP VectorToPairList(SEXP x)
     named = (xnames != R_NilValue);
     xptr = xnew;
     for (i = 0; i < len; i++) {
+	if (NAMED(x) > NAMED(VECTOR_ELT(x, i)))
+	    SET_NAMED(VECTOR_ELT(x, i), NAMED(x));
 	SETCAR(xptr, VECTOR_ELT(x, i));
 	if (named && CHAR(STRING_ELT(xnames, i))[0] != '\0') /* ASCII */
 	    SET_TAG(xptr, installTrChar(STRING_ELT(xnames, i)));
