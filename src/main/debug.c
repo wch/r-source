0a59564f4b234cdbbe51ef5cecd6d42172ba3ab1
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2014-08-10 13:11:45 +0000

    Fix for incomplete chenge in ro_retracemem in r66326.
    git-svn-id: https://svn.r-project.org/R/trunk@66330 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/debug.c
+++ src/main/debug.c
@@ -219,13 +219,13 @@ SEXP attribute_hidden do_retracemem(SEXP call, SEXP op, SEXP args, SEXP rho)
     if(CAR(argList) == R_MissingArg) SETCAR(argList, R_NilValue);
     if(CADR(argList) == R_MissingArg) SETCAR(CDR(argList), R_NilValue);
 
-    object = CAR(ap);
+    object = CAR(argList);
     if (TYPEOF(object) == CLOSXP ||
 	TYPEOF(object) == BUILTINSXP ||
 	TYPEOF(object) == SPECIALSXP)
 	errorcall(call, _("argument must not be a function"));
 
-    previous = CADR(ap);
+    previous = CADR(argList);
     if(!isNull(previous) && !isString(previous))
 	    errorcall(call, _("invalid '%s' argument"), "previous");
 
