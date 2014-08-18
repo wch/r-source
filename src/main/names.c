cb9721f70192e3e98c532af56130d5da912d2efa
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2014-08-18 18:53:55 +0000

    Have install only check that symbol name has valid length if the symbol does not already exist; from Tomas Kalibera.
    git-svn-id: https://svn.r-project.org/R/trunk@66422 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/names.c
+++ src/main/names.c
@@ -1176,16 +1176,16 @@ SEXP install(const char *name)
     SEXP sym;
     int i, hashcode;
 
-    if (*name == '\0')
-	error(_("attempt to use zero-length variable name"));
-    if (strlen(name) > MAXIDSIZE)
-	error(_("variable names are limited to %d bytes"), MAXIDSIZE);
     hashcode = R_Newhashpjw(name);
     i = hashcode % HSIZE;
     /* Check to see if the symbol is already present;  if it is, return it. */
     for (sym = R_SymbolTable[i]; sym != R_NilValue; sym = CDR(sym))
 	if (strcmp(name, CHAR(PRINTNAME(CAR(sym)))) == 0) return (CAR(sym));
     /* Create a new symbol node and link it into the table. */
+    if (*name == '\0')
+	error(_("attempt to use zero-length variable name"));
+    if (strlen(name) > MAXIDSIZE)
+	error(_("variable names are limited to %d bytes"), MAXIDSIZE);
     sym = mkSYMSXP(mkChar(name), R_UnboundValue);
     SET_HASHVALUE(PRINTNAME(sym), hashcode);
     SET_HASHASH(PRINTNAME(sym), 1);
