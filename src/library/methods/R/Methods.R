675bdd4dcdc26a5b7e4f3161586bbf29a50344cd
jmc <jmc@00db46b3-68df-0310-9c12-caf00c1e9a41>
2014-08-19 00:14:23 +0000

    callGeneric() could fail if "..." was a formal argument (bug #15937) because substitute() misbehaves for "...". Revision uses sys.call() instead of substitute() Also some regression tests for callGeneric() added.
    git-svn-id: https://svn.r-project.org/R/branches/R-3-1-branch@66424 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/methods/R/Methods.R
+++ src/library/methods/R/Methods.R
@@ -1236,7 +1236,8 @@ callGeneric <- function(...)
             }
         }
         else {
-            call <- substitute(fname(...))
+            call <- sys.call() # just use the arguments to callGeneric()
+            call[[1]] <- fname
         }
     }
     eval(call, sys.frame(sys.parent()))
