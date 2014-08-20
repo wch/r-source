4c50be027452353e16b2037d686beb18b77227a1
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2014-08-20 21:03:15 +0000

    not yet: for testing
    git-svn-id: https://svn.r-project.org/R/trunk@66447 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/memory.c
+++ src/main/memory.c
@@ -2108,12 +2108,9 @@ void vmaxset(const void *ovmax)
 
 char *R_alloc(size_t nelem, int eltsize)
 {
-    /* From a historical mistake, this over-allocated by 1 byte.
-       Circumvent bugs in packages by still doing so. */
-    // To ensure 16-byte alignment, initially over-allocate by 16
-    R_size_t size = nelem * eltsize + 16;
+    R_size_t size = nelem * eltsize;
     /* doubles are a precaution against integer overflow on 32-bit */
-    double dsize = (double) nelem * eltsize + 16;
+    double dsize = (double) nelem * eltsize;
     if (dsize > 0) {
 	SEXP s;
 #ifdef LONG_VECTOR_SUPPORT
@@ -2121,18 +2118,16 @@ char *R_alloc(size_t nelem, int eltsize)
 	if(dsize > R_XLEN_T_MAX)  /* currently 4096 TB */
 	    error(_("cannot allocate memory block of size %0.f Tb"),
 		  dsize/R_pow_di(1024.0, 4));
-	s = allocVector(RAWSXP, size);
+	s = allocVector(RAWSXP, size + 1);
 #else
 	if(dsize > R_LEN_T_MAX) /* must be in the Gb range */
 	    error(_("cannot allocate memory block of size %0.1f Gb"),
 		  dsize/R_pow_di(1024.0, 3));
-	s = allocVector(RAWSXP, size);
+	s = allocVector(RAWSXP, size + 1);
 #endif
 	ATTRIB(s) = R_VStack;
 	R_VStack = s;
-	char *res = (char *) DATAPTR(s);
-	unsigned int align = ((uintptr_t) res) % 16;
-	return (align > 0) ? res + (16-align) : res;
+	return (char *) DATAPTR(s);
     }
     /* One programmer has relied on this, but it is undocumented! */
     else return NULL;
