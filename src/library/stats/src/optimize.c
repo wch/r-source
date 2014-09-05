6c9cdfea47c7f0d709f6376b15302b6fc2aaa873
lawrence <lawrence@00db46b3-68df-0310-9c12-caf00c1e9a41>
2014-09-05 21:10:06 +0000

    Proposed fix for #15958 that takes the simple approach of allocating a new vector with each invocation of the nlm() callback
    git-svn-id: https://svn.r-project.org/R/trunk@66528 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/src/optimize.c
+++ src/library/stats/src/optimize.c
@@ -514,7 +514,8 @@ static void fcn(int n, const double x[], double *f, function_info
 	return;
     }
 				/* calculate for a new value of x */
-    s = CADR(R_fcall);
+    s = allocVector(REALSXP, n);
+    SETCADR(R_fcall, s);
     for (i = 0; i < n; i++) {
 	if (!R_FINITE(x[i])) error(_("non-finite value supplied by 'nlm'"));
 	REAL(s)[i] = x[i];
@@ -779,11 +780,9 @@ SEXP nlm(SEXP call, SEXP op, SEXP args, SEXP rho)
     R_gradientSymbol = install("gradient");
     R_hessianSymbol = install("hessian");
 
-    /* This vector is shared with all subsequent calls */
     v = allocVector(REALSXP, n);
     for (i = 0; i < n; i++) REAL(v)[i] = x[i];
     SETCADR(state->R_fcall, v);
-    SET_NAMED(v, 2); // in case the functions try to alter it
     value = eval(state->R_fcall, state->R_env);
 
     v = getAttrib(value, R_gradientSymbol);
