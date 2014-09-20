cf505d5f81b312b205dba7b4c4397c09bd86cb64
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2014-09-20 20:19:07 +0000

    compute INTEGER(.) only once
    git-svn-id: https://svn.r-project.org/R/trunk@66649 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/src/hclust-utils.c
+++ src/library/stats/src/hclust-utils.c
@@ -1,7 +1,7 @@
 /*
  *  R : A Computer Language for Statistical Data Analysis
 
- *  Copyright (C) 1999-2008   The R Core Team
+ *  Copyright (C) 1999-2014   The R Core Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -30,10 +30,10 @@ SEXP cutree(SEXP merge, SEXP which)
     SEXP ans;
     int n, k, l, nclust, m1, m2, j, mm = 0;
     Rboolean found_j, *sing;
-    int *m_nr, *z;
+    int *m_nr, *z, *i_merge, *i_which, *i_ans;
 
-    PROTECT(merge = coerceVector(merge, INTSXP));
-    PROTECT(which = coerceVector(which, INTSXP));
+    PROTECT(merge = coerceVector(merge, INTSXP)); i_merge = INTEGER(merge);
+    PROTECT(which = coerceVector(which, INTSXP)); i_which = INTEGER(which);
 
     n = nrows(merge)+1;
     /* using 1-based indices ==> "--" */
@@ -41,6 +41,7 @@ SEXP cutree(SEXP merge, SEXP which)
     m_nr = (int *) R_alloc(n, sizeof(int)); m_nr--;
     z	 = (int *) R_alloc(n, sizeof(int)); z--;
     PROTECT(ans = allocMatrix(INTSXP, n, LENGTH(which)));
+    i_ans = INTEGER(ans);
 
     for(k = 1; k <= n; k++) {
 	sing[k] = TRUE;/* is k-th obs. still alone in cluster ? */
@@ -49,8 +50,8 @@ SEXP cutree(SEXP merge, SEXP which)
 
     for(k = 1; k <= n-1; k++) {
 	/* k-th merge, from n-k+1 to n-k atoms: (m1,m2) = merge[ k , ] */
-	m1 = INTEGER(merge)[k-1];
-	m2 = INTEGER(merge)[n-1+k-1];
+	m1 = i_merge[k-1];
+	m2 = i_merge[n-1+k-1];
 
 	if(m1 < 0 && m2 < 0) {/* merging atoms [-m1] and [-m2] */
 	    m_nr[-m1] = m_nr[-m2] = k;
@@ -76,7 +77,7 @@ SEXP cutree(SEXP merge, SEXP which)
 	 * if yes, find j (maybe multiple ones): */
 	found_j = FALSE;
 	for(j = 0; j < LENGTH(which); j++) {
-	    if(INTEGER(which)[j] == n - k) {
+	    if(i_which[j] == n - k) {
 		if(!found_j) { /* first match (and usually only one) */
 		    found_j = TRUE;
 		    for(l = 1; l <= n; l++)
@@ -85,17 +86,17 @@ SEXP cutree(SEXP merge, SEXP which)
 		    mm = j*n; /*may want to copy this column of ans[] */
 		    for(l = 1, m1 = mm; l <= n; l++, m1++) {
 			if(sing[l])
-			    INTEGER(ans)[m1] = ++nclust;
+			    i_ans[m1] = ++nclust;
 			else {
 			    if (z[m_nr[l]] == 0)
 				z[m_nr[l]] = ++nclust;
-			    INTEGER(ans)[m1] = z[m_nr[l]];
+			    i_ans[m1] = z[m_nr[l]];
 			}
 		    }
 		}
 		else { /* found_j: another which[j] == n-k : copy column */
 		    for(l = 1, m1 = j*n, m2 = mm; l <= n; l++, m1++, m2++)
-			INTEGER(ans)[m1] = INTEGER(ans)[m2];
+			i_ans[m1] = i_ans[m2];
 		}
 	    } /* if ( match ) */
 	} /* for(j .. which[j] ) */
@@ -103,9 +104,9 @@ SEXP cutree(SEXP merge, SEXP which)
 
     /* Dealing with trivial case which[] = n : */
     for(j = 0; j < LENGTH(which); j++)
-	if(INTEGER(which)[j] == n)
+	if(i_which[j] == n)
 	    for(l = 1, m1 = j*n; l <= n; l++, m1++)
-		INTEGER(ans)[m1] = l;
+		i_ans[m1] = l;
 
     UNPROTECT(3);
     return(ans);
