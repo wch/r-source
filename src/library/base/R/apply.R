107ad44a54a825555618cb8c0ebaf9beac04ae2e
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2014-10-07 14:46:43 +0000

    apply() preserving names(dimnames(.)) more often
    git-svn-id: https://svn.r-project.org/R/trunk@66721 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/apply.R
+++ src/library/base/R/apply.R
@@ -98,7 +98,10 @@ apply <- function(X, MARGIN, FUN, ...)
 	return(array(ans, d.ans, dn.ans))
     if(len.a && len.a %% d2 == 0L) {
         if(is.null(dn.ans)) dn.ans <- vector(mode="list", length(d.ans))
-        dn.ans <- c(list(ans.names), dn.ans)
+	dn1 <- if((na <- length(ans.names)) > 0 && length(dn.call) &&
+		  na == length(dn.call[[1L]])) dn.call[1L]
+	       else list(ans.names)
+	dn.ans <- c(dn1, dn.ans)
 	return(array(ans, c(len.a %/% d2, d.ans),
 		     if(!all(vapply(dn.ans, is.null, NA))) dn.ans))
     }
