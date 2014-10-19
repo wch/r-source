83ee4ddb9d962f305d3af1ca914c5513bdcc4993
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2014-10-19 08:13:17 +0000

    add aliases for more environment variables
    git-svn-id: https://svn.r-project.org/R/trunk@66794 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/factor.R
+++ src/library/base/R/factor.R
@@ -1,7 +1,7 @@
 #  File src/library/base/R/factor.R
 #  Part of the R package, http://www.R-project.org
 #
-#  Copyright (C) 1995-2013 The R Core Team
+#  Copyright (C) 1995-2014 The R Core Team
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
