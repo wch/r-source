c5c71ca824c1a899f803c968e01ca27ea10ba028
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2014-10-20 08:38:45 +0000

    tweak
    git-svn-id: https://svn.r-project.org/R/trunk@66804 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/grid/R/primitives.R
+++ src/library/grid/R/primitives.R
@@ -929,7 +929,7 @@ validDetails.beziergrob <- function(x) {
     if (!is.null(x$id.lengths))
         x$id.lengths <- as.integer(x$id.lengths)
     if (is.null(x$id) && is.null(x$id.lengths)) {
-        if (length(x$x) != 4)
+        if (length(x$x) != 4L)
             stop("must have exactly 4 control points")
     } else {
         if (is.null(x$id)) {
@@ -938,7 +938,7 @@ validDetails.beziergrob <- function(x) {
             id <- x$id
         }
         xper <- split(x$x, id)
-        if (any(sapply(xper, length) != 4))
+        if (any(sapply(xper, length) != 4L))
             stop("must have exactly 4 control points per Bezier curve")
     }
     if (!(is.null(x$arrow) || inherits(x$arrow, "arrow")))
