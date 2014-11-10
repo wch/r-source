125a8ac01c5263b36a64089118f2707adf5a291e
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2014-11-10 16:39:13 +0000

    example(*, run.dontcheck=interactive()) no longer runs \dontcheck{.} parts  when used non-interactively, as in R CMD check <pkg>
    git-svn-id: https://svn.r-project.org/R/trunk@66951 00db46b3-68df-0310-9c12-caf00c1e9a41

--- tests/reg-examples2.R
+++ tests/reg-examples2.R
@@ -1,5 +1,8 @@
 ## For examples skipped in testing because they are 'random'
 
+## some should still be skipped when  --with-recommended-packages=no :
+base.and.rec <- .packages(all.available=TRUE, lib=.Library)
+
 set.seed(1)
 if(.Platform$OS.type == "windows") options(pager = "console")
 
@@ -7,27 +10,27 @@ pdf("reg-examples-2.pdf", encoding = "ISOLatin1.enc")
 
 
 ## stats
-example(SSasympOrig)
-example(SSlogis)
-example(constrOptim)
-example(cancor)
-example(aov)
+example(SSasympOrig, run.dontcheck=TRUE)
+example(SSlogis, run.dontcheck=TRUE)
+example(constrOptim, run.dontcheck=TRUE)
+example(cancor, run.dontcheck=TRUE)
+example(aov, run.dontcheck=TRUE)
 # signs for promax rotation are arbitrary
-example(factanal)
-example(family)
-example(glm)
-example(glm.control)
+example(factanal, run.dontcheck=TRUE)
+example(family, run.dontcheck=TRUE)
+example(glm, run.dontcheck = any("MASS" == base.and.rec))
+example(glm.control, run.dontcheck=TRUE)
 # from extractAIC
-extractAIC(glm.D93)
-example(influence.measures)
-example(lm)
-example(ls.diag)
-example(model.tables)
-example(nlminb)
-example(optim)
-example(prcomp)
-example(step)
-example(summary.manova)
-example(uniroot)
+extractAIC(glm.D93, run.dontcheck=TRUE)
+example(influence.measures, run.dontcheck=TRUE)
+example(lm, run.dontcheck=TRUE)
+example(ls.diag, run.dontcheck=TRUE)
+example(model.tables, run.dontcheck=TRUE)
+example(nlminb, run.dontcheck=TRUE)
+example(optim, run.dontcheck=TRUE)
+example(prcomp, run.dontcheck=TRUE)
+example(step, run.dontcheck=TRUE)
+example(summary.manova, run.dontcheck=TRUE)
+example(uniroot, run.dontcheck=TRUE)
 
 proc.time()
