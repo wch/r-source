5e85c3317fcf4c0d46e8e25809396b5e9f528a80
murdoch <murdoch@00db46b3-68df-0310-9c12-caf00c1e9a41>
2014-11-14 16:59:24 +0000

    Patch has been ported to R-patched.
    git-svn-id: https://svn.r-project.org/R/trunk@66982 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/console.c
+++ src/gnuwin32/console.c
@@ -3,7 +3,7 @@
  *  file console.c
  *  Copyright (C) 1998--2003  Guido Masarotto and Brian Ripley
  *  Copyright (C) 2004-8      The R Foundation
- *  Copyright (C) 2004-2013   The R Core Team
+ *  Copyright (C) 2004-2014   The R Core Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
