179cdf5e4ef11fc85d1a639a0459ae8e056a28c0
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2015-01-14 11:07:54 +0000

    Temporary workaround for c67440.
    git-svn-id: https://svn.r-project.org/R/trunk@67460 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/QC.R
+++ src/library/tools/R/QC.R
@@ -3956,6 +3956,16 @@ function(package, lib.loc = NULL)
 format.check_code_usage_in_package <-
 function(x, ...)
 {
+    ## <FIXME>
+    ## Temporarily try to remove
+    ##   ... used in a situation where it does not exist
+    ## notes caused by c67440
+    if(length(x)) {
+        ind <- grepl("... used in a situation where it does not exist",
+                     x, fixed = TRUE)
+        if(any(ind)) x <- x[!ind]
+    }
+    ## </FIXME>
     if(length(x)) {
         ## There seems no easy we can gather usage diagnostics by type,
         ## so try to rearrange to some extent when formatting.
