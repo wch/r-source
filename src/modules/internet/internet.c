070c57a4f9e2c1203665654a88082ff124837e3d
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2015-01-18 11:37:04 +0000

    put back lost line
    git-svn-id: https://svn.r-project.org/R/trunk@67513 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/modules/internet/internet.c
+++ src/modules/internet/internet.c
@@ -142,6 +142,7 @@ static void url_close(Rconnection con)
 {
     UrlScheme type = ((Rurlconn)(con->private))->type;
     switch(type) {
+    case HTTPsh:
     case HTTPSsh:
 	in_R_HTTPClose(((Rurlconn)(con->private))->ctxt);
 	break;
