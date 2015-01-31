e4f12c04ea2a0c9ff7f3fbfa445ae2ae7300200a
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2015-01-31 12:03:40 +0000

    Add trimws().
    git-svn-id: https://svn.r-project.org/R/trunk@67678 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/strwrap.R
+++ src/library/base/R/strwrap.R
@@ -197,3 +197,15 @@ function(x, y, style = c("table", "list"),
     }
     r
 }
+
+trimws <-
+function(x, which = c("both", "left", "right"))
+{
+    which <- match.arg(which)
+    mysub <- function(re, x) sub(re, "", x, perl = TRUE)
+    if(which == "left")
+        return(mysub("^[ \t\r\n]+", x))
+    if(which == "right")
+        return(mysub("[ \t\r\n]+$", x))
+    mysub("[ \t\r\n]+$", mysub("^[ \t\r\n]+", x))
+}
