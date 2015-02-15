fe33b9b74c2622f7cda451cfd864652bc5b388ec
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2015-02-15 15:05:45 +0000

    port non-test part of r67822 from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-3-1-branch@67823 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/utils/R/glob2rx.R
+++ src/library/utils/R/glob2rx.R
@@ -1,7 +1,7 @@
 #  File src/library/utils/R/glob2rx.R
 #  Part of the R package, http://www.R-project.org
 #
-#  Copyright (C) 1995-2012 The R Core Team
+#  Copyright (C) 1995-2015 The R Core Team
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -18,6 +18,9 @@
 
 glob2rx <- function(pattern, trim.head = FALSE, trim.tail = TRUE)
 {
+    ## special case, since paste ignores 0-length inputs (PR#16205)
+    if(!length(pattern)) return(character())
+
     ## Purpose: Change 'ls' aka 'wildcard' aka 'globbing' _pattern_ to
     ##	      Regular Expression (as in grep, perl, emacs, ...)
     ## -------------------------------------------------------------------------
