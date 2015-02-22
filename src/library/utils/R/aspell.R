8e715220c5b03f59f63b4ea82c812df55b931f0e
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2015-02-22 19:27:53 +0000

    Add (internal) convenience function aspell_package().
    git-svn-id: https://svn.r-project.org/R/trunk@67878 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/utils/R/aspell.R
+++ src/library/utils/R/aspell.R
@@ -1079,6 +1079,28 @@ function(dir, ignore = character(),
            dictionaries = dictionaries)
 }
 
+## For spell checking packages.
+
+aspell_package <-
+function(dir,
+         control = list(), program = NULL, dictionaries = character())
+{
+    args <- list(dir = dir,
+                 program = program,
+                 control = control,
+                 dictionaries = dictionaries)
+    a <- rbind(do.call(aspell_package_description, args),
+               do.call(aspell_package_Rd_files, args),
+               do.call(aspell_package_vignettes, args),
+               do.call(aspell_package_R_files, args),
+               do.call(aspell_package_C_files, args))
+    if(nrow(a)) {
+        a$File <- tools:::.file_path_relative_to_dir(a$File,
+                                                     dirname(dir))
+    }
+    a
+}
+
 ## For writing personal dictionaries:
 
 aspell_write_personal_dictionary_file <-
