3e0cbcb591a9ecee86d1b19ea4bb72201f7051f9
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2015-03-20 08:55:30 +0000

    missed by svn merge
    git-svn-id: https://svn.r-project.org/R/branches/R-3-2-branch@68037 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/extra/tzone/localtime.c
+++ src/extra/tzone/localtime.c
@@ -1,6 +1,6 @@
 /*
  *  R : A Computer Language for Statistical Data Analysis
- *  Modifications copyright (C) 2007-2013  The R Core Team
+ *  Modifications copyright (C) 2007-2015  The R Core Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
