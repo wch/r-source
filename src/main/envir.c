da12188057e2155cc178d62edc845aca8c857be4
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2015-04-21 19:21:41 +0000

    Merged r68191 and r68192 (sanity check and object table fixes) from trunk.
    git-svn-id: https://svn.r-project.org/R/branches/R-3-2-branch@68221 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/envir.c
+++ src/main/envir.c
@@ -2588,8 +2588,14 @@ static void FrameValues(SEXP frame, int all, SEXP values, int *indx)
 #undef DO_FrameValues
 #undef NONEMPTY_
 
+#define CHECK_HASH_TABLE(table) do {		\
+	if (TYPEOF(table) != VECSXP)		\
+	    error("bad hash table contents");	\
+    } while (0)
+
 static int HashTableSize(SEXP table, int all)
 {
+    CHECK_HASH_TABLE(table);
     int count = 0;
     int n = length(table);
     int i;
@@ -2600,6 +2606,7 @@ static int HashTableSize(SEXP table, int all)
 
 static void HashTableNames(SEXP table, int all, SEXP names, int *indx)
 {
+    CHECK_HASH_TABLE(table);
     int n = length(table);
     int i;
     for (i = 0; i < n; i++)
@@ -2608,6 +2615,7 @@ static void HashTableNames(SEXP table, int all, SEXP names, int *indx)
 
 static void HashTableValues(SEXP table, int all, SEXP values, int *indx)
 {
+    CHECK_HASH_TABLE(table);
     int n = length(table);
     int i;
     for (i = 0; i < n; i++)
@@ -2716,6 +2724,12 @@ SEXP attribute_hidden do_ls(SEXP call, SEXP op, SEXP args, SEXP rho)
    names and a boolean if sorted is desired */
 SEXP R_lsInternal3(SEXP env, Rboolean all, Rboolean sorted)
 {
+    if(IS_USER_DATABASE(env)) {
+	R_ObjectTable *tb = (R_ObjectTable*)
+	    R_ExternalPtrAddr(HASHTAB(env));
+	return(tb->objects(tb));
+    }
+
     /* Step 1 : Compute the Vector Size */
     int k = 0;
     if (env == R_BaseEnv || env == R_BaseNamespace)
