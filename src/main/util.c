a0536fc70ca57b667b20247445b8c8d83e857f99
murdoch <murdoch@00db46b3-68df-0310-9c12-caf00c1e9a41>
2015-05-02 23:03:50 +0000

    Fix for PR#16358.
    git-svn-id: https://svn.r-project.org/R/trunk@68308 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/util.c
+++ src/main/util.c
@@ -1669,7 +1669,10 @@ double R_strtod5(const char *str, char **endptr, char dec,
 	    case '+': p++;
 	    default: ;
 	    }
-	    for (n = 0; *p >= '0' && *p <= '9'; p++) n = n * 10 + (*p - '0');
+	    /* The test for n is in response to PR#16358; it's not right if the exponent is 
+	       very large, but the overflow or underflow below will handle it. */
+#define MAX_EXPONENT_PREFIX 9999
+	    for (n = 0; *p >= '0' && *p <= '9'; p++) n = (n < MAX_EXPONENT_PREFIX) ? n * 10 + (*p - '0') : n;
 	    if (ans != 0.0) { /* PR#15976:  allow big exponents on 0 */
 		expn += expsign * n;
 		if(exph > 0) expn -= exph;
@@ -1705,7 +1708,7 @@ double R_strtod5(const char *str, char **endptr, char dec,
 	case '+': p++;
 	default: ;
 	}
-	for (n = 0; *p >= '0' && *p <= '9'; p++) n = n * 10 + (*p - '0');
+	for (n = 0; *p >= '0' && *p <= '9'; p++) n = (n < MAX_EXPONENT_PREFIX) ? n * 10 + (*p - '0') : n;
 	expn += expsign * n;
     }
 
