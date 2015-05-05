d8f36f6009faa2a2677f42b9b327e3671383714a
murdoch <murdoch@00db46b3-68df-0310-9c12-caf00c1e9a41>
2015-05-05 23:36:17 +0000

    Add length check (PR#16367)
    git-svn-id: https://svn.r-project.org/R/trunk@68331 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/array.c
+++ src/main/array.c
@@ -1368,6 +1368,8 @@ SEXP attribute_hidden do_colsum(SEXP call, SEXP op, SEXP args, SEXP rho)
     if (p == NA_INTEGER || p < 0)
 	error(_("invalid '%s' argument"), "p");
     if (NaRm == NA_LOGICAL) error(_("invalid '%s' argument"), "na.rm");
+    if (n*p > xlength(x))
+    	error(_("'X' is too short")); /* PR#16367 */
     keepNA = !NaRm;
 
     int OP = PRIMVAL(op);
@@ -1376,7 +1378,7 @@ SEXP attribute_hidden do_colsum(SEXP call, SEXP op, SEXP args, SEXP rho)
     case INTSXP: break;
     case REALSXP: break;
     default:
-	error(_("'x' must be numeric"));
+	error(_("'X' must be numeric"));
     }
 
     if (OP == 0 || OP == 1) { /* columns */
