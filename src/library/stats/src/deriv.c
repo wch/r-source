bf0b33b0f716cbae355db3cd161aea5c908f8a6f
murdoch <murdoch@00db46b3-68df-0310-9c12-caf00c1e9a41>
2015-05-08 12:41:36 +0000

    D("x^2", "x") now gives an error, not NA.
    git-svn-id: https://svn.r-project.org/R/trunk@68345 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/src/deriv.c
+++ src/library/stats/src/deriv.c
@@ -637,6 +637,8 @@ SEXP doD(SEXP args)
     args = CDR(args);
     if (isExpression(CAR(args))) expr = VECTOR_ELT(CAR(args), 0);
     else expr = CAR(args);
+    if (!(isLanguage(expr) || isNumeric(expr) || isComplex(expr)))
+        error(_("'expr' must be an expression or call"));
     var = CADR(args);
     if (!isString(var) || length(var) < 1)
 	error(_("variable must be a character string"));
