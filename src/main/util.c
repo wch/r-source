545d365bd0485e5f0913a7d609c2c21d1f43145a
murdoch <murdoch@00db46b3-68df-0310-9c12-caf00c1e9a41>
2015-06-08 15:06:18 +0000

    Bug fix for bad string comparison following locale change
    git-svn-id: https://svn.r-project.org/R/trunk@68488 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/util.c
+++ src/main/util.c
@@ -2044,6 +2044,7 @@ attribute_hidden
 int Scollate(SEXP a, SEXP b)
 {
     if (!collationLocaleSet) {
+    	int errsv = errno;      /* OSX may set errno in the operations below. */
 	collationLocaleSet = 1;
 #ifndef Win32
 	if (strcmp("C", getLocale()) ) {
@@ -2061,6 +2062,7 @@ int Scollate(SEXP a, SEXP b)
 		error("failed to open ICU collator (%d)", status);
 	    }
 	}
+	errno = errsv;
     }
     if (collator == NULL)
 	return collationLocaleSet == 2 ?
