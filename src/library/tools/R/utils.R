c7be931c22036c8dd58d24c30cb2e864afeab210
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2015-06-09 15:56:33 +0000

    add "sigma" cases from CRAN packages
    git-svn-id: https://svn.r-project.org/R/trunk@68497 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/utils.R
+++ src/library/tools/R/utils.R
@@ -1398,6 +1398,7 @@ nonS3methods <- function(package)
              HyperbolicDist = "log.hist",
              MASS = c("frequency.polygon", "gamma.dispersion", "gamma.shape",
                       "hist.FD", "hist.scott"),
+             LinearizedSVR = "sigma.est",
              ## FIXME: since these are already listed with 'base',
              ##        they should not need to be repeated here:
              Matrix = c("qr.Q", "qr.R", "qr.coef", "qr.fitted",
@@ -1406,9 +1407,11 @@ nonS3methods <- function(package)
              RNetCDF = c("close.nc", "dim.def.nc", "dim.inq.nc",
                          "dim.rename.nc", "open.nc", "print.nc"),
              SMPracticals = "exp.gibbs",
+             TANOVA = "sigma.hat",
+             TeachingDemos = "sigma.test",
              XML = "text.SAX",
              ape = "sort.index",
-             arm = "sigma.hat", # lme4 has sigma()
+             arm = "sigma.hat",
              assist = "chol.new",
              boot = "exp.tilt",
              car = "scatterplot.matrix",
@@ -1418,6 +1421,7 @@ nonS3methods <- function(package)
              crossdes = "all.combn",
              ctv = "update.views",
              deSolve = "plot.1D",
+             elliptic = "sigma.laurent",
              equivalence = "sign.boot",
              fields = c("qr.q2ty", "qr.yq2"),
              gbm = c("pretty.gbm.tree", "quantile.rug"),
@@ -1447,6 +1451,7 @@ nonS3methods <- function(package)
 	     stats = c("anova.lmlist", "expand.model.frame", "fitted.values",
 		       "influence.measures", "lag.plot", "t.test",
                        "plot.spec.phase", "plot.spec.coherency"),
+             stremo = "sigma.hat",
              supclust = c("sign.change", "sign.flip"),
              tensorA = "chol.tensor",
              utils = c("close.socket", "flush.console", "update.packages")
