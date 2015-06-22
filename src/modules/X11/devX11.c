a87e4c215d48a60a35041a46f1f232ad89d715aa
murdoch <murdoch@00db46b3-68df-0310-9c12-caf00c1e9a41>
2015-06-22 17:29:17 +0000

    Protect against using NULL pointers (PR#16438) or uninitialized labels in getGraphicsEvent.
    git-svn-id: https://svn.r-project.org/R/trunk@68569 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/modules/X11/devX11.c
+++ src/modules/X11/devX11.c
@@ -1,7 +1,7 @@
 /*
  *  R : A Computer Language for Statistical Data Analysis
  *  Copyright (C) 1995, 1996  Robert Gentleman and Ross Ihaka
- *  Copyright (C) 1997--2013  The R Core Team
+ *  Copyright (C) 1997--2015  The R Core Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -2598,11 +2598,12 @@ static void X11_eventHelper(pDevDesc dd, int code)
     	R_ProcessX11Events((void*)NULL);	/* discard pending events */
     	if (isEnvironment(dd->eventEnv)) {
     	    SEXP prompt = findVar(install("prompt"), dd->eventEnv);
-    	    if (length(prompt) == 1) {
+    	    if (isString(prompt) && length(prompt) == 1) {
     		 PROTECT(prompt);
     		 XStoreName(display, xd->window, CHAR(asChar(prompt)));
     		 UNPROTECT(1);
-    	    }
+    	    } else 
+    	    	XStoreName(display, xd->window, "");
     	}
     	XSync(display, 1);
     } else if (code == 2) {
