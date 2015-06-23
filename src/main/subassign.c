ec0e1f9423be4ac5a0db2985a6b00f11ae57075c
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2015-06-23 15:07:42 +0000

    Ported r68572 (protect fixes from Tomas Kalibara) from trunk.
    git-svn-id: https://svn.r-project.org/R/branches/R-3-2-branch@68573 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/subassign.c
+++ src/main/subassign.c
@@ -1930,8 +1930,11 @@ SEXP attribute_hidden do_subassign3(SEXP call, SEXP op, SEXP args, SEXP env)
     if(R_DispatchOrEvalSP(call, op, "$<-", args, env, &ans))
       return(ans);
 
-    if (! iS)
+    if (! iS) {
+	PROTECT(ans);
 	nlist = installTrChar(STRING_ELT(input, 0));
+	UNPROTECT(1);
+    }
 
     return R_subassign3_dflt(call, CAR(ans), nlist, CADDR(ans));
 }
