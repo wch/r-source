9e5ccd7977970242a2f854fa24e7ed28b3701442
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2015-07-20 20:26:37 +0000

    Add imports_for_undefined_globals().
    git-svn-id: https://svn.r-project.org/R/trunk@68704 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/utils.R
+++ src/library/tools/R/utils.R
@@ -1167,6 +1167,28 @@ function(pattern, replacement, x, trafo, count, ...)
     x
 }
 
+### imports_for_undefined_globals
+
+imports_for_undefined_globals <-
+function(txt, lst, selective = TRUE)
+{
+    if(!missing(txt))
+        lst <- scan(what = character(), text = txt, quiet = TRUE)
+    lst <- sort(unique(lst))
+    nms <- lapply(lst, find)
+    ind <- sapply(nms, length) > 0L
+    imp <- split(lst[ind], substring(unlist(nms[ind]), 9L))
+    if(selective) {
+        sprintf("importFrom(%s)",
+                vapply(Map(c, names(imp), imp),
+                       function(e)
+                           paste0("\"", e, "\"", collapse = ", "),
+                       ""))
+    } else {
+        sprintf("import(\"%s\")", names(imp))
+    }
+}
+
 ### ** .is_ASCII
 
 .is_ASCII <-
