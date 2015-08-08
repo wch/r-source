dc947afcaa3d19177f47009487405ae923604d08
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2015-08-08 09:45:46 +0000

    use https: for out-of-box default BioC mirror
    git-svn-id: https://svn.r-project.org/R/trunk@68918 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/utils.R
+++ src/library/tools/R/utils.R
@@ -1661,12 +1661,13 @@ function(file)
     db
 }
 
+### default changed to https: for R 3.3.0
 .expand_BioC_repository_URLs <-
 function(x)
 {
     x <- sub("%bm",
              as.character(getOption("BioC_mirror",
-                                    "http://bioconductor.org")),
+                                    "https://bioconductor.org")),
              x, fixed = TRUE)
     sub("%v",
         as.character(.BioC_version_associated_with_R_version()),
