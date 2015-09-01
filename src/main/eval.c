108c900c008b29ff54cb265e6102889dc8294d06
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2015-09-01 18:22:24 +0000

    More protext fixes from Tomas Kalibara.
    git-svn-id: https://svn.r-project.org/R/trunk@69257 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/eval.c
+++ src/main/eval.c
@@ -1511,8 +1511,6 @@ SEXP attribute_hidden do_for(SEXP call, SEXP op, SEXP args, SEXP rho)
     PROTECT(args);
     PROTECT(rho);
     PROTECT(val = eval(val, rho));
-    defineVar(sym, R_NilValue, rho);
-    PROTECT(cell = GET_BINDING_CELL(sym, rho));
 
     /* deal with the case where we are iterating over a factor
        we need to coerce to character - then iterate */
@@ -1530,6 +1528,8 @@ SEXP attribute_hidden do_for(SEXP call, SEXP op, SEXP args, SEXP rho)
 
     val_type = TYPEOF(val);
 
+    defineVar(sym, R_NilValue, rho);
+    PROTECT(cell = GET_BINDING_CELL(sym, rho));
     dbg = RDEBUG(rho);
     bgn = BodyHasBraces(body);
 
@@ -5448,6 +5448,7 @@ static SEXP bcEval(SEXP body, SEXP rho, Rboolean useCache)
 	if (ftype != SPECIALSXP) {
 	  SEXP h = findVar(R_DotsSymbol, rho);
 	  if (TYPEOF(h) == DOTSXP || h == R_NilValue) {
+	    PROTECT(h);
 	    for (; h != R_NilValue; h = CDR(h)) {
 	      SEXP val;
 	      if (ftype == BUILTINSXP) val = eval(CAR(h), rho);
@@ -5455,6 +5456,7 @@ static SEXP bcEval(SEXP body, SEXP rho, Rboolean useCache)
 	      PUSHCALLARG(val);
 	      SETCALLARG_TAG(TAG(h));
 	    }
+	    UNPROTECT(1); /* h */
 	  }
 	  else if (h != R_MissingArg)
 	    error(_("'...' used in an incorrect context"));
