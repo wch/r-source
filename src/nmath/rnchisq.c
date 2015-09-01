96607df604b06e6edbc0757ac2267fd0244b6150
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2015-09-01 07:21:18 +0000

    ported r69237 [ d/rnchisq(*, df=0, ncp)] from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-3-2-branch@69247 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/nmath/rnchisq.c
+++ src/nmath/rnchisq.c
@@ -1,6 +1,6 @@
 /*
  *  Mathlib : A C Library of Special Functions
- *  Copyright (C) 2003 The R Foundation
+ *  Copyright (C) 2003--2015 The R Foundation
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -62,8 +62,7 @@ double rnchisq(double df, double lambda)
 	ML_ERR_return_NAN;
 
     if(lambda == 0.) {
-	if (df == 0.) ML_ERR_return_NAN;
-	return rgamma(df / 2., 2.);
+	return (df == 0.) ? 0. : rgamma(df / 2., 2.);
     }
     else {
 	double r = rpois( lambda / 2.);
