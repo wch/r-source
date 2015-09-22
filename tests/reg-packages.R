3f163d914bc0bf6036dc70e0058b48edaabcd399
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2015-09-22 14:00:30 +0000

    if pkgA is not there, cannot use pkgB either
    git-svn-id: https://svn.r-project.org/R/trunk@69418 00db46b3-68df-0310-9c12-caf00c1e9a41

--- tests/reg-packages.R
+++ tests/reg-packages.R
@@ -90,8 +90,8 @@ pkgApath <- file.path(pkgPath, "pkgA")
 if("pkgA" %in% p.lis && !dir.exists(d <- pkgApath)) {
     cat("symlink 'pkgA' does not exist as directory ",d,"; copying it\n", sep='')
     file.copy(file.path(pkgPath, "xDir", "pkg"), to = d, recursive=TRUE)
-    ## if even the copy failed :
-    if(!dir.exists(d)) p.lis <- p.lis[p.lis != "pkgA"]
+    ## if even the copy failed (NB: pkgB depends on pkgA)
+    if(!dir.exists(d)) p.lis <- p.lis[!(p.lis %in% c("pkgA", "pkgB"))]
 }
 for(p. in p.lis) {
     cat("building package", p., "...\n")
