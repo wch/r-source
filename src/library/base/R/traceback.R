d04596fd54c4a4f88506ea5b8defa281b4bc0035
murdoch <murdoch@00db46b3-68df-0310-9c12-caf00c1e9a41>
2015-10-22 18:07:36 +0000

    Fix traceback() problem reported by Kirill Mueller.
    git-svn-id: https://svn.r-project.org/R/trunk@69553 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/traceback.R
+++ src/library/base/R/traceback.R
@@ -28,23 +28,27 @@ function(x = NULL, max.lines = getOption("deparse.max.lines"))
         cat(gettext("No traceback available"), "\n")
     else {
         for(i in 1L:n) {
+            xi <- x[[i]]
             label <- paste0(n-i+1L, ": ")
-            m <- length(x[[i]])
-            if (!is.null(srcref <- attr(x[[i]], "srcref"))) {
-            	srcfile <- attr(srcref, "srcfile")
-            	x[[i]][m] <- paste0(x[[i]][m], " at ",
-				    basename(srcfile$filename), "#", srcref[1L])
+            m <- length(xi)
+            # Find source location (NULL if not available)
+            srcloc <- if (!is.null(srcref <- attr(xi, "srcref"))) {
+                srcfile <- attr(srcref, "srcfile")
+                paste0(" at ", basename(srcfile$filename), "#", srcref[1L])
+            }
+            # Truncate deparsed code (destroys attributes of xi)
+            if(is.numeric(max.lines) && max.lines > 0L && max.lines < m) {
+                xi <- c(xi[seq_len(max.lines)], " ...")
+                m <- length(xi)
+            }
+            if (!is.null(srcloc)) {
+                xi[m] <- paste0(xi[m], srcloc)
             }
             if(m > 1)
                 label <- c(label, rep(substr("          ", 1L,
                                              nchar(label, type="w")),
                                       m - 1L))
-            if(is.numeric(max.lines) && max.lines > 0L && max.lines < m) {
-                cat(paste0(label[1L:max.lines], x[[i]][1L:max.lines]),
-                    sep = "\n")
-                cat(label[max.lines+1L], " ...\n")
-            } else
-            	cat(paste0(label, x[[i]]), sep="\n")
+            cat(paste0(label, xi), sep="\n")
         }
     }
     invisible(x)
