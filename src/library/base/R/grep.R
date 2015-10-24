2348d966bbcb8e4cda821053bb4190eed1788b81
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2015-10-24 13:49:21 +0000

    regmatches() now can also extract both non-matched and matched parts.
    git-svn-id: https://svn.r-project.org/R/trunk@69569 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/grep.R
+++ src/library/base/R/grep.R
@@ -278,50 +278,76 @@ function(x, m, invert = FALSE)
     ## direct matches give nothing and inverse matches give NA (as
     ## nothing was matched).
 
-    if(!ili && !invert) {
+    if(!ili && identical(invert, FALSE)) {
         so <- m[ind <- (!is.na(m) & (m > -1L))]
         eo <- so + attr(m, "match.length")[ind] - 1L
         return(substring(x[ind], so, eo))
     }
 
-    y <- if(invert) {
+    y <- if(is.na(invert)) {
         Map(function(u, so, ml) {
-            if((n <- length(so)) == 1L) {
-                if(is.na(so) )
-                    return(NA_character_) # Or u ...
-                else if(so == -1L)
-                    return(u)
-            }
-            beg <- if(n > 1L) {
-                ## regexec() could give overlapping matches.
-                ## Matches are non-overlapping iff
-                ##   eo[i] < so[i + 1], i = 1, ..., n - 1.
+                if((n <- length(so)) == 1L) {
+                    if(is.na(so) )
+                        return(NA_character_) # Or u ...
+                    else if(so == -1L)
+                        return(u)
+                }
                 eo <- so + ml - 1L
-                if(any(eo[-n] >= so[-1L]))
-                    stop(gettextf("need non-overlapping matches for %s",
-                                  sQuote("invert = TRUE")),
-                         domain = NA)
-                c(1L, eo + 1L)
-            } else {
-                c(1L, so + ml)
-            }
-            end <- c(so - 1L, nchar(u))
-            substring(u, beg, end)
-        },
+                if(n > 1L) {
+                    ## regexec() could give overlapping matches.
+                    ## Matches are non-overlapping iff
+                    ##   eo[i] < so[i + 1], i = 1, ..., n - 1.
+                    if(any(eo[-n] >= so[-1L]))
+                        stop(gettextf("need non-overlapping matches for %s",
+                                      sQuote("invert = NA")),
+                             domain = NA)
+                }
+                beg <- c(1L, c(rbind(so, eo + 1L)))
+                end <- c(c(rbind(so - 1L, eo)), nchar(u))
+                substring(u, beg, end)
+            },
             x, m,
             if(ili)
-            lapply(m, attr, "match.length")
+                lapply(m, attr, "match.length")
             else
-            attr(m, "match.length"),
+                attr(m, "match.length"),
+            USE.NAMES = FALSE)
+    } else if(invert) {
+        Map(function(u, so, ml) {
+                if((n <- length(so)) == 1L) {
+                    if(is.na(so) )
+                        return(NA_character_) # Or u ...
+                    else if(so == -1L)
+                        return(u)
+                }
+                beg <- if(n > 1L) {
+                    ## See above.
+                    eo <- so + ml - 1L
+                    if(any(eo[-n] >= so[-1L]))
+                        stop(gettextf("need non-overlapping matches for %s",
+                                      sQuote("invert = TRUE")),
+                             domain = NA)
+                    c(1L, eo + 1L)
+                } else {
+                    c(1L, so + ml)
+                }
+                end <- c(so - 1L, nchar(u))
+                substring(u, beg, end)
+            },
+            x, m,
+            if(ili)
+                lapply(m, attr, "match.length")
+            else
+                attr(m, "match.length"),
             USE.NAMES = FALSE)
     } else {
         Map(function(u, so, ml) {
-            if(length(so) == 1L) {
-                if(is.na(so) || (so == -1L))
-                    return(character())
-            }
-            substring(u, so, so + ml - 1L)
-        },
+                if(length(so) == 1L) {
+                    if(is.na(so) || (so == -1L))
+                        return(character())
+                }
+                substring(u, so, so + ml - 1L)
+            },
             x, m,
             lapply(m, attr, "match.length"),
             USE.NAMES = FALSE)
