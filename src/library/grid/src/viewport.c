1efe5e93a90ce87dcbc74d9824d681349f5c8e72
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2015-11-22 15:50:36 +0000

    more missing initializations
    git-svn-id: https://svn.r-project.org/R/trunk@69684 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/grid/src/viewport.c
+++ src/library/grid/src/viewport.c
@@ -208,6 +208,7 @@ void calcViewportTransform(SEXP vp, SEXP parent, Rboolean incremental,
     LViewportContext vpc, parentContext;
     R_GE_gcontext gc, parentgc;
     memset(gc.fontfamily, 0, 201); // initialize for VFontFamilyCode
+    memset(parentgc.fontfamily, 0, 201);
     LTransform thisLocation, thisRotation, thisJustification, thisTransform;
     LTransform tempTransform, parentTransform, transform;
     SEXP currentWidthCM, currentHeightCM, currentRotation;
@@ -239,7 +240,6 @@ void calcViewportTransform(SEXP vp, SEXP parent, Rboolean incremental,
 	parentgc.lineheight = 1.2;
 	parentgc.cex = 1;
 	parentgc.fontface = 1;
-	memset(parentgc.fontfamily, 0, 201);
 	/* The device is not rotated
 	 */
 	parentAngle = 0;
