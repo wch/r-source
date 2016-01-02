7fef22d15d38a7c7ac2121d603a9dd7e82b239dd
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2016-01-02 22:01:46 +0000

    faster tapply()
    git-svn-id: https://svn.r-project.org/R/trunk@69837 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/tapply.R
+++ src/library/base/R/tapply.R
@@ -1,7 +1,7 @@
 #  File src/library/base/R/tapply.R
 #  Part of the R package, https://www.R-project.org
 #
-#  Copyright (C) 1995-2015 The R Core Team
+#  Copyright (C) 1995-2016 The R Core Team
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -20,27 +20,29 @@ tapply <- function (X, INDEX, FUN = NULL, ..., simplify = TRUE)
 {
     FUN <- if (!is.null(FUN)) match.fun(FUN)
     if (!is.list(INDEX)) INDEX <- list(INDEX)
-    nI <- length(INDEX)
+    INDEX <- lapply(INDEX, as.factor)
+    nI <- length(INDEX)  # now, 'INDEX' is not classed
     if (!nI) stop("'INDEX' is of length zero")
-    namelist <- vector("list", nI)
-    names(namelist) <- names(INDEX)
-    extent <- integer(nI)
-    nx <- length(X)
-    one <- 1L
-    group <- rep.int(one, nx) #- to contain the splitting vector
-    ngroup <- one
-    for (i in seq_along(INDEX)) {
-	index <- as.factor(INDEX[[i]])
-	if (length(index) != nx)
-	    stop("arguments must have same length")
-	namelist[[i]] <- levels(index)#- all of them, yes !
-	extent[i] <- nlevels(index)
-	group <- group + ngroup * (as.integer(index) - one)
-	ngroup <- ngroup * nlevels(index)
-    }
+    if (!all(lengths(INDEX) == length(X)))
+        stop("arguments must have same length")
+    namelist <- lapply(INDEX, levels)#- all of them, yes !
+    extent <- lengths(namelist, use.names = FALSE)
+    cumextent <- cumprod(extent)
+    if (cumextent[nI] > .Machine$integer.max)
+        stop("total number of levels >= 2^31")
+    storage.mode(cumextent) <- "integer"
+    ngroup <- cumextent[nI]
+    group <- as.integer(INDEX[[1L]]) #- to contain the splitting vector
+    if (nI > 1L)
+        for (i in 2L:nI)
+           group <- group + cumextent[i - 1L] * (as.integer(INDEX[[i]]) - 1L)
     if (is.null(FUN)) return(group)
-    ans <- lapply(X = split(X, group), FUN = FUN, ...)
-    index <- as.integer(names(ans))
+    levels(group) <- as.character(seq_len(ngroup))
+    class(group) <- "factor"
+    ans <- split.default(X, group)
+    names(ans) <- NULL
+    index <- as.logical(lengths(ans))  # equivalently, lengths(ans) > 0L
+    ans <- lapply(X = ans[index], FUN = FUN, ...)
     if (simplify && all(lengths(ans) == 1L)) {
 	ansmat <- array(dim = extent, dimnames = namelist)
 	ans <- unlist(ans, recursive = FALSE)
@@ -48,14 +50,8 @@ tapply <- function (X, INDEX, FUN = NULL, ..., simplify = TRUE)
 	ansmat <- array(vector("list", prod(extent)),
 			dim = extent, dimnames = namelist)
     }
-    if(length(index)) {
-        names(ans) <- NULL
-        ansmat[index] <- ans
+    if(length(ans)) {
+	ansmat[index] <- ans
     }
     ansmat
 }
-
-
-
-
-
