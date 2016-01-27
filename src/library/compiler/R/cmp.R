4238c782ee17bccec179ffeaf011f4d1e030640f
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2016-01-27 14:08:18 +0000

    Compiler option fixes from Tomas Kalibera.
    git-svn-id: https://svn.r-project.org/R/trunk@70021 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/compiler/R/cmp.R
+++ src/library/compiler/R/cmp.R
@@ -2812,7 +2812,7 @@ setCompilerOptions <- function(...) {
             stop(gettextf("'%s' is not a valid compiler option", n),
                  domain = NA)
     old <- list()
-    newOptions <- compilerOptions
+    newOptions <- as.list(compilerOptions) # copy options
     for (n in nm) {
         op <- options[[n]]
         switch(n,
@@ -2841,7 +2841,9 @@ setCompilerOptions <- function(...) {
                })
     }
     jitEnabled <- enableJIT(-1)
-    checkCompilerOptions(jitEnabled, newOptions)
+    if (checkCompilerOptions(jitEnabled, newOptions))
+        for(n in names(newOptions)) # commit the new options
+            assign(n, newOptions[[n]], compilerOptions)
     invisible(old)
 }
 
