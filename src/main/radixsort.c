53111c4a2a293c4cfc07bbcd1da7fb4b7744c897
lawrence <lawrence@00db46b3-68df-0310-9c12-caf00c1e9a41>
2016-01-27 17:17:03 +0000

    grouping() supports empty vectors
    git-svn-id: https://svn.r-project.org/R/trunk@70022 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/radixsort.c
+++ src/main/radixsort.c
@@ -1919,12 +1919,16 @@ SEXP attribute_hidden do_radixsort(SEXP call, SEXP op, SEXP args, SEXP rho)
     ustr_alloc = 0;
 
     if (retGrp) {
+        int maxgrpn = NA_INTEGER;
         ngrp = gsngrp[flip];
         setAttrib(ans, install("ends"), x = allocVector(INTSXP, ngrp));
-        INTEGER(x)[0] = gs[flip][0];
-        for (int i = 1; i < ngrp; i++)
-            INTEGER(x)[i] = INTEGER(x)[i - 1] + gs[flip][i];
-        setAttrib(ans, install("maxgrpn"), ScalarInteger(gsmax[flip]));
+        if (ngrp > 0) {
+            INTEGER(x)[0] = gs[flip][0];
+            for (int i = 1; i < ngrp; i++)
+                INTEGER(x)[i] = INTEGER(x)[i - 1] + gs[flip][i];
+            maxgrpn = gsmax[flip];
+        }
+        setAttrib(ans, install("maxgrpn"), ScalarInteger(maxgrpn));
         setAttrib(ans, R_ClassSymbol, mkString("grouping"));
     }
 
