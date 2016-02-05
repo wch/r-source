706a85983bc9ae29ffa51c85dfd75decf285be19
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2016-02-05 12:59:23 +0000

    tweaks to docs on parallel::detectCores
    git-svn-id: https://svn.r-project.org/R/trunk@70104 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/parallel/R/detectCores.R
+++ src/library/parallel/R/detectCores.R
@@ -31,8 +31,8 @@ detectCores <-
             systems <-
                 list(linux =
                      if(logical) "grep processor /proc/cpuinfo 2>/dev/null | wc -l" else "cat /proc/cpuinfo | grep 'cpu cores'| uniq | cut -f2 -d:",
-                     ##  hw.physicalcpu may need > 10.9
-                     darwin = if(logical) "/usr/sbin/sysctl -n hw.ncpu 2>/dev/null" else "/usr/sbin/sysctl -n hw.physicalcpu 2>/dev/null",
+                     ## hw.physicalcpu is not documented for 10.9, but works
+                     darwin = if(logical) "/usr/sbin/sysctl -n hw.logicalcpu 2>/dev/null" else "/usr/sbin/sysctl -n hw.physicalcpu 2>/dev/null",
                      solaris = if(logical) "/usr/sbin/psrinfo -v | grep 'Status of.*processor' | wc -l" else "/bin/kstat -p -m cpu_info | grep :core_id | cut -f2 | uniq | wc -l",
                      freebsd = "/sbin/sysctl -n hw.ncpu 2>/dev/null",
                      openbsd = "/sbin/sysctl -n hw.ncpu 2>/dev/null",
