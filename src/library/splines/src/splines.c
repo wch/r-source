5a1c44337266b2d159e949863459c3282a948e0c
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2016-02-09 07:52:07 +0000

    use attributes no cobs98 is no longer relevant
    git-svn-id: https://svn.r-project.org/R/trunk@70128 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/splines/src/splines.c
+++ src/library/splines/src/splines.c
@@ -239,9 +239,12 @@ const static R_CallMethodDef R_CallDef[] = {
 
 
 void
+#ifdef HAVE_VISIBILITY_ATTRIBUTE
+__attribute__ ((visibility ("default")))
+#endif
 R_init_splines(DllInfo *dll)
 {
     R_registerRoutines(dll, NULL, R_CallDef, NULL, NULL);
     R_useDynamicSymbols(dll, FALSE);
-//    R_forceSymbols(dll, TRUE); // too few to worry about, used in cobs*
+//    R_forceSymbols(dll, TRUE); // too few to worry about
 }
