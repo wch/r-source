55cfb6cce93ee57c86b21a513dbcb4e2c30d0e07
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2016-03-01 10:31:31 +0000

    port r70233-70234,70242 ([dpqr]nbinom(*, size=Inf, mu)) from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-3-2-branch@70255 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/nmath/qnbinom.c
+++ src/nmath/qnbinom.c
@@ -1,8 +1,8 @@
 /*
  *  Mathlib : A C Library of Special Functions
  *  Copyright (C) 1998 Ross Ihaka
- *  Copyright (C) 2000-2014 The R Core Team
- *  Copyright (C) 2005 The R Foundation
+ *  Copyright (C) 2000-2016 The R Core Team
+ *  Copyright (C) 2005-2016 The R Foundation
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -47,8 +47,7 @@
 static double
 do_search(double y, double *z, double p, double n, double pr, double incr)
 {
-    if(*z >= p) {
-			/* search to the left */
+    if(*z >= p) {	/* search to the left */
 	for(;;) {
 	    if(y == 0 ||
 	       (*z = pnbinom(y - incr, n, pr, /*l._t.*/TRUE, /*log_p*/FALSE)) < p)
@@ -57,7 +56,6 @@ do_search(double y, double *z, double p, double n, double pr, double incr)
 	}
     }
     else {		/* search to the right */
-
 	for(;;) {
 	    y = y + incr;
 	    if((*z = pnbinom(y, n, pr, /*l._t.*/TRUE, /*log_p*/FALSE)) >= p)
@@ -82,7 +80,7 @@ double qnbinom(double p, double size, double prob, int lower_tail, int log_p)
     if (prob == 0 && size == 0) return 0;
 
     if (prob <= 0 || prob > 1 || size < 0) ML_ERR_return_NAN;
- 
+
     if (prob == 1 || size == 0) return 0;
 
     R_Q_P01_boundaries(p, 0, ML_POSINF);
@@ -128,6 +126,8 @@ double qnbinom(double p, double size, double prob, int lower_tail, int log_p)
 
 double qnbinom_mu(double p, double size, double mu, int lower_tail, int log_p)
 {
+    if (size == ML_POSINF) // limit case: Poisson
+	return(qpois(p, mu, lower_tail, log_p));
 /* FIXME!  Implement properly!! (not losing accuracy for very large size (prob ~= 1)*/
     return qnbinom(p, size, /* prob = */ size/(size+mu), lower_tail, log_p);
 }
