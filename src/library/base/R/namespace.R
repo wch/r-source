6e17521ccbb262619171717cff41d94c48b0361b
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2016-03-02 11:36:19 +0000

    unloadNamespace() no longer loads & unloads an unloaded namespace - fixing PR#16731
    git-svn-id: https://svn.r-project.org/R/trunk@70262 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/namespace.R
+++ src/library/base/R/namespace.R
@@ -695,37 +695,39 @@ topenv <- function(envir = parent.frame(),
 
 unloadNamespace <- function(ns)
 {
-    ## only used to run .onUnload
-    runHook <- function(hookname, env, ...) {
-	if (!is.null(fun <- env[[hookname]])) {
-            res <- tryCatch(fun(...), error=identity)
-            if (inherits(res, "error")) {
-                warning(gettextf("%s failed in %s() for '%s', details:\n  call: %s\n  error: %s",
-                                 hookname, "unloadNamespace", nsname,
-                                 deparse(conditionCall(res))[1L],
-                                 conditionMessage(res)),
-                        call. = FALSE, domain = NA)
-            }
-        }
+    if (any(ns == loadedNamespaces()) { # not to load & unload
+	## only used to run .onUnload
+	runHook <- function(hookname, env, ...) {
+	    if (!is.null(fun <- env[[hookname]])) {
+		res <- tryCatch(fun(...), error=identity)
+		if (inherits(res, "error")) {
+		    warning(gettextf("%s failed in %s() for '%s', details:\n  call: %s\n  error: %s",
+				     hookname, "unloadNamespace", nsname,
+				     deparse(conditionCall(res))[1L],
+				     conditionMessage(res)),
+			    call. = FALSE, domain = NA)
+		}
+	    }
+	}
+	ns <- asNamespace(ns, base.OK = FALSE)
+	nsname <- getNamespaceName(ns)
+	pos <- match(paste("package", nsname, sep = ":"), search())
+	if (! is.na(pos)) detach(pos = pos)
+	users <- getNamespaceUsers(ns)
+	if (length(users))
+	    stop(gettextf("namespace %s is imported by %s so cannot be unloaded",
+			  sQuote(getNamespaceName(ns)),
+			  paste(sQuote(users), collapse = ", ")),
+		 domain = NA)
+	nspath <- .getNamespaceInfo(ns, "path")
+	hook <- getHook(packageEvent(nsname, "onUnload")) # might be list()
+	for(fun in rev(hook)) try(fun(nsname, nspath))
+	runHook(".onUnload", ns, nspath)
+	.Internal(unregisterNamespace(nsname))
+	if(.isMethodsDispatchOn() && methods:::.hasS4MetaData(ns))
+	    methods::cacheMetaData(ns, FALSE, ns)
+	.Internal(lazyLoadDBflush(paste0(nspath, "/R/", nsname, ".rdb")))
     }
-    ns <- asNamespace(ns, base.OK = FALSE)
-    nsname <- getNamespaceName(ns)
-    pos <- match(paste("package", nsname, sep = ":"), search())
-    if (! is.na(pos)) detach(pos = pos)
-    users <- getNamespaceUsers(ns)
-    if (length(users))
-        stop(gettextf("namespace %s is imported by %s so cannot be unloaded",
-                      sQuote(getNamespaceName(ns)),
-                      paste(sQuote(users), collapse = ", ")),
-             domain = NA)
-    nspath <- .getNamespaceInfo(ns, "path")
-    hook <- getHook(packageEvent(nsname, "onUnload")) # might be list()
-    for(fun in rev(hook)) try(fun(nsname, nspath))
-    runHook(".onUnload", ns, nspath)
-    .Internal(unregisterNamespace(nsname))
-    if(.isMethodsDispatchOn() && methods:::.hasS4MetaData(ns))
-        methods::cacheMetaData(ns, FALSE, ns)
-    .Internal(lazyLoadDBflush(paste0(nspath, "/R/", nsname, ".rdb")))
     invisible()
 }
 
