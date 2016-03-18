cc026d7f5ab40ee8e409a2e23a18edaf53944c0f
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2016-03-18 11:07:15 +0000

    dynGet(): use get0()
    git-svn-id: https://svn.r-project.org/R/trunk@70350 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/formals.R
+++ src/library/base/R/formals.R
@@ -1,7 +1,7 @@
 #  File src/library/base/R/formals.R
 #  Part of the R package, https://www.R-project.org
 #
-#  Copyright (C) 1995-2012 The R Core Team
+#  Copyright (C) 1995-2016 The R Core Team
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -31,6 +31,7 @@ body <- function(fun = sys.function(sys.parent())) {
 alist <- function (...) as.list(sys.call())[-1L]
 
 `body<-` <- function (fun, envir = environment(fun), value) {
+    if(!is.function(fun)) warning("'fun' is not a function") # TODO[2017]: stop()
     if (is.expression(value)) {
         if (length(value) > 1L)
             warning("using the first element of 'value' of type \"expression\"")
@@ -41,6 +42,7 @@ alist <- function (...) as.list(sys.call())[-1L]
 
 `formals<-` <- function (fun, envir = environment(fun), value)
 {
+    if(!is.function(fun)) warning("'fun' is not a function") # TODO[2017]: stop()
     bd <- body(fun)
     as.function(c(value,
                   if(is.null(bd) || is.list(bd)) list(bd) else bd),
