8f9a3e9623c5693dc8297315341400eb99d65612
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2016-03-22 18:07:35 +0000

    Mask differences in infinite recursion errors between AST and byte code interpreters; from Tomas Kalibera.
    git-svn-id: https://svn.r-project.org/R/trunk@70362 00db46b3-68df-0310-9c12-caf00c1e9a41

--- tests/ok-errors.R
+++ tests/ok-errors.R
@@ -15,6 +15,10 @@ vector("list", 2^30+2)
 }
 
 ## bad infinite recursion / on.exit / ... interactions
+##   catch the error to permit different error messages emitted
+##   (handling of infinite recursion is different in the AST interpreter
+##   and the byte-code interpreter)
+
 bar <- function() 1+1
 foo <- function() { on.exit(bar()); foo() }
-foo() # now simple "infinite recursion"
+tryCatch(foo(), error=function(x) TRUE) # now simple "infinite recursion"
