e1e74227fbfd520510d717d1431ade4e6029ea86
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2016-04-05 18:31:18 +0000

    Port c70423 from trunk.
    git-svn-id: https://svn.r-project.org/R/branches/R-3-3-branch@70425 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/utils/R/tar.R
+++ src/library/utils/R/tar.R
@@ -245,7 +245,7 @@ untar2 <- function(tarfile, files = NULL, list = FALSE, exdir = ".",
                             warn1 <- c(warn1, "restoring symbolic link as a file copy")
                    } else {
                        mydir.create(dirname(name))
-                       od <- setwd(dirname(name))
+                       od0 <- setwd(dirname(name))
                        nm <- basename(name)
                        unlink(nm)
                        if(!file.symlink(name2, nm)) { # will give a warning
@@ -255,7 +255,7 @@ untar2 <- function(tarfile, files = NULL, list = FALSE, exdir = ".",
                            else
                                warning(gettextf("failed to copy %s to %s", sQuote(from), sQuote(name)), domain = NA)
                        }
-                       setwd(od)
+                       setwd(od0)
                    }
                 }
             }
