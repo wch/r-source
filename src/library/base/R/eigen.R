4ed3c7302aed099496b84838dbdf8fa161398f47
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2016-04-09 14:25:10 +0000

    faster isSymmetric() in "large, obviously FALSE" case
    git-svn-id: https://svn.r-project.org/R/trunk@70454 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/eigen.R
+++ src/library/base/R/eigen.R
@@ -1,7 +1,7 @@
 #  File src/library/base/R/eigen.R
 #  Part of the R package, https://www.R-project.org
 #
-#  Copyright (C) 1995-2015 The R Core Team
+#  Copyright (C) 1995-2016 The R Core Team
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -24,7 +24,15 @@ isSymmetric.matrix <- function(object, tol = 100*.Machine$double.eps, ...)
     if(!is.matrix(object)) return(FALSE) ## we test for  symmetric *matrix*
     ## cheap pretest: is it square?
     d <- dim(object)
-    if(d[1L] != d[2L]) return(FALSE)
+    if((n <- d[1L]) != d[2L]) return(FALSE)
+    if(n <= 1L) return(TRUE)
+    ## else: square (n x n) matrix, n >= 2 :
+    ## initial tests, fast for large non-symmetric:
+    if(is.character(all.equal(object[1L, 2L],
+			      object[2L, 1L], tolerance = tol))) return(FALSE)
+    for(i in unique(c(1L, 2L, n-1L, n)))
+	if(is.character(all.equal(object[i, ],
+				  object[, i], tolerance = tol))) return(FALSE)
     test <-
         if(is.complex(object))
             all.equal.numeric(object, Conj(t(object)), tolerance = tol, ...)
