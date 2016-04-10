785d9e95c7e47972728418ae09db34eae7f3cba6
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2016-04-10 10:59:02 +0000

    isSymmetric part not yet tested on packages
    git-svn-id: https://svn.r-project.org/R/trunk@70458 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/eigen.R
+++ src/library/base/R/eigen.R
@@ -28,19 +28,11 @@ isSymmetric.matrix <- function(object, tol = 100*.Machine$double.eps, ...)
     if(n <= 1L) return(TRUE)
     ## else: square (n x n) matrix, n >= 2 :
     ## initial tests, fast for large non-symmetric:
-    if(is.complex(object)) {
-        if(is.character(all.equal(object[1L, 2L], Conj(object[2L, 1L]),
-                                  tolerance = tol))) return(FALSE)
-        for(i in unique(c(1L, 2L, n-1L, n)))
-            if(is.character(all.equal(object[i, ], Conj(object[, i]),
-                                      tolerance = tol))) return(FALSE)
-    } else {
-        if(is.character(all.equal(object[1L, 2L], object[2L, 1L],
-                                  tolerance = tol))) return(FALSE)
-        for(i in unique(c(1L, 2L, n-1L, n)))
-            if(is.character(all.equal(object[i, ], object[, i],
-                                      tolerance = tol))) return(FALSE)
-    }
+    if(is.character(all.equal(object[1L, 2L],
+			      object[2L, 1L], tolerance = tol))) return(FALSE)
+    for(i in unique(c(1L, 2L, n-1L, n)))
+	if(is.character(all.equal(object[i, ],
+				  object[, i], tolerance = tol))) return(FALSE)
     test <-
         if(is.complex(object))
             all.equal.numeric(object, Conj(t(object)), tolerance = tol, ...)
