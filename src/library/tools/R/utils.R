49fb720d65545957fa094dc5da31c22fb368f226
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2016-04-16 11:58:54 +0000

    New convenience function Rcmd().
    git-svn-id: https://svn.r-project.org/R/trunk@70491 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/utils.R
+++ src/library/tools/R/utils.R
@@ -2016,6 +2016,17 @@ function(args, msg)
               msg)
 }
 
+### * Miscellania
+
+### ** Rcmd
+
+Rcmd <- function(args, ...)
+{
+    if(.Platform$OS.type == "windows")
+        system2(file.path(R.home("bin"), "Rcmd.exe"), args, ...)
+    else
+        system2(file.path(R.home("bin"), "R"), c("CMD", args), ...)
+}
 
 ### ** pskill
 
