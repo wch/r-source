769af1f162fff41a2b94ba3a91f02d6309c5ddc9
murdoch <murdoch@00db46b3-68df-0310-9c12-caf00c1e9a41>
2016-04-24 23:55:51 +0000

    Port r70543 from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-3-3-branch@70544 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/array.c
+++ src/main/array.c
@@ -1701,7 +1701,7 @@ SEXP attribute_hidden do_diag(SEXP call, SEXP op, SEXP args, SEXP rho)
     if (nc < 0)
 	error(_("invalid 'ncol' value (< 0)"));
     int mn = (nr < nc) ? nr : nc;
-    if (mn > 0 && LENGTH(x) == 0)
+    if (mn > 0 && length(x) == 0)
 	error(_("'x' must have positive length"));
 
 #ifndef LONG_VECTOR_SUPPORT
