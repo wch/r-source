8f73fec3fa60ff2c01a7892aa3412ea1e2794757
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2016-05-30 12:11:07 +0000

    addNA() part of PR#16895
    git-svn-id: https://svn.r-project.org/R/trunk@70684 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/factor.R
+++ src/library/base/R/factor.R
@@ -341,8 +341,9 @@ Summary.ordered <- function(..., na.rm)
 addNA <- function(x, ifany=FALSE)
 {
     if (!is.factor(x)) x <- factor(x)
-    if (ifany & !anyNA(x)) return(x)
+    if (ifany && !anyNA(x)) return(x)
     ll <- levels(x)
     if (!anyNA(ll)) ll <- c(ll, NA)
+    else if (!ifany && !anyNA(x)) return(x)
     factor(x, levels=ll, exclude=NULL)
 }
