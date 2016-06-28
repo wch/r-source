c02b6c0326029b0c87efbba3502c03c07c14d592
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2016-06-28 13:48:59 +0000

    Removed stray REprintf.
    git-svn-id: https://svn.r-project.org/R/trunk@70846 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/eval.c
+++ src/main/eval.c
@@ -1138,7 +1138,6 @@ SEXP applyClosure(SEXP call, SEXP op, SEXP arglist, SEXP rho, SEXP suppliedvars)
     if ((SETJMP(cntxt.cjmpbuf))) {
 	if (! cntxt.jumptarget && /* ignores intermediate jumps for on.exits */
 	    R_ReturnedValue == R_RestartToken) {
-	    REprintf("flag = %d\n", cntxt.callflag);
 	    cntxt.callflag = CTXT_RETURN;  /* turn restart off */
 	    R_ReturnedValue = R_NilValue;  /* remove restart token */
 	    PROTECT(tmp = eval(body, newrho));
