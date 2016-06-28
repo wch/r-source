6a311cad72d8140219ad8fa59c2a912b10b7eaa6
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2016-06-28 12:27:18 +0000

    Ported r70843 (protext fix) from trunk.
    git-svn-id: https://svn.r-project.org/R/branches/R-3-3-branch@70844 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/datetime.c
+++ src/main/datetime.c
@@ -759,9 +759,8 @@ SEXP attribute_hidden do_asPOSIXlt(SEXP call, SEXP op, SEXP args, SEXP env)
     setAttrib(ans, install("tzone"), tzone);
     SEXP nm = getAttrib(x, R_NamesSymbol);
     if(nm != R_NilValue) setAttrib(VECTOR_ELT(ans, 5), R_NamesSymbol, nm);
-    UNPROTECT(6);
-
     if(settz) reset_tz(oldtz);
+    UNPROTECT(6);
     return ans;
 }
 
