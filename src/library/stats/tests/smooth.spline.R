bfcc78b7daf2cabf4c5edd41fd181ee0e2a4e229
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2016-07-29 13:11:40 +0000

    again; replacing outdated version
    git-svn-id: https://svn.r-project.org/R/trunk@71000 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/tests/smooth.spline.R
+++ src/library/stats/tests/smooth.spline.R
@@ -1,8 +1,9 @@
-## moved from ?smooth.spline, quite system-specific.
-y18 <- c(1:3,5,4,7:3,2*(2:5),rep(10,4))
+## partly moved from ../man/smooth.spline.Rd , quite system-specific.
+##-- artificial example
+y18 <- c(1:3, 5, 4, 7:3, 2*(2:5), rep(10, 4))
 xx  <- seq(1,length(y18), len=201)
 s2. <- smooth.spline(y18, cv = TRUE,
-                     control = list(trace=TRUE, tol=1e-6,low= -3,maxit=20))
+                     control = list(trace=TRUE, tol=1e-6, low= -3, maxit=20))
 s2. ## i386-Linux: Df ~= (even! > ) 18 : interpolating -- much smaller PRESS
 ## {others, e.g., may end quite differently!}
 plot(y18)
@@ -19,16 +20,23 @@ e <- try(smooth.spline(y18, spar = -9)) #>> error : .. too small'
 stopifnot(inherits(e, "try-error"))
 
 
-## "extreme" range of spar / lambda --> problem/bug for too large lambda
+## "extreme" range of spar, i.e., 'lambda' directly  (" spar = c(lambda = *) "):
+##  ---------------------  --> problem/bug for too large lambda
 e10 <- c(-20, -10, -7, -4:4, 7, 10)
-rss <- lapply(setNames(10^e10, paste0("lambda = 10^", e10)),
-              function(LAM) smooth.spline(y18, spar = c(lambda = LAM)))
-rss
+(lams <- setNames(10^e10, paste0("lambda = 10^", e10)))
+lamExp <- as.expression(lapply(e10, function(E)
+				substitute(lambda == 10^e, list(e = E))))
+sspl <- lapply(lams, function(LAM) smooth.spline(y18, spar = c(lambda = LAM)))
+sspl
+ssGet  <- function(ch) t(sapply(sspl, `[` , ch))
+ssGet1 <- function(ch)   sapply(sspl, `[[`, ch)
+stopifnot(all.equal(ssGet1("crit"), ssGet1("cv.crit"), tol = 1e-10))# seeing rel.diff = 6.57e-12
+## Interesting:  for really large lambda, solution "diverges" from the straight line
+ssGet(c("lambda", "df", "crit", "pen.crit"))
 
-invisible(lapply(seq_along(rss), function(i) lines(predict(rss[[i]], xx), col=i)))
+plot(y18); lines(predict(s2., xx), lwd = 5, col = adjustcolor(4, 1/4))
+invisible(lapply(seq_along(sspl), function(i) lines(predict(sspl[[i]], xx), col=i)))
 i18 <- 1:18
 abline(lm(y18 ~ i18), col = adjustcolor('tomato',1/2), lwd = 5, lty = 3)
-## --> lambda = 10^10 is clearly wrong
-legend("topleft", as.expression(lapply(e10, function(E)
-				substitute(lambda == 10^e, list(e = E)))),
-       ncol = 2, bty = "n", col = seq_along(rss), lty=1)
+## --> lambda = 10^10 is clearly wrong: a *line* but not the L.S. one
+legend("topleft", lamExp, ncol = 2, bty = "n", col = seq_along(sspl), lty=1)
