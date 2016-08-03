707c0853e0e9a6a2ddaf9e094007aa3d9a62e53e
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2016-08-03 13:13:43 +0000

    improve pmin() and pmax()`s implementation for is.object(.) case
    git-svn-id: https://svn.r-project.org/R/trunk@71023 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/pmax.R
+++ src/library/base/R/pmax.R
@@ -1,7 +1,7 @@
 #  File src/library/base/R/pmax.R
 #  Part of the R package, https://www.R-project.org
 #
-#  Copyright (C) 1995-2012 The R Core Team
+#  Copyright (C) 1995-2016 The R Core Team
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -23,73 +23,85 @@ pmax <- function (..., na.rm = FALSE)
 {
     elts <- list(...)
     if(length(elts) == 0L) stop("no arguments")
-    if(all(vapply(elts, function(x) is.atomic(x) && !is.object(x), NA))) {
-        ## NB: NULL passes is.atomic
-        mmm <- .Internal(pmax(na.rm, ...))
+    i4 <- isS4(elts[[1L]])
+    if(!i4 && all(vapply(elts, function(x) is.atomic(x) && !is.object(x), NA))) {
+	## NB: NULL passes is.atomic
+	mmm <- .Internal(pmax(na.rm, ...))
     } else {
-        mmm <- elts[[1L]]
-        attr(mmm, "dim") <- NULL  # dim<- would drop names
-        has.na <- FALSE
-        for (each in elts[-1L]) {
-            attr(each, "dim") <- NULL
-            l1 <- length(each); l2 <- length(mmm)
-            if(l2 < l1) {
-                if (l2 && l1 %% l2)
-                    warning("an argument will be fractionally recycled")
-                mmm <- rep(mmm, length.out = l1)
-            } else if(l1 && l1 < l2) {
-                if (l2 %% l1)
-                    warning("an argument will be fractionally recycled")
-                each <- rep(each, length.out = l2)
-            }
-            nas <- cbind(is.na(mmm), is.na(each))
-            if(has.na || (has.na <- any(nas))) {
-                mmm[nas[, 1L]] <- each[nas[, 1L]]
-                each[nas[, 2L]] <- mmm[nas[, 2L]]
-            }
-            change <- mmm < each
-            change <- change & !is.na(change)
-            mmm[change] <- each[change]
-            if (has.na && !na.rm) mmm[nas[, 1L] | nas[, 2L]] <- NA
-        }
+	mmm <- as.vector(elts[[1L]]) # attr(mmm, "dim") <- NULL  # dim<- would drop names
+	has.na <- FALSE
+	for (each in elts[-1L]) {
+	    each <- as.vector(each) # attr(each, "dim") <- NULL
+	    l1 <- length(each); l2 <- length(mmm)
+	    if(l2 < l1) {
+		if (l2 && l1 %% l2)
+		    warning("an argument will be fractionally recycled")
+		mmm <- rep(mmm, length.out = l1)
+	    } else if(l1 && l1 < l2) {
+		if (l2 %% l1)
+		    warning("an argument will be fractionally recycled")
+		each <- rep(each, length.out = l2)
+	    }
+	    nas <- cbind(is.na(mmm), is.na(each))
+	    if(has.na || (has.na <- any(nas))) {
+		mmm [nas[, 1L]] <- each[nas[, 1L]]
+		each[nas[, 2L]] <- mmm [nas[, 2L]]
+	    }
+	    change <- mmm < each
+	    change <- change & !is.na(change)
+	    mmm[change] <- each[change]
+	    if (has.na && !na.rm) mmm[nas[, 1L] | nas[, 2L]] <- NA
+	}
     }
-    mostattributes(mmm) <- attributes(elts[[1L]])
-    mmm
+    if(i4) {
+	r <- elts[[1L]] # i.e. a valid S4 object
+	tryCatch({ r[] <- mmm; r }, error = mmm)
+    }
+    else { # !isS4(.) ; to improve for S3 ?
+	mostattributes(mmm) <- attributes(elts[[1L]])
+	mmm
+    }
 }
 
 pmin <- function (..., na.rm = FALSE)
 {
     elts <- list(...)
     if(length(elts) == 0L) stop("no arguments")
-    if(all(vapply(elts, function(x) is.atomic(x) && !is.object(x), NA))) {
-        mmm <- .Internal(pmin(na.rm, ...))
+    i4 <- isS4(elts[[1L]])
+    if(!i4 && all(vapply(elts, function(x) is.atomic(x) && !is.object(x), NA))) {
+	mmm <- .Internal(pmin(na.rm, ...))
     } else {
-        mmm <- elts[[1L]]
-        attr(mmm, "dim") <- NULL  # dim<- would drop names
-        has.na <- FALSE
-        for (each in elts[-1L]) {
-            attr(each, "dim") <- NULL
-            l1 <- length(each); l2 <- length(mmm)
-            if(l2 < l1) {
-                if (l2 && l1 %% l2)
-                    warning("an argument will be fractionally recycled")
-                mmm <- rep(mmm, length.out = l1)
-            } else if(l1 && l1 < l2) {
-                if (l2 %% l1)
-                    warning("an argument will be fractionally recycled")
-                each <- rep(each, length.out = l2)
-            }
-            nas <- cbind(is.na(mmm), is.na(each))
-            if(has.na || (has.na <- any(nas))) {
-                mmm[nas[, 1L]] <- each[nas[, 1L]]
-                each[nas[, 2L]] <- mmm[nas[, 2L]]
-            }
-            change <- mmm > each
-            change <- change & !is.na(change)
-            mmm[change] <- each[change]
-            if (has.na && !na.rm) mmm[nas[, 1L] | nas[, 2L]] <- NA
-        }
+	mmm <- as.vector(elts[[1L]]) # attr(mmm, "dim") <- NULL  # dim<- would drop names
+	has.na <- FALSE
+	for (each in elts[-1L]) {
+	    each <- as.vector(each) # attr(each, "dim") <- NULL
+	    l1 <- length(each); l2 <- length(mmm)
+	    if(l2 < l1) {
+		if (l2 && l1 %% l2)
+		    warning("an argument will be fractionally recycled")
+		mmm <- rep(mmm, length.out = l1)
+	    } else if(l1 && l1 < l2) {
+		if (l2 %% l1)
+		    warning("an argument will be fractionally recycled")
+		each <- rep(each, length.out = l2)
+	    }
+	    nas <- cbind(is.na(mmm), is.na(each))
+	    if(has.na || (has.na <- any(nas))) {
+		mmm [nas[, 1L]] <- each[nas[, 1L]]
+		each[nas[, 2L]] <- mmm [nas[, 2L]]
+	    }
+	    change <- mmm > each
+	    change <- change & !is.na(change)
+	    mmm[change] <- each[change]
+	    if (has.na && !na.rm) mmm[nas[, 1L] | nas[, 2L]] <- NA
+	}
     }
-    mostattributes(mmm) <- attributes(elts[[1L]])
-    mmm
+    if(i4) {
+	r <- elts[[1L]] # i.e. a valid S4 object
+	tryCatch({ r[] <- mmm; r }, error = mmm)
+    }
+    else { # !isS4(.) ; to improve for S3 ?
+	mostattributes(mmm) <- attributes(elts[[1L]])
+	mmm
+    }
 }
