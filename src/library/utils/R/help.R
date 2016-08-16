f91d0d89c771139bf348cc776f680246001d168c
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2016-08-16 15:47:09 +0000

    cosmetic simplifications
    git-svn-id: https://svn.r-project.org/R/trunk@71103 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/utils/R/help.R
+++ src/library/utils/R/help.R
@@ -1,7 +1,7 @@
 #  File src/library/utils/R/help.R
 #  Part of the R package, https://www.R-project.org
 #
-#  Copyright (C) 1995-2015 The R Core Team
+#  Copyright (C) 1995-2016 The R Core Team
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -23,15 +23,14 @@ function(topic, package = NULL, lib.loc = NULL,
          help_type = getOption("help_type"))
 {
     types <- c("text", "html", "pdf")
+    help_type <- if(!length(help_type)) "text"
+		 else match.arg(tolower(help_type), types)
     if(!missing(package)) # Don't check for NULL; may be nonstandard eval
         if(is.name(y <- substitute(package)))
             package <- as.character(y)
-
     ## If no topic was given ...
     if(missing(topic)) {
         if(!is.null(package)) {	# "Help" on package.
-            help_type <- if(!length(help_type)) "text"
-            else match.arg(tolower(help_type), types)
             ## Carter Butts and others misuse 'help(package=)' in startup
             if (interactive() && help_type == "html") {
                 port <- tools::startDynamicHelp(NA)
@@ -69,16 +68,12 @@ function(topic, package = NULL, lib.loc = NULL,
         topic <- stopic
     }
 
-    help_type <- if(!length(help_type)) "text"
-    else match.arg(tolower(help_type), types)
-
     paths <- index.search(topic,
                           find.package(if (is.null(package)) loadedNamespaces() else package,
 			               lib.loc, verbose = verbose))
-    tried_all_packages <- FALSE
-    if(!length(paths)
-       && is.logical(try.all.packages) && !is.na(try.all.packages)
-       && try.all.packages && is.null(package) && is.null(lib.loc)) {
+    try.all.packages <- !length(paths) && is.logical(try.all.packages) &&
+        !is.na(try.all.packages) && try.all.packages && is.null(package) && is.null(lib.loc)
+    if(try.all.packages) {
         ## Try all the remaining packages.
         for(lib in .libPaths()) {
             packages <- .packages(TRUE, lib)
@@ -86,15 +81,12 @@ function(topic, package = NULL, lib.loc = NULL,
             paths <- c(paths, index.search(topic, file.path(lib, packages)))
         }
         paths <- paths[nzchar(paths)]
-        tried_all_packages <- TRUE
     }
 
-    paths <- unique(paths)
-    attributes(paths) <-
-        list(call = match.call(), topic = topic,
-             tried_all_packages = tried_all_packages, type = help_type)
-    class(paths) <- "help_files_with_topic"
-    paths
+    structure(unique(paths),
+	      call = match.call(), topic = topic,
+	      tried_all_packages = try.all.packages, type = help_type,
+	      class = "help_files_with_topic")
 }
 
 print.help_files_with_topic <- function(x, ...)
