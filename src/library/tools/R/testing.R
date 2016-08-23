311670d603d3b99b79e0b6bdc76bdab0da716617
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2016-08-23 17:55:57 +0000

    Add support for IGNORE_RDIFF_{BEGIN,END} tags.
    git-svn-id: https://svn.r-project.org/R/trunk@71136 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/testing.R
+++ src/library/tools/R/testing.R
@@ -158,9 +158,14 @@ Rdiff <- function(from, to, useDiff = FALSE, forEx = FALSE,
         if(length(ll)) txt <- txt[seq_len(max(ll) - 1L)]
         ## remove BATCH footer
         nl <- length(txt)
-        if(nl > 3L && startsWith(txt[nl-2L], "> proc.time()")) txt <- txt[1:(nl-3L)]
+        if(nl > 3L && startsWith(txt[nl-2L], "> proc.time()"))
+            txt <- txt[1:(nl-3L)]
+        ## remove text between IGNORE_RDIFF markers.
+        txt[(cumsum(txt == "> ## IGNORE_RDIFF_BEGIN") >
+             cumsum(txt == "> ## IGNORE_RDIFF_END"))] <- ""
+        ## (Keeps the end markers, but that's ok.)
         if (nullPointers)
-        ## remove pointer addresses from listings
+            ## remove pointer addresses from listings
             txt <- gsub("<(environment|bytecode|pointer|promise): [x[:xdigit:]]+>", "<\\1: 0>", txt)
         ## regularize fancy quotes.  First UTF-8 ones:
         txt <- .canonicalize_quotes(txt)
