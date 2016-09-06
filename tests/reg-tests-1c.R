09e65469765ec8f1b7c61276a121ce5f95c6cb91
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2016-09-06 19:22:31 +0000

    op-consistency for arrays: visually show two "groups" of changes
    git-svn-id: https://svn.r-project.org/R/trunk@71218 00db46b3-68df-0310-9c12-caf00c1e9a41

--- tests/reg-tests-1c.R
+++ tests/reg-tests-1c.R
@@ -1906,22 +1906,13 @@ stopifnot(identical(faN, factor(faN, exclude="c")))
 Lm <- m; storage.mode(Lm) <- "logical"
 Im <- m; storage.mode(Im) <- "integer"
 stopifnot(
-    identical( m, m + 1 ), identical( m,  m + 1 [0]), identical( m,  m + NULL)
-   ,
-    identical(Im, Im+ 1L), identical(Im, Im + 1L[0]), identical(Im, Im + NULL)
-   ,
-    identical(m, m+2:3)
-   ,
-    identical(Im, Im+2:3)
-   ,
-    identical(Lm, m & 1),       identical(Lm, m | 2:3)
-   ,
-    identical(Lm, m & TRUE[0]), identical(Lm, Lm | FALSE[0])
-    ,
-    identical(Lm, m & NULL) # gave Error (*only* place where NULL was not allowed)
-   ,
-    identical(Lm, m > 1), identical(Lm, m > .1[0]), identical(Lm, m > NULL)
-   ,
+    identical( m, m + 1 ), identical( m,  m + 1 [0]), identical( m,  m + NULL),
+    identical(Im, Im+ 1L), identical(Im, Im + 1L[0]), identical(Im, Im + NULL),
+    identical(m, m + 2:3), identical(Im, Im + 2:3),
+    identical(Lm, m & 1),  identical(Lm,  m | 2:3),
+    identical(Lm, m & TRUE[0]), identical(Lm, Lm | FALSE[0]),
+    identical(Lm, m & NULL), # gave Error (*only* place where NULL was not allowed)
+    identical(Lm, m > 1), identical(Lm, m > .1[0]), identical(Lm, m > NULL),
     identical(Lm, m <= 2:3)
 )
 mm <- m[,c(1:2,2:1,2)]
@@ -1933,8 +1924,6 @@ tools::assertError(m == mm)# ... non-conformable arrays
 ## arithmetic, logic, and comparison (relop) -- inconsistency for 1x1 array o <vector >= 2>:
 (m1 <- matrix(1,1,1, dimnames=list("Ro","col")))
 (m2 <- matrix(1,2,1, dimnames=list(c("A","B"),"col")))
-##    col
-## Ro   1
 if(FALSE) { # in the future (~ 2018):
 tools::assertError(m1  + 1:2) ## was [1] 2 3  even w/o warning in R <= 3.3.x
 } else tools::assertWarning(m1v <- m1 + 1:2); stopifnot(identical(m1v, 1+1:2))
