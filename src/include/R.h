199cb3f42d2f3fe91370014d547a109977a87b73
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2016-09-16 08:03:59 +0000

    more Solaris undefs, expand notes
    git-svn-id: https://svn.r-project.org/R/trunk@71267 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/include/R.h
+++ src/include/R.h
@@ -48,15 +48,19 @@
 #  include <math.h>
 # endif 
 /* 
-   math.h   is also included by R_ext/Arith.h, except in C++ code
-   stddef.h is included by R_ext/Memory.h
-   string.h is included by R_ext/RS.h
+   math.h is also included by R_ext/Arith.h, except in C++ code
+   stddef.h (or cstddef) is included by R_ext/Memory.h
+   string.h (or cstring) is included by R_ext/RS.h
    All guarded by NO_C_HEADERS.
 */
 # if defined(__sun)
 /* Solaris' stdlib.h includes a header which defines these (and more) */
+#  undef CS
 #  undef DO
 #  undef DS
+#  undef ES
+#  undef FS
+#  undef GS
 #  undef SO
 #  undef SS
 # endif
