cb03c5cb6d8cde00c576f0c15a8fcac87cd05144
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2016-09-17 09:38:00 +0000

    port r71290 from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-3-3-branch@71291 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/check.R
+++ src/library/tools/R/check.R
@@ -3539,6 +3539,8 @@ setRlibs <-
                              ## ": #warning",
                              ## gcc indents these, igraph has space after #
                              "^ *# *warning",
+                             ## Solaris cc has
+                             "Warning: # *warning",
                              # these are from era of static HTML
                              "missing links?:")
                 ## Warnings spotted by gcc with
