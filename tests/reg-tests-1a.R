99cdf04bc5808a910d1eb39a17e732c9366eba24
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2016-10-10 09:02:35 +0000

    smooth.spline(): use 'low = -3' only on 64-bit
    git-svn-id: https://svn.r-project.org/R/trunk@71483 00db46b3-68df-0310-9c12-caf00c1e9a41

--- tests/reg-tests-1a.R
+++ tests/reg-tests-1a.R
@@ -2472,9 +2472,11 @@ update(pc.cr, ~ . + Rape)
 ## smooth.spline.Rd
 y18 <- c(1:3,5,4,7:3,2*(2:5),rep(10,4))
 xx  <- seq(1,length(y18), len=201)
-s2. <- smooth.spline(y18, cv=TRUE,con=list(trace=TRUE, tol=1e-6,low= -3,maxit=20))
-s2. ## Intel-Linux: Df ~= (even! > ) 18 : interpolating -- much smaller PRESS
+(use.l3 <- (Sys.info()[["machine"]] == "x86_64"))
+## 32 bit: now signals error ("too small ..") when low = -3
 ## {others, e.g., may end quite differently!}
+(s2. <- smooth.spline(y18, cv=TRUE, control =
+         list(trace=TRUE, tol=1e-6, low = if(use.l3) -3 else -2)))
 lines(predict(s2., xx), col = 4)
 mtext(deparse(s2.$call,200), side= 1, line= -1, cex= 0.8, col= 4)
 
