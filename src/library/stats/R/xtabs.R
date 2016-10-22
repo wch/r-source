9c884f4575e21cf02c9264ff3bd4f454fd732feb
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2016-10-22 08:37:07 +0000

    ported r71533,6 [PR#17147: xtabs(~ exclude)] from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-3-3-branch@71560 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/R/xtabs.R
+++ src/library/stats/R/xtabs.R
@@ -1,7 +1,7 @@
 #  File src/library/stats/R/xtabs.R
 #  Part of the R package, https://www.R-project.org
 #
-#  Copyright (C) 1995-2015 The R Core Team
+#  Copyright (C) 1995-2016 The R Core Team
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -58,7 +58,7 @@ xtabs <- function(formula = ~., data = parent.frame(), subset, sparse = FALSE,
     if(!sparse) {
 	x <-
 	    if(is.null(y))
-		do.call("table", by)
+		table(by, dnn = names(by))
 	    else if(NCOL(y) == 1L)
 		tapply(y, by, sum)
 	    else {
