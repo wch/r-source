84ab1d91758ee2e09531aa8731a8377444620315
plummer <plummer@00db46b3-68df-0310-9c12-caf00c1e9a41>
2016-11-14 16:24:32 +0000

    Port r71657 [Clarify license for LGPL headers] from trunk.
    git-svn-id: https://svn.r-project.org/R/branches/R-3-3-branch@71658 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/include/Rdynpriv.h
+++ src/include/Rdynpriv.h
@@ -10,7 +10,7 @@
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
+ *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, a copy is available at
