41e49dedecad0884d55ae6ed16fc00e67c86667b
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2016-12-14 09:17:45 +0000

    report failed http -> https redirections
    git-svn-id: https://svn.r-project.org/R/trunk@71787 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/modules/internet/nanohttp.c
+++ src/modules/internet/nanohttp.c
@@ -1,7 +1,7 @@
 /*
  *  R : A Computer Language for Statistical Data Analysis
  *  Copyright (C) 1998-2001  Daniel Veillard.
- *  Copyright (C) 2001-2015   The R Core Team.
+ *  Copyright (C) 2001-2016   The R Core Team.
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -1474,7 +1474,11 @@ RxmlNanoHTTPMethod(const char *URL, const char *method, const char *input,
 
     if ((ctxt->location != NULL) && (ctxt->returnValue >= 300) &&
         (ctxt->returnValue < 400)) {
-	RxmlMessage(1, _("redirect to: '%s'"), ctxt->location);
+	if(strncmp(ctxt->location, "https://", 8) == 0)
+	    RxmlMessage(2, _("cannot handle https redirection to: '%s'"),
+			ctxt->location);
+	else
+	    RxmlMessage(1, _("redirect to: '%s'"), ctxt->location);
 	while (RxmlNanoHTTPRecv(ctxt)) 
 	    ;  // clang likes this on a separate line
         if (nbRedirects < XML_NANO_HTTP_MAX_REDIR) {
