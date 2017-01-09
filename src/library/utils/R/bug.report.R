9f972330fb530fcb35511cf762b47be2ae464575
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2017-01-09 11:08:10 +0000

    bug.report() tries email addresses in BugReports then Contacts
    git-svn-id: https://svn.r-project.org/R/trunk@71942 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/utils/R/bug.report.R
+++ src/library/utils/R/bug.report.R
@@ -1,7 +1,7 @@
 #  File src/library/utils/R/unix/bug.report.R
 #  Part of the R package, https://www.R-project.org
 #
-#  Copyright (C) 1995-2012 The R Core Team
+#  Copyright (C) 1995-2017 The R Core Team
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -45,11 +45,6 @@ bug.report <- function(subject = "", address,
         browseURL("https://bugs.r-project.org/bugzilla3/index.cgi")
     }
 
-    findEmail <- function(x) {
-        ## extract the part within the first < >: the rest may be invalid.
-        x <- paste(x, collapse = " ") # could be multiple lines
-        sub("[^<]*<([^>]+)>.*", "\\1", x)
-    }
     if (is.null(package)) return(baseR())
 
     DESC <- packageDescription(package, lib.loc)
@@ -62,14 +57,25 @@ bug.report <- function(subject = "", address,
     info <- c(info, "", bug.report.info())
     if(identical(DESC$Priority, "base")) return(baseR())
 
+    findEmail2 <- function(x) {
+        x <- paste(x, collapse = " ") # could be multiple lines
+        if (grepl("mailto:", x))
+            sub(".*mailto:([^ ]+).*", "\\1", x)
+        if (grepl("[^<]*<([^>]+)", x))
+            sub("[^<]*<([^>]+)>.*", "\\1", x)
+        else if (grepl("(^|.* )[^ ]+@[[:alnum:]._]+", x)) # too generous
+            sub("(^|.* )([^ ]+@[[:alnum:]._]+).*", "\\2", x)
+        else NA_character_
+    }
+
     BR <- DESC$BugReports
     if (!is.null(BR) && nzchar(BR)) {
         ## do some basic validity checking!
-        if (grepl("http(,s)://", BR)) {
+        if (grepl("https?://", BR)) {
             writeLines(info)
             cat("\nThis package has a bug submission web page, which we will now attempt\n",
                 "to open.  The information above may be useful in your report. If the web\n",
-                "page doesn't work, you should send email to the maintainer,\n",
+                "page does not work, you should send email to the maintainer,\n",
                 DESC$Maintainer, ".\n",
                 sep = "")
             flush.console()
@@ -77,15 +83,40 @@ bug.report <- function(subject = "", address,
             browseURL(DESC$BugReports)
             return(invisible())
         } else {
-            cat("\nThis package has a BugReports field which is not the URL of a web page:\n\n",
-                "  BugReports: ", BR,
-                "\n\nWe will ignore it and email the maintainer.\n\n", sep = "")
+            cat("This package has a BugReports field which is not the URL of a web page:\n\n",
+                "  BugReports: ", BR, "\n\n", sep = "")
+            em <- findEmail2(BR)
+            if (!is.na(em)) {
+                cat("It appears to contain an email address, so we will try that.\n\n")
+                address <- em
+            } else cat("We will ignore it and email the maintainer.\n\n")
             flush.console()
             Sys.sleep(2)
        }
     }
 
-    if (missing(address)) address <- findEmail(DESC$Maintainer)
+    CT <- DESC$Contact
+    if (!is.null(CT) && nzchar(CT)) {
+        cat("This package has a Contact field:\n\n",
+            "  Contact: ", CT, "\n\n", sep = "")
+        em <- findEmail2(CT)
+        if (!is.na(em)) {
+            cat("That appears to contain an email address, so we will try that\n")
+            address <- em
+        } else cat("We cannot make sense of that, so will ignore it.\n\n");
+        flush.console()
+        Sys.sleep(2)
+    }
+
+    if (missing(address)) {
+        findEmail <- function(x) {
+            ## extract the part within the first < >: the rest may be invalid.
+            x <- paste(x, collapse = " ") # could be multiple lines
+            sub("[^<]*<([^>]+)>.*", "\\1", x)
+        }
+        address <- findEmail(DESC$Maintainer)
+    }
+
     create.post(instructions = c("", "<<insert bug report here>>", rep("", 3)),
                 description = "bug report",
                 subject = subject, address = address,
