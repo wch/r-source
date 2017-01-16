3f9449a79acf1b820ba4683951b7b3ff26115dbf
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2017-01-16 08:05:09 +0000

    back out r71966 as it is causing segfaults
    git-svn-id: https://svn.r-project.org/R/branches/R-3-3-branch@71983 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/src/bandwidths.c
+++ src/library/stats/src/bandwidths.c
@@ -1,7 +1,7 @@
 /*
  *  R : A Computer Language for Statistical Data Analysis
  *  bandwidth.c by W. N. Venables and B. D. Ripley  Copyright (C) 1994-2001
- *  Copyright (C) 2012-2017  The R Core Team
+ *  Copyright (C) 2012-2014  The R Core Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -20,8 +20,6 @@
 
 #include <stdlib.h> //abs
 #include <math.h>
-
-#include <Rmath.h> // M_* constants
 #include <Rinternals.h>
 
 // or include "stats.h"
@@ -30,6 +28,16 @@
 #define _(String) dgettext ("stats", String)
 #else
 #define _(String) (String)
+#endif
+
+#ifndef max
+#  define max(a,b) ((a) > (b) ? (a) : (b))
+#  define min(a,b) ((a) < (b) ? (a) : (b))
+#endif
+
+
+#if !defined(PI)
+#  define PI 3.14159265
 #endif
 
 #define DELMAX 1000
@@ -48,8 +56,7 @@ SEXP bw_ucv(SEXP sn, SEXP sd, SEXP cnt, SEXP sh)
 	term = exp(-delta / 4) - sqrt(8.0) * exp(-delta / 2);
 	sum += term * x[i];
     }
-    u = (0.5 + sum) / (n * h * M_SQRT_PI);
-    // = 1 / (2 * n * h * sqrt(PI)) + sum / (n * n * h * sqrt(PI));
+    u = 1 / (2 * n * h * sqrt(PI)) + sum / (n * n * h * sqrt(PI));
     return ScalarReal(u);
 }
 
@@ -65,60 +72,42 @@ SEXP bw_bcv(SEXP sn, SEXP sd, SEXP cnt, SEXP sh)
 	term = exp(-delta / 4) * (delta * delta - 12 * delta + 12);
 	sum += term * x[i];
     }
-    u = (1 + sum/(32*n)) / (2 * n * h * M_SQRT_PI);
-    // = 1 / (2 * n * h * sqrt(PI)) + sum / (64 * n * n * h * sqrt(PI));
+    u = 1 / (2 * n * h * sqrt(PI)) + sum / (64 * n * n * h * sqrt(PI));
     return ScalarReal(u);
 }
 
 SEXP bw_phi4(SEXP sn, SEXP sd, SEXP cnt, SEXP sh)
 {
     double h = asReal(sh), d = asReal(sd), sum = 0.0, term, u;
-    int n = asInteger(sn), nbin = LENGTH(cnt);
+    int n = asInteger(sn), nbin = LENGTH(cnt), *x = INTEGER(cnt);
 
-#define PHI4_SUM							\
-    for (int i = 0; i < nbin; i++) {					\
-	double delta = i * d / h; delta *= delta;			\
-	if (delta >= DELMAX) break;					\
-	term = exp(-delta / 2) * ((delta - 6)* delta + 3);		\
-	sum += term * x[i];						\
-    }
-    if(TYPEOF(cnt) == INTSXP) {
-	int* x = INTEGER(cnt);
-	PHI4_SUM;
-    } else { // TYPEOF(cnt) == REALSXP
-	double* x = REAL(cnt);
-	PHI4_SUM;
+    for (int i = 0; i < nbin; i++) {
+	double delta = i * d / h; delta *= delta;
+	if (delta >= DELMAX) break;
+	term = exp(-delta / 2) * (delta * delta - 6 * delta + 3);
+	sum += term * x[i];
     }
     sum = 2 * sum + n * 3;	/* add in diagonal */
-    u = sum / (n * ((double)(n - 1)) * pow(h, 5.0)) * M_1_SQRT_2PI;
+    u = sum / (n * (n - 1) * pow(h, 5.0) * sqrt(2 * PI));
     return ScalarReal(u);
 }
-#undef PHI4_SUM
 
 SEXP bw_phi6(SEXP sn, SEXP sd, SEXP cnt, SEXP sh)
 {
     double h = asReal(sh), d = asReal(sd), sum = 0.0, term, u;
-    int n = asInteger(sn), nbin = LENGTH(cnt);
-#define PHI6_SUM							\
-    for (int i = 0; i < nbin; i++) {					\
-	double delta = i * d / h; delta *= delta;			\
-	if (delta >= DELMAX) break;					\
-	term = exp(-delta / 2) *					\
-	    (((delta - 15) * delta + 45) * delta - 15);			\
-	sum += term * x[i];						\
-    }
-    if(TYPEOF(cnt) == INTSXP) {
-	int* x = INTEGER(cnt);
-	PHI6_SUM;
-    } else { // TYPEOF(cnt) == REALSXP
-	double* x = REAL(cnt);
-	PHI6_SUM;
+    int n = asInteger(sn), nbin = LENGTH(cnt), *x = INTEGER(cnt);
+
+    for (int i = 0; i < nbin; i++) {
+	double delta = i * d / h; delta *= delta;
+	if (delta >= DELMAX) break;
+	term = exp(-delta / 2) *
+	    (delta * delta * delta - 15 * delta * delta + 45 * delta - 15);
+	sum += term * x[i];
     }
     sum = 2 * sum - 15 * n;	/* add in diagonal */
-    u = sum / (n * ((double)(n - 1)) * pow(h, 7.0)) * M_1_SQRT_2PI;
+    u = sum / (n * (n - 1) * pow(h, 7.0) * sqrt(2 * PI));
     return ScalarReal(u);
 }
-#undef PHI6_SUM
 
 /* This would be impracticable for long vectors.  Better to bin x first */
 SEXP bw_den(SEXP nbin, SEXP sx)
@@ -130,35 +119,24 @@ SEXP bw_den(SEXP nbin, SEXP sx)
     for (int i = 0; i < n; i++) {
 	if(!R_FINITE(x[i]))
 	    error(_("non-finite x[%d] in bandwidth calculation"), i+1);
-	if     (x[i] < xmin) xmin = x[i];
-	else if(x[i] > xmax) xmax = x[i];
+	xmin = min(xmin, x[i]);
+	xmax = max(xmax, x[i]);
     }
     rang = (xmax - xmin) * 1.01;
     dd = rang / nb;
 
-    Rboolean n_lrg = n >= 65537; // <=> n(n-1)/2 > 2^31 - 1
     SEXP ans = PROTECT(allocVector(VECSXP, 2)),
-	sc = SET_VECTOR_ELT(ans, 1, allocVector(n_lrg ? REALSXP : INTSXP, nb));
+	sc = SET_VECTOR_ELT(ans, 1, allocVector(INTSXP, nb));
     SET_VECTOR_ELT(ans, 0, ScalarReal(dd));
+    int *cnt = INTEGER(sc);
+    for (int i = 0; i < nb; i++) cnt[i] = 0;
 
-#define DO_CNT_I_J							\
-    for (int i = 0; i < nb; i++) cnt[i] = 0;				\
-									\
-    /* -> O(n^2) ; sum(cnt[]) == n(n-1)/2  => can overflow if(n_lrg) */ \
-    for (int i = 1; i < n; i++) {					\
-	int ii = (int)(x[i] / dd);					\
-	for (int j = 0; j < i; j++) {					\
-	    int jj = (int)(x[j] / dd);					\
-	    cnt[abs(ii - jj)]++;					\
-	}								\
-    }
-
-    if(n_lrg) {
-	double *cnt = REAL(sc);
-	DO_CNT_I_J
-    } else {
-	int *cnt = INTEGER(sc);
-	DO_CNT_I_J
+    for (int i = 1; i < n; i++) {
+	int ii = (int)(x[i] / dd);
+	for (int j = 0; j < i; j++) {
+	    int jj = (int)(x[j] / dd);
+	    cnt[abs(ii - jj)]++;
+	}
     }
 
     UNPROTECT(1);
