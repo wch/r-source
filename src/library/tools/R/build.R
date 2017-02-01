60b0c660e1e0266193e5e53e64d7671962e49ba5
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2017-02-01 14:31:27 +0000

    update copyright dates
    git-svn-id: https://svn.r-project.org/R/branches/R-3-3-branch@72072 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/build.R
+++ src/library/tools/R/build.R
@@ -789,7 +789,7 @@ get_exclude_patterns <- function()
                 R.version[["major"]], ".",  R.version[["minor"]],
                 " (r", R.version[["svn rev"]], ")\n", sep = "")
             cat("",
-                "Copyright (C) 1997-2013 The R Core Team.",
+                "Copyright (C) 1997-2016 The R Core Team.",
                 "This is free software; see the GNU General Public License version 2",
                 "or later for copying conditions.  There is NO warranty.",
                 sep = "\n")
