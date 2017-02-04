c2152968c88efd56e4730f834bc26d0824b4c25c
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2017-02-04 07:03:26 +0000

    assume more POSIX-compliant getrlimit
    git-svn-id: https://svn.r-project.org/R/trunk@72101 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/unix/system.c
+++ src/unix/system.c
@@ -1,7 +1,7 @@
 /*
  *  R : A Computer Language for Statistical Data Analysis
  *  Copyright (C) 1995, 1996  Robert Gentleman and Ross Ihaka
- *  Copyright (C) 1997--2015  The R Core Team
+ *  Copyright (C) 1997--2017  The R Core Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -126,7 +126,10 @@ void R_setupHistory()
 }
 
 #if defined(HAVE_SYS_RESOURCE_H) && defined(HAVE_GETRLIMIT)
-/* on macOS it seems sys/resource.h needs sys/time.h first */
+/*
+  Needed by AIX and formerly by macOS (but not by POSIX).
+  http://www.ibm.com/support/knowledgecenter/ssw_aix_61/com.ibm.aix.basetrf1/getrlimit_64.htm
+ */
 # ifdef HAVE_SYS_TIME_H
 #  include <sys/time.h>
 # endif
@@ -178,6 +181,9 @@ int Rf_initialize_R(int ac, char **av)
 
 #if defined(HAVE_SYS_RESOURCE_H) && defined(HAVE_GETRLIMIT)
 {
+    /* getrlimit is POSIX:
+       http://pubs.opengroup.org/onlinepubs/9699919799/functions/getrlimit.html
+    */
     struct rlimit rlim;
 
     {
@@ -188,10 +194,29 @@ int Rf_initialize_R(int ac, char **av)
     }
 
     if(getrlimit(RLIMIT_STACK, &rlim) == 0) {
-	unsigned long lim1, lim2;
-	lim1 = (unsigned long) rlim.rlim_cur;
-	lim2 = (unsigned long) rlim.rlim_max; /* Usually unlimited */
-	R_CStackLimit = lim1 < lim2 ? lim1 : lim2;
+	/* 'unlimited' is represented by RLIM_INFINITY, which is a
+	   very large (but maybe not the largest) representable value.
+
+	   The standard allows the values RLIM_SAVED_CUR and
+	   RLIB_SAVED_MAX, apparently used on 32-bit AIX.  
+	   (http://www.ibm.com/support/knowledgecenter/ssw_aix_61/com.ibm.aix.basetrf1/getrlimit_64.htm)
+
+	   These may or may not be different from RLIM_INFINITY (they
+	   are the same on Linux and macOS but not Solaris where they
+	   are larger).  We will assume that unrepresentable limits
+	   are very large.
+
+	   This is exceedingly cautious: one would expect cur <= max
+	   and it is extremely unlikely that the soft limit is either
+	   unlimited or unrepresentable.
+	*/
+	rlim_t lim1 = rlim.rlim_cur, lim2 = rlim.rlim_max;
+	if (lim1 == RLIM_SAVED_CUR || lim1 == RLIM_SAVED_MAX) 
+	    lim1 = RLIM_INFINITY;
+	if (lim2 == RLIM_SAVED_CUR || lim2 == RLIM_SAVED_MAX) 
+	    lim2 = RLIM_INFINITY;
+	rlim_t lim = lim1 < lim2 ? lim1 : lim2;
+	if (lim != RLIM_INFINITY) R_CStackLimit = (uintptr_t) lim;
     }
 #if defined(HAVE_LIBC_STACK_END)
     R_CStackStart = (uintptr_t) __libc_stack_end;
@@ -467,20 +492,26 @@ int R_EditFiles(int nfile, const char **file, const char **title,
     return 1;
 }
 
-/* Returns the limit on the number of open files. On error or when no limit is known,
-   returns a negative number. */
+/* Returns the limit on the number of open files. On error or when no
+   limit is known, returns a negative number. */
 int R_GetFDLimit() {
 
 #if defined(HAVE_SYS_RESOURCE_H) && defined(HAVE_GETRLIMIT)
     struct rlimit rlim;
+    /* Historically this was RLIMIT_OFILE on BSD, but we require the
+       POSIX version.
+
+       Note that 'unlimited' here probably does not mean it:
+       e.g. there is a kernel limit of OPEN_MAX on macOS.
+    */
     if (getrlimit(RLIMIT_NOFILE, &rlim) == 0) {
-	/* Unlimited number is represented by RLIM_INFINITY, which is larger than any
-	   other limit value. */
-	unsigned long lim1, lim2, lim;
-	lim1 = (unsigned long) rlim.rlim_cur;
-	lim2 = (unsigned long) rlim.rlim_max; 
-	lim = lim1 < lim2 ? lim1 : lim2;
-	return (lim > INT_MAX) ? INT_MAX : lim;
+	rlim_t lim1 = rlim.rlim_cur, lim2 = rlim.rlim_max;
+	if (lim1 == RLIM_SAVED_CUR || lim1 == RLIM_SAVED_MAX) 
+	    lim1 = RLIM_INFINITY;
+	if (lim2 == RLIM_SAVED_CUR || lim2 == RLIM_SAVED_MAX) 
+	    lim2 = RLIM_INFINITY;
+	rlim_t lim = lim1 < lim2 ? lim1 : lim2;
+	return (int)((lim > INT_MAX) ? INT_MAX : lim);
     }
 #endif
     return -1;
