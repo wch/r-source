77eae7f0a8affbd6acb489eabc2ec43946eaabf3
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2017-02-17 10:43:29 +0000

    make non-zero statuses an error in download.file()
    git-svn-id: https://svn.r-project.org/R/trunk@72200 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/utils/R/unix/download.file.R
+++ src/library/utils/R/unix/download.file.R
@@ -54,6 +54,7 @@ download.file <-
 				      paste(extra, collapse = " "),
 				      shQuote(url),
 				      "-O", shQuote(path.expand(destfile))))
+               if(status) stop("'wget' call had nonzero exit status")
 	   },
 	   "curl" = {
 	       if(length(url) != 1L || typeof(url) != "character")
@@ -66,6 +67,7 @@ download.file <-
 				      paste(extra, collapse = " "),
 				      shQuote(url),
 				      " -o", shQuote(path.expand(destfile))))
+               if(status) stop("'curl' call had nonzero exit status")
 	   },
 	   "lynx" =
 	       stop("method 'lynx' is defunct", domain = NA))
