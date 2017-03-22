d60c742aa8acc764874db87e3c748e27986e1134
morgan <morgan@00db46b3-68df-0310-9c12-caf00c1e9a41>
2017-03-22 10:17:24 +0000

    do_length() missing PROTECT()
    - ans unprotected when coerceVector() called
    
    
    git-svn-id: https://svn.r-project.org/R/trunk@72383 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/array.c
+++ src/main/array.c
@@ -436,8 +436,12 @@ SEXP attribute_hidden do_length(SEXP call, SEXP op, SEXP args, SEXP rho)
        DispatchOrEval(call, op, "length", args, rho, &ans, 0, 1)) {
 	if (length(ans) == 1 && TYPEOF(ans) == REALSXP) {
 	    double d = REAL(ans)[0];
-	    if (R_FINITE(d) && d >= 0. && d <= INT_MAX && floor(d) == d)
-		return coerceVector(ans, INTSXP);
+	    if (R_FINITE(d) && d >= 0. && d <= INT_MAX && floor(d) == d) {
+                PROTECT(ans);
+                ans = coerceVector(ans, INTSXP);
+                UNPROTECT(1);
+                return(ans);
+            }
 	}
 	return(ans);
     }
