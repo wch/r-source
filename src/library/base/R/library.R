eb44613a45a0451ba28c56202958e0eb219ff9cf
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2017-04-17 17:33:39 +0000

    drop unused 'res' assignment
    git-svn-id: https://svn.r-project.org/R/trunk@72530 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/library.R
+++ src/library/base/R/library.R
@@ -280,15 +280,15 @@ function(package, help, pos = 2, lib.loc = NULL, character.only = FALSE,
                     oldversion <- as.numeric_version(getNamespaceVersion(package))
                     if (newversion != oldversion) {
                     	## No, so try to unload the previous one
-                    	res <- tryCatch(unloadNamespace(package),
-					error = function(e) {
-					    P <- if(!is.null(cc <- conditionCall(e)))
-						     paste("Error in", deparse(cc)[1L], ": ")
-						 else "Error : "
-					    stop(gettextf("Package %s version %s cannot be unloaded:\n %s",
-							  sQuote(package), oldversion,
-							  paste0(P, conditionMessage(e),"\n")),
-						 domain=NA)})
+			tryCatch(unloadNamespace(package),
+				 error = function(e) {
+				     P <- if(!is.null(cc <- conditionCall(e)))
+					      paste("Error in", deparse(cc)[1L], ": ")
+					  else "Error : "
+				     stop(gettextf("Package %s version %s cannot be unloaded:\n %s",
+						   sQuote(package), oldversion,
+						   paste0(P, conditionMessage(e),"\n")),
+					  domain=NA)})
                     }
                 }
 		tt <- tryCatch({
