fc2af555bf08f29638cb133768af83c38bb54c38
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2017-04-24 09:24:10 +0000

    port tweaks from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-3-4-branch@72608 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/modules/internet/libcurl.c
+++ src/modules/internet/libcurl.c
@@ -324,7 +324,10 @@ in_do_curlGetHeaders(SEXP call, SEXP op, SEXP args, SEXP rho)
     CURLcode ret = curl_easy_perform(hnd);
     if (ret != CURLE_OK) {
 	if (errbuf[0]) 
-	    error(_("libcurl error code %d\n\t%s\n"), ret, errbuf);
+	    error(_("libcurl error code %d:\n\t%s\n"), ret, errbuf);
+	else if(ret == 77)
+	    error(_("libcurl error code %d:\n\t%s\n"), ret, 
+		  "unable to access SSL/TLS CA certificates");
 	else // rare case, error but no message
 	    error("libcurl error code %d\n", ret);
     }
