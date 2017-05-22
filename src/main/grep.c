wch/r-source

commit 318e389ca6376cf8193c29150743ce977fd4bc8d

Clean up wcstoutf8 definition and usage
git-svn-id: https://svn.r-project.org/R/trunk@72714 00db46b3-68df-0310-9c12-caf00c1e9a41

src/main/grep.c
@@ -138,21 +138,19 @@ static SEXP mkCharWLen(const wchar_t *wc, int nc)
     R_CheckStack2(sizeof(wchar_t)*(nc+1));
     wt = (wchar_t *) alloca((nc+1)*sizeof(wchar_t));
     wcsncpy(wt, wc, nc); wt[nc] = 0;
-    nb = wcstoutf8(NULL, wt, nc);
-    R_CheckStack2(sizeof(char)*(nb+1));
-    xi = (char *) alloca((nb+1)*sizeof(char));
-    wcstoutf8(xi, wt, nb + 1);
-    if (nb > INT_MAX)
-	error("R character strings are limited to 2^31-1 bytes");
+    nb = wcstoutf8(NULL, wt, INT_MAX);
+    R_CheckStack2(sizeof(char)*nb);
+    xi = (char *) alloca(nb*sizeof(char));
+    wcstoutf8(xi, wt, nb);
     return mkCharLenCE(xi, (int)nb, CE_UTF8);
 }
 
 static SEXP mkCharW(const wchar_t *wc)
 {
-    size_t nb = wcstoutf8(NULL, wc, 0);
-    char *xi = (char *) Calloc(nb+1, char);
+    size_t nb = wcstoutf8(NULL, wc, INT_MAX);
+    char *xi = (char *) Calloc(nb, char);
     SEXP ans;
-    wcstoutf8(xi, wc, nb + 1);
+    wcstoutf8(xi, wc, nb);
     ans = mkCharCE(xi, CE_UTF8);
     Free(xi);
     return ans;
@@ -1593,8 +1591,8 @@ char *pcre_string_adj(char *target, const char *orig, const char *repl,
 			wc = (wchar_t *) alloca((nc+1)*sizeof(wchar_t));
 			utf8towcs(wc, xi, nc + 1);
 			for (j = 0; j < nc; j++) wc[j] = towctrans(wc[j], tr);
-			nb = (int) wcstoutf8(NULL, wc, 0);
-			wcstoutf8(xi, wc, nb + 1);
+			nb = (int) wcstoutf8(NULL, wc, INT_MAX);
+			wcstoutf8(xi, wc, nb);
 			for (j = 0; j < nb; j++) *t++ = *xi++;
 		    }
 		} else

src/gnuwin32/editor.c
@@ -1,6 +1,6 @@
 /*
  *  R : A Computer Language for Statistical Data Analysis
- *  Copyright (C) 1999-2014  The R Core Team
+ *  Copyright (C) 1999-2017  The R Core Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -187,7 +187,7 @@ static void editorsaveas(editor c)
     wname = askfilesaveW(G_("Save script as"), "");
     if (wname) {
 	char name[4*MAX_PATH+1];
-	wcstoutf8(name, wname, MAX_PATH);
+	wcstoutf8(name, wname, sizeof(name));
 	/* now check if it has an extension */
 	char *q = strchr(name, '.');
 	if(!q) strncat(name, ".R", 4*MAX_PATH);
@@ -372,13 +372,13 @@ void menueditornew(control m)
 static void editoropen(const char *default_name)
 {
     wchar_t *wname;
-    char name[4*MAX_PATH], title[4*MAX_PATH];
+    char name[4*MAX_PATH + 1], title[4*MAX_PATH];
 
     int i; textbox t; EditorData p;
     setuserfilterW(L"R files (*.R)\0*.R\0S files (*.q, *.ssc, *.S)\0*.q;*.ssc;*.S\0All files (*.*)\0*.*\0\0");
     wname = askfilenameW(G_("Open script"), default_name); /* returns NULL if open dialog cancelled */
     if (wname) {
-	wcstoutf8(name, wname, MAX_PATH);
+	wcstoutf8(name, wname, sizeof(name));
 	/* check if file is already open in an editor. If so, close and open again */
 	for (i = 0; i < neditors; ++i) {
 	    t = getdata(REditors[i]);

src/gnuwin32/extra.c
@@ -3,7 +3,7 @@
  *  file extra.c
  *  Copyright (C) 1998--2003  Guido Masarotto and Brian Ripley
  *  Copyright (C) 2004	      The R Foundation
- *  Copyright (C) 2005--2015  The R Core Team
+ *  Copyright (C) 2005--2017  The R Core Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -242,7 +242,7 @@ SEXP do_sysinfo(SEXP call, SEXP op, SEXP args, SEXP rho)
 		 LOWORD(osvi.dwBuildNumber), osvi.szCSDVersion);
     SET_STRING_ELT(ans, 2, mkChar(ver));
     GetComputerNameW(name, &namelen);
-    wcstoutf8(buf, name, 1000);
+    wcstoutf8(buf, name, sizeof(buf));
     SET_STRING_ELT(ans, 3, mkCharCE(buf, CE_UTF8));
 #ifdef _WIN64
     SET_STRING_ELT(ans, 4, mkChar("x86-64"));
@@ -250,7 +250,7 @@ SEXP do_sysinfo(SEXP call, SEXP op, SEXP args, SEXP rho)
     SET_STRING_ELT(ans, 4, mkChar("x86"));
 #endif
     GetUserNameW(user, &userlen);
-    wcstoutf8(buf, user, 1000);
+    wcstoutf8(buf, user, sizeof(buf));
     SET_STRING_ELT(ans, 5, mkCharCE(buf, CE_UTF8));
     SET_STRING_ELT(ans, 6, STRING_ELT(ans, 5));
     SET_STRING_ELT(ans, 7, STRING_ELT(ans, 5));
@@ -418,7 +418,7 @@ SEXP do_normalizepath(SEXP call, SEXP op, SEXP args, SEXP rho)
 {
     SEXP ans, paths = CAR(args), el, slash;
     int i, n = LENGTH(paths), res;
-    char tmp[MAX_PATH], longpath[MAX_PATH], *tmp2;
+    char tmp[4*MAX_PATH+1], longpath[4*MAX_PATH+1], *tmp2;
     wchar_t wtmp[32768], wlongpath[32768], *wtmp2;
     int mustWork, fslash = 0;
 
@@ -447,7 +447,7 @@ SEXP do_normalizepath(SEXP call, SEXP op, SEXP args, SEXP rho)
 					wtmp, &wtmp2)) && res <= 32768) {
 		if ((res = GetLongPathNameW(wtmp, wlongpath, 32768))
 		    && res <= 32768) {
-	    	    wcstoutf8(longpath, wlongpath, wcslen(wlongpath)+1);
+	    	    wcstoutf8(longpath, wlongpath, sizeof(longpath));
 		    if(fslash) R_UTF8fixslash(longpath);
 	    	    result = mkCharCE(longpath, CE_UTF8);
 		} else if(mustWork == 1) {
@@ -455,7 +455,7 @@ SEXP do_normalizepath(SEXP call, SEXP op, SEXP args, SEXP rho)
 			      translateChar(el), 
 			      formatError(GetLastError()));	
 	    	} else {
-	    	    wcstoutf8(tmp, wtmp, wcslen(wtmp)+1);
+	    	    wcstoutf8(tmp, wtmp, sizeof(tmp));
 		    if(fslash) R_UTF8fixslash(tmp);
 	    	    result = mkCharCE(tmp, CE_UTF8);
 	    	    warn = 1;
@@ -520,7 +520,7 @@ SEXP in_shortpath(SEXP paths)
 {
     SEXP ans, el;
     int i, n = LENGTH(paths);
-    char tmp[MAX_PATH];
+    char tmp[4*MAX_PATH+1];
     wchar_t wtmp[32768];
     DWORD res;
     const void *vmax = vmaxget();
@@ -533,7 +533,7 @@ SEXP in_shortpath(SEXP paths)
 	if(getCharCE(el) == CE_UTF8) {
 	    res = GetShortPathNameW(filenameToWchar(el, FALSE), wtmp, 32768);
 	    if (res && res <= 32768)
-		wcstoutf8(tmp, wtmp, wcslen(wtmp)+1);
+		wcstoutf8(tmp, wtmp, sizeof(tmp));
 	    else
 		strcpy(tmp, translateChar(el));
 	    /* documented to return paths using \, which the API call does
@@ -825,7 +825,7 @@ SEXP attribute_hidden do_filechoose(SEXP call, SEXP op, SEXP args, SEXP rho)
     fn = askfilenameW(G_("Select file"), "");
     if (!fn)
 	error(_("file choice cancelled"));
-    wcstoutf8(str, fn, 4*MAX_PATH+1);
+    wcstoutf8(str, fn, sizeof(str));
     PROTECT(ans = allocVector(STRSXP, 1));
     SET_STRING_ELT(ans, 0, mkCharCE(str, CE_UTF8));
     UNPROTECT(1);

src/library/utils/src/windows/util.c
@@ -3,7 +3,7 @@
  *  file util.c
  *  Copyright (C) 1998--2003  Guido Masarotto and Brian Ripley
  *  Copyright (C) 2004	      The R Foundation
- *  Copyright (C) 2005--2013  The R Core Team
+ *  Copyright (C) 2005--2017  The R Core Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -248,9 +248,9 @@ SEXP readClipboard(SEXP sformat, SEXP sraw)
 		int n, ienc = CE_NATIVE;
 		const wchar_t *wpc = (wchar_t *) pc;
 		n = wcslen(wpc);
-		char text[2 * (n+1)];  /* UTF-8 is at most 1.5x longer */
+		char text[4*n+1];  
 		R_CheckStack();
-		wcstoutf8(text, wpc, n+1);
+		wcstoutf8(text, wpc, sizeof(text));
 		if(!strIsASCII(text)) ienc = CE_UTF8;
 		ans = splitClipboardText(text, ienc);
 	    } else if (format == CF_TEXT || format == CF_OEMTEXT || format == CF_DIF) {

src/library/utils/src/windows/widgets.c
@@ -163,8 +163,8 @@ static int countFilenamesW(const wchar_t *list)
 
 static SEXP mkCharUTF8(const wchar_t *wc)
 {
-    char s[4*MAX_PATH];
-    wcstoutf8(s, wc, 4*MAX_PATH);
+    char s[4*MAX_PATH + 1];
+    wcstoutf8(s, wc, sizeof(s));
     return mkCharCE(s, CE_UTF8);
 }
 

src/main/character.c
@@ -769,9 +769,9 @@ donewsc:
 		if (iswspace((int)wc[i])) mywcscpy(wc + i, wc + i + 1);
     }
 
-    int nb = (int) wcstoutf8(NULL, wc, 0);
-    char *cbuf = CallocCharBuf(nb+1);
-    wcstoutf8(cbuf, wc, nb + 1);
+    int nb = (int) wcstoutf8(NULL, wc, INT_MAX);
+    char *cbuf = CallocCharBuf(nb);
+    wcstoutf8(cbuf, wc, nb);
     SEXP ans = mkCharCE(cbuf, CE_UTF8);
     Free(cbuf);
     return ans;
@@ -987,9 +987,9 @@ SEXP attribute_hidden do_tolower(SEXP call, SEXP op, SEXP args, SEXP env)
 		    if (ienc == CE_UTF8) {
 			utf8towcs(wc, xi, nc + 1);
 			for (j = 0; j < nc; j++) wc[j] = towctrans(wc[j], tr);
-			nb = (int) wcstoutf8(NULL, wc, 0);
+			nb = (int) wcstoutf8(NULL, wc, INT_MAX);
 			cbuf = CallocCharBuf(nb);
-			wcstoutf8(cbuf, wc, nb + 1);
+			wcstoutf8(cbuf, wc, nb);
 			SET_STRING_ELT(y, i, mkCharCE(cbuf, CE_UTF8));
 		    } else {
 			mbstowcs(wc, xi, nc + 1);
@@ -1426,9 +1426,9 @@ SEXP attribute_hidden do_chartr(SEXP call, SEXP op, SEXP args, SEXP env)
 		    if (tbl) wc[j] = tbl->c_new;
 		}
 		if (ienc == CE_UTF8) {
-		    nb = (int) wcstoutf8(NULL, wc, 0);
+		    nb = (int) wcstoutf8(NULL, wc, INT_MAX);
 		    cbuf = CallocCharBuf(nb);
-		    wcstoutf8(cbuf, wc, nb + 1);
+		    wcstoutf8(cbuf, wc, nb);
 		    SET_STRING_ELT(y, i, mkCharCE(cbuf, CE_UTF8));
 		} else {
 		    nb = (int) wcstombs(NULL, wc, 0);

src/main/gram.y
@@ -2279,7 +2279,7 @@ static SEXP mkStringUTF8(const ucs_t *wcs, int cnt)
 #ifdef WC_NOT_UNICODE
     for(char *ss = s; *wcs; wcs++) ss += ucstoutf8(ss, *wcs);
 #else
-    wcstoutf8(s, wcs, nb);
+    wcstoutf8(s, wcs, sizeof(s));
 #endif
     PROTECT(t = allocVector(STRSXP, 1));
     SET_STRING_ELT(t, 0, mkCharCE(s, CE_UTF8));

commit 327ff6d2e0fbf62298b79d807e159d507f792a74

Fix bug in strsplit introduced in r72714
git-svn-id: https://svn.r-project.org/R/trunk@72716 00db46b3-68df-0310-9c12-caf00c1e9a41

src/main/grep.c
@@ -142,7 +142,7 @@ static SEXP mkCharWLen(const wchar_t *wc, int nc)
     R_CheckStack2(sizeof(char)*nb);
     xi = (char *) alloca(nb*sizeof(char));
     wcstoutf8(xi, wt, nb);
-    return mkCharLenCE(xi, (int)nb, CE_UTF8);
+    return mkCharLenCE(xi, (int)nb-1, CE_UTF8);
 }
 
 static SEXP mkCharW(const wchar_t *wc)