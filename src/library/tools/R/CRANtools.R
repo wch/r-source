2bd0f23241d8ed9758f5d00648fa3e8d4c4fe92d
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2017-05-29 12:55:17 +0000

    Tweaks.
    git-svn-id: https://svn.r-project.org/R/trunk@72744 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/CRANtools.R
+++ src/library/tools/R/CRANtools.R
@@ -132,10 +132,12 @@ function(packages, results = NULL, details = NULL, issues = NULL)
 
     summarize_issues <- function(i) {
         if(!length(i)) return(character())
-        ## No need for hyperrefs: these can be obtained from the package
-        ## check results page already pointed to by summarize_results().
-        sprintf("Additional issues: %s",
-                paste(unique(i$kind), collapse = " "))
+        ## In principle the hyperrefs can be obtained from the package
+        ## check results page already pointed to by summarize_results(),
+        ## but this is not convenient for plain text processing ...
+        paste(c("Additional issues:",
+                sprintf("  %s <%s>", i$kind, i$href)),
+              collapse = "\n")
     }
 
     summarize <- function(p, r, d, i) {
