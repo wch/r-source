ba6665deace4a8dc239aebec977c17d0975fbc27
kalibera <kalibera@00db46b3-68df-0310-9c12-caf00c1e9a41>
2017-05-30 09:37:14 +0000

    Add missing newline after Rprofmem reports with empty R stack trace.
    git-svn-id: https://svn.r-project.org/R/trunk@72747 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/memory.c
+++ src/main/memory.c
@@ -3803,20 +3803,17 @@ static R_size_t R_MemReportingThreshold;
 
 static void R_OutputStackTrace(FILE *file)
 {
-    int newline = 0;
     RCNTXT *cptr;
 
     for (cptr = R_GlobalContext; cptr; cptr = cptr->nextcontext) {
 	if ((cptr->callflag & (CTXT_FUNCTION | CTXT_BUILTIN))
 	    && TYPEOF(cptr->call) == LANGSXP) {
 	    SEXP fun = CAR(cptr->call);
-	    if (!newline) newline = 1;
 	    fprintf(file, "\"%s\" ",
 		    TYPEOF(fun) == SYMSXP ? CHAR(PRINTNAME(fun)) :
 		    "<Anonymous>");
 	}
     }
-    if (newline) fprintf(file, "\n");
 }
 
 static void R_ReportAllocation(R_size_t size)
@@ -3825,6 +3822,7 @@ static void R_ReportAllocation(R_size_t size)
 	if(size > R_MemReportingThreshold) {
 	    fprintf(R_MemReportingOutfile, "%lu :", (unsigned long) size);
 	    R_OutputStackTrace(R_MemReportingOutfile);
+	    fprintf(R_MemReportingOutfile, "\n");
 	}
     }
     return;
@@ -3835,6 +3833,7 @@ static void R_ReportNewPage(void)
     if (R_IsMemReporting) {
 	fprintf(R_MemReportingOutfile, "new page:");
 	R_OutputStackTrace(R_MemReportingOutfile);
+	fprintf(R_MemReportingOutfile, "\n");
     }
     return;
 }
