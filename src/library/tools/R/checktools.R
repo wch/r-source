73fc2478070b8278139d3d7b64bb9fd69d43ccc4
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2017-07-04 08:24:58 +0000

    Allow subscripting check details changes according to changes in severity.
    git-svn-id: https://svn.r-project.org/R/trunk@72885 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/checktools.R
+++ src/library/tools/R/checktools.R
@@ -1144,6 +1144,25 @@ function(new, old, outputs = FALSE)
     db
 }
 
+`[.check_details_changes` <-
+function(x, i, j, drop = FALSE)
+{
+    if(((na <- nargs() - (!missing(drop))) == 3L)
+       && (length(i) == 1L)
+       && any(!is.na(match(i, c("==", "!=", "<", "<=", ">", ">="))))) {
+        levels <- c("", "OK", "NOTE", "WARNING", "ERROR", "FAIL")        
+        encode <- function(s) {
+            s <- sub("\n.*", "", s)
+            s[is.na(match(s, levels))] <- ""
+            ordered(s, levels)
+        }
+        old <- encode(x$Old)
+        new <- encode(x$New)
+        i <- do.call(i, list(old, new))
+    }
+    NextMethod()
+}
+
 format.check_details_changes <-
 function(x, ...)
 {
