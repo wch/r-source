31c253583833791e6b55c48e83591df0ea432c72
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2017-08-01 16:47:36 +0000

    tweak: strrep() saves ~ 10 sec
    git-svn-id: https://svn.r-project.org/R/trunk@73010 00db46b3-68df-0310-9c12-caf00c1e9a41

--- tests/reg-large.R
+++ tests/reg-large.R
@@ -83,15 +83,14 @@ gc() # back to ~ 18 GB
 
 
 ## Large string's encodeString() -- PR#15885
-if(availableGB > 4) system.time({
-    r <- rep("test me:", 53687091)
-    txt <- paste(r, collapse=""); object.size(txt) # 429'496'824 bytes
-    nc <- nchar(txt); nq <- nc*5+8 # is larger than maximal integer:
+if(availableGB > 4) system.time(withAutoprint({
+    txt <- strrep("test me:", 53687091); object.size(txt) # 429'496'824 bytes
+    nc <- nchar(txt); ## NB this is larger than maximal integer:
     nc*5L+8L # NA + Warning   'NAs produced by integer overflow'
     en <- encodeString(txt)
     ## encodeString() seg.faulted in R <= 3.4.1
     stopifnot(identical(txt,en)) # encoding did not change simple ASCII
-})
+}))
 ## 52 sec elapsed [nb-mm4, 8 GB]
 
 
