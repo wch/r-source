db747289b11a9f8c38a5a6f1493276a608c53410
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2017-08-08 07:17:11 +0000

    Improve code and docs for RdMacros packages.
    git-svn-id: https://svn.r-project.org/R/trunk@73061 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/Rd.R
+++ src/library/tools/R/Rd.R
@@ -887,10 +887,15 @@ initialRdMacros <- function(pkglist = NULL,
     	others <- trimws(unlist(strsplit(pkglist, ",")))
 
     	for (p in others) {
-    	    if (dir.exists(system.file("help/macros", package = p)))
+            if((fp <- system.file(package = p)) == "")
+                warning(gettextf("Rd macro package '%s' is not installed.",
+                                 p),
+                        call. = FALSE)
+            else if(dir.exists(file.path(fp, "help", "macros")))
     	    	macros <- loadPkgRdMacros(system.file(package = p), macros)
     	    else
-    	    	warning(gettextf("No Rd macros in package '%s'.", p), call. = FALSE)
+    	    	warning(gettextf("No Rd macros in package '%s'.", p),
+                        call. = FALSE)
         }
     } else if (is.character(macros))
     	macros <- loadRdMacros(file = macros)
