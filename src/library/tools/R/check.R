9be3401581c17e70e5365ad3721f1eb88c2de260
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2017-08-09 07:46:46 +0000

    filter ms_printf only if(WINDOWS)
    git-svn-id: https://svn.r-project.org/R/trunk@73073 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/check.R
+++ src/library/tools/R/check.R
@@ -3974,8 +3974,9 @@ setRlibs <-
                                   lines, invert = TRUE, value = TRUE, useBytes = TRUE)
                     lines <- grep("warning: *ISO C forbids.*function pointer",
                                   lines, invert = TRUE, value = TRUE, useBytes = TRUE)
-                    lines <- grep("warning: *ISO C does not support.*ms_printf length modifier",
-                                  lines, invert = TRUE, value = TRUE, useBytes = TRUE)
+                    if(WINDOWS)
+                        lines <- grep("warning: *ISO C does not support.*ms_printf length modifier",
+                                      lines, invert = TRUE, value = TRUE, useBytes = TRUE)
                 }
 
                 ## Warnings spotted by gcc with
