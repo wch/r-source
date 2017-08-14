2335878476b6a0b9bd448d65c1ff15b83988a053
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2017-08-14 07:48:48 +0000

    Add reader for machine-readable debian/copyright files.
    git-svn-id: https://svn.r-project.org/R/trunk@73091 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/license.R
+++ src/library/tools/R/license.R
@@ -856,3 +856,46 @@ function(com, dir)
 
     0L
 }
+
+read_debian_copyright_file <-
+function(file, keep = TRUE)
+{
+    ## See
+    ## <https://www.debian.org/doc/packaging-manuals/copyright-format/1.0/>.
+    
+    select <- function(x, i, vars, keep = TRUE) {
+        pos <- match(vars, colnames(x), nomatch = 0L)
+        if(keep && any(!pos)) {
+            x <- cbind(x, NA_character_)
+            pos[!pos] <- ncol(x)
+        }
+        y <- x[i, pos, drop = FALSE]
+        colnames(y) <- vars
+        y
+    }
+
+    fields_in_header_para <-
+        c("Format", "Upstream-Name", "Upstream-Contact", "Source",
+          "Disclaimer", "Comment", "License", "Copyright")
+    fields_in_files_para <-
+        c("Files", "Copyright", "License", "Comment")
+    fields_in_license_para <-
+        c("License", "Comment")
+    
+    x <- tryCatch(read.dcf(file, keep.white = TRUE),
+                  error = identity)
+    if(inherits(x, "error") || !length(x)) return()
+
+    header <- drop(select(x, 1L, fields_in_header_para, keep))
+    if(is.na(fmt <- header["Format"]) ||
+       !grepl("copyright-format", fmt))
+        return()
+
+    pos <- which(!is.na(x[, "Files"]))
+
+    list(header = header,
+         files =
+             select(x, pos, fields_in_files_para, keep),
+         licenses =
+             select(x, - c(1L, pos), fields_in_license_para, keep))
+}
