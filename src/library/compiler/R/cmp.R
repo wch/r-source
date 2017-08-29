8847f4d837d0b18cd29bc61d4677f69c242943b7
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2017-08-29 16:15:05 +0000

    Fixed some issues in the compiler identified by codetools.
    git-svn-id: https://svn.r-project.org/R/trunk@73153 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/compiler/R/cmp.R
+++ src/library/compiler/R/cmp.R
@@ -89,7 +89,6 @@ missingArgs <- function(args) {
 frameTypes <- function(env) {
     top <- topenv(env)
     empty <- emptyenv()
-    base <- baseenv()
     nl <- 0
     while (! identical(env, top)) {
         if (isNamespace(env))
@@ -1340,7 +1339,7 @@ tryInline <- function(e, cb, cntxt) {
         if (! is.null(h)) {
             if (info$guard) {
                 tailcall <- cntxt$tailcall
-                if (tailcall) cntxttailcall <- FALSE
+                if (tailcall) cntxt$tailcall <- FALSE
                 expridx <- cb$putconst(e)
                 endlabel <- cb$makelabel()
                 cb$putcode(BASEGUARD.OP, expridx, endlabel)
@@ -2497,7 +2496,6 @@ is.simpleInternal <- function(def) {
 inlineSimpleInternalCall <- function(e, def) {
     if (! dots.or.missing(e) && is.simpleInternal(def)) {
         forms <- formals(def)
-        fnames <- names(forms)
         b <- body(def)
         if (typeof(b) == "language" && length(b) == 2 && b[[1]] == "{")
             b <- b[[2]]
@@ -3024,7 +3022,7 @@ loadcmp <- function (file, envir = .GlobalEnv, chdir = FALSE) {
         setwd(path)
     }
     for (i in exprs) {
-        yy <- eval(i, envir)
+        eval(i, envir)
     }
     invisible()
 }
