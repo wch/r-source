f9745892e6eeec75f799d1dbce641614792856b9
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2017-09-04 16:58:57 +0000

    Added ENSURE_NAMEDMAX function for barrier testing.
    git-svn-id: https://svn.r-project.org/R/trunk@73202 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/memory.c
+++ src/main/memory.c
@@ -3426,6 +3426,8 @@ void SHALLOW_DUPLICATE_ATTRIB(SEXP to, SEXP from) {
     IS_S4_OBJECT(from) ?  SET_S4_OBJECT(to) : UNSET_S4_OBJECT(to);
 }
 
+void (ENSURE_NAMEDMAX)(SEXP x) { ENSURE_NAMEDMAX(CHK(x)); }
+
 /* S4 object testing */
 int (IS_S4_OBJECT)(SEXP x){ return IS_S4_OBJECT(CHK(x)); }
 void (SET_S4_OBJECT)(SEXP x){ SET_S4_OBJECT(CHK(x)); }
