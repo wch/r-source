cc517b2d089ac33c24aadeb90707a1229ef587d0
lawrence <lawrence@00db46b3-68df-0310-9c12-caf00c1e9a41>
2017-09-16 19:25:11 +0000

    fix calling length<-() on NULL, and printing duplicate methods
    git-svn-id: https://svn.r-project.org/R/trunk@73291 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/methods/R/methodsTable.R
+++ src/library/methods/R/methodsTable.R
@@ -1063,12 +1063,10 @@
     ## else: non-empty methods list
     doFun(f,p)
     for(m in mget(labels, table)) {
+        pkgs <- NULL
 	if(is.environment(m)) {  ## duplicate class case -- compare .findMethodInTable()
             pkgs <- names(m)
-            if(length(pkgs) == 1)
-                m <- m[[pkgs]]
-            else if(length(pkgs) > 1)
-                cf("  (", length(pkgs), " methods defined for this signature, with different packages)\n")
+            m <- m[[pkgs[1L]]]
         }
 	if( is(m, "MethodDefinition")) {
 	    t <- m@target
@@ -1083,6 +1081,8 @@
             if(!.identC(m@generic, f) && length(m@generic) == 1L &&
                nzchar(m@generic))
 		cf("    (definition from function \"", m@generic, "\")\n")
+            if(length(pkgs) > 1)
+                cf("  (", length(pkgs), " methods defined for this signature, with different packages)\n")
 	}
 	if(includeDefs && is(m, "function")) {
 	    if(is(m, "MethodDefinition"))
@@ -1243,14 +1243,18 @@ outerLabels <- function(labels, new) {
       sig <- c(as.character(sig), rep("ANY", more))
   }
   else if(n > nargs) { #reset table?
-    if(all(sig[(nargs+1):n] == "ANY"))
-      length(sig) <- length(pkgs) <- nargs
-    else {
+    if(all(sig[(nargs+1):n] == "ANY")) {
+        length(sig) <- nargs
+        if (!is.null(pkgs))
+            length(pkgs) <- nargs
+    } else {
       while(sig[[n]] == "ANY")
         n <- n-1
       if(reset)
         .resetSigLength(fdef, n)
-      length(sig) <- length(pkgs) <- n
+      length(sig) <- n
+      if (!is.null(pkgs))
+          length(pkgs) <- n
     }
   }
   packageSlot(sig) <- pkgs
