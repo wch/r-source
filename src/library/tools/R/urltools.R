bc547231116f589f96e8be9767a91379dae1bcd2
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2017-10-06 14:22:50 +0000

    Improve URL checking.
    git-svn-id: https://svn.r-project.org/R/trunk@73478 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/urltools.R
+++ src/library/tools/R/urltools.R
@@ -617,6 +617,18 @@ function(x, ...)
     invisible(x)
 }
 
+as.matrix.check_url_db <-
+function(x, ...)
+{
+    n <- lengths(x[["From"]])
+    y <- do.call(cbind,
+                 c(list(URL = rep.int(x[["URL"]], n),
+                        Parent = unlist(x[["From"]])),
+                   lapply(x[-c(1L, 2L)], rep.int, n)))
+    rownames(y) <- NULL
+    y
+}
+
 .curl_GET_status <-
 function(u, verbose = FALSE)
 {
