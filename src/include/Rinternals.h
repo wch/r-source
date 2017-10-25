d49a8f2e6e1175a8c91bdab7d5501bb86004d2f3
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2017-10-25 16:55:18 +0000

    Defining NO_RINLINEDFUNS disables use of Rinlinedfuns.h to simulate platforms where this is not available.
    git-svn-id: https://svn.r-project.org/R/trunk@73612 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/include/Rinternals.h
+++ src/include/Rinternals.h
@@ -1409,7 +1409,9 @@ void R_orderVector1(int *indx, int n, SEXP x,       Rboolean nalast, Rboolean de
 
 #endif
 
-#if defined(CALLED_FROM_DEFN_H) && !defined(__MAIN__) && (defined(COMPILING_R) || ( __GNUC__ && !defined(__INTEL_COMPILER) ))
+/* Defining NO_RINLINEDFUNS disables use to simulate platforms where
+   this is not available */
+#if defined(CALLED_FROM_DEFN_H) && !defined(__MAIN__) && (defined(COMPILING_R) || ( __GNUC__ && !defined(__INTEL_COMPILER) )) && (defined(COMPILING_R) || !defined(NO_RINLINEDFUNS))
 #include "Rinlinedfuns.h"
 #else
 /* need remapped names here for use with R_NO_REMAP */
