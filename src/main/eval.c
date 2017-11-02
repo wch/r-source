a46559e8f728317da979be60e401899ae60086b2
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2017-11-02 00:01:56 +0000

    Reduce compiler/interpreter discrepancy is pacement of some NAMED adjustments.
    git-svn-id: https://svn.r-project.org/R/trunk@73648 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/eval.c
+++ src/main/eval.c
@@ -5427,12 +5427,15 @@ static R_INLINE Rboolean setElementFromScalar(SEXP vec, R_xlen_t i, int typev,
 	int typev = bcStackScalar(srhs, &v);				\
 	if (setElementFromScalar(vec, i, typev, &v)) {			\
 	    SETSTACK_PTR(sv, vec);					\
+	    if (NAMED(vec) == 1) SET_NAMED(vec, 0);			\
 	    return;							\
 	}								\
-	else if (subassign2 && TYPEOF(vec) == VECSXP && i < XLENGTH(vec)) { \
+	else if (subassign2 && TYPEOF(vec) == VECSXP &&			\
+		 i < XLENGTH(vec)) {					\
 	    SEXP rhs = R_FixupRHS(vec, GETSTACK_PTR(srhs));		\
 	    if (rhs != R_NilValue) {					\
 		SET_VECTOR_ELT(vec, i, rhs);				\
+		if (NAMED(vec) == 1) SET_NAMED(vec, 0);			\
 		SETSTACK_PTR(sv, vec);					\
 		return;							\
 	    }								\
@@ -5451,8 +5454,6 @@ static R_INLINE void VECSUBASSIGN_PTR(R_bcstack_t *sx, R_bcstack_t *srhs,
 	vec = duplicate(vec);
 	SETSTACK_PTR(sx, vec);
     }
-    else if (NAMED(vec) == 1)
-	SET_NAMED(vec, 0);
 
     R_xlen_t i = bcStackIndex(si) - 1;
     if (i >= 0)
@@ -5496,8 +5497,6 @@ static R_INLINE void MATSUBASSIGN_PTR(R_bcstack_t *sx, R_bcstack_t *srhs,
 	mat = duplicate(mat);
 	SETSTACK_PTR(sx, mat);
     }
-    else if (NAMED(mat) == 1)
-	SET_NAMED(mat, 0);
 
     dim = getMatrixDim(mat);
 
@@ -5553,8 +5552,6 @@ static R_INLINE void SUBASSIGN_N_PTR(R_bcstack_t *sx, int rank,
 	x = duplicate(x);
 	SETSTACK_PTR(sx, x);
     }
-    else if (NAMED(x) == 1)
-	SET_NAMED(x, 0);
 
     dim = getArrayDim(x);
 
