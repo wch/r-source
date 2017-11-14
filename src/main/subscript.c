ca02c8e4c73bf558fb89df81f13d6da7a6e57134
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2017-11-14 08:50:14 +0000

    More use of read-only vector data pointers.
    git-svn-id: https://svn.r-project.org/R/trunk@73729 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/subscript.c
+++ src/main/subscript.c
@@ -361,7 +361,7 @@ SEXP attribute_hidden mat2indsub(SEXP dims, SEXP s, SEXP call)
     R_xlen_t NR = nrs;
     SEXP rvec;
     int ndim = LENGTH(dims);
-    int *pdims = INTEGER(dims);
+    const int *pdims = INTEGER_RO(dims);
 
     if (ncols(s) != ndim) {
 	ECALL(call, _("incorrect number of columns in matrix subscript"));
@@ -379,7 +379,7 @@ SEXP attribute_hidden mat2indsub(SEXP dims, SEXP s, SEXP call)
 	if (TYPEOF(s) == REALSXP) {
 	    for (int i = 0; i < nrs; i++) {
 		R_xlen_t tdim = 1;
-		double *ps = REAL(s);
+		const double *ps = REAL_RO(s);
 		for (int j = 0; j < ndim; j++) {
 		    double k = ps[i + j * NR];
 		    if(ISNAN(k)) {rv[i] = NA_REAL; break;}
@@ -396,7 +396,7 @@ SEXP attribute_hidden mat2indsub(SEXP dims, SEXP s, SEXP call)
 	    }
 	} else {
 	    s = coerceVector(s, INTSXP);
-	    int *ps = INTEGER(s);
+	    const int *ps = INTEGER_RO(s);
 	    for (int i = 0; i < nrs; i++) {
 		R_xlen_t tdim = 1;
 		for (int j = 0; j < ndim; j++) {
@@ -515,11 +515,11 @@ logicalSubscript(SEXP s, R_xlen_t ns, R_xlen_t nx, R_xlen_t *stretch, SEXP call)
     nmax = (ns > nx) ? ns : nx;
     *stretch = (ns > nx) ? ns : 0;
     if (ns == 0) return(allocVector(INTSXP, 0));
-    int *ps = LOGICAL(s);    /* Calling LOCICAL here may force a large
-				allocation, but no larger than the one
-				made by R_alloc below. This could use
-				rewriting to better handle a sparse
-				logical index. */
+    const int *ps = LOGICAL_RO(s);    /* Calling LOCICAL_RO here may force a
+					 large allocation, but no larger than
+					 the one made by R_alloc below. This
+					 could use rewriting to better handle
+					 a sparse logical index. */
 #ifdef LONG_VECTOR_SUPPORT
     if (nmax > R_SHORT_LEN_MAX) {
 	if (ns == nmax) { /* no recycling - use fast single-index code */
@@ -633,7 +633,7 @@ static SEXP negativeSubscript(SEXP s, R_xlen_t ns, R_xlen_t nx, SEXP call)
     int *pindx = LOGICAL(indx);
     for (i = 0; i < nx; i++)
 	pindx[i] = 1;
-    int *ps = INTEGER(s);
+    const int *ps = INTEGER_RO(s);
     for (i = 0; i < ns; i++) {
 	int ix = ps[i];
 	if (ix != 0 && ix != NA_INTEGER && -ix <= nx)
@@ -648,7 +648,7 @@ static SEXP positiveSubscript(SEXP s, R_xlen_t ns, R_xlen_t nx)
 {
     SEXP indx;
     R_xlen_t i, zct = 0;
-    int *ps = INTEGER(s);
+    const int *ps = INTEGER_RO(s);
     for (i = 0; i < ns; i++) if (ps[i] == 0) zct++;
     if (zct) {
 	indx = allocVector(INTSXP, (ns - zct));
@@ -671,7 +671,7 @@ integerSubscript(SEXP s, R_xlen_t ns, R_xlen_t nx, R_xlen_t *stretch, SEXP call)
     *stretch = 0;
     neg = FALSE;
     max = 0;
-    int *ps = INTEGER(s);
+    const int *ps = INTEGER_RO(s);
     for (i = 0; i < ns; i++) {
 	ii = ps[i];
 	if (ii < 0) {
@@ -710,7 +710,7 @@ realSubscript(SEXP s, R_xlen_t ns, R_xlen_t nx, R_xlen_t *stretch, SEXP call)
     *stretch = 0;
     min = 0;
     max = 0;
-    double *ps = REAL(s);
+    const double *ps = REAL_RO(s);
     for (i = 0; i < ns; i++) {
 	ii = ps[i];
 	if (R_FINITE(ii)) {
@@ -835,7 +835,7 @@ stringSubscript(SEXP s, R_xlen_t ns, R_xlen_t nx, SEXP names,
 	/* must be internal, so names contains a character vector */
 	/* NB: this does not behave in the same way with respect to ""
 	   and NA names: they will match */
-	PROTECT(indx = match(names, s, 0));
+	PROTECT(indx = match(names, s, 0)); /**** guaranteed to be fresh???*/
 	/* second pass to correct this */
 	int *pindx = INTEGER(indx);
 	for (i = 0; i < ns; i++)
