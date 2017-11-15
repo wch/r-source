159998b30dd728abc8a0324fff28d3b6ba9d6c02
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2017-11-15 08:49:37 +0000

    Removed stray character.
    git-svn-id: https://svn.r-project.org/R/trunk@73733 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/complex.c
+++ src/main/complex.c
@@ -1,4 +1,4 @@
-x/*
+/*
  *  R : A Computer Language for Statistical Data Analysis
  *  Copyright (C) 1995, 1996, 1997  Robert Gentleman and Ross Ihaka
  *  Copyright (C) 2000-2016	    The R Core Team
