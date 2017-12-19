b3c8510624e6b29a80f75ab765ebc8306c522e1a
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2017-12-19 17:25:41 +0000

    introduce packageDate(), from PR#17324
    git-svn-id: https://svn.r-project.org/R/trunk@73925 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/utils/R/indices.R
+++ src/library/utils/R/indices.R
@@ -159,6 +159,39 @@ packageVersion <- function(pkg, lib.loc = NULL)
     stop(gettextf("package %s not found", sQuote(pkg)), domain = NA)
 }
 
+##' Auxiliary: generalize extraction from "Built"
+asDateBuilt <- function(built) {
+    as.Date(strsplit(built, split="; ", fixed=TRUE)[[1L]][[3L]],
+            format = "%Y-%m-%d")
+}
+
+packageDate <- function(pkg, lib.loc = NULL,
+	date.fields = c("Date", "Packaged", "Date/Publication", "Built"),
+        tryFormats = c("%Y-%m-%d", "%Y/%m/%d", "%D", "%m/%d/%y"),
+	desc = packageDescription(pkg, lib.loc=lib.loc, fields=date.fields))
+{
+    useDesc <- is.list(desc) && length(names(desc)) >= 1
+    for (fld in date.fields) {
+	res <- if(useDesc) {
+		   r <- desc[[fld]]
+		   if(is.null(r)) NA_character_ else r
+	       } else
+		   packageDescription(pkg, lib.loc=lib.loc, fields = fld)
+	res <- if(fld == "Built" && !is.na(res))
+		   tryCatch(asDateBuilt(res),
+			    error = function(e) {
+				warning("Invalid \"Built\": ", conditionMessage(e))
+				NA_character_
+			    })
+	       else as.Date(res, tryFormats=tryFormats,
+			    optional = TRUE)# NA instead of errror
+	if (!is.na(res))
+	    break
+    }
+    if(is.na(res)) res else structure(res, field = fld) # NA or 'Date' object
+}
+
+
 ## used with firstOnly = TRUE for example()
 ## used with firstOnly = FALSE in help()
 index.search <- function(topic, paths, firstOnly = FALSE)
