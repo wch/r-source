e797b4237a0416607187c44e2dc7fcdca95f0d95
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2018-01-01 09:52:43 +0000

    port r74007 from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-3-4-branch@74008 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/modules/lapack/Lapack.c
+++ src/modules/lapack/Lapack.c
@@ -1375,7 +1375,7 @@ static SEXP mod_do_lapack(SEXP call, SEXP op, SEXP args, SEXP env)
     {
 #if defined(HAVE_DLADDR) && defined(HAVE_REALPATH)
 	Dl_info dl_info;
-	if (dladdr(F77_NAME(ilaver), &dl_info)) {
+	if (dladdr((const void *) F77_NAME(ilaver), &dl_info)) {
 	    char buf[PATH_MAX+1];
 	    char *res = realpath(dl_info.dli_fname, buf);
 	    if (res) {
