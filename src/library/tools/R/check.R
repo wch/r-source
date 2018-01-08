c4e90c97bcf8bf14c1ba1ddeecd69dec125da14b
kalibera <kalibera@00db46b3-68df-0310-9c12-caf00c1e9a41>
2018-01-08 16:17:53 +0000

    Avoid non-ascii character in literals in base package source code.
    git-svn-id: https://svn.r-project.org/R/trunk@74098 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/check.R
+++ src/library/tools/R/check.R
@@ -233,21 +233,21 @@ add_dummies <- function(dir, Log)
     }
     Sys.setenv(PATH = env_path(dir1, Sys.getenv("PATH")))
     if(.Platform$OS.type != "windows") {
-        writeLines(c('echo "\'R\' should not be used without a path -- see §1.6 of the manual"',
+        writeLines(c('echo "\'R\' should not be used without a path -- see par. 1.6 of the manual"',
                      'exit 1'),
                    p1 <- file.path(dir1, "R"))
-        writeLines(c('echo "\'Rscript\' should not be used without a path -- see §1.6 of the manual"',
+        writeLines(c('echo "\'Rscript\' should not be used without a path -- see par. 1.6 of the manual"',
                      'exit 1'),
                    p2 <- file.path(dir1, "Rscript"))
         Sys.chmod(c(p1, p2), "0755")
     } else {
         ## currently untested
         writeLines(c('@ECHO OFF',
-                     'echo "\'R\' should not be used without a path -- see §1.6 of the manual"',
+                     'echo "\'R\' should not be used without a path -- see par. 1.6 of the manual"',
                      'exit /b 1'),
                    p1 <- file.path(dir1, "R.bat"))
         writeLines(c('@ECHO OFF',
-                     'echo "\'Rscript\' should not be used without a path -- see §1.6 of the manual"',
+                     'echo "\'Rscript\' should not be used without a path -- see par. 1.6 of the manual"',
                      'exit /b 1'),
                    p2 <- file.path(dir1, "Rscript.bat"))
    }
