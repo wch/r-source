c4bbf5561b07f377b28bc16e8177d585e8fd8c84
kalibera <kalibera@00db46b3-68df-0310-9c12-caf00c1e9a41>
2018-02-14 13:34:14 +0000

    Added extension for load serialization files.
    git-svn-id: https://svn.r-project.org/R/trunk@74249 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/check.R
+++ src/library/tools/R/check.R
@@ -87,7 +87,8 @@ get_serialization_version <- function(allfiles)
         tryCatch(.Internal(serializeInfoFromConn(con))$version,
                  error = function(e) 0L)
     }
-    loadfiles <- grep("[.](rda|RData|rdata|Rda|bam|Rbin)$", allfiles, value = TRUE)
+    loadfiles <- grep("[.](rda|RData|Rdata|rdata|Rda|bam|Rbin)$",
+                      allfiles, value = TRUE)
     serfiles <- c(grep("[.](rds|RDS|Rds|rdx)$", allfiles, value = TRUE),
                   grep("build/partial[.]rdb$", allfiles, value = TRUE))
     vers1 <- sapply(loadfiles, getVerLoad)
