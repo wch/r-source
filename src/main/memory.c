64e7b84f1709288411877ddb7fda1cca213e7a26
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2018-03-10 17:16:03 +0000

    Don't do a zero-length check in STDVEC_DATAPTR for CHARSXP since these always have a trailing zero byte.
    git-svn-id: https://svn.r-project.org/R/trunk@74379 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/memory.c
+++ src/main/memory.c
@@ -3535,10 +3535,12 @@ SEXP (VECTOR_ELT)(SEXP x, R_xlen_t i) {
    result in a segfault, rather than read and write random memory.
    Returning NULL would be more natural, but Matrix seems to assume
    that even zero-length vectors have non-NULL data pointers, so
-   return (void *) NULL instead. */
-# define CHKZLN(x) do {					\
-	CHK(x);						\
-	if (STDVEC_LENGTH(x) == 0) return (void *) 1;	\
+   return (void *) NULL instead. Zero-length CHARSXP objects still
+   have a trailing zero byte so they are not handled. */
+# define CHKZLN(x) do {					   \
+	CHK(x);						   \
+	if (STDVEC_LENGTH(x) == 0 && TYPEOF(x) != CHARSXP) \
+	    return (void *) 1;				   \
     } while (0)
 #else
 # define CHKZLN(x) do { } while (0)
