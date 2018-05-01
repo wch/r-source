5c2ca5e27cc04df94ee52af9fc7c23a5fcd39df7
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2018-05-01 07:56:04 +0000

    drop CO == utils::capture.output from EPD checks
    git-svn-id: https://svn.r-project.org/R/trunk@74675 00db46b3-68df-0310-9c12-caf00c1e9a41

--- tests/eval-etc.R
+++ tests/eval-etc.R
@@ -150,6 +150,7 @@ stopifnot(
     identical(r1,r2)
 )
 ## partly failed in R 3.4.0 alpha
+rm(CO) # as its deparse() depends on if utils was installed w/ keep.source.pkgs=TRUE
 
 source(file.path(Sys.getenv("SRCDIR"), "eval-fns.R"), echo = TRUE)
                                         #---------
