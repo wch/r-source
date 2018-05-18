c4c5579591966d304e6f69c45628a1da77a03a27
kalibera <kalibera@00db46b3-68df-0310-9c12-caf00c1e9a41>
2018-05-18 14:02:38 +0000

    Ensure C collation order when checking .S3_methods_table in tests for tools.
    git-svn-id: https://svn.r-project.org/R/trunk@74742 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/tests/S3.R
+++ src/library/tools/tests/S3.R
@@ -1,4 +1,8 @@
 ## check that all S3 methods in base are registered.
 ## See the end of src/library/base/R/zzz.R ...
-stopifnot(identical(base:::.S3_methods_table,
-                    tools:::.make_S3_methods_table_for_base()))
+(function() {
+  old <- Sys.setlocale("LC_COLLATE", "C")
+  on.exit(Sys.setlocale("LC_COLLATE", old))
+  stopifnot(identical(base:::.S3_methods_table,
+                      tools:::.make_S3_methods_table_for_base()))
+})()
