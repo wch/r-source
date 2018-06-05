93948581a5c5a74e712e303b714da74bd70b292a
kalibera <kalibera@00db46b3-68df-0310-9c12-caf00c1e9a41>
2018-06-05 13:21:54 +0000

    Fix reading from connections with encoding conversion but without buffering.
    git-svn-id: https://svn.r-project.org/R/trunk@74848 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/connections.c
+++ src/main/connections.c
@@ -508,7 +508,10 @@ int dummy_fgetc(Rconnection con)
 	    }
 	    p = con->iconvbuff + con->inavail;
 	    for(i = con->inavail; i < 25; i++) {
-		c = buff_fgetc(con);
+		if (con->buff)
+		    c = buff_fgetc(con);
+		else
+		    c = con->fgetc_internal(con);
 		if(c == R_EOF){ con->EOF_signalled = TRUE; break; }
 		*p++ = (char) c;
 		con->inavail++;
