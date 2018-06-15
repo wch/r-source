b68a1480e99f488f1fa681c66ec66949ce57d01f
kalibera <kalibera@00db46b3-68df-0310-9c12-caf00c1e9a41>
2018-06-15 08:17:42 +0000

    Port 74899 from trunk (R CMD build generates files in serialize format 2, even when the default format is changed to 3 via environment variables).
    git-svn-id: https://svn.r-project.org/R/branches/R-3-5-branch@74900 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/build.R
+++ src/library/tools/R/build.R
@@ -386,8 +386,10 @@ inRbuildignore <- function(files, pkgdir) {
 
 		## Save the list
 		dir.create("build", showWarnings = FALSE)
+		## version = 2L for maximal back-compatibility
 		saveRDS(vignetteIndex,
-			file = vignette_index_path)
+			file = vignette_index_path,
+			version = 2L)
             }
         } else {
             fv <- file.path("build", "vignette.rds")
@@ -596,7 +598,8 @@ inRbuildignore <- function(files, pkgdir) {
 	    messageLog(Log, "saving partial Rd database")
 	    partial <- db[containsBuildSexprs]
 	    dir.create("build", showWarnings = FALSE)
-	    saveRDS(partial, build_partial_Rd_db_path)
+	    ## version = 2L for maximal back-compatibility
+	    saveRDS(partial, build_partial_Rd_db_path, version = 2L)
 	}
 	needRefman <- manual &&
             parse_description_field(desc, "BuildManual", TRUE) &&
@@ -764,10 +767,12 @@ inRbuildignore <- function(files, pkgdir) {
             lapply(Rs, function(x){
                 envir <- new.env(hash = TRUE)
                 sys.source(x, chdir = TRUE, envir = envir)
+                ## version = 2L for maximal back-compatibility
                 save(list = ls(envir, all.names = TRUE),
                      file = sub("\\.[Rr]$", ".rda", x),
                      compress = TRUE, compression_level = 9,
-                     envir = envir)
+                     envir = envir,
+                     version = 2L)
                 resaved <<- c(resaved, x)
             })
             printLog(Log,
