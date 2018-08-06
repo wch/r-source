edad48246e25f3eaa51dbb756045549e6dabb9c3
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2018-08-06 11:30:00 +0000

    news() gets support for 'NEWS.md' files.
    git-svn-id: https://svn.r-project.org/R/trunk@75069 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/news.R
+++ src/library/tools/R/news.R
@@ -59,13 +59,18 @@ function(package, lib.loc = NULL, format = NULL, reader = NULL)
     ## little point in providing format/reader support ...
     ## </FIXME>
 
-    ## Look for new-style inst/NEWS.Rd installed as NEWS.Rd.
-    ## If not found, look for old-style
+    ## Look for new-style inst/NEWS.Rd installed as NEWS.Rd
+    ## If not found, look for NEWS.md.
+    ## If not found, look at old-style
     ##   NEWS inst/NEWS
     ## installed as NEWS (and ignore ChangeLog files).
     nfile <- file.path(dir, "NEWS.Rd")
     if(file_test("-f", nfile))
         return(.build_news_db_from_package_NEWS_Rd(nfile))
+
+    nfile <- file.path(dir, "NEWS.md")
+    if(file_test("-f", nfile))
+        return(.build_news_db_from_package_NEWS_md(nfile))
 
     nfile <- file.path(dir, "NEWS")
     if(!file_test("-f", nfile))
@@ -696,3 +701,168 @@ function(x)
     y
 
 }
+
+.build_news_db_from_package_NEWS_md <-
+function(f)
+{
+    md <- readLines(f, encoding = "UTF-8", warn = FALSE)
+
+    ## Handle YAML header.
+    if(md[1L] == "---") {
+        for(pos in seq.int(2L, length(md)))
+            if(md[pos] == "---") break
+        md[seq_len(pos)] <- ""
+    }
+
+    doc <- commonmark::markdown_xml(md,
+                                    extensions = TRUE,
+                                    sourcepos = TRUE)
+    doc <- xml2::xml_ns_strip(xml2::read_xml(doc))
+
+    nodes <- xml2::xml_children(doc)    # Need xml2::xml_root()?
+
+    ## Inline for efficiency.
+    .markdown_text <- commonmark::markdown_text
+    .markdown_html <- commonmark::markdown_html
+    .xml_attr <- xml2::xml_attr
+    .xml_name <- xml2::xml_name
+    .xml_text <- xml2::xml_text
+    
+    get_text_and_HTML <- function(sp) {
+        ## Sourcepos sp already split into l1 c2 l2 c2, for legibility:
+        l1 <- sp[1L]; c1 <- sp[2L]; l2 <- sp[3L]; c2 <- sp[4L]
+        txt <- if(l1 < l2) {
+                   c(substring(md[l1], c1),
+                     md[seq.int(from = l1 + 1L,
+                                length.out = l2 - l1 - 1L)],
+                     substring(md[l2], 1L, c2))
+               } else
+                   substring(md[l1], c1, c2)
+        c(.markdown_text(txt, width = 72L),
+          .markdown_html(txt))
+    }
+
+    do_vchunk <- function(nodes) {
+        ## Get version and date from heading.
+        version <- .xml_text(nodes[[1L]])
+        nodes <- nodes[-1L]
+        if(!length(nodes))
+            return(rbind(c(version, "", "", "")))
+        ind <- .xml_name(nodes) == "heading"
+        pos <- which(ind)
+        if(length(pos)) {
+            lev <- .xml_attr(nodes[pos], "level")
+            ind[pos] <- (lev == lev[1L])
+            if((pos[1L]) > 1L) {
+                ini <- seq_len(pos[1L] - 1L)
+                out <- list(do_cchunk(nodes[ini], FALSE))
+                nodes <- nodes[-ini]
+                ind <- ind[-ini]
+            } else
+                out <- list()
+            out <- c(out,
+                     lapply(split(nodes, cumsum(ind)),
+                            do_cchunk, TRUE))
+            cbind(version, do.call(rbind, out))
+        } else {
+            rbind(c(version,
+                    do_cchunk(nodes, FALSE)))
+        }
+
+    }
+
+    do_cchunk <- function(nodes, heading) {
+        if(heading) {
+            category <- .xml_text(nodes[[1L]])
+            nodes <- nodes[-1L]
+        } else {
+            category <- ""
+        }
+        if(!length(nodes))
+            return(c(category, "", ""))
+        sp <- c(.xml_attr(nodes[[1L]], "sourcepos"),
+                .xml_attr(nodes[[length(nodes)]], "sourcepos"))
+        sp <- as.integer(unlist(strsplit(sp, "[:-]"))[c(1L, 2L, 7L, 8L)])
+        
+        c(category, get_text_and_HTML(sp))
+    }
+         
+    ind <- .xml_name(nodes) == "heading"
+    pos <- which(ind)
+    if(!length(pos)) return()
+
+    re_v <- sprintf("(^|.*[[:space:]]+)[vV]?(%s).*$",
+                    .standard_regexps()$valid_package_version)
+    while(length(pos) &&
+          !grepl(re_v, .xml_text(nodes[[pos[1L]]])))
+        pos <- pos[-1L]
+    if(!length(pos)) return()
+    
+    lev <- .xml_attr(nodes[pos], "level")
+    ind[pos] <- (lev == lev[1L])
+    if(pos[1L] > 1L) {
+        ini <- seq_len(pos[1L] - 1L)
+        nodes <- nodes[-ini]
+        ind <- ind[-ini]
+    }
+    vchunks <- split(nodes, cumsum(ind))
+    db <- do.call(rbind, lapply(vchunks, do_vchunk))
+
+    ## Very similar to .build_news_db_from_package_NEWS_Rd() ...
+
+    ## Post-process section names to extract versions and dates.
+    reDt <- "[[:digit:]]{4}-[[:digit:]]{2}-[[:digit:]]{2}"
+    rEnd <- "[[:punct:][:space:]]*$"
+    re_d1 <- sprintf(paste0("^.*(%s)", rEnd), reDt)
+    ## or ending with '(YYYY-MM-DD, <note>)'
+    re_d2 <- sprintf(paste0("^.*\\((%s)[[:punct:]] .*\\)", rEnd), reDt)
+    nms <- db[, 1L]
+    ind <- grepl(re_v, nms, ignore.case = TRUE)
+    if(!all(ind))
+        warning("Cannot extract version info from the following section titles:\n",
+                paste(unique(nms[!ind]), collapse = "  "))
+
+    .make_news_db(cbind(ifelse(ind,
+                               sub(re_v, "\\2", nms, ignore.case = TRUE),
+                               NA_character_),
+                        ifelse(grepl(re_d1, nms, perl = TRUE),
+                               sub(re_d1, "\\1", nms, perl = TRUE),
+                               ifelse(grepl(re_d2, nms, perl = TRUE),
+                                      sub(re_d2, "\\1", nms, perl = TRUE),
+                                      NA_character_)),
+                        db[, 2L],
+                        Text = sub("\n*$", "", db[, 3L]),
+                        HTML = db[, 4L]),
+                  logical(nrow(db)),
+                  "news_db_from_md")
+}
+
+format.news_db_from_md <-
+function(x, ...)
+{
+    do_vchunk <- function(vchunk) {
+        z <- unlist(Map(c, vchunk$Category, vchunk$Text),
+                    use.names = FALSE)
+        z[nzchar(z)]
+    }
+
+    vchunks <- split(x, x$Version)
+    ## Re-order according to decreasing version.
+    vchunks <- vchunks[order(numeric_version(names(vchunks),
+                                             strict = FALSE),
+                             decreasing = TRUE)]
+    if(!length(vchunks))
+        return(character())
+    
+    dates <- sapply(vchunks, function(v) v$Date[1L])
+    vheaders <-
+        format(sprintf("Changes in version %s%s",
+                       names(vchunks),
+                       ifelse(is.na(dates), "",
+                              sprintf(" (%s)", dates))),
+               justify = "centre", width = 72L)
+
+    Map(c, vheaders, lapply(vchunks, do_vchunk),
+        USE.NAMES = FALSE)
+}
+
--- src/library/tools/R/toHTML.R
+++ src/library/tools/R/toHTML.R
@@ -149,6 +149,35 @@ function(x, ...)
       "</body></html>")
 }
 
+toHTML.news_db_from_md <-
+function(x, ...)
+{
+    do_vchunk <- function(vchunk) {
+        cheaders <- vchunk$Category
+        ind <- nzchar(cheaders)
+        cheaders[ind] <- paste0("<h3>", cheaders[ind], "</h3>")
+        z <- unlist(Map(c, cheaders, vchunk$HTML),
+                    use.names = FALSE)
+        z[nzchar(z)]
+    }
+
+    vchunks <- split(x, x$Version)
+    ## Re-order according to decreasing version.
+    vchunks <- vchunks[order(numeric_version(names(vchunks),
+                                             strict = FALSE),
+                             decreasing = TRUE)]
+
+    dates <- sapply(vchunks, function(v) v$Date[1L])    
+    vheaders <- sprintf("<h2>Changes in version %s%s</h2>",
+                        names(vchunks),
+                        ifelse(is.na(dates), "",
+                               sprintf(" (%s)", dates)))
+
+    c(HTMLheader(),
+      unlist(Map(c, vheaders, lapply(vchunks, do_vchunk))),
+      "</body></html>")
+}
+
 # To support static linking, URLs should be relative.
 # Argument "depth" below says how far down in the hierarchy
 # we are starting from, e.g. /library/stats/html/mean.html
