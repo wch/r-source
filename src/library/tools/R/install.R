828a04f9c428403e476620b1905a1d8ca41d0bcd
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2018-08-18 13:00:53 +0000

    careful options(warn=1) inside "R CMD INSTALL"
    git-svn-id: https://svn.r-project.org/R/trunk@75166 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/install.R
+++ src/library/tools/R/install.R
@@ -50,7 +50,7 @@ if(FALSE) {
 
 
 ##' @return ...
-.install_packages <- function(args = NULL, no.q = interactive())
+.install_packages <- function(args = NULL, no.q = interactive(), warnOption = 1)
 {
     ## calls system() on Windows for
     ## sh (configure.win/cleanup.win) make zip
@@ -148,7 +148,10 @@ if(FALSE) {
                              perl = TRUE))
     }
 
-    options(warn = 1)
+    if(getOption("warn") < warnOption) {
+        op <- options(warn = warnOption)
+        on.exit(options(op), add=TRUE)
+    }
     invisible(Sys.setlocale("LC_COLLATE", "C")) # discard output
 
     if (WINDOWS) {
