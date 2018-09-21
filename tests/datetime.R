7f7e5ca37551cd512d8e66f015d33fc8ab73a592
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2018-09-21 09:51:08 +0000

    only test round-trip future conversions
    git-svn-id: https://svn.r-project.org/R/trunk@75348 00db46b3-68df-0310-9c12-caf00c1e9a41

--- tests/datetime.R
+++ tests/datetime.R
@@ -14,24 +14,22 @@ c(unclass(z))
 Sys.setenv(TZ = "Europe/London")  # pretty much portable.
 (z <- as.POSIXct("1848-01-01 12:00"))
 c(unclass(z))
-(z <- as.POSIXct("2040-01-01 12:00"))
-c(unclass(z))
-(z <- as.POSIXct("2040-07-01 12:00"))
-c(unclass(z))
+## We don't know the operation of timezones next year let alone in 1940
+## but these should at least round-trip
+as.POSIXct("2040-01-01 12:00")
+as.POSIXct("2040-07-01 12:00")
 
-Sys.setenv(TZ = "EST5EDT")
+Sys.setenv(TZ = "EST5EDT")  # also pretty much portable.
 (z <- as.POSIXct("1848-01-01 12:00"))
 c(unclass(z))
-(z <- as.POSIXct("2040-01-01 12:00"))
-c(unclass(z))
-(z <- as.POSIXct("2040-07-01 12:00"))
-c(unclass(z))
+as.POSIXct("2040-01-01 12:00")
+as.POSIXct("2040-07-01 12:00")
 
 ## PR15613: had day as > 24hrs.
 as.POSIXlt(ISOdate(2071,1,13,0,0,tz="Etc/GMT-1"))$wday
 as.POSIXlt(ISOdate(2071,1,13,0,1,tz="Etc/GMT-1"))$wday
 
 
-## Incorrect use of %d should work even though abbreviation does match
+## Incorrect use of %b should work even though abbreviation does match
 old <- Sys.setlocale("LC_TIME", "C") # to be sure
 stopifnot(!is.na(strptime("11-August-1903", "%d-%b-%Y")))
