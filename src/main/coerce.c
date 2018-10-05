78cc6b7e5d8e12fa67c6fff5752dd992dce2d66b
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2018-10-05 14:30:01 +0000

    correct thinko (from r75289)
    git-svn-id: https://svn.r-project.org/R/trunk@75405 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/coerce.c
+++ src/main/coerce.c
@@ -1709,7 +1709,7 @@ int asLogical2(SEXP x, int checking, SEXP call)
 }
 int asLogical(SEXP x)
 {
-    return asLogical2(x, /* checking = */ 0, NILSXP);
+    return asLogical2(x, /* checking = */ 0, R_NilValue);
 }
 
 
