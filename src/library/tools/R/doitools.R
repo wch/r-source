3f1fdebea0638c36b398dfea7a694f0fa420cb44
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2018-11-10 22:07:56 +0000

    Tweaks.
    git-svn-id: https://svn.r-project.org/R/trunk@75583 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/doitools.R
+++ src/library/tools/R/doitools.R
@@ -64,7 +64,7 @@ doi_db_from_package_Rd_db <-
 function(db)
 {
     dois <- Filter(length, lapply(db, .get_dois_from_Rd))
-    doi_db(unlist(.canonicalize_doi(dois), use.names = FALSE),
+    doi_db(.canonicalize_doi(unlist(dois, use.names = FALSE)),
            rep.int(file.path("man", names(dois)),
                    lengths(dois)))
 }
