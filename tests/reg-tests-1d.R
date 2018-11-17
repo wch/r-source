027d86d14f889dbae04cf59a29fec98a7f9c7ce4
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2018-11-17 14:16:27 +0000

    PR#17504 = duplicate(PR#15362): more regr.checks
    git-svn-id: https://svn.r-project.org/R/trunk@75624 00db46b3-68df-0310-9c12-caf00c1e9a41

--- tests/reg-tests-1d.R
+++ tests/reg-tests-1d.R
@@ -2227,7 +2227,18 @@ stopifnot(exprs = {
 
 
 
-## Assigning multiple new data.frame columns (with specified row), PR#17504
+## Subassigning multiple new data.frame columns (with specified row), PR#15362, 17504
+z0 <- z1 <- data.frame(a=1, s=1)
+z0[2, c("a","r","e")] <- data.frame(a=1, r=8, e=9)
+z1[2, "r"] <- data.frame(r=8)
+x <- x0 <- data.frame(a=1:3, s=1:3)
+x[2, 3:4] <- data.frame(r=8, e=9)
+stopifnot(exprs = {
+    identical(z0, data.frame(a = c(1, 1), s = c(1, NA), r = c(NA, 8), e = c(NA, 9)))
+    identical(z1, data.frame(a = c(1,NA), s = c(1, NA), r = c(NA, 8)))
+    identical(x, cbind(x0,
+                       data.frame(r = c(NA, 8, NA), e = c(NA, 9, NA))))
+})
 d0 <- d1 <- d2 <- d3 <- d4 <- d5 <- d6 <- d7 <- data.frame(n=1:4)
 ##
 d0[, 2] <- c2 <- 5:8
