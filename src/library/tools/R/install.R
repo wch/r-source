d59145511d88a99ab142d96b51d05a235b8b8355
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2018-11-29 08:33:54 +0000

    rename FCLIBS to FCLIBS_XTRA
    git-svn-id: https://svn.r-project.org/R/trunk@75714 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/install.R
+++ src/library/tools/R/install.R
@@ -2031,7 +2031,7 @@ if(FALSE) {
     }
     if (with_objc) shlib_libadd <- c(shlib_libadd, "$(OBJC_LIBS)")
     if (with_f77) shlib_libadd <- c(shlib_libadd, "$(FLIBS)")
-    if (with_f9x) shlib_libadd <- c(shlib_libadd, "$(FLIBS) $(FCLIBS)")
+    if (with_f9x) shlib_libadd <- c(shlib_libadd, "$(FLIBS) $(FCLIBS_XTRA)")
 
     if (length(pkg_libs))
         makeargs <- c(makeargs,
