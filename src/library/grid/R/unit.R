88c40706870cc90bb25a3f1fcdb5d95467592c52
murrell <murrell@00db46b3-68df-0310-9c12-caf00c1e9a41>
2018-12-10 01:56:46 +0000

    fixes for grobAscent.gList(), grobWidth.gList(), etc;  thanks to Thomas Lin Pedersen
    git-svn-id: https://svn.r-project.org/R/trunk@75816 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/grid/R/unit.R
+++ src/library/grid/R/unit.R
@@ -695,7 +695,7 @@ grobX.grob <- function(x, theta) {
 }
 
 grobX.gList <- function(x, theta) {
-  unit(rep(convertTheta(theta), length(gList)), "grobx", data=x)
+  unit(rep(convertTheta(theta), length(x)), "grobx", data=x)
 }
 
 grobX.gPath <- function(x, theta) {
@@ -716,7 +716,7 @@ grobY.grob <- function(x, theta) {
 }
 
 grobY.gList <- function(x, theta) {
-  unit(rep(convertTheta(theta), length(gList)), "groby", data=x)
+  unit(rep(convertTheta(theta), length(x)), "groby", data=x)
 }
 
 grobY.gPath <- function(x, theta) {
@@ -737,7 +737,7 @@ grobWidth.grob <- function(x) {
 }
 
 grobWidth.gList <- function(x) {
-  unit(rep_len(1, length(gList)), "grobwidth", data=x)
+  unit(rep_len(1, length(x)), "grobwidth", data=x)
 }
 
 grobWidth.gPath <- function(x) {
@@ -758,7 +758,7 @@ grobHeight.grob <- function(x) {
 }
 
 grobHeight.gList <- function(x) {
-  unit(rep_len(1, length(gList)), "grobheight", data=x)
+  unit(rep_len(1, length(x)), "grobheight", data=x)
 }
 
 grobHeight.gPath <- function(x) {
@@ -779,7 +779,7 @@ grobAscent.grob <- function(x) {
 }
 
 grobAscent.gList <- function(x) {
-  unit(rep_len(1, length(gList)), "grobascent", data=x)
+  unit(rep_len(1, length(x)), "grobascent", data=x)
 }
 
 grobAscent.gPath <- function(x) {
@@ -800,7 +800,7 @@ grobDescent.grob <- function(x) {
 }
 
 grobDescent.gList <- function(x) {
-  unit(rep_len(1, length(gList)), "grobdescent", data=x)
+  unit(rep_len(1, length(x)), "grobdescent", data=x)
 }
 
 grobDescent.gPath <- function(x) {
