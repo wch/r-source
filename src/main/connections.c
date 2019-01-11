ba015a315e9c3d3976a4f79027c3dcb584b5ebcf
kalibera <kalibera@00db46b3-68df-0310-9c12-caf00c1e9a41>
2019-01-11 10:05:19 +0000

    Throw error when a directory is being opened using *file() functions.
    git-svn-id: https://svn.r-project.org/R/trunk@75970 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/connections.c
+++ src/main/connections.c
@@ -1,6 +1,6 @@
 /*
  *  R : A Computer Language for Statistical Data Analysis
- *  Copyright (C) 2000-2018   The R Core Team.
+ *  Copyright (C) 2000-2019   The R Core Team.
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -674,6 +674,35 @@ static Rboolean shouldBuffer(int fd) {
 #endif
 }
 
+/* returns FALSE on error */
+static Rboolean isDir(FILE *fd)
+{
+#ifdef HAVE_SYS_STAT_H
+    struct stat sb;
+    int err = fstat(fileno(fd), &sb);
+    return err ? FALSE : S_ISDIR(sb.st_mode);
+#else
+    return FALSE;
+#endif
+}
+
+/* returns FALSE on error */
+static Rboolean isDirPath(const char *path)
+{
+#ifdef HAVE_SYS_STAT_H
+# ifdef Win32
+    struct _stati64 sb;
+    if (!_stati64(path, &sb) && (sb.st_mode & S_IFDIR))
+	return TRUE;
+# else
+    struct stat sb;
+    if (!stat(path, &sb) && S_ISDIR(sb.st_mode))
+	return TRUE;
+# endif
+#endif
+    return FALSE;
+}
+
 static Rboolean file_open(Rconnection con)
 {
     const char *name;
@@ -716,11 +745,16 @@ static Rboolean file_open(Rconnection con)
 		warning(_("cannot open file '%ls': %s"), wname, strerror(errno));
 		return FALSE;
 	    }
+	    if (isDir(fp)) {
+		warning(_("cannot open file '%ls': it is a directory"), wname);
+		fclose(fp);
+		return FALSE;
+	    }
 	} else {
 	    fp = R_fopen(name, mode);
 	}
 #else
-    fp = R_fopen(name, con->mode);
+	fp = R_fopen(name, con->mode);
 #endif
     } else {  /* use file("stdin") to refer to the file and not the console */
 #ifdef HAVE_FDOPEN
@@ -739,6 +773,11 @@ static Rboolean file_open(Rconnection con)
     }
     if(!fp) {
 	warning(_("cannot open file '%s': %s"), name, strerror(errno));
+	return FALSE;
+    }
+    if (isDir(fp)) {
+	warning(_("cannot open file '%s': it is a directory"), name);
+	fclose(fp);
 	return FALSE;
     }
     if(temp) {
@@ -1631,6 +1670,7 @@ static Rboolean gzfile_open(Rconnection con)
     gzFile fp;
     char mode[6];
     Rgzfileconn gzcon = con->private;
+    const char *name;
 
     strcpy(mode, con->mode);
     /* Must open as binary */
@@ -1638,10 +1678,17 @@ static Rboolean gzfile_open(Rconnection con)
     else if (con->mode[0] == 'a') snprintf(mode, 6, "ab%1d", gzcon->compress);
     else strcpy(mode, "rb");
     errno = 0; /* precaution */
-    fp = R_gzopen(R_ExpandFileName(con->description), mode);
+    name = R_ExpandFileName(con->description);
+    /* We cannot use isDir, because we cannot get the fd from gzFile
+       (it would be possible with gzdopen, if supported) */
+    if (isDirPath(name)) {
+	warning(_("cannot open file '%s': it is a directory"), name);
+	return FALSE;
+    }
+    fp = R_gzopen(name, mode);
     if(!fp) {
 	warning(_("cannot open compressed file '%s', probable reason '%s'"),
-		R_ExpandFileName(con->description), strerror(errno));
+	        name, strerror(errno));
 	return FALSE;
     }
     ((Rgzfileconn)(con->private))->fp = fp;
@@ -1770,6 +1817,7 @@ static Rboolean bzfile_open(Rconnection con)
     BZFILE* bfp;
     int bzerror;
     char mode[] = "rb";
+    const char *name;
 
     con->canwrite = (con->mode[0] == 'w' || con->mode[0] == 'a');
     con->canread = !con->canwrite;
@@ -1777,10 +1825,16 @@ static Rboolean bzfile_open(Rconnection con)
        binary mode where it matters */
     mode[0] = con->mode[0];
     errno = 0; /* precaution */
-    fp = R_fopen(R_ExpandFileName(con->description), mode);
+    name = R_ExpandFileName(con->description);
+    fp = R_fopen(name, mode);
     if(!fp) {
 	warning(_("cannot open bzip2-ed file '%s', probable reason '%s'"),
-		R_ExpandFileName(con->description), strerror(errno));
+		name, strerror(errno));
+	return FALSE;
+    }
+    if (isDir(fp)) {
+	warning(_("cannot open file '%s': it is a directory"), name);
+	fclose(fp);
 	return FALSE;
     }
     if(con->canread) {
@@ -1959,6 +2013,7 @@ static Rboolean xzfile_open(Rconnection con)
     Rxzfileconn xz = con->private;
     lzma_ret ret;
     char mode[] = "rb";
+    const char *name;
 
     con->canwrite = (con->mode[0] == 'w' || con->mode[0] == 'a');
     con->canread = !con->canwrite;
@@ -1966,10 +2021,16 @@ static Rboolean xzfile_open(Rconnection con)
        binary mode where it matters */
     mode[0] = con->mode[0];
     errno = 0; /* precaution */
-    xz->fp = R_fopen(R_ExpandFileName(con->description), mode);
+    name = R_ExpandFileName(con->description);
+    xz->fp = R_fopen(name, mode);
     if(!xz->fp) {
 	warning(_("cannot open compressed file '%s', probable reason '%s'"),
-		R_ExpandFileName(con->description), strerror(errno));
+		name, strerror(errno));
+	return FALSE;
+    }
+    if (isDir(xz->fp)) {
+	warning(_("cannot open file '%s': it is a directory"), name);
+	fclose(xz->fp);
 	return FALSE;
     }
     if(con->canread) {
