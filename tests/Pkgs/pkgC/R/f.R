77d8054b8cf25fc424db9ab288a128e756446598
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2019-01-25 16:50:43 +0000

    now installation of `pkgC` reproduces the strange installation warning CRAN pretests had detected when installing `ecd` in `Rmpfr`s rev.dep. checking
    git-svn-id: https://svn.r-project.org/R/trunk@76015 00db46b3-68df-0310-9c12-caf00c1e9a41

--- tests/Pkgs/pkgC/R/f.R
+++ tests/Pkgs/pkgC/R/f.R
@@ -6,3 +6,17 @@ Cfun <- function(x) {
     new("classA", # classA was imported from 'pkgA'
        cbind(x, b=x+2))
 }
+
+## Pkg 'ecd' had
+## setClassUnion("numericMpfr", c("numeric", "mpfr", "mpfrArray"))
+##
+## which gave an 'R CMD INSTALL'  warning with Rmpfr 0.7-2
+'
+** byte-compile and prepare package for lazy loading
+Warning: subclass "summaryMpfr" of class "mpfr" is not local and cannot be updated for new inheritance information; consider setClassUnion()
+'
+## 'Rmpfr' there corresponds to 'pkgA' here;
+##  ----   class "mpfr"        to "classA"   here,
+##         class "summaryMpfr" to "classApp" here .. :
+setClassUnion("numericA", c("numeric", "classA", "matrix"))
+
