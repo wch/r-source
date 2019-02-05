3e2385028a9086d95b1d6b60aaed1f0f3ab15ef8
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2019-02-05 09:03:44 +0000

    ported r76050 [writeLines(readL..)] from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-3-5-branch@76056 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/connections.R
+++ src/library/base/R/connections.R
@@ -1,7 +1,7 @@
 #  File src/library/base/R/connections.R
 #  Part of the R package, https://www.R-project.org
 #
-#  Copyright (C) 1995-2016 The R Core Team
+#  Copyright (C) 1995-2019 The R Core Team
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -38,6 +38,8 @@ readLines <- function(con = stdin(), n = -1L, ok = TRUE, warn = TRUE,
 
 writeLines <- function(text, con = stdout(), sep = "\n", useBytes = FALSE)
 {
+    if(!is.character(text))
+        stop("can only write character objects")
     if(is.character(con)) {
         con <- file(con, "w")
         on.exit(close(con))
