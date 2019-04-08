bc033ec21b82122e9e819aded7df30348d5ed3f5
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2019-04-08 09:34:25 +0000

    tweak message
    git-svn-id: https://svn.r-project.org/R/branches/R-3-6-branch@76344 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/check.R
+++ src/library/tools/R/check.R
@@ -2965,7 +2965,7 @@ add_dummies <- function(dir, Log)
                         if(f == "F" && !(have_f || have_f9x)) {
                             if (!any) noteLog(Log)
                             any <- TRUE
-                            msg <- "SHLIB_OPENMP_FFLAGS is included in PKG_FFLAGS without any fixed-form Fortran files\n"
+                            msg <- "SHLIB_OPENMP_FFLAGS is included in PKG_FFLAGS without any Fortran files\n"
                             printLog(Log, "  ", m, ": ", msg)
                             next
                         }
