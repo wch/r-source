56ac60cde219584e4dc46c20d063c9f88715cc12
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2019-04-10 20:23:35 +0000

    fix match(x, table) for long vector x; btw fix FIXME about length/LENGTH
    git-svn-id: https://svn.r-project.org/R/trunk@76362 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/unique.c
+++ src/main/unique.c
@@ -1,6 +1,6 @@
 /*
  *  R : A Computer Language for Statistical Data Analysis
- *  Copyright (C) 1997--2018  The R Core Team
+ *  Copyright (C) 1997--2019  The R Core Team
  *  Copyright (C) 1995, 1996  Robert Gentleman and Ross Ihaka
  *
  *  This program is free software; you can redistribute it and/or modify
@@ -909,11 +909,6 @@ SEXP match5(SEXP itable, SEXP ix, int nmatch, SEXP incomp, SEXP env)
     // special case scalar x -- for speed only :
     if(XLENGTH(x) == 1 && !incomp) {
       int val = nmatch;
-      /* FIXME:
-	 The code for the special case uses LENGTH(table),
-	 the code for the regular case uses length(table):
-	 These should use the same.
-      */
       int ntable = LENGTH(table);
       switch (type) {
       case STRSXP: {
@@ -979,7 +974,7 @@ SEXP match5(SEXP itable, SEXP ix, int nmatch, SEXP incomp, SEXP env)
 	    Rboolean useBytes = FALSE;
 	    Rboolean useUTF8 = FALSE;
 	    Rboolean useCache = TRUE;
-	    for(R_xlen_t i = 0; i < length(x); i++) {
+	    for(R_xlen_t i = 0; i < xlength(x); i++) {
 		SEXP s = STRING_ELT(x, i);
 		if(IS_BYTES(s)) {
 		    useBytes = TRUE;
@@ -995,7 +990,7 @@ SEXP match5(SEXP itable, SEXP ix, int nmatch, SEXP incomp, SEXP env)
 		}
 	    }
 	    if(!useBytes || useCache) {
-		for(int i = 0; i < length(table); i++) {
+		for(int i = 0; i < LENGTH(table); i++) {
 		    SEXP s = STRING_ELT(table, i);
 		    if(IS_BYTES(s)) {
 			useBytes = TRUE;
