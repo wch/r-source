e46e3d371d016d3a1b60ba6d34902152d3be58be
kalibera <kalibera@00db46b3-68df-0310-9c12-caf00c1e9a41>
2019-05-07 11:57:42 +0000

    Port 76455 from trunk.
    git-svn-id: https://svn.r-project.org/R/branches/R-3-6-branch@76464 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/parallel/R/unix/mcparallel.R
+++ src/library/parallel/R/unix/mcparallel.R
@@ -1,7 +1,7 @@
 #  File src/library/parallel/R/unix/mcparallel.R
 #  Part of the R package, https://www.R-project.org
 #
-#  Copyright (C) 1995-2018 The R Core Team
+#  Copyright (C) 1995-2019 The R Core Team
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -71,7 +71,8 @@ mccollect <- function(jobs, wait = TRUE, timeout = 0, intermediate = FALSE)
             r <- readChild(x)
             if (is.raw(r)) {
                 rmChild(x) ## avoid zombie process without waiting
-                res[[i]] <- unserialize(r)
+                ## unserialize(r) might be null
+                res[i] <- list(unserialize(r))
                 delivered.result <- delivered.result + 1L
             }
         }
