0137fef1e9c221039667a98f897f59e9a9dd3a11
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2019-05-17 16:05:36 +0000

    ported r76503 [boxplot(*, horizontal)] from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-3-6-branch@76524 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/graphics/R/boxplot.R
+++ src/library/graphics/R/boxplot.R
@@ -93,18 +93,22 @@ boxplot.matrix <- function(x, use.cols = TRUE, ...)
 
 boxplot.formula <-
     function(formula, data = NULL, ..., subset, na.action = NULL,
-             xlab = paste(names(mf)[-response], collapse = " : "),
-             ylab = names(mf)[ response],
-             add = FALSE, ann = !add,
+             xlab = mklab(y_var = horizontal),
+             ylab = mklab(y_var =!horizontal),
+             add = FALSE, ann = !add, horizontal = FALSE,
 	     drop = FALSE, sep = ".", lex.order = FALSE)
 {
     if(missing(formula) || (length(formula) != 3L))
 	stop("'formula' missing or incorrect")
+    if(missing(xlab) || missing(ylab))
+        mklab <- function(y_var)
+            if(y_var)  names(mf)[ response]
+            else paste(names(mf)[-response], collapse = " : ")
     m <- match.call(expand.dots = FALSE)
     if(is.matrix(eval(m$data, parent.frame())))
 	m$data <- as.data.frame(data)
     m$... <- m$drop <- m$sep <- m$lex.order <- NULL
-    m$xlab <- m$ylab  <- m$add <- m$ann <- NULL
+    m$xlab <- m$ylab  <- m$add <- m$ann <- m$horizontal <- NULL
     m$na.action <- na.action # force use of default for this method
     ## need stats:: for non-standard evaluation
     m[[1L]] <- quote(stats::model.frame.default)
@@ -112,7 +116,7 @@ boxplot.formula <-
     response <- attr(attr(mf, "terms"), "response")
     boxplot(split(mf[[response]], mf[-response],
 		  drop = drop, sep = sep, lex.order = lex.order),
-	    xlab = xlab, ylab = ylab, add = add, ann = ann,
+	    xlab = xlab, ylab = ylab, add = add, ann = ann, horizontal = horizontal,
 	    ...)
 }
 
