ae685326ba2c5ea535aa1805bbf3ae8f32d1030f
kalibera <kalibera@00db46b3-68df-0310-9c12-caf00c1e9a41>
2019-06-19 09:51:39 +0000

    Port 76705 and 76715.
    git-svn-id: https://svn.r-project.org/R/branches/R-3-6-branch@76716 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/utils/R/windows/install.packages.R
+++ src/library/utils/R/windows/install.packages.R
@@ -18,7 +18,7 @@
 
 ## Unexported helper
 unpackPkgZip <- function(pkg, pkgname, lib, libs_only = FALSE,
-                         lock = FALSE, quiet = FALSE)
+                         lock = FALSE, quiet = FALSE, reuse_lockdir = FALSE)
 {
     .zip.unpack <- function(zipname, dest)
     {
@@ -104,11 +104,13 @@ unpackPkgZip <- function(pkg, pkgname, lib, libs_only = FALSE,
 	    lockdir <- if(identical(lock, "pkglock"))
                 file.path(lib, paste0("00LOCK-", pkgname))
             else file.path(lib, "00LOCK")
-	    if (file.exists(lockdir)) {
-                stop(gettextf("ERROR: failed to lock directory %s for modifying\nTry removing %s",
-                              sQuote(lib), sQuote(lockdir)), domain = NA)
-	    }
-	    dir.create(lockdir, recursive = TRUE)
+            if (!reuse_lockdir) {
+  	        if (file.exists(lockdir)) {
+                    stop(gettextf("ERROR: failed to lock directory %s for modifying\nTry removing %s",
+                                  sQuote(lib), sQuote(lockdir)), domain = NA)
+	        }
+	        dir.create(lockdir, recursive = TRUE)
+            }
 	    if (!dir.exists(lockdir))
                 stop(gettextf("ERROR: failed to create lock directory %s",
                               sQuote(lockdir)), domain = NA)
@@ -126,7 +128,12 @@ unpackPkgZip <- function(pkg, pkgname, lib, libs_only = FALSE,
         	}, add=TRUE)
         	restorePrevious <- FALSE
             }
-	    on.exit(unlink(lockdir, recursive = TRUE), add=TRUE)
+            on.exit({
+                ldel <- if (reuse_lockdir) 
+                            file.path(lockdir, pkgname)
+                        else lockdir
+                unlink(ldel, recursive = TRUE)
+            }, add=TRUE)
         }
 
         if(libs_only) {
