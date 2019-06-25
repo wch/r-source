3a1ec60935e5aef5dff4794561d9e3ff2e30bd14
kalibera <kalibera@00db46b3-68df-0310-9c12-caf00c1e9a41>
2019-06-25 08:50:17 +0000

    Move constants checking and gc reporting out of RunGenCollect, to avoid interference with code that locks GC.
    git-svn-id: https://svn.r-project.org/R/trunk@76738 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/memory.c
+++ src/main/memory.c
@@ -1587,7 +1587,7 @@ SEXP attribute_hidden do_regFinaliz(SEXP call, SEXP op, SEXP args, SEXP rho)
     } \
 } while (0)
 
-static void RunGenCollect(R_size_t size_needed)
+static int RunGenCollect(R_size_t size_needed)
 {
     int i, gen, gens_collected;
     RCNTXT *ctxt;
@@ -1914,18 +1914,9 @@ static void RunGenCollect(R_size_t size_needed)
 	SortNodes();
 #endif
 
-    if (R_check_constants > 2 ||
-	    (R_check_constants > 1 && gens_collected == NUM_OLD_GENERATIONS))
-	R_checkConstants(TRUE);
-
-    if (gc_reporting) {
-	REprintf("Garbage collection %d = %d", gc_count, gen_gc_counts[0]);
-	for (i = 0; i < NUM_OLD_GENERATIONS; i++)
-	    REprintf("+%d", gen_gc_counts[i + 1]);
-	REprintf(" (level %d) ... ", gens_collected);
-	DEBUG_GC_SUMMARY(gens_collected == NUM_OLD_GENERATIONS);
-    }
+    return gens_collected;
 }
+
 
 /* public interface for controlling GC torture settings */
 /* maybe, but in no header */
@@ -3069,6 +3060,7 @@ static void R_gc_internal(R_size_t size_needed)
 #endif
     SEXP first_bad_sexp_type_sexp = NULL;
     int first_bad_sexp_type_line = 0;
+    int i, gens_collected = 0;
 
 #ifdef IMMEDIATE_FINALIZERS
     Rboolean first = TRUE;
@@ -3083,10 +3075,22 @@ static void R_gc_internal(R_size_t size_needed)
     BEGIN_SUSPEND_INTERRUPTS {
 	R_in_gc = TRUE;
 	gc_start_timing();
-	RunGenCollect(size_needed);
+	gens_collected = RunGenCollect(size_needed);
 	gc_end_timing();
 	R_in_gc = FALSE;
     } END_SUSPEND_INTERRUPTS;
+
+    if (R_check_constants > 2 ||
+	    (R_check_constants > 1 && gens_collected == NUM_OLD_GENERATIONS))
+	R_checkConstants(TRUE);
+
+    if (gc_reporting) {
+	REprintf("Garbage collection %d = %d", gc_count, gen_gc_counts[0]);
+	for (i = 0; i < NUM_OLD_GENERATIONS; i++)
+	    REprintf("+%d", gen_gc_counts[i + 1]);
+	REprintf(" (level %d) ... ", gens_collected);
+	DEBUG_GC_SUMMARY(gens_collected == NUM_OLD_GENERATIONS);
+    }
 
     if (bad_sexp_type_seen != 0 && first_bad_sexp_type == 0) {
 	first_bad_sexp_type = bad_sexp_type_seen;
@@ -3164,7 +3168,6 @@ static void R_gc_internal(R_size_t size_needed)
 	LOGICAL(R_LogicalNAValue)[0] = NA_LOGICAL;
 	error("internal logical NA value has been modified");
     }
-    /* compiler constants are checked in RunGenCollect */
 }
 
 
