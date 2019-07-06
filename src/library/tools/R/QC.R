bc83f2625c4e1a86f4df7719e1ce8d05fb509009
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2019-07-06 08:34:02 +0000

    Allow CRAN incoming checks to find DESCRIPTION fields with placeholder content.
    git-svn-id: https://svn.r-project.org/R/trunk@76790 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/QC.R
+++ src/library/tools/R/QC.R
@@ -7265,6 +7265,25 @@ function(dir, localOnly = FALSE)
         }
     }
 
+    ## Check DESCRIPTION placeholders
+    placeholders <-
+        c(if(!is.na(x <- tolower(meta["Title"])) &&
+             startsWith(x, "what the package does"))
+              x,
+          if(!is.na(x <- meta["Author"]) &&
+             (x == "Who wrote it"))
+              x,
+          if(!is.na(x <- meta["Maintainer"]) &&
+             (startsWith(x, "Who to complain to") ||
+              startsWith(x, "The package maintainer")))
+              x,
+          if(!is.na(x <- tolower(meta["Description"])) &&
+             (startsWith(x, "what the package does") ||
+              startsWith(x, "more about what it does")))
+              x)
+    if(length(placeholders))
+        out$placeholders <- placeholders
+
     ## Are there non-ASCII characters in the R source code without a
     ## package encoding in DESCRIPTION?
     ## Note that checking always runs .check_package_ASCII_code() which
@@ -8159,6 +8178,15 @@ function(x, ...)
                 "The Date field is over a month old."
             })),
       if(length(y <- x$build_time_stamp_msg)) y,
+      if(length(y <- x$placeholders)) {
+          paste(c("DESCRIPTION fields with placeholder content:",
+                  paste0("  ",
+                         unlist(strsplit(formatDL(y,
+                                                  style = "list",
+                                                  indent = 2L),
+                                         "\n", fixed = TRUE)))),
+                collapse = "\n")
+      },
       if(length(y <- x$size_of_tarball))
           paste("Size of tarball:", y, "bytes"),
       fmt(c(if(length(y <- x$Rd_keywords_or_concepts_with_Rd_markup))
