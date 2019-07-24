b1f2298832cb663b1497bb8bee4837b35e209a14
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2019-07-24 07:26:09 +0000

    LTO missed uses with SAFE-FFLAGS
    git-svn-id: https://svn.r-project.org/R/trunk@76877 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/src/port.c
+++ src/library/stats/src/port.c
@@ -300,6 +300,8 @@ void F77_NAME(i7pnvr)(int *n, int x[], const int y[])
 }
 
 /* stopx.... returns .true. if the break key has been pressed. */
+/* should match Fortran LOGICAL, in gfortran int_least32_t but
+   compiler-specific */
 int F77_NAME(stopx)(void)
 {
     return 0;			/* interrupts are caught elsewhere */
