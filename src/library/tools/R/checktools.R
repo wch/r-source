98b498b7a13781ec455c0051a9e663a7bc33f3ca
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2019-12-06 11:53:50 +0000

    Tweaks.
    git-svn-id: https://svn.r-project.org/R/trunk@77532 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/checktools.R
+++ src/library/tools/R/checktools.R
@@ -859,11 +859,24 @@ function(log, drop_ok = TRUE, ...)
         ##   * loading checks for arch
         ##   * checking examples ...
         ##   * checking tests ...
-        ## headers: drop these (unless in the last line, where they
-        ## indicate failure).
-        re <- "^\\* (loading checks for arch|checking (examples|tests) \\.\\.\\.$)"
-        pos <- grep(re, lines, perl = TRUE, useBytes = TRUE)
-        if(length(pos <- pos[pos < length(lines)]))
+        ## headers: drop these unless not followed by the appropriate
+        ## 'subsection', which indicates failure.
+        pos <- which(startsWith(lines, "* loading checks for arch"))
+        pos <- pos[pos < length(lines)]
+        pos <- pos[startsWith(lines[pos + 1L], "** checking")]
+        if(length(pos))
+            lines <- lines[-pos]
+        pos <- which(startsWith(lines, "* checking examples"))
+        pos <- pos[pos < length(lines)]
+        pos <- pos[startsWith(lines[pos + 1L],
+                              "** running examples for arch")]
+        if(length(pos))
+            lines <- lines[-pos]
+        pos <- which(startsWith(lines, "* checking tests"))
+        pos <- pos[pos < length(lines)]
+        pos <- pos[startsWith(lines[pos + 1L],
+                              "** running tests for arch")]
+        if(length(pos))
             lines <- lines[-pos]
         ## We might still have
         ##   * package encoding:
