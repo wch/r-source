823a43b9b4a1c583e54bfbe8a8a8bd5fb049d91e
pd <pd@00db46b3-68df-0310-9c12-caf00c1e9a41>
2020-01-21 12:48:25 +0000

    named dimnames for sweep()
    git-svn-id: https://svn.r-project.org/R/trunk@77683 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/sweep.R
+++ src/library/base/R/sweep.R
@@ -20,6 +20,18 @@ sweep <- function(x, MARGIN, STATS, FUN = "-", check.margin = TRUE, ...)
 {
     FUN <- match.fun(FUN)
     dims <- dim(x)
+
+    ## Extract the margins and associated dimnames
+
+    if (is.character(MARGIN)) {
+        dn <- dimnames(x) # possibly NULL
+        if(is.null(dnn <- names(dn))) # names(NULL) is NULL
+           stop("'x' must have named dimnames")
+        MARGIN <- match(MARGIN, dnn)
+        if (anyNA(MARGIN))
+            stop("not all elements of 'MARGIN' are names of dimensions")
+    }
+
     if (check.margin) {
         dimmargin <- dims[MARGIN]
         dimstats <- dim(STATS)
