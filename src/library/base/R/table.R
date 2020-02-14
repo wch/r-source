4d365348fb7a0476cce064a9489e229f344625c3
pd <pd@00db46b3-68df-0310-9c12-caf00c1e9a41>
2020-02-14 08:53:31 +0000

    fix case of apply losing dims
    git-svn-id: https://svn.r-project.org/R/trunk@77802 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/table.R
+++ src/library/base/R/table.R
@@ -264,6 +264,18 @@ marginSums <- function (x, margin = NULL)
       
    if (length(margin)) {
       z <- apply(x, margin, sum)
+      ## apply() may lose dims, in which case we need to put them
+      ## back. It is probably only strictly necessary if margin
+      ## has length 1. Need to convert margin to numeric for this 
+      ## to work, but can assume that x has named dimnames in that 
+      ## case (or apply() would have complained.
+      if (! is.array(z))
+      {
+        if (is.character(margin))
+            margin <- match(margin, names(dimnames(x)))
+        dim(z) <- dim(x)[margin] 
+        dimnames(z) <- dimnames(x)[margin]
+      }
       class(z) <- oldClass(x)
       z
    }
