641c3dd48b2b918cef16a34823a7b69f311e569c
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2020-03-07 15:56:54 +0000

    Fix PR#17729.
    git-svn-id: https://svn.r-project.org/R/trunk@77914 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/utils.R
+++ src/library/tools/R/utils.R
@@ -1152,15 +1152,28 @@ function(include_group_generics = TRUE)
 {
     if(include_group_generics)
         c(base::.S3PrimitiveGenerics,
-          "abs", "sign", "sqrt", "floor", "ceiling", "trunc", "round",
-          "signif", "exp", "log", "expm1", "log1p",
-          "cos", "sin", "tan", "acos", "asin", "atan",
-          "cosh", "sinh", "tanh", "acosh", "asinh", "atanh",
+          ## Keep this in sync with ? groupGeneric:
+          ## Group 'Math':
+          "abs", "sign", "sqrt",
+          "floor", "ceiling", "trunc",
+          "round", "signif",
+          "exp", "log", "expm1", "log1p",
+          "cos", "sin", "tan",
+          "cospi", "sinpi", "tanpi",
+          "acos", "asin", "atan",
+          "cosh", "sinh", "tanh",
+          "acosh", "asinh", "atanh",
           "lgamma", "gamma", "digamma", "trigamma",
           "cumsum", "cumprod", "cummax", "cummin",
-          "+", "-", "*", "/", "^", "%%", "%/%", "&", "|", "!", "==",
-          "!=", "<", "<=", ">=", ">",
+          ## Group 'Ops':
+          "+", "-", "*", "/",
+          "^", "%%", "%/%",
+          "&", "|", "!",
+          "==", "!=",
+          "<", "<=", ">=", ">",
+          ## Group 'Summary':
           "all", "any", "sum", "prod", "max", "min", "range",
+          ## Group 'Complex':
           "Arg", "Conj", "Im", "Mod", "Re")
     else
         base::.S3PrimitiveGenerics
