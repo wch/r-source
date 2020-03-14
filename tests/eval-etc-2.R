b3d79c300ddaf86b2bf204aa4ea6956ea2fa50a1
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2020-03-14 13:56:14 +0000

    version that does not need copying eval-fns.R
    git-svn-id: https://svn.r-project.org/R/trunk@77965 00db46b3-68df-0310-9c12-caf00c1e9a41

--- tests/eval-etc-2.R
+++ tests/eval-etc-2.R
@@ -3,8 +3,9 @@
 ####  Part 2
 ####  ======  Recommended packages allowed  .. output tests *sloppily*
 
-invisible(file.copy(file.path(Sys.getenv("SRCDIR"), "eval-fns.R"), "eval-fns.R"))
-source("./eval-fns.R", echo = TRUE)
+srcdir <- file.path(Sys.getenv("SRCDIR"), "eval-fns.R")
+source(if(file.exists(srcdir)) srcdir else "./eval-fns.R", echo = TRUE)
+rm("srcdir")
 
 if(require("Matrix")) withAutoprint({ cat("Trying some Matrix objects, too\n")
     D5. <- Diagonal(x = 5:1)
