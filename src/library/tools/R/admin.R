946ad5a0b0852926ef1f4de6c6e9dd5c69a26776
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2020-05-03 09:21:41 +0000

    Port c78311 from trunk.
    git-svn-id: https://svn.r-project.org/R/branches/R-4-0-branch@78344 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/admin.R
+++ src/library/tools/R/admin.R
@@ -148,7 +148,8 @@ function(db, verbose = FALSE)
         ## several packages have 'Depends: R', which is a noop.
         if(verbose && length(Rdeps) == 1L)
              message("WARNING: omitting pointless dependence on 'R' without a version requirement")
-        if(length(Rdeps) <= 1L) Rdeps <- NULL
+        if(length(Rdeps) <= 1L)
+            Rdeps2 <- Rdeps <- NULL
     } else Rdeps2 <- Rdeps <- NULL
     Rdeps <- as.vector(Rdeps)
     Suggests <- .split_dependencies(db[names(db) %in% "Suggests"])
