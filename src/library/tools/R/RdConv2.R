a0693d6f1eeeef6391a82f2119d3b5b68a3ad2d5
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2020-05-08 07:05:29 +0000

    more fsub(): no-semantic (nor white space) change of att.2599, PR#17728
    git-svn-id: https://svn.r-project.org/R/trunk@78390 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/RdConv2.R
+++ src/library/tools/R/RdConv2.R
@@ -1,7 +1,7 @@
 #  File src/library/tools/R/RdConv2.R
 #  Part of the R package, https://www.R-project.org
 #
-#  Copyright (C) 1995-2018 The R Core Team
+#  Copyright (C) 1995-2020 The R Core Team
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -1023,7 +1023,7 @@ toRd <- function(obj, ...)
     UseMethod("toRd")
 
 toRd.default <- function(obj, ...) {
-    fsub <- function(from, to, x) gsub(from, to, x, fixed=TRUE)
+    fsub <- function(from, to, x) gsub(from, to, x, fixed=TRUE)# useBytes=FALSE  {really?}
     fsub("%", "\\%",
      fsub("}", "\\}",
       fsub("{", "\\{",
