10f8657cbb340a59fdd95afdbdec231ed61e3629
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2020-05-14 22:47:18 +0000

    Rsockread now checks that maxlen is non-negative and uses a buffer on the heap instead of on the stack. Fixes PR 17801.
    git-svn-id: https://svn.r-project.org/R/trunk@78466 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/internet.c
+++ src/main/internet.c
@@ -226,8 +226,13 @@ SEXP Rsockconnect(SEXP sport, SEXP shost)
 SEXP Rsockread(SEXP ssock, SEXP smaxlen)
 {
     if (length(ssock) != 1) error("invalid 'socket' argument");
-    int sock = asInteger(ssock), maxlen = asInteger(smaxlen);
-    char buf[maxlen+1], *abuf[1];
+    int sock = asInteger(ssock);
+    int maxlen = asInteger(smaxlen);
+    if (maxlen < 0) /* also catches NA_INTEGER */
+	error(_("maxlen must be non-negative"));
+    SEXP rbuf = allocVector(RAWSXP, maxlen + 1);
+    PROTECT(rbuf);
+    char *buf = (char *) RAW(rbuf), *abuf[1];
     abuf[0] = buf;
     if(!initialized) internet_Init();
     if(initialized > 0)
@@ -238,7 +243,7 @@ SEXP Rsockread(SEXP ssock, SEXP smaxlen)
 	error("Error reading data in Rsockread");
     SEXP ans = PROTECT(allocVector(STRSXP, 1));
     SET_STRING_ELT(ans, 0, mkCharLen(buf, maxlen));
-    UNPROTECT(1);
+    UNPROTECT(2); /* rbuf, ans */
     return ans;
 		       
 }
