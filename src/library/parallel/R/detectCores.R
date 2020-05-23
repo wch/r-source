5a332c18259258d710a0e4dbce1a2faea8f21f47
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2020-05-23 13:04:58 +0000

    tweaks to parallel::detectCores
    git-svn-id: https://svn.r-project.org/R/branches/R-4-0-branch@78550 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/parallel/R/detectCores.R
+++ src/library/parallel/R/detectCores.R
@@ -1,7 +1,7 @@
 #  File src/library/parallel/R/detectCores.R
 #  Part of the R package, https://www.R-project.org
 #
-#  Copyright (C) 1995-2018 The R Core Team
+#  Copyright (C) 1995-2020 The R Core Team
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -30,16 +30,28 @@ detectCores <-
             ## Commoner OSes first
             ## for Linux systems, physical id is 1 for second hyperthread
             systems <-
-                list(linux = "grep ^processor /proc/cpuinfo 2>/dev/null | wc -l",
+                ## quoting needed for a Bourne shell
+                list(linux = 'grep "^processor" /proc/cpuinfo 2>/dev/null | wc -l',
                      ## hw.physicalcpu is not documented for 10.9, but works
                      darwin = if(logical) "/usr/sbin/sysctl -n hw.logicalcpu 2>/dev/null" else "/usr/sbin/sysctl -n hw.physicalcpu 2>/dev/null",
                      solaris = if(logical) "/usr/sbin/psrinfo -v | grep 'Status of.*processor' | wc -l" else "/bin/kstat -p -m cpu_info | grep :core_id | cut -f2 | uniq | wc -l",
                      freebsd = "/sbin/sysctl -n hw.ncpu 2>/dev/null",
                      openbsd = "/sbin/sysctl -n hw.ncpu 2>/dev/null",
                      irix  = c("hinv | grep Processors | sed 's: .*::'", "hinv | grep '^Processor '| wc -l"))
-            for (i in seq(systems))
-                if(all.tests ||
-		   length(grep(paste0("^", names(systems)[i]), R.version$os)))
+            nm <- names(systems)
+            m <- pmatch(nm, R.version$os); m <- nm[!is.na(m)]
+            if (length(m)) {
+                for (cmd in systems[[m]]) {
+                    if(!is.null(a <-
+                                    tryCatch(suppressWarnings(system(cmd, TRUE)),
+                                             error = function(e) NULL))) {
+                        a <- gsub("^ +","", a[1])
+                        if (grepl("^[1-9]", a)) return(as.integer(a))
+                    }
+                }
+            }
+            if (all.tests) {
+                for (i in seq(systems))
                     for (cmd in systems[i]) {
 			if(is.null(a <- tryCatch(suppressWarnings(system(cmd, TRUE)),
 						 error = function(e) NULL)))
@@ -47,6 +59,7 @@ detectCores <-
                         a <- gsub("^ +","", a[1])
                         if (grepl("^[1-9]", a)) return(as.integer(a))
                     }
+            }
             NA_integer_
         }
     }
