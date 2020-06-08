240810d69d74f16a482f67a3aaffad1182780cd5
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2020-06-08 06:15:16 +0000

    port r78598,78632 from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-4-0-branch@78655 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/check.R
+++ src/library/tools/R/check.R
@@ -5509,6 +5509,7 @@ add_dummies <- function(dir, Log)
                          "skipped",
                          "hdOnly",
                          "orphaned2", "orphaned",
+                         "required_for_checking_but_not_installed",
                          if(!check_incoming) "bad_engine")
             if(!all(names(res) %in% allowed)) {
                 errorLog(Log)
@@ -5523,6 +5524,10 @@ add_dummies <- function(dir, Log)
                 wrapLog(msg_DESCRIPTION)
                 summaryLog(Log)
                 do_exit(1L)
+            } else if (length(res$required_for_checking_but_not_installed)) {
+                warningLog(Log, "Skipping vignette re-building")
+                do_build_vignettes  <<- FALSE
+                printLog0(Log, paste(out, collapse = "\n"))
             } else {
                 if(length(res[["orphaned"]])) warningLog(Log) else noteLog(Log)
                 printLog0(Log, paste(out, collapse = "\n"))
