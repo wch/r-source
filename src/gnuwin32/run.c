750974ee8518857da4c8bf2773bc39da7c1dba59
kalibera <kalibera@00db46b3-68df-0310-9c12-caf00c1e9a41>
2020-09-24 13:21:39 +0000

    Give child process run via pipe on Windows some time to finish gracefully when closing the connection (reported by Brodie Gaslam).
    git-svn-id: https://svn.r-project.org/R/trunk@79250 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/run.c
+++ src/gnuwin32/run.c
@@ -755,6 +755,7 @@ rpipeGetc(rpipe * r)
 	/* we want to look for user break here */
 	while (peekevent()) doevent();
 	if (UserBreak) {
+	    /* FIXME: close handles */
 	    rpipeTerminate(r);
 	    break;
 	}
@@ -797,6 +798,23 @@ int rpipeClose(rpipe *r, int *timedout)
     int   i;
 
     if (!r) return NOLAUNCH;
+    /* Close both pipe ends before forcibly terminating the child process to
+       let it read all data (if it is reading) and exit gracefully.
+
+       r->write and r->read are set to hNULL for the case that threadedwait
+       ends up flushing file buffers
+
+       FIXME: should we be forcing the termination at all? */
+    HANDLE hNULL = CreateFile("NUL:", GENERIC_READ | GENERIC_WRITE, 0,
+                              NULL, OPEN_EXISTING, 0, NULL);
+    HANDLE tmp;
+    tmp = r->read;
+    r->read = hNULL;
+    CloseHandle(tmp);
+    tmp = r->write;
+    r->write = hNULL;
+    CloseHandle(tmp);
+
     rpipeTerminate(r);
     /* threadedwait may have obtained the exit code of the pipe process,
        but also may have been terminated too early; retrieve the exit
@@ -804,9 +822,8 @@ int rpipeClose(rpipe *r, int *timedout)
     DWORD ret;
     GetExitCodeProcess(r->pi.pi.hProcess, &ret);
     r->exitcode = ret;
-    CloseHandle(r->read);
-    CloseHandle(r->write);
     CloseHandle(r->pi.pi.hProcess);
+    CloseHandle(hNULL);
     i = r->exitcode;
     if (timedout)
 	*timedout = r->timedout;
