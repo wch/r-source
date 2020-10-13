6ea3ff33f2a6f4b629c902bebe12c9f115cd9774
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2020-10-13 07:27:19 +0000

    ported r79221 [all.equal.factor( <NA> )] from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-4-0-branch@79334 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/all.equal.R
+++ src/library/base/R/all.equal.R
@@ -230,14 +230,24 @@ all.equal.environment <- function (target, current, all.names=TRUE, ...) {
 
 all.equal.factor <- function(target, current, ..., check.attributes = TRUE)
 {
-    if(!inherits(target, "factor"))
-	return("'target' is not a factor")
     if(!inherits(current, "factor"))
 	return("'current' is not a factor")
     msg <-  if(check.attributes) attr.all.equal(target, current, ...)
-    n <- all.equal(as.character(target), as.character(current),
-                   check.attributes = check.attributes, ...)
-    if(is.character(n)) msg <- c(msg, n)
+    class(target) <- class(current) <- NULL
+    nax <- is.na(target)
+    nay <- is.na(current)
+    n <- sum(nax != nay)
+    if(n > 1L)
+	msg <- c(msg, paste(n, "NA mismatches"))
+    else if (n == 1L)
+        msg <- c(msg, paste("1, NA mismatch"))
+    else { ## n == 0: nax == nay (w/ recycling!)
+	target  <- levels(target) [target [!nax]]
+	current <- levels(current)[current[!nay]]
+	n <- all.equal(target, current,
+		       check.attributes = check.attributes, ...)
+	if(is.character(n)) msg <- c(msg, n)
+    }
     if(is.null(msg)) TRUE else msg
 }
 
