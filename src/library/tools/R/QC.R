2d0229b1118844650a63b9385eee0d989323e17f
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2020-10-14 15:13:42 +0000

    Tweak.
    git-svn-id: https://svn.r-project.org/R/trunk@79338 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/QC.R
+++ src/library/tools/R/QC.R
@@ -4702,8 +4702,9 @@ function(x, ...)
 function(fileName, pkgname)
 {
     oldSearch <- search()
-    dataEnv <- new.env(hash = TRUE);
-    utils::data(list = fileName, package = pkgname, envir = dataEnv);
+    dataEnv <- new.env(hash = TRUE)
+    suppressMessages(utils::data(list = fileName, package = pkgname,
+                                 envir = dataEnv))
     if (!length((ls(dataEnv)))) message("No dataset created in 'envir'")
     if (!identical(search(), oldSearch)) message("Search path was changed")
     invisible(NULL)
