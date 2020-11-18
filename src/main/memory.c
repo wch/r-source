bed3b1da0e5a818c0f1a3eb469e89e4e7ade2aca
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2020-11-18 21:03:40 +0000

    Merged r79446 (bump REFCNTs when creating external pointers) from trunk.
    git-svn-id: https://svn.r-project.org/R/branches/R-4-0-branch@79447 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/memory.c
+++ src/main/memory.c
@@ -3619,8 +3619,8 @@ SEXP R_MakeExternalPtr(void *p, SEXP tag, SEXP prot)
 {
     SEXP s = allocSExp(EXTPTRSXP);
     EXTPTR_PTR(s) = p;
-    EXTPTR_PROT(s) = CHK(prot);
-    EXTPTR_TAG(s) = CHK(tag);
+    EXTPTR_PROT(s) = CHK(prot); if (prot) INCREMENT_REFCNT(prot);
+    EXTPTR_TAG(s) = CHK(tag); if (tag) INCREMENT_REFCNT(tag);
     return s;
 }
 
@@ -3675,8 +3675,8 @@ SEXP R_MakeExternalPtrFn(DL_FUNC p, SEXP tag, SEXP prot)
     SEXP s = allocSExp(EXTPTRSXP);
     tmp.fn = p;
     EXTPTR_PTR(s) = tmp.p;
-    EXTPTR_PROT(s) = CHK(prot);
-    EXTPTR_TAG(s) = CHK(tag);
+    EXTPTR_PROT(s) = CHK(prot); if (prot) INCREMENT_REFCNT(prot);
+    EXTPTR_TAG(s) = CHK(tag); if (tag) INCREMENT_REFCNT(tag);
     return s;
 }
 
