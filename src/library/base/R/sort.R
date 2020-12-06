8696236cdc7a2be91605f048164e23430aabf0b1
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2020-12-06 13:22:30 +0000

    Have xtfrm.default() check its value for correct length and type.
    git-svn-id: https://svn.r-project.org/R/trunk@79572 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/sort.R
+++ src/library/base/R/sort.R
@@ -263,9 +263,15 @@ Have you called 'sort' on a list?")
 
 ## xtfrm is now primitive
 ## xtfrm <- function(x) UseMethod("xtfrm")
-xtfrm.default <- function(x)
-    if(is.numeric(x)) unclass(x) else as.vector(rank(x, ties.method = "min",
-                                                     na.last = "keep"))
+xtfrm.default <- function(x) {
+    y <- if(is.numeric(x))
+             unclass(x)
+         else as.vector(rank(x, ties.method = "min", na.last = "keep"))
+    if(!is.numeric(y) || (length(y) != length(x)))
+        stop("cannot xtfrm 'x'")
+    y
+}
+
 xtfrm.factor <- function(x) as.integer(x) # primitive, so needs a wrapper
 
 ## ## Moved to package survival
