2a57fd6741e96e59088b9c055a4001e737c29f66
murrell <murrell@00db46b3-68df-0310-9c12-caf00c1e9a41>
2021-01-22 02:06:15 +0000

    shift checking of plot(type=) to R code (as a simple fix to avoid truncating non-ASCII values in C code)
    git-svn-id: https://svn.r-project.org/R/trunk@79855 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/graphics/R/plot.R
+++ src/library/graphics/R/plot.R
@@ -347,8 +347,19 @@ text.formula <- function(formula, data = parent.frame(), ..., subset)
 plot.xy <- function(xy, type, pch = par("pch"), lty = par("lty"),
                     col = par("col"), bg = NA, cex = 1, lwd = par("lwd"),
                     ...)
-    invisible(.External.graphics(C_plotXY, xy, type, pch, lty, col, bg, cex, lwd, ...))
-
+{
+    if (is.null(type)) type <- "p"
+    type <- as.character(type)
+    if (length(type) != 1L || !nzchar(type) || is.na(type))
+        stop(gettextf("invalid plot type"))
+    if (nchar(type) > 1L)
+        warning(gettextf("plot type '%s' will be truncated to first character",
+                         type))
+    t <- substr(type, 1L, 1L)
+    if (!isTRUE(t %in% c("l", "o", "b", "c", "s", "S", "h", "p", "n")))
+        stop(gettextf("invalid plot type '%s'", t))
+    invisible(.External.graphics(C_plotXY, xy, t, pch, lty, col, bg, cex, lwd, ...))
+}
 
 plot.new <- function()
 {
