e3933c9b81cd1b16c7da665a48bc38fd7ef8eaff
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2021-02-01 10:31:11 +0000

    do not by default report missing namespaces with where="<unknown>"
    git-svn-id: https://svn.r-project.org/R/trunk@79914 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/namespace.R
+++ src/library/base/R/namespace.R
@@ -34,17 +34,17 @@ getNamespace <- function(name) {
 ..getNamespace <- function(name, where) {
     ns <- .Internal(getRegisteredNamespace(name))
     if (!is.null(ns)) ns
-    else tryCatch(loadNamespace(name),
-                  error = function(e) {
-                    if(!nzchar(Sys.getenv("_R_NO_REPORT_MISSING_NAMESPACES_"))) {
-                       warning(gettextf("namespace %s is not available and has been replaced\nby .GlobalEnv when processing object %s",
-                                        sQuote(name)[1L], sQuote(where)),
-                               domain = NA, call. = FALSE, immediate. = TRUE)
-                       if(nzchar(Sys.getenv("_R_CALLS_MISSING_NAMESPACES_")))
-                           print(sys.calls())
-                    }
-                    .GlobalEnv
-                  })
+    else tryCatch(loadNamespace(name), error = function(e) {
+             tr <- Sys.getenv("_R_NO_REPORT_MISSING_NAMESPACES_")
+             if( tr == "false" || (where != "<unknown>" && !nzchar(tr)) ) {
+                 warning(gettextf("namespace %s is not available and has been replaced\nby .GlobalEnv when processing object %s",
+                                  sQuote(name)[1L], sQuote(where)),
+                         domain = NA, call. = FALSE, immediate. = TRUE)
+                 if(nzchar(Sys.getenv("_R_CALLS_MISSING_NAMESPACES_")))
+                     print(sys.calls())
+             }
+             .GlobalEnv
+         })
 }
 
 loadedNamespaces <- function() names(.Internal(getNamespaceRegistry()))
