768c3c6f3ac618a3b319b06094ba8af010a3aef8
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2021-02-15 15:46:08 +0000

    Fix PR#18052.  By Sebastian Meyer <seb.meyer@fau.de>.
    git-svn-id: https://svn.r-project.org/R/trunk@80007 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/tests/Rd2HTML.R
+++ src/library/tools/tests/Rd2HTML.R
@@ -1,6 +1,35 @@
+require("tools")
+
 x <- tools::Rd_db("base")
 system.time(y <- lapply(x, function(e)
     tryCatch(tools::Rd2HTML(e, out = nullfile()),
 	     error = identity))) # 3-5 sec
 stopifnot(!vapply(y, inherits, NA, "error"))
 ## Gave error when "running" \Sexpr{.} DateTimeClasses.Rd
+
+
+## PR#18052: \dots must not be interpreted inside \preformatted
+Rdsnippet <- tempfile()
+writeLines(r"(\preformatted{
+\item{\dots}{foo(arg = "\\\\dots", ...)}
+})", Rdsnippet)
+#file.show(Rdsnippet)
+stopifnot(exprs = {
+    identical(capture.output(Rd2HTML(Rdsnippet, fragment = TRUE))[2L],
+              r"(\item{\dots}{foo(arg = "\\dots", ...)})")
+    identical(capture.output(Rd2txt(Rdsnippet, fragment = TRUE))[2L],
+              r"(\item{\dots}{foo(arg = "\\dots", ...)})")
+    identical(capture.output(Rd2latex(Rdsnippet, fragment = TRUE))[2L],
+              r"(\bsl{}item\{\bsl{}dots\}\{foo(arg = "\bsl{}\bsl{}dots", ...)\})")
+}) # the last two failed in R < 4.1.0
+
+## also do not translate \dots in R code lines in \examples
+Rdsnippet <- tempfile()
+writeLines(r"(\examples{
+foo <- function(arg = "\\\\dots", ...) NULL # \dots
+})", Rdsnippet)
+#file.show(Rdsnippet)
+stopifnot(exprs = {
+    identical(capture.output(Rd2ex(parse_Rd(Rdsnippet), fragment = TRUE))[5L],
+              r"(foo <- function(arg = "\\dots", ...) NULL # \dots)")
+}) # failed in R < 4.1.0
