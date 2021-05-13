3f5c798bff014c1b3e6b585c720bdf538d53216a
kalibera <kalibera@00db46b3-68df-0310-9c12-caf00c1e9a41>
2021-05-13 15:21:59 +0000

    Use macro to build on Solaris.
    git-svn-id: https://svn.r-project.org/R/trunk@80292 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/Rdynload.c
+++ src/main/Rdynload.c
@@ -239,10 +239,10 @@ static void initLoadedDLL()
     R_PreserveObject(SymbolEptrs);
 }
 
+#define MAXCOUNT 10
 static void
 R_registerSymbolEptr(SEXP eptr, SEXP einfo)
 {
-    const int MAXCOUNT = 10;
     static int cleancount = MAXCOUNT;
 
     /* remove unneeded entries from the list */
