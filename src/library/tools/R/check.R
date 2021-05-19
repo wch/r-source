95011a3501f715f449498649a6cf30379b5d23c4
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2021-05-19 03:47:15 +0000

    add _R_CHECK_MATRIX_DATA_
    git-svn-id: https://svn.r-project.org/R/trunk@80325 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/check.R
+++ src/library/tools/R/check.R
@@ -6226,6 +6226,8 @@ add_dummies <- function(dir, Log)
         Sys.setenv("_R_OPTIONS_STRINGS_AS_FACTORS_" = "FALSE")
 ##        Sys.setenv("_R_CHECK_XREFS_PKGS_ARE_DECLARED_" = "TRUE")
 ##        Sys.setenv("_R_CHECK_XREFS_MIND_SUSPECT_ANCHORS_" = "TRUE")
+        prev = Sys.getenv("_R_CHECK_MATRIX_DATA_",  NA_character_)
+        if(is.na(prev)) Sys.setenv("_R_CHECK_MATRIX_DATA_" = "TRUE")
         R_check_vc_dirs <- TRUE
         R_check_executables_exclusions <- FALSE
         R_check_doc_sizes2 <- TRUE
