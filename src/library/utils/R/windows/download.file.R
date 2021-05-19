54e98dde0bfedb7d9d1e063444729120d7c7f2f9
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2021-05-19 18:20:15 +0000

    typo
    git-svn-id: https://svn.r-project.org/R/trunk@80339 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/utils/R/windows/download.file.R
+++ src/library/utils/R/windows/download.file.R
@@ -35,7 +35,7 @@ download.file <-
         if(length(url) != 1L || typeof(url) != "character")
             stop("'url' must be a length-one character vector");
         ## as from 4.2.0 always have libcurl
-	method <- if(startsWith(url, "file:") "wininet" else "libcurl"
+	method <- if(startsWith(url, "file:")) "wininet" else "libcurl"
     }
 
     nh <- names(headers)
