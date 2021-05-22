a944e2b867d9fb0982da43b6cd6c7eb766740889
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2021-05-22 10:07:16 +0000

    clean up for rare (and untested) OSes
    git-svn-id: https://svn.r-project.org/R/trunk@80355 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/printutils.c
+++ src/main/printutils.c
@@ -752,7 +752,7 @@ const char *EncodeString(SEXP s, int w, int quote, Rprt_adj justify)
 			for(int j = 0; j < res; j++) *q++ = *p++;
 		    } else {
 # if !defined (__STDC_ISO_10646__) && !defined (Win32)
-			if(!use_ucs) Unicode_warning = TRUE;
+			Unicode_warning = TRUE;
 # endif
 			if(k > 0xffff)
 			    snprintf(buf, 13, "\\U{%06x}", k);
