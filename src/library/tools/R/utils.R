a2f5f7d18e0170af5e2271d84ab4e290aa7037b8
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2021-05-31 09:02:02 +0000

    Add file_path_relative_to().
    git-svn-id: https://svn.r-project.org/R/trunk@80419 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/utils.R
+++ src/library/tools/R/utils.R
@@ -44,6 +44,44 @@ function(x)
     normalizePath(epath, "/", TRUE)
 }
 
+### ** file_path_relative_to
+
+file_path_relative_to <-
+function(x, start = getwd(), parent = TRUE)
+{
+    x <- normalizePath(x, "/", mustWork = FALSE)
+    if(!parent) {
+        p <- normalizePath(start[1L], "/", mustWork = TRUE)
+        if(any(i <- startsWith(x, p))) {
+            ## Assume .Platform$file.sep is a single character.
+            x[i] <- substring(x[i], nchar(p) + 2L)
+        }
+        x
+    } else {
+        p <- strsplit(normalizePath(start, "/", mustWork = FALSE),
+                      "/", fixed = TRUE)[[1L]]
+        y <- strsplit(x, "/", fixed = TRUE)
+        f <- function(u, v) {
+            i <- 1L
+            while(i <= min(length(v), length(p))) {
+                if(v[i] == p[i])
+                    i <- i + 1L
+                else
+                    break
+            }
+            if(i == 1L) {
+                ## Paths start differently, so relative cannot work
+                u
+            } else {
+                i <- i - 1L
+                paste(c(rep_len("..", length(p) - i), v[-seq_len(i)]),
+                      collapse = .Platform$file.sep)
+            }
+        }
+        unlist(Map(f, x, y, USE.NAMES = FALSE))
+    }
+}
+    
 ### ** file_path_sans_ext
 
 file_path_sans_ext <-
