5cd95706d258859f6e5fcb34f28ba32acbb6c8d0
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2021-08-21 08:10:43 +0000

    use snprintf in PROBLEM and MESSAGE
    git-svn-id: https://svn.r-project.org/R/trunk@80805 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/include/R_ext/RS.h
+++ src/include/R_ext/RS.h
@@ -55,8 +55,8 @@ extern "C" {
     
 #define R_PROBLEM_BUFSIZE	4096
 /* Parentheses added for FC4 with gcc4 and -D_FORTIFY_SOURCE=2 */
-#define PROBLEM			{char R_problem_buf[R_PROBLEM_BUFSIZE];(sprintf)(R_problem_buf,
-#define MESSAGE                 {char R_problem_buf[R_PROBLEM_BUFSIZE];(sprintf)(R_problem_buf,
+#define PROBLEM			{char R_problem_buf[R_PROBLEM_BUFSIZE];(snprintf)(R_problem_buf, R_PROBLEM_BUFSIZE,
+#define MESSAGE                 {char R_problem_buf[R_PROBLEM_BUFSIZE];(snprintf)(R_problem_buf, R_PROBLEM_BUFSIZE,
 #define ERROR			),Rf_error(R_problem_buf);}
 #define RECOVER(x)		),Rf_error(R_problem_buf);}
 #define WARNING(x)		),Rf_warning(R_problem_buf);}
