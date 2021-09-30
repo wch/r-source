e1e395544105e46f6ed6167ae2d3385862bccf0e
urbaneks <urbaneks@00db46b3-68df-0310-9c12-caf00c1e9a41>
2021-09-30 04:15:19 +0000

    R_NO_SEGV_HANDLER=1 disables SEGV handler UI
    git-svn-id: https://svn.r-project.org/R/trunk@80992 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/main.c
+++ src/main/main.c
@@ -653,25 +653,32 @@ static void *signal_stack;
 #define R_USAGE 100000 /* Just a guess */
 static void init_signal_handlers(void)
 {
-    /* <FIXME> may need to reinstall this if we do recover. */
-    struct sigaction sa;
-    signal_stack = malloc(SIGSTKSZ + R_USAGE);
-    if (signal_stack != NULL) {
-	sigstk.ss_sp = signal_stack;
-	sigstk.ss_size = SIGSTKSZ + R_USAGE;
-	sigstk.ss_flags = 0;
-	if(sigaltstack(&sigstk, NULL) < 0)
-	    warning("failed to set alternate signal stack");
-    } else
-	warning("failed to allocate alternate signal stack");
-    sa.sa_sigaction = sigactionSegv;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = SA_ONSTACK | SA_SIGINFO;
-    sigaction(SIGSEGV, &sa, NULL);
-    sigaction(SIGILL, &sa, NULL);
+    /* Do not set the (since 2005 experimantal) SEGV handler
+       UI if R_NO_SEGV_HANDLER env var is non-empty.
+       This is needed to debug crashes in the handler
+       (which happen as they involve the console interface). */
+    const char *val = getenv("R_NO_SEGV_HANDLER");
+    if (!val || !*val) {
+	/* <FIXME> may need to reinstall this if we do recover. */
+	struct sigaction sa;
+	signal_stack = malloc(SIGSTKSZ + R_USAGE);
+	if (signal_stack != NULL) {
+	    sigstk.ss_sp = signal_stack;
+	    sigstk.ss_size = SIGSTKSZ + R_USAGE;
+	    sigstk.ss_flags = 0;
+	    if(sigaltstack(&sigstk, NULL) < 0)
+		warning("failed to set alternate signal stack");
+	} else
+	    warning("failed to allocate alternate signal stack");
+	sa.sa_sigaction = sigactionSegv;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_ONSTACK | SA_SIGINFO;
+	sigaction(SIGSEGV, &sa, NULL);
+	sigaction(SIGILL, &sa, NULL);
 #ifdef SIGBUS
-    sigaction(SIGBUS, &sa, NULL);
+	sigaction(SIGBUS, &sa, NULL);
 #endif
+    }
 
     signal(SIGINT,  handleInterrupt);
     signal(SIGUSR1, onsigusr1);
