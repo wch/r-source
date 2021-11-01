f62918147685a0111981a6915e02c2cffff40d8c
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2021-11-01 15:24:20 +0000

    port r81106 from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-4-1-branch@81123 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/utils/R/sessionInfo.R
+++ src/library/utils/R/sessionInfo.R
@@ -67,9 +67,9 @@
                                       "13" = "High Sierra",
                                       "14" = "Mojave",
                                       "15" = "Catalina",
-                                      ## used for early pre-releases
+                                      ## used for early pre-releases of Big Sur
                                       ## and still reported by Xcode 10's SDK
-                                      "16" = "Big Sur",
+                                      "16" = "Big Sur/Monterey",
                                       ""),
                                ver)
                    else if(ver1[1L] <= "12")
