9217adda4337e4d7e71b427b4aee875568a5f7f3
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2022-02-03 10:45:43 +0000

    Update for search.R-project.org relaunch.  By Gennadiy Starostin.
    git-svn-id: https://svn.r-project.org/R/trunk@81644 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/utils/R/RSiteSearch.R
+++ src/library/utils/R/RSiteSearch.R
@@ -1,7 +1,7 @@
 #  File src/library/utils/R/RSiteSearch.R
 #  Part of the R package, https://www.R-project.org
 #
-#  Copyright (C) 1995-2015 The R Core Team
+#  Copyright (C) 1995-2022 The R Core Team
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -17,40 +17,63 @@
 #  https://www.R-project.org/Licenses/
 
 RSiteSearch <- function(string,
-                        restrict = c("functions", "vignettes", "views"),
-			format = c("normal", "short"),
+      restrict = c("functions", "descriptions", "news", "Rfunctions",
+                   "Rmanuals", "READMEs", "views", "vignettes"),
+      format,
 			sortby = c("score", "date:late", "date:early",
-			"subject", "subject:descending",
-			"from", "from:descending", "size", "size:descending"),
-			matchesPerPage = 20)
+			           "subject", "subject:descending",
+		             "size", "size:descending"),
+			matchesPerPage = 20,
+			words = c("all", "any"))
 {
-    string <- paste0("http://search.r-project.org/cgi-bin/namazu.cgi?query=",
-		     URLencode(gsub(" ", "+", string, fixed=TRUE), reserved = TRUE))
-    mpp <- paste0("max=", matchesPerPage)
-    format <- paste0("result=", match.arg(format))
+    matchingDB = list(functions = "cran-help", descriptions = "cran-info", news = "cran-news",
+                      Rfunctions = "r-help", Rmanuals = "r-manuals", READMEs = "cran-readme",
+                      views = "cran-views", vignettes = "cran-vignettes")
 
-    restrictVALS <- c("functions", "vignettes", "views")
-    restr <- match.arg(restrict, choices = restrictVALS, several.ok = TRUE)
-    restr <- paste(paste0("idxname=", restr), collapse = "&")
+    ## Currently search.r-project.org uses double qoutes for phrase searching.
+    ## Fomerly (before 2021) it was braces, {}.
+    ## For compatibility reasons, we convert them to double qoutes, "".
+    ## It also might be a convenient notation to keep.
+    string <- paste0("https://search.r-project.org/?FMT=query&P=",
+		     gsub("%22", '"', gsub("%20", "+",
+		          URLencode(gsub("[\\{\\}]", '"', trimws(string)), reserved = TRUE),
+		        fixed = TRUE), fixed = TRUE)
+              )
+
+    mpp <- paste0("HITSPERPAGE=", matchesPerPage)
+
+    restr <- match.arg(restrict, several.ok = TRUE)
+    if(length(restr) != length(restrict))
+        warning("some options in argument 'restrict' were not recognized and hence ignored")
+    restr <- paste(paste0("DB=", matchingDB[restr]), collapse = "&")
 
     sortby <- match.arg(sortby)
-    sortby <- paste0("sort=",
+    sortby <- paste0("SORT=",
 		     switch(sortby,
-			    "score"=, "date:late"=, "date:early" = sortby,
-			    "subject"		 = "field:subject:ascending",
-			    "subject:descending" = "field:subject:descending",
-			    "from"		 = "field:from:ascending",
-			    "from:descending"	 = "field:from:descending",
-			    "size"		 = "field:size:ascending",
-			    "size:descending"	 = "field:size:descending"))
+		            "score" = "",
+		            "date:late" = "-0",
+		            "date:early" = "%2B0",
+		            "subject" = "%2B3",
+		            "subject:descending" = "-3",
+		            "size" = "%2B2",
+		            "size:descending" = "-2")
+		          )
 
-    ## we know this is a http:// URL, so encoding should be safe.
-    ## it seems that firefox on macOS needs it for {...}
-    ## OTOH, Namazu does not decode in, say, sort=date:late.
-    qstring <- paste(string, mpp, format, sortby, restr, sep = "&")
+    words <- match.arg(words)
+    words <- paste0("DEFAULTOP=",
+          switch(words,
+                "all" = "and",
+                "any" = "or")
+              )
+
+    qstring <- paste(string, mpp, sortby, restr, words, sep = "&")
     browseURL(qstring)
+    
+    if(!missing("format"))
+        warning("argument 'format' is deprecated and has no affect on search results")
+    
     cat(gettextf("A search query has been submitted to %s",
-                 "http://search.r-project.org"), "\n", sep = "")
+                 "https://search.r-project.org"), "\n", sep = "")
     cat(gettext("The results page should open in your browser shortly\n"))
     invisible(qstring)
 }
