c7970d5f45bdcbc054a834d92f2fdb555798e70e
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2022-02-15 10:10:13 +0000

    Enhance ks.test() to provide exact p-values also with ties and MC p-values in the two-sample (Smirnov) case.  By Torsten Hothorn.
    git-svn-id: https://svn.r-project.org/R/trunk@81742 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/R/ks.test.R
+++ src/library/stats/R/ks.test.R
@@ -1,7 +1,7 @@
 #  File src/library/stats/R/ks.test.R
 #  Part of the R package, https://www.R-project.org
 #
-#  Copyright (C) 1995-2019 The R Core Team
+#  Copyright (C) 1995-2022 The R Core Team
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -20,7 +20,7 @@ ks.test <- function(x, ...) UseMethod("ks.test")
 
 ks.test.default <-
     function(x, y, ..., alternative = c("two.sided", "less", "greater"),
-             exact = NULL)
+             exact = NULL, simulate.p.value = FALSE, B = 2000)
 {
     alternative <- match.arg(alternative)
     DNAME <- deparse1(substitute(x))
@@ -30,7 +30,14 @@ ks.test.default <-
         stop("not enough 'x' data")
     PVAL <- NULL
 
+    ### ordered variables can be treated as numeric ones as ties are handled
+    ### now
+    if (is.ordered(y)) y <- unclass(y)
+
     if(is.numeric(y)) { ## two-sample case
+        args <- list(...)
+        if (length(args) > 0L) 
+            warning("Parameter(s) ", paste(names(args), collapse = ", "), " ignored")
         DNAME <- paste(DNAME, "and", deparse1(substitute(y)))
         y <- y[!is.na(y)]
         n.x <- as.double(n)             # to avoid integer overflow
@@ -39,19 +46,21 @@ ks.test.default <-
             stop("not enough 'y' data")
         if(is.null(exact))
             exact <- (n.x * n.y < 10000)
-        METHOD <- "Two-sample Kolmogorov-Smirnov test"
+        if (!simulate.p.value) {
+            METHOD <- paste(c("Asymptotic", "Exact")[exact + 1L], 
+                            "two-sample Smirnov test")
+        } else {
+            METHOD <- "Monte-Carlo two-sample Smirnov test"
+        }
         TIES <- FALSE
         n <- n.x * n.y / (n.x + n.y)
         w <- c(x, y)
         z <- cumsum(ifelse(order(w) <= n.x, 1 / n.x, - 1 / n.y))
         if(length(unique(w)) < (n.x + n.y)) {
-            if (exact) {
-                warning("cannot compute exact p-value with ties")
-                exact <- FALSE
-            } else
-                warning("p-value will be approximate in the presence of ties")
             z <- z[c(which(diff(sort(w)) != 0), n.x + n.y)]
             TIES <- TRUE
+            if (!exact & !simulate.p.value)
+                warning("p-value will be approximate in the presence of ties")
         }
         STATISTIC <- switch(alternative,
                             "two.sided" = max(abs(z)),
@@ -61,20 +70,32 @@ ks.test.default <-
                                  "two.sided" = "two-sided",
                                  "less" = "the CDF of x lies below that of y",
                                  "greater" = "the CDF of x lies above that of y")
-        if(exact && (alternative == "two.sided") && !TIES)
-            PVAL <- 1 - .Call(C_pSmirnov2x, STATISTIC, n.x, n.y)
+        z <- NULL
+        if (TIES)
+            z <- w
+        PVAL <- switch(alternative,
+            "two.sided" = psmirnov(STATISTIC, m = n.x, n = n.y, z = w, exact = exact, 
+                                   simulate = simulate.p.value, B = B,
+                                   lower.tail = FALSE),
+            "less" = psmirnov(STATISTIC, m = n.x, n = n.y, z = w,  exact = exact,
+                              simulate = simulate.p.value, B = B,
+                              two.sided = FALSE, lower.tail = FALSE),
+            "greater" = psmirnov(STATISTIC, m = n.x, n = n.y, z = w, exact = exact,
+                                 simulate = simulate.p.value, B = B,
+                                 two.sided = FALSE, lower.tail = FALSE))
     } else { ## one-sample case
         if(is.character(y)) # avoid matching anything in this function
             y <- get(y, mode = "function", envir = parent.frame())
         if(!is.function(y))
             stop("'y' must be numeric or a function or a string naming a valid function")
-        METHOD <- "One-sample Kolmogorov-Smirnov test"
         TIES <- FALSE
         if(length(unique(x)) < n) {
             warning("ties should not be present for the Kolmogorov-Smirnov test")
             TIES <- TRUE
         }
         if(is.null(exact)) exact <- (n < 100) && !TIES
+        METHOD <- paste(c("Asymptotic", "Exact")[exact + 1L], 
+                        "one-sample Kolmogorov-Smirnov test")
         x <- y(sort(x), ...) - (0 : (n-1)) / n
         STATISTIC <- switch(alternative,
                             "two.sided" = max(c(x, 1/n - x)),
@@ -97,6 +118,10 @@ ks.test.default <-
                 }
                 pkolmogorov1x(STATISTIC, n)
             }
+        } else {
+            PVAL <- if(alternative == "two.sided")
+                        1 - .Call(C_pKS2, sqrt(n) * STATISTIC, tol = 1e-6)
+                    else exp(- 2 * n * STATISTIC^2)
         }
         nm_alternative <-
             switch(alternative,
@@ -110,42 +135,16 @@ ks.test.default <-
                                "greater" = "D^+",
                                "less" = "D^-")
 
-    if(is.null(PVAL)) { ## so not exact
-        pkstwo <- function(x, tol = 1e-6) {
-            ## Compute \sum_{-\infty}^\infty (-1)^k e^{-2k^2x^2}
-            ## Not really needed at this generality for computing a single
-            ## asymptotic p-value as below.
-            if(is.numeric(x)) x <- as.double(x)
-            else stop("argument 'x' must be numeric")
-            p <- rep(0, length(x))
-            p[is.na(x)] <- NA
-            IND <- which(!is.na(x) & (x > 0))
-            if(length(IND)) p[IND] <- .Call(C_pKS2, p = x[IND], tol)
-            p
-        }
-        ## <FIXME>
-        ## Currently, p-values for the two-sided two-sample case are
-        ## exact if n.x * n.y < 10000 (unless controlled explicitly).
-        ## In all other cases, the asymptotic distribution is used
-        ## directly.  But: let m and n be the min and max of the sample
-        ## sizes, respectively.  Then, according to Kim and Jennrich
-        ## (1973), if m < n/10, we should use the
-        ## * Kolmogorov approximation with c.c. -1/(2*n) if 1 < m < 80;
-        ## * Smirnov approximation with c.c. 1/(2*sqrt(n)) if m >= 80.
-        PVAL <- if(alternative == "two.sided")
-                    1 - pkstwo(sqrt(n) * STATISTIC)
-                else exp(- 2 * n * STATISTIC^2)
-        ## </FIXME>
-    }
-
     ## fix up possible overshoot (PR#14671)
     PVAL <- min(1.0, max(0.0, PVAL))
     RVAL <- list(statistic = STATISTIC,
                  p.value = PVAL,
                  alternative = nm_alternative,
                  method = METHOD,
-                 data.name = DNAME)
-    class(RVAL) <- "htest"
+                 data.name = DNAME,
+                 data = list(x = x, y = y), # FIXME confband
+                 exact = exact)             # FIXME confband
+    class(RVAL) <- c("ks.test", "htest")
     return(RVAL)
 }
 
@@ -192,3 +191,178 @@ function(formula, data, subset, na.action, ...)
     y$data.name <- DNAME
     y
 }
+
+
+psmirnov <-
+function(q, m, n = length(z) - m, z = NULL, 
+         two.sided = TRUE, exact = TRUE, simulate = FALSE, B = 2000,
+         lower.tail = TRUE, log.p = FALSE) {
+
+    ##
+    ## Distribution function Prob(D < q) for the two-sample Smirnov test
+    ## statistic
+    ##
+    ##   D = sup_c | ECDF_x(c) - ECDF_y(c) | 	(two.sided)
+    ##
+    ##   D = sup_c ( ECDF_x(c) - ECDF_y(c) ) 	(!two.sided)
+    ##
+    ## Implementation translated from APL code in Appendix C.2.3 of
+    ##
+    ##   Gunar Schröer, Computergestützte statistische Inferenz am
+    ##   Beispiel der Kolmogorov-Smirnov Tests,
+    ##   Diplomarbeit Universität Osnabrück, 1991
+    ##
+    ## see also
+    ##     
+    ##   Gunar Schröer and Dietrich Trenkler (1995),
+    ##   Exact and Randomization Distributions of Kolmogorov-Smirnov
+    ##   Tests for Two or Three Samples,
+    ##   Computational Statistics & Data Analysis, 20, 185--202
+    ##
+
+    if (is.numeric(q)) 
+        q <- as.double(q)
+    else stop("argument 'q' must be numeric")
+    ret <- rep(0, length(q))
+    ret[is.na(q) | q < 0 | q > 1] <- NA
+    IND <- which(!is.na(ret))
+    if (!length(IND)) return(ret)
+    if (m < 1) stop("not enough 'x' data")
+    if (n < 1) stop("not enough 'y' data")
+    n.x <- floor(m)
+    n.y <- floor(n)
+    N <- n.x + n.y
+    n <- n.x * n.y / (n.x + n.y)
+
+    if (!exact) {
+        if (simulate) {
+            if (is.null(z)) {
+                rt <- rep(1, length = N)
+            } else {
+                rt <- table(z)
+            }
+            Dsim <- .Call(C_Smirnov_sim,
+                          as.integer(rt),
+                          as.integer(c(n.x, n.y)),
+                          as.integer(B),
+                          as.integer(two.sided))
+            ### need P(D < q)
+            ret[IND] <- ecdf(Dsim)(q - sqrt(.Machine$double.eps)) 
+            if (log.p & lower.tail)
+                return(log(ret))
+            if (!log.p & lower.tail)
+                return(ret)
+            if (log.p & !lower.tail)
+                return(log1p(-ret))
+            if (!log.p & !lower.tail)
+                return(1 - ret)
+        }
+        ## <FIXME> let m and n be the min and max of the sample
+        ## sizes, respectively.  Then, according to Kim and Jennrich
+        ## (1973), if m < n/10, we should use the
+        ## * Kolmogorov approximation with c.c. -1/(2*n) if 1 < m < 80;
+        ## * Smirnov approximation with c.c. 1/(2*sqrt(n)) if m >= 80.
+        if (two.sided) {
+            ret[IND] <- .Call(C_pKS2, p = sqrt(n) * q[IND], tol = 1e-6)
+            ### note: C_pKS2(0) = NA but Prob(D < 0) = 0
+            ret[q < .Machine$double.eps] <- 0
+        } else {
+            ret[IND] <- 1 - exp(- 2 * n * q^2)
+        }
+        if (log.p & lower.tail)
+            return(log(ret))
+        if (!log.p & lower.tail)
+            return(ret)
+        if (log.p & !lower.tail)
+            return(log1p(-ret))
+        if (!log.p & !lower.tail)
+            return(1 - ret)
+    }
+
+    ### no ties, use C_pSmirnov2x
+    if (is.null(z)) {
+        ret[IND] <- sapply(q[IND],
+                           function(x) .Call(C_pSmirnov2x, x, n.x, n.y))
+        if (log.p & lower.tail)
+            return(log(ret))
+        if (!log.p & lower.tail)
+            return(ret)
+        if (log.p & !lower.tail)
+            return(log1p(-ret))
+        if (!log.p & !lower.tail)
+            return(1 - ret)
+    }
+
+    TIES <- if (!is.null(z))
+        c(diff(sort(z)) != 0, TRUE)
+    else
+        rep(TRUE, N)
+
+    ### see stats/src/ks.c line 103ff
+    stat <- (0.5 + floor(as.double(q) * n.x * n.y - 1e-7)) / (n.x * n.y);
+
+    pfun <- function(q) {
+        k <- diag <- 1
+        u <- 0
+        repeat {
+            u <- c(u, 1 + u[length(u)])
+            v <- k - u
+            diag_bit <- (u <= n.x) & (v <= n.y) & (u >= 0) & (v >= 0)
+            u <- u[diag_bit]
+            v <- v[diag_bit]
+            d <- u / n.x - v / n.y
+            if (two.sided) d <- abs(d)
+            diag <- (c(diag, 0) + c(0, diag))[diag_bit]
+            if (TIES[k])
+                diag <- diag * (q > d)
+            k <- k + 1
+            if (N < k) break
+        }
+        diag
+    }
+    ret[IND] <- sapply(stat[IND], pfun)
+    if (any(is.na(ret[IND]))) {
+        warning("computation of exact probability failed, returning Monte Carlo approximation")
+        return(psmirnov(q = q, m = n.x, n = n.y, z = z, 
+                        two.sided = two.sided, exact = FALSE, 
+                        simulate = TRUE, B = B,
+                        lower.tail = lower.tail, log.p = log.p))
+    }
+
+    logdenom <- lgamma(N + 1) - lgamma(n.x + 1) - lgamma(n.y + 1)
+    if (log.p & lower.tail)
+        return(log(ret) - logdenom)
+    if (!log.p & lower.tail)
+        return(ret / exp(logdenom))
+    if (log.p & !lower.tail)
+        return(log1p(-ret / exp(logdenom)))
+    if (!log.p & !lower.tail)
+        return(1 - ret / exp(logdenom))
+}
+
+qsmirnov <-
+function(p, m, n = length(z) - m, z = NULL, two.sided = TRUE, ...)
+{
+    n.x <- floor(m)
+    n.y <- floor(n)
+    if (n.x * n.y < 1e4) {
+        ### note: The support is also OK in case of ties
+        stat <- sort(unique(c(outer(0:n.x/n.x, 0:n.y/n.y, "-"))))
+    } else {
+        stat <- (-1e4):1e4 / (1e4 + 1)
+    }
+    if (two.sided) stat <- abs(stat)
+    prb <- psmirnov(stat, m = n.x, n = n.y, z = z, 
+                    two.sided = TRUE, log.p = FALSE, ...)
+    if (is.null(p)) return(list(stat = stat, prob = prb))
+    if (is.numeric(p)) 
+        p <- as.double(p)
+    else stop("argument 'p' must be numeric")
+    ret <- rep(0, length(p))
+    ret[is.na(p) | p < 0 | p > 1] <- NA
+    IND <- which(!is.na(ret))
+    if (!length(IND)) return(ret)
+    ret[IND] <- sapply(p[IND], function(u) min(stat[prb >= u]))
+    ret
+}
+
--- src/library/stats/src/ks.c
+++ src/library/stats/src/ks.c
@@ -28,6 +28,8 @@
 #include <Rinternals.h>
 #include <Rmath.h>		/* constants */
 
+#include "stats.h"		// for rcont2
+
 static double K(int n, double d);
 static void m_multiply(double *A, double *B, double *C, int m);
 static void m_power(double *A, int eA, double *V, int *eV, int m, int n);
@@ -262,4 +264,60 @@ SEXP pKolmogorov2x(SEXP statistic, SEXP sn)
     double st = asReal(statistic), p;
     p = K(n, st);
     return ScalarReal(p);
+}
+
+static void
+Smirnov_sim_wrk(int nrow, int ncol,
+		const int nrowt[], const int ncolt[],
+		int n, int B, int *observed, int twosided,
+		double *fact, int *jwork, double *results)
+{
+    /* Calculate log-factorials.  fact[i] = lgamma(i+1) */
+    fact[0] = fact[1] = 0.;
+    for(int i = 2; i <= n; i++)
+        fact[i] = fact[i - 1] + log(i);
+
+    GetRNGstate();
+
+    for(int iter = 0; iter < B; ++iter) {
+        rcont2(nrow, ncol, nrowt, ncolt, n, fact, jwork, observed);
+        double S = 0., diff = 0.;
+        int cs0 = 0, cs1 = 0;
+        for (int j = 0; j < nrow; j++) {
+            cs0 += observed[j];
+            cs1 += observed[nrow + j];
+            diff = ((double) cs0) / ncolt[0] - ((double) cs1) / ncolt[1];
+            if (twosided) diff = fabs(diff);
+            if (diff > S) S = diff;
+        }
+        results[iter] = S;
+    }
+
+    PutRNGstate();
+
+    return;
+}
+
+SEXP Smirnov_sim(SEXP sr, SEXP sc, SEXP sB, SEXP twosided)
+{
+    sr = PROTECT(coerceVector(sr, INTSXP));
+    sc = PROTECT(coerceVector(sc, INTSXP));
+    int nr = LENGTH(sr), nc = LENGTH(sc), B = asInteger(sB);
+    if (nc != 2)
+        error("Smirnov statistic only defined for two groups"); 
+    int n = 0, *isr = INTEGER(sr);
+    for (int i = 0; i < nr; i++) {
+        /* avoid integer overflow */
+        if (n > INT_MAX - isr[i]) 
+            error("Sample size too large");
+        n += isr[i];
+    }
+    int *observed = (int *) R_alloc(nr * nc, sizeof(int));
+    double *fact = (double *) R_alloc(n+1, sizeof(double));
+    int *jwork = (int *) R_alloc(nc, sizeof(int));
+    SEXP ans = PROTECT(allocVector(REALSXP, B));
+    Smirnov_sim_wrk(nr, nc, isr, INTEGER(sc), n, B, observed, 
+		    INTEGER(twosided)[0], fact, jwork, REAL(ans));
+    UNPROTECT(3);
+    return ans;
 }
