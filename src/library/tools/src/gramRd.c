28a02bdb7b0ca07baead8b537ec720a1167c64bd
pd <pd@00db46b3-68df-0310-9c12-caf00c1e9a41>
2022-03-27 00:20:21 +0000

    maintainer-mode changes
    git-svn-id: https://svn.r-project.org/R/trunk@81995 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/src/gramRd.c
+++ src/library/tools/src/gramRd.c
@@ -3161,10 +3161,6 @@ static Rboolean isComment(SEXP elt)
            !strcmp(CHAR(STRING_ELT(a, 0)), "COMMENT");
 }
 
-static Rboolean needsEscaping(char c) {
-    return strchr("%{}", c) != NULL;
-}
-
 static SEXP xxusermacro(SEXP macro, SEXP args, YYLTYPE *lloc)
 {
     SEXP ans, value, nextarg;
@@ -3245,11 +3241,7 @@ static SEXP xxusermacro(SEXP macro, SEXP args, YYLTYPE *lloc)
 		error(_("Not enough arguments passed to user macro '%s'"),
 		        CHAR(STRING_ELT(macro,0)));
     	    const char *arg = CHAR(STRING_ELT(ans, which));
-    	    for (size_t ii = strlen(arg); ii > 0; ii--) {
-    		xxungetc(arg[ii-1]);
-    		if (needsEscaping(arg[ii-1]))
-    		    xxungetc('\\');
-	    };
+    	    for (size_t ii = strlen(arg); ii > 0; ii--) xxungetc(arg[ii-1]);
     	    c--;
 	} else
     	    xxungetc(*(c-1));
