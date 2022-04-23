2e5ae192920247af75544f30becd9628b39d70bf
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2022-04-23 09:11:53 +0000

    Tweaks for c82068.
    git-svn-id: https://svn.r-project.org/R/trunk@82236 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/RdConv2.R
+++ src/library/tools/R/RdConv2.R
@@ -922,7 +922,7 @@ checkRd <- function(Rd, defines=.Platform$OS.type, stages = "render",
                 if((blocktag %in% c("\\describe", "\\arguments",
                                     "\\value")) &&
                     isBlankRd(block[[1L]]))
-                    warnRd(block[[1L]], Rdfile, level = 7,
+                    warnRd(block, Rdfile, level = 7,
                            "\\item in ", blocktag,
                            " must have non-empty label")
     		switch(blocktag,
