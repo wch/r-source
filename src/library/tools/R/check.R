fc244224eebdcc19b650d8fd38b5f27a1431aec6
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2022-04-28 14:47:18 +0000

    tweak --as-cran
    git-svn-id: https://svn.r-project.org/R/trunk@82269 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/check.R
+++ src/library/tools/R/check.R
@@ -6463,9 +6463,9 @@ add_dummies <- function(dir, Log)
         Sys.setenv("_R_CHECK_SHLIB_OPENMP_FLAGS_" = "TRUE")
         Sys.setenv("_R_CHECK_FUTURE_FILE_TIMESTAMPS_" = "TRUE")
         Sys.setenv("_R_CHECK_RD_CONTENTS_KEYWORDS_" = "TRUE")
-        ## CRAN incoming checks do use abort,verbose
-        chkPkg.v <- "package:_R_CHECK_PACKAGE_NAME_,abort,verbose"
-        Sys.setenv1("_R_CHECK_LENGTH_1_LOGIC2_"   , chkPkg.v)
+#        ## CRAN incoming checks do use abort,verbose
+#        chkPkg.v <- "package:_R_CHECK_PACKAGE_NAME_,abort,verbose"
+#        Sys.setenv1("_R_CHECK_LENGTH_1_LOGIC2_"   , chkPkg.v)
         Sys.setenv("_R_CHECK_CODOC_VARIABLES_IN_USAGES_" = "TRUE")
         Sys.setenv("_R_CHECK_DATALIST_" = "TRUE")
         if(!WINDOWS) Sys.setenv("_R_CHECK_BASHISMS_" = "TRUE")
