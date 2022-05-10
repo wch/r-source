911e8220b460d2906bb166847a211f4b26f96c9b
kalibera <kalibera@00db46b3-68df-0310-9c12-caf00c1e9a41>
2022-05-10 14:40:16 +0000

    Ensure packages-head-utf8.html is really in UTF-8 (PR#18342).
    git-svn-id: https://svn.r-project.org/R/trunk@82333 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/utils/R/linkhtml.R
+++ src/library/utils/R/linkhtml.R
@@ -1,7 +1,7 @@
 #  File src/library/utils/R/linkhtml.R
 #  Part of the R package, https://www.R-project.org
 #
-#  Copyright (C) 1995-2015 The R Core Team
+#  Copyright (C) 1995-2022 The R Core Team
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -61,7 +61,7 @@ make.packages.html <-
     }
     file.append(f.tg,
                 file.path(R.home("doc"), "html", "packages-head-utf8.html"))
-    out <- file(f.tg, open = "a")
+    out <- file(f.tg, open = "a", encoding = "UTF-8")
     on.exit(close(out))
     if(WINDOWS) {
         rh <- chartr("\\", "/", R.home())
