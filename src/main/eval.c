978c4b3fb9926c19005214bb78104dcfc8b4f1ca
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2022-05-29 22:41:15 +0000

    Merged r82422 and r82423 (fix and test for PR18349 (from trunk).
    git-svn-id: https://svn.r-project.org/R/branches/R-4-2-branch@82424 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/eval.c
+++ src/main/eval.c
@@ -7309,7 +7309,7 @@ static SEXP bcEval(SEXP body, SEXP rho, Rboolean useCache)
 	if (dispatched)
 	    SETSTACK(-1, value);
 	else
-	    SETSTACK(-1, R_subset3_dflt(x, PRINTNAME(symbol), R_NilValue));
+	    SETSTACK(-1, R_subset3_dflt(x, PRINTNAME(symbol), call));
 	R_Visible = TRUE;
 	NEXT();
       }
