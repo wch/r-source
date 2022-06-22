442da5cff9ff366f4e0586719685e66cf36c2b01
kalibera <kalibera@00db46b3-68df-0310-9c12-caf00c1e9a41>
2022-06-22 08:48:15 +0000

    Do not warn when destroying a closed connection from the finalizer.
    git-svn-id: https://svn.r-project.org/R/trunk@82511 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/connections.c
+++ src/main/connections.c
@@ -203,7 +203,7 @@ static void conFinalizer(SEXP ptr)
     if(i >= NCONNECTIONS) return;
     {
 	Rconnection this = getConnection(ncon);
-	if(strcmp(this->class, "textConnection"))
+	if(this->isopen && strcmp(this->class, "textConnection"))
 	    warning(_("closing unused connection %d (%s)\n"),
 		    ncon, this->description);
     }
