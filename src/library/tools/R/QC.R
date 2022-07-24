dd66bfd6044139e3bf1a3642d5a6d57557447427
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2022-07-24 16:51:10 +0000

    incoming check reports use of ACM-licensed packages
    git-svn-id: https://svn.r-project.org/R/trunk@82619 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/QC.R
+++ src/library/tools/R/QC.R
@@ -7199,8 +7199,7 @@ function(dir, localOnly = FALSE, pkgSize = NA)
         out$fields <- nms
 
 
-    uses <- character()
-    BUGS <- character()
+    uses <-  BUGS <- ACM <- character()
     for (field in c("Depends", "Imports", "Suggests")) {
         p <- strsplit(meta[field], " *, *")[[1L]]
         p2 <- grep("^(multicore|snow|igraph0|doSNOW)( |\\(|$)", p, value = TRUE)
@@ -7208,11 +7207,16 @@ function(dir, localOnly = FALSE, pkgSize = NA)
         p2 <- grep("^(BRugs|R2OpenBUGS|R2WinBUGS|mzR|xcms|MSnbase)( |\\(|$)",
                    p, value = TRUE)
         BUGS <- c(BUGS, p2)
+        p2 <- grep("^(Akima|tripack)( |\\(|$)",
+                   p, value = TRUE)
+        ACM <- c(ACM, p2)
     }
     if (length(uses))
         out$uses <- sort(unique(gsub("[[:space:]]+", " ", uses)))
     if (length(BUGS))
         out$BUGS <- sort(unique(gsub("[[:space:]]+", " ", BUGS)))
+    if (length(ACM))
+        out$ACM <- sort(unique(gsub("[[:space:]]+", " ", ACM)))
 
     ## Check for non-Sweave vignettes (as indicated by the presence of a
     ## 'VignetteBuilder' field in DESCRIPTION) without
@@ -8346,6 +8350,12 @@ function(x, ...)
           paste(if(length(y) > 1L)
 		"Uses the non-portable packages:" else
 		"Uses the non-portable package:",
+                paste(sQuote(y), collapse = ", "))
+      },
+      if(length(y <- x$ACM)) {
+          paste(if(length(y) > 1L)
+		"Uses the ACM-licensed packages:" else
+		"Uses the ACM-licensed package:",
                 paste(sQuote(y), collapse = ", "))
       },
       if(length(y <- x$authors_at_R_calls)) {
