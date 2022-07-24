b737dbe5f65b9829a7bdecbaad2b7478c26ce4da
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2022-07-24 11:36:52 +0000

    Optionally check Rd equation math rendering via KaTeX.
    git-svn-id: https://svn.r-project.org/R/trunk@82617 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/Rd.R
+++ src/library/tools/R/Rd.R
@@ -884,6 +884,41 @@ function(db)
     unlist(Rd_names)
 }
 
+### * ..Rd_get_equations_from_Rd
+
+.Rd_get_equations_from_Rd <-
+function(x)
+{
+    y <- .Rd_find_nodes_with_tags(x, c("\\eqn", "\\deqn"))
+    if(!length(y)) return(matrix(character(), 0L, 5L))
+    z <- lapply(y, function(e) {
+        c(attr(e, "Rd_tag"),
+          ## % is treated verbatim in the first arg of equations as per
+          ## "Exceptions to special character handling" in parseRd.pdf.
+          .Rd_deparse(e[[1L]], tag = FALSE),
+          if(length(e) > 1L)
+              trimws(.Rd_deparse(e[[2L]], tag = FALSE))
+          else
+              NA_character_,
+          if(!is.null(loc <- attr(e, "srcref")))
+              loc[c(1L, 3L)]
+          else
+              rep.int(NA_character_, 2L))
+    })
+    do.call(rbind, z)
+}
+
+### * .Rd_get_equations_from_Rd_db
+
+.Rd_get_equations_from_Rd_db <-
+function(x)
+{
+    if(!length(x)) return(matrix(character(), 0L, 6L))
+    m <- lapply(x, .Rd_get_equations_from_Rd)
+    cbind(rep.int(names(m), vapply(m, nrow, 0L)),
+          do.call(rbind, m))
+}
+
 ### * .Rd_format_title
 
 .Rd_format_title <-
