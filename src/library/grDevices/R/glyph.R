31a885247a75f62445f2f5ebcbf306ce88960ec8
murrell <murrell@00db46b3-68df-0310-9c12-caf00c1e9a41>
2022-09-26 05:19:22 +0000

    require left, right, bottom, top, and centre anchors in glyphInfo()
    git-svn-id: https://svn.r-project.org/R/branches/R-typeset@82918 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/grDevices/R/glyph.R
+++ src/library/grDevices/R/glyph.R
@@ -46,9 +46,11 @@ glyphInfo <- function(id, x, y,
         stop("Width and height must be numeric (and not zero-length)")
     ## Check anchors
     if (missing(hAnchor))
-        hAnchor <- c(left=min(x))
+        hAnchor <- c(left=min(x), right=max(x), centre=(max(x) - min(x))/2,
+                     center=(max(x) - min(x))/2)
     if (missing(vAnchor))
-        vAnchor <- c(bottom=min(y))
+        vAnchor <- c(bottom=min(y), top=max(y), centre=(max(y) - min(y))/2,
+                     center=(max(y) - min(y))/2)
     if (!is.numeric(hAnchor) || !is.numeric(vAnchor))
         stop("Anchors must be numeric")
     hNames <- names(hAnchor)
@@ -56,8 +58,15 @@ glyphInfo <- function(id, x, y,
     if (length(unique(hNames)) != length(hAnchor) ||
         length(unique(vNames)) != length(vAnchor))
         stop("Every anchor must have a unique name")
-    if (!("left" %in% hNames && "bottom" %in% vNames))
-        stop('There must be anchors named "left" and "bottom"')
+    if (!("left" %in% hNames && "right" %in% hNames &&
+          ("centre" %in% hNames || "center" %in% hNames) &&
+          "bottom" %in% vNames && "top" %in% vNames &&
+          ("centre" %in% vNames || "center" %in% vNames)))
+        stop('There must be anchors named "left", "right", "bottom", "top", and "centre" (or "center")')
+    if (!"centre" %in% hNames) hAnchor <- c(hAnchor, centre=hAnchor["center"])
+    if (!"centre" %in% vNames) vAnchor <- c(vAnchor, centre=vAnchor["center"])
+    if (!"center" %in% hNames) hAnchor <- c(hAnchor, centre=hAnchor["centre"])
+    if (!"center" %in% vNames) vAnchor <- c(vAnchor, centre=vAnchor["centre"])
     info <- data.frame(id, x, y,
                        family, weight, style, file, index, size)
     attr(info, "width") <- width
