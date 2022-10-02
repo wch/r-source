da9e58c91725c8471f3e8579f0bd668b45973092
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2022-10-02 11:36:07 +0000

    port r82987 from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-4-2-branch@82988 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/g_cntrlify.h
+++ src/main/g_cntrlify.h
@@ -426,11 +426,11 @@ typedef struct
 
 static const Raiseinfo _raised_char_tbl[NUM_RAISED_CHARS] =
 {
-  {170, 97, true},			/* ordfeminine mapped to 'a' */
-  {178, 50, false},			/* twosuperior mapped to '2' */
-  {179, 51, false},			/* threesuperior mapped to '3' */
-  {185, 49, false},			/* onesuperior mapped to '1' */
-  {186, 111, true},			/* ordmasculine mapped to 'o'*/
+  {170, 97, true_},			/* ordfeminine mapped to 'a' */
+  {178, 50, false_},			/* twosuperior mapped to '2' */
+  {179, 51, false_},			/* threesuperior mapped to '3' */
+  {185, 49, false_},			/* onesuperior mapped to '1' */
+  {186, 111, true_},			/* ordmasculine mapped to 'o'*/
 };
 
 /* The single-character `deligature' table, applying to all ISO-8859-1
