3582e26bff5a6ef8d30b1ee0da7e46b926ad092a
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2022-11-02 11:24:08 +0000

    print.listof() gets option `nl=FALSE`
    git-svn-id: https://svn.r-project.org/R/trunk@83230 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/print.R
+++ src/library/base/R/print.R
@@ -1,7 +1,7 @@
 #  File src/library/base/R/print.R
 #  Part of the R package, https://www.R-project.org
 #
-#  Copyright (C) 1995-2020 The R Core Team
+#  Copyright (C) 1995-2022 The R Core Team
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -99,13 +99,13 @@ print.noquote <- function(x, quote = FALSE, right = FALSE, ...) {
 }
 
 ## for alias.lm, aov
-print.listof <- function(x, ...)
+print.listof <- function(x, nl = TRUE, ...)
 {
     nn <- names(x)
     ll <- length(x)
     if(length(nn) != ll) nn <- paste("Component", seq.int(ll))
     for(i in seq_len(ll)) {
-	cat(nn[i], ":\n"); print(x[[i]], ...); cat("\n")
+	cat(nn[i], if(nl) ":\n" else ": "); print(x[[i]], ...); if(nl) cat("\n")
     }
     invisible(x)
 }
