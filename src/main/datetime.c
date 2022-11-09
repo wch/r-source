b40948417652e2d7f1658c6473fe419a361541fd
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2022-11-09 12:13:10 +0000

    validate tzone attribute
    git-svn-id: https://svn.r-project.org/R/trunk@83319 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/datetime.c
+++ src/main/datetime.c
@@ -66,8 +66,8 @@
   name is known or is "UTC', of length 3 including the abbreviations
   for all other timezones. (If the timezone does not use DST, the
   second abbreviation may be empty or may repeat the first, depending
-  on the platform.)  However, if the call to strptime() does not
-  specify 'tz', this attribute is omitted.
+  on the platform., but it woll always be present.)  However, if the
+  call to strptime() does not specify 'tz', this attribute is omitted.
 
   Names for the date-times are optional (and rarely supplied): 
   they are attached to the 'year' element' and used by strptime().
@@ -930,7 +930,16 @@ static Rboolean valid_POSIXlt(SEXP x, int nm)
 		  ltnames[10]);
     }
 
-    // FIXME check the tzone attribute.
+    // check the tzone attribute.
+    SEXP tz = getAttrib(x, install("tzone"));
+    if (!isNull(tz)) {
+	if(!isString(tz))
+	    error(_("invalid '%s'"), "attr(x, \"tzone\")");
+	int l = LENGTH(tz);
+	if(l != 1 && l != 3)
+	    error(_("attr(x, \"tzone\") shouls have length 1 or 3"));
+    }
+
     return TRUE;
 }
 
@@ -1347,6 +1356,7 @@ SEXP attribute_hidden do_formatPOSIXlt(SEXP call, SEXP op, SEXP args, SEXP env)
 	}
     }
 
+    // FIXME: this needs to recycle names
     SEXP nm = getAttrib(VECTOR_ELT(x, 5), R_NamesSymbol);
     if (nm != R_NilValue) setAttrib(ans, R_NamesSymbol, nm);
 
