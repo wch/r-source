25dd1037f8cadb796e728139cd76092e5cfb3d4b
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2022-12-14 13:25:52 +0000

    more moving NORET to the front
    git-svn-id: https://svn.r-project.org/R/trunk@83455 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/modules/X11/rbitmap.c
+++ src/modules/X11/rbitmap.c
@@ -325,7 +325,7 @@ typedef struct my_error_mgr * my_error_ptr;
  * Here's the routine that will replace the standard error_exit method:
 */
 
-static void NORET my_error_exit (j_common_ptr cinfo)
+NORET static void my_error_exit (j_common_ptr cinfo)
 {
     /* cinfo->err really points to a my_error_mgr struct, so coerce pointer */
     my_error_ptr myerr = (my_error_ptr) cinfo->err;
