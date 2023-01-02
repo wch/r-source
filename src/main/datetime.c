d30629906c958d994e75dda3ecc478cb130a7a89
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2023-01-02 21:08:51 +0000

    ported r83530 [format.POSIXlt(*, "%OS<n>")] from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-4-2-branch@83548 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/datetime.c
+++ src/main/datetime.c
@@ -1004,7 +1004,7 @@ SEXP attribute_hidden do_formatPOSIXlt(SEXP call, SEXP op, SEXP args, SEXP env)
 		if(ns > 6) ns = 6;
 		if(ns > 0) {
 		    /* truncate to avoid nuisances such as PR#14579 */
-		    double s = secs, t = Rexp10((double) ns);
+		    double s = tm.tm_sec + (secs - fsecs), t = Rexp10((double) ns);
 		    s = ((int) (s*t))/t;
 		    sprintf(p2, "%0*.*f", ns+3, ns, s);
 		    strcat(buf2, p+nused);
