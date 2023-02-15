ac96ca1431df67caad6493f1c0f4051c2a34c50c
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2023-02-15 15:12:27 +0000

    Add a transform() method for bibentry objects.
    git-svn-id: https://svn.r-project.org/R/trunk@83844 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/utils/R/citation.R
+++ src/library/utils/R/citation.R
@@ -1178,6 +1178,29 @@ function(x, ...)
 
 as.data.frame.bibentry <- as.data.frame.vector
 
+transform.bibentry <-
+function(`_data`, ...)
+{
+    tags <- unique(unlist(lapply(unclass(`_data`), names)))
+    vals <- lapply(tags, function(e) eval(call("$", `_data`, e)))
+    ## Or use eval(substitute(`$`(`_data`, e), list(e = e))) ...
+    names(vals) <- tags
+    
+    e <- eval(substitute(list(...)), vals, parent.frame())
+
+    for(i in setdiff(names(e), c("mheader", "mfooter"))) {
+        `_data` <- eval(call("$<-", `_data`, i, e[[i]]))
+    }
+    ## Or use eval(substitute(`$<-`(`_data`, i, e[[i]]), list(i = i))) ...
+
+    if("mheader" %in% names(e))
+        attr(`_data`, "mheader") <- e[["mheader"]]
+    if("mfooter" %in% names(e))
+        attr(`_data`, "mfooter") <- e[["mfooter"]]
+
+    return(`_data`)
+}
+
 ######################################################################
 
 citEntry <-
