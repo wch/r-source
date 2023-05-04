1832b6ecee8c877e27df3f32742ec94770f6dd86
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2023-05-04 08:37:04 +0000

    prefer https:// versions
    git-svn-id: https://svn.r-project.org/R/trunk@84391 00db46b3-68df-0310-9c12-caf00c1e9a41

--- tests/libcurl.R
+++ tests/libcurl.R
@@ -1,26 +1,31 @@
 ## These are tests that require libcurl functionality (available
 ## everywhere as from R 4.2.0) and a working Internet connection.
 
-## Nowadays method = "libcurl" is the default everwhere for http[s]://
+## Nowadays method = "libcurl" is the default everywhere for http[s]://
+
+## This used to check that https:// worked and skip if not, but then
+## all the tests redirected to to https://
 
 ## check basic Internet access
 if(.Platform$OS.type == "unix" &&
    is.null(nsl("cran.r-project.org"))) q()
 
 tf <- tempfile()
-download.file("http://cran.r-project.org/", tf)
+download.file("https://cran.r-project.org/", tf)
 file.size(tf)
 unlink(tf)
 
 
-## test url connections on http -- this now redirects to http://
+## test url connections on http -- this now redirects to https://
 str(readLines(zz <- url("http://cran.r-project.org/")))
 zz
 stopifnot(identical(summary(zz)$class, "url-libcurl"))
 close(zz)
+## and check that direct https:// works
+str(readLines(zz <- url("https://cran.r-project.org/")))
+close(zz)
 
-
-##  via read.table (which closes the connection) -- this now redirects to http://
+##  via read.table (which closes the connection) -- this now redirects to https://
 tail(read.table(url("http://www.stats.ox.ac.uk/pub/datasets/csb/ch11b.dat")))
 
 ## check option works
@@ -29,27 +34,9 @@ zz <- url("http://www.stats.ox.ac.uk/pub/datasets/csb/ch11b.dat")
 stopifnot(identical(summary(zz)$class, "url-libcurl"))
 close(zz)
 
-showConnections(all = TRUE)
-
-## --------------------------------------------------------------
-## Some platforms have problems with certificates,
-## so allow them to skip the https tests
-junk <- tryCatch(curlGetHeaders("http://bugs.r-project.org"),
-                 error = function(e) {
-			 message("Check for working https failed:\n\t",
-				 conditionMessage(e),
-				 "skipping https tests\n")
-			 q()
-		 })
-
+curlGetHeaders("https://developer.r-project.org")
 example(curlGetHeaders, run.donttest = TRUE)
 
-## https URL
-head(readLines(zz <- url("https://cran.r-project.org"), warn = FALSE))
-close(zz)
-
-## redirection (to a https:// URL)
-head(readLines(zz <- url("http://bugs.r-project.org"), warn = FALSE))
-close(zz)
+showConnections(all = TRUE)
 
 proc.time()
