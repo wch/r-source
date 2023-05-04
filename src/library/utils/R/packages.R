64139bacefe017cf029dacd2c465653b5d370a97
urbaneks <urbaneks@00db46b3-68df-0310-9c12-caf00c1e9a41>
2023-05-04 02:33:48 +0000

    add name= argument to setRepositories()
    git-svn-id: https://svn.r-project.org/R/trunk@84387 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/utils/R/packages.R
+++ src/library/utils/R/packages.R
@@ -953,7 +953,7 @@ chooseBioCmirror <- function(graphics = getOption("menu.graphics"), ind = NULL,
 
 setRepositories <-
     function(graphics = getOption("menu.graphics"), ind = NULL,
-             addURLs = character())
+             addURLs = character(), name = NULL)
 {
     if(is.null(ind) && !interactive())
         stop("cannot set repositories non-interactively")
@@ -984,7 +984,13 @@ setRepositories <-
 
     default <- a[["default"]]
 
-    res <- if(length(ind)) as.integer(ind)
+    res <- if (length(name)) {
+        m <- match(tolower(name), tolower(row.names(a)))
+        if (any(is.na(m)))
+            stop("No matching repositories found for ",
+                 paste(name[is.na(m)], collapse=', '))
+        m
+    } else if(length(ind)) as.integer(ind)
     else {
         title <- if(graphics) "Repositories" else gettext("--- Please select repositories for use in this session ---\n")
         match(select.list(a[, 1L], a[default, 1L], multiple = TRUE, title,
