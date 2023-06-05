0ae4d38416768f5a12c52ebadb2fa44bdf0717e3
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2023-06-05 17:02:29 +0000

    Ported c84497 from trunk.
    git-svn-id: https://svn.r-project.org/R/branches/R-4-3-branch@84498 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/utils/R/citation.R
+++ src/library/utils/R/citation.R
@@ -1014,7 +1014,7 @@ function(x)
                 function(e) {
                     e <- e[!vapply(e, is.null, NA)]
                     cargs <-
-                        sprintf("%s = %s", names(e), sapply(e, deparse))
+                        sprintf("%s = %s", names(e), sapply(e, deparse1))
                     .format_call_RR("person", cargs)
                 })
     if(length(s) > 1L)
