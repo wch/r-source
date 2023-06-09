cb52635b4162b8ebb8bfc02d3064d108f6324d2d
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2023-06-09 07:41:39 +0000

    message tweaks
    git-svn-id: https://svn.r-project.org/R/trunk@84524 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/arithmetic.c
+++ src/main/arithmetic.c
@@ -601,7 +601,7 @@ attribute_hidden SEXP R_binary(SEXP call, SEXP op, SEXP x, SEXP y)
 	    if(ny != 0)
 		warningcall(call, _(
 	"Recycling array of length 1 in array-vector arithmetic is deprecated.\n\
-  Use c() or as.vector() instead.\n"));
+  Use c() or as.vector() instead."));
     	    REPROTECT(x = duplicate(x), xpi);
     	    setAttrib(x, R_DimSymbol, R_NilValue);
     	}
@@ -609,7 +609,7 @@ attribute_hidden SEXP R_binary(SEXP call, SEXP op, SEXP x, SEXP y)
 	    if(nx != 0)
 		warningcall(call, _(
 	"Recycling array of length 1 in vector-array arithmetic is deprecated.\n\
-  Use c() or as.vector() instead.\n"));
+  Use c() or as.vector() instead."));
     	    REPROTECT(y = duplicate(y), ypi);
     	    setAttrib(y, R_DimSymbol, R_NilValue);
     	}
