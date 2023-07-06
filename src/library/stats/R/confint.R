35b2bb59bea35717fd9be9d115fe619bc0061edc
pd <pd@00db46b3-68df-0310-9c12-caf00c1e9a41>
2023-07-06 18:21:26 +0000

    changes to confint and profiling
    git-svn-id: https://svn.r-project.org/R/trunk@84646 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/R/confint.R
+++ src/library/stats/R/confint.R
@@ -1,8 +1,10 @@
 #  File src/library/stats/R/confint.R
 #  Part of the R package, https://www.R-project.org
+
+# Portions copied from MASS/R/confint.R
 #
 #  Copyright (C) 2003-2023 The R Core Team
-#  Copyright (C) 1994-2003 W. N. Venables and B. D. Ripley
+#  Copyright (C) 1994-2006 W. N. Venables and B. D. Ripley
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -43,21 +45,114 @@ confint.lm <- function(object, parm, level = 0.95, ...)
     ci
 }
 
-## loading the MASS namespace will overwrite these in the registry.
-## stub is a specialized version of MASS:::confint.xxx with specific message
-confint.glm <- function(object, parm, level = 0.95, ...)
+##Copied from MASS, modified for score tests ("Rao") by pd July 2023
+confint.glm <- function(object, parm, level = 0.95, trace = FALSE, test = c("LRT", "Rao"), ...)
 {
-    if(!requireNamespace("MASS", quietly = TRUE))
-        stop("package 'MASS' must be installed")
-    asNamespace("MASS")$confint.glm(object, parm, level, ...)
+    test <- match.arg(test)
+    pnames <- names(coef(object))
+    if(missing(parm)) parm <- seq_along(pnames)
+    else if(is.character(parm))  parm <- match(parm, pnames, nomatch = 0L)
+    message("Waiting for profiling to be done...")
+    utils::flush.console()
+    object <- profile(object, which = parm, alpha = (1. - level)/4.,
+                      trace = trace, test = test)
+    confint(object, parm=parm, level=level, trace = trace, test = test, ...)
 }
 
-confint.nls <- function(object, parm, level = 0.95, ...)
+confint.profile.glm <- function(object, parm = seq_along(pnames), level = 0.95,
+                                trace = FALSE, test = attr(object, "test"), ...)
 {
-    if(!requireNamespace("MASS", quietly = TRUE))
-        stop("package 'MASS' must be installed")
-    asNamespace("MASS")$confint.nls(object, parm, level, ...)
+    if (is.null(test)) test <- "LRT" # For backwards compatibility
+
+    of <- attr(object, "original.fit")
+    pnames <- names(coef(of))
+    if(is.character(parm))  parm <- match(parm, pnames, nomatch = 0L)
+
+    if (is.null(attr(object,"test"))) # For backwards compatibility
+        attr(object,"test") <- "LRT" 
+    if (test != attr(object, "test")) {
+        message("Reprofiling for ", test, " statistic. Waiting...")
+        utils::flush.console()
+        object <- profile(of, which = parm, alpha = (1. - level)/4.,
+                          trace = trace, test = test)
+    }
+ 
+    a <- (1-level)/2
+    a <- c(a, 1-a)
+    pct <- paste(round(100*a, 1), "%")
+    ci <- array(NA, dim = c(length(parm), 2L),
+                dimnames = list(pnames[parm], pct))
+    ## Hmm: This could have a df correction if there's an estimated dispersion parameter
+    ## Leave for now. -pd
+    cutoff <- qnorm(a)
+    for(pm in parm) {
+        pro <- object[[ pnames[pm] ]]
+        ## skip aliased params
+        if(is.null(pro)) next
+        if(length(pnames) > 1L)
+            sp <- spline(x = pro[, "par.vals"][, pm], y = pro[, 1])
+        else sp <- spline(x = pro[, "par.vals"], y = pro[, 1])
+        ci[pnames[pm], ] <- approx(sp$y, sp$x, xout = cutoff)$y
+    }
+    drop(ci)
+    ## This could be nice, but maybe also disruptive...
+    # attr(ci, "test") <- "test" 
 }
+
+## loading the MASS namespace will overwrite these in the registry.
+## stub is a specialized version of MASS:::confint.xxx with specific message
+
+## Disabled for glm -pd
+#confint.glm <- function(object, parm, level = 0.95, ...)
+#{
+#    if(!requireNamespace("MASS", quietly = TRUE))
+#        stop("package 'MASS' must be installed")
+#    asNamespace("MASS")$confint.glm(object, parm, level, ...)
+#}
+
+#confint.nls <- function(object, parm, level = 0.95, ...)
+#{
+#    if(!requireNamespace("MASS", quietly = TRUE))
+#        stop("package 'MASS' must be installed")
+#    asNamespace("MASS")$confint.nls(object, parm, level, ...)
+#}
+
+confint.nls <-
+  function(object, parm, level = 0.95, ...)
+{
+  pnames <- names(coef(object))
+  if(missing(parm)) parm <- seq_along(pnames)
+  if(is.numeric(parm))  parm <- pnames[parm]
+  message("Waiting for profiling to be done...")
+  utils::flush.console()
+  object <- profile(object, which = parm, alphamax = (1. - level)/4.)
+  confint(object, parm=parm, level=level, ...)
+}
+
+confint.profile.nls <-
+  function(object, parm = seq_along(pnames), level = 0.95, ...)
+{
+  pnames <- names(object) # non-linear pars only
+  ncoefs <- length(coef(attr(object, "original.fit")))
+  of <- attr(object, "original.fit")
+  if(is.numeric(parm))  parm <- pnames[parm]
+  ## drop any plinear paramaters
+  parm <- parm[parm %in% pnames]
+  n <- length(fitted(of)) - length(of$m$getPars())
+  a <- (1-level)/2
+  a <- c(a, 1-a)
+  pct <- paste(round(100*a, 1), "%", sep = "")
+  ci <- array(NA, dim = c(length(parm), 2L), dimnames = list(parm, pct))
+  cutoff <- qt(a, n)
+  for(pm in parm) {
+    pro <- object[[pm]]
+    sp <- if(ncoefs > 1) spline(x = pro[, "par.vals"][, pm], y = pro$tau)
+    else spline(x = pro[, "par.vals"], y = pro$tau)
+    ci[pm, ] <- approx(sp$y, sp$x, xout = cutoff)$y
+  }
+  drop(ci)
+}
+
 
 confint.default <- function (object, parm, level = 0.95, ...)
 {
---
+++ src/library/stats/R/glm-profile.R
@@ -0,0 +1,238 @@
+##  File src/library/stats/R/glm-profile.R
+##  Part of the R package, https://www.R-project.org
+
+## Based on
+# File MASS/profiles.q copyright (C) 1996 D. M. Bates and W. N. Venables.
+#
+# port to R by B. D. Ripley copyright (C) 1998
+#
+# corrections copyright (C) 2000,3,6,7 B. D. Ripley
+
+## Modified for Rao Score test ("test=" argument) by Peter Dalgaard 2023
+## Modifications Copyright (C) 2023 The R Core Team
+
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 or 3 of the License
+#  (at your option).
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  A copy of the GNU General Public License is available at
+#  http://www.r-project.org/Licenses/
+
+profile.glm <- function(fitted, which = 1:p, alpha = 0.01,
+			maxsteps = 10, del = zmax/5, trace = FALSE, test = c("LRT", "Rao"), ...)
+{
+    test <- match.arg(test)
+    Pnames <- names(B0 <- coef(fitted))
+    nonA <- !is.na(B0)
+    pv0 <- t(as.matrix(B0))
+    p <- length(Pnames)
+    if(is.character(which)) which <- match(which, Pnames)
+    summ <- summary(fitted)
+    std.err <- summ$coefficients[, "Std. Error", drop = FALSE] # unaliased only
+    mf <- model.frame(fitted)
+    Y <- model.response(mf) ### change
+    n <- NROW(Y)            ### change: NROW() rather than length()
+    O <- model.offset(mf)
+    if(!length(O)) O <- rep(0, n)
+    W <- model.weights(mf)
+    if(length(W) == 0L) W <- rep(1, n)
+    OriginalDeviance <- deviance(fitted)
+    DispersionParameter <- summ$dispersion
+    X <- model.matrix(fitted)
+    fam <- family(fitted)
+    switch(fam$family,
+           binomial = ,
+           poisson = ,
+           `Negative Binomial` = {
+               zmax <- sqrt(qchisq(1 - alpha, 1))
+               profName <- "z"
+           }
+           ,
+           gaussian = ,
+           quasi = ,
+           inverse.gaussian = ,
+           quasibinomial = ,
+           quasipoisson = ,
+       {
+	   zmax <- sqrt(qf(1 - alpha, 1, n - p))
+	   profName <- "tau"
+       }
+           )
+    prof <- vector("list", length=length(which))
+    names(prof) <- Pnames[which]
+    for(i in which) {
+        if(!nonA[i]) next
+        zi <- 0
+        pvi <- pv0
+        a <- nonA; a[i] <- FALSE
+        Xi <- X[, a, drop = FALSE]
+        pi <- Pnames[i]
+        for(sgn in c(-1, 1)) {
+            if(trace)
+                message("\nParameter: ", pi, " ",
+                        c("down", "up")[(sgn + 1)/2 + 1])
+            step <- 0
+            z <- 0
+            ## LP is the linear predictor including offset.
+            ## we need to take care to avoid aliased-out coefficients.
+            LP <- X[, nonA, drop = FALSE] %*% B0[nonA] + O
+            while((step <- step + 1) < maxsteps && abs(z) < zmax) {
+                bi <- B0[i] + sgn * step * del * std.err[Pnames[i], 1]
+                o <- O + X[, i] * bi
+                ## call to glm.fit.null not needed from 1.4.1 on
+                
+                ## FIXME: in non-canonical models, o or LP+o can end
+                ##        up outside valid range. (E.g. binomial("identity").) This
+                ##        really should be checked for and the range adjusted.
+                ##        
+                
+                fm <- glm.fit(x = Xi, y = Y, weights = W, etastart = LP,
+                              offset = o, family = fam,
+                              control = fitted$control)
+                LP <- Xi %*% fm$coefficients + o
+                ri <- pv0
+                ri[, names(coef(fm))] <- coef(fm)
+                ri[, pi] <- bi
+                pvi <- rbind(pvi, ri)
+                zz <- (fm$deviance - OriginalDeviance)/DispersionParameter
+                if(zz > - 1e-3) zz <- max(zz, 0)
+                else stop("profiling has found a better solution, so original fit had not converged")
+                if (test == "Rao"){
+                    ## Local fit to residual, using WLS:
+                    r <- fm$residuals
+                    w <- fm$weights
+                    fml <- glm.fit(x = X, y = r, weights = w,
+                                   control = fitted$control, intercept=FALSE)
+                    zz <- (fml$null.deviance - fml$deviance)/DispersionParameter
+                    zz <- max(zz, 0) # small negative can happen at bi == 0...
+                }
+                z <- sgn * sqrt(zz)
+                zi <- c(zi, z)
+            }
+        }
+        si <- order(zi)
+        prof[[pi]] <- structure(data.frame(zi[si]), names = profName)
+        prof[[pi]]$par.vals <- pvi[si, ,drop=FALSE]
+    }
+    val <- structure(prof, original.fit = fitted, summary = summ)
+    class(val) <- c("profile.glm", "profile")
+    attr(val, "test") <- test
+    val
+}
+
+plot.profile <-
+  ## R version: non-Trellis-based replacement for plot.profile
+  function(x, ...)
+{
+    nulls <- sapply(x, is.null)
+    if (all(nulls)) return(NULL)
+    x <- x[!nulls]
+    nm <- names(x)
+    nr <- ceiling(sqrt(length(nm)))
+    oldpar <- par(mfrow = c(nr, nr))
+    on.exit(par(oldpar))
+    for(nm in names(x)) {
+        tau <- x[[nm]][[1L]]
+        parval <- x[[nm]][[2L]][, nm]
+        dev.hold()
+        plot(parval, tau, xlab = nm, ylab = "tau", type="n")
+        ## allow for profiling failures
+        if(sum(tau == 0) == 1) points(parval[tau == 0], 0, pch = 3)
+        splineVals <- spline(parval, tau)
+        lines(splineVals$x, splineVals$y)
+        dev.flush()
+    }
+}
+
+pairs.profile <-
+    ## Another plot method for profile objects showing pairwise traces.
+    ## Recommended only for diagnostic purposes.
+
+    ## FIXME (pd): Should work better if profiling a subset of parameters
+    ## via profile(...which=...). Currently, you get a rather odd plot.
+
+    ## FIXME (pd): Could have markings according to approx. 2D
+    ## conf. regions supplementing/replacing the equidistant steps
+    
+function(x, colours = 2:3, ...)
+{
+    parvals <- lapply(x, "[[", "par.vals")
+    rng <- apply(do.call("rbind", parvals), 2L, range, na.rm = TRUE)
+    Pnames <- colnames(rng)
+    npar <- length(Pnames)
+    coefs <- coef(attr(x, "original.fit"))
+    form <- paste(as.character(formula(attr(x, "original.fit")))[c(2, 1, 3)],
+                  collapse = "")
+    oldpar <- par(mar = c(0, 0, 0, 0), mfrow = c(1, 1),
+                  oma = c(3, 3, 6, 3), las = 1)
+    on.exit(par(oldpar))
+    ##
+    ## The following dodge ensures that the plot region is square
+    ##
+    fin <- par("fin")
+    dif <- (fin[2L] - fin[1L])/2
+    if(dif > 0) adj <- c(dif, 0, dif, 0)
+    else adj <- c(0,  - dif, 0,  - dif)
+    par(omi = par("omi") + adj)
+    ##
+    ##
+    cex <- 1 + 1/npar
+    frame()
+    mtext(form, side = 3, line = 3, cex = 1.5, outer = TRUE)
+    del <- 1/npar
+    for(i in 1L:npar) {
+        ci <- npar - i
+        pi <- Pnames[i]
+        for(j in 1L:npar) {
+            dev.hold()
+            pj <- Pnames[j]
+            par(fig = del * c(j - 1, j, ci, ci + 1))
+            if(i == j) {
+                par(new=TRUE)
+                plot(rng[, pj], rng[, pi], axes = FALSE,
+                     xlab = "", ylab = "", type = "n")
+                op <- par(usr = c(-1, 1, -1, 1))
+                text(0, 0, pi, cex = cex, adj = 0.5)
+                par(op)
+            } else {
+                col <- colours
+                if(i < j) col <- col[2:1]
+                if(!is.null(parvals[[pj]])) {
+                    par(new=TRUE)
+                    plot(spline(x <- parvals[[pj]][, pj],
+                                y <- parvals[[pj]][, pi]),
+                         type = "l", xlim = rng[, pj],
+                         ylim = rng[, pi], axes = FALSE,
+                         xlab = "", ylab = "", col = col[2L])
+                    pu <- par("usr")
+                    smidge <- 2/100 * (pu[4L] - pu[3L])
+                    segments(x, pmax(pu[3L], y - smidge), x,
+                             pmin(pu[4L], y + smidge))
+                } else
+                plot(rng[, pj], rng[, pi], axes = FALSE,
+                     xlab = "", ylab = "", type = "n")
+                if(!is.null(parvals[[pi]])) {
+                    lines(x <- parvals[[pi]][, pj], y <- parvals[[pi]][, pi],
+                          type = "l", col = col[1L])
+                    pu <- par("usr")
+                    smidge <- 2/100 * (pu[2L] - pu[1L])
+                    segments(pmax(pu[1L], x - smidge), y, pmin(pu[2L], x + smidge), y)
+                }
+                points(coefs[pj], coefs[pi], pch = 3, cex = 3)
+            }
+            if(i == npar) axis(1)
+            if(j == 1) axis(2)
+            if(i == 1) axis(3)
+            if(j == npar) axis(4)
+            dev.flush()
+        }
+    }
+    par(fig = c(0, 1, 0, 1))
+    invisible(x)
+}
