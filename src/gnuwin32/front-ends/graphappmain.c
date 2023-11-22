a4162b8b0be50170a92657d2598fe30e94c82234
kalibera <kalibera@00db46b3-68df-0310-9c12-caf00c1e9a41>
2023-11-22 10:51:24 +0000

    Fix declaration.
    git-svn-id: https://svn.r-project.org/R/trunk@85596 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/front-ends/graphappmain.c
+++ src/gnuwin32/front-ends/graphappmain.c
@@ -16,15 +16,9 @@ WinMain (HINSTANCE Instance, HINSTANCE PrevInstance, LPSTR CmdLine,
     extern void AppMain(int argc, char **argv);
 
 #ifdef _W64
-    __declspec(dllimport) extern int __argc;
-    __declspec(dllimport) extern char **__argv;
-
     GA_startgraphapp(Instance, PrevInstance, CmdShow);
     AppMain(__argc, __argv);
 #else
-    __declspec(dllimport) extern int _argc;
-    __declspec(dllimport) extern char **_argv;
-
     GA_startgraphapp(Instance, PrevInstance, CmdShow);
     AppMain(_argc, _argv);
 #endif
