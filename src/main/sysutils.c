c2a279a654a1d6b8cea8ebe492661a7d346998a8
kalibera <kalibera@00db46b3-68df-0310-9c12-caf00c1e9a41>
2023-12-01 14:43:15 +0000

    Experimental code to avoid iconv transliteration on macOS (not enabled by default).
    git-svn-id: https://svn.r-project.org/R/trunk@85655 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/sysutils.c
+++ src/main/sysutils.c
@@ -935,12 +935,39 @@ cetype_t getCharCE(SEXP x)
    This work-around automatically re-sets the converter in Riconv in case
    of error for stateless encodings. */
 # define R_MACOS_LIBICONV_WORKAROUND
+
+/* A hack to detect and undo transliteration (experimental, likely to change
+   or be removed).  While POSIX says that iconv should transliterate or
+   substitute valid input characters not representable in the output
+   encoding, this typically is not the case.  Instead, non-representable
+   characters usually cause an error (EILSEQ or even EINVAL) and R ended up
+   depending on that.
+
+   macOS 14.1 has a libiconv implementation which transliterates many
+   characters.
+
+   This feature, currently only available for stateless conversions together
+   with R_MACOS_LIBICONV_WORKAROUND, detects transliteration by converting
+   the result back to the original encoding, compares with the original and
+   then re-rums the conversion only to the to-be-transliterated character. 
+   This wouldn't work in cases when the conversion isn't unique, but such
+   cases are unlikely (note implementations of iconv, including libiconv in
+   macOS 14.1, do not support decomposed forms).
+   
+   Enabled at runtime via _R_ICONV_UNDO_TRANSLITERATION_.
+*/
+
+# define R_MACOS_LIBICONV_UNDO_TRANSLITERATION
 #endif
 
 #ifdef R_MACOS_LIBICONV_WORKAROUND
 typedef struct {
     iconv_t cd;
     Rboolean reset_after_error;
+    iconv_t cd_back;
+    Rboolean undo_transliteration;
+    size_t buflen;
+    char *buf;
 } Riconv_cd;
 
 static Rboolean is_stateful(const char *code)
@@ -974,6 +1001,26 @@ static Rboolean is_stateful(const char *code)
 	    return TRUE;
     return FALSE;
 }
+
+# ifdef R_MACOS_LIBICONV_UNDO_TRANSLITERATION
+static Rboolean is_unicode(const char *code)
+{
+    /* list from libiconv 1.17, but names are system-specific */
+    static char *unicode[] = {
+        "UTF-8",
+        "UCS-4", "UCS-4BE", "UCS-4LE",
+        "UTF-16", "UTF-16BE", "UTF-16LE",
+        "UTF-32", "UTF-32BE", "UTF-32LE",
+        "UTF-7",
+        "C99", "JAVA", NULL
+    };
+
+    for(int i = 0; unicode[i] ; i++)
+	if (!strcasecmp(code, unicode[i]))
+	    return TRUE;
+    return FALSE;
+}
+# endif 
 #endif
 
 static void *iconv_open_internal(const char *tocode, const char *fromcode)
@@ -992,6 +1039,32 @@ static void *iconv_open_internal(const char *tocode, const char *fromcode)
     }
     rcd->cd = cd;
     rcd->reset_after_error = !(is_stateful(tocode) || is_stateful(fromcode));
+
+    rcd->undo_transliteration = FALSE;
+# ifdef R_MACOS_LIBICONV_UNDO_TRANSLITERATION
+    rcd->undo_transliteration =
+        !is_unicode(tocode) && rcd->reset_after_error;
+	/* (!(is_stateful(tocode) || is_stateful(fromcode))  */
+
+    char *p = getenv("_R_ICONV_UNDO_TRANSLITERATION_");
+    if (!p || !StringTrue(p))
+	rcd->undo_transliteration = FALSE;
+
+    if (rcd->undo_transliteration) {
+	rcd->cd_back = iconv_open(fromcode, tocode);
+	if (rcd->cd_back == (iconv_t)-1) {
+	    iconv_close((iconv_t)rcd->cd);
+	    return (iconv_t)-1;
+	}
+	rcd->buflen = 8192;
+	rcd->buf = malloc(rcd->buflen);
+	if (!rcd->buf) {
+	    iconv_close((iconv_t)rcd->cd);
+	    iconv_close((iconv_t)rcd->cd_back);
+	    return (iconv_t)-1;
+	}
+    }
+# endif
     return rcd;
 #endif
 }
@@ -1031,10 +1104,28 @@ void * Riconv_open (const char* tocode, const char* fromcode)
 size_t Riconv (void *cd, const char **inbuf, size_t *inbytesleft,
 	       char **outbuf, size_t *outbytesleft)
 {
+#ifdef R_MACOS_LIBICONV_WORKAROUND
+    Riconv_cd *rcd = (Riconv_cd *)cd;
+
+# ifdef R_MACOS_LIBICONV_UNDO_TRANSLITERATION
+    const char *old_inbuf = NULL;
+    size_t old_inbytesleft = 0;
+    char *old_outbuf = NULL;
+    size_t old_outbytesleft = 0;
+
+    if (rcd->undo_transliteration) {
+	old_inbuf = inbuf ? *inbuf : NULL;
+	old_inbytesleft = inbytesleft ? *inbytesleft : 0;
+	old_outbuf = outbuf ? *outbuf : NULL;
+	old_outbytesleft = outbytesleft ? *outbytesleft : 0;
+    }
+# endif
+#endif
+
     /* here libiconv has const char **, glibc has char ** for inbuf */
     size_t res = iconv(
 #ifdef R_MACOS_LIBICONV_WORKAROUND
-                       ((Riconv_cd *)cd)->cd,
+                       rcd->cd,
 #else
                        (iconv_t)cd,
 #endif
@@ -1042,14 +1133,81 @@ size_t Riconv (void *cd, const char **inbuf, size_t *inbytesleft,
                        outbuf, outbytesleft);
 
 #ifdef R_MACOS_LIBICONV_WORKAROUND
-    if (res == (size_t)-1 && (errno == EILSEQ || errno == EINVAL)) {
-	Riconv_cd *rcd = (Riconv_cd *)cd;
-	if (rcd->reset_after_error) {
-	    int saveerrno = errno;
-	    iconv(rcd->cd, NULL, NULL, NULL, NULL);
-	    errno = saveerrno;
+    if (rcd->reset_after_error &&
+	(res == (size_t)-1 && (errno == EILSEQ || errno == EINVAL))) {
+
+	int saveerrno = errno;
+	iconv(rcd->cd, NULL, NULL, NULL, NULL);
+	errno = saveerrno;
 	}
-    }
+
+# ifdef R_MACOS_LIBICONV_UNDO_TRANSLITERATION
+    if (rcd->undo_transliteration &&
+        inbuf && inbytesleft && outbuf && outbytesleft) {
+
+	int saveerrno = errno;
+	size_t needed = old_inbytesleft - *inbytesleft;
+	if (rcd->buflen < needed) {
+	    free(rcd->buf);
+	    rcd->buf = malloc(needed);
+	    if (!rcd->buf) {
+		errno = ENOMEM;
+		return -1;
+	    }
+	    rcd->buflen = needed;
+	}
+
+	const char *back_inbuf = old_outbuf;
+	size_t back_inbytesleft = old_outbytesleft - *outbytesleft;
+	char *back_outbuf = rcd->buf;
+	size_t back_outbytesleft = needed;
+
+	iconv(rcd->cd_back, NULL, NULL, NULL, NULL);
+	int back_res = iconv(rcd->cd_back,
+	      (ICONV_CONST char **) &back_inbuf,
+	      &back_inbytesleft,
+	      (ICONV_CONST char **) &back_outbuf,
+	      &back_outbytesleft);
+
+	if (back_res == (size_t)-1) {
+	    /* should not happen */
+	    errno = saveerrno;
+	    return res;
+	}
+
+	if (back_outbytesleft == 0 && back_inbytesleft == 0 &&
+	    !memcmp(rcd->buf, old_inbuf, needed)) {
+
+	    /* no transliteration happened */
+	    errno = saveerrno;
+	    return res;
+	}
+
+	size_t stored = needed - back_outbytesleft;
+	size_t i;
+	for(i = 0; i < stored; i++)
+	    if (rcd->buf[i] != old_inbuf[i]) {
+		/* byte at index i in old_inbuf was probably transliterated,
+		   so convert again only i bytes and report error */
+		*inbuf = old_inbuf;
+		*outbuf = old_outbuf;
+		*outbytesleft = old_outbytesleft;
+		size_t reduced = i;
+		res = iconv(rcd->cd,
+		            (ICONV_CONST char **) inbuf,
+		            &reduced,
+		            (ICONV_CONST char **) outbuf,
+		            outbytesleft);
+
+		/* "reduced" should be 0 now */
+		*inbytesleft = old_inbytesleft - (i - reduced);
+		errno = EILSEQ;
+		return -1;
+	    }
+	/* should not be reached */
+	errno = saveerrno;
+    } 
+# endif
 #endif
     return res;
 }
@@ -1058,8 +1216,17 @@ int Riconv_close (void *cd)
 {
 #ifndef R_MACOS_LIBICONV_WORKAROUND
     return iconv_close((iconv_t) cd);
+
 #else
-    return iconv_close(((Riconv_cd *)cd)->cd);
+    Riconv_cd *rcd = (Riconv_cd *)cd;
+    int res = iconv_close(rcd->cd);
+# ifdef R_MACOS_LIBICONV_UNDO_TRANSLITERATION
+    if (rcd->undo_transliteration) {
+	free(rcd->buf);
+	if (iconv_close(rcd->cd_back)) res = -1;
+    }
+# endif
+    return res;
 #endif
 }
 
