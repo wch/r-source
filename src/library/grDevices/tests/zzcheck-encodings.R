def68c391f83e140c0a3df34108fecfd78d50fcf
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2023-12-28 09:59:29 +0000

    add reference output
    git-svn-id: https://svn.r-project.org/R/trunk@85743 00db46b3-68df-0310-9c12-caf00c1e9a41

---
+++ src/library/grDevices/tests/zzcheck-encodings.R
@@ -0,0 +1,34 @@
+### Optonally compare encodings results with the ref directories
+
+if(is.na(Sys.getenv("R_GRDEVICES_COMPARE_PS_PDF", unset = NA))) q("no")
+## mustl has a very different libiconv.
+musl <- grepl("musl", R.version$os)
+if(musl) q("no")
+
+## This must follow encodings*.R
+
+options(warn = 1)
+library(tools)
+files <- dir("ref", full.names = TRUE)
+sys <- Sys.info()
+## different reference output for macOS >= 14
+## reference done with 14.2.
+if(sys["sysname"] == "Darwin" &&
+   as.numeric_version(sys["release"]) >= "23") {
+    ind <- files %in% c("ref/PDF-encoding.pdf", "ref/PS-encoding.ps")
+    files[ind] <- sub("^ref", "ref-macOS", files[ind])
+}
+
+cnt <- 0L
+for(f in files) {
+    ff <- basename(f)
+    message("comparing ", sQuote(ff))
+    res <- Rdiff(ff, f, useDiff = TRUE)
+    if(res == 0) message("OK")
+    else {
+        message("Differences")
+        cnt <- cnt + 1L
+    }
+}
+
+if(cnt) stop("Differences found")
