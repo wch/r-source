50adf28705f0a316b6b3431582469e3d52e0d4bf
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2024-01-22 17:23:52 +0000

    update (*necessary)
    git-svn-id: https://svn.r-project.org/R/trunk@85820 00db46b3-68df-0310-9c12-caf00c1e9a41

--- tests/reg-large.R
+++ tests/reg-large.R
@@ -389,17 +389,17 @@ if(availableGB > 10) withAutoprint({ ## PR#18612
     ## was "data length [-2147483648] is not a sub-multiple or multiple ...."
     (m2 <- conditionMessage(tryCatch(matrix(x, 1L, M), warning = identity)))
     ## was "data length [-2147483648] is not a sub-multiple or multiple ...."
-    (m3 <- conditionMessage(tryCatch(matrix(y, M, 1L), error = identity)))
+    (m3 <- conditionMessage(tryCatch(matrix(y, M, 1L), warning = identity)))
     ## was "data length differs from size of matrix: [-2 != 2147483647 x 1]"
-    (m4 <- conditionMessage(tryCatch(matrix(y, 1L, M), error = identity)))
+    (m4 <- conditionMessage(tryCatch(matrix(y, 1L, M), warning = identity)))
     ## "data length differs from size of matrix: [-2 != 1 x 2147483647]"
     ## triggering those in dimsgets():
     (m5 <- conditionMessage(tryCatch(dim(y) <- c(1L, M), error = identity)))
     (m6 <- conditionMessage(tryCatch(dim(x) <- c(M, 1L), error = identity)))
     stopifnot(exprs = {
-        grepl(paste0("data length [", M+1, "] is not"), c(m1,m2), fixed=TRUE)
-        grepl(paste0("size of matrix: [", 2*M, " != "), c(m3,m4), fixed=TRUE)
-        grepl(paste0("dims [product ", M, "] do not "), c(m5,m6), fixed=TRUE)
+        grepl(paste0("data length [", M+1, "] is not"),   c(m1,m2), fixed=TRUE)
+        grepl(sprintf("size of matrix: [%.0f != ", 2*M),  c(m3,m4), fixed=TRUE)
+        grepl(paste0("dims [product ", M, "] do not "),   c(m5,m6), fixed=TRUE)
     })
     rm(y)
 })
@@ -413,7 +413,7 @@ rm(r,x)
 
 ## rank(<largish) -- PR#18630
 vals <- 1:1475000000 # (immedate, thanks to ALTREP)
-system.time( ranks <- rank(vals) ) # takes too long for regular regr.tests
+system.time( ranks <- rank(vals) ) # ~ 130 sec
 head(ranks, 11)
 stopifnot(ranks[1:11] == 1:11, min(ranks) == 1)
 ## min(ranks) was -1073741824, in R <= 4.3.2
@@ -421,4 +421,4 @@ stopifnot(ranks[1:11] == 1:11, min(ranks) == 1)
 
 
 gc() # NB the "max used"
-proc.time() # total  [ ~ 40 minutes in full case, 2019-04-12]
+proc.time() # total  [ ~ 50 minutes in full case, 2024-01; was 40' in 2019-04-12]
