097a4bacff640312c0d972554f5f2c9fde585df5
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2024-02-09 09:16:02 +0000

    zapsmall() gets more flexible w/ `mFUN`, `min.d`
    git-svn-id: https://svn.r-project.org/R/trunk@85877 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/zapsmall.R
+++ src/library/base/R/zapsmall.R
@@ -1,7 +1,7 @@
 #  File src/library/base/R/zapsmall.R
 #  Part of the R package, https://www.R-project.org
 #
-#  Copyright (C) 1995-2018 The R Core Team
+#  Copyright (C) 1995-2024 The R Core Team
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -16,12 +16,15 @@
 #  A copy of the GNU General Public License is available at
 #  https://www.R-project.org/Licenses/
 
-zapsmall <- function(x, digits = getOption("digits"))
+
+zapsmall <- function(x, digits = getOption("digits"),
+                     mFUN = function(x, ina) max(abs(x[!ina])), min.d = 0L)
+    ## NB: keep in sync w/ implicit generic in ../../methods/R/makeBasicFunsList.R !
 {
     if (length(digits) == 0L)
         stop("invalid 'digits'")
     if (all(ina <- is.na(x)))
         return(x)
-    mx <- max(abs(x[!ina]))
-    round(x, digits = if(mx > 0) max(0L, digits - as.numeric(log10(mx))) else digits)
+    mx <- mFUN(x, ina)
+    round(x, digits = if(mx > 0) max(min.d, digits - as.numeric(log10(mx))) else digits)
 }
--- src/library/methods/R/makeBasicFunsList.R
+++ src/library/methods/R/makeBasicFunsList.R
@@ -1,7 +1,7 @@
 #  File src/library/methods/R/makeBasicFunsList.R
 #  Part of the R package, https://www.R-project.org
 #
-#  Copyright (C) 1995-2023 The R Core Team
+#  Copyright (C) 1995-2024 The R Core Team
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -286,6 +286,17 @@ utils::globalVariables(".addBasicGeneric")
 	       useAsDefault= function(x, ...) base::svd(x, ...),
 	       signature = "x", where = where)
     setGenericImplicit("svd", where, FALSE)
+
+
+    ## zapsmall(): signature  only  "x"
+    setGeneric("zapsmall", function(x, digits = getOption("digits"),
+                                    mFUN = function(x, ina) max(abs(x[!ina])), min.d = 0L, ...)
+        standardGeneric("zapsmall"),
+        useAsDefault = function(x, digits = getOption("digits"),
+                                mFUN = function(x, ina) max(abs(x[!ina])), min.d = 0L, ...)
+            base::zapsmall(x, digits=digits, mFUN=mFUN, min.d=min.d), # swallow '...'
+        signature = "x", where = where)
+    setGenericImplicit("zapsmall", where, FALSE)
 
 
     ## not implicitGeneric() which is not yet available "here"
