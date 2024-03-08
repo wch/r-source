2251b9da325eb59df87eb118798dda94b639ffaa
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2024-03-08 08:00:36 +0000

    extend the export stoplist: .__global__ etc
    git-svn-id: https://svn.r-project.org/R/trunk@86066 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/namespace.R
+++ src/library/base/R/namespace.R
@@ -5,7 +5,7 @@
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
+#  the Free Software Foundation; either version 3 of the License, or
 #  (at your option) any later version.
 #
 #  This program is distributed in the hope that it will be useful,
@@ -806,7 +806,8 @@ loadNamespace <- function (package, lib.loc = NULL,
         if (length(exports)) {
             stoplist <- c(".__NAMESPACE__.", ".__S3MethodsTable__.",
                           ".packageName", ".First.lib", ".onLoad",
-                          ".onAttach", ".conflicts.OK", ".noGenerics")
+                          ".onAttach", ".conflicts.OK", ".noGenerics",
+                          ".__global__", ".__suppressForeign__")
             exports <- exports[! exports %in% stoplist]
         }
 	if(lev > 2L) message("--- processing exports for ", dQuote(package))
