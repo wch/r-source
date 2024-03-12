82043584cd73aece45d38a90d5ce23acd38fbbe4
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2024-03-12 18:37:13 +0000

    Add list_code_files_in_package().
    git-svn-id: https://svn.r-project.org/R/trunk@86108 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/utils.R
+++ src/library/tools/R/utils.R
@@ -186,6 +186,31 @@ function(dir, type, all.files = FALSE, full.names = TRUE,
     files
 }
 
+### ** list_code_files_in_package
+
+list_code_files_in_package <-
+function(dir) {
+    dir <- normalizePath(dir)
+    code_dir <- file.path(dir, "R")
+    code_files <- list_files_with_type(code_dir, "code")
+    if(!length(code_files)) return(code_files)
+    meta <- .get_package_metadata(dir)
+    collate_fields <- c(paste0("Collate.", .OStype()), "Collate")
+    if(any(i <- (collate_fields %in% names(meta)))) {
+        collate <-
+            file.path(code_dir,
+                      .read_collate_field(meta[collate_fields[i][1L]]))
+        ## Note that matching code files and collate spec really only is
+        ## appropriate after having run configure as part of installing,
+        ## as this can create code files (e.g., from a .R.in code file).
+        ## Note also that using set ops is not appropriate here, as
+        ## these re-sort according to the current locale.
+        code_files <- collate[collate %in% code_files]
+    }
+    code_files
+}
+
+
 ### ** reQuote
 
 ## <FIXME>
