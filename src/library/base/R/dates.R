041ae4a55b841c811716e46b45def83af8b3ec21
smeyer <smeyer@00db46b3-68df-0310-9c12-caf00c1e9a41>
2024-03-14 22:52:26 +0000

    avoid tryFormats in format.Date()
    git-svn-id: https://svn.r-project.org/R/trunk@86130 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/dates.R
+++ src/library/base/R/dates.R
@@ -1,7 +1,7 @@
 #  File src/library/base/R/dates.R
 #  Part of the R package, https://www.R-project.org
 #
-#  Copyright (C) 1995-2022 The R Core Team
+#  Copyright (C) 1995-2024 The R Core Team
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -115,7 +115,8 @@ as.Date.default <- function(x, ...)
 ##                          deparse1(substitute(x)) ))
 ## }
 
-format.Date <- function(x, ...) format(as.POSIXlt(x), ...) # does keep names
+format.Date <- function(x, format = "%Y-%m-%d", ...)
+    format(as.POSIXlt(x), format = format, ...) # does keep names
 
 ## keep in sync with  print.POSIX?t()  in ./datetime.R
 print.Date <- function(x, max = NULL, ...)
