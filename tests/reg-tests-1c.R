7ca4b5e4352e726c522b3271651ab33d97ba2f20
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2024-03-26 18:01:43 +0000

    Add replications in timing test to reduce variability.
    git-svn-id: https://svn.r-project.org/R/trunk@86204 00db46b3-68df-0310-9c12-caf00c1e9a41

--- tests/reg-tests-1c.R
+++ tests/reg-tests-1c.R
@@ -54,9 +54,10 @@ stopifnot(!is.unsorted(NA))
 ## str(.) for large factors should be fast:
 u <- as.character(runif(1e5))
 dummy <- str(u); dummy <- str(u); # force compilation of str
-t1 <- max(0.001, system.time(str(u))[[1]]) # get a baseline > 0
+R <- 50
+t1 <- max(0.001, system.time(replicate(R, str(u))[[1]])) # get a baseline > 0
 uf <- factor(u)
-(t2 <- system.time(str(uf))[[1]]) / t1 # typically around 1--2
+(t2 <- system.time(replicate(R, str(uf)))[[1]]) / t1 # typically around 5--10
 stopifnot(t2  / t1 < 30)
 ## was around 600--850 for R <= 3.0.1
 
