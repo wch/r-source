34a0f412ecfb2d5716f2e4c231585683d0b9c2ee
kalibera <kalibera@00db46b3-68df-0310-9c12-caf00c1e9a41>
2024-04-04 10:13:02 +0000

    Improve portability of the test by using only valid characters where platforms agree on encodings.
    git-svn-id: https://svn.r-project.org/R/trunk@86330 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/grDevices/tests/encodings2.R
+++ src/library/grDevices/tests/encodings2.R
@@ -16,26 +16,6 @@ if (!l10n_info()[["UTF-8"]]) {
 }
 musl <- grepl("musl", R.version$os)
 
-do_one <-function(name, encoding, all = FALSE)
-{
-    par(pty="s")
-    plot(c(-1,16), c(-1,16), type="n", xlab="", ylab="", xaxs="i", yaxs="i")
-    title(paste("Centred chars in", name))
-    grid(17, 17, lty=1)
-    known <- if(all) c(32:126, 128:255) else c(32:126, 160:255)
-    x <- rawToChar(as.raw(known), TRUE)
-    z <- rep(NA_character_, 256)
-    z[known] <- iconv(x, encoding, "UTF-8")
-    ##print(z)
-
-    for(i in known) {
-        x <- i %% 16
-        y <- i %/% 16
-        points(x, y, pch = z[i])
-    }
-    par(pty="m")
-}
-
 pdfenc <- c("ISOLatin1", "ISOLatin2", "ISOLatin7", "ISOLatin9",
            "Greek", "Cyrillic", "KOI8-R", "KOI8-U",
            "WinAnsi", "CP1250", "CP1251", "CP1253", "CP1257")
@@ -49,6 +29,55 @@ if(musl) {
              "CP1252", "CP1250", "CP1251", "CP1253", "CP1257")
 }
 
+## only use valid printable characters from the encodings and exclude those
+## that have a diffent UTF-8 mapping on Windows (iso-8859-7, KOI8-R)
+##
+## invalid characters are sometimes converted to unexpected UTF-8 mappings on
+## Windows (rather than NA)
+valid_printable <- list(
+    c(32:126,160:255), # latin1
+    c(32:126,160:255), # latin2
+    c(32:126,160:255), # iso-8859-13
+    c(32:126,160:255), # latin-9
+    c(32:126,160,163,166:169,171:173,175:209,211:254), # iso-8859-7
+      # 161, 162 - Windows uses normal quotes, but Linux/macOS uses fancy
+      # 164, 165, 170 - 2003 additions not supported by Windows
+    c(32:126,160:255), # iso-8859-5
+    c(32:126,160:255), # KOI8-R
+    c(32:126,128:173,175:189,191:255), # KOI8-U
+      # 174, 190 - Windows uses characters from KOI8-RU (U+45e, U+40e),
+      #            which differs from Unix (R+255d, U+256c)
+    c(32:126,128,130:140,142,145:156,158:255), # CP1252
+    c(32:126,128,130,132:135,137:143,145:151,153:255), # CP1250
+    c(32:126,128:151,153:255), # CP1251
+    c(32:126,128,130:135,137,139,145:151,153,155,160:169,171:209,211:254), # CP1253
+    c(32:126,128,130,132:135,137,139,141:143,145:151,153,155,157:158,160,162:164,166:255) # CP1257
+)
+
+do_one <-function(ei)
+{
+    name <- pdfenc[ei]
+    encoding <- enc[ei]
+
+    par(pty="s")
+    plot(c(-1,16), c(-1,16), type="n", xlab="", ylab="", xaxs="i", yaxs="i")
+    title(paste("Centred chars in", name))
+    grid(17, 17, lty=1)
+    known <- valid_printable[[ei]]
+    x <- rawToChar(as.raw(known), TRUE)
+    z <- rep(NA_character_, 256)
+    z[known] <- iconv(x, encoding, "UTF-8")
+    ##print(z)
+
+    for(i in known) {
+        x <- i %% 16
+        y <- i %/% 16
+        points(x, y, pch = z[i])
+    }
+    par(pty="m")
+}
+
+
 ## Expect centring to fail for most chars in
 ## CP1253 KOI8-* Cyrillic CP1251 Greek CP1253
 ## as those chars are not in the Adobe afms.
@@ -61,7 +90,7 @@ for(i in seq_along(pdfenc)) {
                    compress = FALSE))
     if(!inherits(res, "try-error")) {
         ## and this may not work either
-        try(do_one(pdfenc[i], enc[i], i >= 7))
+        try(do_one(i))
         dev.off()
     }
 }
@@ -74,7 +103,7 @@ for(i in seq_along(pdfenc)) {
     res <- try(postscript(ff, encoding = pdfenc[i], width = 9, height = 9))
     if(!inherits(res, "try-error")) {
         ## and this may not work either
-        try(do_one(pdfenc[i], enc[i], i >= 7))
+        try(do_one(i))
         dev.off()
     }
 }
@@ -82,8 +111,8 @@ for(i in seq_along(pdfenc)) {
 if(!capabilities("cairo")) q("no")
 ## Now test cairo_pdf(): do not expect any messages
 cairo_pdf("cairo_pdf-encodings.pdf", width = 9, height = 9, onefile = TRUE)
-for (e in enc) {
-    message("testing ", e, " with cairo_pdf")
-    do_one(e, e, TRUE)
+for (i in seq_along(pdfenc)) {
+    message("testing ", pdfenc[i], " with cairo_pdf")
+    do_one(i)
 }
 dev.off()
