5d1bda1a61b240560f4fccc6405730e3e5943068
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2024-04-19 16:18:40 +0000

    add non-API functions in Utils.h
    git-svn-id: https://svn.r-project.org/R/trunk@86448 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/sotools.R
+++ src/library/tools/R/sotools.R
@@ -657,6 +657,8 @@ nonAPI <- c("chol_", "chol2inv_", "cg_", "ch_", "rg_",
 
 ## non-API and long deprecated in R_ext/RS.h (and as call_S in S.h)
             "call_R",
+## non-API, declared in Utils.h
+            "StringFalse", "StringTrue", "isBlankString",
 
 ## in the non-API header R_ext/Connections.h
             "R_new_custom_connection", "R_ReadConnection",
