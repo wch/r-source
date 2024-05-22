21ce99f232a7dd625a5af3d1b2921b5411363ca5
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2024-05-22 00:16:05 +0000

    Install info from WRE in rools package directory at build time.
    git-svn-id: https://svn.r-project.org/R/trunk@86581 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/apitools.R
+++ src/library/tools/R/apitools.R
@@ -28,7 +28,7 @@ apidata <-
     list2env(
         list(
             wrelines = NULL,
-            wreloc = "https://svn.r-project.org/R/trunk/doc/manual/R-exts.texi",
+            wreloc = NULL,
             fapi = NULL,
             rfuns = NULL))
 
@@ -41,8 +41,15 @@ resetAPI <- function(newloc = "") {
 }
 
 WRE <- function() {
-    if (is.null(apidata$wrelines))
+    if (is.null(apidata$wrelines)) {
+        if (is.null(apidata$wreloc)) {
+            apidata$wreloc <- system.file(package = "tools", "wre.txt")
+            if (apidata$wreloc == "")
+                apidata$wreloc <-
+                    "https://svn.r-project.org/R/trunk/doc/manual/R-exts.texi"
+        }
         apidata$wrelines <- readLines(apidata$wreloc)
+    }
     apidata$wrelines
 }
 
