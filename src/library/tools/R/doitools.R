20bedadbd57c51801709930a88648795f6056adc
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2024-06-03 13:45:06 +0000

    Add check_package_dois().
    git-svn-id: https://svn.r-project.org/R/trunk@86674 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/doitools.R
+++ src/library/tools/R/doitools.R
@@ -255,3 +255,10 @@ function(x, ...)
         writeLines(paste(format(x), collapse = "\n\n"))
     invisible(x)
 }
+
+check_package_dois <-
+function(dir, verbose = FALSE)
+{
+    db <- doi_db_from_package_sources(dir, Rd = TRUE)
+    check_doi_db(db, verbose = verbose, parallel = TRUE)
+}
