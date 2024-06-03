9abe16a78508c54f61523ad8748a83a0a5bce8d0
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2024-06-03 13:44:47 +0000

    Add check_package_urls().
    git-svn-id: https://svn.r-project.org/R/trunk@86673 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/urltools.R
+++ src/library/tools/R/urltools.R
@@ -914,3 +914,10 @@ function(x)
 .curl_handle_default_opts <-
     list(cookiesession = 1L,
          followlocation = 1L)
+
+check_package_urls <-
+function(dir, verbose = FALSE)
+{
+    db <- url_db_from_package_sources(dir)
+    check_url_db(db, verbose = verbose, parallel = TRUE)
+}
