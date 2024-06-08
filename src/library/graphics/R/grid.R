c9d650396b5c4072d01b83ecf740e168e8941d74
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2024-06-08 16:45:15 +0000

    graphics :: grid() gains `nintLog` option
    git-svn-id: https://svn.r-project.org/R/trunk@86706 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/graphics/R/grid.R
+++ src/library/graphics/R/grid.R
@@ -1,7 +1,7 @@
 #  File src/library/graphics/R/grid.R
 #  Part of the R package, https://www.R-project.org
 #
-#  Copyright (C) 1995-2023 The R Core Team
+#  Copyright (C) 1995-2024 The R Core Team
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -16,12 +16,12 @@
 #  A copy of the GNU General Public License is available at
 #  https://www.R-project.org/Licenses/
 
-.grid.at <- function(side, n, log, equilogs, axp, usr2) {
+.grid.at <- function(side, n, log, equilogs, axp, usr2, nintLog = NULL) {
     ## return 'at'
     if(is.null(n)) { ## align to tickmarks
         stopifnot(is.numeric(ax <- axp), length(ax) == 3L)
         if(log && equilogs && ax[3L] > 0) ax[3L] <- 1
-        axTicks(side, axp = ax, usr=usr2, log=log)
+        axTicks(side, axp = ax, usr=usr2, log=log, nintLog = nintLog)
     } else if(!is.na(n) && (n <- as.integer(n)) >= 1L) { # equidistant, also from box borders
         at <- seq.int(usr2[1L], usr2[2L], length.out = n+1L)
         (if(log) 10^at else at)[-c(1L,n+1L)]
@@ -29,13 +29,15 @@
 }
 
 grid <- function (nx = NULL, ny = nx, col="lightgray", lty="dotted",
-                  lwd = par("lwd"), equilogs = TRUE)
+                  lwd = par("lwd"), equilogs = TRUE, nintLog = NULL)
 {
     atx <- if(is.null(nx) || (!is.na(nx) && nx >= 1))
-	       .grid.at(1L, nx, log = par("xlog"), equilogs, axp = par("xaxp"), usr2 = par("usr")[1:2])
+	       .grid.at(1L, nx, log = par("xlog"), equilogs, axp = par("xaxp"), usr2 = par("usr")[1:2],
+                        nintLog = nintLog[1])
     ## else NULL
     aty <- if(is.null(ny) || (!is.na(ny) && ny >= 1))
-	       .grid.at(2L, ny, log = par("ylog"), equilogs, axp = par("yaxp"), usr2 = par("usr")[3:4])
+	       .grid.at(2L, ny, log = par("ylog"), equilogs, axp = par("yaxp"), usr2 = par("usr")[3:4],
+                        nintLog = nintLog[min(2L, length(nintLog))])
     abline(v = atx, h = aty, col = col, lty = lty, lwd = lwd)
     invisible(list(atx = atx, aty = aty))
 }
