b496233cab61e514c04e0f90130ddd313e7e04e2
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2024-07-02 15:43:38 +0000

    R CMD check --as-cran now notes Rd xrefs with missing package anchors.
    git-svn-id: https://svn.r-project.org/R/trunk@86861 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/check.R
+++ src/library/tools/R/check.R
@@ -7128,6 +7128,7 @@ add_dummies <- function(dir, Log)
         Sys.setenv("_R_CHECK_MBCS_CONVERSION_FAILURE_" = "TRUE")
         Sys.setenv("_R_CHECK_VALIDATE_UTF8_" = "TRUE")
         Sys.setenv("_R_CXX_USE_NO_REMAP_" = "TRUE")
+        Sys.setenv("_R_CHECK_XREFS_NOTE_MISSING_PACKAGE_ANCHORS_" = "TRUE")
         R_check_vc_dirs <- TRUE
         R_check_executables_exclusions <- FALSE
         R_check_doc_sizes2 <- TRUE
