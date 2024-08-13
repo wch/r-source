f3398fdafc63c904c84ff951c9a001bdb910bdc7
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2024-08-13 11:20:55 +0000

    comments on using from C++
    git-svn-id: https://svn.r-project.org/R/trunk@87004 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/include/R_ext/stats_stubs.h
+++ src/include/R_ext/stats_stubs.h
@@ -1,6 +1,6 @@
 /*
  *  R : A Computer Language for Statistical Data Analysis
- *  Copyright (C) 2007--2020  The R Core Team.
+ *  Copyright (C) 2007--2024  The R Core Team.
  *
  *  This header file is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as published by
@@ -21,7 +21,7 @@
  *  https://www.R-project.org/Licenses/
  */
 
-/* Not part of the API */
+/* Not part of the API, not callable from C++ */
 
 #include <Rconfig.h>
 #include <Rinternals.h>
