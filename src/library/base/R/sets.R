2beba10180407feff2180af65202b3aff629fe90
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2024-09-01 08:13:49 +0000

    Avoid as.vector() trafo for same-kind apparently vector-like operands.
    git-svn-id: https://svn.r-project.org/R/trunk@87083 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/sets.R
+++ src/library/base/R/sets.R
@@ -1,7 +1,7 @@
 #  File src/library/base/R/sets.R
 #  Part of the R package, https://www.R-project.org
 #
-#  Copyright (C) 1995-2021 The R Core Team
+#  Copyright (C) 1995-2024 The R Core Team
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -16,76 +16,110 @@
 #  A copy of the GNU General Public License is available at
 #  https://www.R-project.org/Licenses/
 
-union <- function(x, y) {
-    u <- as.vector(x)
-    v <- as.vector(y)
-    ## <FIXME>
-    ## Remove eventually: not safe enough for arbitrary classes.
-    ##   if(!is.object(x) || !is.object(y) ||
-    ##      !identical(class(x), class(y))) {
-    ##       x <- u
-    ##       y <- v
-    ##   }
-    ##   z <- c(x[!duplicated(unclass(u))],
-    ##          y[!duplicated(unclass(v)) & (match(v, u, 0L) == 0L)])
-    ##   names(z) <- NULL
-    ##   z
-    ## </FIXME>
-    ## Could do
-    ##   c(u[!duplicated(unclass(u))],
-    ##     v[!duplicated(unclass(v)) & (match(v, u, 0L) == 0L)])
-    ## but the following is faster and "basically the same":
-    unique(c(u, v))
+## <NOTE>
+## The set ops have always been documented to work for args that are
+## "same-kind" (same mode in the unclassed case) and sequences of items,
+## i.e., "vector-like".
+## In the "same-kind" case we test for vector-like whether subscripting
+## no items from x or y retains the class.
+## Where needed, we also check whether duplicated() on x or y has the
+## same length as x or y: for consistency this could always be done, at
+## a possible loss of efficiency where not needed.
+## </NOTE>
+
+union <-
+function(x, y)
+{
+    if(is.null(x)) return(y)
+    if(is.null(y)) return(x)
+    cx <- class(x)
+    cy <- class(y)
+    if((isa(x, cy) || isa(y, cx)) &&
+       identical(class(y0 <- y[integer()]), cy) &&
+       (length(dx <- duplicated(x)) == length(x)) &&
+       (length(dy <- duplicated(y)) == length(y))) {
+        if(!isa(x, cy))
+            x <- c(y0, x)
+    } else {
+        x <- as.vector(x)
+        y <- as.vector(y)
+        y0 <- y[integer()]
+        dx <- duplicated(x)
+        dy <- duplicated(y)
+    }
+    x <- x[!dx]
+    y <- y[!dy & (match(y, x, 0L) == 0L)]
+    c(x, y)
 }
 
-intersect <- function(x, y)
+intersect <-
+function(x, y)
 {
     if(is.null(x) || is.null(y))
         return(NULL)
-    u <- as.vector(x)
-    v <- as.vector(y)
-    ## <FIXME>
-    ## Remove eventually: not safe enough for arbitrary classes.
-    ##   if(!is.object(x) || !is.object(y) ||
-    ##      !identical(class(x), class(y))) {
-    ##       x <- u
-    ##       y <- v
-    ##   }
-    ##   z <- c(x[!duplicated(unclass(u)) & (match(u, v, 0L) > 0L)],
-    ##          y[numeric()])
-    ##   ## (Combining with y[numeric()] in the common class case is needed
-    ##   ## e.g. for factors to combine levels.)
-    ##   names(z) <- NULL
-    ##   z
-    ## </FIXME>
-    c(u[!duplicated(unclass(u)) & (match(u, v, 0L) > 0L)],
-      v[numeric()])
+    cx <- class(x)
+    cy <- class(y)    
+    if((isa(x, cy) || isa(y, cx)) &&
+       identical(class(y0 <- y[integer()]), cy) &&
+       (length(dx <- duplicated(x)) == length(x))) {
+        x <- x[!dx]
+    } else {
+        x <- as.vector(x)
+        x <- x[!duplicated(x)]
+        y <- as.vector(y)
+        y0 <- y[integer()]
+    }
+    ## Combining with y0 in the common class case is needed e.g. for
+    ## factors to combine levels, and otherwise to get a common mode.
+    c(x[match(x, y, 0L) > 0L], y0)
 }
 
-setdiff <- function(x, y)
+setdiff <-
+function(x, y)
 {
-    u <- as.vector(x)
-    v <- as.vector(y)
-    ## <FIXME>
-    ## Remove eventually: not safe enough for arbitrary classes.
-    ##   z <- x[!duplicated(unclass(u)) & (match(u, v, 0L) == 0L)]
-    ##   names(z) <- NULL
-    ##   z
-    ## </FIXME>
-    u[!duplicated(unclass(u)) & (match(u, v, 0L) == 0L)]
+    if(is.null(x) || is.null(y))
+        return(x)
+    cx <- class(x)
+    cy <- class(y)    
+    if((isa(x, cy) || isa(y, cx)) &&
+       identical(class(x[integer()]), cx) &&
+       (length(dx <- duplicated(x)) == length(x))) {
+        x <- x[!dx]
+    } else {
+        x <- as.vector(x)
+        x <- x[!duplicated(x)]
+        y <- as.vector(y)
+    }
+    x[match(x, y, 0L) == 0L]
 }
 
-## speed optimization etc: R-devel, Jan.4-6, 2000; then again 15 yrs later
-setequal <- function(x, y)
+setequal <-
+function(x, y)
 {
-    x <- as.vector(x)
-    y <- as.vector(y)
+    cx <- class(x)
+    cy <- class(y)    
+    if(!((isa(x, cy) || isa(y, cx)) &&
+         identical(class(x[integer()]), cx))) {
+        x <- as.vector(x)
+        y <- as.vector(y)
+    }
     !( anyNA(match(x, y)) || anyNA(match(y, x)) )
 }
 
-## same as %in% ( ./match.R ) but different arg names, and use match()
-## on as.vector() transformations for consistency with the other set
-## functions.
-is.element <- function(el, set)
-    match(as.vector(el), as.vector(set), 0L) > 0L
-
+## same as %in% ( ./match.R ) but different arg names (and possible
+## as.vactor() transformation in the case args are not vector-like
+## same-kind. 
+is.element <-
+function(el, set)
+{
+    x <- el
+    y <- set
+    cx <- class(x)
+    cy <- class(y)    
+    if(!((isa(x, cy) || isa(y, cx)) &&
+         identical(class(x[integer()]), cx))) {
+        x <- as.vector(x)
+        y <- as.vector(y)
+    }
+    match(x, y, 0L) > 0L
+}
