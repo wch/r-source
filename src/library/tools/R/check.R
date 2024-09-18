f5a9fc88e971940163b72955f3099faf6456a3f9
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2024-09-18 11:26:28 +0000

    Make code work as documented.
    git-svn-id: https://svn.r-project.org/R/trunk@87171 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/check.R
+++ src/library/tools/R/check.R
@@ -7237,6 +7237,7 @@ add_dummies <- function(dir, Log)
         Sys.setenv("_R_CHECK_VALIDATE_UTF8_" = "TRUE")
         Sys.setenv("_R_CXX_USE_NO_REMAP_" = "TRUE")
         Sys.setenv("_R_USE_STRICT_R_HEADERS_" = "TRUE")
+        Sys.setenv("_R_CHECK_S3_METHODS_SHOW_POSSIBLE_ISSUES_" = "TRUE")
         Sys.setenv("_R_CHECK_XREFS_NOTE_MISSING_PACKAGE_ANCHORS_" = "TRUE")
         R_check_vc_dirs <- TRUE
         R_check_executables_exclusions <- FALSE
