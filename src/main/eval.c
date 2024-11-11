ebf6511e0f63961042c39077e4e373266897f321
kalibera <kalibera@00db46b3-68df-0310-9c12-caf00c1e9a41>
2024-11-11 11:27:24 +0000

    Port 87298 from trunk.
    git-svn-id: https://svn.r-project.org/R/branches/R-4-4-branch@87318 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/eval.c
+++ src/main/eval.c
@@ -5851,12 +5851,15 @@ static R_INLINE SEXP getvar(SEXP symbol, SEXP rho,
 	    return PRVALUE(value);
 	else {
 	    /**** R_isMissing is inefficient */
-	    if (keepmiss && R_isMissing(symbol, rho))
-		return R_MissingArg;
-	    else {
-		forcePromise(value);
-		return PRVALUE(value);
+	    if (keepmiss) {
+		PROTECT(value);
+		Rboolean miss = R_isMissing(symbol, rho);
+		UNPROTECT(1);
+		if (miss)
+		    return R_MissingArg;
 	    }
+	    forcePromise(value);
+	    return PRVALUE(value);
 	}
     }
     else {
