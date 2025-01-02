4998dd1ffdccaf65008f592bb17149699a14ea14
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2025-01-02 17:26:24 +0000

    a bit more; incl current default normal RNG
    git-svn-id: https://svn.r-project.org/R/trunk@87515 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/nmath/standalone/test.c
+++ src/nmath/standalone/test.c
@@ -1,6 +1,6 @@
 /*
  *  Mathlib : A C Library of Special Functions
- *  Copyright (C) 2000-7  The R Core Team
+ *  Copyright (C) 2000--2025  The R Core Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -35,14 +35,20 @@ int
 main(int argc, char** argv)
 {
 /* something to force the library to be included */
-    qnorm(0.7, 0.0, 1.0, 0, 0);
     printf("*** loaded '%s'\n", argv[0]);
-    set_seed(123, 456);
+    double
+	dn = dnorm(0.7, 0., 1., 0),
+	qn = qnorm(0.7, 0., 1., 0, 0);
+    printf("dnorm(0.7) = %.12g\n", dn);
+    printf("qnorm(0.7) = %.12g\n", qn);
+    printf("pnorm(qnorm(..)) = %.12g\n", pnorm(qn, 0., 1., 0, 0));
+    // now _two_ of the  normal.kind :
     N01_kind = AHRENS_DIETER;
-    printf("one normal %f\n", norm_rand());
-    set_seed(123, 456);
+    set_seed(123, 456); printf("one normal %f\n", norm_rand());
     N01_kind = BOX_MULLER;
-    printf("normal via BM %f\n", norm_rand());
-    
+    set_seed(123, 456); printf("normal via Box_M %f\n", norm_rand());
+    N01_kind = INVERSION;
+    set_seed(123, 456); printf("normal via Inv.  %f\n", norm_rand());
+
     return 0;
 }
