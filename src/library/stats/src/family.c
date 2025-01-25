6852f6a9e9e52b82c030f7ed3aae8db4c6e497df
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2025-01-25 15:56:18 +0000

    ported r87546,53 [binomial$linkinv() & my.eta()] from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-4-4-branch@87631 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/src/family.c
+++ src/library/stats/src/family.c
@@ -1,6 +1,6 @@
 /*
  *  R : A Computer Language for Statistical Data Analysis
- *  Copyright (C) 2005-2022  The R Core Team
+ *  Copyright (C) 2005-2025  The R Core Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -59,51 +59,50 @@ static R_INLINE double x_d_opx(double x) {return x/(1 + x);}
 SEXP logit_link(SEXP mu)
 {
     int i, n = LENGTH(mu);
+    if (!n || !isReal(mu))
+	error(_("Argument %s must be a nonempty numeric vector"), "mu");
     SEXP ans = PROTECT(shallow_duplicate(mu));
     double *rans = REAL(ans), *rmu=REAL(mu);
 
-    if (!n || !isReal(mu))
-	error(_("Argument %s must be a nonempty numeric vector"), "mu");
     for (i = 0; i < n; i++)
-	rans[i] = log(x_d_omx(rmu[i]));
+	rans[i] = log(x_d_omx(rmu[i]));// log( x/(1-x) )
     UNPROTECT(1);
     return ans;
 }
 
 SEXP logit_linkinv(SEXP eta)
 {
+    int i, n = LENGTH(eta), nprot = 1;
+    if (!n || !isNumeric(eta))
+	error(_("Argument %s must be a nonempty numeric vector"), "eta");
+    if (!isReal(eta)) {eta = PROTECT(coerceVector(eta, REALSXP)); nprot++;}
     SEXP ans = PROTECT(shallow_duplicate(eta));
-    int i, n = LENGTH(eta);
     double *rans = REAL(ans), *reta = REAL(eta);
 
-    if (!n || !isReal(eta))
-	error(_("Argument %s must be a nonempty numeric vector"), "eta");
     for (i = 0; i < n; i++) {
 	double etai = reta[i], tmp;
 	tmp = (etai < MTHRESH) ? DBL_EPSILON :
 	    ((etai > THRESH) ? INVEPS : exp(etai));
 	rans[i] = x_d_opx(tmp);
     }
-    UNPROTECT(1);
+    UNPROTECT(nprot);
     return ans;
 }
 
 SEXP logit_mu_eta(SEXP eta)
 {
+    int i, n = LENGTH(eta), nprot = 1;
+    if (!n || !isNumeric(eta))
+	error(_("Argument %s must be a nonempty numeric vector"), "eta");
+    if (!isReal(eta)) {eta = PROTECT(coerceVector(eta, REALSXP)); nprot++;}
     SEXP ans = PROTECT(shallow_duplicate(eta));
-    int i, n = LENGTH(eta);
     double *rans = REAL(ans), *reta = REAL(eta);
 
-    if (!n || !isReal(eta))
-	error(_("Argument %s must be a nonempty numeric vector"), "eta");
     for (i = 0; i < n; i++) {
-	double etai = reta[i];
-	double opexp = 1 + exp(etai);
-
-	rans[i] = (etai > THRESH || etai < MTHRESH) ? DBL_EPSILON :
-	    exp(etai)/(opexp * opexp);
+	double etai = reta[i], expE = exp(etai), opexp = 1 + expE;
+	rans[i] = (etai > THRESH || etai < MTHRESH) ? DBL_EPSILON : expE/(opexp * opexp);
     }
-    UNPROTECT(1);
+    UNPROTECT(nprot);
     return ans;
 }
 
