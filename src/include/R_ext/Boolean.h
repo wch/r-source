f5bcb93071a53e42d29883ff2c18fcfe3959aafc
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2025-01-28 12:27:50 +0000

    add section on C23doc/manual/R-exts.texi
    git-svn-id: https://svn.r-project.org/R/trunk@87656 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/include/R_ext/Boolean.h
+++ src/include/R_ext/Boolean.h
@@ -1,6 +1,6 @@
 /*
  *  R : A Computer Language for Statistical Data Analysis
- *  Copyright (C) 2000, 2001 The R Core Team.
+ *  Copyright (C) 2000, 2025 The R Core Team.
  *
  *  This header file is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as published by
@@ -29,13 +29,56 @@
 #undef FALSE
 #undef TRUE
 
-#ifdef  __cplusplus
-extern "C" {
+/* This can eventually be simplified to
+#if (!defined __cplusplus) && !(defined __STDC_VERSION__ && __STDC_VERSION__ > 201710L)
+# include <stdbool.h>
 #endif
-typedef enum { FALSE = 0, TRUE /*, MAYBE */ } Rboolean;
 
 #ifdef  __cplusplus
+extern "C" {
+typedef bool Rboolean;
+#define FALSE false
+#define TRUE true
 }
+#define _R_RBOOLEAN_IS_BOOL_ 1
 #endif
+*/
+
+#ifdef  __cplusplus
+
+extern "C" {
+    /* once cp11 is soerted
+typedef bool Rboolean;
+#define FALSE false
+#define TRUE true
+    */
+typedef enum { FALSE = 0, TRUE } Rboolean;
+}
+
+# else
+
+// Also include standard C boolean type
+#if defined __STDC_VERSION__ && __STDC_VERSION__ > 201710L
+// C23 so these are keywords
+#else
+// stdbool.h is C99, so available everywhere
+//# include <stdbool.h>
+#endif
+
+/* 
+   __bool_true_false_are_defined is defined in stdbool.h, and C23, but
+   it and stdbool.h are declared obsolescent in C23.
+*/
+#ifdef __bool_true_false_are_defined
+typedef bool Rboolean;
+# define FALSE false
+# define TRUE true
+# define _R_RBOOLEAN_IS_BOOL_ 1
+#else
+typedef enum { FALSE = 0, TRUE } Rboolean;
+#endif //__bool_true_false_are_defined
+
+# endif // __cplusplus
+
 
 #endif /* R_EXT_BOOLEAN_H_ */
