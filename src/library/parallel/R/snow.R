20049bbfbcd8707f662fd0fc45718eef794346b7
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2025-02-20 19:56:34 +0000

    Add documentation chenges from PR18587.
    git-svn-id: https://svn.r-project.org/R/trunk@87768 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/parallel/R/snow.R
+++ src/library/parallel/R/snow.R
@@ -1,7 +1,7 @@
 #  File src/library/parallel/R/snow.R
 #  Part of the R package, https://www.R-project.org
 #
-#  Copyright (C) 1995-2021 The R Core Team
+#  Copyright (C) 1995-2025 The R Core Team
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
