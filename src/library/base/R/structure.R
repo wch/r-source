2b2453ba2e76eea2d8c28df28939ee71c0a88441
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2025-03-31 08:06:20 +0000

    structure(NULL, <n> = v) is defunct now (also illegal for primitives)
    git-svn-id: https://svn.r-project.org/R/trunk@88075 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/structure.R
+++ src/library/base/R/structure.R
@@ -1,7 +1,7 @@
 #  File src/library/base/R/structure.R
 #  Part of the R package, https://www.R-project.org
 #
-#  Copyright (C) 1995-2019 The R Core Team
+#  Copyright (C) 1995-2025 The R Core Team
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -21,8 +21,7 @@
 structure <- function (.Data, ...)
 {
     if(is.null(.Data))
-        warning("Calling 'structure(NULL, *)' is deprecated, as NULL cannot have attributes.\n  Consider 'structure(list(), *)' instead.")
-        ## to become: stop("attempt to set an attribute on NULL")
+        stop("attempt to set an attribute on NULL")
     attrib <- list(...)
     if(length(attrib)) {
         specials <- c(".Dim", ".Dimnames", ".Names", ".Tsp", ".Label")
