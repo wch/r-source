112fa06d05bd08c178d4daab8b1e8e92d4ddebee
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2025-04-21 15:00:09 +0000

    factanal() - sortLoadings() rotm adjustment
    git-svn-id: https://svn.r-project.org/R/trunk@88166 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/tests/factanal-tst.R
+++ src/library/stats/tests/factanal-tst.R
@@ -9,15 +9,20 @@ res <- factanal(covmat = ability.cov$cov, factors = 3, rotation = "none")
 if(FALSE) {
     ## create the output from oblimin of GPArotation as a list;
     require(GPArotation)
+    ## keep only what's needed from rotation:
+    keepImp <- function(rot)
+        `[<-`(rot, setdiff(names(rot), c("loadings", "Phi", "Th", "orthogonal")), NULL)
     set.seed(100)
-    obli100 <- oblimin(res$loadings, Tmat = Random.Start(3), eps = 1e-8, maxit = 10000)
-    ## keep only what's needed:
-    obli100[setdiff(names(obli100),c("loadings", "Phi", "Th", "orthogonal"))] <- NULL
+    obli100 <- keepImp(oblimin(res$loadings, Tmat = Random.Start(3), eps = 1e-8, maxit = 10000))
+    set.seed(108)
+    obli108 <- keepImp(oblimin(res$loadings, eps = 1e-7, randomStarts = 1))
     saveRDS(obli100, "obli100.rds")
+    saveRDS(obli108, "obli108.rds")
 }
 
 obli100 <- readRDS("obli100.rds") # in ../stats/tests/
-if(getRversion() >= "4.6") { ## perform the factanal using this oblimin rotation [ rotation now may be _function_
+## perform the factanal using this oblimin rotation
+if(getRversion() >= "4.5") { # rotation now may be _function_
   resO100 <- factanal(covmat = ability.cov$cov, factors = 3, rotation = \(...) obli100)
 } else { ## old R:
   obli_FN <- function(...) obli100
@@ -28,15 +33,27 @@ resO100 # print.factanal(..)  --> print.loadings(..)
 oriL <-   res     $ loadings
 ldns   <- resO100 $ loadings
 rotmat <- resO100 $ rotmat
-# no longer can the unrotated matrix be recreated. the max abs diff between
-# unrotated and recreated unrotated loadings should be near zero but is not:
+# max |diff| between unrotated and recreated unrotated loadings should be near zero:
 print.default(dev <- ldns %*% solve(rotmat) - oriL)
-stopifnot(max(abs(dev)) < 1e-9) # max|dev| was 2.000982 (R <= 4.4.x), now 2.22e-16 [Lnx x86_64, gcc]; 6e-12 on M1mac
+stopifnot(max(abs(dev)) < 1e-9) # max|dev| was 2.000982 (R <= 4.4.x),
+##                                now 2.22e-16 [Lnx x86_64, gcc]; 6e-12 on M1mac
 
 ldns # prints loadings too
 ## check the "SS loadings .... " line
 L <- ldns
 Phi <- attr(L, "covariance")
-(SSld <- diag(Phi %*% crossprod(L)))
-##      1.896492 1.124306 1.037336
+print(digits = 11, SSld <- diag(Phi %*% crossprod(L)))
 stopifnot(all.equal(c(1.8964924541, 1.1243062406, 1.0373362639), SSld)) # seen  8.49e-12
+
+##----------- Checking rotmat in the 108 case ------------------
+## [Bug 18886] New: R 4.5.0: rotmat not correctly updated  in factanal sortLoadings
+## 20 Apr 2025 / Reporter: coen.bernaards@gmail.com
+
+obli108 <- readRDS("obli108.rds")
+
+res108 <- factanal(covmat = ability.cov$cov, factors = 3, rotation = \(...) obli108)
+res108
+(Cov <- tcrossprod( solve(res108$rotmat) ))
+## this difference should be near 0 but was not:
+(Dcov <- attr(res108$loadings, "covariance")  -  Cov) # seen 3.33e-16, 4.44e-16 [Lnx x86_64, gcc]
+stopifnot(max(abs(Dcov)) < 1e-9)
