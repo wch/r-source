f9a01665fae911c57ba58bd3924920978618beb6
smeyer <smeyer@00db46b3-68df-0310-9c12-caf00c1e9a41>
2025-07-18 12:56:40 +0000

    use ...names()
    git-svn-id: https://svn.r-project.org/R/trunk@88431 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/methods/R/addedFunctions.R
+++ src/library/methods/R/addedFunctions.R
@@ -1,7 +1,7 @@
 #  File src/library/methods/R/addedFunctions.R
 #  Part of the R package, https://www.R-project.org
 #
-#  Copyright (C) 1995-2018 The R Core Team
+#  Copyright (C) 1995-2025 The R Core Team
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -138,8 +138,8 @@ hasArg <- function(name) {
         if(is.na(match("...", fnames)))
             FALSE
         else {
-            dotsCall <- eval(quote(substitute(list(...))), sys.parent())
-            !is.na(match(aname, names(dotsCall)))
+            dotsNames <- eval(quote(...names()), sys.parent())
+            !is.na(match(aname, dotsNames))
         }
     }
     else
