dd5e6631648bafdbb510bbb14a27fb7fd4840894
smeyer <smeyer@00db46b3-68df-0310-9c12-caf00c1e9a41>
2025-07-31 12:52:06 +0000

    tweaks
    git-svn-id: https://svn.r-project.org/R/trunk@88477 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/Rd2latex.R
+++ src/library/tools/R/Rd2latex.R
@@ -123,15 +123,10 @@ Rd2latex <- function(Rd, out = "", defines = .Platform$OS.type,
     sectionLevel <- 0
     hasFigures <- FALSE
 
-    startByte <- function(x) {
-    	srcref <- attr(x, "srcref")
-    	if (is.null(srcref)) -1L
-    	else srcref[2L]
-    }
-
-    addParaBreaks <- function(x, tag) {
-        if (isBlankLineRd(x)) "\n"
-        else if (startByte(x) == 1L) psub("^\\s+", "", x)
+    startByte <- utils:::getSrcByte
+    addParaBreaks <- function(x) {
+        ## this only removes indentation (for cleaner/smaller output)
+        if (startByte(x) == 1L) psub1("^[[:blank:]]+", "", x)
         else x
     }
 
@@ -367,7 +362,7 @@ Rd2latex <- function(Rd, out = "", defines = .Platform$OS.type,
                UNKNOWN =,
                VERB = of1(texify(block, TRUE)),
                RCODE = of1(texify(block, TRUE)),
-               TEXT = of1(addParaBreaks(texify(block), blocktag)),
+               TEXT = of1(addParaBreaks(texify(block))),
                USERMACRO =,
                "\\newcommand" =,
                "\\renewcommand" = {},
