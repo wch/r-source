90c8fb94489322b640a25ec5246b2de487e6559e
smeyer <smeyer@00db46b3-68df-0310-9c12-caf00c1e9a41>
2025-08-10 08:24:40 +0000

    tweaks, comments
    git-svn-id: https://svn.r-project.org/R/trunk@88554 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/admin.R
+++ src/library/tools/R/admin.R
@@ -826,31 +826,33 @@ function(dir) {
 
 ### * .install_package_Rd_objects
 
-## called from src/library/Makefile
+## called from src/library/Makefile and .install_packages
 .install_package_Rd_objects <-
 function(dir, outDir, encoding = "unknown")
 {
+    packageName <- basename(outDir)
     dir <- file_path_as_absolute(dir)
     mandir <- file.path(dir, "man")
     manfiles <- if(!dir.exists(mandir)) character()
-    else list_files_with_type(mandir, "docs")
+                else list_files_with_type(mandir, "docs")
     manOutDir <- file.path(outDir, "help")
     dir.create(manOutDir, FALSE)
-    db_file <- file.path(manOutDir,
-                         paste0(basename(outDir), ".rdx"))
+    db_file <- file.path(manOutDir, paste0(packageName, ".rdx"))
     built_file <- file.path(dir, "build", "partial.rdb")
-    macro_files <- list.files(file.path(dir, "man", "macros"), pattern = "\\.Rd$", full.names = TRUE)
+    macro_files <- list.files(file.path(dir, "man", "macros"),
+                              pattern = "\\.Rd$", full.names = TRUE)
     if (length(macro_files)) {
     	macroDir <- file.path(manOutDir, "macros")
     	dir.create(macroDir, FALSE)
     	file.copy(macro_files, macroDir, overwrite = TRUE)
     }
     ## Avoid (costly) rebuilding if not needed.
-    ## Actually, it seems no more costly than these tests, which it also does
+    ## Remaking Rdobjects of base packages takes 4s, but only 0.5s if skipped.
     pathsFile <- file.path(manOutDir, "paths.rds")
-    if(!file_test("-f", db_file) || !file.exists(pathsFile) ||
-       !identical(sort(manfiles), sort(readRDS(pathsFile))) ||
-       !all(file_test("-nt", db_file, manfiles))) {
+    upToDate <- file_test("-f", db_file) && file.exists(pathsFile) &&
+        identical(sort(manfiles), sort(readRDS(pathsFile))) &&
+        all(file_test("-nt", db_file, manfiles))
+    if(!upToDate) {
         db <- .build_Rd_db(dir, manfiles, db_file = db_file,
                            encoding = encoding, built_file = built_file)
         nm <- as.character(names(db)) # Might be NULL
@@ -858,7 +860,7 @@ function(dir, outDir, encoding = "unknown")
                           first = nchar(file.path(mandir)) + 2L),
                 pathsFile)
         names(db) <- sub("\\.[Rr]d$", "", basename(nm))
-        makeLazyLoadDB(db, file.path(manOutDir, basename(outDir)))
+        makeLazyLoadDB(db, file.path(manOutDir, packageName))
     }
     invisible()
 }
