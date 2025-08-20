d7f6290979d6d452ba5209d976b9b6f5e55b5c87
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2025-08-20 13:04:11 +0000

    note unused args to loadNamespace()
    git-svn-id: https://svn.r-project.org/R/trunk@88659 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/namespace.R
+++ src/library/base/R/namespace.R
@@ -207,7 +207,16 @@ loadNamespace <- function (package, lib.loc = NULL,
                               sQuote(package), current, zop, zversion),
                      domain = NA)
         }
-        ## silently ignore all other arguments.
+        ## used to silently ignore all other arguments. Still ignore 'lib.loc'
+        if(!missing(keep.source))
+            message(gettextf("namespace '%s' is already loaded so argument '%s' will bw ignored.",
+                             package, "keep.source"))
+        if(!missing(partial))
+            message(gettextf("namespace '%s' is already loaded so argument '%s' will bw ignored.",
+                             package, "partial"))
+        if(!missing(keep.parse.data))
+            message(gettextf("namespace '%s' is already loaded so argument '%s' will bw ignored/",
+                             package, "keep.parse.data"))
         ## return
         ns
     } else {
