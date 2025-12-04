e3fa2c0effb3ffbc4e0601ec980f7e867d7d1dbe
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2025-12-04 09:18:35 +0000

    FIXME done in diff.ts
    git-svn-id: https://svn.r-project.org/R/trunk@89092 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/stats/R/ts.R
+++ src/library/stats/R/ts.R
@@ -266,17 +266,9 @@ diff.ts <- function (x, lag = 1, differences = 1, ...)
         stop("bad value for 'lag' or 'differences'")
     if (lag * differences >= NROW(x))
         return(if(is.matrix(x)) x[0L, , drop = FALSE] else x[0L])
-    ## <FIXME>
-    ## lag() and its default method are defined in package ts, so we
-    ## need to provide our own implementation.
-    tsLag <- function(x, k = 1) {
-        p <- tsp(x)
-        tsp(x) <- p - (k/p[3L]) * c(1, 1, 0)
-        x
-    }
     r <- x
     for (i in 1L:differences) {
-        r <- r - tsLag(r, -lag)
+        r <- r - lag(r, -lag)
     }
     xtsp <- attr(x, "tsp")
     if(is.matrix(x)) colnames(r) <- colnames(x)
