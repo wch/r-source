2f46888beea157849b94cfe16708ad9e07aad211
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2025-12-15 09:17:02 +0000

    Improve URLs.
    git-svn-id: https://svn.r-project.org/R/trunk@89172 00db46b3-68df-0310-9c12-caf00c1e9a41

--- share/bibliographies/utils.R
+++ share/bibliographies/utils.R
@@ -23,7 +23,7 @@ bibentry(bibtype = "InProceedings",
                            family = "Rönz")),
          publisher = "Physica Verlag, Heidelberg",
          isbn = "3-7908-1517-9",
-         url = "https://doi.org/10.57938/9f6e8c91-0b5a-4353-9802-f20479f0f5e2")
+         doi = "10.1007/978-3-642-57489-4_89")
 
 bibentry(bibtype = "Article",
          key = "R:Hornik+Murdoch:2011",
