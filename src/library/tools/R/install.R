9e5b188f08abd0f27c243ba47d8dfcec289a510b
smeyer <smeyer@00db46b3-68df-0310-9c12-caf00c1e9a41>
2025-12-15 16:21:16 +0000

    port c89174 (avoid misleading 'sh: --version: not found' extra error messages) from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-4-5-branch@89175 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/install.R
+++ src/library/tools/R/install.R
@@ -2779,8 +2779,12 @@ if(FALSE) {
         system(paste(cmd, "-n"))
         res <- 0
     } else {
+        ## first report versions of involved compilers
         lines <- system(paste(MAKE, p1(paste("-f", shQuote(makefiles))),
                               "compilers"), intern = TRUE)
+        ## (unless make fails anyway, such as from syntax errors in makefiles)
+        if (is.null(attr(lines, "status"))) {
+
         if (with_c) {
             cc <- lines[grep("^CC =", lines)]
             cc <- sub("CC = ", "", cc)
@@ -2814,6 +2818,8 @@ if(FALSE) {
                         message("using C++", use_cxxstd)
                 }
             }
+        }
+
         }
         if (Sys.info()["sysname"] == "Darwin" &&
             (with_c|| with_f77 || with_f9x || with_cxx)) {
