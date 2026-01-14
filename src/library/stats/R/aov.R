7d23fab2478721fdef652efcb8c895007a1dd73c
Jan Gleixner <jan.gleixner@gmail.com>
2026-01-14 10:49:28 +0100

    Pass ... to coef in coef.listof

--- src/library/stats/R/aov.R
+++ src/library/stats/R/aov.R
@@ -533,7 +533,7 @@ print.summary.aovlist <- function(x, ...)
 coef.listof <- function(object, ...)
 {
     val <- setNames(vector("list", length(object)), names(object))
-    for(i in seq_along(object)) val[[i]] <- coef(object[[i]])
+    for(i in seq_along(object)) val[[i]] <- coef(object[[i]], ...)
     class(val) <- "listof"
     val
 }
