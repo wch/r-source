8f0973e31f87f6f994f17482ebb5bd24739bcd8d
smeyer <smeyer@00db46b3-68df-0310-9c12-caf00c1e9a41>
2026-03-02 17:32:28 +0000

    test HTML conversion of Rd bibtools
    git-svn-id: https://svn.r-project.org/R/trunk@89511 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/tests/bibtools.R
+++ src/library/tools/tests/bibtools.R
@@ -4,6 +4,13 @@ Rd2txt_options(underline_titles = FALSE)
 Rd2txt("bibtools.Rd", stages = "build") |>
     assertWarning(verbose = TRUE)
 
+## Also test for changes in HTML conversion
+Rd2HTML("bibtools.Rd", htmlfile <- tempfile(),
+        stages = "build", standalone = FALSE) |>
+    assertWarning() # the same
+## FIXME: carry-over of unshown citation (cache not cleared)
+Rdiff(htmlfile, "bibtools.html.save")
+
 ## Check cited but not shown
 pkgdir <- package.skeleton("bibtools", list = ".NotYetImplemented",
                            path = tempdir()) |> suppressMessages()
