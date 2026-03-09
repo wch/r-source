f5c7b2ae7b23baf82ce890e847f6f2e3f789012a
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2026-03-09 08:10:28 +0000

    Bump default BioC version to 3.23.
    git-svn-id: https://svn.r-project.org/R/trunk@89580 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/utils/R/packages.R
+++ src/library/utils/R/packages.R
@@ -1347,7 +1347,7 @@ function(repos, file = stdout(), ...)
 }
 
 ## default is included in setRepositories.Rd (via \Sexpr)
-.BioC_version_associated_with_R_version_default <- "3.22"
+.BioC_version_associated_with_R_version_default <- "3.23"
 .BioC_version_associated_with_R_version <- function ()
     numeric_version(Sys.getenv("R_BIOC_VERSION",
                                .BioC_version_associated_with_R_version_default))
