5cdee1a6034ec34ff90ea3add008fcc0c51dfc33
smeyer <smeyer@00db46b3-68df-0310-9c12-caf00c1e9a41>
2026-05-06 20:33:21 +0000

    amend r89981 for musl
    git-svn-id: https://svn.r-project.org/R/trunk@90009 00db46b3-68df-0310-9c12-caf00c1e9a41

--- tests/reg-tests-1e.R
+++ tests/reg-tests-1e.R
@@ -3232,7 +3232,7 @@ for(loc in c("C", if(onWindows) c("", "English_US.utf8") else "C.UTF-8")) {
         print(a1) # correctly "přst" *in* case
         print(hasUTF8 <- grepl("utf-?8$", print(Sys.getlocale("LC_CTYPE")), ignore.case=TRUE))
         stopifnot(identical(ch1, names(a1)),
-                  identical(c(112L, 345L, if(hasUTF8 || onWindows || grepl("macOS", osVersion))
+                  identical(c(112L, 345L, if(hasUTF8 || onWindows || grepl("macOS", osVersion) || grepl("musl", R.version$os))
                                                c(115L, 116L) # also: if(onWindows) 345L else 116L
                                           else c(345L, 353L)),
                             print(utf8ToInt(a1))))
