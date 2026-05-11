9012ff76a00fd6479320e219a4be9758c2d46954
smeyer <smeyer@00db46b3-68df-0310-9c12-caf00c1e9a41>
2026-05-11 18:07:22 +0000

    remove redundant options() change
    git-svn-id: https://svn.r-project.org/R/trunk@90044 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/tools/R/Rd2latex.R
+++ src/library/tools/R/Rd2latex.R
@@ -26,8 +26,7 @@
 function(x)
 {
     tf <- tempfile()
-    save <- options(useFancyQuotes = FALSE)
-    on.exit({options(save); unlink(tf)})
+    on.exit(unlink(tf))
     tryCatch(Rd2latex(x, tf, fragment = TRUE, outputEncoding = "UTF-8"),
              error = function(e) return(character()))
     enc2utf8(readLines(tf, warn = FALSE, encoding = "UTF-8"))
