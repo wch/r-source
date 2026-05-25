43872874355bf6bfd9ef835b9c7847f56d861bef
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2026-05-25 07:49:19 +0000

    port r89974 [abbreviate(<non-ASCII>)] from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-4-6-branch@90073 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/character.c
+++ src/main/character.c
@@ -1,6 +1,6 @@
 /*
  *  R : A Computer Language for Statistical Data Analysis
- *  Copyright (C) 1997--2025  The R Core Team
+ *  Copyright (C) 1997--2026  The R Core Team
  *  Copyright (C) 1995, 1996  Robert Gentleman and Ross Ihaka
  *
  *  This program is free software; you can redistribute it and/or modify
@@ -899,14 +899,14 @@ static SEXP wstripchars(const wchar_t * const inchar, int minlen, int usecl)
 	}
 
 	for (i = WUP; i > 0; i--) {
-	    if (islower((int)wc[i]) && LASTCHARW(i))
+	    if (iswlower((wint_t)wc[i]) && LASTCHARW(i))
 		mywcscpy(wc + i, wc + i + 1);
 	    if (wcslen(wc) - nspace <= minlen)
 		goto donewsc;
 	}
 
 	for (i = WUP; i > 0; i--) {
-	    if (islower((int)wc[i]) && !FIRSTCHARW(i))
+	    if (iswlower((wint_t)wc[i]) && !FIRSTCHARW(i))
 		mywcscpy(wc + i, wc + i + 1);
 	    if (wcslen(wc) - nspace <= minlen)
 		goto donewsc;
