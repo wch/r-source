debab7ce48438f3c9deba28bf16f0590d3aa524e
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2026-06-01 20:55:53 +0000

    port r90062 [zapsmall()] from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-4-6-branch@90092 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/zapsmall.R
+++ src/library/base/R/zapsmall.R
@@ -1,7 +1,7 @@
 #  File src/library/base/R/zapsmall.R
 #  Part of the R package, https://www.R-project.org
 #
-#  Copyright (C) 1995-2024 The R Core Team
+#  Copyright (C) 1995-2026 The R Core Team
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -21,10 +21,13 @@ zapsmall <- function(x, digits = getOption("digits"),
                      mFUN = function(x, ina) max(abs(x[!ina])), min.d = 0L)
     ## NB: keep in sync w/ implicit generic in ../../methods/R/makeBasicFunsList.R !
 {
-    if (length(digits) == 0L)
+    if (length(digits) == 0L) # may be > 1, recycled below
         stop("invalid 'digits'")
     if (all(ina <- is.na(x)))
         return(x)
     mx <- mFUN(x, ina)
-    round(x, digits = if(mx > 0) max(min.d, digits - as.numeric(log10(mx))) else digits)
+    if(is.infinite(mx) && any(is.infinite(digits)))
+        x
+    else
+        round(x, digits = if(mx > 0) max(min.d, digits - as.numeric(log10(mx))) else digits)
 }
