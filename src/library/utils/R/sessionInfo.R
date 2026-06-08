ce4e51f7349e37b7c196e7eff7cba274a9c9a244
ripley <ripley@00db46b3-68df-0310-9c12-caf00c1e9a41>
2026-06-08 18:36:21 +0000

    port r90119 from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-4-6-branch@90120 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/utils/R/sessionInfo.R
+++ src/library/utils/R/sessionInfo.R
@@ -1,7 +1,7 @@
 #  File src/library/utils/R/sessionInfo.R
 #  Part of the R package, https://www.R-project.org
 #
-#  Copyright (C) 1995-2025 The R Core Team
+#  Copyright (C) 1995-2026 The R Core Team
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -53,6 +53,8 @@
                    ver1 <- strsplit(ver, ".", fixed = TRUE)[[1L]]
                    ver2 <- ver1[2L]
                    if(ver1[1L] == "10")
+                       ## We bo longer support 10.x, bur user do
+                       ## report on very old versions.
                        sprintf("%s %s %s",
                                ifelse(as.numeric(ver2) < 12, "OS X", "macOS"),
                                switch(ver2,
@@ -72,7 +74,8 @@
                                       "16" = "Big Sur ...",
                                       ""),
                                ver)
-                   else if(ver1[1L] <= "15"  || ver1[1L]  == "26")
+                   else if(ver1[1L] <= "15"  ||
+                           (ver1[1L]  >= "26" && ver1[1L]  <= "27"))
                         sprintf("macOS %s %s",
                                switch(ver1[1L],
                                       "11" = "Big Sur",
@@ -80,8 +83,10 @@
                                       "13" = "Ventura",
                                       "14" = "Sonoma",
                                       "15" = "Sequoia",
-                                      "26" = "Tahoe"
-                                      ## if you add an entry here, change the numeric raange above.
+                                      "26" = "Tahoe",
+                                      "27" = "Golden Gate"
+                                      ## If you add an entry here, will need
+                                      ## to change the raange above.
                                       ),
                                ver)
                    else
