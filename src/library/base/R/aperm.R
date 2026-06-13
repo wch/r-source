b4b04614bbe865bf985305601bad8016220a9f0b
hornik <hornik@00db46b3-68df-0310-9c12-caf00c1e9a41>
2026-06-13 06:51:19 +0000

    Tweaks for c90136.
    git-svn-id: https://svn.r-project.org/R/trunk@90142 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/base/R/aperm.R
+++ src/library/base/R/aperm.R
@@ -24,6 +24,6 @@ aperm.default <- function (a, perm = NULL, resize = TRUE, ...)
 aperm.table <- function(a, perm = NULL, resize = TRUE, keep.class = TRUE, ...)
 {
      r <- aperm.default(a, perm, resize=resize)
-     if(keep.class) class(r) <- class(a)
+     if(!keep.class) class(r) <- NULL
      r
 }
