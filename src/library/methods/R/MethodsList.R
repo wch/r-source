053988897b83ab5f62bf3d09468a76d7c549fe05
smeyer <smeyer@00db46b3-68df-0310-9c12-caf00c1e9a41>
2026-08-03 11:49:25 +0000

    remove long unused (r26017) and never exported insertMethodInEmptyList()
    git-svn-id: https://svn.r-project.org/R/trunk@90344 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/library/methods/R/MethodsList.R
+++ src/library/methods/R/MethodsList.R
@@ -190,18 +190,6 @@ emptyMethodsList <- function(mlist, thisClass = "ANY", sublist = list()) {
     new("EmptyMethodsList", argument = mlist@argument, sublist = sublist)
 }
 
-insertMethodInEmptyList <- function(mlist, def) {
-    .MlistDefunct("insertMethodInEmptyList()")
-    value <- new("MethodsList", argument = mlist@argument)
-    sublist <- mlist@sublist
-    submethods <- sublist[[1L]]
-    if(is.null(submethods))
-        sublist[[1L]] <- def
-    else
-        sublist[[1L]] <- Recall(submethods, def)
-    value@allMethods <- sublist
-    value
-}
 
 
 
