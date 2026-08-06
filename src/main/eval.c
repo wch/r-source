48750c1118ea7a800ec01ec1853af10f96905255
luke <luke@00db46b3-68df-0310-9c12-caf00c1e9a41>
2026-08-06 16:41:21 +0000

    Fix prodect bug in byte code switch() handler (PR19118; patch from Kevin Ushey).
    git-svn-id: https://svn.r-project.org/R/trunk@90362 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/eval.c
+++ src/main/eval.c
@@ -8629,7 +8629,10 @@ static SEXP bcEval_loop(struct bcEval_locals *ploc)
        SEXP names = GETCONST(constants, GETOP());
        SEXP coffsets = GETCONST(constants, GETOP());
        SEXP ioffsets = GETCONST(constants, GETOP());
-       SEXP value = BCNPOP();
+       /* Leave the value on the node stack for the whole instruction: the
+	  node stack below R_BCNodeStackTop is the only thing rooting it, and
+	  warningcall() below allocates and can run R-level handlers. */
+       SEXP value = GETSTACK(-1);
        if (!isVector(value) || length(value) != 1)
 	   errorcall(call, _("EXPR must be a length 1 vector"));
        if (isFactor(value))
@@ -8676,6 +8679,7 @@ static SEXP bcEval_loop(struct bcEval_locals *ploc)
 	       warningcall(call, _("'switch' with no alternatives"));
 	   pc = codebase + INTEGER(ioffsets)[which];
        }
+       BCNPOP_IGNORE_VALUE();
        NEXT();
     }
     OP(RETURNJMP, 0): {
