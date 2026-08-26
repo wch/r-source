b88f92a3eeb2c958421a0848955423de69d01f34
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2026-08-26 08:56:18 +0000

    port r90446 [dim2total() - speed] from trunk
    git-svn-id: https://svn.r-project.org/R/branches/R-4-6-branch@90448 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/main/attrib.c
+++ src/main/attrib.c
@@ -1253,15 +1253,17 @@ SEXP dimgets(SEXP vec, SEXP val)
     UNPROTECT(1);
     PROTECT(val);
 
-    int ndim = length(val);
-    R_xlen_t total = dim2total(val, ndim, _("too many elements specified")),
-	len = xlength(vec);
+    bool err;
+    R_xlen_t total = dim2total(val, &err);
+    if(err) error(_("too many elements specified"));
+    R_xlen_t len = xlength(vec);
     if (total != len) {
 	error(_("dims [product %lld] do not match the length of object [%lld]"),
 	      (long long)total, (long long)len);
     }
 #if 0
 // currently it is documented that `dim<-` removes dimnames() .. but ..
+    int ndim = LENGTH(val);
     SEXP odim = getAttrib0(vec, R_DimSymbol); // keep dimnames(.) if dim() entries are unchanged
     if((LENGTH(odim) != ndim) || memcmp((void *)INTEGER(odim),
 					(void *)INTEGER(val), ndim * sizeof(int)))
