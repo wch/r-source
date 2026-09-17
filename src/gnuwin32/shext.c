e1e4c5d5c7d47d484ceba8fa827b24321b54cb29
maechler <maechler@00db46b3-68df-0310-9c12-caf00c1e9a41>
2026-09-17 16:20:03 +0000

    Windows: use "wide char" user home path, resulting in UTF-8 when ASCII is not ok
    git-svn-id: https://svn.r-project.org/R/trunk@90559 00db46b3-68df-0310-9c12-caf00c1e9a41

--- src/gnuwin32/shext.c
+++ src/gnuwin32/shext.c
@@ -33,12 +33,12 @@ static char *ShellGetPersonalDirectory(void)
     /* CSIDL_PERSONAL */
     if (SHGetKnownFolderPath(&FOLDERID_Documents, KF_FLAG_CREATE, NULL,
                              &wpath) == S_OK) {
-	size_t needed = wcstombs(NULL, wpath, 0);
-	if (needed != (size_t)-1) {
+	int needed = WideCharToMultiByte(CP_ACP, 0, wpath, -1, NULL, 0, NULL, NULL);
+	if (needed != -1) {
 	    result = (char *)malloc(needed + 1);
 	    if (result)
 		/* NOTE: some characters may not be representable */
-		wcstombs(result, wpath, needed + 1);
+		WideCharToMultiByte(CP_ACP, 0, wpath, -1, result, needed+1, NULL, NULL);
 	}
     }
     CoTaskMemFree(wpath);
